47ab23044f90f9d75b66d100ea79d885727dcdeb
Jonathan Matthew Brunt <jonbrunt@Jonathans-MacBook-Pro.local>
2018-02-26 13:46:43 -0600

    Index.js Setup for Redux
    add redux devtool syntax needed
    allow for future middleware
    render App inside Provider Tags in root hook

--- babylon/src/index.js
+++ babylon/src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import App from './components/App';
+import './index.css';
+import { rootReducer } from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) 
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+
+const store = createStore(rootReducer, enhancer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
