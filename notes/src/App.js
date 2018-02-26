47d3da67c0f83dd62ee6549d2df789b95e317c65
Jonathan <jonathanbry@comcast.net>
2018-02-26 13:21:06 -0800

    first commit - Jonathan Bry

---
+++ notes/src/App.js
@@ -0,0 +1,22 @@
+import { NotesDefaultSource } from './components/NotesDefaultSource';
+import React, { Component } from 'react';
+
+class App extends Component {
+
+    state = {
+        Title: 'Default Title',
+        NoteBody: 'What doesnt kill you makes you stronger',
+        ToggleEdit: false,
+        Delete: false
+    };
+    
+    render() {
+        return (
+            <div>
+           
+            </div>
+        )
+    }
+}
+
+export default App;
