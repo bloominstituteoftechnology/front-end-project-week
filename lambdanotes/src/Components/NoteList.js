d79a026b5a8522fe7d9f18ecf1d37ff8abc4a319
Tommy Coleman <tommycoleman87@gmail.com>
2018-02-26 13:20:57 -0500

    created notelist barebones component

---
+++ lambdanotes/src/Components/NoteList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class NoteList extends Component {
+    state = {
+        Notes: [],
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+export default NoteList;
