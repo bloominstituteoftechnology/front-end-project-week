d39378e36ea1625cc41dc71f61834f16b1d3141d
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-02-26 13:42:35 -0500

    Populated Initial State to display
    reducers/index.js --- Created 9 notes to display in the initial state.

--- lambda-notes/src/reducers/index.js
+++ lambda-notes/src/reducers/index.js
@@ -1,11 +1,56 @@
 import * as actions from '../actions';
 
 const initialState = {
-    notes = [],
+    notes = [
+        {
+            id: 0,
+            title: 'Yup',
+            body: "Wubba Lubba Dub-Dub is Rick's catchphrase, which he recurrently uses in the show, mostly in season one. He uses this phrase every time he's happy or makes a joke.",
+        },
+        {
+            id: 1,
+            title: 'Yup',
+            body: "Wubba Lubba Dub-Dub is Rick's catchphrase, which he recurrently uses in the show, mostly in season one. He uses this phrase every time he's happy or makes a joke.",
+        },
+        {
+            id: 2,
+            title: 'Yup',
+            body: "Wubba Lubba Dub-Dub is Rick's catchphrase, which he recurrently uses in the show, mostly in season one. He uses this phrase every time he's happy or makes a joke.",
+        },
+        {
+            id: 3,
+            title: 'Yup',
+            body: "Wubba Lubba Dub-Dub is Rick's catchphrase, which he recurrently uses in the show, mostly in season one. He uses this phrase every time he's happy or makes a joke.",
+        },
+        {
+            id: 4,
+            title: 'Yup',
+            body: "Wubba Lubba Dub-Dub is Rick's catchphrase, which he recurrently uses in the show, mostly in season one. He uses this phrase every time he's happy or makes a joke.",
+        },
+        {
+            id: 5,
+            title: 'Yup',
+            body: "Wubba Lubba Dub-Dub is Rick's catchphrase, which he recurrently uses in the show, mostly in season one. He uses this phrase every time he's happy or makes a joke.",
+        },
+        {
+            id: 6,
+            title: 'Yup',
+            body: "Wubba Lubba Dub-Dub is Rick's catchphrase, which he recurrently uses in the show, mostly in season one. He uses this phrase every time he's happy or makes a joke.",
+        },
+        {
+            id: 7,
+            title: 'Yup',
+            body: "Wubba Lubba Dub-Dub is Rick's catchphrase, which he recurrently uses in the show, mostly in season one. He uses this phrase every time he's happy or makes a joke.",
+        },
+        {
+            id: 8,
+            title: 'Yup',
+            body: "Wubba Lubba Dub-Dub is Rick's catchphrase, which he recurrently uses in the show, mostly in season one. He uses this phrase every time he's happy or makes a joke.",
+        },
+    ],
 }
 
 export default (state = initialState, action) => {
-  console.log('Action: ', action);
   switch (action.type) {
     default:
       return state;
