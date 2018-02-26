d45cb2348baa536ed0e9a56a1597bed0443f9da4
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-02-26 17:22:41 -0500

    Created dummy-data.js and populated with dummy data

--- lambdanotes/src/components/notes-list.js
+++ lambdanotes/src/components/notes-list.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class NotesList extends Component {
   render() {
     return (
-      <div className="NotesList-container" style={{ border: "2px solid blue", width: "100%" }}>
+      <div className="NotesList-container" style={{ border: "3px solid blue", width: "100%", padding: "10px" }}>
         <h1>Your Notes:</h1>
         Notes will appear here
       </div>
--- lambdanotes/src/components/sidebar.js
+++ lambdanotes/src/components/sidebar.js
@@ -8,7 +8,7 @@ import styled from 'styled-components';
 
 const Sidebar = (props) => {
   return(
-    <div className="Sidebar-container" style={{ display: "flex", flexDirection: "column", width: "200px", border: "2px solid red" }} >
+    <div className="Sidebar-container" style={{ display: "flex", flexDirection: "column", width: "200px", border: "3px solid red", padding: "10px" }} >
       <h1>Lambda Notes</h1>
       <button>View Your Notes</button>
       <button>+ Create New Notes</button>
