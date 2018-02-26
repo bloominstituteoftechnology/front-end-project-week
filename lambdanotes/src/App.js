f36d09948e212093c49f5db840adf233b7429f60
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-02-26 15:05:22 -0500

    Added basic sidebar layout

--- lambdanotes/src/App.js
+++ lambdanotes/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Sidebar from './components/sidebar';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <div>Sidebar</div>
+      <div className="App-container" style={{ display: "flex", flexFlow: "row no-wrap", border: "2px solid black", height: "700px"}}>
+        <Sidebar />
         <div>Notes List</div>
       </div>
     );
---
+++ lambdanotes/src/components/sidebar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+// const styles = {
+//   display: "flex",
+//   flex-direction: "column",
+// }
+
+const Sidebar = (props) => {
+  return(
+    <div className="Sidebar-container" style={{ display: "flex", flexDirection: "column", width: "200px", border: "2px solid red" }} >
+      <h1>Lambda Notes</h1>
+      <button>View Your Notes</button>
+      <button>+ Create New Notes</button>
+    </div>
+  )
+}
+
+export default Sidebar;
