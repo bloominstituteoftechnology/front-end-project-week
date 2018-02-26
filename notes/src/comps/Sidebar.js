9abc0da06b2bc5d8dda94e8e9fe1a689251df162
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-26 16:45:37 -0600

    Add sidebar styling and reset
    Sidebar.js: Lines 8-9 changed the buttons' classes to match

--- notes/src/comps/Sidebar.js
+++ notes/src/comps/Sidebar.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import '../styles/Sidebar.css';
 
 const Sidebar = props => {
   return (
     <div className="sidebar">
       <span className="sidebar__title">Lambda Notes</span>
       <div className="sidebar__buttons">
-        <button className="sidebar__buttons-view">View Your Notes</button>
-        <button className="sidebar__buttons-create">+ Create New Note</button>
+        <button className="sidebar__button">View Your Notes</button>
+        <button className="sidebar__button">+ Create New Note</button>
       </div>
     </div>
   );
--- notes/src/index.js
+++ notes/src/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import './styles/reset.css';
 
 // Reducer
 import reducer from './reducer';
