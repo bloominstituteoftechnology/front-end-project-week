c0924b2809ded1a3a2516b09932e3d139d7cdda0
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-02-26 18:42:44 -0500

    Started to add a bit of styling
    Added some styling here and there to better visualize how
    everything might look in the end.
    
    Functionality still not complete.
    
    Added some LESS files to compile their respective CSS files.

--- lambda_notes/src/components/CreateNewNote/CreateNewNote.js
+++ lambda_notes/src/components/CreateNewNote/CreateNewNote.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import Sidebar from '../Sidebar/Sidebar';
 import { Link } from 'react-router-dom';
+import './CreateNewNote.css';
 
 const CreateNewNote = () => {
   return (
-    <div className="NewNote">
-      <h2>Create New Note: </h2>
-      <input type="text" placeholder="Note Title"/>
-      <textarea rows="20" cols="50" placeholder="Note Content">
-      </textarea>
-      <Link to="/"><button>Save</button></Link>
+    <div class="Container">
+      <div className="Sidebar">
+        <Sidebar />
+      </div>
+      <div className="New-Note">
+        <h2>Create New Note: </h2>
+        <input type="text" placeholder="Note Title"/>
+        <textarea rows="20" cols="50" placeholder="Note Content">
+        </textarea>
+        <Link to="/"><button>Save</button></Link>
+      </div>
     </div>
   )
 }
