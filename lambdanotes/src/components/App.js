e9a3412e5320feabd5b2c9dbb37d8cf218297444
Mephestys <mephestys@gmail.com>
2018-02-26 13:46:30 -0700

    Changes in this commit: - Added 'react-router-dom' for easier routing implementation. - Sidebar and NoteList rendered by App using react-router-dom - Sidebar displays "Lambda Notes" title and two buttons - NoteList simply displays the words "NoteList"

--- lambdanotes/src/components/App.js
+++ lambdanotes/src/components/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
+import Sidebar from './Sidebar';
+import NoteList from './NoteList';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>LambdaNotes</h1>
-      </div>
+      <Router>
+        <div>
+          <Sidebar />
+          <Route path="/" component={NoteList} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ lambdanotes/src/components/NoteList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class NoteList extends React.Component {
+  state = {};
+
+  render() {
+    return (
+      <h1>NoteList</h1>
+    );
+  }
+}
