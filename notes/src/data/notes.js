74ac7c65ce445f69ac355ac9216283d09e61f617
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-26 17:25:09 -0600

    add id to note data

--- notes/src/data/notes.js
+++ notes/src/data/notes.js
@@ -3,21 +3,25 @@ const notes = [
     title: 'Note 1',
     content:
       "Now we'll take the almighty fan brush. Just relax and let it flow. That easy. Trees cover up a multitude of sins. The very fact that you're aware of suffering is enough reason to be overjoyed that you're alive and can experience it. Let all these little things happen. Don't fight them. Learn to use them. If we're gonna walk though the woods, we need a little path.",
+    id: 1,
   },
   {
     title: 'Note 2',
     content:
       "We'll lay all these little funky little things in there. We have all at one time or another mixed some mud. The more we do this - the more it will do good things to our heart. Maybe there's a happy little bush that lives right there. Just think about these things in your mind - then bring them into your world.",
+    id: 2,
   },
   {
     title: 'Note 3',
     content:
       'All you need to paint is a few tools, a little instruction, and a vision in your mind. See there, told you that would be easy. In your imagination you can go anywhere you want. The only thing worse than yellow snow is green snow.',
+    id: 3,
   },
   {
     title: 'Note 4',
     content:
       'Get away from those little Christmas tree things we used to make in school. All you have to learn here is how to have fun. And right there you got an almighty cloud. It is a lot of fun. Nature is so fantastic, enjoy it. Let it make you happy.',
+    id: 4,
   },
 ];
 
