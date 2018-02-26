d257fb50037cc14f015250101e92eab8a9667962
Peter Gray <peter@petergraycreative.com>
2018-02-26 12:52:10 -0600

    Add logger to index.js
    Built out index.js with Redux provider component

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -4,6 +4,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
