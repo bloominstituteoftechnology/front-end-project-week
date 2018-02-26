4304f87b1e225cab9f6993386ab6e19b4bbb93fa
Smithface <kylewibblesmith@gmail.com>
2018-02-26 17:57:50 -0500

    added form to CreateNote.js

--- lambda_notes/src/components/mainpage/CreateNote.js
+++ lambda_notes/src/components/mainpage/CreateNote.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import './createnote.css';
 
 const CreateNote = () => {
-  return null;
+  return (
+    <div>
+      <form>
+        <input className="note__title" type="text"/>
+        <textarea cols="60" rows="10"></textarea>
+        <button className="note__button">Save</button>
+      </form>
+    </div>
+  );
 }
 
 export default CreateNote;
