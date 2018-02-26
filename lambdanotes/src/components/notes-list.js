ea0b8e37bd7c4827b1f2a8e73fd27a58cf93e2c2
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-02-26 17:49:57 -0500

    Imported dummyData to notes-lists' state and got it rendering the titles

--- lambdanotes/src/components/notes-list.js
+++ lambdanotes/src/components/notes-list.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import dummyData from '../dummy-data';
 
 class NotesList extends Component {
+  state = {
+    notes: [...dummyData],
+    test: true,
+  }
+
+  componentDidMount() {
+    console.log(this.state);
+  }
+
   render() {
     return (
       <div className="NotesList-container" style={{ border: "3px solid blue", width: "100%", padding: "10px" }}>
         <h1>Your Notes:</h1>
         Notes will appear here
+        {this.state.notes.map((note) => {
+          return <div key={note.id}>{note.title}</div>
+        })}
       </div>
     )
   }
