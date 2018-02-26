59b644f3ac009073267fb293142b28a78cc18e85
itsdevinci <devinbaldwin315@gmail.com>
2018-02-26 12:32:07 -0800

    Create Now Note (Page Progress)

--- lambda_notes/src/App.js
+++ lambda_notes/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Menu">
+          <h1 id="title">Lambda<br/> Notes</h1>
+          <button><b>View Your Notes</b></button><br/>
+          <button><b>+ Create New Note</b></button>
+        </div>
+        <div className="Notes">
+          <h1>Create New Note:</h1>
+            
+        </div>
+      
       </div>
     );
   }
