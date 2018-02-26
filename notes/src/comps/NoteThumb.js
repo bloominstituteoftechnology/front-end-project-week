1d49b952b3fb6ebc4cbd94f92721c28dbe9ee4e2
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-26 17:30:06 -0600

    set max characters for content shown in NoteThumb to 140

--- notes/src/comps/NoteThumb.js
+++ notes/src/comps/NoteThumb.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 // props.note is a note object with title and content
 const NoteThumb = props => {
+  const charsVisible =
+    props.note.content.length > 140 ? 140 : props.note.content.length;
   return (
     <div className="noteThumb__container">
       <h3>{props.note.title}</h3>
       <hr />
-      <p>{props.note.content}</p>
+      <p>{props.note.content.slice(0, charsVisible)}..</p>
     </div>
   );
 };
