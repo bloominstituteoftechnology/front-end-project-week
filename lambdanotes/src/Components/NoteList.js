7fa69c0594bfd5b70cb3e2990687ab5977eec35d
Tommy Coleman <tommycoleman87@gmail.com>
2018-02-26 13:31:44 -0500

    made NoteList functional, added stateand mapped over state to render notes

--- lambdanotes/src/Components/NoteList.js
+++ lambdanotes/src/Components/NoteList.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 
 class NoteList extends Component {
     state = {
-        Notes: [],
+        notes: [
+            {
+                note: 'Create App',
+                details: 'Get app up and working',
+            }
+        ],
     }
 
     render() {
         return (
             <div>
+                <ul>
+                    {this.state.notes.map(note => {
+                        return (
+                            <li>{note.note}
+                            <p>{note.details}</p>
+                            </li>
+                        )
+                    })}
+                </ul>
             </div>
         )
     }
