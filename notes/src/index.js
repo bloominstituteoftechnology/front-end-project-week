5bfede9ea238afef3f7b8a1f16d30f160693df0a
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-26 15:19:38 -0600

    refactor for redux and install react-router
    src/index.js:
    - lines 3-4: import Provider and createStore for redux
    - line 5: import Router functionality (for future use)
    -line 8: import the reducer to make the redux store
    - lines 14-20: create the redux store and enable redux devtools
    
    reducer/index.js:
    - create the reducer with dummy data for the initial state
    - no actions to import yet

--- notes/src/index.js
+++ notes/src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+// Reducer
+import reducer from './reducer';
 
 // List of Components
 import App from './App';
 
-render(<App />, document.getElementById('root'));
+render(
+  <Provider
+    store={createStore(
+      reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
