a36771b3e4b342a595c5f34118c87e3b85a2445b
Smithface <kylewibblesmith@gmail.com>
2018-02-26 14:01:09 -0500

    added skeleton of components

--- lambda_notes/src/App.js
+++ lambda_notes/src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import NavBar from './components/NavBar';
+import MainPage from './components/MainPage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div>
+          <NavBar />
+        </div>
+        <div>
+          <MainPage />
+        </div>
       </div>
     );
   }
---
+++ lambda_notes/src/components/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+  state = {
+
+  };
+
+  render() {
+    return null;
+  }
+}
+
+export default MainPage;
