00e9a3e41291236d1c48f2bc30e3f636b126087d
ashleijo <shuryjo@gmail.com>
2018-02-26 18:30:38 -0500

    creating a component for the left panel which holds two button

--- lambdanotes/src/App.js
+++ lambdanotes/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './css/App.css';
 import dummyData from './dummy-data';
-import NoteContainer from "./components/NoteContainer/NoteContainer";
-
+import NoteContainer from './components/NoteContainer/NoteContainer';
+import Panel from './components/Panel/Panel';
 class App extends Component {
 
   constructor() {
@@ -15,11 +15,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+          <Panel />
+        <div className="Notes">
           {this.state.notes.map((note, i) => {
             return (
               <NoteContainer key={i} noteTitle={note.noteTitle} noteContent={note.noteContent} />
             );
           })}
+        </div>
       </div>
     );
   }
---
+++ lambdanotes/src/components/Panel/Panel.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const Panel = (props) => {
+  return (
+    <div className="Panel">
+      <p>Lambda Notes</p>
+      <button>View Your Notes</button>
+      <button>+ Create New Note</button>
+    </div>
+  );
+};
+
+export default Panel;
