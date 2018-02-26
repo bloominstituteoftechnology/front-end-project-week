2f7cd014cab4dcd0a85313b5c41b8f46589c5e59
Peter Gray <peter@petergraycreative.com>
2018-02-26 11:47:50 -0600

    Peter Gray - Initial Commit - WIP

---
+++ lambda-notes/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import thunk from 'redux-thunk';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(thunk, logger)
+));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
