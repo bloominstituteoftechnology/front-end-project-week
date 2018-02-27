92cde345fed6565e5cc23f7879924a9b0f04fd3d
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-26 23:46:57 -0600

    change max character length to 120
    fixes the issue where text was going outside the div

--- notes/src/comps/NoteThumb.js
+++ notes/src/comps/NoteThumb.js
@@ -4,7 +4,7 @@ import '../styles/NoteThumb.css';
 // props.note is a note object with title and content
 const NoteThumb = props => {
   const charsVisible =
-    props.note.content.length > 140 ? 140 : props.note.content.length;
+    props.note.content.length > 120 ? 120 : props.note.content.length;
   return (
     <div className="noteThumb__container">
       <h3 className="noteThumb__title">{props.note.title}</h3>
