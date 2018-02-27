1e3f891b2a511eb12d6a81126807b6ac057cdd4f
Michael Marshal <mikemarshalman@gmail.com>
2018-02-26 20:04:32 -0600

    Home Page left side styling/html complete

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -6,8 +6,20 @@ class App extends Component {
     return (
       <div className="App">
         <div className="Home__Left">
+          <div className="Home__Left__Text">
+            <b>Lambda</b>
+            <br/>
+            <b>Notes</b>
+          </div>
+          <div className="button Home__NoteButton">
+            <b>View Your Notes</b>
+          </div>
+          <div className="button Home__NoteButton">
+            <b>+ Create New Note</b>
+          </div>
         </div>
         <div className="Home__Right">
+        Right
         </div>
       </div>
     );
