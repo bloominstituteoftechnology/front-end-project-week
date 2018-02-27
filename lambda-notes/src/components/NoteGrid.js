5567f20fd6667e2462e6d2e7e1d5fa214725568e
Peter Gray <peter@petergraycreative.com>
2018-02-26 18:57:32 -0600

    Built out presentational NoteGrid component
    Used Bootstrap v4 flexbox grid

---
+++ lambda-notes/src/components/NoteGrid.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './NoteGrid.css';
+
+function NoteGrid(props) {
+  return (
+    <div className="col-9 main-content container align-items-center">
+      <div className="row justify-content-between">
+        {props.notes.map(note => {
+          return (
+            <div key={note.id} className="col-md-4 ">
+              <div className="note">
+              <h3>{note.title}</h3>
+              <hr />
+              <p>{note.body}</p>
+            </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+export default NoteGrid;
