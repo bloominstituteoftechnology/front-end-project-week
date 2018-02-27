862f21c5b1a4ea8f6a05e2c5fc992ea6896b2a38
Mike Streltsoff <MStreltsoff@gmail.com>
2018-02-26 19:51:35 -0500

    slow progress

---
+++ myproject/src/components/MainNote.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './MainNote.css'
+
+
+class MainNote extends Component {
+    render() {
+      return (
+        <div>
+          <div className="MainNoteContainer">
+    
+        <div className="main-nav" >
+         Lambda Notes
+        </div>
+
+
+        <div className="main-content">
+        Main content: first in source order
+        </div>
+      
+      
+      
+    
+          </div>
+                  </div>
+      );
+    }
+  }
+  
+  export default MainNote;
