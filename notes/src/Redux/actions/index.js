acf5a04972cf54468aeea07a9d327edd88f3c1b4
Eric <erichech@gmail.com>
2018-02-27 16:40:35 -0500

    Got delete working, with it's css.

--- notes/src/Redux/actions/index.js
+++ notes/src/Redux/actions/index.js
@@ -1,4 +1,5 @@
 export const ADD_NOTE = 'ADD_NOTE';
+export const DELETE_NOTE = 'DELETE_NOTE';
 
 export const addNote = newNote => {
   return {
@@ -6,3 +7,10 @@ export const addNote = newNote => {
     payload: newNote,
   };
 };
+
+export const deleteNote = oldNote => {
+  return {
+    type: DELETE_NOTE,
+    payload: oldNote.id,
+  }
+}
--- notes/src/SingleNote/SingleNote.js
+++ notes/src/SingleNote/SingleNote.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { deleteNote } from '../Redux/actions';
 import './SingleNote.css';
 
 class SingleNote extends Component {
-  render() {
+  state = {
+    note: {},
+  }
+
+  componentDidMount() {
     const id = Number(this.props.match.params.id);
-    const note = this.props.notes.filter(each => each.id === id)[0];
+    const thisNote = this.props.notes.filter(each => each.id === id)[0];
+    this.setState({ note: thisNote });
+  }
+
+  deleteNote = () => {
+    this.props.deleteNote(this.state.note);
+  }
+
+  render() {
     return (
       <div className="single-note_body">
         <div className="edit-delete_links">
           <div className="links">
-            <div className='each-link'>edit</div>
-            <div className='each-link'>delete</div>
+            <div className="each-link">edit</div>
+            <Link to="/" className="each-link" onClick={this.deleteNote}>
+              <div>delete</div>
+            </Link>
           </div>
         </div>
-        <div className="single-note_header">{note.title}</div>
-        <div className="single-note_text">{note.body}</div>
+        <div className="single-note_header">{this.state.note.title}</div>
+        <div className="single-note_text">{this.state.note.body}</div>
       </div>
     );
   }
@@ -28,4 +43,4 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, null)(SingleNote);
+export default connect(mapStateToProps, { deleteNote })(SingleNote);
