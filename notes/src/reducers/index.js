6c069dc363a50d6fd5128cba2536de344edecd14
Ronnie Miksch <fakepreferences@gmail.com>
2018-02-27 13:35:49 -0800

    Added 9 dummy notes and forced to 3 columns unless screen is too small

--- notes/src/reducers/index.js
+++ notes/src/reducers/index.js
@@ -1,14 +1,49 @@
 const initialTestState = {
       notes: [
         {
-          title: 'National',
-          entry: 'Quasi ut accusamus rem.',
-          id: 'FDH352',
+          title: 'Note Title',
+          entry: 'Sit totam a et omnis officia. Laboriosam sed quos dicta. Sunt deleniti occaecati blanditiis animi nihil reiciendis. Aut incidunt aut atque omnis nam quam occaecati corrupti saepe. Nostrum ex quis. Ut voluptatem at omnis et. Molestias a id eos porro voluptas qui ipsum perferendis. Sed provident consequatur facere. Sed quidem tenetur esse aut ut. Sunt quam hic. Magnam aut nobis. Suscipit nulla quos a in voluptas minus qui qui sit. Saepe fugiat quidem cupiditate ullam quidem sunt est.',
+          id: 'JNEI45FO7160593922030378',
         },
         {
-          title: 'Investor',
+          title: 'Note Title',
+          entry: 'Autem inventore quisquam est quia rerum quaerat rem ratione ea. Quo ut beatae nisi eligendi minima sed non eaque et. Similique inventore suscipit quas delectus omnis est qui. Beatae corrupti sequi. Praesentium ipsa et rem quis excepturi earum.',
+          id: 'FDH352ME1634605157006821',
+        },
+        {
+          title: 'Note Title',
           entry: 'Sit totam a et omnis officia. Laboriosam sed quos dicta. Sunt deleniti occaecati blanditiis animi nihil reiciendis. Aut incidunt aut atque omnis nam quam occaecati corrupti saepe. Nostrum ex quis. Ut voluptatem at omnis et. Molestias a id eos porro voluptas qui ipsum perferendis. Sed provident consequatur facere. Sed quidem tenetur esse aut ut. Sunt quam hic. Magnam aut nobis. Suscipit nulla quos a in voluptas minus qui qui sit. Saepe fugiat quidem cupiditate ullam quidem sunt est.',
-          id: 'JNEI45',
+          id: 'J34543I453464663542030378',
+        },
+        {
+          title: 'Note Title',
+          entry: 'Autem inventore quisquam est quia rerum quaerat rem ratione ea. Quo ut beatae nisi eligendi minima sed non eaque et. Similique inventore suscipit quas delectus omnis est qui. Beatae corrupti sequi. Praesentium ipsa et rem quis excepturi earum.',
+          id: 'FDH352ME1632305157006821',
+        },
+        {
+          title: 'Note Title',
+          entry: 'Sit totam a et omnis officia. Laboriosam sed quos dicta. Sunt deleniti occaecati blanditiis animi nihil reiciendis. Aut incidunt aut atque omnis nam quam occaecati corrupti saepe. Nostrum ex quis. Ut voluptatem at omnis et. Molestias a id eos porro voluptas qui ipsum perferendis. Sed provident consequatur facere. Sed quidem tenetur esse aut ut. Sunt quam hic. Magnam aut nobis. Suscipit nulla quos a in voluptas minus qui qui sit. Saepe fugiat quidem cupiditate ullam quidem sunt est.',
+          id: 'JNEI4534647540378',
+        },
+        {
+          title: 'Note Title',
+          entry: 'Autem inventore quisquam est quia rerum quaerat rem ratione ea. Quo ut beatae nisi eligendi minima sed non eaque et. Similique inventore suscipit quas delectus omnis est qui. Beatae corrupti sequi. Praesentium ipsa et rem quis excepturi earum.',
+          id: 'FDH352ME1346157006821',
+        },
+        {
+          title: 'Note Title',
+          entry: 'Sit totam a et omnis officia. Laboriosam sed quos dicta. Sunt deleniti occaecati blanditiis animi nihil reiciendis. Aut incidunt aut atque omnis nam quam occaecati corrupti saepe. Nostrum ex quis. Ut voluptatem at omnis et. Molestias a id eos porro voluptas qui ipsum perferendis. Sed provident consequatur facere. Sed quidem tenetur esse aut ut. Sunt quam hic. Magnam aut nobis. Suscipit nulla quos a in voluptas minus qui qui sit. Saepe fugiat quidem cupiditate ullam quidem sunt est.',
+          id: 'J34543I45566322030378',
+        },
+        {
+          title: 'Note Title',
+          entry: 'Autem inventore quisquam est quia rerum quaerat rem ratione ea. Quo ut beatae nisi eligendi minima sed non eaque et. Similique inventore suscipit quas delectus omnis est qui. Beatae corrupti sequi. Praesentium ipsa et rem quis excepturi earum.',
+          id: 'FDH352ME16234343442006821',
+        },
+        {
+          title: 'Note Title',
+          entry: 'Sit totam a et omnis officia. Laboriosam sed quos dicta. Sunt deleniti occaecati blanditiis animi nihil reiciendis. Aut incidunt aut atque omnis nam quam occaecati corrupti saepe. Nostrum ex quis. Ut voluptatem at omnis et. Molestias a id eos porro voluptas qui ipsum perferendis. Sed provident consequatur facere. Sed quidem tenetur esse aut ut. Sunt quam hic. Magnam aut nobis. Suscipit nulla quos a in voluptas minus qui qui sit. Saepe fugiat quidem cupiditate ullam quidem sunt est.',
+          id: 'JNEI45346476570378',
         },
       ],
     };
