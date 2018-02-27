17aa4346d8bdbba1d3617700caf1a733ee3d1829
Mephestys <mephestys@gmail.com>
2018-02-26 17:38:46 -0700

    Changes in this commit: - Basic note adding ability implemented - Submitting new note also returns to NoteList - No styling for CreateNote yet

--- lambdanotes/src/components/App.js
+++ lambdanotes/src/components/App.js
@@ -6,6 +6,7 @@ import NoteList from './NoteList';
 import CreateNote from './CreateNote';
 
 class App extends React.Component {
+  nextId = 0;
   state = {
     notes: [
       {
@@ -26,13 +27,33 @@ class App extends React.Component {
     ],
   };
 
+  handleCreateNote = inputNote => {
+    const newNote = {
+      id: this.nextId++,
+      title: inputNote.title,
+      body: inputNote.body,
+    };
+    const newNotes = [...this.state.notes, newNote];
+    this.setState({
+      notes: newNotes,
+    })
+  }
+
+  handleEditNote = _ => {
+    //TODO
+  }
+
+  handleDeleteNote = _ => {
+    //TODO
+  }
+
   render() {
     return (
       <Router>
         <div className="App">
             <Sidebar />
             <Route exact path={"/"} render={()=><NoteList notes={this.state.notes} />} />
-            <Route exact path={"/create"} render={()=><CreateNote />} />
+            <Route exact path={"/create"} render={()=><CreateNote createNote={this.handleCreateNote}/>} />
         </div>
       </Router>
     );
--- lambdanotes/src/components/CreateNote.js
+++ lambdanotes/src/components/CreateNote.js
@@ -1,12 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class CreateNote extends React.Component {
-  state = {};
+  state = {
+    title: '',
+    body: '',
+  };
+
+  handleInputChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = _ => {
+    const { title, body } = this.state;
+    this.props.createNote({ title, body });
+    this.setState({ title: '', body: '', });
+  };
 
   render() {
     return (
       <div>
-        Hi there.
+        <h2>Create New Note:</h2>
+        <form>
+          <input
+            value={this.state.title}
+            name="title"
+            type="text"
+            placeholder="Note Title"
+            onChange={this.handleInputChange}
+          />
+          <input
+            value={this.state.body}
+            name="body"
+            type="text"
+            placeholder="Note Content"
+            onChange={this.handleInputChange}
+          />
+          <Link to={"/"} onClick={() => this.handleSubmit()} type="button">Save</Link>
+        </form>
       </div>
     );
   };
