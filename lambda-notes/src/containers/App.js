615f143312dd02c116814f21c5075b4f5a4feb0c
Peter Gray <peter@petergraycreative.com>
2018-02-27 12:50:50 -0600

    Add React-Router-Dom
    Added react router and configured router and switch

--- lambda-notes/src/containers/App.js
+++ lambda-notes/src/containers/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 import Sidebar from './Sidebar';
 import NoteGrid from '../components/NoteGrid';
+import NewNote from '../components/NewNote.js';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import initialState from '../initialState';
 import './App.css';
 
 class App extends Component {
   state = {
     initialState,
-  }
+  };
   render() {
     return (
-      <div className="container-fluid d-flex">
-        <div className="row">
-        <Sidebar />
-          <NoteGrid notes={this.state.initialState}/>
-        </div>
-      </div>
+      <Router>
+        <div className="container-fluid d-flex">
+            <Sidebar />
+            <div className="col-9 main-content container-fluid align-items-center">
+              <Route
+                path="/" exact
+                render={() => <NoteGrid notes={this.state.initialState} />}
+              />
+              <Route path="/new-note" render={() => <NewNote />} />
+              </div>
+          </div>
+      </Router>
     );
   }
 }
