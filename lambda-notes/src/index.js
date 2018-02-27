03d5aa343428f016664c614b6584a17957ac5bf7
Christopher Beards <beardscm@gmail.com>
2018-02-26 21:01:06 -0500

    Installed Axios and Redux Promise
    Preparing to wire the application using Heroku

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
 import './index.css';
 import App from './App';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
 <Provider store={createStoreWithMiddleware}>
