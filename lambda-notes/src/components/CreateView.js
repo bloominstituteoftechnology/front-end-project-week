8593b0ceff53ecdc228f664266c38d9680d3e4e5
cliffkang <cliffgkang@gmail.com>
2018-02-27 12:57:29 -0800

    added onChange and onSubmit to new Note
    -added onChange with corresponding handler function
    -added onSubmit with corresponding handler function that uses addNote action
    -tried to add routing back to home page after
    -added a little bit of css to individual notes on main view after seeing what it looked like once I added some note data

--- lambda-notes/src/components/CreateView.js
+++ lambda-notes/src/components/CreateView.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { addNote } from "../actions";
+import { Link } from 'react-router-dom';
 
 class CreateView extends Component {
   state = {
-    title: '',
-    text: '',
-  }
+    title: "",
+    text: ""
+  };
 
   render() {
     return (
@@ -14,19 +15,40 @@ class CreateView extends Component {
         <div className="header" />
         <h2>Create New View:</h2>
         <div className="NewNote">
-          <form>
-            <input autoFocus='true' type='text' name='title' placeholder='Note Title'/>
-            <textarea name='text' placeholder='Note Content' rows='30' cols='85'></textarea>
-            <button onSubmit={this.handleSubmit}>Save</button>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              autoFocus="true"
+              type="text"
+              name="title"
+              placeholder="Note Title"
+              value={this.state.title}
+              onChange={this.handleChange}
+            />
+            <textarea
+              name="text"
+              placeholder="Note Content"
+              rows="30"
+              cols="85"
+              value={this.state.text}
+              onChange={this.handleChange}
+            />
+            <button><Link to='/'>Save</Link></button>
           </form>
         </div>
       </div>
     );
   }
 
-  handleSubmit = () => {
-
+  handleChange = (event) => {
+    event.preventDefault();
+    this.setState({ [event.target.name]: event.target.value });
   }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+    this.props.addNote(this.state);
+  };
 }
 
 const mapStateToProps = state => {
--- lambda-notes/src/styling/NoteStyled.js
+++ lambda-notes/src/styling/NoteStyled.js
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 
 const NoteStyled = styled.div`
-  padding: 4px;
+  background-color: white;
+  padding: 15px;
+  margin: 10px;
   h4 {
     font-weight: bold;
     border-bottom: 1px solid black;
