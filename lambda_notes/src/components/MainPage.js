8e0f2ee4f83880a660d3c2fee6e5431fe4f37624
Smithface <kylewibblesmith@gmail.com>
2018-02-27 17:19:10 -0500

    finished edit note
    next to fix all the styling

--- lambda_notes/src/components/MainPage.js
+++ lambda_notes/src/components/MainPage.js
@@ -18,21 +18,21 @@ class MainPage extends React.Component {
     return (
       <div>
         <div style={this.state.deleting ? { visibility: 'visible' } : { visibility: 'hidden' } }>
-        <div className="deleteBox__bigBox">
-          <div className="bigBox__whiteBox">
-            <div className="whiteBox__container">
-              <div className="container__top"></div>
-              <div className="container__bottom">
-                <button className="container__button-delete" onClick={() => this.deleteCurrentNote()}>Delete</button>
-                <button className="container__button-cancel" onClick={() => this.toggleDeleting()}>No</button>
+          <div className="deleteBox__bigBox">
+            <div className="bigBox__whiteBox">
+              <div className="whiteBox__container">
+                <div className="container__top"></div>
+                <div className="container__bottom">
+                  <button className="container__button-delete" onClick={() => this.deleteCurrentNote()}>Delete</button>
+                  <button className="container__button-cancel" onClick={() => this.toggleDeleting()}>No</button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        </div>
         <div className="mainPage__topRow" style={this.props.caseValue === 'noteView' ? { visibility: 'visible' } : { visibility: 'hidden' } }>
           <div className="topRow__button-box">
-            <div className="topRow__button">edit</div>
+            <div className="topRow__button" onClick={() => this.props.changeSwitch('Edit Note:', 'editNote')}>edit</div>
             <div className="topRow__button" onClick={() => this.toggleDeleting()}>delete</div>
           </div>
         </div>
@@ -48,16 +48,18 @@ class MainPage extends React.Component {
     switch(param) {
       case 'noteList':
         return <div className="mainPage__noteList">
-          <NoteList notesArr={this.state.notes} changeSwitch={this.props.changeSwitch} viewNote={this.changeCurrentNote}/>
+          <NoteList notesArr={this.state.notes} changeSwitch={this.props.changeSwitch} viewNote={this.changeCurrentNote} />
         </div>;
       case 'noteView':
         return <div className="mainPage__noteView">
           <NoteView currentNote={this.state.currentNote} />
         </div>;
-      case 'editNote':
-        return <EditNote />;
+      case 'editNote': 
+        return <div className="mainPage__noteView">
+          <EditNote currentNote={this.state.currentNote} submitModifiedNote={this.replaceCurrentNoteInArr} changeSwitch={this.props.changeSwitch} />
+        </div>;
       case 'createNote':
-        return <CreateNote addNote={this.addNote}/>;
+        return <CreateNote addNote={this.addNote} />;
       default:
         return <div>Welcome to Lambda Notes</div>;
     }
@@ -70,6 +72,11 @@ class MainPage extends React.Component {
   changeCurrentNote = (nextNote) => {
     this.setState({...this.state, currentNote: nextNote});
   };
+
+  replaceCurrentNoteInArr = (newNote) => {
+    this.setState({...this.state, currentNote: newNote, notes: this.state.notes.map(note => { if (note.id === newNote.id) {return newNote} else {return note} } )
+    });
+  }
 
   toggleDeleting = () => {
     this.setState({...this.state, deleting: !this.state.deleting});
