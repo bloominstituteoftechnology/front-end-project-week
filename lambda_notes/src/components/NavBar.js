1d07e1ed0bb13d09da0937909f490a642e8ef85f
Smithface <kylewibblesmith@gmail.com>
2018-02-27 18:21:42 -0500

    added NavBar styling

--- lambda_notes/src/components/NavBar.js
+++ lambda_notes/src/components/NavBar.js
@@ -9,10 +9,10 @@ const NavBar = (props) => {
         <div>Notes</div>
       </div>
       <div>
-        <button onClick={() => props.changeSwitch('Your Notes:','noteList')} >View Your Notes</button>
+        <button className="navBar__button" onClick={() => props.changeSwitch('Your Notes:','noteList')} >View Your Notes</button>
       </div>
       <div>
-        <button onClick={() => props.changeSwitch('Create New Note:', 'createNote')} >+ Create New Note</button>
+        <button className="navBar__button" onClick={() => props.changeSwitch('Create New Note:', 'createNote')} >+ Create New Note</button>
       </div>
     </div>
   );
