842ba0f1d47db4f5b8e7fef6f888f2d3b8b47722
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-27 11:22:40 -0600

    add connect to Note.js
    - line 2: import connect
    - lines 6-9: gets the specific note object from the store
    - lines 18-21: map the notes array in redux store to props
    - line 24: connect mapStateToProps to Note component

--- notes/src/comps/Note.js
+++ notes/src/comps/Note.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const Note = props => {
+  // this is the note object with the id that matches the url
+  const note = props.notes.filter(
+    note => String(note.id) === props.match.params.id
+  )[0];
+
   return (
     <div className="note__container">
-      <div className="note__title">
-        {props.note.title}
-      </div>
-      <div className="note__content">
-        {props.note.content}
-      </div>
+      <div className="note__title">{note.title}</div>
+      <div className="note__content">{note.content}</div>
     </div>
-  )
-
+  );
 };
 
-export default Note;
+const mapStateToProps = state => {
+  return {
+    notes: state.notes,
+  };
+};
+
+export default connect(mapStateToProps)(Note);
