805a1159e7c3a6676fe5862d2c19d8135facba77
Glenn-David Daniel <d2rd@outlook.com>
2018-02-27 09:19:12 -0800

    Setup additions babel

--- notes/src/App.js
+++ notes/src/App.js
@@ -8,11 +8,15 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Proximo Notes</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="person">
+          <h1>{props.noteTitle}</h1>
+          <p>Note body: {props.noteBody}</p> 
+		    </div>
       </div>
     );
   }
--- notes/src/index.js
+++ notes/src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import note from './components/note';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
