4fbf567743c3e004a66df941405dce3247c71fef
Peter Gray <peter@petergraycreative.com>
2018-02-26 18:58:02 -0600

    import action from actions.js
    Also adjust a css class

--- lambda-notes/src/containers/Sidebar.js
+++ lambda-notes/src/containers/Sidebar.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { addNote } from '../actions/actions'
+import './Sidebar.css'
 
 class Sidebar extends Component {
   render() {
     return (
-      <div className="col-md-2">
+      <div className="col-md-3 sidebar">
         <h1>Lambda Notes</h1>
         <button>View Your Notes</button>
-        <button>+ Create Your Note</button>
+        <button onClick={()}>+ Create Your Note</button>
       </div>
     );
   }
@@ -17,4 +19,4 @@ const mapStateToProps = state => {
   return {};
 };
 
-export default connect(mapStateToProps)(Sidebar);
+export default connect(mapStateToProps, { addNote })(Sidebar);
