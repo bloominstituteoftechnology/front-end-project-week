a409463b003045fa9ecc664884c66bb593e21395
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-27 18:42:59 -0600

    fix characters display in NoteThumb
    fix the issue where even if the number of characters in the content
    were less than 120, it would show ellipses. Now ellipses onlny show when
    the content is more than 120 characters

--- notes/src/comps/NoteThumb.js
+++ notes/src/comps/NoteThumb.js
@@ -4,12 +4,15 @@ import '../styles/NoteThumb.css';
 // props.note is a note object with title and content
 const NoteThumb = props => {
   const charsVisible =
-    props.note.content.length > 120 ? 120 : props.note.content.length;
+    props.note.content.length < 120 ? props.note.content : (props.note.content.slice(0, 120) + '...');
+
   return (
     <div className="noteThumb__container">
       <h3 className="noteThumb__title">{props.note.title}</h3>
       <hr />
-      <p className="noteThumb__content">{props.note.content.slice(0, charsVisible)}..</p>
+      <p className="noteThumb__content">
+        {charsVisible}
+      </p>
     </div>
   );
 };
