b7d7947c450db311d654d1eda5f7b0057982aa01
Tyson Williams <tyson.j.williams@gmail.com>
2018-02-28 16:19:18 -0700

    Update app's index.js

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -3,13 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import LambdaNotes from './components/notes-list/notes-list';
 // Redux
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './reducers/reducers';
+import middleware from './middleware/middleware';
 // Styles
 import './index.css';
 
-let STORAGE_WAREHOUSE = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const STORAGE_WAREHOUSE = createStore(reducers, composeEnhancers(applyMiddleware(...middleware)));
 
 ReactDOM.render(
   <Provider store={STORAGE_WAREHOUSE}>
