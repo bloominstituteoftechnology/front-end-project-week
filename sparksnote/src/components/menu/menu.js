c8e2e757e55aeecfd626ca47e2f487c7ca951614
Charlie Sparks <csparks@antioch.edu>
2018-02-27 19:54:15 -0800

    finished styles, placements, and routing

--- sparksnote/src/components/menu/menu.js
+++ sparksnote/src/components/menu/menu.js
@@ -13,16 +13,16 @@ class Menu extends Component {
     return (
       <div className="menu">
         <div className="menu__header">
-          <h1>SparksNote</h1>
+          <p>Sparks Note</p>
         </div>
 
         <div className="menu__buttons">
           <div className="menu__buttons--align">
-            <p>View Your Notes</p>
+            <p className="menu__buttons__text">View Your Notes</p>
           </div>
 
           <div className="menu__buttons--align">
-            <p>+ Create New Note</p>
+            <p className="menu__buttons__text">+ Create New Note</p>
           </div>
 
         </div>
