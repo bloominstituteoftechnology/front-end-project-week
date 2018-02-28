56e5661f27bfaee9ed72217493b444ff17d1693a
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-27 18:22:24 -0600

    add update note case to reducer

--- notes/src/reducer/index.js
+++ notes/src/reducer/index.js
@@ -2,28 +2,36 @@
 import notes from '../data/notes';
 
 // Action types
-import { ADD_NOTE } from '../actions'; 
+import { ADD_NOTE, UPDATE_NOTE } from '../actions';
 
 const initialState = {
   notes: [],
   id: 1,
-}
+};
 
-const reducer = (state=initialState, action) => {
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_NOTE:
       const newNote = {
         ...action.payload,
         id: state.id,
-      }
+      };
       return {
         ...state,
         notes: [...state.notes, newNote],
         id: state.id + 1,
       };
+    case UPDATE_NOTE:
+      return {
+        ...state,
+        notes: state.notes.map(note => {
+          if (note.id !== action.payload.id) return note;
+          return action.payload;
+        }),
+      };
     default:
       return state;
   }
-}
+};
 
 export default reducer;
