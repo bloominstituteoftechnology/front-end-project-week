5d56e67ece1ca6ea387ca42ebcb47ceeef8f45b6
Tyson Williams <tyson.j.williams@gmail.com>
2018-02-28 13:22:27 -0700

    Import reducers file

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 // Redux
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import reducers from './reducers/reducers';
 // Styles
 import './index.css';
 
