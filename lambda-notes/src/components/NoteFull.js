f4d1e3918c835c3cce168defef5b6425434dc35c
Peter Gray <peter@petergraycreative.com>
2018-02-28 17:14:42 -0600

    Imported react.modal to handle delete modal
    Set up according to comp and it works as expected.

--- lambda-notes/src/components/NoteFull.js
+++ lambda-notes/src/components/NoteFull.js
@@ -4,30 +4,73 @@ import { Redirect, Link } from 'react-router-dom';
 import { getNote, deleteNote } from '../actions/actions';
 import Modal from 'react-modal';
 
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    padding: '50px 100px',
+  },
+};
+
 class NoteFull extends Component {
   state = {
     redirect: false,
-    modalIsOpen: false,
+    modalOpen: false,
   };
   componentDidMount() {
     this.props.getNote(this.props.match.params.id);
   }
+
   deleteNote = () => {
     this.props.deleteNote(this.props.match.params.id);
     this.setState({
       redirect: true,
     });
   };
+  openModal = () => {
+    this.setState({ modalOpen: true });
+  };
+
+  afterOpenModal = () => {};
+
+  closeModal = () => {
+    this.setState({ modalOpen: false });
+  };
+
   render() {
     return (
       <div>
+        <Modal
+          isOpen={this.state.modalOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Delete"
+          shouldCloseOnOverlayClick={true}
+        >
+          <p>Are you sure you want to delete this?</p>
+          <div className='d-flex'>
+          <button onClick={this.deleteNote} type="button" className="btn delete">
+            Delete
+          </button>
+          <button onClick={this.closeModal} type="button" className="btn">
+            No
+          </button></div>
+        </Modal>
         {this.props.currentNote.map(note => {
           return (
             <div key={note.id} className="full-note">
               <p className="text-right">
                 <Link to={`/edit/${note.id}`}>edit</Link>
-                <Link onClick={this.deleteNote} to={`/`}>
-                delete
+                <Link
+                  onClick={this.openModal}
+                  /*onClick={this.deleteNote}*/ to={`#`}
+                >
+                  delete
                 </Link>
               </p>
               <h3>{note.title}</h3>
@@ -40,6 +83,7 @@ class NoteFull extends Component {
     );
   }
 }
+Modal.setAppElement('#root');
 const mapStateToProps = state => {
   return {
     error: state.error,
