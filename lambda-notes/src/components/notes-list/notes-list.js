ff1939695562eaf8952be3b8b4659e795fadbed0
Tyson Williams <tyson.j.williams@gmail.com>
2018-02-28 13:43:33 -0700

    Create initial notes-list component

---
+++ lambda-notes/src/components/notes-list/notes-list.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import actions
+
+class LambdaNotes extends Component {
+  render() {
+    return (
+      <div className="LambdaNotes">
+        <p>Test</p>
+      </div>
+    );
+  }
+}
+
+export default LambdaNotes;
