6ef544e784f51f8cd1f6a3d2a3518008796ed2a3
Mephestys <mephestys@gmail.com>
2018-02-28 13:56:30 -0700

    Added condition check for empty notes array. - Shows a message to the user advising them to click Create New Note.

--- lambdanotes/src/components/NoteList.js
+++ lambdanotes/src/components/NoteList.js
@@ -5,8 +5,18 @@ import Note from './Note';
 import './NoteList.css';
 
 class NoteList extends React.Component {
+  boolEmptyNotes = true;
+
   state = {
     notes: this.props.notes,
+  };
+
+  componentWillMount() {
+    if(this.state.notes.length > 0) {
+      this.boolEmptyNotes = false;
+    } else {
+      this.boolEmptyNotes = true;
+    };
   };
 
   handleNoteIndex = index => {
@@ -17,6 +27,10 @@ class NoteList extends React.Component {
     return (
       <div className="YourNotes">
         <h2 className="SectionTitle">Your Notes:</h2>
+        {this.boolEmptyNotes ? (
+          <h3 className="YourNotes-Empty">Looks like you don't have any notes yet, click "Create New Note" to get started!</h3>
+        ) 
+        : (null)}
         <ul className="YourNotes-NoteList">
           {this.state.notes.map(note => {
             return (
