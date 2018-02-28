5c321678d9fa95118777fdbd6b67e106aefba368
AnthonyCatalfo <anthonycatalfo@gmail.com>
2018-02-28 11:54:23 -0500

    Anthony Catalfo  first commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import {connect} from "react-redux";
+import {bindActionCreators} from "redux";
+import {BrowserRouter, NavLink, Route, Switch} from "react-router-dom";
+
+import * as actions from './actions';
+
+import './App.css';
+import NoteListPage from "./components/NoteListPage";
+import AddNotePage from "./components/AddNotePage";
+import EditNotePage from "./components/EditNotePage";
+import ViewNotePage from "./components/ViewNotePage";
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    props.actions.getSmurfData();
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App container">
+          <div className="sidenav">
+            <h3>Lambda Notes</h3>
+            <NavLink to="/">View Your Notes</NavLink>
+            <NavLink to="/add-note">Create New Note</NavLink>
+          </div>
+          <div className="main">
+            <Switch>
+              <Route exact path="/" component={NoteListPage}/>
+              <Route path="/add-note" component={AddNotePage}/>
+              <Route path="/edit-note" component={EditNotePage}/>
+              <Route path="/view-note" component={ViewNotePage}/>
+              {/*<Route component={NotFoundPage}/>*/}
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    notes: state.notes
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  };
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/components/AddNotePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class AddNotePage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h4>
+          Create New Note
+        </h4>
+      </div>
+    );
+  }
+}
+
+export default AddNotePage;
