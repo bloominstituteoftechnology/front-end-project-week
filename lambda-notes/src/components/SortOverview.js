89c78c3d2246e5c7e54818081f158a2151eea1f0
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-02-28 13:22:57 -0500

    Added reverse alphabetically/chronologically for the hell of it.
    SortOverview.js --- reverse functions for both.

--- lambda-notes/src/components/SortOverview.js
+++ lambda-notes/src/components/SortOverview.js
@@ -8,6 +8,7 @@ import './css/SortOverview.css';
 class SortOverview extends React.Component {
   state = {
     sort: 'sort',
+    reverse: false,
     sorted: [],
   };
   render() {
@@ -17,17 +18,31 @@ class SortOverview extends React.Component {
           <div>
             <h1 className="sort-header"> How Would You Like to Sort? </h1>
             <div>
-              <button onClick={this.clickAlphabetically}> Alphabetically by Title </button>
-              <button onClick={this.clickChronologically}> Chronologically by Date </button>
+              <button onClick={this.clickAlphabetically}> 
+              Alphabetically by Title 
+              </button>
+              <button onClick={this.clickRevAlphabetically}>
+                Reverse Alphabetically by Title
+              </button>
+              <button onClick={this.clickChronologically}> 
+              Chronologically by Date 
+              </button>
+              <button onClick={this.clickRevChronologically}>
+                Reverse Chronologically by Date
+              </button>
             </div>
           </div>
         ) : null}
         {this.state.sort === 'alphabetically' ? (
           <div>
-            <h1 className="sort-header"> Alphabetically Sorted by Title</h1>
+            {this.state.reverse ? (
+              <h1 className="sort-header"> Reverse Alphabetically Sorted by Title</h1>
+            ) : (
+              <h1 className="sort-header"> Alphabetically Sorted by Title</h1>
+            )}
             <ul className="sorted-notes">
               {this.state.sorted.map(note => {
-                const noteObj = { body: note[1], title: note[3], dateString: note[4]};
+                const noteObj = { body: note[1], title: note[3], dateString: note[4] };
                 return <Note key={note[2]} note={noteObj} />;
               })}
             </ul>
@@ -35,37 +50,71 @@ class SortOverview extends React.Component {
         ) : null}
         {this.state.sort === 'chronologically' ? (
           <div>
-          <h1 className="sort-header"> Chronologically Sorted by Date</h1>
-          <ul className="sorted-notes">
-            {this.state.sorted.map(note => {
-              console.log(note)
-              const noteObj = { body: note[2], title: note[1], dateString: note[4] };
-              return <Note key={note[3]} note={noteObj} />;
-            })}
-          </ul>
-        </div>
+            <h1 className="sort-header"> Chronologically Sorted by Date</h1>
+            <ul className="sorted-notes">
+              {this.state.sorted.map(note => {
+                console.log(note);
+                const noteObj = { body: note[2], title: note[1], dateString: note[4] };
+                return <Note key={note[3]} note={noteObj} />;
+              })}
+            </ul>
+          </div>
         ) : null}
       </div>
     );
   }
 
   clickAlphabetically = () => {
-    this.setState({ sort: 'alphabetically' });
+    this.setState({ sort: 'alphabetically', reverse: false });
     let sorted = [];
     this.props.notes.map(note => {
-      return sorted.push([note.title.toLowerCase(), note.body, note.id, note.title, note.dateString]);
+      return sorted.push([
+        note.title.toLowerCase(),
+        note.body,
+        note.id,
+        note.title,
+        note.dateString,
+      ]);
     }, []);
     sorted = sorted.sort();
     this.setState({ sorted });
   };
 
   clickChronologically = () => {
-    this.setState({ sort: 'chronologically' });
+    this.setState({ sort: 'chronologically', reverse: false });
     let sorted = [];
     this.props.notes.map(note => {
       return sorted.push([note.date, note.title, note.body, note.id, note.dateString]);
     }, []);
     sorted = sorted.sort();
+    this.setState({ sorted });
+  };
+
+  clickRevAlphabetically = () => {
+    this.setState({ sort: 'alphabetically', reverse: true });
+    let sorted = [];
+    this.props.notes.map(note => {
+      return sorted.push([
+        note.title.toLowerCase(),
+        note.body,
+        note.id,
+        note.title,
+        note.dateString,
+      ]);
+    }, []);
+    sorted = sorted.sort();
+    sorted = sorted.reverse();
+    this.setState({ sorted });
+  };
+
+  clickRevChronologically = () => {
+    this.setState({ sort: 'chronologically', reverse: true });
+    let sorted = [];
+    this.props.notes.map(note => {
+      return sorted.push([note.date, note.title, note.body, note.id, note.dateString]);
+    }, []);
+    sorted = sorted.sort();
+    sorted = sorted.reverse();
     this.setState({ sorted });
   };
 }
