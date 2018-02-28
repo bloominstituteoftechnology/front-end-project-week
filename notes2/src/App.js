dd3b07b3aaf596d0241202c66dd00262812e9a87
Jonathan <jonathanbry@comcast.net>
2018-02-28 15:30:36 -0800

    commit 2/28 15:27
    -in App i changed styling for the divs in the html code
    -also made style variables above that

--- notes2/src/App.js
+++ notes2/src/App.js
@@ -3,23 +3,36 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
 
+    notestyles = { //individual note css
+        width:200,
+        height:200,
+        backgroundColor: 'grey',
+        padding: 10,
+    }
+
+    containerflow = { //container flow/size
+        display: 'flex',
+        width: 1200,
+        flexFlow: 'row-reverse',
+      }
+
     render() {
         return (
-            <div>
-                <notesDefaultSource class="beard">
-                    <div class="beard">{notesDefaultSource}
+            <div class="containerflow" style={this.containerflow}>
+                <notesDefaultSource id="beard">
+                    <div style={this.notestyles}>{notesDefaultSource}
                     </div>
                 </notesDefaultSource>
-                <notesDefaultSource>
-                    <div>{notesDefaultSource}
+                <notesDefaultSource id="beard">
+                    <div style={this.notestyles}>{notesDefaultSource}
                     </div>
                 </notesDefaultSource>
-                <notesDefaultSource>
-                    <div>{notesDefaultSource}
+                <notesDefaultSource id="beard">
+                    <div style={this.notestyles}>{notesDefaultSource}
                     </div>
                 </notesDefaultSource>
-                <notesDefaultSource>
-                    <div>{notesDefaultSource}
+                <notesDefaultSource id="beard">
+                    <div style={this.notestyles}>{notesDefaultSource}
                     </div>
                 </notesDefaultSource>
             </div>
