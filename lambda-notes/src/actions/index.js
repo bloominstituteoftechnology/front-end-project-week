3743204d822f7e9a98082ba82033e257dfc8f552
Christopher Beards <beardscm@gmail.com>
2018-02-28 09:56:01 -0500

    Created a FetchPost Action

--- lambda-notes/src/actions/index.js
+++ lambda-notes/src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
+export const FETCH_POST = 'FETCH_POST';
 
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
@@ -21,6 +22,15 @@ export const createPost = (values, callback) => {
 
   return {
     type: CREATE_POST,
+    payload: request
+  };
+}
+
+export const fetchPost = (id) => {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: FETCH_POSTS,
     payload: request
   };
 }
