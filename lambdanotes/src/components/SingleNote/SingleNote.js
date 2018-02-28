632eeadc976667c2a740d1a6a1df46f8a088185b
ashleijo <shuryjo@gmail.com>
2018-02-27 21:46:55 -0500

    adding some styling to the view for single notes

--- lambdanotes/src/components/SingleNote/SingleNote.js
+++ lambdanotes/src/components/SingleNote/SingleNote.js
@@ -4,6 +4,7 @@ class SingleNote extends React.Component {
   render() {
     return (
       <div className="single-note">
+        <p className="edit-delete"><span>edit</span><span>delete</span></p>
         <p>{this.props.noteToShow.noteTitle}</p>
         <p>{this.props.noteToShow.noteContent}</p>
       </div>
