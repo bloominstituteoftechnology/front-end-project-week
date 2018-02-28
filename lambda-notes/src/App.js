650b99bbdc29add21ec0fa40b7317e79a3646d2f
Arkoma <aaron@adburk.net>
2018-02-28 11:55:56 -0600

    restore  the state in app
    - it was an empty arry for testing id counter
    - changed to a populated array for styling purposes

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -6,7 +6,52 @@ import CoreRail from './containers/coreRail';
 
 class App extends Component {
   state = {
-    notes: [],
+    notes: [
+      {
+          id: 0,
+          title: "Note Title",
+          body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptatem, error nihil molestias ratione voluptatibus dolorem consequuntur.",
+      },
+      {
+          id: 1,
+          title: "Note Title",
+          body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptatem, error nihil molestias ratione voluptatibus dolorem consequuntur.",
+      },
+      {
+          id: 2,
+          title: "Note Title",
+          body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptatem, error nihil molestias ratione voluptatibus dolorem consequuntur.",
+      },
+      {
+        id: 3,
+        title: "Note Title",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptatem, error nihil molestias ratione voluptatibus dolorem consequuntur.",
+      },
+      {
+        id: 4,
+        title: "Note Title",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptatem, error nihil molestias ratione voluptatibus dolorem consequuntur.",
+      },
+      {
+        id: 5,
+        title: "Note Title",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptatem, error nihil molestias ratione voluptatibus dolorem consequuntur.",
+      },
+      {
+        id: 6,
+        title: "Note Title",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptatem, error nihil molestias ratione voluptatibus dolorem consequuntur.",
+      },
+      {
+        id: 7,
+        title: "Note Title",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptatem, error nihil molestias ratione voluptatibus dolorem consequuntur.",
+      },      {
+        id: 8,
+        title: "Note Title",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptatem, error nihil molestias ratione voluptatibus dolorem consequuntur.",
+      },
+  ],
   }
   render() {
     return (
