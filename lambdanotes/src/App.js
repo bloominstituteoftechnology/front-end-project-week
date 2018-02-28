038eff6b367a8f8c820ad3adff96234e7c153b00
Maxjoeld <30808913+Maxjoeld@users.noreply.github.com>
2018-02-27 21:00:39 -0500

    Test commit--noChanges

--- lambdanotes/src/App.js
+++ lambdanotes/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className="App-intros">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
