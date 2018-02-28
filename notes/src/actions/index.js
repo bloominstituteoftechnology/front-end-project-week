b21e7e5f85d37131b19df0d5268bd45e43a85100
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-27 18:21:50 -0600

    add updateNote action

--- notes/src/actions/index.js
+++ notes/src/actions/index.js
@@ -1,8 +1,16 @@
 export const ADD_NOTE = 'ADD_NOTE';
+export const UPDATE_NOTE = 'UPDATE_NOTE';
 
-export const addNote = (note) => {
+export const addNote = note => {
   return {
     type: ADD_NOTE,
     payload: note,
-  }
-}
+  };
+};
+
+export const updateNote = note => {
+  return {
+    type: UPDATE_NOTE,
+    payload: note,
+  };
+};
