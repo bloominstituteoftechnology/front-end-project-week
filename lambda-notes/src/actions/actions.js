9e1d044f95255ee353713512101622fbc6b987ed
Peter Gray <peter@petergraycreative.com>
2018-02-27 23:01:42 -0600

    Add GETNOTE action
    Dispatches payload of id

--- lambda-notes/src/actions/actions.js
+++ lambda-notes/src/actions/actions.js
@@ -1,9 +1,16 @@
 export const ADDNOTE = 'ADDNOTE';
+export const GETNOTE = 'GETNOTE';
 
 export const addNote = (note) => {
     return {
         type: ADDNOTE,
         payload: note
+    }
+};
+export const getNote = (id) => {
+    return {
+        type: GETNOTE,
+        payload: id
     }
 };
 // export const addNote = (note) => {
