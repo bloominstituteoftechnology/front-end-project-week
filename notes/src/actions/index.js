4a2a782866671b792fae387f0dcd4cbc593c761b
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-28 12:13:20 -0600

    add export for deleteNote type

--- notes/src/actions/index.js
+++ notes/src/actions/index.js
@@ -1,6 +1,7 @@
 export const ADD_NOTE = 'ADD_NOTE';
 export const UPDATE_NOTE = 'UPDATE_NOTE';
 export const TOGGLE_MODAL = 'TOGGLE_MODAL';
+export const DELETE_NOTE = 'DELETE_NOTE';
 
 export const addNote = note => {
   return {
