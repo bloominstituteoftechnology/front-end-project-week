e309238fab456fb315dfc611ad542fbc937422d3
Charlie Sparks <csparks@antioch.edu>
2018-02-27 20:19:14 -0800

    minor problems with flexbox, committing for the night

--- sparksnote/src/App.js
+++ sparksnote/src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import './styles/App.css';
 
 import Menu from "./components/menu/menu";
+import NewNote from "./components/newNote/newNote";
 
 class App extends Component {
   constructor(props) {
@@ -16,8 +17,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="menu--align">
+        <div className="App__menu">
           <Route path="/" component={Menu} />
+        </div>
+        <div className="App__main">
+          <Route path="/newnote" exact component={NewNote} />
         </div>
       </div>
     );
---
+++ sparksnote/src/components/newNote/newNote.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class NewNote extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      title: "",
+      content: ""
+    }
+  }
+
+  render() {
+    return (
+      <div className="newNote">
+        <div className="newNote__header">
+          <p>Create New Note:</p>
+        </div>
+
+        <div className="newNote__input">
+          <form className="newNote__form">
+            <input className="newNote__form__title" />
+            <input className="newNote__form__content" />
+          </form>
+        </div>
+      </div>
+      )
+  }
+}
+
+export default NewNote;
