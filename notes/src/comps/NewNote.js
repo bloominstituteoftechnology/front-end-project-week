eaeb2956a323947877cf67ba5ad9ee8d083ab608
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-27 18:21:28 -0600

    add NewNote styling
    - reformat with prettier
    - remove div on line 35
    - add class name on line 54

--- notes/src/comps/NewNote.js
+++ notes/src/comps/NewNote.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import '../styles/NewNote.css';
 
 import { addNote } from '../actions';
 
@@ -13,46 +14,46 @@ class NewNote extends React.Component {
     redirect: false,
   };
 
-  submitNewNote = (event) => {
+  submitNewNote = event => {
     event.preventDefault();
     this.props.addNote(this.state.newNote);
     this.setState({
       redirect: true,
-    })
-  }
+    });
+  };
 
   updateField = event => {
-    this.setState({ newNote: {
-      ...this.state.newNote,
-      [event.target.name]: event.target.value
-    }});
+    this.setState({
+      newNote: {
+        ...this.state.newNote,
+        [event.target.name]: event.target.value,
+      },
+    });
   };
 
   render() {
     return (
       <div className="newNote__container">
         <div className="newNote__header">Create New Note:</div>
-        <div className="newNote__form">
-          <form onSubmit={this.submitNewNote}>
-            <input
-              type="text"
-              value={this.state.newNote.title}
-              name="title"
-              placeholder="Note Title"
-              onChange={this.updateField}
-              className="newNote__title-field"
-            />
-            <textarea
-              name="content"
-              value={this.state.newNote.content}
-              placeholder="Note Content"
-              onChange={this.updateField}
-              className="newNote__content-field"
-            />
-            <input type="submit" value="Save" />
-          </form>
-          {this.state.redirect && <Redirect to="/" />}
-        </div>
+        <form className="newNote__form" onSubmit={this.submitNewNote}>
+          <input
+            type="text"
+            value={this.state.newNote.title}
+            name="title"
+            placeholder="Note Title"
+            onChange={this.updateField}
+            className="newNote__title-field"
+          />
+          <textarea
+            name="content"
+            value={this.state.newNote.content}
+            placeholder="Note Content"
+            onChange={this.updateField}
+            className="newNote__content-field"
+          />
+          <input type="submit" value="Save" className="newNote__submit" />
+        </form>
+        {this.state.redirect && <Redirect to="/" />}
       </div>
     );
   }
