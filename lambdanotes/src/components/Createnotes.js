65ce20024a266626a13fe04174b28739ced1a623
shobanavv <shobanavv@gmail.com>
2018-02-27 17:50:39 -0800

    Create View modified

--- lambdanotes/src/components/Createnotes.js
+++ lambdanotes/src/components/Createnotes.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import '../styles/App.css';
 
 function Createnotes() {
-    return <li> It Works!</li>
+    return (
+        <div className="notes-container">
+            <form onSubmit="">
+            <input className="new-title"/> 
+            <input className="new-content"/>
+            <button>Save</button>
+            <li> It Works!</li>
+            </form>
+        </div>
+        
+    )
 }
 export default Createnotes;
--- lambdanotes/src/components/NoteView.js
+++ lambdanotes/src/components/NoteView.js
@@ -3,10 +3,7 @@ import '../styles/App.css';
 
 function Noteview(props) {
     return ( 
-    <div className="single-note">
-        <h4>{props.note.title}</h4>
-        <p>{props.note.paragraph}</p>
-    </div>
+    
     );
 }
 
