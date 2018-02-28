b27b906e4b0966df4b795bb239891dd79158c5cf
Arkoma <aaron@adburk.net>
2018-02-28 15:02:59 -0600

    edit component saves changes
    - set state equal to the id of the component
    - wrote a getNoteIndex function to get the index for a splice method
    -completed the changehandler with a splice method.
    - change title to edit note
    - change place holders to have original note text.

--- lambda-notes/src/components/editNote.js
+++ lambda-notes/src/components/editNote.js
@@ -1,49 +1,63 @@
 import React, { Component } from 'react';
 
+
 class EditNote extends Component {
-//   noteIndex = notes.findIndex(note => note.id === id);
+    state ={
+        id: +this.props.match.params.id,
+        title: this.props.notes[this.props.match.params.id].title,
+        body: this.props.notes[this.props.match.params.id].body,
+        
+    }
+  getNoteIndex = () => {
+    for (let i = 0; i < this.props.notes.length; i++) {
+        if (this.props.match.params.id === this.props.notes[i].id) {
+            return i;
+        }
+    }
+  }
   noteSubmitHandler = (event) => {
       event.preventDefault();
+      this.setState({title: this.state.title, body: this.state.body});
   }
   noteChangeHandler = (event) => {
       let { name, value } = event.target;
       this.setState({ [name]: value });
+      this.props.notes.splice(this.getNoteIndex(), 1, this.state)
   }
   render() {
-      // console.log('this is what the id of the last note is: ', this.props.notes.slice(-1)[0].id)
-      // console.log('this note id should be: ', this.getNextId())
-      // console.log('what is state here: ', this.state)
-      // console.log('what is props here: ', this.props)
+      console.log('what is state here: ', this.state)
+      console.log('what is props here: ', this.props)
+    //   console.log('Does my function give me the right index?', this.getNoteIndex());
       console.log(this);
       console.log(this.props);
-      // console.log(this.props.notes);
+      console.log(this.props.notes);
+      console.log('does this give me the right index?', this.getNoteIndex())
       // console.log(this.props.notes.slice(-1));
       // console.log(this.props.notes.slice(-1)[0]);
       // console.log(this.props.notes.slice(-1)[0].id);
       // console.log(this.props.notes.concat(this.state))
-      let id = this.props.match.params.id;
       return (
           <div className="new-note-container">
-            <div className="note-list__title">Create New Note:</div>                
+            <div className="note-list__title">Edit Note:</div>                
             <div className="new-note__title-input" name="id" value="9">
               <textarea 
                 className="title-input" 
-                placeholder="Note Title" 
+                placeholder={this.props.notes[this.props.match.params.id].title}
                 rows="50" cols="1" 
                 maxLength="50" 
                 name="title" 
-                value={this.props.notes[`${id}`].title}
+                value={this.state.title}
                 onChange={this.noteChangeHandler}
               />    
             </div>
             <div className="new-note__content-input">
               <textarea 
                 className="content-input" 
-                placeholder="Note Content" 
+                placeholder={this.props.notes[this.props.match.params.id].body}
                 rows="4" 
                 cols="72" 
                 name="body"
-                value={this.props.notes[`${id}`].body}
+                value={this.state.body}
                 onChange={this.noteChangeHandler}
               />
             </div>
