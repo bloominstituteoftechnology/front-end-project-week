3eb65cab0bfdab3797d955ad95f981b77b8863de
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-02-28 18:04:26 -0500

    Got basic search functionality working

--- lambdanotes/src/components/notes-list.js
+++ lambdanotes/src/components/notes-list.js
@@ -22,21 +22,61 @@ const NoteListStyled = styled.div`
       margin-top: 20px;
     }
   }
-
 `
 
 class NotesList extends Component {
+  state = {
+    // searchInput: '',
+    notesFiltered: [],
+  }
+
+
+  handleSearch = (event) => {
+    console.log(event.target.value)
+    // this.setState({ searchInput: event.target.value })
+    console.log(this.state.searchInput)
+    if (event.target.value.length > 0) {
+      let filteredNotes = this.props.notes.filter(note => note.title.toLowerCase().includes(event.target.value.toLowerCase()))
+      this.setState({ notesFiltered: filteredNotes })
+    }
+    else {
+      this.setState({ notesFiltered: this.props.notes })
+    }
+    console.log(this.state.notesFiltered);
+  }
+
+  componentDidMount() {
+    this.setState({ notesFiltered: this.props.notes });
+  }
+
   render() {
     return (
       <NoteListStyled>
         <header>
           <h1>Your Notes:</h1>
-          <div className="searchContainer">Search: <input /></div>
+          <div className="searchContainer">
+            Search: <input onChange={this.handleSearch} value={this.state.searchInput} />
+          </div>
         </header>
-        <div style={{ display: "flex", flexFlow: "row wrap" }} >
+        {/* <div style={{ display: "flex", flexFlow: "row wrap" }} >
           {this.props.notes.map((note) => {
             return (
-              <Note key={note.id} note={note} showNoteDetails={this.props.showNoteDetails} />
+              <Note
+                key={note.id}
+                note={note}
+                showNoteDetails={this.props.showNoteDetails}
+              />
+            )
+          })}
+        </div> */}
+        <div style={{ display: "flex", flexFlow: "row wrap" }} >
+          {this.state.notesFiltered.map((note) => {
+            return (
+              <Note
+                key={note.id}
+                note={note}
+                showNoteDetails={this.props.showNoteDetails}
+              />
             )
           })}
         </div>
