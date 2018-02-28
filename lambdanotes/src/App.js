1c07ca5a8e07701b6e4c9e989f17cec0a5189473
Nathan Daniel <natdf@icloud.com>
2018-02-28 16:44:23 -0600

    Commit 3 - refactored listing of notes (App.js) - updates to css styling (App.css)

--- lambdanotes/src/App.js
+++ lambdanotes/src/App.js
@@ -2,77 +2,70 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-    state = {
-      title: 'Note Title',
-      message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea ab totam modi. Voluptatum, mollitia quidem necessitatibus vero beatae sequi corporis? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error a deleniti, distinctio, quis autem expedita officia ipsam ...',
-    }
+  state = {
+    lnotes: [
+      {
+        id: 1,
+        title: 'Note Title',
+        message:
+          'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat magnam aspernatur beatae laudantium, optio harum accusantium pariatur voluptatibus earum error.',
+      },
+      {
+        id: 2,
+        title: 'Note Title',
+        message: 'Est excepturi iste praesentium sequi, sint explicabo iure?',
+      },
+      {
+        id: 3,
+        title: 'Note Title',
+        message:
+          'Eum recusandae debitis atque reprehenderit! Sint illo, ut beatae dolor impedit nulla delectus sequi, doloribus quia minus nobis, ducimus quos nemo alias? Numquam suscipit soluta ullam obcaecati vitae consequatur possimus fugit.',
+      },
+      {
+        id: 4,
+        title: 'Note Title',
+        message:
+          'Nam quis sollicitudin nisi, at suscipit est. Suspendisse neque lectus, luctus hendrerit quam ac, lobortis placerat lorem. Nulla facilisi. Aliquam iaculis placerat quam, nec porta ipsum. Duis porttitor vehicula magna fermentum lobortis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras dignissim lorem mi, at pretium massa dignissim nec. Nam auctor tortor et tortor rhoncus, a aliquam sem mollis. Ut volutpat risus nibh, at fermentum dolor volutpat quis.',
+      },
+      {
+        id: 5,
+        title: 'Note Title',
+        message:
+          'Fusce elementum commodo egestas. Sed venenatis ac sapien ac cursus. Nunc porttitor sit amet lectus sed commodo. Aliquam id lacus at nibh iaculis tempus. Sed faucibus vel felis sit amet luctus. Aliquam congue ...',
+      },
+      {
+        id: 6,
+        title: 'Note Title',
+        message:
+          'Nunc in turpis at eros placerat blandit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce tincidunt egestas consectetur. Duis erat tellus, viverra ut dapibus ut, ultricies ac neque. Aenean venenatis quam sit amet congue facilisis. Nulla a laoreet justo.',
+      },
+    ],
+  };
+
   render() {
     return (
       <div className="LambdaNotes">
         <div className="App">
           <div className="sidebar">
             <h1 className="title">Lambda Notes</h1>
-          <div className="button">View Your Notes</div>
-          <div className="button">+ Create New Note</div>
+            <div className="button">View Your Notes</div>
+            <div className="button">+ Create New Note</div>
           </div>
         </div>
-      <ul className="notes">
-        <li className="list-notes">
-          <div className="note-title">{this.state.title}</div>
-          <div><hr width="100%"></hr></div>
-          <div className="note-message">{this.state.message}</div>
-        </li>
-        <li className="list-notes">
-          <div className="note-title">{this.state.title}</div>
-          <div><hr width="100%"></hr></div>
-          <div className="note-message">{this.state.message}</div>
-        </li>
-        <li className="list-notes">
-          <div className="note-title">{this.state.title}</div>
-          <div><hr width="100%"></hr></div>
-          <div className="note-message">{this.state.message}</div>
-        </li>
-        <li className="list-notes">
-          <div className="note-title">{this.state.title}</div>
-          <div><hr width="100%"></hr></div>
-          <div className="note-message">{this.state.message}</div>
-        </li>
-        <li className="list-notes">
-          <div className="note-title">{this.state.title}</div>
-          <div><hr width="100%"></hr></div>
-          <div className="note-message">{this.state.message}</div>
-        </li>
-        <li className="list-notes">
-          <div className="note-title">{this.state.title}</div>
-          <div><hr width="100%"></hr></div>
-          <div className="note-message">{this.state.message}</div>
-        </li>
-        <li className="list-notes">
-          <div className="note-title">{this.state.title}</div>
-          <div><hr width="100%"></hr></div>
-          <div className="note-message">{this.state.message}</div>
-        </li>
-        <li className="list-notes">
-          <div className="note-title">{this.state.title}</div>
-          <div><hr width="100%"></hr></div>
-          <div className="note-message">{this.state.message}</div>
-        </li>
-        <li className="list-notes">
-          <div className="note-title">{this.state.title}</div>
-          <div><hr width="100%"></hr></div>
-          <div className="note-message">{this.state.message}</div>
-        </li>
-        <li className="list-notes">
-          <div className="note-title">{this.state.title}</div>
-          <div><hr width="100%"></hr></div>
-          <div className="note-message">{this.state.message}</div>
-        </li>
-        <li className="list-notes">
-          <div className="note-title">{this.state.title}</div>
-          <div><hr width="100%"></hr></div>
-          <div className="note-message">{this.state.message}</div>
-        </li>
-      </ul>
+        <div className="yournotes-header">Your Notes:</div>
+        <ul className="notes">
+          {this.state.lnotes.map(lnote => {
+            return (
+              <li key={lnote.id} className="list-notes">
+                <div className="note-title">{lnote.title}</div>
+                <div>
+                  <hr width="100%" />
+                </div>
+                <div className="note-message">{lnote.message}</div>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     );
   }
