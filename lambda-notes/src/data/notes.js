0138844c6a97e2646b7515c00694b2102f5f0594
Tyson Williams <tyson.j.williams@gmail.com>
2018-02-28 16:13:13 -0700

    Recommitting mock notes database

---
+++ lambda-notes/src/data/notes.js
@@ -0,0 +1,49 @@
+const notes = [
+  {
+    title: 'Note 1',
+    content: 'Contents of Note 1',
+    id: 1,
+  },
+  {
+    title: 'Note 2',
+    content: 'Contents of Note 2',
+    id: 2,
+  },
+  {
+    title: 'Note 3',
+    content: 'Contents of Note 3',
+    id: 3,
+  },
+  {
+    title: 'Note 4',
+    content: 'Contents of Note 4',
+    id: 4,
+  },
+  {
+    title: 'Note 5',
+    content: 'Contents of Note 5',
+    id: 5,
+  },
+  {
+    title: 'Note 6',
+    content: 'Contents of Note 6',
+    id: 6,
+  },
+  {
+    title: 'Note 7',
+    content: 'Contents of Note 7',
+    id: 7,
+  },
+  {
+    title: 'Note 8',
+    content: 'Contents of Note 8',
+    id: 8,
+  },
+  {
+    title: 'Note 9',
+    content: 'Contents of Note 9',
+    id: 9,
+  },
+];
+
+export default notes;
