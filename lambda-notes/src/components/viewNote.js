72ee69b36f70c2d46485623aa5ac5c606f498a62
Arkoma <aaron@adburk.net>
2018-02-28 11:56:57 -0600

    add edit and delete btns to viewnote component

--- lambda-notes/src/components/viewNote.js
+++ lambda-notes/src/components/viewNote.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const ViewNote = (props) => {
     console.log("NoteView props: ", props);
     return (
-      <div>
-        <h1>
+      <div className="view-note-container">
+        <div className="edit-button">
+          <NavLink className="edit-link" to="/edit/">
+            edit
+          </NavLink>
+        </div>
+        <div className="delete-button"> delete</div>
+        <div className="view-note__title">
         {props.notes[props.match.params.id].title}
-        </h1>
-        <div>
+        </div>
+        <div className="view-note__body">
         {props.notes[props.match.params.id].body}
         </div>
       </div>
