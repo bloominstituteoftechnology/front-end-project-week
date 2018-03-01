ea3a542e296067dae6e331a2db7a0c532b6b9863
Arkoma <aaron@adburk.net>
2018-03-01 13:19:56 -0600

    added meaningful notes with instructions for the app

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -9,47 +9,38 @@ class App extends Component {
     notes: [
       {
           id: 0,
-          title: "First Note",
-          body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptatem, error nihil molestias ratione voluptatibus dolorem consequuntur.",
+          title: "Welcome",
+          body: "Welcome to the Lambda Notes App.  This is a sample note. You can write as many notes as you like.",
       },
       {
           id: 1,
-          title: "Second Note",
-          body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptatem, error nihil molestias ratione voluptatibus dolorem consequuntur.",
+          title: "Get a list",
+          body: "You'll notice on the left of your screen there are two buttons. These are the main buttons for the app. Click on \"View Your Notes\" to see your list of notes.",
       },
       {
           id: 2,
-          title: "Third Note",
-          body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptatem, error nihil molestias ratione voluptatibus dolorem consequuntur.",
+          title: "Create Notes",
+          body: "Below the \"View Your Notes\" button there is a \"+Create New Note\" button. You can create as many notes as you like.",
       },
       {
         id: 3,
-        title: "Fourth Note",
-        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptatem, error nihil molestias ratione voluptatibus dolorem consequuntur.",
+        title: "Edit Notes",
+        body: "Oops. You forgot to add a detail.  No problem. Just click on the note you want to change. On the page that follows you can click an edit button that will allow you to edit your note as you see fit.",
       },
       {
         id: 4,
-        title: "Fifth Note",
-        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptatem, error nihil molestias ratione voluptatibus dolorem consequuntur.",
+        title: "Delete Notes",
+        body: "No longer need that note? No problem. Click on the note preview and you will be presented with an option to delete that note.",
       },
       {
         id: 5,
-        title: "Sixth Note",
-        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptatem, error nihil molestias ratione voluptatibus dolorem consequuntur.",
+        title: "WARNING",
+        body: "This is just a demo. If you leave or refresh this app you will lose all your notes.  We are currently working on a version that will actually save your notes.",
       },
       {
         id: 6,
-        title: "Seventh Note",
-        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptatem, error nihil molestias ratione voluptatibus dolorem consequuntur.",
-      },
-      {
-        id: 7,
-        title: "Eighth Note",
-        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptatem, error nihil molestias ratione voluptatibus dolorem consequuntur.",
-      },      {
-        id: 8,
-        title: "Ninth Note",
-        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptatem, error nihil molestias ratione voluptatibus dolorem consequuntur.",
+        title: "Features Upcoming",
+        body: "We are working on several new features such as: search, markdown support, sorting options, tags, drag sorting, checklists, exporting to CSV, secure login.",
       },
   ],
   }
