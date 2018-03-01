b64f177a953df2f5c4fe8e23a17c93dfcd253994
lillialskye <33971312+lillialskye@users.noreply.github.com>
2018-02-28 19:58:07 -0500

    Tiffany Robbins
    For some reason it would not let me git add 
    fatal: not a git repository <or any of the parent directories
    So I used the upload button
    -on the app only Lambda notes appears

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+/*import logo from './logo.svg'; */
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+         </header>
+          <section className="App-section">
+          <h1 className="App-title">Lambda Notes</h1> 
+       </section>
+       
+      </div>
+    );
+  }
+}
+
+export default App;
