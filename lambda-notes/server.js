5f3b0c49ee007758dc87913fd56b6a7db0f3c43b
rcs784 <rcstinson@hotmail.com>
2018-03-01 16:07:25 -0500

    list view working with ellipsis

--- lambda-notes/server.js
+++ lambda-notes/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-let nextId = 7;
+let nextId = 10;
 
 function getNewId() {
   return nextId++;
@@ -39,6 +39,50 @@ let notes = [
     id: 6,
     title: 'And here\'s one in Russian',
     content: 'Съешь ещё этих мягких французских булок, да выпей же чаю. (Eat more of these soft French loaves and drink tea.)',
+  },
+  {
+    id: 7,
+    title: 'A palindrome',
+    content: 'A man, a plan, a canal: Panama!'
+  },
+  {
+    id: 8,
+    title: 'A really long palindrome',
+    content: `Dammit I’m mad.
+    Evil is a deed as I live.
+    God, am I reviled?
+    I rise, my bed on a sun, I melt.
+    To be not one man emanating is sad. I piss.
+    Alas, it is so late. Who stops to help?
+    Man, it is hot. I’m in it. I tell.
+    I am not a devil. I level “Mad Dog”.
+    Ah, say burning is, as a deified gulp,
+    In my halo of a mired rum tin.
+    I erase many men. Oh, to be man, a sin.
+    Is evil in a clam? In a trap?
+    No. It is open. On it I was stuck.
+    Rats peed on hope. Elsewhere dips a web.
+    Be still if I fill its ebb.
+    Ew, a spider… eh?
+    We sleep. Oh no!
+    Deep, stark cuts saw it in one position.
+    Part animal, can I live? Sin is a name.
+    Both, one… my names are in it.
+    Murder? I’m a fool.
+    A hymn I plug, deified as a sign in ruby ash.
+    A Goddam level I lived at.
+    On mail let it in. I’m it.
+    Oh, sit in ample hot spots. Oh wet!
+    A loss it is alas (sip). I’d assign it a name.
+    Name not one bottle minus an ode by me:
+    "Sir, I deliver. I’m a dog.”
+    Evil is a deed as I live.
+    Dammit I’m mad.`, 
+  },
+  {
+    id: 9,
+    title: 'A word-unit palindrome',
+    content: 'Girl, bathing on Bikini, eyeing boy, sees boy eyeing bikini on bathing girl.',
   },
 ];
 
--- lambda-notes/src/components/NoteItem.js
+++ lambda-notes/src/components/NoteItem.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './NoteItem.css';
 
-const NoteItem = (props) => {
-  const processRedirect = () => {
-    window.location.href = `http://localhost:3000/notes/${props.id}`;
-  };
+class NoteItem extends Component {
+  constructor(props) {
+    super(props);
+    this.processRedirect = this.processRedirect.bind(this);
+  }
+  
+  processRedirect() {
+    window.location.href = `http://localhost:3000/notes/${this.props.id}`;
+  }
 
-  return (
-    <div className="note" onClick={processRedirect}>
-      <div className="note__title">{props.title}</div>
-      <div className="note__content">{props.content}</div>
-      <div className="note__hide-overflow" />
-    </div>
-  );
+  ellipsize() {
+    var el = document.getElementById(`note__content--${this.props.id}`);
+    var wordArray = el.innerHTML.split(' ');
+    while (el.scrollHeight > el.offsetHeight) {
+      wordArray.pop();
+      el.innerHTML = wordArray.join(' ') + "&hellip;";
+    }
+  }
+
+  componentDidMount() {
+    const title = document.getElementById(`note__title--${this.props.id}`)
+    const titleHeight = window.getComputedStyle(title).getPropertyValue('height');
+    document.getElementById(`note__content--${this.props.id}`).style.height =
+      `calc(190px - ${titleHeight})`;
+    this.ellipsize();
+  }
+
+  render() {
+    return (
+      <div className="note" onClick={this.processRedirect}>
+        <div className="note__title" id={`note__title--${this.props.id}`}>{this.props.title}</div>
+        <div className="note__content" id={`note__content--${this.props.id}`}>{this.props.content}</div>
+      </div>
+    );
+  }
 };
 
 export default NoteItem;
