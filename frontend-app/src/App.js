da5eef172ed6e340529ff5ec1aad7591112c0420
das41236 <davsoudry@gmail.com>
2018-02-28 18:08:53 -0600

    updated css

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import NoteList from './components/NoteList';
