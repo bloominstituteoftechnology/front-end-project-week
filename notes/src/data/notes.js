2ada117fe34a2e3c5aae4bf194cdd918e73ea0e4
Dani Tacheny <dani.tacheny@gmail.com>
2018-03-01 11:03:29 -0600

    update style on note view page

--- notes/src/data/notes.js
+++ notes/src/data/notes.js
@@ -1,8 +1,8 @@
 const notes = [
   {
-    title: 'Note 1',
+    title: 'Note Name',
     content:
-      "Now we'll take the almighty fan brush. Just relax and let it flow. That easy. Trees cover up a multitude of sins. The very fact that you're aware of suffering is enough reason to be overjoyed that you're alive and can experience it. Let all these little things happen. Don't fight them. Learn to use them. If we're gonna walk though the woods, we need a little path.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris egestas mauris sed nibh vulputate, ac pharetra lacus aliquam. Duis malesuada justo a iaculis sagittis. Vestibulum ultrices ultricies arcu sit amet pharetra. Duis et lectus arcu. Morbi ornare dignissim dui, eu accumsan sapien lacinia et. Maecenas ultricies, ante in accumsan tempor, tortor diam vulputate elit, id finibus enim magna in massa. Suspendisse vel accumsan nisi. Vivamus elementum auctor ligula, at tempor nisl rutrum in. Sed in eros luctus ligula porta efficitur eu non nibh. Aliquam tellus ipsum, semper id cursus vel, posuere in dolor. Phasellus maximus lacinia dolor eget laoreet.\n\nMorbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis velit. Ut egestas, ante at lobortis ullamcorper, neque odio fringilla odio, non mattis elit lacus ut velit. Sed feugiat nibh vel molestie sollicitudin. Duis tincidunt porttitor sem, sit amet ultricies lacus pellentesque vel. Aenean quis enim placerat, posuere orci ac, condimentum tellus. Vivamus vitae sodales quam, eget ultricies lorem. Nam pellentesque massa nisl, at pellentesque nisi faucibus vitae. Curabitur sit amet turpis quam. Duis eget metus elementum, sollicitudin dui sed, accumsan dui. Donec ut est orci. Nunc fringilla purus sit amet posuere volutpat. Fusce vitae lectus id neque facilisis laoreet eget non odio. Praesent sed mauris porta, volutpat ante hendrerit, ultrices nisl.",
     id: 1,
   },
   {
