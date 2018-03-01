732d561c8452a9d0fc5b3b18f179325f479d54e8
Peter Gray <peter@petergraycreative.com>
2018-03-01 00:14:49 -0600

    Add ability to store state in local storage
    Borrowed from Dan Abramov's egghead.io video

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -3,16 +3,22 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import { rootReducer } from './reducers/reducer';
-// import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import App from './containers/App';
+import { loadState, saveState } from './localStorage';
 import registerServiceWorker from './registerServiceWorker';
 
+const persistedState = loadState();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
+  persistedState,
   composeEnhancers(applyMiddleware(logger))
 );
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 ReactDOM.render(
   <Provider store={store}>
