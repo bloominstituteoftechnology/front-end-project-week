ea654c2e33f931688acc4c280e1b69e378585685
Christopher Beards <beardscm@gmail.com>
2018-03-01 14:08:59 -0500

    Created Reducer

---
+++ lambda-notes/src/reducers/index.js
@@ -0,0 +1,41 @@
+import { ADD_NOTE, DELETE_NOTE } from '../actions';
+
+const initialState = {
+  notes: [
+    {
+      id: 0,
+      title: 'I Am The Title',
+      body: "I Am The Body",
+    },  
+  ],
+  counter: 1,
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_NOTE:
+      return {
+        ...state,
+        notes: [
+          ...state.notes,
+          {
+            title: action.payload.title,
+            body: action.payload.body,
+            id: state.counter,
+          },
+        ],
+        counter: ++state.counter,
+      };
+    case DELETE_NOTE:
+      return {
+        ...state,
+        notes: state.notes.filter(each => {
+          return each.id !== action.payload;
+        }),
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
