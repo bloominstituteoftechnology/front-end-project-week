3d2896dc7b38d899f9b0812744b8c68bb2550cee
Michael Marshal <mikemarshalman@gmail.com>
2018-03-01 21:06:12 -0600

    view note + more styling

--- notes-app/src/components/App.js
+++ notes-app/src/components/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Nav from './NavBar';
 import ViewNotes from './ViewNotes';
 import CreateNote from './CreateNote'
+import ViewSingleNote from './ViewSingleNote'
 
 import './App.css';
 
@@ -16,6 +17,11 @@ class App extends Component {
           <Route path="/" component={ViewNotes} exact />
           <Route path="/viewnotes" component={ViewNotes} exact />
           <Route path="/createnote" component={CreateNote} exact />
+          <Route path="/viewsinglenote/:id"
+            render={props => {
+              return <ViewSingleNote id={props.match.params.id} />}
+            }
+          />
         </div>
       </Router>
     );
--- notes-app/src/components/MultNote.js
+++ notes-app/src/components/MultNote.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function MultNote(props) {
   return (
-    <div className="ViewNotes__Note">
-      <div className="ViewNotes__Note__Title">
-        <h1>{props.title}</h1>
-      </div>
-      <div>
-        <h2>{props.meat}</h2>
-      </div>
-    </div>
+    <NavLink to={`/viewsinglenote/${props.title}`}>
+      <button className="ViewNotes__Note">
+        <div className="ViewNotes__Note__Title">
+          <h1>{props.title}</h1>
+        </div>
+        <div>
+          <h2>{props.meat}</h2>
+        </div>
+      </button>
+    </NavLink>
   );
 }
 
