c0446a60c9931ad1b9fb5fe7e797c268dbc08cf3
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-03-02 15:16:57 -0500

    Delete modal now closes on backdrop click

--- lambda_notes/src/components/Modal/Modal.js
+++ lambda_notes/src/components/Modal/Modal.js
@@ -6,12 +6,20 @@ class Modal extends React.Component {
     if (this.props.isOpen === false) return null;
     return (
       <div>
-        <div className="Backdrop"/>
+        <div className="Backdrop" onClick={event => this.close(event)}/>
         <div className="Modal">
           <div>{this.props.children}</div>
         </div>
       </div>
     )
+  }
+
+  close(event) {
+    event.preventDefault()
+
+    if (this.props.onClose) {
+      this.props.onClose()
+    }
   }
 }
 
