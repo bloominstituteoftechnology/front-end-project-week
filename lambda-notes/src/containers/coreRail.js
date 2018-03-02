05a8d63423ec02d3c2b1ecfa4ad3bb18591ae16c
Arkoma <aaron@adburk.net>
2018-03-02 09:20:24 -0600

    fix scroll position bug
    add scrollToTop component that uses componentDidUpdate to listen for
      route change and then change the window location.

--- lambda-notes/src/containers/coreRail.js
+++ lambda-notes/src/containers/coreRail.js
@@ -4,36 +4,39 @@ import NewNote from '../components/newNote';
 import ViewNote from '../components/viewNote';
 import EditNote from '../components/editNote';
 import DeleteNote from '../components/deleteNote';
+import ScrollToTop from '../components/scrollToTop';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
 
 const CoreRail = (props) => {
     return (
       <div className="core-rail">
-        <Router>
-          <div>
-            <div className="left-top-btn">
-              <NavLink className="navButton-top" to="/">View Your Notes</NavLink>
+        <Router onUpdate={() => window.scrollTo(0, 0)}>
+          <ScrollToTop>
+            <div>
+              <div className="left-top-btn">
+                <NavLink className="navButton-top" to="/">View Your Notes</NavLink>
+              </div>
+              <div className="left-bottom-btn">
+                <NavLink className="navButton-bottom" to="/new">+ Create New Note </NavLink>
+              </div>
+              <Route render={(routeProps) => (
+                      <NoteList {...routeProps} {...props} />
+                  )} path="/" exact/>
+              <Route render={(routeProps) => (
+                      <NewNote {...routeProps} {...props} />
+                  )} path="/new"/>
+              <Route render={(routeProps) => (
+                      <ViewNote {...routeProps} {...props} />
+                  )} path="/note/:id/:title/:body" />
+              <Route render={(routeProps) => (
+                      <EditNote {...routeProps} {...props} />
+                  )} path="/edit/:id/:title/:body" />
+              <Route render={(routeProps) => (
+                      <DeleteNote {...routeProps} {...props} />
+                  )} path="/delete/:id/:title/:body" />
             </div>
-            <div className="left-bottom-btn">
-              <NavLink className="navButton-bottom" to="/new">+ Create New Note </NavLink>
-            </div>
-            <Route render={(routeProps) => (
-                    <NoteList {...routeProps} {...props} />
-                )} path="/" exact/>
-            <Route render={(routeProps) => (
-                    <NewNote {...routeProps} {...props} />
-                )} path="/new"/>
-            <Route render={(routeProps) => (
-                    <ViewNote {...routeProps} {...props} />
-                )} path="/note/:id/:title/:body" />
-            <Route render={(routeProps) => (
-                    <EditNote {...routeProps} {...props} />
-                )} path="/edit/:id/:title/:body" />
-            <Route render={(routeProps) => (
-                    <DeleteNote {...routeProps} {...props} />
-                )} path="/delete/:id/:title/:body" />
-          </div>
+          </ScrollToTop>
         </Router>
       </div>
     )
