ce9c99f8b6ad57dc9bee834290905a8090526d25
Giraud Julemis <giraud.julemis@gmail.com>
2018-03-02 10:19:35 -0500

    update app component

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import uuid from 'uuid';
+import { connect } from 'react-redux';
+import { addNote } from './actions';
+import NoteList from './components/NoteList';
 
 class App extends Component {
   state = {
@@ -15,19 +18,32 @@ class App extends Component {
     this.setState({
       title: '',
       message: ''
-    })
-  }
+    });
+  };
   updateNote = (event) => {
-
-  }
+    this.setState({
+      title: event.target.value,
+      message: event.target.value
+    });
+  };
   render() {
     console.log(this.state.notes);
     return (
       <div className="App container">
-
+        <form onSubmit={this.createNote}>
+          <input onChange={this.updateNote} type="text" placeholder="Note Title" value={this.state.title} />
+          <textarea onChange={this.updateNote} placeholder="Note Message" value={this.state.message}></textarea>
+        </form>
+        <NoteList notes={this.props.notes} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    notes: state.notes
+  }
+};
+
+export default connect(mapStateToProps, { addNote })(App);
