34814c1bb5ee7baef2ecf94b55bbd32dd6b35a52
Amanda Phillips <amanda@phillipsdev.org>
2018-03-02 10:18:01 -0600

    fixed view note issue that would not allow new note to be viewed

--- lambda-notes/src/components/singleNote.js
+++ lambda-notes/src/components/singleNote.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-const SingleNote = props => {
-  console.log("props", props);
-  const id = props.match.params.id - 1;
-  return (
-    <div>
-      <h3 className="heading">{props.notes[id].title}</h3>
-      <p>{props.notes[id].text}</p>
-      <div>Edit</div>
-      <div>Delete</div>
-    </div>
-  );
+class SingleNote extends Component {
+  state = {
+    title: "",
+    text: "",
+    id: "",
+  };
+  render() {
+    console.log("props", this.props);
+    const id = this.props.match.params.id - 1;
+    return (
+      <div>
+        <h3 className="heading">{this.props.notes[id].title}</h3>
+        <p>{this.props.notes[id].text}</p>
+        <div>Edit</div>
+        <div>Delete</div>
+      </div>
+    );
+  }
+}
+const mapStateToProps = state => {
+  return {
+    current: state.current,
+    notes: state.notes,
+  };
 };
-
-export default SingleNote;
+export default connect(mapStateToProps)(SingleNote);
