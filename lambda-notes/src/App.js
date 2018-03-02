d6e9888ea9460a7c5488879715237b279375131c
rcs784 <rcstinson@hotmail.com>
2018-03-02 01:46:23 -0500

    debug commit 3

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -8,28 +8,6 @@ import Edit from './components/Edit';
 import './App.css';
 
 class App extends Component {
-  state = {
-    deleteId: null,
-  }
-
-  toggleDeleteModal(id) {
-    document.getElementById('delete-modal').classList.toggle('delete-modal--display');
-    document.getElementById('opaque-shield').classList.toggle('opaque-shield--display');
-    this.setState({
-      deleteId: this.state.deleteId === null ? id : null,
-    });
-  }
-
-  processDelete() {
-    axios.delete(`http://localhost:4444/notes/${this.state.deleteId}`).then(() => {
-      window.location.href = '/';
-    })
-    .catch((error) => {
-      alert('Server error: Please try again later.');
-      this.toggleDeleteModal(this.state.deleteId);
-    });
-  }
-  
   render() {
     return (
       <Router>
@@ -53,13 +31,6 @@ class App extends Component {
               processDelete={this.processDelete} />
             <Route path="/create" component={Create} />
             <Route path="/edit/:id" component={Edit} />
-          </div>
-          <div className="delete-modal" id="delete-modal">
-            <div className="delete-modal__warning">Are you sure you want to delete this?</div>
-            <div className="delete-modal__delete-button" onClick={this.processDelete}>Delete</div>
-            <div className="delete-modal__cancel-button" onClick={this.toggleDeleteModal(this.state.deleteId)}>
-              No
-            </div>
           </div>
           <div className="opaque-shield" id="opaque-shield" />
         </div>
