e810bbcd44377873a6b0b4165d035bf29c6f8727
Dani Tacheny <dani.tacheny@gmail.com>
2018-03-02 12:30:35 -0600

    add sort notes case to reducer

--- notes/src/reducer/index.js
+++ notes/src/reducer/index.js
@@ -8,14 +8,26 @@ import {
   TOGGLE_MODAL,
   DELETE_NOTE,
   SELECT_NOTE,
+  SORT_NOTES,
 } from '../actions';
 
 const initialState = {
   notes: notes,
   id: notes.length + 1,
   modalVisible: false,
-  selectedNote: {}
+  selectedNote: {},
+  sortType: 'id',
 };
+
+const sortNotes = (notes, sortType, prop) => {
+  let notesCopy = notes.slice(0);
+  if (sortType === prop) return notesCopy.reverse();
+  return notesCopy.sort((a,b) => {
+    if (a[prop] < b[prop]) return -1;
+    if (a[prop] > b[prop]) return 1;
+    return 0;
+  })
+}
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
@@ -55,6 +67,13 @@ const reducer = (state = initialState, action) => {
         ...state,
         selectedNote: state.notes.filter(note => note.id === action.payload)[0],
       }
+    case SORT_NOTES:
+    console.log(action);
+    return {
+      ...state,
+      notes: sortNotes(state.notes, state.sortType, action.payload),
+      sortType: action.payload,
+    }
     default:
       return state;
   }
