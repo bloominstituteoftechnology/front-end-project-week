7bc3906a7ac198374b8dbaa2b6d7d96d03293de2
Giraud Julemis <giraud.julemis@gmail.com>
2018-03-02 08:27:22 -0500

    created sidebar component

---
+++ lambda-notes/src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Lambda Notes</h1>
+        ...
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
