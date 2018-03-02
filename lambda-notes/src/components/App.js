656bbfbee325a93f066aeb7db0cd1f019ec07a49
Giraud Julemis <giraud.julemis@gmail.com>
2018-03-02 08:37:24 -0500

    edited wrong path

--- lambda-notes/src/components/App.js
+++ lambda-notes/src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import uuid from 'uuid';
-import CreateNote from './components/CreateNote';
+import CreateNote from './CreateNote';
 import NotesList from './NotesList';
 
 class App extends Component {
