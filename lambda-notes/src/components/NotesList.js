3843058cb320a7d2c24298d6e4cf6acbc30acdb2
Giraud Julemis <giraud.julemis@gmail.com>
2018-03-05 05:30:56 -0500

    create notes list component

---
+++ lambda-notes/src/components/NotesList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class NotesList extends React.Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <nav id="sidebar">
+          <div className="sidebar-header">
+            <h3>Lambda Notes</h3>
+          </div>
+          ...
+        </nav>
+      </div>
+    )
+  }
+}
+
+export default NotesList;
