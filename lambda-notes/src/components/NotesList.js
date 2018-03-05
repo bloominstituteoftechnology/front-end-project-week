b4bb2e76892fff47b322d2f71941dc4663c441f2
Giraud Julemis <giraud.julemis@gmail.com>
2018-03-05 07:50:57 -0500

    Styled sidebar

--- lambda-notes/src/components/NotesList.js
+++ lambda-notes/src/components/NotesList.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 
 class NotesList extends React.Component {
   render() {
     return (
       <div className="wrapper">
         <nav id="sidebar">
-          <div className="sidebar-header">
-            <h3>Lambda Notes</h3>
+          <div id="sidebar-header">
+            <h1>Lambda Notes</h1>
           </div>
-          ...
+          <ul>
+            <li>
+              <a className="btn btn-info btn-block" href="/">View Notes</a>
+            </li>
+            <li>
+              <a className="btn btn-info btn-block" href="/create">+ Create New Note</a>
+            </li>
+          </ul>
         </nav>
+        <div id="content">
+          <p>Header here...</p>
+        </div>
       </div>
     )
   }
