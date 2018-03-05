88804b98a72cf6857d122325f6f350489cc8ca8d
Giraud Julemis <giraud.julemis@gmail.com>
2018-03-05 06:11:41 -0500

    update entry point

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router history={browserHistory} routes={routes} />, document.getElementById('root'));
