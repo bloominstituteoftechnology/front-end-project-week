42dda1c424440d21f379dea156ffb7566e883749
Giraud Julemis <giraud.julemis@gmail.com>
2018-03-05 05:18:12 -0500

    reset index

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducer from './reducers';
 
-ReactDOM.render(
-  <Provider store={createStore(reducer)}>
-    <App />
-  </Provider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
