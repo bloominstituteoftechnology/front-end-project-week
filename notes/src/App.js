55e139e67c99c7b1bf80c74846ce251f470ff885
Kevin Jolley <30558808+Serrowxd@users.noreply.github.com>
2018-03-26 14:12:43 -0500

    Adjustments // Reactstrap/Bootstrap
    Removed Semantic, going to be more headache than I need for this project.
    
    Added Reactstrap/Bootstrap here.

--- notes/src/App.js
+++ notes/src/App.js
@@ -3,12 +3,9 @@ import './App.css';
 
 import Sidebar from './components/Sidebar/Sidebar.js';
 import Notes from './components/Notes/Notes.js';
-import CreateNew from './components/CreateNew.js';
-import NoteView from './components/NoteView';
-import EditNote from './components/EditNote';
-
-import 'semantic-ui-css/semantic.min.css';
-
+import CreateNew from './components/CreateNew/CreateNew.js';
+import NoteView from './components/NoteView/NoteView.js';
+import EditNote from './components/EditNote/EditNote.js';
 
 class App extends Component {
   render() {
