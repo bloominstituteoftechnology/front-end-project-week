ba49124bfce1778dc800740040c34b0749723157
mjd02 <mostdarryl@gmail.com>
2018-03-26 13:47:14 -0400

    better css of sidebar

--- notes/src/Components/View/View.js
+++ notes/src/Components/View/View.js
@@ -1,17 +1,19 @@
 import React, {Component} from 'react';
 import {Grid,Row,Col,Button} from 'react-bootstrap';
+import "./View.css";
 
 class View extends Component{
   render(){
     return(
-      <Grid>
+      <Grid className="grid">
         <Row className="show-grid">
-          <Col xs={4} md={4} lg={4}>
-            <div>
+          <Col xs={4} md={4} lg={4} className="col">
+            <div className="side-bar">
               <h1>Lambda</h1>
-              <h1>Note</h1>
-              <Button>View Your Notes</Button>
-              <Button>+ Create New Note</Button>
+              <h1 id="note">Note</h1>
+              <Button className="btn">View Your Notes</Button>
+              <br/>
+              <Button className="btn">+ Create New Note</Button>
             </div>
           </Col>
         </Row>
