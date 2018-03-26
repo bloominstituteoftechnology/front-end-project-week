2a9b2960b5ada88d1720563badff34de1f551501
Kevin Jolley <30558808+Serrowxd@users.noreply.github.com>
2018-03-26 12:18:17 -0500

    Framework
    Page Framework

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Sidebar from './components/Sidebar/Sidebar.js';
+import Notes from './components/Notes/Notes.js';
+import CreateNew from './components/CreateNew.js';
+import NoteView from './components/NoteView';
+import EditNote from './components/EditNote';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Sidebar />
+        <Notes />
+        <CreateNew />
+        <NoteView />
+        <EditNote />
       </div>
     );
   }
---
+++ notes/src/components/CreateNew/CreateNew.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CreateNew extends Component {
+  render() {
+    return(
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default CreateNew;
