6a6268b955088a9a84ceeb136551cd633534f11d
unknown <adfaris@gmail.com>
2018-03-26 18:59:16 -0400

    Working on the Note List to display all Notes

--- lambda_notes/src/components/NoteList.js
+++ lambda_notes/src/components/NoteList.js
@@ -1,44 +1,66 @@
 import React, { Component } from "react";
-import { Card, CardImg, CardText, CardBody,CardTitle, CardSubtitle, Button } from 'reactstrap';
-import { Container, Row, Col } from 'reactstrap';
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button
+} from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 import "./NoteList.css";
 
+class NoteList extends Component {
+  render() {
+    return (
+      <div className="NoteList">
+        
+       <Container className="Left_Container">
+       <span className="LeftSpan">Lambda Notes</span>
+       <Row>
+           <Col sm="3"><Button className="LeftBtn" color="info">View Your Notes</Button>{" "}</Col>
+           <Col sm ="3"><Button className="LeftBtn" color="info">+Create New Note</Button> {" "}</Col>
+            
+            
+       </Row>
+       </Container>
 
-class NoteList extends Component{
-render(){
-    return(
-    <div className="NoteList">
-    <span>Your Notes:</span>
-    <Container>
+        <Container className="Right_Container">
         <Row>
-            <Col xs="6">
-                <Card>
-                    <CardBody>
-                        <CardTitle>Note Title</CardTitle>
-                        <CardText> Morbi pellentesque euismod ventetatis. Nulla ut nibh nunc, Phasellus diam metus, blandic ac puris a, efficitr mollis </CardText>
-                    </CardBody>
-                </Card>
+        <span className= "RightSpan">Your Notes:</span>
+        <Col sm="4">
+          <Card className="Notes">
+              <CardTitle>Note Title</CardTitle>
+              <CardText>
+                {" "}Morbi pellentesque euismod ventetatis. Nulla ut nibh nunc,
+                Phasellus diam metus, blandic ac puris a, efficitr mollis{" "}
+              </CardText>
+          </Card>
 
-                <Card>
-                    <CardBody>
-                        <CardTitle>Card title</CardTitle>
-                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                    </CardBody>
-                </Card>
-            </Col>
+          <Card className="Notes">
+              <CardTitle>Note Title</CardTitle>
+              <CardText>
+                {" "}Morbi pellentesque euismod ventetatis. Nulla ut nibh nunc,
+                Phasellus diam metus, blandic ac puris a, efficitr mollis{" "}
+              </CardText>
+          </Card>
 
-            <Col>
-            <Card>
-                    <CardBody>
-                        <CardTitle>Card title</CardTitle>
-                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                    </CardBody>
-                </Card>
-            </Col>
-        </Row>
-    </Container>
-    </div>
-        );
-    }
+          <Card className="Notes">
+              <CardTitle>Note Title</CardTitle>
+              <CardText>
+                {" "}Morbi pellentesque euismod ventetatis. Nulla ut nibh nunc,
+                Phasellus diam metus, blandic ac puris a, efficitr mollis{" "}
+              </CardText>
+          </Card>
+
+
+        </Col>
+    </Row>
+          
+        </Container>
+      </div>
+    );
+  }
 }
 export default NoteList;
