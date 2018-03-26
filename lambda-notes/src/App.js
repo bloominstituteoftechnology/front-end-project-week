0dc2fe6f04eae8870d8ef5b4a03ae37be554416f
noxasaxon <saxon.h@outlook.com>
2018-03-26 09:32:42 -0700

    initialized

---
+++ lambda-notes/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <div className="App" />;
+  }
+}
+
+export default App;
