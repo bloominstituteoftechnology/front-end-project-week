71c1582421543abab90e0a9657da954855559506
csstone12 <36163530+csstone12@users.noreply.github.com>
2018-03-26 17:33:37 -0400

    Stone Chris | Front End Project
    Stone Chris | Front End Project

--- front-end-project/src/App.js
+++ front-end-project/src/App.js
@@ -1,18 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className='container'>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Lambda Notes</h1>
+          <Button className='App-Intro'>
+            View Your Notes
+          </Button>
+          <Button className='App-Intro'>
+            + Create New Note
+          </Button>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </div>
+        <div className='rightSide'>
+            <p className='RS-title'>Your Notes:</p>
+            <div className = 'allCards'>
+            <div className='card1'>
+              <t className='t'>Note Title</t>
+              <b className='b'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis …</b>
+            </div>
+            <div className='card2'>
+              <t className='t'>Note Title</t>
+              <b className='b'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis …</b>
+            </div>
+            <div className='card3'>
+              <t className='t'>Note Title</t>
+              <b className='b'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis …</b>
+            </div>
+            <div className='card4'>
+              <t className='t'>Note Title</t>
+              <b className='b'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis …</b>
+            </div>
+            <div className='card5'>
+              <t className='t'>Note Title</t>
+              <b className='b'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis …</b>
+            </div>
+            <div className='card6'>
+              <t className='t'>Note Title</t>
+              <b className='b'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis …</b>
+            </div>
+            <div className='card7'>
+              <t className='t'>Note Title</t>
+              <b className='b'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis …</b>
+            </div>
+            <div className='card8'>
+              <t className='t'>Note Title</t>
+              <b className='b'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis …</b>
+            </div>
+            <div className='card9'>
+              <t className='t'>Note Title</t>
+              <b className='b'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis …</b>
+            </div>
+            </div>
+        </div>
       </div>
     );
   }
