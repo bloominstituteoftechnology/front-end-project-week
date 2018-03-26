9caa17d74eabadef0c69f458c808f53985341ef7
mvpache <mvpache@msn.com>
2018-03-26 10:35:55 -0700

    buttons added with styles, no functionality

--- LambdaNotes/src/components/SideBar.jsx
+++ LambdaNotes/src/components/SideBar.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Button } from 'reactstrap';
 
 class SideBar extends Component {
   render() {
     return (
       <div className='sideBar-Contents'>
         <h1>Lambda Notes</h1>
+        <Button className='sideBar-Button view'>View Your Notes</Button>
+        <Button className='sideBar-Button create'>+ Create New Note</Button>
       </div>
     );
   }
