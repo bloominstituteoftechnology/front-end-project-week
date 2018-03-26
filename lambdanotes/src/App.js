bb90424fe5a5b54045b02daf5ee5cd40365dece6
Juber Ahmed <a.juber@hotmail.com>
2018-03-26 18:42:40 -0400

    Displaying a list of notes

--- lambdanotes/src/App.js
+++ lambdanotes/src/App.js
@@ -1,11 +1,66 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Button } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Lambda Notes! #NeverForget
+      <div class="container">
+        <div className="listView">
+          <div className="titleLambda">Lambda</div>
+          <div className="titleNotes">Notes</div>
+          <div className="lambdabutton">
+            <Button color="info">View your notes</Button>
+          </div>
+          <div className="lambdabutton">
+            <Button color="info">+ Create New Note</Button>{' '}
+          </div>
+        </div>
+
+        <div className="notesView">
+          <div className="notes">
+            <h3>Title</h3>
+            <h4>Notes</h4>
+          </div>
+
+          <div className="notes">
+            <h3>Title</h3>
+            <h4>Notes</h4>
+          </div>
+          <div className="notes">
+            <h3>Title</h3>
+            <h4>Notes</h4>
+          </div>
+          <div className="notes">
+            <h3>Title</h3>
+            <h4>Notes</h4>
+          </div>
+
+          <div className="notes">
+            <h3>Title</h3>
+            <h4>Notes</h4>
+          </div>
+
+          <div className="notes">
+            <h3>Title</h3>
+            <h4>Notes</h4>
+          </div>
+
+          <div className="notes">
+            <h3>Title</h3>
+            <h4>Notes</h4>
+          </div>
+
+          <div className="notes">
+            <h3>Title</h3>
+            <h4>Notes</h4>
+          </div>
+
+          <div className="notes">
+            <h3>Title</h3>
+            <h4>Notes</h4>
+          </div>
+        </div>
       </div>
     );
   }
