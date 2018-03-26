3a60bf3f224429505fc54b796985c67d0055c171
mvpache <mvpache@msn.com>
2018-03-26 13:11:14 -0700

    Creat Note Form setup, not functional

--- LambdaNotes/src/components/contentViews/CreateNote.jsx
+++ LambdaNotes/src/components/contentViews/CreateNote.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import { connect }  from 'react-redux';
+import { Button, Form, FormGroup, Input } from 'reactstrap';
 
 class CreateNote extends Component {
   render() {
     return (
       <div>
         <h1>Create New Note: </h1>
+        <Form>
+          <FormGroup>
+            <Input name="title" placeholder="Note Title"/>
+            <Input type="textarea" name="text" placeholder="Note Content"/>
+            <Button>Save</Button>
+          </FormGroup>
+        </Form>
       </div>
     )
   }
 }
+
+//need action
+//need local state
+
 
 export default CreateNote;
