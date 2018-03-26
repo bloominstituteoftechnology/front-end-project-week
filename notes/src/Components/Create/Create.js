94529c2f386a1c6e731ac88f33fa24c41a5d821b
mjd02 <mostdarryl@gmail.com>
2018-03-26 14:22:10 -0400

    created router and create note component

---
+++ notes/src/Components/Create/Create.js
@@ -0,0 +1,13 @@
+import "./Create.css";
+import React, {Component} from 'react';
+
+class Create extends Component{
+  render()
+  {
+    return(
+    <h1>test</h1>
+    );
+  }
+}
+
+export default Create;
--- notes/src/index.js
+++ notes/src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import View from './Components/View/View.js';
+import Create from './Components/Create/Create.js'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Router>
+  <div>
+    <Route exact path="/" component={View} />
+    <Route path="/create" component={Create} />
+  </div>
+</Router>
+, document.getElementById('root'));
 registerServiceWorker();
