5148edef30586a552252616b8db1a9a5b1973f46
mjd02 <mostdarryl@gmail.com>
2018-03-26 13:38:07 -0400

    completed side bar

--- notes/src/Components/View/View.js
+++ notes/src/Components/View/View.js
@@ -1,18 +1,23 @@
 import React, {Component} from 'react';
-
+import {Grid,Row,Col,Button} from 'react-bootstrap';
 
 class View extends Component{
   render(){
     return(
-    <h1>
-    test
-    </h1>
-
+      <Grid>
+        <Row className="show-grid">
+          <Col xs={4} md={4} lg={4}>
+            <div>
+              <h1>Lambda</h1>
+              <h1>Note</h1>
+              <Button>View Your Notes</Button>
+              <Button>+ Create New Note</Button>
+            </div>
+          </Col>
+        </Row>
+      </Grid>
     );
-
   }
-
 }
-
 
 export default View;
