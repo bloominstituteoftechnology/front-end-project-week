7bb0a7a90d152d43c6c0f13c9622bf567ab5f60a
zackhitch <zackhitch@users.noreply.github.com>
2018-03-26 14:42:32 -0700

    Added one Route for ListNotes - ZH

--- lambda_notes/src/Components/App.js
+++ lambda_notes/src/Components/App.js
@@ -3,6 +3,7 @@ import '../Styles/App.css';
 
 import Navigation from './Navigation/Navigation';
 import ListNotes from './ListNotes/ListNotes';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -13,7 +14,7 @@ class App extends Component {
             <Navigation />
           </div>
           <div className="listCol">
-            <ListNotes />
+            <Route exact path="/" component={ListNotes} />
           </div>
         </div>
       </Fragment>
