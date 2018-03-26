3c236a03b8d9c3e09ce49e11a124f1ae0726d428
Kevin Jolley <30558808+Serrowxd@users.noreply.github.com>
2018-03-26 15:07:36 -0500

    Sidebar WIP
    Implemented buttons for the sidebar and text, creating a grid next.

--- notes/src/components/Sidebar/Sidebar.js
+++ notes/src/components/Sidebar/Sidebar.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { Container, Row, Col, Button } from 'reactstrap';
+
+import './Sidebar.css';
 
 class Sidebar extends Component {
   render() {
     return(
-      <div>
-
-      </div>
+      <Container className="container">
+        <Row>
+          <div className="SidebarText"> Lambda Notes </div>
+        </Row>
+        <div className="SidebarButtons">
+          <Row className="buttonRow1">
+            <Button color="primary" className="Button1"> View Your Notes </Button>{' '}
+          </Row>
+          <Row classname="buttonRow2">
+            <Button color="primary" className="Button2"> + Create New Note </Button>{' '}
+          </Row>
+        </div>
+      </Container>
     );
   }
 }
