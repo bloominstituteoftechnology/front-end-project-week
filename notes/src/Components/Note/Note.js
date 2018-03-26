ea55688a2f29c9ce32a7cabcf206913a66cb3936
mjd02 <mostdarryl@gmail.com>
2018-03-26 18:21:48 -0400

    successfully implemented redux notes to view

--- notes/src/Components/Note/Note.js
+++ notes/src/Components/Note/Note.js
@@ -5,7 +5,7 @@ class Note extends Component{
   render(){
     return(
           <Panel className="notePanel">
-            <Panel.Body>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida neque pulvinar elit semper  Suspendisse rutrum velit ...</Panel.Body>
+            <Panel.Body>{this.props.text}</Panel.Body>
           </Panel>
     );
   }
--- notes/src/Components/View/View.js
+++ notes/src/Components/View/View.js
@@ -1,18 +1,22 @@
 import React, {Component} from 'react';
 import {Grid,Row,Col,Button} from 'react-bootstrap';
 import "./View.css";
+import {add} from '../../actions';
 import {Link} from 'react-router-dom';
 import Note from "../Note/Note.js";
 import {connect} from 'react-redux';
 
 class View extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      test:true
-    }
-  }
   componentDidMount(){
+    this.props.add('blahlbah',0);
+    this.props.add('blahlbah',1);
+    this.props.add('blahlbah',2);
+    this.props.add('blahlbah',3);
+    this.props.add('blahlbah',4);
+    this.props.add('blahlbah',5);
+    this.props.add('blahlbah',6);
+    this.props.add('blahlbah',7);
+    this.props.add('blahlbah',8);
   }
   render(){
       return(
@@ -32,42 +36,24 @@ class View extends Component{
             <Col xs={9} md={9} lg={9} className="col">
               <div className="main">
                 <Grid className="grid">
-                  <Row className="show-grid">
-                    <h3 id="mainHeader">Your Notes:</h3>
-                  </Row>
-                  <Row className="show-grid">
-                    <Col xs={4} md={4} lg={4} className="col">
-                      <Note/>
-                    </Col>
-                    <Col xs={4} md={4} lg={4} className="col">
-                      <Note/>
-                    </Col>
-                    <Col xs={4} md={4} lg={4} className="col">
-                      <Note/>
-                    </Col>
-                  </Row>
-                  <Row className="show-grid">
-                    <Col xs={4} md={4} lg={4} className="col">
-                      <Note/>
-                    </Col>
-                    <Col xs={4} md={4} lg={4} className="col">
-                      <Note/>
-                    </Col>
-                    <Col xs={4} md={4} lg={4} className="col">
-                      <Note/>
-                    </Col>
-                  </Row>
-                  <Row className="show-grid">
-                    <Col xs={4} md={4} lg={4} className="col">
-                      <Note/>
-                    </Col>
-                    <Col xs={4} md={4} lg={4} className="col">
-                      <Note/>
-                    </Col>
-                    <Col xs={4} md={4} lg={4} className="col">
-                      <Note/>
-                    </Col>
-                  </Row>
+                  {this.props.info.notes.map( (e,i)=>{
+                    if(i%3 === 0 && i !== 0){
+                      return(
+                      <div key={i}>
+                        <Row className="show-grid"></Row>
+                        <Col key={i} xs={3} md={3} lg={3} className="col">
+                          <Note text = {e.text}/>
+                        </Col>
+                      </div>
+                    )}
+                    else{
+                      return(
+                      <Col key={i} xs={3} md={3} lg={3} className="col">
+                        <Note text = {e.text}/>
+                      </Col>
+                    )}
+                  })
+                }
                 </Grid>
               </div>
             </Col>
@@ -83,4 +69,4 @@ const mapStateToProps = (state) =>{
   }
 }
 
-export default connect(mapStateToProps)(View);
+export default connect(mapStateToProps,{add})(View);
