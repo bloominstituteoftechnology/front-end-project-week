9cb732ffead3fb26fcb1cc49ffc4304bef782370
mvpache <mvpache@msn.com>
2018-03-26 14:46:29 -0700

    create note works

--- LambdaNotes/src/components/contentViews/CreateNote.jsx
+++ LambdaNotes/src/components/contentViews/CreateNote.jsx
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
 import { connect }  from 'react-redux';
 import { Button, Form, FormGroup, Input } from 'reactstrap';
+import { addNote } from '../../actions';
 
 class CreateNote extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      title: '',
+      text: ''
+    };
+  }
+
+  handleInput = (event) => {
+    if (event.target.name === 'title') {
+      this.setState({ title: event.target.value });
+    } else {
+      this.setState({ text: event.target.value });
+    }
+  }
+
+  submitNote = (event) => {
+    event.preventDefault();
+    this.props.addNote(this.state);
+    this.setState({
+      title: '',
+      text: ''
+    });
+  };
+
   render() {
     return (
       <div>
         <h1>Create New Note: </h1>
-        <Form>
+        <Form onSubmit={this.submitNote}>
           <FormGroup>
-            <Input name="title" placeholder="Note Title"/>
-            <Input type="textarea" name="text" placeholder="Note Content"/>
+            <Input name="title" type="text" onChange={this.handleInput}placeholder="Note Title"/>
+            <Input type="textarea" name="text" type="text" onChange={this.handleInput} placeholder="Note Content"/>
             <Button>Save</Button>
           </FormGroup>
         </Form>
@@ -19,8 +46,6 @@ class CreateNote extends Component {
   }
 }
 
-//need action
-//need local state
+//need input handler
 
-
-export default CreateNote;
+export default connect(null, { addNote })(CreateNote);
--- LambdaNotes/src/reducers/index.js
+++ LambdaNotes/src/reducers/index.js
@@ -1,15 +1,34 @@
 import defaultNotes from './defaultNotes';
 
-let idCount = 7;
+import { ADD_NOTE } from '../actions';
+
+let nextId = 7;
 
 const intialState = {
-  nextId: idCount,
+  idCount: nextId,
   notes: defaultNotes
 }
 
 export default (state = intialState, action) => {
   switch (action.type) {
+    case ADD_NOTE:
+    const newNote = {
+        id: state.idCount++,
+        title: action.title,
+        text: action.text
+    }
+      return { ...state, notes: state.notes.concat(newNote) };
     default:
       return state;
   }
 }
+
+/*export const ADD_NOTE = 'ADD_NOTE';
+
+export const addNote = (note) => {
+  return {
+    type: ADD_NOTE,
+    title: note.title,
+    text: note.text
+  };
+}*/
