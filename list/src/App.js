3db57d36e9e4347f62c8d8f305aa4bc1d98ec3ea
arcadia-ego <electronics.crawford@gmail.com>
2018-03-27 08:47:02 -0700

    working on display

--- list/src/App.js
+++ list/src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
 import './App.css';
 
+
 class App extends Component {
+
+  state = {
+    notes: [{id: 0, title:'Title1', content: 'testContent1'}, {id: 1, title: 'Title2', content: 'testContent2'}],
+    newNote: {id: 0, title: '', content: ''}
+  }
+
+  componentDidMount() {
+    this.setState();
+  }
+
+  handleChange = event => {
+    console.log(this.state)
+    this.setState({ [event.target.name]: event.target.value});
+  }
+
+  addNote = (event) => {
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.notes.map(( note => {
+          return(
+            <div key={note.id} className="charCard">
+              {note.id}{note.title}{note.content}
+            </div>
+          )
+        }))}
+
+        <form>
+          <input onChange={this.handleChange} type="text" name="title" value={this.state.newNote.title} ></input>
+          <input onChange={this.handleChange} type="text" name="content" value={this.state.newNote.content} ></input>
+          <button onClick={this.addNote} ></button>
+
+        </form>
       </div>
     );
   }
