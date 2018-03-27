6573893a315c60dd75b8d648881803fd13f10674
nourmhassan <nour-pop1@hotmail.com>
2018-03-27 12:59:25 -0700

    nav,list title

--- lambda-notes2/src/App.js
+++ lambda-notes2/src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
+import ListView from './components/ListView';
+import NavBar from './components/NavBar';
+import AddNote from './components/AddNote';
+import EditNote from './components/EditNote';
+import Note from './components/Note';
 
 class App extends Component {
+
+  state = {
+    notes: [ 
+        { title: 'Note Title 1', text: 'Lambda School trains people to be software engineers at no up-front cost.', id: 0 },
+        { title: 'Note Title 2', text: 'Lambda School trains people to be software engineers at no up-front cost.', id: 1 },
+        { title: 'Note Title 3', text: 'Lambda School trains people to be software engineers at no up-front cost.', id: 2 },
+        { title: 'Note Title 4', text: 'Lambda School trains people to be software engineers at no up-front cost.', id: 3 },
+        { title: 'Note Title 5', text: 'Lambda School trains people to be software engineers at no up-front cost.', id: 4 },
+        { title: 'Note Title 6', text: 'Lambda School trains people to be software engineers at no up-front cost.', id: 5 },
+    ],
+}
+
   render() {
     return (
       <div className="App">
-          <h1 className="App-title">Welcome to React</h1>
+        <Route path="/" component={NavBar} />
+        <Route exact path="/" render={(props) => <ListView {...props} notes={this.state.notes} />} />
+        <Route exact path="/AddNote" component={AddNote} />
+        <Route exact path="/notes/:id" component={Note} />
+        <Route exact path="/notes/:id/EditNote" component={EditNote} />
       </div>
     );
   }
---
+++ lambda-notes2/src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = props => {
+    return (
+        <div className="NavBar">
+            <h1>Lambda Notes</h1>
+            <Link to="/">View Your Notes</Link>
+            <Link to="/AddNote">+ Create New Note</Link>
+        </div>
+    );
+};
+
+    
+export default NavBar;
