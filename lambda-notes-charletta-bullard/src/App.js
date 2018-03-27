7f5dfaa248808108c768712108b2b4eba58f3950
Charletta <charlettabullard12@outlook.com>
2018-03-27 14:17:05 -0400

    Installed Redux, React-Redux, Redux-Thunk

--- lambda-notes-charletta-bullard/src/App.js
+++ lambda-notes-charletta-bullard/src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import SideBar from './components/pages/SideBar'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import SideBar from './components/pages/SideBar';
+import ViewNotesList from './components/pages/ViewNotesList';
 
 
 class App extends Component {
   state = {
     notes: getNotes(),
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className='App'>
+          <Route path='/' component={SideBar} />
+          <Route path='/ViewNotesList' render={state => <ViewNotesList notes={this.state.notes} exact />} />
+        </div>
+      </Router>
+    );
   }
 
 
