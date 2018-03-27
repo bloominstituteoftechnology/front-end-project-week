20b2f4ddd3fe268e76d0f022648e45d21707a743
DragonRydrz <DragonRydrz@me.com>
2018-03-27 11:47:08 -0400

    fonts inported from google

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import App from "./App";
 import thunk from "redux-thunk";
@@ -7,7 +8,6 @@ import logger from "redux-logger";
 import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.css";
 
 import rootReducer from "./reducers";
 
