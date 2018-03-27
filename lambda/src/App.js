b94450191788db733db4eeb1fe37661aa810ce91
unknown <bonifacemtk15@yahoo.com>
2018-03-27 11:19:23 -0400

    wip

---
+++ lambda/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import ListView from './components/ListView';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+    <ListView  />
+      
+     </div>
+    );
+  }
+}
+
+export default App;
