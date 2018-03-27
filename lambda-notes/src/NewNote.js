4a6d7a139a24e90315b3c2afe761d02b42f422ab
astro11 <aquila.strother@asu.edu>
2018-03-27 13:47:07 -0400

    styled New Note View form fields

--- lambda-notes/src/NewNote.js
+++ lambda-notes/src/NewNote.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import { Button } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import Buttons  from './Buttons';
+import './NewNote.css';
 
 class NewNote extends React.Component {
     render() {
       return (
         <div>
-          <Link to="/">
+          <div className="">        
+        <div className="row no-gutters">
+          <div className="sideBar col-2">
+          <span className="lambdaNotes">Lambda <br /> Notes</span>
+          <span className="buttons"><Buttons /></span>
+          </div>
+            <div className="noteSection col-md-10">
+            {/* <Link to="/">
             <button>Back Home</button>
-              </Link>
-                <form>
-                  <input placeholder="Note Title" />
-                  <input placeholder=" Note Content" />
+              </Link> */}
+            <span className="yourNotes">Create New Note:</span>            
+                <form className="Form">
+                  <input className="noteTitle" type="text" placeholder="Note Title" /><br />
+                  <p><textarea className="noteContent" cols="60" rows="10" placeholder="Note Content"></textarea></p>
                 <Button type="submit">Save</Button>
-              </form>        
+              </form>  
+            </div>  
+        </div>
+      </div>          
+                
         </div>
       );
     }
