ed652c3bbc2670fcfe1236fccfd76050c8f51ba2
astro11 <aquila.strother@asu.edu>
2018-03-27 18:41:23 -0400

    added handleChange event on title and note description input

--- lambda-notes/src/NewNote.js
+++ lambda-notes/src/NewNote.js
@@ -6,6 +6,20 @@ import './Buttons.css';
 import { Link } from 'react-router-dom';
 
 class NewNote extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      noteTitle: '',
+      noteDescription: ''
+    }
+  }
+
+  handleChange = (event) => {
+    const { name , value } = event.target;
+    this.setState({ [name]: value })
+    console.log(this.state);
+  }
+
     render() {
       return (
         <div>
@@ -18,8 +32,8 @@ class NewNote extends React.Component {
                 <div className="noteSection col-md-10">
                   <span className="yourNotes">Create New Note:</span>            
                     <form className="Form">
-                      <input className="noteTitle" name="title" type="text" placeholder="Note Title" /><br />
-                        <p><textarea className="noteContent" cols="60" rows="10" placeholder="Note Content"></textarea></p>
+                      <input name="noteTitle" value={this.state.noteTitle} onChange={this.handleChange} className="noteTitle" type="text" placeholder="Note Title" /><br />
+                        <p><textarea name="noteDescription" onChange={this.handleChange} className="noteContent" cols="60" rows="10" placeholder="Note Content"></textarea></p>
                           <Link to="/Save"><Button className="BTN btn btn-info" type="submit">Save</Button></Link>
                     </form>  
                 </div>  
