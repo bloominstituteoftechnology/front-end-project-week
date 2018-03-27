496384461ca05e187c449025c1a591e6e39cac33
Charletta Bullard <charlettabullard12@outlook.com>
2018-03-27 15:30:20 -0400

    Added Actions and Reducers folder/Redux Setup in index.js

--- lambda-notes-charletta-bullard/src/index.js
+++ lambda-notes-charletta-bullard/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
 import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { rootReducer } from './reducers';
 
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-<BrowserRouter>
+<Provider store={store}>
   <App />, 
-</BrowserRouter>,
+</Provider>,
 document.getElementById('root'));
 
