8a3c8a2e231ade1f2dbe8e0e75c6e5e5a2c1bf57
Edward Manda <lilkups@yahoo.com>
2018-03-27 19:47:32 -0500

    wip

--- notes/src/components/note.js
+++ notes/src/components/note.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { 
     Row,
     Col, 
@@ -22,16 +23,24 @@ class Note extends React.Component {
 
     render() {
         return (
-            <Col xs="9">
-            {(this.state.Note) ? (
-            <Row className="mb-5" >
-                <Col >
-                    <H3>{this.state.Note.title}</H3>
-                    <p>{this.state.Note.content}</p>
+            <React.Fragment>
+                {(this.state.Note) ? (
+                <Col xs="9">
+                <Row>
+                    <Col className="d-flex justify-content-end mr-5 mt-3">
+                        <Link className= "mr-2" to={`/note/edit/${this.state.Note.id}`} >edit</Link>
+                        <Link className= "ml-2" to={`/note/${this.state.Note.id}`} >delete</Link>                        
+                    </Col>
+                </Row>
+                <Row className="mb-5" >
+                    <Col >
+                        <H3>{this.state.Note.title}</H3>
+                        <p>{this.state.Note.content}</p>
+                    </Col>
+                </Row>
                 </Col>
-            </Row>
-            ) : ('') }  
-            </Col>
+                ) : ('') }  
+            </React.Fragment>
         );
     }
 
