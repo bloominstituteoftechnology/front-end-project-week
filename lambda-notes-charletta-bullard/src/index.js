a15fa16a23e0398e0c533bce08cdfbc15110ab41
Charletta Bullard <charlettabullard12@outlook.com>
2018-03-28 16:32:31 -0400

    added redux dev tools extension

--- lambda-notes-charletta-bullard/src/index.js
+++ lambda-notes-charletta-bullard/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { rootReducer } from './reducers/reducers';
 
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
 <Provider store={store}>
