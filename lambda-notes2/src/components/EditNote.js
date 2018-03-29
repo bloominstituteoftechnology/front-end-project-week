873fcd6495dbf8dff1d1695b52447734ab6e208d
nourmhassan <nour-pop1@hotmail.com>
2018-03-29 12:55:08 -0700

    working in styling

--- lambda-notes2/src/components/EditNote.js
+++ lambda-notes2/src/components/EditNote.js
@@ -21,8 +21,8 @@ class EditNote extends Component {
             <div className="EditNote">
                 <h4 className="Title">Edit Note:</h4>
                 <form className="Form">
-                    <input className="Inputtext" type='text' name='name' placeholder='New Title' onChange={this.handleChange}></input>
-                    <input className="Inputfield" type='text' name='text' placeholder='New Content' onChange={this.handleChange}></input>
+                    <input className="InputTitel" type='text' name='name' placeholder='New Title' onChange={this.handleChange}></input>
+                    <input className="InputContent" type='text' name='text' placeholder='New Content' onChange={this.handleChange}></input>
                     <button className="Button" onClick={this.handleSubmit}>Update</button>
                 </form>
             </div>
