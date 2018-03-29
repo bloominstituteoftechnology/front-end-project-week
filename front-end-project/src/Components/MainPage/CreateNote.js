7a3b2d937a3a433014aeea914b6234d533a1f5e6
csstone12 <36163530+csstone12@users.noreply.github.com>
2018-03-29 11:31:25 -0400

    Stone Chris | Front End Project

---
+++ front-end-project/src/Components/MainPage/CreateNote.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './CreateNote.css';
+
+let idCounter = 0;
+
+class CreateNote extends React.Component {
+  state = {
+    title: '',
+    content: '',
+  }
+
+  render() {
+    return (
+      <div>
+        <form className="note__form" onSubmit={this.addNewNote}>
+          <input name="title" className="note__title" type="text" placeholder="Note Title" onChange={this.handleChange} value={this.state.title}/>
+          <textarea name="content" className="note__content" cols="60" rows="10" type="text" placeholder="Note Content" onChange={this.handleChange} value={this.state.content}></textarea>
+          <button className="note__button" type="submit">Save</button>
+        </form>
+      </div>
+    );
+  }
+
+  handleChange = (event) => {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  addNewNote = (event) => {
+    event.preventDefault();
+    const newNote = Object.assign({}, this.state);
+    newNote.id = idCounter;
+    this.props.addNote(newNote);
+    this.setState({title: '', content: ''});
+  }
+}
+
+export default CreateNote
---
+++ front-end-project/src/Components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = (props) => {
+  return (
+    <div className="navbar__container">
+      <div className="title">
+        <div>Lambda</div>
+        <div>Notes</div>
+      </div>
+      <div>
+        <button className="navBar__button" onClick={() => props.changeSwitch('Your Notes:','noteList')} >View Your Notes</button>
+      </div>
+      <div>
+        <button className="navBar__button" onClick={() => props.changeSwitch('Create New Note:', 'createNote')} >+ Create New Note</button>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
