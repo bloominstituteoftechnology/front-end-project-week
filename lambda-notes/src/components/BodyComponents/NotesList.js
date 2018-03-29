0d5f0c3797bb2bfa5ab6c4544ea767aedebf2da0
DragonRydrz <DragonRydrz@me.com>
2018-03-29 17:09:55 -0400

    can download current user notes as .csv file

--- lambda-notes/src/components/BodyComponents/NotesList.js
+++ lambda-notes/src/components/BodyComponents/NotesList.js
@@ -2,11 +2,18 @@ import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import { Card, CardTitle, CardText } from "reactstrap";
 import { Link } from "react-router-dom";
+import { CSVLink } from "react-csv";
 
 class NotesList extends Component {
   render() {
     return (
       <Fragment>
+        <div
+          style={{ textDecoration: "underline" }}
+          className="d-flex justify-content-end"
+        >
+          <CSVLink data={this.props.notes}>Download notes as CSV File</CSVLink>
+        </div>
         <h3 className="mt-5 ml-3">Your Notes:</h3>
         <div className="d-flex flex-wrap justify-content-around">
           {/* <CardColumns> */}
