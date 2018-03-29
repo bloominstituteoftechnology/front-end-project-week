2ab47a17eb3d75f65d24cb520dfb1a1469c4f950
Viveca Valdez <vivecavaldez@Vivecas-MBP.fios-router.home>
2018-03-29 18:44:45 -0400

    Some CSS done WIP

--- notes/src/App.js
+++ notes/src/App.js
@@ -6,13 +6,33 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
+        <div className="Sidebar-container">
+          <div className="Side-title">Lambda Notes</div>
+          <button name="button1" type="button">View Your Notes</button>
+          <br />
+          <button name="button2" type="button">+ Create New Note</button>
+        </div>
+        <div className="Notes-section-container">
+          <div className="Notes-title">Your Notes:</div>
+          <div className="Notes-container">
+            <div className="Box1 Notes">hi</div>
+            <div className="Box2 Notes">hi</div>
+            <div className="Box3 Notes">hi</div>
+            <div className="Box4 Notes">hi</div>
+            <div className="Box5 Notes">hi</div>
+            <div className="Box6 Notes">hi</div>
+            <div className="Box7 Notes">hi</div>
+            <div className="Box8 Notes">hi</div>
+            <div className="Box9 Notes">hi</div>
+          </div>
+        </div>
       </div>
     );
   }
