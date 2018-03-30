24b9afb6b53d6b1aa202a19697a81bd40a480138
Charletta Bullard <charlettabullard12@outlook.com>
2018-03-30 12:34:48 -0400

    Added styling to the side bar component

--- lambda-notes-charletta-bullard/src/components/pages/SideBar.js
+++ lambda-notes-charletta-bullard/src/components/pages/SideBar.js
@@ -1,5 +1,6 @@
 import React  from 'react';
 import { Link } from 'react-router-dom';
+import './SideBar.css';
 
 const SideBar = () => {
   
@@ -7,10 +8,10 @@ const SideBar = () => {
       <div className='sidebar'>
         <h1 className='title'>Lambda Notes</h1>
         <Link to='/NotesList'>
-          <button className='view-btn'>View Your Notes</button>
+          <button className='buttons'>View Your Notes</button>
         </Link>
         <Link to='/NoteForm'>
-          <button className='create-btn'>+Create New Note</button>
+          <button className='buttons'>+Create New Note</button>
         </Link>
       </div>
     );
