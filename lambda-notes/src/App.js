6e1d49daeefe4b66405af6b2cf26f881550e5cae
DragonRydrz <DragonRydrz@me.com>
2018-03-30 15:55:50 -0400

    removed Redirect from App.js.

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -10,7 +10,7 @@ import {
   Form,
   Input
 } from "reactstrap";
-import { Route, withRouter, Redirect } from "react-router-dom";
+import { Route, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { createUser } from "./actions/createUser";
 import { login } from "./actions/login";
