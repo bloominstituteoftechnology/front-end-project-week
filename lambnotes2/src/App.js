aeddcda84e228b6158638a43cec9a2d9b3157eaf
Tiffany Robbins-Cabell <lillialskye@yahoo.com>
2018-03-30 11:34:31 -0400

    Tiffany Robbins

---
+++ lambnotes2/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+import './noteForm.js'
+
+class App extends Component {
+  render() {
+    return (
+   <div className="App">
+     <h1 className= "App-columnl">Lambda Notes</h1>
+     <h1 className= "App-columnr">Your Notes</h1>
+     <h1 className= "App-box">It's not easy being green</h1>
+     <h1 className= "App-box2">It's not easy being green</h1>
+     <h1 className= "App-box3">It's not easy being green</h1>
+     <h1 className= "App-box4">It's not easy being green</h1> 
+          </div>
+
+);
+}
+}
+
+
+
+
+       export default App;
