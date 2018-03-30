3a090ddc41fd652287d66ee83d6c5e9f2a8f4283
Liz McKenzie <elizabethbraggisgreat@gmail.com>
2018-03-30 14:56:03 -0400

    Started NoteView component, and fixed some styling

--- lambda-notes/src/components/SideNav.js
+++ lambda-notes/src/components/SideNav.js
@@ -8,8 +8,8 @@ const SideNav = props => {
         // https://reactstrap.github.io/components/layout/
         <div className="SideNav">
         <h1>Lambda Notes</h1>
-        <Button className="navButton" color="primary" size="lg"> View Your Notes </Button> <br />
-        <Button className="navButton" size="lg"> + Create New Note </Button>
+        <Button className="navButton" color="primary" size="lg" href="/" > View Your Notes </Button> <br />
+        <Button className="navButton" size="lg" href="./newnote"> + Create New Note </Button>
         </div>
     )
 }
