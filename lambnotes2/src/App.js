a1ca02a47b9e59f8f5cf14a261df65b4cd78266a
Tiffany Robbins-Cabell <lillialskye@yahoo.com>
2018-03-30 16:50:43 -0400

    Tiffany Robbins

--- lambnotes2/src/App.js
+++ lambnotes2/src/App.js
@@ -7,18 +7,43 @@ import './noteForm.js'
 class App extends Component {
   render() {
     return (
-   <div className="App">
+   
+
+<div className="App">
      <h1 className= "App-columnl">Lambda Notes</h1>
+
      <h1 className= "App-columnr">Your Notes</h1>
+
+
+<ul class = "navbar">
+  <li>< a href ="index.html"> View Your Notes </a>
+  </li>
+  <li>< a href ="index.html"> + Create New Note </a>
+  </li> 
+</ul>
+     
+<ul class = "notebar"> 
+      <li>< a href ="./noteForm.js"> Title </a>
+  </li>
+  <li>< a href ="./noteForm.js"> New Note </a>
+  </li> 
+</ul>
+     
+      <h1 className = "App-box">It's not easy being green </h1>
+     
      <h1 className= "App-box">It's not easy being green</h1>
      <h1 className= "App-box2">It's not easy being green</h1>
      <h1 className= "App-box3">It's not easy being green</h1>
      <h1 className= "App-box4">It's not easy being green</h1> 
           </div>
 
-);
+)
 }
 }
+
+
+
+
 
 
 
