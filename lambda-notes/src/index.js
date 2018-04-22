5d903633b5125c5b520365407ac02eb94e1919a7
Kyle Kauzlarich <kauzlarich.kyle@gmail.com>
2018-04-21 22:01:39 -0700

    index.js in src/ added in the import for provider from redux, nested the App call in the Provider

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>,
+document.getElementById('root'));
 registerServiceWorker();
