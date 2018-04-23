75a7f0a5ddbefbbe25dc40a47f75347066755e32
jeffrey <jeffreyneilflynn@gmail.com>
2018-04-23 13:29:50 -0400

    notes displayed on screen

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -9,7 +9,7 @@ import {
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App__container">
         <NavColumn />
         <PrimaryContainer />
       </div>
--- lambda-notes/src/components/primary-container.js
+++ lambda-notes/src/components/primary-container.js
@@ -1,7 +1,57 @@
 import React from 'react';
+import { Card, CardTitle, CardBody } from 'reactstrap';
 
 export default () => {
+  const tempCard = [
+    {
+      title: "Note Title",
+      content: "Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis ..."
+    },
+    {
+      title: "Note Title",
+      content: "Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis ..."
+    },
+    {
+      title: "Note Title",
+      content: "Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis ..."
+    },
+    {
+      title: "Note Title",
+      content: "Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis ..."
+    },
+    {
+      title: "Note Title",
+      content: "Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis ..."
+    },
+    {
+      title: "Note Title",
+      content: "Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis ..."
+    },
+    {
+      title: "Note Title",
+      content: "Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis ..."
+    },
+    {
+      title: "Note Title",
+      content: "Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis ..."
+    },
+    {
+      title: "Note Title",
+      content: "Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis ..."
+    },
+    
+  ]
   return (
-    <div></div>
+    <div className="PrimaryContainer">
+      <h1 className="PrimaryContainer__header">Your Notes:</h1>
+      <div className="PrimaryContainer__cardContainer">
+        {tempCard.map((note, index) => (
+          <Card key={index} className="col-4 NoteCard">
+            <CardTitle>{note.title}</CardTitle>
+            <CardBody>{note.content}</CardBody>
+          </Card>
+        ))}
+      </div>
+    </div>
   )
 }
