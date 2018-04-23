5ebe5d992de9a84cc2842e1167854f0d5da030f6
Harrison Brock <harrison@harrisonbrock.com>
2018-04-23 13:37:04 -0400

    Added MainMenu Component with Header and Buttons. Layout is not great.

--- lambdanotes/src/App.js
+++ lambdanotes/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MainMenu from './components/MainMenu/MainMenu';
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,9 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <MainMenu/>
       </div>
     );
   }
---
+++ lambdanotes/src/components/MainMenu/MainMenu.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Button, Row, Col} from 'reactstrap';
+class MainMenu extends React.Component {
+
+    render() {
+        return(
+            <div className='container'>
+                <Row>
+                    <Col>
+                        <h2>Lambda Notes</h2>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Button>View Your Notes</Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Button>+Create New Note</Button>
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
+}
+export default MainMenu;
