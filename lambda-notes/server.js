a46e597991a08cde44f97bb757ee8e97c30a34d6
Kyle Kauzlarich <kauzlarich.kyle@gmail.com>
2018-04-22 20:03:37 -0700

    added in another 8 notes for testing

--- lambda-notes/server.js
+++ lambda-notes/server.js
@@ -12,6 +12,54 @@ const notes = [
 		id: 0,
 		title: 'Test note 1',
 		body: 'Test note body 1',
+	},
+
+	{
+		id: 1,
+		title: 'Test note 2',
+		body: 'Test note body 2',
+	},
+
+	{
+		id: 2,
+		title: 'Test note 3',
+		body: 'Test note body 3',
+	},
+
+	{
+		id: 3,
+		title: 'Test note 4',
+		body: 'Test note body 4',
+	},
+
+	{
+		id: 4,
+		title: 'Test note 5',
+		body: 'Test note body 5',
+	},
+
+	{
+		id: 5,
+		title: 'Test note 6',
+		body: 'Test note body 6',
+	},
+
+	{
+		id: 6,
+		title: 'Test note 7',
+		body: 'Test note body 7',
+	},
+
+	{
+		id: 7,
+		title: 'Test note 8',
+		body: 'Test note body 8',
+	},
+
+	{
+		id: 8,
+		title: 'Test note 9',
+		body: 'Test note body 9',
 	}
 	
 ];
