1dc3a7191aa73008b3573418369f4eca12063439
Justin Hong <jhong2009@gmail.com>
2018-04-23 11:28:55 -0700

    in reducers/index.js: completed base reducer functions

--- LambdaNotes/src/reducers/index.js
+++ LambdaNotes/src/reducers/index.js
@@ -1,23 +1,47 @@
+// action types
+import {
+	ADDTODO,
+	DELETETODO,
+	UPDATETODO,
+	TOGGLETODO
+} from "../acions/index.js";
+
 const initialState = {
 	todos: [],
 	addingTodo: false,
-	editingTodo: false,
+	updatingTodo: false,
 	deletingTodo: false,
 	viewingTodo: false
 };
 
-const todosReducer = (state=initialState, action) => {
-	switch(action.type) {
+const todosReducer = (state = initialState, action) => {
+	switch (action.type) {
 		// add new todo
-		case:
-			return ;
+		case ADDTODO:
+			return Object.assign({}, state, {
+				// append new todo objects to the list
+				todos: [...state.todos, action.payload]
+			});
 		// edit new todo
-		case:
-			return ;
+		case UPDATETODO:
+			let updatedTodos = state.todos.map((todo, index) => {
+				if (index === action.payload.index) {
+					// only change the text of a todo if its index matches
+					todo.text = action.payload.text;
+				}
+			});
+			return Object.assign({}, state, {
+				todos: updatedTodos
+			});
 		// delete new todo
-		case:
-			return ;
-		default: 
+		case DELETETODO:
+			return Object.assign({}, state, {
+				todos: state.todos.filter((todo, index) => {
+					// return todos that don't match the target's index
+					return index !== action.payload;
+				})
+			});
+		default:
 			return state;
 	}
-}
+};
