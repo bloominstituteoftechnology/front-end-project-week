80a84def1922c9e9a4358abb0a8a7554d396c06d
Shawn Stewart <s.stewart0220@gmail.com>
2018-04-23 18:48:15 -0500

    Started working on the notes list component.

---
+++ lambda-notes/src/Components/NotesList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Content.css';
+import './NotesList.css';
+
+export const NotesList = () => {
+  return(
+    <div className='Content__Wrapper'>
+      <h3 className='Content__Heading'>Your Notes:</h3>
+      <div className='Content NotesList'>Hi</div>
+    </div>
+  );
+}
