fd08ca361fc5a0c4239fcc24f30fc776f183cfb0
JacobLeonlyerla <umasour@yahoo.com>
2018-04-23 14:16:40 -0700

    setting my list on state, not done yet

--- lambda-notes/src/components/App.js
+++ lambda-notes/src/components/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import {notes} from '../notes/notes.js';
 
 class App extends Component {
+state={
+  notes:[]
+}
+componentDidMount(){
+  this.setState(notes:notes)
+  console.log(this.state)
+}
   render() {
     return (
       <div className="App">
