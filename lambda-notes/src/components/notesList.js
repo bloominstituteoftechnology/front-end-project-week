fd893c749236d40c1db6623ef85de834e7aa1cf0
Mark Wong <fmarkwong@gmail.com>
2018-04-23 11:30:27 -0700

    List View: Set up Initial Component Structure

---
+++ lambda-notes/src/components/notesList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class NotesList extends Component {
+  render() {
+    return (
+      <div>
+        first note
+      </div>
+    );
+  }
+
+}
