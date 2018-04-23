4ab7685b62d8a2f6cd1f3efff537c628e36cb026
Jared Cuffe <jcuffe@gmail.com>
2018-04-23 16:42:34 -0700

    added styled-components dependency

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 // @flow
 
 import React, { Component } from 'react';
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+
+`
 
 class App extends Component<{}> {
   render() {
     return (
-      <div className="App">
-      </div>
+      <Wrapper>
+      </Wrapper>
     );
   }
 }
