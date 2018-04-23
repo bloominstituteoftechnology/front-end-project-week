3537f23f3e1de0dcd7132fab1419bb2ab3b5c25e
Justin Hong <jhong2009@gmail.com>
2018-04-23 10:56:06 -0700

    in src/index.js: set up store and middleware

--- LambdaNotes/src/index.js
+++ LambdaNotes/src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import Provider from "react-redux";
+import "./index.css";
+import App from "./App";
+import rootReducer from "./reducers.index";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+	<Provider>
+		<App />
+	</Provider>,
+	document.getElementById("root")
+);
 registerServiceWorker();
