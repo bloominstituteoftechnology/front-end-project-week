f90a6f4ff8385fe655863caccebda1136372ba5c
tanking <tanking@github.com>
2018-04-23 17:27:35 -0400

    Added note cards and have notes displayed, styled to match example

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className= 'App-notes'> 
+        <div className= 'App-feature'> 
           <h1> Lambda Notes </h1>
           <p className = 'App-buttons'> View Your Notes </p> 
           <p className = 'App-buttons'> +Create New Notes </p> 
         </div>
+        <div className='App-content'>
+          <h1> Your Notes: </h1>
+          <div className= 'App-notesDisplay'>
+            <p className='App-notes'> 
+            <h3> Note Title </h3>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, molestias porro. 
+            Reiciendis velit accusamus ea laborum, adipisci facere possimus fugiat incidunt ullam architecto ad beatae eos 
+            rem, perferendis ex alias.
+            </p>
+            <p className='App-notes'> 
+            <h3> Note Title </h3>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, molestias porro. 
+            Reiciendis velit accusamus ea laborum, adipisci facere possimus fugiat incidunt ullam architecto ad beatae eos 
+            rem, perferendis ex alias.
+            </p>
+            <p className='App-notes'> 
+            <h3> Note Title </h3>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, molestias porro. 
+            Reiciendis velit accusamus ea laborum, adipisci facere possimus fugiat incidunt ullam architecto ad beatae eos 
+            rem, perferendis ex alias.
+            </p>
+            <p className='App-notes'> 
+            <h3> Note Title </h3>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, molestias porro. 
+            Reiciendis velit accusamus ea laborum, adipisci facere possimus fugiat incidunt ullam architecto ad beatae eos 
+            rem, perferendis ex alias.
+            </p>
+            <p className='App-notes'> 
+            <h3> Note Title </h3>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, molestias porro. 
+            Reiciendis velit accusamus ea laborum, adipisci facere possimus fugiat incidunt ullam architecto ad beatae eos 
+            rem, perferendis ex alias.
+            </p>
+            <p className='App-notes'> 
+            <h3> Note Title </h3>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, molestias porro. 
+            Reiciendis velit accusamus ea laborum, adipisci facere possimus fugiat incidunt ullam architecto ad beatae eos 
+            rem, perferendis ex alias.
+            </p>
+            <p className='App-notes'> 
+            <h3> Note Title </h3>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, molestias porro. 
+            Reiciendis velit accusamus ea laborum, adipisci facere possimus fugiat incidunt ullam architecto ad beatae eos 
+            rem, perferendis ex alias.
+            </p>
+            <p className='App-notes'> 
+            <h3> Note Title </h3>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, molestias porro. 
+            Reiciendis velit accusamus ea laborum, adipisci facere possimus fugiat incidunt ullam architecto ad beatae eos 
+            rem, perferendis ex alias.
+            </p>
+            <p className='App-notes'> 
+            <h3> Note Title </h3>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, molestias porro. 
+            Reiciendis velit accusamus ea laborum, adipisci facere possimus fugiat incidunt ullam architecto ad beatae eos 
+            rem, perferendis ex alias.
+            </p>
+        </div>
+      </div>
       </div>
     );
   }
