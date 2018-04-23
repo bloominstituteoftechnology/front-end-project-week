346456b8b3964c3f94a94806f6539dab81cc6829
Justin M <jekm321@gmail.com>
2018-04-23 14:21:25 -0400

    still formatting folder structure, setting up future routing plans, redux and router blueprints being made, setting up possible links

---
+++ lambda-notes/src/Components/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import SideNav from './SideNav/SideNav';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Switch>
+          <Route path="/"
+          render = {(props) => <SideNav />} 
+          />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
