f03c3bd7bb386a90856a2e3e0a834254df24b9f1
Anthony <anthony@anthonys-mbp.lan>
2018-04-23 19:33:45 -0400

    Finished the nav bar

--- note-app/src/App.js
+++ note-app/src/App.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Combine into a single file at end
+import Header from './components/header/header.js';
+import NavBar from './components/navBar/navBar.js';
+
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+      <Header title='List View' />
+      
+      <NavBar />
+      
+      
       </div>
     );
   }
---
+++ note-app/src/components/header/header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './header.css';
+
+const Header = (props) => {
+    return <div className='Title'>{props.title}</div> }
+
+export default Header;
