740bfc57a48d04c8c751a94100eb11f1053a5011
Nedu Robert <nedumrobert@gmail.com>
2018-04-23 23:47:43 +0100

    added the App component

--- lambdanotes/src/App.js
+++ lambdanotes/src/App.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+// import './App.css';
+import NoteList from './components/NoteList';
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  width: 893px;
+  max-width: 100%;
+`
+
+const Heading = styled.h1`
+  color: #000;
+  font-size: 2.2em;
+  margin-top: 9px;
+  line-height: 1.1;
+  margin-bottom: 10px;
+  font-weight: bold;
+`;
+
+const Rule = styled.hr`
+  margin-top: 0;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Wrapper>
+        <Heading>List View</Heading>
+        <Rule />
+        <NoteList />
+      </Wrapper>
     );
   }
 }
