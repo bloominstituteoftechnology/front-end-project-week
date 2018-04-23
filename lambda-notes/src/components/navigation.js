fb727162d988ec8ba3202289b4ba7e989885e0a1
jeffrey <jeffreyneilflynn@gmail.com>
2018-04-23 11:11:37 -0400

    constructed nav column w/ basic styling

--- lambda-notes/src/components/navigation.js
+++ lambda-notes/src/components/navigation.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default () => {
   return (
     <div className="NavContainer">
-      <h2>Lambda Notes</h2>
-      <button>View Your Notes</button>
-      <button>+ Create New Note</button>
+      <h2 className="Nav__header">Lambda Notes</h2>
+      <button className="Button">View Your Notes</button>
+      <button className="Button">+ Create New Note</button>
     </div>
   )
 }
