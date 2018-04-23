c534825dc15d382b530d3a858b8d69a99f840f2b
JacobLeonlyerla <umasour@yahoo.com>
2018-04-23 14:30:45 -0700

    i got my list displaying on the screen so everything is set to state and i can map over it

--- lambda-notes/src/components/App.js
+++ lambda-notes/src/components/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 import {notes} from '../notes/notes.js';
-
+import Notes from "./ListView"
 class App extends Component {
-state={
-  notes:[]
-}
-componentDidMount(){
-  this.setState(notes:notes)
-  console.log(this.state)
-}
+  constructor(){
+    super();
+    this.state={
+      notes:[]
+    };
+  }
+  componentDidMount(){
+    this.setState({ notes: notes});
+    console.log(this.state)
+   }
+
   render() {
+    console.log(this.state)
     return (
       <div className="App">
-     test
+          <Notes  notes={this.state.notes}/>
       </div>
     );
   }
--- lambda-notes/src/components/ListView.js
+++ lambda-notes/src/components/ListView.js
@@ -1,2 +1,7 @@
 import React from "react";
-export const
+ const Notes = props => {
+    console.log("porps", props);
+    
+    return <div>{props.notes.map(note => <div>{`${note.title} ${note.textbody}`}</div>)}</div>
+}
+export default Notes
