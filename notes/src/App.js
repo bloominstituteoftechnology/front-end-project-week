86836e003555ad2c1adf75c79c71a9919cce2188
David Alexander Morales <damorales89@gmail.com>
2018-04-23 19:23:30 -0400

    more nonfunc components

--- notes/src/App.js
+++ notes/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
           <p className="App-intro">
             To get started, edit <code>src/App.js</code> and save to reload.
           </p>
-          <ListView />
+          <Route exact patch='./ListView' ListView />
         </Switch>
       </div>
     );
