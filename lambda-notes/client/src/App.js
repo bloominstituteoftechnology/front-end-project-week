63c915a5922166f139ad22bdf4b6f06694a7a1df
Kyle Kauzlarich <kauzlarich.kyle@gmail.com>
2018-04-22 18:50:33 -0700

    app.js and app.css added in the background to the app.css for left and right sides of the page, app.js i moved most of the added code to its own component files

--- lambda-notes/client/src/App.js
+++ lambda-notes/client/src/App.js
@@ -1,27 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
-const NoteDisplay = (props) => {
-  console.log('props', props)
-  return (
-    <div>
-    <h1> {props.data.name} </h1>
-    <p> {props.data.body} </p>
-    </div>
-  )
-}
+//import axios from 'axios';
+import NoteList from './components/Notes/NoteList'
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      notes: {
-        name: 'testing',
-        body: 'some testing text',
-      }
-    }
-  }
+
   render() {
     return (
       <div className='App__container'>
@@ -30,7 +14,7 @@ class App extends Component {
         </div> {/* end of left side div */}
 
         <div className='App__right'> {/* start of right side div */}
-        <NoteDisplay data={this.state.notes} />
+        <NoteList />
         </div> {/* end of right side div */}
 
       </div> // container div end
