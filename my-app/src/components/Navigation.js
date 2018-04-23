9fedc1786b0586e51914f1b2b3d2f3b82319404e
tanking <tanking@github.com>
2018-04-23 18:32:32 -0400

    Setting up Navigation for react-router

---
+++ my-app/src/components/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Navigation {
+    render() {
+        return (
+            <Link to= '/'> View Your Notes </Link>
+        )
+    }
+}
+
+export default Navigation;
