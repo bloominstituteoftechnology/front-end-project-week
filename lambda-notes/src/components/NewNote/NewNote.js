d017bc428a742e340a53b008bd2b881a25a6439f
Justin M <jekm321@gmail.com>
2018-04-23 17:22:17 -0400

    making form to add new notes

--- lambda-notes/src/components/NewNote/NewNote.js
+++ lambda-notes/src/components/NewNote/NewNote.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
 class NewNote extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            noteName: '',
+            noteBody: ''
+        };
+    }
+
+    handleInputChange = event => {
+        this.setState({ [event.target.name]: event.target.value })
+    }
 
     render() {
         return (
@@ -8,9 +19,26 @@ class NewNote extends React.Component {
                 <div className="directory__title mainContent__title" >
                     Create New Note:
                 </div>
-                <div className="mainContent__Form mainContent__content">
-                    Form will be here
-                </div>                
+                <div className="mainContent__content">
+                    <form className="mainContent__Form" onSubmit={this.createSmurf} >
+                        <input
+                            className="form__input form__input--title"
+                            onChange={this.handleInputChange}
+                            placeholder="Note Title"
+                            value={this.state.noteName}
+                            name="noteName"
+                        />
+                        <input
+                            className="form__input form__input--body"
+                            type="textarea"
+                            onChange={this.handleInputChange}
+                            placeholder="Note Content"
+                            value={this.state.noteBody}
+                            name="noteBody"
+                        />
+                        <button className="link__button" type="submit">Save</button>
+                    </form>
+                </div>
             </div>
         )
     }
