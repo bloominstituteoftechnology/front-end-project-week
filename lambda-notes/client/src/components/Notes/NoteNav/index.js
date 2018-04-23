468d1d106d2831a3a6906c2b374a540910bbae60
Kyle Kauzlarich <kauzlarich.kyle@gmail.com>
2018-04-23 15:39:48 -0700

    New component NoteNav this component is responsible for generating a list of routes to the indavidual notes, the css file is not used at this time

---
+++ lambda-notes/client/src/components/Notes/NoteNav/index.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './index.css';
+import { Route } from 'react-router-dom';
+import ViewNote from '../ViewNote';
+
+
+const NoteRoutes = (props) => {
+    console.log('props', props)
+    return (
+      <div>
+        {props.data.map((u, index) => 
+        <Route exact path={'/Notes/' + index} component={ViewNote} />
+        )}
+      </div>
+    )
+  }
+//<Route exact path={'/Notes/' + index} component={ViewNote} />
+
+class NoteNav extends Component {
+    constructor() {
+      super()
+      this.state = {
+        notes: []
+      }
+    }
+    theMount() {
+      axios
+        .get('http://localhost:5000/api/notes')
+        .then(response => {
+          this.setState(() => ({ notes: response.data }));
+        })
+        .catch(error => {
+          console.error('Server Error', error);
+        });
+    }
+    componentDidMount() {
+      this.theMount();
+    }
+  
+    render() {
+      return (
+          <div className='note__container-bg'>
+            <NoteRoutes data={this.state.notes} />
+          </div>
+      );
+    }
+  }
+
+  export default NoteNav;
