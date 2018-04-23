21c4fab68543f026df6284262085870a4fcb4b05
Shawn Stewart <s.stewart0220@gmail.com>
2018-04-23 18:46:24 -0500

    Set up the basic layout.

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 import { Navigation } from './Components/Navigation';
+import { NotesList } from './Components/NotesList'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
+        <Switch>
+          <Route exact path='/Notes' component={NotesList} />
+        </Switch>
       </div>
     );
   }
