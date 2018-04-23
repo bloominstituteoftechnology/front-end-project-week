eea83e44336657080369b37e72ca8c078a4d1402
JacobLeonlyerla <umasour@yahoo.com>
2018-04-23 13:49:51 -0700

    made an object full of the data i will need to put on state.

---
+++ lambda-notes/src/Notes/notes.js
@@ -0,0 +1,38 @@
+export const notes =[
+    {
+     "title":"Note Title",   
+     "textbody":"morbi pellentesque euismos venmenatis. Nulla ut nibH nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis ..."
+    },
+    {
+        "title":"Note Title",   
+        "textbody":"morbi pellentesque euismos venmenatis. Nulla ut nibH nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis ..."
+       },
+       {
+        "title":"Note Title",   
+        "textbody":"morbi pellentesque euismos venmenatis. Nulla ut nibH nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis ..."
+       },
+       {
+        "title":"Note Title",   
+        "textbody":"morbi pellentesque euismos venmenatis. Nulla ut nibH nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis ..."
+       },
+       {
+        "title":"Note Title",   
+        "textbody":"morbi pellentesque euismos venmenatis. Nulla ut nibH nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis ..."
+       },
+       {
+        "title":"Note Title",   
+        "textbody":"morbi pellentesque euismos venmenatis. Nulla ut nibH nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis ..."
+       },
+       {
+        "title":"Note Title",   
+        "textbody":"morbi pellentesque euismos venmenatis. Nulla ut nibH nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis ..."
+       },
+       {
+        "title":"Note Title",   
+        "textbody":"morbi pellentesque euismos venmenatis. Nulla ut nibH nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis ..."
+       },
+       {
+        "title":"Note Title",   
+        "textbody":"morbi pellentesque euismos venmenatis. Nulla ut nibH nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis ..."
+       },
+]
--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
+import App from './components/App';
 
 import registerServiceWorker from './registerServiceWorker';
 
