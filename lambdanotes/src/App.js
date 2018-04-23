30c48d52a7c2d553cf624cac2b8d7a681589b33e
Harrison Brock <harrison@harrisonbrock.com>
2018-04-23 19:57:39 -0400

    Fixed layout and spacing between main menu and list notes

--- lambdanotes/src/App.js
+++ lambdanotes/src/App.js
@@ -11,27 +11,27 @@ class App extends Component {
         super(props);
         this.state = {
             notes: [
-                {'title': 'Note 1', 'content': 'some test'},
-                {'title': 'Note 2', 'content': 'some test'},
-                {'title': 'Note 3', 'content': 'some test'},
-                {'title': 'Note 4', 'content': 'some test'},
-                {'title': 'Note 5', 'content': 'some test'},
-                {'title': 'Note 6', 'content': 'some test'},
-                {'title': 'Note 7', 'content': 'some test'},
-                {'title': 'Note 8', 'content': 'some test'},
-                {'title': 'Note 9', 'content': 'some test'},
-                {'title': 'Note 10', 'content': 'some test'},
-                {'title': 'Note 11', 'content': 'some test'},
-                {'title': 'Note 12', 'content': 'some test'},
-                {'title': 'Note 13', 'content': 'some test'},
-                {'title': 'Note 14', 'content': 'some test'},
-                {'title': 'Note 15', 'content': 'some test'}
+                {id:0,'title': 'Note 1', 'content': 'some test'},
+                {id:1,'title': 'Note 2', 'content': 'some test'},
+                {id:2,'title': 'Note 3', 'content': 'some test'},
+                {id:3,'title': 'Note 4', 'content': 'some test'},
+                {id:4,'title': 'Note 5', 'content': 'some test'},
+                {id:5,'title': 'Note 6', 'content': 'some test'},
+                {id:6,'title': 'Note 7', 'content': 'some test'},
+                {id:7,'title': 'Note 8', 'content': 'some test'},
+                {id:8,'title': 'Note 9', 'content': 'some test'},
+                {id:9,'title': 'Note 10', 'content': 'some test'},
+                {id:10,'title': 'Note 11', 'content': 'some test'},
+                {id:11,'title': 'Note 12', 'content': 'some test'},
+                {id:12,'title': 'Note 13', 'content': 'some test'},
+                {id:13,'title': 'Note 14', 'content': 'some test'},
+                {id:14,'title': 'Note 15', 'content': 'some test'}
             ]
         };
     }
   render() {
     return (
-        <Container>
+        <div className='container'>
             <Row>
                 <Col>
                     <MainMenu/>
@@ -41,7 +41,7 @@ class App extends Component {
                 </Col>
             </Row>
 
-        </Container>
+        </div>
     );
   }
 }
