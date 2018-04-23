9c8d4f8b4afbf64876e9e8ed81481f449a170ca7
Justin Hong <jhong2009@gmail.com>
2018-04-23 12:13:07 -0700

    in App.js: set up app to render Todos and form

--- LambdaNotes/src/App.js
+++ LambdaNotes/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+// components
+import CreateTodoForm from "./components/CreateTodoForm.js";
+import Todos from "./components/Todos.js";
 
 class App extends Component {
   render() {
@@ -13,6 +16,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {/* Render CreateTodoForm here */}
+        <CreateTodoForm />
+        {/* Render Todos here */}
+        <Todos />
+        {/* Render Routes here */}
       </div>
     );
   }
