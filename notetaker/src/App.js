56a06fd665b5345542c0a0a0f3eafbac2da77871
totsuya <supertatortots@gmail.com>
2018-04-23 15:22:23 -0400

    gave CreateNote.js input fields

--- notetaker/src/App.js
+++ notetaker/src/App.js
@@ -1,17 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       titleInput: "",
-      descriptionInput:"",
+      descriptionInput: "",
       noteList: []
     };
   }
-}
 
+  handleAddNote = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  //   handleSubmitNote = e => {
+  //     this.setState({
+  //       const
+  //     })
+  //   }
+  
+  render() {
+    return <div>Placeholder Text</div>;
+  }
+}
 
 export default App;
--- notetaker/src/components/CreateNote/CreateNote.js
+++ notetaker/src/components/CreateNote/CreateNote.js
@@ -1,1 +1,11 @@
 import { React } from 'react';
+
+const createNote = props => {
+  return (
+    //input fields for title and description here
+    <div>
+      <input type="text" name="Title" placeholder="Title" />
+      <input type="text" name="Description" placeholder="Type in your description here!"/>
+    </div>
+  )
+}
