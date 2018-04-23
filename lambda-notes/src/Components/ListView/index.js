dba0840a6243ed7b1d7ff393c09c5410a5b06d7b
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-04-23 15:22:30 -0700

    working on list view page

--- lambda-notes/src/Components/ListView/index.js
+++ lambda-notes/src/Components/ListView/index.js
@@ -3,9 +3,17 @@ import React from 'react';
 export const NoteList = props => {
     return (
         <div>
-            {props.Notes.map(note => {
-                return <div>{note.title}, {note.body}</div>
-            })}
+            <div>
+                <h1>Lambda Notes</h1>
+                <button>View Your Notes</button>
+                <button>+Create New Note</button>
+            </div>
+            <div>
+                <h4>Your Notes:</h4>
+                {/* {this.props.Notes.map(note => {
+                    return <div>{note.title}, {note.body}</div>
+                })} */}
+            </div>
         </div>
     )
 }
