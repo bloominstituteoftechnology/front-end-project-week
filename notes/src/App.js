d7f0db2c8b51c46570c6acbc3ad5fb38edc6c143
David Alexander Morales <damorales89@gmail.com>
2018-04-23 18:31:32 -0400

    react router setup in index.js

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Route, Switch } from "react-router-dom"
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Switch>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -13,6 +16,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        </Switch>
       </div>
     );
   }
