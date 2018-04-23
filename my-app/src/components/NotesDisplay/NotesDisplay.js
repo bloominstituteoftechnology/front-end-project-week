5f81d0c3b1871435914524cc9c105769098ad6b6
tanking <tanking@github.com>
2018-04-23 18:18:15 -0400

    Separated list of notes display to separate component

---
+++ my-app/src/components/NotesDisplay/NotesDisplay.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import './NotesDisplay.css';
+
+class NotesDisplay extends Component {
+    render() {
+        return (
+            <div className="Note">
+            <div className= 'Note-feature'> 
+            <h1> Lambda Notes </h1>
+                <p className = 'Note-buttons'> View Your Notes </p> 
+                <p className = 'Note-buttons'> +Create New Notes </p> 
+            </div>
+            <div className='Note-content'>
+                <h1> Your Notes: </h1>
+                <div className= 'Note-notesDisplay'>
+                    <p className='Note-notes'> 
+                    <h3> Note Title </h3>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, molestias porro. 
+                    Reiciendis velit accusamus ea laborum, adipisci facere possimus fugiat incidunt ullam architecto ad beatae eos 
+                    rem, perferendis ex alias.
+                    </p>
+                    <p className='Note-notes'> 
+                    <h3> Note Title </h3>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, molestias porro. 
+                    Reiciendis velit accusamus ea laborum, adipisci facere possimus fugiat incidunt ullam architecto ad beatae eos 
+                    rem, perferendis ex alias.
+                    </p>
+                    <p className='Note-notes'> 
+                    <h3> Note Title </h3>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, molestias porro. 
+                    Reiciendis velit accusamus ea laborum, adipisci facere possimus fugiat incidunt ullam architecto ad beatae eos 
+                    rem, perferendis ex alias.
+                    </p>
+                    <p className='Note-notes'> 
+                    <h3> Note Title </h3>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, molestias porro. 
+                    Reiciendis velit accusamus ea laborum, adipisci facere possimus fugiat incidunt ullam architecto ad beatae eos 
+                    rem, perferendis ex alias.
+                    </p>
+                    <p className='Note-notes'> 
+                    <h3> Note Title </h3>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, molestias porro. 
+                    Reiciendis velit accusamus ea laborum, adipisci facere possimus fugiat incidunt ullam architecto ad beatae eos 
+                    rem, perferendis ex alias.
+                    </p>
+                    <p className='Note-notes'> 
+                    <h3> Note Title </h3>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, molestias porro. 
+                    Reiciendis velit accusamus ea laborum, adipisci facere possimus fugiat incidunt ullam architecto ad beatae eos 
+                    rem, perferendis ex alias.
+                    </p>
+                    <p className='Note-notes'> 
+                    <h3> Note Title </h3>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, molestias porro. 
+                    Reiciendis velit accusamus ea laborum, adipisci facere possimus fugiat incidunt ullam architecto ad beatae eos 
+                    rem, perferendis ex alias.
+                    </p>
+                    <p className='Note-notes'> 
+                    <h3> Note Title </h3>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, molestias porro. 
+                    Reiciendis velit accusamus ea laborum, adipisci facere possimus fugiat incidunt ullam architecto ad beatae eos 
+                    rem, perferendis ex alias.
+                    </p>
+                    <p className='Note-notes'> 
+                    <h3> Note Title </h3>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, molestias porro. 
+                    Reiciendis velit accusamus ea laborum, adipisci facere possimus fugiat incidunt ullam architecto ad beatae eos 
+                    rem, perferendis ex alias.
+                    </p>
+                </div>
+            </div>
+        </div>
+        );
+    }
+}
+
+export default NotesDisplay;
