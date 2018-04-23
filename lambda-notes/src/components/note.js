e54b41073a13c4fa195177ced800f21b07c88371
Mark Wong <fmarkwong@gmail.com>
2018-04-23 12:59:12 -0700

    initial card list layout

--- lambda-notes/src/components/note.js
+++ lambda-notes/src/components/note.js
@@ -2,23 +2,18 @@ import React, { Component } from 'react';
 import { Card, CardText, CardBody, CardTitle, Button } from 'reactstrap';
 
 export default class Note extends Component {
-  constructor() {
-    super();
-    this.state = {
-      title: "Card title",
-      text: "Some quick example text to build on the card title and make up the bulk of the card's content."
-    };
-  }
 
   render() {
     return (
-      <Card>
-        <CardBody>
-          <CardTitle><h4>{this.state.title}</h4></CardTitle>
-          <hr />
-          <CardText>{this.state.text}</CardText>
-        </CardBody>
-      </Card>
+      <div className="note">
+        <Card>
+          <CardBody>
+            <CardTitle><h4>{this.props.title}</h4></CardTitle>
+            <hr />
+            <CardText>{this.props.text}</CardText>
+          </CardBody>
+        </Card>
+      </div>
     );
   }
 
--- lambda-notes/src/components/notesList.js
+++ lambda-notes/src/components/notesList.js
@@ -3,10 +3,22 @@ import React, { Component } from 'react';
 import Note from './note';
 
 export default class NotesList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cards: [
+        { title: "Card title # 1", text: "Some quick example text to build on the card title and make up the bulk of the card's content." },
+        { title: "Card title # 2", text: "Some quick example text to build on the card title and make up the bulk of the card's content." },
+        { title: "Card title # 3", text: "Some quick example text to build on the card title and make up the bulk of the card's content." },
+        { title: "Card title # 2", text: "Some quick example text to build on the card title and make up the bulk of the card's content." },
+        { title: "Card title # 3", text: "Some quick example text to build on the card title and make up the bulk of the card's content." },
+      ]
+    }
+  }
   render() {
     return (
-      <div>
-        <Note />
+      <div className="note-list">
+        { this.state.cards.map( card => <Note { ...card } /> ) }
       </div>
     );
   }
