976be81648a19d7c2dc28fe2bfc9e5503e65e3ee
totsuya <supertatortots@gmail.com>
2018-04-23 17:30:10 -0400

    created map for NoteList

--- notetaker/src/components/NoteList/NoteList.js
+++ notetaker/src/components/NoteList/NoteList.js
@@ -1,1 +1,21 @@
 import { React } from 'react';
+import Note from './Note/Note.js';
+
+class NoteList extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>List of Notes</h2>
+        {this.props.notes.map((note) => {
+          return <Note title={note.title} description={note.description}/>
+        })}
+      </div>
+    )
+  };
+}
+
+export default NoteList;
