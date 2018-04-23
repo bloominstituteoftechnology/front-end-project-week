922a0ce56104b6e88e74bf8d316e5d626139c2a6
sadrak105 <sadrak10@gmail.com>
2018-04-23 15:52:44 -0500

    added router

--- lambdanotes/src/index.js
+++ lambdanotes/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom'; //Import Router
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Apply the Router to the App
+ReactDOM.render(
+<Router>
+    <App />
+</Router>
+    , document.getElementById('root'));
 registerServiceWorker();
