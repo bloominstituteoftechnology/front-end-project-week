7c51569d52e51e53d6b7aa306aace4f5b6dbf820
Nedu Robert <nedumrobert@gmail.com>
2018-04-23 23:46:50 +0100

    imported and implemented reset css styles

--- lambdanotes/src/index.js
+++ lambdanotes/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import './reset.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
