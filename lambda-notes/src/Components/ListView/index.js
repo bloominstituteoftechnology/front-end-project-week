0cdfbe82461ce6751e7f290335e7e77a664fd62e
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-04-23 15:31:51 -0700

    added some styling to list view

--- lambda-notes/src/Components/ListView/index.js
+++ lambda-notes/src/Components/ListView/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import './styles.css';
 
 export const NoteList = props => {
     return (
-        <div>
-            <div>
+        <div className="notes">
+            <div className="side">
                 <h1>Lambda Notes</h1>
-                <button>View Your Notes</button>
-                <button>+Create New Note</button>
+                <div className="links">View Your Notes</div>
+                <div className="links">+Create New Note</div>
             </div>
-            <div>
+            <div className="center">
                 <h4>Your Notes:</h4>
                 {/* {this.props.Notes.map(note => {
                     return <div>{note.title}, {note.body}</div>
