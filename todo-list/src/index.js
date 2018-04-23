eeaa80572f3d74afc4fc1b4f475a5d0bf056a921
Devon Bacon <devon.bacon@gmail.com>
2018-04-23 08:09:27 -0700

    Set up basics for index.js in src dir

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css';
+import App from './components/App';
+// Import reducer and pass into createStore
+
+const store = createStore(
+  () => {},
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+ );
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
