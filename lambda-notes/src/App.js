1d44986ba15ea9880fb4fe84660de389413ca982
Velsu <riftmage50@gmail.com>
2018-04-23 22:22:57 +0200

    Designed initial app layout and wrapped Router

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,18 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+          <div className="row">
+            <div className="col-3 left__side">
+              <h2 className="sidebar__head">Lambda Notes</h2>
+              <a className="sidebar__button">View Your Notes</a>
+              <a className="sidebar__button">Create New Note</a>
+            </div>
+            <div className="col-9 right__side">
+              <div className="row notes__head">
+                <h4>Your Notes</h4>
+              </div>
+              <div className="row">
+                List of Notes List of Notes List of Notes List of Notes List of
+                Notes List of Notes List of Notes List of Notes List of Notes
+                List of Notes List of Notes List of Notes List of Notes List of
+                Notes
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
