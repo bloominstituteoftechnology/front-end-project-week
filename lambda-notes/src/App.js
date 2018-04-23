2ba18ce593548133c2bd5ae579e0d8f868a50eb5
Mark Wong <fmarkwong@gmail.com>
2018-04-23 14:45:27 -0700

    Set up react router

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import { Container, Row, Col, Button } from 'reactstrap';
 
 import NotesList from './components/notesList';
@@ -7,19 +9,23 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <Container>
-        <Row>
-          <div className="side-nav col-2">
-            <h2>Lambda<br />Notes</h2>
-            <Button className="btn-info btn-block">View Your Notes</Button>
-            <br />
-            <Button className="btn-info btn-block">+ Create New Note</Button>
-          </div>
-          <div className="main-view col-10">
-              <NotesList />
-          </div>
-        </Row>
-      </Container>
+      <Router>
+        <Container>
+          <Row>
+            <div className="side-nav col-2">
+              <br />
+              <h2>Lambda<br />Notes</h2>
+              <Button className="btn-info btn-block">View Your Notes</Button>
+              <br />
+              <Button className="btn-info btn-block">+ Create New Note</Button>
+            </div>
+            <div className="main-view col-10">
+              <Route exact path="/" component={NotesList} />
+              <Route component={NotesList} />
+            </div>
+          </Row>
+        </Container>
+      </Router>
     );
   }
 }
