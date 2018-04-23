1751690477203a39a167410af62a5d5ccf21ab9d
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-04-23 17:40:40 -0400

    Robert Hogan. Additional progress on my Navigator component with styling.

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import PageNavigator from './components/PageNavigator';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app-wrapper">
+          <PageNavigator />
       </div>
     );
   }
---
+++ lambda-notes/src/components/PageNavigator.js
@@ -0,0 +1,23 @@
+import React, { Component }from 'react';
+import './PageNavigator.css';
+
+class PageNavigator extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <div className="page-navigator">
+                <div className="page-navigator-heading">
+                    <h1 className="heading-lambda">Lambda</h1>
+                    <h1 className="heading-notes">Notes</h1>
+                    <button className="view-button">View Your Notes</button>
+                    <button className="create-button">+ Create New Note</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default PageNavigator;
