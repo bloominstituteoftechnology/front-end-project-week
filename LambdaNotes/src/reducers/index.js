3c811fccb891981300b3c5416153e94ad52d00d1
Justin Hong <jhong2009@gmail.com>
2018-04-23 11:02:58 -0700

    in reducers/index.js: set up initial state and todosReducer(root) structure

---
+++ LambdaNotes/src/reducers/index.js
@@ -0,0 +1,23 @@
+const initialState = {
+	todos: [],
+	addingTodo: false,
+	editingTodo: false,
+	deletingTodo: false,
+	viewingTodo: false
+};
+
+const todosReducer = (state=initialState, action) => {
+	switch(action.type) {
+		// add new todo
+		case:
+			return ;
+		// edit new todo
+		case:
+			return ;
+		// delete new todo
+		case:
+			return ;
+		default: 
+			return state;
+	}
+}
