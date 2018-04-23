3d3a92a1013ec958ce7f8addad623fe4c6bea835
JacobLeonlyerla <umasour@yahoo.com>
2018-04-23 15:49:53 -0700

    working on getting reactstap working, getting some results still need more time to mess with it

--- lambda-notes/src/components/ListView.js
+++ lambda-notes/src/components/ListView.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
 
  const Notes = props => {
     console.log("porps", props);
     
     return(<React.Fragment>
-     {props.notes.map(note =>( <div>
-        <div>{note.title}</div>
-        <div>{note.textbody}</div>
+     {props.notes.map(note =>(
+       <div>
+        <Card>
+       <CardBody>
+        <CardTitle>{note.title}</CardTitle>
+        <CardText>{note.textbody}</CardText>
+        </CardBody>
+        </Card>
         </div>
    ))}
      </React.Fragment>)
