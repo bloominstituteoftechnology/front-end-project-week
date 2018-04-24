b15abffe8d7761627746fc8af5aadeb736ea2fa1
Justin Hong <jhong2009@gmail.com>
2018-04-24 11:28:13 -0700

    in SingleTodos.css: used material ui Card component to display a todo's data. in SingleTodos.css: gave basic styling to each card

--- LambdaNotes/src/components/SingleTodo.js
+++ LambdaNotes/src/components/SingleTodo.js
@@ -1,6 +1,19 @@
 import React, { Component } from "react";
+// material components
+import { Card, CardHeader, CardTitle, CardText } from "material-ui/Card";
+// styles
+import "../styles/SingleTodos.css";
+
 const SingleTodo = props => {
-	return [<div>{props.todo.text}</div>, <div>{props.todo.title}</div>];
+	console.log("PROPS singletodo:", props.todo.text);
+	return (
+		<Card className="SingleTodo">
+			<CardText>
+				<h3>Hello World</h3>
+				<p>{props.todo.text}</p>
+			</CardText>
+		</Card>
+	);
 };
 
 export default SingleTodo;
