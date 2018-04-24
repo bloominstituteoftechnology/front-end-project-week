5cd9c009fe46c894ed623f02cfd4f78a433ba1c4
2940cristian <2940cristian@gmail.com>
2018-04-23 21:12:04 -0700

    Hard coded list view

--- lambda-notes/src/Components/ListView/ListView.js
+++ lambda-notes/src/Components/ListView/ListView.js
@@ -15,6 +15,85 @@ class ListView extends Component {
                     <button>View Your Notes</button>
                     <button>+Create New Note</button>
                 </div>
+
+                <div className = "notes">
+                    <div className = "list">
+                        <h2>Your Notes:</h2>
+                    </div>
+                    <div className = "row">
+                        <div className = "card">
+                            <h2 className = "underline">Todo</h2>
+                            <p>Lorem ipsum dolor sit amet, no natum omnis cum, 
+                                ius an natum ceteros, qui cibo volumus aliquando cu.</p>
+                        </div>
+                        <div className = "card">
+                            <h2 className = "underline">Todo</h2>
+                            <p>Lorem ipsum dolor sit amet, no natum omnis cum, 
+                                ius an natum ceteros, qui cibo volumus aliquando cu.</p>
+                        </div>
+                        <div className = "card">
+                            <h2 className = "underline">Todo</h2>
+                            <p>Lorem ipsum dolor sit amet, no natum omnis cum, 
+                                ius an natum ceteros, qui cibo volumus aliquando cu.</p>
+                        </div>
+                    </div>
+
+                    <div className = "row">
+                        <div className = "card">
+                            <h2 className = "underline">Todo</h2>
+                            <p>Lorem ipsum dolor sit amet, no natum omnis cum, 
+                                ius an natum ceteros, qui cibo volumus aliquando cu.</p>
+                        </div>
+                        <div className = "card">
+                            <h2 className = "underline">Todo</h2>
+                            <p>Lorem ipsum dolor sit amet, no natum omnis cum, 
+                                ius an natum ceteros, qui cibo volumus aliquando cu.</p>
+                        </div>
+                        <div className = "card">
+                            <h2 className = "underline">Todo</h2>
+                            <p>Lorem ipsum dolor sit amet, no natum omnis cum, 
+                                ius an natum ceteros, qui cibo volumus aliquando cu.</p>
+                        </div>
+                    </div>
+
+
+                    <div className = "row">
+                        <div className = "card">
+                            <h2 className = "underline">Todo</h2>
+                            <p>Lorem ipsum dolor sit amet, no natum omnis cum, 
+                                ius an natum ceteros, qui cibo volumus aliquando cu.</p>
+                        </div>
+                        <div className = "card">
+                            <h2 className = "underline">Todo</h2>
+                            <p>Lorem ipsum dolor sit amet, no natum omnis cum, 
+                                ius an natum ceteros, qui cibo volumus aliquando cu.</p>
+                        </div>
+                        <div className = "card">
+                            <h2 className = "underline">Todo</h2>
+                            <p>Lorem ipsum dolor sit amet, no natum omnis cum, 
+                                ius an natum ceteros, qui cibo volumus aliquando cu.</p>
+                        </div>
+                    </div>
+
+
+                    <div className = "row">
+                        <div className = "card">
+                            <h2 className = "underline">Todo</h2>
+                            <p>Lorem ipsum dolor sit amet, no natum omnis cum, 
+                                ius an natum ceteros, qui cibo volumus aliquando cu.</p>
+                        </div>
+                        <div className = "card">
+                            <h2 className = "underline">Todo</h2>
+                            <p>Lorem ipsum dolor sit amet, no natum omnis cum, 
+                                ius an natum ceteros, qui cibo volumus aliquando cu.</p>
+                        </div>
+                        <div className = "card">
+                            <h2 className = "underline">Todo</h2>
+                            <p>Lorem ipsum dolor sit amet, no natum omnis cum, 
+                                ius an natum ceteros, qui cibo volumus aliquando cu.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
