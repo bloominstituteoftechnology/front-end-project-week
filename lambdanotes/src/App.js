cea81b3b31d861c3a3fb97127ace648c5b7c6e2a
cgoez <goez2001@gmail.com>
2018-04-24 16:33:58 -0700

    Add reactstrap, Note.js styling

--- lambdanotes/src/App.js
+++ lambdanotes/src/App.js
@@ -13,7 +13,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Sidebar />
+        {/* <Sidebar /> */}
         <Switch>
           <Route exact path="/" component={NotesList} />
           {/* <Route path ='/:id' component={Note} /> */}
--- lambdanotes/src/components/Note/Note.js
+++ lambdanotes/src/components/Note/Note.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { Card, CardText, CardBody, CardTitle, Col, Row } from "reactstrap";
 
 const Note = props => {
   const { title, content } = props.note;
   return (
-    <div className="note">
-      <h1>{title}</h1>
-      <p>{content}</p>
+    // <div className="note">
+    <div>
+      <Row>
+        <Col sm={{ size: 4, offset: 1 }}>
+          <Card>
+            <CardBody>
+              <CardTitle>{title}</CardTitle>
+              <CardText>{content}</CardText>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 };
