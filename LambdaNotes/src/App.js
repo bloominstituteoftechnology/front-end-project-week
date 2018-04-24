fc2a104050907da2145b6dcefa93e306a9dcc159
Justin Hong <jhong2009@gmail.com>
2018-04-23 19:57:28 -0700

    in App.js: added Route for Todos, re-arranged child components inside a Bootstrap row. Control Panel always renders despite route changes

--- LambdaNotes/src/App.js
+++ LambdaNotes/src/App.js
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
+import "./bootstrap/css/bootstrap.css";
 // components
 import CreateTodoForm from "./components/CreateTodoForm.js";
+import ControlPanel from "./components/ControlPanel";
 import Todos from "./components/Todos.js";
 import Todo from "./components/Todo.js";
 
@@ -11,18 +13,24 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        {/* Render Todos here */}
-        <Todos />
-        {/* Render Routes here */}
-        <Route path="/addTodo" component={CreateTodoForm} />
-        <Route path="/todo/:id" component={Todo} />
+        <div className="row">
+          {/* Render ControlPanel here */}
+          <div className="col-md-4">
+            <ControlPanel />
+          </div>
+          {/* Render Todos here */}
+          <div className="col-md-8">
+            <Route exact path="/" component={Todos} />
+          </div>
+          {/* Render CreateTodoForm only at /addTodo */}
+          <div className="col-md-8">
+            <Route exact path="/addTodo" component={CreateTodoForm} />
+          </div>
+          {/* Render Todo only at /todo/:id */}
+          <div className="col-md-8">
+            <Route exact path="/todo/:id" component={Todo} />
+          </div>
+        </div>
       </div>
     );
   }
