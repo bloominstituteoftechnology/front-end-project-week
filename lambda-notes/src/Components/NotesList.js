9650fae3910ef1d46824a09a797524ffa1eadc5d
Shawn Stewart <s.stewart0220@gmail.com>
2018-04-24 12:31:42 -0500

    Created placeholder notes and styled the notes list.

--- lambda-notes/src/Components/NotesList.js
+++ lambda-notes/src/Components/NotesList.js
@@ -7,7 +7,24 @@ export const NotesList = () => {
   return(
     <div className='Content__Wrapper'>
       <h3 className='Content__Heading'>Your Notes:</h3>
-      <div className='Content NotesList'>Hi</div>
+      <div className='Content NotesList'>
+        <div className='Note'>
+          <h4 className='Note__Heading'>Note Title</h4>
+          <p className='Note__Content'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div className='Note'>
+          <h4 className='Note__Heading'>Note Title</h4>
+          <p className='Note__Content'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div className='Note'>
+          <h4 className='Note__Heading'>Note Title</h4>
+          <p className='Note__Content'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div className='Note'>
+          <h4 className='Note__Heading'>Note Title</h4>
+          <p className='Note__Content'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet.</p>
+        </div>
+      </div>
     </div>
   );
 }
