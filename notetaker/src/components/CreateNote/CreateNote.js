ab0c42d9fe4e265ff289d48b911728d713e2db44
totsuya <supertatortots@gmail.com>
2018-04-24 19:02:00 -0400

    fixed CreateNote.js

--- notetaker/src/components/CreateNote/CreateNote.js
+++ notetaker/src/components/CreateNote/CreateNote.js
@@ -1,11 +1,15 @@
 import { React } from 'react';
 
-const createNote = props => {
+
+const CreateNote = props => {
   return (
     //input fields for title and description here
     <div>
       <input type="text" name="Title" placeholder="Title" />
       <input type="text" name="Description" placeholder="Type in your description here!"/>
+
     </div>
   )
 }
+
+export default CreateNote;
