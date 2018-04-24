b0bbccf9c4ee5cbe56eb2f8e5ff535234d656715
Nedu Robert <nedumrobert@gmail.com>
2018-04-24 21:28:05 +0100

    renamed initial notes data file and cleaned up its contents

--- lambdanotes/src/notes.js
+++ lambdanotes/src/notes.js
@@ -1,51 +1,51 @@
-export const notesData = [
+export const notes = [
   {
-    'id': 11,
+    'id': 0,
     'title': 'Universidad del Istmo',
     'text':
       'lobortis ligula sit amet eleifend pede libero quis orci nullam molestie nibh in lectus',
   },
   {
-    'id': 55,
+    'id': 1,
     'title': 'Universidad Kino',
     'text':
       'eu est congue elementum in hac habitasse platea dictumst morbi vestibulum velit id',
   },
   {
-    'id': 41,
+    'id': 2,
     'title': 'International Buddhist University',
     'text': 'ligula sit amet eleifend pede libero quis orci nullam molestie',
   },
   {
-    'id': 13,
+    'id': 3,
     'title': 'Southwest University of Political Science and Law',
     'text': 'sapien urna pretium nisl ut volutpat sapien arcu sed augue aliquam',
   },
   {
-    'id': 39,
+    'id': 4,
     'title': 'University of Tampere',
     'text':
       'diam neque vestibulum eget vulputate ut ultrices vel augue vestibulum ante ipsum primis in faucibus orci luctus et ultrices',
   },
   {
-    'id': 77,
+    'id': 5,
     'title': 'Integral University',
     'text': 'potenti nullam porttitor lacus at turpis donec posuere metus vitae',
   },
   {
-    'id': 30,
+    'id': 6,
     'title': 'Hawler Medical University',
     'text':
       'tristique est et tempus semper est quam pharetra magna ac consequat metus sapien ut nunc vestibulum ante ipsum primis in',
   },
   {
-    'id': 56,
+    'id': 7,
     'title': 'University of Evansville',
     'text':
       'porta volutpat erat quisque erat eros viverra eget congue eget semper rutrum nulla nunc purus',
   },
   {
-    'id': 90,
+    'id': 8,
     'title': 'University College of Skövde',
     'text': 'suscipit a feugiat et eros vestibulum ac est lacinia nisi',
   },
