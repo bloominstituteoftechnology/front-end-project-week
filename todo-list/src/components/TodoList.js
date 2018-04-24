167cda5ed92349b766b65b64fac14489744f4071
Devon Bacon <devon.bacon@gmail.com>
2018-04-23 19:11:47 -0700

    Finished template for TodoList

--- todo-list/src/components/TodoList.js
+++ todo-list/src/components/TodoList.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import '../styles/TodoList.css';
 import { TodoCard } from './TodoCard';
 
 export const TodoList = props => {
   return (
     <div className="TodoList">
+      <h4>Your Notes:</h4>
       {props.notes.map(note => 
-        <TodoCard note={note}/>
+        <TodoCard key={note.id} note={note} />
       )}
     </div>
   )
