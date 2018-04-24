57df7c5776e1222c713b415e9793f4e5354688ba
tanking <tanking@github.com>
2018-04-24 13:23:46 -0400

    Set up layout for New Note

--- my-app/src/components/NewNote/NewNote.js
+++ my-app/src/components/NewNote/NewNote.js
@@ -4,12 +4,23 @@ class NewNote extends Component {
     render() {
         return(
             <div className="Note">
-                <div className= 'Note-feature'> 
-                    <div>
-                        <h1> Lambda Notes </h1>
-                        <p className = 'Note-buttons'> View Your Notes </p> 
-                        <p className = 'Note-buttons'> +Create New Notes </p> 
-                    </div>
+                <div className='Note-feature'> 
+                    <h1> Lambda Notes </h1>
+                    <p className = 'Note-buttons'> View Your Notes </p> 
+                    <p className = 'Note-buttons'> +Create New Notes </p> 
+                </div>
+                <div className='Note-create'>
+                    <h2> Create New Note: </h2>
+                    <input
+                        type='text'
+                        placeholder='Note Title'
+                        name='Note Title'
+                    />
+                    <input
+                        type='text'
+                        placeholder='Note Content'
+                        name='Note Content'
+                    />
                 </div>
             </div>
         )
