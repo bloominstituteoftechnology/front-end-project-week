031eb35cad931740cbf64742c0e0a4144f3cacf7
sadrak105 <sadrak10@gmail.com>
2018-04-24 17:57:44 -0400

    finalize notes

---
+++ lambdanotes/src/Notes/notes.js
@@ -0,0 +1,49 @@
+const notes = [
+    {
+        id: 1,
+        title: 'Note Title',
+        text: 'Lorem ipsum dolor amet echo park tote bag meh, fanny pack roof party franzen direct trade.'
+    },
+    {
+        id: 2,
+        title: 'Note Title',
+        text: 'Lorem ipsum dolor amet echo park tote bag meh, fanny pack roof party franzen direct trade.'
+    },
+    {
+        id: 3,
+        title: 'Note Title',
+        text: 'Lorem ipsum dolor amet echo park tote bag meh, fanny pack roof party franzen direct trade.'
+    },
+    {
+        id: 4,
+        title: 'Note Title',
+        text: 'Lorem ipsum dolor amet echo park tote bag meh, fanny pack roof party franzen direct trade.'
+    },
+    {
+        id: 5,
+        title: 'Note Title',
+        text: 'Lorem ipsum dolor amet echo park tote bag meh, fanny pack roof party franzen direct trade.'
+    },
+    {
+        id: 6,
+        title: 'Note Title',
+        text: 'Lorem ipsum dolor amet echo park tote bag meh, fanny pack roof party franzen direct trade.'
+    },
+    {
+        id: 7,
+        title: 'Note Title',
+        text: 'Lorem ipsum dolor amet echo park tote bag meh, fanny pack roof party franzen direct trade.'
+    },
+    {
+        id: 8,
+        title: 'Note Title',
+        text: 'Lorem ipsum dolor amet echo park tote bag meh, fanny pack roof party franzen direct trade.'
+    },
+    {
+        id: 9,
+        title: 'Note Title',
+        text: 'Lorem ipsum dolor amet echo park tote bag meh, fanny pack roof party franzen direct trade.'
+    }
+]
+
+export default notes;
