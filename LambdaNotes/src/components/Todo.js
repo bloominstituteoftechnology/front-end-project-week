b17f827cd62f2c1f543613c028506879f8872454
Justin Hong <jhong2009@gmail.com>
2018-04-24 14:53:43 -0700

    in Todo.js: set up edit/delete buttons and material Paper to wrap a todo's content

--- LambdaNotes/src/components/Todo.js
+++ LambdaNotes/src/components/Todo.js
@@ -3,26 +3,59 @@ import { connect } from "react-redux";
 // actions
 import { deleteTodo, updateTodo } from "../actions/index";
 // material components
+import Paper from "material-ui/Paper";
+import FlatButton from "material-ui/FlatButton";
 // styles
+import "../bootstrap/css/bootstrap.css";
+import "../styles/Todo.css";
 
 class Todo extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			title: "",
-			text: "",
-			isComplete: ""
-		};
-	}
 	render() {
-		// console.log("Todo PROPS: ", this.props);
+		console.log("Todo PROPS: ", this.props);
+		const { id } = this.props.match.params;
+		console.log("id: ", id);
 		return (
-			// {this.props.todos.forEach((todo,index) => {
-			// 	if ()
-			// })}
-
 			<div className="Todo">
-				<h3>Hello world</h3>
+				{/* one row for edit and delete buttons*/}
+				<div className="row">
+					<div className="col-md-12">
+						{/* Link edit button to EditTodoForm */}
+						<FlatButton className="Todo_button">Edit</FlatButton>
+						{/* Trigger modal to delete a todo */}
+						<FlatButton className="Todo_button">Delete</FlatButton>
+					</div>
+				</div>
+				{/* one row for todo title and todo content*/}
+				<div className="row">
+					<div className="col-md-12">
+						<Paper className="Todo_content">
+							<h3 className="Todo_header">{this.props.todos[id].title}</h3>
+							<p className="Todo_text">
+								Bacon ipsum dolor amet boudin leberkas beef ribs jowl andouille
+								brisket. Burgdoggen turkey cupim salami, capicola biltong
+								tri-tip leberkas ball tip tongue beef hamburger corned beef pig
+								sirloin. Landjaeger bacon salami, pig biltong venison kielbasa
+								meatloaf pork chop tail picanha doner drumstick. Pork chop
+								ribeye biltong pork belly kielbasa cow ground round. Pork loin
+								andouille shankle ground round, cow venison capicola leberkas.
+								Bacon ipsum dolor amet boudin leberkas beef ribs jowl andouille
+								brisket. Burgdoggen turkey cupim salami, capicola biltong
+								tri-tip leberkas ball tip tongue beef hamburger corned beef pig
+								sirloin. Landjaeger bacon salami, pig biltong venison kielbasa
+								meatloaf pork chop tail picanha doner drumstick. Pork chop
+								ribeye biltong pork belly kielbasa cow ground round. Pork loin
+								andouille shankle ground round, cow venison capicola leberkas.
+								Bacon ipsum dolor amet boudin leberkas beef ribs jowl andouille
+								brisket. Burgdoggen turkey cupim salami, capicola biltong
+								tri-tip leberkas ball tip tongue beef hamburger corned beef pig
+								sirloin. Landjaeger bacon salami, pig biltong venison kielbasa
+								meatloaf pork chop tail picanha doner drumstick. Pork chop
+								ribeye biltong pork belly kielbasa cow ground round. Pork loin
+								andouille shankle ground round, cow venison capicola leberkas.
+							</p>
+						</Paper>
+					</div>
+				</div>
 			</div>
 		);
 	}
