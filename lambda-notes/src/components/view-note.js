f2339a55c0b56914e7a0a8ee496a1839d1b636fb
jeffrey <jeffreyneilflynn@gmail.com>
2018-04-24 12:05:19 -0400

    styled delete modal

--- lambda-notes/src/components/view-note.js
+++ lambda-notes/src/components/view-note.js
@@ -2,14 +2,23 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { deleteNote } from '../REDUX/actions';
+import { Button, Modal, ModalBody } from 'reactstrap';
 
 class ViewNote extends Component {
   constructor() {
     super();
     this.state = {
       title: "",
-      content: ""
+      content: "",
+      modal: false
     }
+  }
+
+  toggleModal = () => { this.setState({ modal: !this.state.modal }) }
+
+  handleDelete = id => {
+    this.toggleModal();
+    this.props.deleteNote(id);
   }
 
   render() {
@@ -18,11 +27,22 @@ class ViewNote extends Component {
       <div className="PrimaryContainer__newNote"> 
         <div className="ViewNote__editOptions">
           <Link to={{ pathname: `/edit/${note.id}`, state: { note: note } }} className="ViewNote__editOptions--click">edit</Link>
-          <Link to="/" onClick={() => this.props.deleteNote(note.id)} className="ViewNote__editOptions--click">delete</Link>
+          <button onClick={() => this.toggleModal()} className="ViewNote__editOptions--click">delete</button>
         </div>
         <h1 className="PrimaryContainer__header">{note.title}</h1>
         <div>{note.content}</div>
         <div>{note.id}</div>
+        {this.state.modal ? (
+          <Modal isOpen={this.state.modal}>
+            <ModalBody className="Modal__header">Are you sure you want to delete this?</ModalBody>
+            <div className="Modal__ButtonsContainer">
+              <Link to="/" onClick={() => this.handleDelete(note.id)} className="Modal__button--link">
+                <Button color="danger" className="Modal__button--link--reg">Delete</Button>
+              </Link>
+              <Button color="primary" onClick={() => this.toggleModal()} className="Modal__button--reg">No</Button>
+            </div>
+          </Modal>
+        ) : null}
       </div>
     )
   }
