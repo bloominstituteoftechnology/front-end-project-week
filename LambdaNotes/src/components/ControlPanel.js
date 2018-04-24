6fdd9807ce7683aef50add352faf7d2e64ab0443
Justin Hong <jhong2009@gmail.com>
2018-04-23 19:43:58 -0700

    in ControlPanel.js: added header, basic buttons, and basic styles

---
+++ LambdaNotes/src/components/ControlPanel.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ControlPanel extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return <h3>control panel goes here</h3>;
+	}
+}
+
+export default ControlPanel;
