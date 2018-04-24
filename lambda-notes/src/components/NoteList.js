8be58ba031be914dd908555157c45f1628a0d0af
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-04-23 22:55:08 -0400

    Robert Hogan. Additional work to components. Have list of videos displaying on the screen with proper styling. One mvp completed.

--- lambda-notes/src/components/NoteList.js
+++ lambda-notes/src/components/NoteList.js
@@ -4,6 +4,7 @@ import './NoteList.css';
 export const NoteList = props => {
     return (
         <div className="note-list-wrapper">
+        <h3 className="your-notes">Your Notes:</h3>
             <div className="note-wrapper">{props.Note.map((lambdaNotes, index) => 
                 <div key={"my" + index} className="note-object-wrapper">
                     <div className="note-title">{lambdaNotes.title}</div>
