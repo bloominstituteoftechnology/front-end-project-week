66a1e364660a3cd467105375c8653ad32fefca81
totsuya <supertatortots@gmail.com>
2018-04-24 19:02:10 -0400

    created placeholder for NoteList

--- notetaker/src/components/NoteList/NoteList.js
+++ notetaker/src/components/NoteList/NoteList.js
@@ -1,13 +1,26 @@
-import React, { Component } from "react";
-import  Note  from '../Note/Note.js';
+// import React, { Component } from "react";
+// import  Note  from '../Note/Note.js';
 
-const NoteList = props => {
-    return <div>
-        <h2>Your Notes:</h2>
-        <div className='notelist'>
-        {props.notes.map(note => <Note key ={note} note={note} />)}
-        </div>
-      </div>;
-  };
+// export default class NoteList extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state {
+//       notes: [
+//         {title: "placeholder title", description: "placeholder description"},
+//         {title: "placeholder title", description: "placeholder description"},
+//         {title: "placeholder title", description: "placeholder description"},
+//         {title: "placeholder title", description: "placeholder description"},
+//       ]
+//     }
+//   }
+//     return <div>
+//         <h2>Your Notes:</h2>
+//         <div className='notelist'>
+//         {props.notes.map(note => <Note key={note} note={note} />)}
+//         </div>
+//       </div>;
+//     };
 
-export default NoteList;
+//   }
+
+// export default NoteList;
