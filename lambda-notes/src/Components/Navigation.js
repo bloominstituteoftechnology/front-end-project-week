28916f464e6c339c6d8ea8641e36662224a48b01
Shawn Stewart <s.stewart0220@gmail.com>
2018-04-24 12:30:55 -0500

    Added routing functionality to the navigation bar.

--- lambda-notes/src/Components/Navigation.js
+++ lambda-notes/src/Components/Navigation.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import './Navigation.css';
 
 export const Navigation = () => {
   return(
     <div className='Navigation'>
-      <h1 className='Navigation__Heading'>Lambda Notes</h1>
+      <NavLink to='/' className='Link'><h1 className='Navigation__Heading'>Lambda Notes</h1></NavLink>
       <div className='Navigation__Buttons'>
-        <button className='Navigation__Button'>View Your Notes</button>
+        <NavLink to='/Notes'><button className='Navigation__Button'>View Your Notes</button></NavLink>
         <button className='Navigation__Button'>+ Create New Note</button>
       </div>
     </div>
