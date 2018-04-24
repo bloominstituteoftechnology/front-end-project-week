ed2ef46c890827e93b8242224107e4490bdc9e3b
Mark Wong <fmarkwong@gmail.com>
2018-04-24 14:07:45 -0700

    Create New Note test

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -15,10 +15,10 @@ class App extends Component {
     this.state = {
       cards: [
         { title: "Card title # 1", content: "Some quick example text to build on the card title and make up the bulk of the card's content." },
-        { title: "Card title # 2", content: "Some quick example text to build on the card title and make up the bulk of the card's content." },
-        { title: "Card title # 3", content: "Some quick example text to build on the card title and make up the bulk of the card's content." },
-        { title: "Card title # 2", content: "Some quick example text to build on the card title and make up the bulk of the card's content." },
-        { title: "Card title # 3", content: "Some quick example text to build on the card title and make up the bulk of the card's content." },
+        // { title: "Card title # 2", content: "Some quick example text to build on the card title and make up the bulk of the card's content." },
+        // { title: "Card title # 3", content: "Some quick example text to build on the card title and make up the bulk of the card's content." },
+        // { title: "Card title # 2", content: "Some quick example text to build on the card title and make up the bulk of the card's content." },
+        // { title: "Card title # 3", content: "Some quick example text to build on the card title and make up the bulk of the card's content." },
       ],
       newNote: {
         title: '',
--- lambda-notes/src/tests/createNote.test.js
+++ lambda-notes/src/tests/createNote.test.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import {withRouter} from 'react-router'
-import {Link, Route, Router, Switch} from 'react-router-dom'
-import {createMemoryHistory} from 'history'
-import {render, Simulate} from './'
+import { withRouter } from 'react-router'
+import { Link, Route, Router, Switch } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
+import { render, Simulate } from './'
 
 import App from '../App';
 
@@ -23,13 +23,32 @@ function renderWithRouter(
   }
 }
 
+const leftClick = {button: 0};
+const { container, getByText, getByPlaceholderText } = renderWithRouter(<App />);
 
-test('full app rendering/navigating', () => {
-  const {container, getByText} = renderWithRouter(<App />)
-  // normally I'd use a data-testid, but just wanted to show this is also possible
+test('I can see home page', () => {
   expect(container.innerHTML).toMatch('Your Notes')
-  const leftClick = {button: 0}
+
+});
+
+test('I can create a new note', () => {
   Simulate.click(getByText('+ Create New Note'), leftClick)
-  // normally I'd use a data-testid, but just wanted to show this is also possible
   expect(container.innerHTML).toMatch('Create New Note')
-})
+
+  const titleInput = getByPlaceholderText('Note Title');
+  const contentInput = getByPlaceholderText('Note Content');
+
+  const testTitle = 'A Test Note';
+  const testContent = 'Just a test.  Nothing to see. Move along.';
+
+  titleInput.value = testTitle; 
+  Simulate.change(titleInput);
+
+  contentInput.value = testContent; 
+  Simulate.change(contentInput);
+
+  Simulate.click(getByText('Save'), leftClick)
+
+  expect(container.innerHTML).toMatch(testTitle);
+  expect(container.innerHTML).toMatch(testContent);
+});
