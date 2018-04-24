0458d92015854070ea9307390481f468473dd2bb
Justin Hong <jhong2009@gmail.com>
2018-04-24 14:55:42 -0700

    in SingleTodo.js: refactored each Todo Card to be a link to a todo's note view

--- LambdaNotes/src/components/SingleTodo.js
+++ LambdaNotes/src/components/SingleTodo.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Route } from "react-router-dom";
+// components
+import Todo from "./Todo";
 // material components
 import { Card, CardHeader, CardTitle, CardText } from "material-ui/Card";
 // styles
@@ -6,14 +10,22 @@ import "../styles/SingleTodos.css";
 
 const SingleTodo = props => {
 	console.log("PROPS singletodo:", props.todo.text);
-	return (
-		<Card className="SingleTodo">
-			<CardText>
-				<h3>Hello World</h3>
-				<p>{props.todo.text}</p>
-			</CardText>
-		</Card>
-	);
+	return [
+		<Link to={`/todo/${props.index}`}>
+			<Card className="SingleTodo">
+				<CardText>
+					<h3>Hello World</h3>
+					<p>{props.todo.text}</p>
+				</CardText>
+			</Card>
+		</Link>,
+		<Route
+			exact
+			path="/todo/:id"
+			{...props}
+			render={props => <Todo {...props} />}
+		/>
+	];
 };
 
 export default SingleTodo;
