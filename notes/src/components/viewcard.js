d71d21e60ffd32d30de16aaef3e4d23517871714
Cruise Brantley <cruise.brantley@gmail.com>
2018-04-25 14:36:29 -0500

    Modal 100%

--- notes/src/components/viewcard.js
+++ notes/src/components/viewcard.js
@@ -1,25 +1,63 @@
 import React from "react";
+import "react-responsive-modal/lib/react-responsive-modal.css";
+import Modal from "react-responsive-modal/lib/css";
 
-const ViewCard = props => {
-  return (
-    <div className="right-div">
-      <div className="link-text-div">
-        <span className="link-text" onClick={() => props.changeToEdit()}>
-          <u>edit</u>
-        </span>
-        <span
-          className="link-text"
-          onClick={() => props.removeNote(props.note.id)}
+class ViewCard extends React.Component {
+  state = {
+    open: false
+  };
+
+  onOpenModal = () => {
+    this.setState({ open: true });
+  };
+
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { open } = this.state;
+    return (
+      <div className="right-div">
+        <div className="link-text-div">
+          <span className="link-text" onClick={() => this.props.changeToEdit()}>
+            <u>edit</u>
+          </span>
+          <span className="link-text" onClick={this.onOpenModal}>
+            <u>delete</u>
+          </span>
+        </div>
+        <div className="view-card">
+          <h3 className="notes-h3 view-h3">{this.props.note.title}</h3>
+          <span className="notes-text-full">{this.props.note.text}</span>
+        </div>
+        <Modal
+          modal="modal"
+          open={open}
+          onClose={this.onCloseModal}
+          little
+          showCloseIcon={false}
         >
-          <u>delete</u>
-        </span>
+          <div className="modal-content-div">
+            <span>
+              <b>Are you sure you want to delete this?</b>
+            </span>
+            <div className="modal-button-div">
+              <button
+                className="button delete-notes-button"
+                onClick={e => this.props.removeNote(this.props.note.id)}
+              >
+                Delete
+              </button>
+              <button className="button" onClick={this.onCloseModal}>
+                No
+              </button>
+            </div>
+          </div>
+        </Modal>
       </div>
-      <div className="view-card">
-        <h3 className="notes-h3 view-h3">{props.note.title}</h3>
-        <span className="notes-text-full">{props.note.text}</span>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ViewCard;
