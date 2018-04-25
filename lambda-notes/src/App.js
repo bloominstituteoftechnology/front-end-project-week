c4fa1ce81852c079aa958566ce64821398de067d
Jonathan Greene <greeneselectronics@gmail.com>
2018-04-25 13:00:48 -0600

    Refactored the code to exist in its own components and so that it is being passed down to props

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -7,8 +7,6 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-        // noteTitle: 'I am a started note',
-        // noteBody: 'I am a started note',
         newNote: '',
         notes: [{
           title: 'Hello',
@@ -21,19 +19,6 @@ class App extends Component {
   // componentDidMount() {
   //   this.setState({notes})
   // }
-
-  noteHandler = (e) => {
-    console.log('this is a test');
-    this.setState({[e.target.name]: e.target.value})
-  }
-
-  handleSubmitNote = () => {
-    const {notes} = this.state;
-    const myNote = {title: this.state.newNoteTitle, body: this.state.newNoteBody}
-    notes.push(myNote);
-    this.setState({notes, newNoteTitle: '', newNoteBody: ''});
-  }
-
   createNewCard(props) {
     console.log('testing create new card.' + props)
   }
@@ -52,9 +37,6 @@ class App extends Component {
           <div className="col-9">
             <div className="rightbox">
                 <h3>Your Notes:</h3>
-                <input type= 'text' placeholder='Add Note Title' name='newNoteTitle' value={this.state.newNoteTitle} onChange={this.noteHandler}/>
-                <input type= 'text' placeholder='Add Note Body' name='newNoteBody' value={this.state.newNoteBody} onChange={this.noteHandler}/>
-              <button onClick={this.handleSubmitNote}>Add Note</button>
               <NotesList notes={this.state.notes}/>
             </div>
           </div>
--- lambda-notes/src/Components/CreateNote.js
+++ lambda-notes/src/Components/CreateNote.js
@@ -1,15 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class CreateNote extends Component {
-    render() {
+const CreateNote = props => {
+
+
+
+
+    noteHandler = (e) => {
+        console.log('this is a test');
+        this.setState({[e.target.name]: e.target.value})
+      }
+    
+      handleSubmitNote = () => {
+        const {notes} = this.state;
+        const myNote = {title: this.state.newNoteTitle, body: this.state.newNoteBody}
+        notes.push(myNote);
+        this.setState({notes, newNoteTitle: '', newNoteBody: ''});
+      }
+    
         return (
             
             <div>
-                <h3>Create New Note:</h3>
-                <p>this is a bunch of stuff and stuff to do the whole world for cats kittens money </p>
+                <input className ='notetitle' type= 'text' placeholder='Add Note Title' name='newNoteTitle' value={this.state.newNoteTitle} onChange={this.noteHandler}/>
+                <input className ='notebody' type= 'text' placeholder='Add Note Body' name='newNoteBody' value={this.state.newNoteBody} onChange={this.noteHandler}/>
+              <button className = 'addnote' onClick={this.handleSubmitNote}>Add Note</button>
             </div>
         );
-    }
 }
 
 export default CreateNote;
