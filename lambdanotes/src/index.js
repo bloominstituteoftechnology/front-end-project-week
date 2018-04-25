becdf95b96bc0f0fee653f001291fb7bc3c177b9
Nedu Robert <nedumrobert@gmail.com>
2018-04-25 18:49:15 +0100

    imported bootstrap into the app

--- lambdanotes/src/index.js
+++ lambdanotes/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './reset.css';
 import './index.css';
