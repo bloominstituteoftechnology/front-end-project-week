2c7b565fdebc3e433feae8c06eb5593584981f95
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-04-25 10:53:16 -0700

    working on getting individual notes to show

---
+++ lambda-notes/src/Components/ViewNote/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NoteList } from '../ListView';
+
+const viewNote = props => {
+    return (
+        <div className="col-9 right__side">
+            <div className="row">
+                
+            </div>
+        </div>
+    )
+}
