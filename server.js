8564f2961f4ed71980bc0a1ad8b33b73bf07c9d1
Shawn Stewart <s.stewart0220@gmail.com>
2018-04-25 13:09:15 -0500

    Added cors.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const port = 3333;
 
 const server = express();
 server.use(bodyParser.json());
+server.use(cors());
 
 const sendUserError = (msg, res) => {
   res.status(422);
