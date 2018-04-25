2bdd9e3b2c130d9c01fd6bdf32a9d1cf636bdaf9
tanking <tanking@github.com>
2018-04-25 18:52:15 -0400

    Fixed styling with View Notes display

--- my-app/src/components/NotesDisplay/NotesDisplay.js
+++ my-app/src/components/NotesDisplay/NotesDisplay.js
@@ -6,27 +6,90 @@ class NotesDisplay extends Component {
     render() {
         return (
             <div className="Note">
-            <div className= 'Note-feature'> 
-                <h1> Lambda Notes </h1>
-                <button className ='Note-buttons'>
-                    <Link to= '/'> View Your Notes </Link>
-                </button>
-                <button className ='Note-buttons'>
-                    <Link to= '/newNote'> +Create New Notes </Link>
-                </button>
-            </div>
+                <div className= 'Note-feature'> 
+                    <h1> Lambda Notes </h1>
+                    <button className ='Note-buttons'>
+                        <Link to= '/'> View Your Notes </Link>
+                    </button>
+                    <button className ='Note-buttons'>
+                        <Link to= '/newNote'> +Create New Note </Link>
+                    </button>
+                </div>
             <div className='Note-content'>
+                <h1> Your Notes: </h1> 
                 <div className= 'Note-notesDisplay'>
-                    <h1> Your Notes: </h1>
-                    <Link to= '/noteView'>   
                         <div className='Note-notes'>
-                            <h3> Note Title </h3> 
-                            <p> 
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia nulla vitae error commodi. Beatae, ea! Unde veniam voluptas earum fugit quasi, nemo aperiam vero! Fugit, repellat. Iste quos sunt amet.
-                            </p>
+                            <Link to= '/noteView'>  
+                                <h3> Note Title </h3> 
+                                <p> 
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia nulla vitae error commodi. Beatae, ea! Unde veniam voluptas earum fugit quasi, nemo aperiam vero! Fugit, repellat. Iste quos sunt amet.
+                                </p>
+                            </Link>
                         </div>
-                    </Link>
-                   
+                        <div className='Note-notes'>
+                            <Link to= '/noteView'>  
+                                <h3> Note Title </h3> 
+                                <p> 
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia nulla vitae error commodi. Beatae, ea! Unde veniam voluptas earum fugit quasi, nemo aperiam vero! Fugit, repellat. Iste quos sunt amet.
+                                </p>
+                            </Link>
+                        </div>
+                        <div className='Note-notes'>
+                            <Link to= '/noteView'>  
+                                <h3> Note Title </h3> 
+                                <p> 
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia nulla vitae error commodi. Beatae, ea! Unde veniam voluptas earum fugit quasi, nemo aperiam vero! Fugit, repellat. Iste quos sunt amet.
+                                </p>
+                            </Link>
+                        </div>
+                        <div className='Note-notes'>
+                            <Link to= '/noteView'>  
+                                <h3> Note Title </h3> 
+                                <p> 
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia nulla vitae error commodi. Beatae, ea! Unde veniam voluptas earum fugit quasi, nemo aperiam vero! Fugit, repellat. Iste quos sunt amet.
+                                </p>
+                            </Link>
+                        </div>
+                        <div className='Note-notes'>
+                            <Link to= '/noteView'>  
+                                <h3> Note Title </h3> 
+                                <p> 
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia nulla vitae error commodi. Beatae, ea! Unde veniam voluptas earum fugit quasi, nemo aperiam vero! Fugit, repellat. Iste quos sunt amet.
+                                </p>
+                            </Link>
+                        </div>
+                        <div className='Note-notes'>
+                            <Link to= '/noteView'>  
+                                <h3> Note Title </h3> 
+                                <p> 
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia nulla vitae error commodi. Beatae, ea! Unde veniam voluptas earum fugit quasi, nemo aperiam vero! Fugit, repellat. Iste quos sunt amet.
+                                </p>
+                            </Link>
+                        </div>
+                        <div className='Note-notes'>
+                            <Link to= '/noteView'>  
+                                <h3> Note Title </h3> 
+                                <p> 
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia nulla vitae error commodi. Beatae, ea! Unde veniam voluptas earum fugit quasi, nemo aperiam vero! Fugit, repellat. Iste quos sunt amet.
+                                </p>
+                            </Link>
+                        </div>
+                        <div className='Note-notes'>
+                            <Link to= '/noteView'>  
+                                <h3> Note Title </h3> 
+                                <p> 
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia nulla vitae error commodi. Beatae, ea! Unde veniam voluptas earum fugit quasi, nemo aperiam vero! Fugit, repellat. Iste quos sunt amet.
+                                </p>
+                            </Link>
+                        </div>
+                        <div className='Note-notes'>
+                            <Link to= '/noteView'>  
+                                <h3> Note Title </h3> 
+                                <p> 
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia nulla vitae error commodi. Beatae, ea! Unde veniam voluptas earum fugit quasi, nemo aperiam vero! Fugit, repellat. Iste quos sunt amet.
+                                </p>
+                            </Link>
+                        </div>
                 </div>
             </div>
         </div>
