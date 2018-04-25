4d75e616122f92788285607f4ceafbb6d9ed2f99
totsuya <supertatortots@gmail.com>
2018-04-25 05:01:33 -0400

    added links to buttons for later

--- notetaker/src/components/Sidebar/Sidebar.js
+++ notetaker/src/components/Sidebar/Sidebar.js
@@ -1,6 +1,7 @@
 import  React  from 'react';
 import { Button, Row, Col } from 'reactstrap';
 import './Sidebar.css';
+import { Link } from 'react-router-dom';
 //sidebar should show up in every page
 //has two buttons, View Your Notes & +Create New Note
 
@@ -8,15 +9,20 @@ const Sidebar = props => {
   return <div class="container">
       <div class="row">
         <div class="col" className="Sidebar">
-        <h1>
-          Lambda
-          Notes
-        </h1>
+          <h1>Lambda Notes</h1>
           <div>
-            <Button className="SidebarButton">View Your Notes</Button>
+            <Button className="SidebarButton">
+              <Link to="/" className="Link">
+                View Your Notes
+              </Link>
+            </Button>
           </div>
           <div>
-            <Button className="SidebarButton">+Create New Note</Button>
+            <Button className="SidebarButton">
+              <Link to="/CreateNote" className="Link">
+                +Create New Note
+              </Link>
+            </Button>
           </div>
         </div>
       </div>
