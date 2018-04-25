25b045370f4443a691e87cee429c09b3422ee42e
2940cristian <2940cristian@gmail.com>
2018-04-25 13:26:39 -0700

    working on styling for single note view

--- lambda-notes/src/Components/CreateNote/CreateNote.js
+++ lambda-notes/src/Components/CreateNote/CreateNote.js
@@ -20,7 +20,7 @@ class CreateNote extends Component {
                 <div className = "create">
                     <h1>Create New Note:</h1>
                     <input onChange={this.handleInputChange} type="text" placeholder="Note Title" name="title"/>
-                    <textarea onChange={this.handleInputChange} name="note" cols="118" rows="10" placeholder="Note Content"></textarea>
+                    <textarea onChange={this.handleInputChange} name="note" cols="100" rows="10" placeholder="Note Content"></textarea>
                     <button onClick={() => this.props.newNote({title: this.state.title, note: this.state.note})}>Save</button>
                 </div>
             </div>
--- lambda-notes/src/Components/NoteView/NoteView.js
+++ lambda-notes/src/Components/NoteView/NoteView.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import SideBar from "../SideBar/SideBar"
 import {Route} from 'react-router-dom'
 import "./NoteView.css"
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux';
 
 
@@ -15,7 +16,18 @@ class NoteView extends Component {
         return(
             <div className="noteView">
                 <SideBar/>
-                <div>Hello</div>
+                <div className="noteCard">
+                    <div className = "links">
+                        <Link to="#">edit</Link>
+                        <Link to="#">delete</Link>
+                    </div>
+                    <h1 className="title">Note Name</h1>
+                    <p className="note">
+                    Lorem ipsum dolor sit amet, legimus similique adolescens per an, feugiat eruditi et cum, eos an diam corpora. Vel tritani probatus ei, ei laudem tempor detracto pri. Vix lorem legere deserunt in, sed luptatum gubergren adversarium no. Sit et nihil tacimates, indoctum neglegentur eum cu.
+
+Liber mollis vocibus nam eu, ea prima idque est, ius et quidam dicunt constituam. Ius amet wisi feugiat ei. Ut fabellas scribentur duo. Agam scripta pri cu, usu appareat expetendis vituperatoribus te.
+                    </p>
+                </div>
             </div>
         )
     }
