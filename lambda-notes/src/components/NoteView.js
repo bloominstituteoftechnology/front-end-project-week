d28f64237b3aab07c25269c3d5c8790535ce0c32
JacobLeonlyerla <umasour@yahoo.com>
2018-04-25 01:02:27 -0700

    modal is up and styled working correctly and routing

--- lambda-notes/src/components/NoteView.js
+++ lambda-notes/src/components/NoteView.js
@@ -1,13 +1,16 @@
 import React,  { Component } from "react";
 import {notes} from '../Notes/notes'
 import { Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button, Row,Col } from 'reactstrap';
+    CardTitle, CardSubtitle, Button, Row,Col,  Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+    import{ Link } from 'react-router-dom'
 class NoteView extends Component{
 constructor(props){
     super(props)
     this.state={
-        note:[]
-    } 
+        note:[],
+        modal:false
+    };
+    this.toggle = this.toggle.bind(this); 
 }
    
     componentDidMount(){
@@ -15,6 +18,11 @@ constructor(props){
        console.log(this.state)
         this.setState({note:notes[id]})
     }
+    toggle() {
+        this.setState({
+          modal: !this.state.modal
+        });
+      }
 
 render(){
    
@@ -28,8 +36,21 @@ render(){
     <Card>
       <Row>  
 <Col className="link-styles">
-    <Button color="link" className="link">edit</Button>
-<Button color="link" className="link">delete</Button>
+    <Button color="link" className="link" >edit</Button>
+  
+<Button color="link" className="link" onClick={this.toggle} >delete</Button>
+<Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+       
+       <ModalBody>
+        Are you sure you want to delete this?
+       </ModalBody>
+       <ModalFooter>
+         <Link to="/">
+         <Button color="danger" className="button danger-styles modal-button" onClick={this.toggle}>Delete</Button>
+         </Link>
+         <Button color="secondary" className="button modal-button" onClick={this.toggle}>No</Button>
+       </ModalFooter>
+     </Modal>
 </Col>
 </Row>
 
