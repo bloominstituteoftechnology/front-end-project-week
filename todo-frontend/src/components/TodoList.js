a162b9e55a94fc488a0aa920e0980c3173d5bed5
Axhon <axhonruiz@gmail.com>
2018-04-25 11:58:02 -1000

    styled the notes list

--- todo-frontend/src/components/TodoList.js
+++ todo-frontend/src/components/TodoList.js
@@ -2,24 +2,29 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Styled from 'styled-components'
 import { Todo } from 'components/Todo'
-import { AddTodoForm } from 'components/AddTodoForm'
 
-const ListContainer = Styled.section`
+const SectionContainer = Styled.section`
+  background-color: rgb(242,243,242);
+  padding: 5px 20px;
+`
+
+const ListContainer = Styled.div`
   width: 100%;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-column-gap: 10px;
   grid-row-gap: 10px;
   padding: 10px;
-  background-color: rgb(242,243,242);
 `
 
 /** @type React.StatelessComponent<{todos}> */
 export const TodoList = ({ todos }) => (
-  <ListContainer>
-    <AddTodoForm />
-    {todos.map(todo => <Todo {...todo} key={todo.id} />)}
-  </ListContainer>
+  <SectionContainer>
+    <h3>Your Notes</h3>
+    <ListContainer>
+      {todos.map(todo => <Todo {...todo} key={todo.id} />)}
+    </ListContainer>
+  </SectionContainer>
 )
 
 TodoList.propTypes = {
