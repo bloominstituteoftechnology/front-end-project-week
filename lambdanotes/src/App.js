0511617e220b92667846964102900279d4c82b35
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-04-25 10:40:03 -0700

    Display Main View Notes Body

--- lambdanotes/src/App.js
+++ lambdanotes/src/App.js
@@ -1,37 +1,60 @@
+// import React, { Component } from 'react';
+// import './App.css';
+// import { Container, Row, Col, Button } from 'reactstrap';
+// import { getNotes } from './dummyNotes';
+
+// class App extends Component {
+//   state = {
+//     notes: [
+//       {
+//         id: 1,
+//         title: 'Donec tincidunt!',
+//         message:
+//           'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat magnam aspernatur beatae laudantium, optio harum accusantium pariatur voluptatibus earum error.',
+//       },
+//       // }
+//     // ] getNotes(),
+//     ]
+//   };
+
+//   render() {
+//     return (
+//       <div class="container-fluid">
+//         <div class="row justify-content-center title">
+//           <div class="col-2 Nav">
+//             <h4>Lambda Notes</h4>
+//               <Button color="info" size="sm" block >View Your Notes</Button>{' '}
+//               <Button color="info" size="sm" block>+ Create New Note</Button>{' '}
+//           </div>
+//           <div class="col-9 Main">
+//             Your Notes:
+//             <newNotes />
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import ViewNotes from './components/ViewNotes';
+
+
 import './App.css';
-import { Container, Row, Col, Button } from 'reactstrap';
-import { getNotes } from './dummyNotes';
 
 class App extends Component {
-  state = {
-    notes: [
-      {
-        id: 1,
-        title: 'Donec tincidunt!',
-        message:
-          'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat magnam aspernatur beatae laudantium, optio harum accusantium pariatur voluptatibus earum error.',
-      },
-      // }
-    // ] getNotes(),
-    ]
-  };
-
   render() {
     return (
-      <div class="container-fluid">
-        <div class="row justify-content-center title">
-          <div class="col-2 Nav">
-            <h4>Lambda Notes</h4>
-              <Button color="info" size="sm" block >View Your Notes</Button>{' '}
-              <Button color="info" size="sm" block>+ Create New Note</Button>{' '}
-          </div>
-          <div class="col-9 Main">
-            Your Notes:
-            <newNotes />
-          </div>
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={ViewNotes} />
+
+
         </div>
-      </div>
+      </BrowserRouter>
     );
   }
 }
--- lambdanotes/src/index.js
+++ lambdanotes/src/index.js
@@ -1,9 +1,26 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { reducer } from './reducers';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
