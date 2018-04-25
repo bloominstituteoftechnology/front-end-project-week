690efbad3700aac98d7cfed01a5eaf8af961c9d3
Velsu <riftmage50@gmail.com>
2018-04-25 21:50:32 +0200

    Completely rewrote Edit component for values retention, added overflow to notes

--- lambda-notes/src/components/EditNote.js
+++ lambda-notes/src/components/EditNote.js
@@ -1,61 +1,70 @@
 import React from "react";
 
-export const EditNote = ({ notes, updateNote, match, history }) => {
-  let titleInput;
-  let textInput;
+export class EditNote extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: "",
+      title: "",
+      text: ""
+    };
+  }
+  componentDidMount() {
+    const note = this.props.notes
+      .filter(note => note.id === parseInt(this.props.match.params.id, 10))
+      .shift();
+    this.setState({ id: note.id, title: note.title, text: note.text });
+  }
 
-  const note = notes
-    .filter(note => note.id === parseInt(match.params.id, 10))
-    .shift();
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-  // const handleEdit = event => {
-  //   event.preventDefault();
-  //   updateNote({
-  //     id: match.params.id,
-  //     title: titleInput.value,
-  //     text: textInput.value
-  //   });
-  //   history.push(`/viewnote/${match.params.id}`);
-  // };
-  return (
-    <div className="col-9 right__side">
-      <div className="row">
-        <div className="col-3 notes__head">
-          <h4>Edit Note:</h4>
+  render() {
+    const { history, updateNote, match } = this.props;
+    const { id, title, text } = this.state;
+    return (
+      <div className="col-9 right__side">
+        <div className="row">
+          <div className="col-3 notes__head">
+            <h4>Edit Note:</h4>
+          </div>
+        </div>
+        <div className="row">
+          <form
+            className="add__form"
+            onSubmit={event => {
+              event.preventDefault();
+              updateNote({
+                id: id,
+                title: title,
+                text: text
+              });
+              history.push(`/viewnote/${match.params.id}`);
+            }}
+          >
+            <input
+              className="input__title"
+              name="title"
+              value={this.state.title}
+              onChange={this.handleChange}
+              required
+            />
+            <textarea
+              cols="66"
+              rows="20"
+              name="text"
+              className="input__text"
+              value={this.state.text}
+              onChange={this.handleChange}
+              required
+            />
+            <button type="submit" className="save__button">
+              Save
+            </button>
+          </form>
         </div>
       </div>
-      <div className="row">
-        <form
-          className="add__form"
-          onSubmit={event => {
-            event.preventDefault();
-            updateNote({
-              id: match.params.id,
-              title: titleInput.value,
-              text: textInput.value
-            });
-            history.push(`/viewnote/${match.params.id}`);
-          }}
-        >
-          <input
-            className="input__title"
-            ref={title => (titleInput = title)}
-            placeholder={note.title}
-            required
-          />
-          <textarea
-            cols="66"
-            rows="20"
-            className="input__text"
-            ref={text => (textInput = text)}
-            placeholder={note.text}
-            required
-          />
-          <button type="submit" className="save__button">
-            Save
-          </button>
-        </form>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
