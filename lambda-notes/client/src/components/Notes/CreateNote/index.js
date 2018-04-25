a724b2044079061fbc11fbec6512bf69d22b04a0
Kyle Kauzlarich <kauzlarich.kyle@gmail.com>
2018-04-25 12:11:50 -0700

    Refactored it is now writing notes to local storage and i fixed the id bug i encountered yesterday - id now persists

--- lambda-notes/client/src/components/Notes/CreateNote/index.js
+++ lambda-notes/client/src/components/Notes/CreateNote/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './index.css';
 
-let id = 8;
 
 class CreateNote extends Component {
     constructor() {
@@ -10,40 +9,62 @@ class CreateNote extends Component {
         this.state = {
           title: [],
           body: [],
+          notes: [],
         }
     }
+    componentDidMount() {
+      let data = JSON.parse(localStorage.getItem("note data string"))
+      this.setState({ notes: data })
+    }
     idGen = () => {
-   //   if (id === -5) id = 0;
-   return ++id;
+   let id = this.state.notes.length;
+   return id;
+  //   const something = function() {
+  //    ++id
+  //    return id;
+  //    return something();
+  //  }
     }
     handleAddNote = event => {
       this.setState({ [event.target.name]: event.target.value });
     };
+
+    NewNote = () => {
+      let obj = {
+        id: this.idGen(),
+        title: this.state.title,
+        body: this.state.body
+      }
+      this.state.notes.push(obj);
+    }
     AddNote = () => {
-      axios.post(`http://localhost:5000/api/notes/`, {
-            "id": ++id,
-            "title": this.state.title,
-            "body": this.state.body
-        })
-        .then(response => {
-       //   this.setState(() => ({ movie: response.data }));
-       console.log('check postman and all other sources')
-        })
-        .catch(error => {
-          console.error(error);
-        });
+      // axios.post(`http://localhost:5000/api/notes/`, {
+      //       "id": ++id,
+      //       "title": this.state.title,
+      //       "body": this.state.body
+      //   })
+      //   .then(response => {
+      //  //   this.setState(() => ({ movie: response.data }));
+      //  console.log('check postman and all other sources')
+      //   })
+      //   .catch(error => {
+      //     console.error(error);
+      //   });
     };
     handleSubmitNote = () => {
 //     const { title, body } = this.state;
 //     title.push(this.state.title);
 //     body.push(this.state.body);
-     this.AddNote();
+//     this.AddNote();
+     this.NewNote();
      this.setState({ title: ""});
      this.setState({ body: ""});
+     localStorage.setItem("names", JSON.stringify(this.state.notes));
     };
 
     render() {
       console.log('state', this.state)
+      console.log('idgen', this.idGen())
       return (
         <div className='Create__note'>
         <h1>Create New Note:</h1>
