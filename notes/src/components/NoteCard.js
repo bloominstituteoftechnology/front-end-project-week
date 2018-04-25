d6c4eba7aba0415fe9705c105cf4f485df352171
Chris <cjmarsh725@gmail.com>
2018-04-25 07:45:10 -0700

    Note card tag display

--- notes/src/components/NoteCard.js
+++ notes/src/components/NoteCard.js
@@ -6,9 +6,12 @@ const NoteCard = props => {
   return (
     <div className="NoteCard-area">
       <div className="NoteCard-container">
-        <h3>{props.title}</h3>
-        <div className="NoteCard-divider"/>
-        <Markdown className="NoteCard-content" source={props.content}/>
+        <div className="NoteCard-header">
+          <h3>{props.title}</h3>
+          <div className="NoteCard-tags">{props.tags.length > 0 ? "Tags: " : ""}{props.tags.join(", ")}</div>
+        </div>
+        <div className="NoteCard-divider" />
+        <Markdown className="NoteCard-content" source={props.content} />
       </div>
     </div>
   );
