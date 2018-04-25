7f203562241d1662d6ca9589e76c6b7d8e60ebcf
Axhon <axhonruiz@gmail.com>
2018-04-25 11:52:26 -1000

    styled sidebar

--- todo-frontend/src/components/Sidebar.js
+++ todo-frontend/src/components/Sidebar.js
@@ -5,14 +5,26 @@ const SidebarContainer = Styled.aside`
     position: sticky;
     height: 100vh;
     top: 0;
+    background-color: rgb(211,211,211);
 `
 
-const Button = Styled.button``
+const Button = Styled.button`
+    background-color: rgb(0,184,188);
+    color: rgb(213,241,241);
+    font-weight: bold;
+    font-size:1.3rem;  
+    width: 80%; 
+    padding: 10px;
+`
+
+const Title = Styled.h2`
+    color: rgb(22,22,22);
+`
 
 /** @type React.StatelessComponent<{}> */
 const Sidebar = props => (
   <SidebarContainer>
-    <h2>Lambda Notes</h2>
+    <Title>Lambda Notes</Title>
     <Button>View your Notes</Button>
     <Button>+Create New Note</Button>
   </SidebarContainer>
