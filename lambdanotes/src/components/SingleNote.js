49265347c3dcf52dd55dc53d3b38141e297ba2ad
Nedu Robert <nedumrobert@gmail.com>
2018-04-26 00:47:30 +0100

    added styling for modal buttons

--- lambdanotes/src/components/SingleNote.js
+++ lambdanotes/src/components/SingleNote.js
@@ -51,7 +51,26 @@ const DeleteButton = styled.button`
     font-size: 1.5em;
     margin: 0 1em 1.3em 0em;
     cursor: pointer;
-`
+`;
+
+const StyledButton = styled(Button)`
+    padding: 0.1em 2em;
+    font-size: 1em;
+    font-weight: bold;
+    border-radius: 0;
+    margin: 1em;
+`;
+
+const CancelButton = styled.button`
+    background-color: #00b9bc;
+    color: #fff;
+    padding: 0.4em 3em;
+    font-size: 1em;
+    font-weight: bold;
+    border-radius: 0;
+    cursor: pointer;
+    border: none;
+`;
 
 class SingleNote extends Component {
     constructor(props) {
@@ -85,7 +104,6 @@ class SingleNote extends Component {
         const NoteId = this.props.match.params.id;
         const note = this.props.notes.find(item => item.id === NoteId);
         
-        
         return <Wrapper>
             <LinksContainer>
               <StyledLink to={`/edit/${note.id}`}>
@@ -96,11 +114,11 @@ class SingleNote extends Component {
                   <StyledModalBody>
                     Are you sure you want to delete this?
                     <Link to="/notes" onClick={() => this.handleDelete(NoteId)}>
-                      <Button color="danger">Delete</Button>
+                      <StyledButton color="danger">Delete</StyledButton>
                     </Link>
-                    <Button color="secondary" onClick={() => this.toggleModal()}>
+                    <CancelButton color="secondary" onClick={() => this.toggleModal()}>
                       No
-                    </Button>
+                    </CancelButton>
                   </StyledModalBody>
                 </StyledModal> : null}
             </LinksContainer>
