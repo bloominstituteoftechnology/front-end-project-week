f38d6301a59e853d002c1e9bbef3d1667d7db9f9
Cruise Brantley <cruise.brantley@gmail.com>
2018-04-25 17:50:21 -0500

    https://cruise-notes.firebaseapp.com firebase hosted, not databased yet

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import Notes from './components/notes';
-import './App.css';
+import React, { Component } from "react";
+import Notes from "./components/notes";
+import "./App.css";
 
 class App extends Component {
   render() {
---
+++ notes/src/fire.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+var config = {
+  apiKey: "AIzaSyDoUmaL2sDHRdgO5BHwZt6gVU2wcw1hQEM",
+  authDomain: "cruise-notes.firebaseapp.com",
+  databaseURL: "https://cruise-notes.firebaseio.com",
+  projectId: "cruise-notes",
+  storageBucket: "cruise-notes.appspot.com",
+  messagingSenderId: "43897345232"
+};
+firebase.initializeApp(config);
+export default fire;
