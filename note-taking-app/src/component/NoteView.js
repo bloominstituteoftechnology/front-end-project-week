eb57315ddfadda4b1a627e3e3891b10e296507a0
Mack <macknwandu@github.com>
2018-04-25 18:47:36 -0500

    I don't even know anymore

--- note-taking-app/src/component/NoteView.js
+++ note-taking-app/src/component/NoteView.js
@@ -3,19 +3,38 @@ import { Link } from 'react-router-dom';
 import DeleteNote from './DeleteNote';
 import { Col } from 'reactstrap';
 
-const NoteView = props => {
-  console.log('props', props);
-  return (
-    <Col className='note'>
-      <div className='note__options'>
-        <Link className='link' to='/notes/edit/:id'>Edit</Link>
-        <DeleteNote label='Delete note'/>
-      </div>
-      <h4>{props.match.params.title}</h4>
-      <p>{props.match.params.content}</p>
-    </Col>
-  )
+export default class NoteView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: []
+    }
+  }
+  render() {
+    return (
+      <Col className='note'>
+        <div className='note__options'>
+          <Link className='link' to='/notes/edit/:id'>Edit</Link>
+          <DeleteNote label='Delete note'/>
+        </div>
+        <h4>{this.props.match.params.title}</h4>
+        <p>{this.props.match.params.content}</p>
+      </Col>
+    )
+  }
 }
+// const NoteView = props => {
+//   return (
+//     <Col className='note'>
+//       <div className='note__options'>
+//         <Link className='link' to='/notes/edit/:id'>Edit</Link>
+//         <DeleteNote label='Delete note'/>
+//       </div>
+//       <h4>{props.title}</h4>
+//       <p>{props.content}</p>
+//     </Col>
+//   )
+// }
 
 
-export default NoteView;
+// export default NoteView;
