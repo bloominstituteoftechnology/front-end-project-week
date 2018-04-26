8f57c3c76337a4b5beac3ee4305da178ee05e8e1
Jared Cuffe <jcuffe@gmail.com>
2018-04-26 14:21:16 -0700

    Implemented drag sort by holding notes in state.
    Refactored logic so that state.notes is always sorted and filtered.

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -3,6 +3,11 @@ import { compose } from 'redux'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { firebaseConnect, isLoaded, isEmpty } from 'react-redux-firebase'
+import {
+  SortableContainer,
+  SortableElement,
+  arrayMove
+} from 'react-sortable-hoc'
 import { Markdown, DownloadButton } from '.'
 import styled from 'styled-components'
 
@@ -10,7 +15,9 @@ const Container = styled.div`
   display: flex
   flex-direction: column
 
-  div:last-child { align-self: center }
+  div:last-child {
+    align-self: center
+  }
 `
 
 const Controls = styled.div`
@@ -18,23 +25,24 @@ const Controls = styled.div`
   align-items: center
   align-self: flex-end
   label {
-    margin-right: 10px;
+    margin-right: 10px
   }
   input {
-    margin-right: 20px;
+    margin-right: 20px
   }
 `
 
 const ListContainer = styled.div`
   display: flex
-  justify-content: ${(props) =>
-    props.children.length >= 3 ? 'space-between' : 'space-evenly'}
+  justify-content: ${(props) => props.children.length >= 3 
+    ? 'space-between' 
+    : 'space-evenly'
+  }
   flex-wrap: wrap 
   width: 100%
 `
 
 const NoteContainer = styled.div`
-  width: 31.5%
   height: 200px
   overflow: hidden
   background-color: white
@@ -55,42 +63,46 @@ class NotesList extends Component {
     filter: ''
   }
 
-  noteElements = (orderBy) => {
-    const notes = this.notes()
-    return !isLoaded(notes)
-      ? 'Loading'
-      : isEmpty(notes)
-        ? 'No notes to display'
-        : notes
-            .filter(
-              ({ value: { title, content } }) =>
-                title.toLowerCase().includes(this.state.filter) ||
-                content.toLowerCase().includes(this.state.filter)
-            )
-            .map(({ key, value }, index) => (
-              <Note
-                key={index}
-                id={key}
-                title={value.title}
-                content={value.content}
-              />
-            ))
+  static getDerivedStateFromProps(nextProps, prevState) {
+    return {
+      ...prevState,
+      notes: nextProps.notes[prevState.orderBy]
+    }
+  }
+  
+  onSortEnd = ({ oldIndex, newIndex }) => {
+    this.setState({ notes: arrayMove(this.state.notes, oldIndex, newIndex) })
   }
 
-  notes = () => this.props.notes[this.state.orderBy]
+  noteElements = () => {
+    const { notes } = this.state
+    if (isLoaded(notes) && !isEmpty(notes)) {
+      return <SortableNotesList notes={notes} onSortEnd={this.onSortEnd} />
+    }
+
+    return <h3>Loading...</h3>
+  }
 
   makeCSV = () => ({
     mime: 'text/plain',
     filename: 'notes.csv',
     contents: this.notes()
-      .map(({ key, value: { title, content } }) =>
-        [key, title, content].join()
-      )
+      .map(({ key, value: { title, content } }) => [key, title, content].join())
       .join('\r\n')
   })
 
   handleSelect = (e) => this.setState({ orderBy: e.target.value })
-  handleFilter = (e) => this.setState({ filter: e.target.value.toLowerCase() })
+  handleFilter = (e) => {
+    const { notes } = this.state
+    const filter = e.target.value.toLowerCase()
+    this.setState({ 
+      filter,
+      notes: notes.filter(({ value: { title, content } }) => (
+          title.toLowerCase().includes(filter) ||
+          content.toLowerCase().includes(filter)
+      ))
+    })
+  }
 
   render() {
     return (
@@ -105,12 +117,29 @@ class NotesList extends Component {
           </select>
         </Controls>
         <h1>Your notes:</h1>
-        <ListContainer>{this.noteElements(this.state.orderBy)}</ListContainer>
+        <ListContainer>
+          {this.noteElements()}
+        </ListContainer>
         <DownloadButton genFile={this.makeCSV} downloadTitle="Export to CSV" />
       </Container>
     )
   }
 }
+
+const SortableNotesList = SortableContainer(({ notes }) => (
+  <ul>
+    {notes.map((note, index) => {
+      return <SortableNote key={`note-${index}`} index={index} note={note} />
+    })}
+  </ul>
+))
+
+const SortableNote = SortableElement(({ note: { key, value } }) => (
+  <Note
+    id={key}
+    title={value.title}
+    content={value.content} />
+))
 
 const Note = ({ id, title, content }) => (
   <NoteContainer>
