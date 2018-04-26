006b5ba00ed67b63a7344dbd9dd6e49173dbec36
tanking <tanking@github.com>
2018-04-26 18:22:25 -0400

    Refactored code to pass down object to different components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 
 import './App.css';
-import NotesDisplay from './components/NotesDisplay/NotesDisplay';
+import { NotesDisplay } from './components/NotesDisplay/NotesDisplay';
 import NewNote from './components/NewNote/NewNote';
 import Notes from './components/Notes/Notes';
 import Edit from './components/Edit/Edit';  
@@ -15,10 +15,18 @@ class App extends Component {
     }
   }
   
+  componentDidMount() {
+    const note = ['Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut deserunt consequuntur autem, deleniti perspiciatis placeat commodi inventore ducimus'];
+    this.setState ({ note });
+}
+
   render() {
     return (
       <div>
-        <Route exact path = '/' component={ NotesDisplay } />
+        <Route exact path = '/' render={(props) => (
+          <NotesDisplay {...props} note={this.state.note} /> 
+          )}/>
+        />
         <Route path = '/newNote' component={ NewNote } />
         <Route path = '/noteView' component={ Notes } />
         <Route path = '/edit' component= { Edit } />
--- my-app/src/components/NotesDisplay/NotesDisplay.js
+++ my-app/src/components/NotesDisplay/NotesDisplay.js
@@ -1,103 +1,90 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './NotesDisplay.css';
 
 import { Link } from 'react-router-dom';
-class NotesDisplay extends Component {
-    constructor() {
-        super();
-        this.state = {
-            note: []
-        }
-    }
 
-    componentDidMount() {
-        const note = ['Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut deserunt consequuntur autem, deleniti perspiciatis placeat commodi inventore ducimus'];
-        this.setState ({ note });
-    }
-
-    render() {
-        return (
-            <div className="Note">
-                <div className= 'Note-feature'> 
-                    <h1> Lambda Notes </h1>
-                    <button className ='Note-buttons'>
-                        <Link to= '/'> View Your Notes </Link>
-                    </button>
-                    <button className ='Note-buttons'>
-                        <Link to= '/newNote'> +Create New Note </Link>
-                    </button>
-                </div>
-                <div className='Note-content'>
-                    <h1> Your Notes: </h1> 
-                    <div className= 'Note-notesDisplay'>
-                        {this.state.note.map(note => {
-                            return [<div className = 'Note-notes'> 
-                            <Link to = '/noteView' >
-                            <h3>Note Title:</h3> <p>{ note }</p>
-                        </Link>
-                        </div>];
-                         })}
-                        {this.state.note.map(note => {
-                            return [<div className = 'Note-notes'> 
-                            <Link to = '/noteView' >
-                            <h3>Note Title:</h3> <p>{ note }</p>
-                        </Link>
-                        </div>];
-                         })}
-                        {this.state.note.map(note => {
-                            return [<div className = 'Note-notes'> 
-                            <Link to = '/noteView' >
-                            <h3>Note Title:</h3> <p>{ note }</p>
-                        </Link>
-                        </div>];
-                         })}
-                        {this.state.note.map(note => {
-                            return [<div className = 'Note-notes'> 
-                            <Link to = '/noteView' >
-                            <h3>Note Title:</h3> <p>{ note }</p>
-                        </Link>
-                        </div>];
-                         })}
-                        {this.state.note.map(note => {
-                            return [<div className = 'Note-notes'> 
-                            <Link to = '/noteView' >
-                            <h3>Note Title:</h3> <p>{ note }</p>
-                        </Link>
-                        </div>];
-                         })}
-                        {this.state.note.map(note => {
-                            return [<div className = 'Note-notes'> 
-                            <Link to = '/noteView' >
-                            <h3>Note Title:</h3> <p>{ note }</p>
-                        </Link>
-                        </div>];
-                         })}
-                        {this.state.note.map(note => {
-                            return [<div className = 'Note-notes'> 
-                            <Link to = '/noteView' >
-                            <h3>Note Title:</h3> <p>{ note }</p>
-                        </Link>
-                        </div>];
-                         })}
-                        {this.state.note.map(note => {
-                            return [<div className = 'Note-notes'> 
-                            <Link to = '/noteView' >
-                            <h3>Note Title:</h3> <p>{ note }</p>
-                        </Link>
-                        </div>];
-                         })}
-                        {this.state.note.map(note => {
-                            return [<div className = 'Note-notes'> 
-                            <Link to = '/noteView' >
-                            <h3>Note Title:</h3> <p>{ note }</p>
-                        </Link>
-                        </div>];
-                         })}
-                    </div>
+export const NotesDisplay = (props) => {
+    return (
+        <div className="Note">
+            <div className= 'Note-feature'> 
+                <h1> Lambda Notes </h1>
+                <button className ='Note-buttons'>
+                    <Link to= '/'> View Your Notes </Link>
+                </button>
+                <button className ='Note-buttons'>
+                    <Link to= '/newNote'> +Create New Note </Link>
+                </button>
+            </div>
+            <div className='Note-content'>
+                <h1> Your Notes: </h1> 
+                <div className= 'Note-notesDisplay'>
+                    {props.note.map(note => {
+                        return [<div className = 'Note-notes'> 
+                        <Link to = '/noteView' >
+                        <h3>Note Title</h3> <p>{ note }</p>
+                    </Link>
+                    </div>];
+                        })}
+                    {props.note.map(note => {
+                        return [<div className = 'Note-notes'> 
+                        <Link to = '/noteView' >
+                        <h3>Note Title</h3> <p>{ note }</p>
+                    </Link>
+                    </div>];
+                        })}
+                    {props.note.map(note => {
+                        return [<div className = 'Note-notes'> 
+                        <Link to = '/noteView' >
+                        <h3>Note Title</h3> <p>{ note }</p>
+                    </Link>
+                    </div>];
+                        })}
+                    {props.note.map(note => {
+                        return [<div className = 'Note-notes'> 
+                        <Link to = '/noteView' >
+                        <h3>Note Title</h3> <p>{ note }</p>
+                    </Link>
+                    </div>];
+                        })}
+                    {props.note.map(note => {
+                        return [<div className = 'Note-notes'> 
+                        <Link to = '/noteView' >
+                        <h3>Note Title</h3> <p>{ note }</p>
+                    </Link>
+                    </div>];
+                        })}
+                    {props.note.map(note => {
+                        return [<div className = 'Note-notes'> 
+                        <Link to = '/noteView' >
+                        <h3>Note Title</h3> <p>{ note }</p>
+                    </Link>
+                    </div>];
+                        })}
+                    {props.note.map(note => {
+                        return [<div className = 'Note-notes'> 
+                        <Link to = '/noteView' >
+                        <h3>Note Title</h3> <p>{ note }</p>
+                    </Link>
+                    </div>];
+                        })}
+                    {props.note.map(note => {
+                        return [<div className = 'Note-notes'> 
+                        <Link to = '/noteView' >
+                        <h3>Note Title</h3> <p>{ note }</p>
+                    </Link>
+                    </div>];
+                        })}
+                    {props.note.map(note => {
+                        return [<div className = 'Note-notes'> 
+                        <Link to = '/noteView' >
+                        <h3>Note Title</h3> <p>{ note }</p>
+                    </Link>
+                    </div>];
+                        })}
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
-export default NotesDisplay;
+
