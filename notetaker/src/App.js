eb8903b7a5c27d6bb0c4645f879e8e4e44ce4e38
totsuya <supertatortots@gmail.com>
2018-04-26 04:57:36 -0400

    added addNote and editNote methods

--- notetaker/src/App.js
+++ notetaker/src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
 import { Route } from "react-router";
 import { Row, Col, Container } from "reactstrap";
-import Sidebar from './components/Sidebar/Sidebar.js';
+import Sidebar from "./components/Sidebar/Sidebar.js";
 import "./App.css";
 import NoteList from "./components/NoteList/NoteList";
 import CreateNote from "./components/CreateNote/CreateNote";
 // import Note from "./components/Note/Note,";
-
-
 
 class App extends Component {
   constructor() {
@@ -17,19 +15,29 @@ class App extends Component {
     };
   }
 
+  addNote = newNote => {
+    this.setState({ note: [...this.state.cards, newNote] });
+  };
+
+  editNote = updateNote => {
+    this.setState({ note: [...this.state.updateNote]});
+  }
+
   render() {
-    return <div className="App">
+    return (
+      <div className="App">
         <Container>
           <Sidebar />
           <Row>
             <Col>
-              <Route exact path="/" component={ NoteList }/>
-              <Route path = '/CreateNote' component = { CreateNote }/>
+              <Route exact path="/" component={NoteList} />
+              <Route path="/CreateNote" component={CreateNote} />
               {/* <Route path = '/NoteView' component = { Notes }/> */}
             </Col>
           </Row>
         </Container>
-      </div>;
+      </div>
+    );
   }
 }
 
