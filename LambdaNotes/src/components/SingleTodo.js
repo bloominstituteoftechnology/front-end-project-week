34fc136067cac758ac4674450ad90db427fafa68
Justin Hong <jhong2009@gmail.com>
2018-04-26 11:14:39 -0700

    in SingleTodo.js: converted to class component with toggle handler. apply strike through to a note's content if its isComplete property is true

--- LambdaNotes/src/components/SingleTodo.js
+++ LambdaNotes/src/components/SingleTodo.js
@@ -1,24 +1,48 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Route } from "react-router-dom";
+import { connect } from "react-redux";
+// actions
+import { toggleTodo } from "../actions/index";
 // components
 import Todo from "./Todo";
 // material components
 import { Card, CardText } from "material-ui/Card";
 // styles
 import "../styles/SingleTodos.css";
-// <Route path="/todo/:id" {...props} render={props => <Todo {...props} />} />
-const SingleTodo = props => {
-	return [
-		<Link to={`/todo/${props.index}`}>
+
+// const SingleTodo = props => {
+class SingleTodo extends React.Component {
+	// change a todo's completion status
+	handleToggleTodo = id => {
+		console.log("MADE IT TO handleToggleTodo: ", id);
+		this.props.toggleTodo(id);
+	};
+
+	render() {
+		// console.log("SINGLETODO PROPS: ", this.props);
+		const { id } = this.props.todo;
+		// console.log("SINGLETODO ID: ", this.props.todo.id);
+		console.log("COMPLETE: ", this.props.todo.isComplete);
+		return [
 			<Card className="SingleTodo">
-				<CardText>
-					<h3>{props.todo.title}</h3>
-					<p>{props.todo.text}</p>
+				<CardText onClick={() => this.handleToggleTodo(id)}>
+					<Link to={`/todo/${this.props.index}`}>
+						<h3>{this.props.todo.title}</h3>
+					</Link>
+					<p
+						style={{
+							textDecoration: this.props.todo.isComplete
+								? "line-through"
+								: "none"
+						}}
+					>
+						{this.props.todo.text}
+					</p>
 				</CardText>
 			</Card>
-		</Link>
-	];
-};
+		];
+	}
+}
 
-export default SingleTodo;
+export default connect(null, { toggleTodo })(SingleTodo);
