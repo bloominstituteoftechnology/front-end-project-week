b0ece06c9bc4a932419b92177111c01f6d52582e
Henry K <mykim1891@hotmail.com>
2018-04-26 16:09:53 -0700

    more CSS changes

--- front-end/src/components/sub-component/CreateNoteItem.js
+++ front-end/src/components/sub-component/CreateNoteItem.js
@@ -2,6 +2,7 @@ import "./CreateNoteItem.css";
 
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Button } from "reactstrap";
 
 class CreateNoteItem extends Component {
   constructor(props) {
@@ -18,33 +19,44 @@ class CreateNoteItem extends Component {
       <div className="Note_Creator">
         <h1 className="CreateNoteText">Create New Note:</h1>
         <div className="CreateNoteContainer">
-          <input
-            type="text"
-            placeholder="Title"
-            name="title"
-            value={this.state.title}
-            onChange={e => this.setState({ [e.target.name]: e.target.value })}
-          />
-          <input
-            type="text"
-            placeholder="Content"
-            name="content"
-            value={this.state.content}
-            onChange={e => this.setState({ [e.target.name]: e.target.value })}
-          />
-          <Link to="/">
-            <button
-              onClick={() => {
-                this.props.mainProps.createNote({
-                  title: this.state.title,
-                  content: this.state.content
-                });
-                this.setState({ title: "", content: "" });
-              }}
-            >
-              Save
-            </button>
-          </Link>
+          <div className="createInputTitle">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Title"
+              name="title"
+              value={this.state.title}
+              onChange={e => this.setState({ [e.target.name]: e.target.value })}
+            />
+          </div>
+          <div className="createInputContent">
+            <textarea
+              className="form-control"
+              type="text"
+              placeholder="Write your note here!"
+              name="content"
+              rows="10"
+              value={this.state.content}
+              onChange={e => this.setState({ [e.target.name]: e.target.value })}
+            />
+          </div>
+          <div className="createSaveButton">
+            <Link to="/">
+              <Button
+                className="btn btn-lg btn-block"
+                color="primary"
+                onClick={() => {
+                  this.props.mainProps.createNote({
+                    title: this.state.title,
+                    content: this.state.content
+                  });
+                  this.setState({ title: "", content: "" });
+                }}
+              >
+                Save
+              </Button>
+            </Link>
+          </div>
         </div>
       </div>
     );
