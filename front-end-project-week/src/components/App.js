71f8cc0307361c2275cd7ea0726a5de92b50dc80
Your Name <you@example.com>
2018-04-26 15:43:58 -0700

    message

--- front-end-project-week/src/components/App.js
+++ front-end-project-week/src/components/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import ReactDom from "react-dom";
-import statement from "./App-intro";
-import list from "App.js";
+import logo from "../logo.svg";
+import "../styles/App.css";
+// import ReactDom from "react-dom";
 
 class App extends Component {
   render() {
@@ -13,8 +11,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="">
+          To get started, edit <code>Lambda Notes</code> and save to reload.
         </p>
       </div>
     );
--- front-end-project-week/src/index.js
+++ front-end-project-week/src/index.js
@@ -1,12 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import styles from "./index.css";
-import App from "./App";
+import "./index.css";
+import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
 
-const ADD_TODO = DocumentType;
-VBArray;
-NoteList: {
-}
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
