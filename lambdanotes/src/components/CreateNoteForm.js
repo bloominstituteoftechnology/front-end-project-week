45f6f061ada3b25ae319768817db47f09a80ac54
Nedu Robert <nedumrobert@gmail.com>
2018-04-26 23:12:11 +0100

    refactored spacing

--- lambdanotes/src/components/CreateNoteForm.js
+++ lambdanotes/src/components/CreateNoteForm.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  background-color: #f2f1f2; 
-  width: 75%;
-  padding: 60px 0 70px 2.4em;
+    background-color: #f2f1f2; 
+    width: 75%;
+    padding: 60px 0 70px 2.4em;
 `;
 
 const Heading = styled.h3`
-  font-size: 2em;
-  font-weight: bold;
-  line-height: 0.8;
+    font-size: 2em;
+    font-weight: bold;
+    line-height: 0.8;
 `;
 
 const Form = styled.form`
@@ -31,15 +31,15 @@ const TextArea = styled.textarea`
 `;
 
 const Button = styled.button`
-  background-color: #00b9bc;
-  color: #fff;
-  font-size: 1.5em;
-  font-weight: bold;
-  margin-bottom: 18px;
-  padding: 14px 2.3em 8px 2.5em;
-  margin-left: 0.2em;
-  cursor: pointer;
-  width: 25%;
+    background-color: #00b9bc;
+    color: #fff;
+    font-size: 1.5em;
+    font-weight: bold;
+    margin-bottom: 18px;
+    padding: 14px 2.3em 8px 2.5em;
+    margin-left: 0.2em;
+    cursor: pointer;
+    width: 25%;
 `;
 
 class CreateNoteForm extends Component {
