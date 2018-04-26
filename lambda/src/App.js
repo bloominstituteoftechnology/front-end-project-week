2ed5223e86d80a3f4b6a9fb6754f8b8e550fba90
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-04-25 21:30:24 -0600

    Created component

--- lambda/src/App.js
+++ lambda/src/App.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
-import { Button } from 'reactstrap';
+import NotesList from './components/NotesList.js'; 
+import CreateNew from './components/CreateNew.js';
+import ListView from './components/ListView.js';
+import Navigation from './components/Navigation.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      notes: []
+    };
+  }
+
+  componentDidMount() {
+    this.newNotes();
+
+    newNotes = () => {
+    axios
+    .get('http://localhost:3000')
+    .then(response => {
+      this.setState({ notes: response.data });
+  })
+  .catch(err => {
+  });
+ }
+}
   render() {
+    const { notes } = this.state;
     return (
       <div className="App">
         <header className="App-header">
-         
-          <h1 className="App-title">Welcome to React</h1>
+         <h1 className="App-title">Lambda Notes</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+       </div>
     );
   }
 }
