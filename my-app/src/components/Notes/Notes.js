c9546f7cfb68ea6e06fc9a7bf66ff4d614e8f9f1
tanking <tanking@github.com>
2018-04-26 13:35:01 -0400

    Fixed styling on Notes display

--- my-app/src/components/Notes/Notes.js
+++ my-app/src/components/Notes/Notes.js
@@ -24,9 +24,9 @@ class Notes extends Component {
                     </div>
                     <div className='Note-body'>
                         <h1> Note Name </h1>
-                        <p> 
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia nulla vitae error commodi. Beatae, ea! Unde veniam voluptas earum fugit quasi, nemo aperiam vero! Fugit, repellat. Iste quos sunt amet.
-                        </p>
+                        <div className='Note-text'>
+                            <p> lorem </p>
+                        </div>
                     </div>
                 </div>
             </div>
