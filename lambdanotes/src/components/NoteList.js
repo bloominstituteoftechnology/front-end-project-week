14c813a6075df0e75f92cd349c1bdeaa9c4a3df8
nickbrenn <nbren@tutanota.com>
2018-04-26 12:32:49 -0400

    Implemented a search bar that can display a list of notes matching either title or content

--- lambdanotes/src/components/NoteList.js
+++ lambdanotes/src/components/NoteList.js
@@ -1,17 +1,46 @@
 import React, { Component } from "react";
-import { Container } from "reactstrap";
+import { Container, Input } from "reactstrap";
 
 import NoteCard from "./NoteCard";
 import "./NoteList.css";
 
 class NoteList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchInput: ""
+    };
+  }
+
+  handleSearchInput = e => {
+    this.setState({ searchInput: e.target.value });
+  };
+
   render() {
     return (
       <div>
-        <h3>Your Notes: </h3>
+        <Input
+          className="searchbar"
+          type="text"
+          placeholder="search by title"
+          onChange={this.handleSearchInput}
+          value={this.state.searchInput}
+        />
+        <h3 id="your-notes">Your Notes: </h3>
         <Container className="note-list">
           {this.props.notes.map((note, index) => {
-            return <NoteCard key={note.title + note.id} note={note} />;
+            if (this.state.searchInput === "") {
+              return <NoteCard key={note.title + note.id} note={note} />;
+            } else if (
+              note.title
+                .toLowerCase()
+                .includes(this.state.searchInput.toLowerCase()) ||
+              note.content
+                .toLowerCase()
+                .includes(this.state.searchInput.toLowerCase())
+            ) {
+              return <NoteCard key={note.title + note.id} note={note} />;
+            }
           })}
         </Container>
       </div>
