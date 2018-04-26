dde614d5e677c67e4843867f3abe7078feadc2a4
Shawn Stewart <s.stewart0220@gmail.com>
2018-04-26 14:05:05 -0500

    Added scroll bars for BIG notes.

--- lambda-notes/src/Components/LandingPage.js
+++ lambda-notes/src/Components/LandingPage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import './Content.css';
+import './LandingPage.css';
 
 export const LandingPage = () => {
   return(
     <div className='Content__Wrapper'>
-      <h3 className='Content__Heading'>Welcome back to Lambda Notes, [USER]</h3>
-      <p className='Content'>The best place to keep your notes!</p>
+      <h3 className='Content__Heading Greeting'>Welcome back to Lambda Notes, [USER]</h3>
+      <p className='Content Greeting__Message'>The best place to keep your notes!</p>
     </div>
   );
 }
