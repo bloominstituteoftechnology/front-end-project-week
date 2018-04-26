93e20f93a1a54f7671d2a081014317801f72a293
Jonathan Greene <greeneselectronics@gmail.com>
2018-04-26 14:01:14 -0600

    Setup routing for each note card

--- lambda-notes/src/Components/Note.js
+++ lambda-notes/src/Components/Note.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import { Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+import { BrowserRouter as Router, Route, Link,Switch } from "react-router-dom";
+// import {notesdata} from './App';
 
+// this holds the edit and delete buttons 
 export default class Note extends Component {
     constructor(props) {
         super(props);
@@ -11,10 +14,14 @@ export default class Note extends Component {
     render(){
     return <div>
         {console.log("this test is " + this.props)}
-        <Card body>
-                    <CardTitle> Title: {this.props.note.title}</CardTitle>
-                    <CardText>Body: {this.props.note.body}</CardText>   
-        </Card>
+                <Link to='/EditNote' className ='editnote' type= 'text' placeholder='edit' name='editNote'/>
+                <Link to='/CreateNote' className ='deletenote' type= 'text' placeholder='delete' name='deleteNote'/>
+        <Link to={`/Note/${this.props.note.title}`}>
+            <Card body>
+                        <CardTitle> Title: {this.props.note.title}</CardTitle>
+                        <CardText>Body: {this.props.note.body}</CardText>   
+            </Card>
+        </Link>
     </div>
     }
 }
