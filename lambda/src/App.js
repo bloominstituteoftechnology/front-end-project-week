b2d1e29e778ac3f128ea6d9f3e981a5ea084598d
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-04-26 18:47:56 -0600

    refactor components

--- lambda/src/App.js
+++ lambda/src/App.js
@@ -1,36 +1,27 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+//import './App.css';
+import NewNote from './components/NewNote.js';
+import EditNote from './components/EditNote.js';
+import DeleteNote from './components/DeleteNote.js';
+import ListNotes from './components/ListNotes.js';
 
-
-import './App.css';
-import { createNew, ListView, Navigation, NotesList } from './components';
-
-export default class App extends Component {
+ class App extends Component {
   constructor() {
     super()
     this.state = {
-      notes: []
+      notes: [{}]
     }
   }
-
-addToNotesList = (notes) => {
-  const notesList = this.state.notesList;
-  notesList.push(notes);
-  this.setState({ notesList });
-}
 
 render() {
   return (
     <div>
-     <notesList list={this.state.notesList} />
-     <Route exact path="/" component={listView} />
-     <Route path="/notes/:id" render={ (props) => {
-       return(<Note {...props} addToNotesList={this.addToNotesList}/>)
-     }}/>
+    <h2>Lambda Notes</h2>
+
      </div>
-      )    
+      );    
     }    
   }  
   
-
+export default App;
 
