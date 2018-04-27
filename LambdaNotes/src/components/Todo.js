bc8f1540bd3cae54f1f34c9a4d4b7ce5bb8ab6c8
Justin Hong <jhong2009@gmail.com>
2018-04-26 18:35:13 -0700

    in Todo.js: added a toggle that allows user to switch to a check list view and click on checkbox to complete an item

--- LambdaNotes/src/components/Todo.js
+++ LambdaNotes/src/components/Todo.js
@@ -3,20 +3,28 @@ import { connect } from "react-redux";
 import { Redirect } from "react-router";
 import { Link } from "react-router-dom";
 // actions
-import { deleteTodo } from "../actions/index";
+import { deleteTodo, toggleTodo } from "../actions/index";
 // material components
 import Paper from "material-ui/Paper";
 import FlatButton from "material-ui/FlatButton";
 import Dialog from "material-ui/Dialog";
+import Toggle from "material-ui/Toggle";
+import { List, ListItem } from "material-ui/List";
+import Checkbox from "material-ui/Checkbox";
 // styles
 import "../bootstrap/css/bootstrap.css";
 import "../styles/Todo.css";
+
+// toggle should toggle whether or not a list view of a card appears [onToggle]
+// default toggle status is false
+// new tasks should autogenerate another list field
 
 class Todo extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			open: false
+			open: false,
+			toggled: true
 		};
 	}
 	handleOpen = () => {
@@ -27,6 +35,12 @@ class Todo extends Component {
 	};
 	handleDeleteTodo = id => {
 		this.props.deleteTodo(id);
+	};
+	handleToggle = () => {
+		this.setState({ toggled: !this.state.toggled });
+	};
+	handleComplete = id => {
+		this.props.toggleTodo(this.props.todos[id].id);
 	};
 
 	render() {
@@ -51,6 +65,7 @@ class Todo extends Component {
 
 		return (
 			<div className="Todo">
+				{/* Delete Modal */}
 				<Dialog
 					title="Dialog With Actions"
 					actions={actions}
@@ -58,8 +73,9 @@ class Todo extends Component {
 					open={this.state.open}
 					onRequestClose={this.handleClose}
 				/>
+				{/* Note/List View Toggle, Edit Delete Buttons */}
 				<div className="row">
-					<div className="col-md-12">
+					<div className="col-md-12 Todo_button_container">
 						<Link
 							to={{
 								pathname: "/editTodo",
@@ -73,18 +89,48 @@ class Todo extends Component {
 						<FlatButton className="Todo_button" onClick={this.handleOpen}>
 							Delete
 						</FlatButton>
+						<Toggle className="Todo_toggle" onToggle={this.handleToggle} />
 					</div>
 				</div>
-
+				{/*	Note View */}
 				{this.props.todos.includes(this.props.todos[id]) ? (
-					<div className="row">
-						<div className="col-md-12">
-							<Paper className="Todo_content">
-								<h3 className="Todo_header">{this.props.todos[id].title}</h3>
-								<p className="Todo_text">{this.props.todos[id].text}</p>
-							</Paper>
+					this.state.toggled ? (
+						<Paper className="Todo_content">
+							<List>
+								<ListItem
+									primaryText={this.props.todos[id].text}
+									secondaryText={this.props.todos[id].title}
+									leftCheckbox={
+										<Checkbox onCheck={() => this.handleComplete(id)} />
+									}
+									style={{
+										textDecoration: this.props.todos[id].isComplete
+											? "line-through"
+											: "none"
+									}}
+								/>
+							</List>
+						</Paper>
+					) : (
+						<div className="row">
+							<div className="col-md-12">
+								<Paper className="Todo_content">
+									<h3 className="Todo_header">{this.props.todos[id].title}</h3>
+									<p
+										style={{
+											textDecoration: this.props.todos[id].isComplete
+												? "line-through"
+												: "none"
+										}}
+										// onClick={this.handleComplete(id)}
+										className="Todo_text"
+									>
+										{this.props.todos[id].text}
+									</p>
+								</Paper>
+							</div>
 						</div>
-					</div>
+					)
 				) : (
 					<Redirect to="/" />
 				)}
@@ -100,5 +146,6 @@ const mapStateToProps = state => {
 };
 
 export default connect(mapStateToProps, {
-	deleteTodo
+	deleteTodo,
+	toggleTodo
 })(Todo);
