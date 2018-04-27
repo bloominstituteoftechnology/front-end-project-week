08836614eb8b3e918f7915eca87adf31c18b06b2
Shawn Stewart <s.stewart0220@gmail.com>
2018-04-27 15:39:14 -0500

    Added a 404 component.

---
+++ lambda-notes/src/Components/FourOhFour.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Content.css'
+
+export const FourOhFour = () => {
+  return(
+    <div className='Content__Wrapper'>
+      <h3 className='Content__Heading'>404 Page not found...</h3>
+    </div>
+  );
+}
