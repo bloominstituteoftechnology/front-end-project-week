01ff03422fdd18abbb50da4c51154ac48b32b381
Justin Hong <jhong2009@gmail.com>
2018-04-27 13:00:54 -0700

    in SingleTodo.js: added a footer with material icon menu to each card in list view

--- LambdaNotes/src/components/SingleTodo.js
+++ LambdaNotes/src/components/SingleTodo.js
@@ -5,7 +5,12 @@ import { connect } from "react-redux";
 import { toggleTodo } from "../actions/index";
 // material components
 import { Card, CardText } from "material-ui/Card";
+import IconMenu from "material-ui/IconMenu";
+import MenuItem from "material-ui/MenuItem";
+import IconButton from "material-ui/IconButton";
+import MoreVertIcon from "material-ui/svg-icons/navigation/more-vert";
 // styles
+import "../bootstrap/css/bootstrap.css";
 import "../styles/SingleTodos.css";
 
 // const SingleTodo = props => {
@@ -24,6 +29,7 @@ class SingleTodo extends React.Component {
 						<h3>{this.props.todo.title}</h3>
 					</Link>
 					<p
+						className="SingleTodo_content"
 						onClick={() => this.handleToggleTodo(id)}
 						style={{
 							textDecoration: this.props.todo.isComplete
@@ -33,6 +39,23 @@ class SingleTodo extends React.Component {
 					>
 						{this.props.todo.text}
 					</p>
+					<div className="row SingleTodo_footer">
+						<IconMenu
+							iconButtonElement={
+								<IconButton>
+									<MoreVertIcon className="SingleTodo__menu" />
+								</IconButton>
+							}
+							anchorOrigin={{ horizontal: "left", vertical: "top" }}
+							targetOrigin={{ horizontal: "left", vertical: "top" }}
+						>
+							<MenuItem primaryText="Refresh" />
+							<MenuItem primaryText="Send feedback" />
+							<MenuItem primaryText="Settings" />
+							<MenuItem primaryText="Help" />
+							<MenuItem primaryText="Sign out" />
+						</IconMenu>
+					</div>
 				</CardText>
 			</Card>
 		];
