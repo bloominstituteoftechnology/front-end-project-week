8777390dd678adf4f95210cce4df92c064228d84
Nedu Robert <nedumrobert@gmail.com>
2018-04-27 16:47:39 +0100

    removed reset css

--- lambdanotes/src/index.js
+++ lambdanotes/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import './reset.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
