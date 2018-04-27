52f2ffa8db430e86477ddf0205d5e485713969cd
mgchmiel <michaelchmielniak@yahoo.com>
2018-04-26 23:53:01 -0400

    added functionality to edit and delete in editnote.js and deletenote.js and also added in naviagation in nav.js

---
+++ lambda/src/Components/DeleteNote/DeleteNote.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { delete_note } from '../actions';
+import { Modal, ModalBody, ModalFooter } from 'reactstrap';
+import './DeleteNote.css';
+
+class DeleteNote extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false
+    };
+  }
+
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
+  render() {
+    return (
+      <div >
+        <button onClick={this.toggle} className='link'>delete</button>
+        <Modal isOpen={this.state.modal} toggle={this.toggle}>
+          <ModalBody>
+            Are you sure you want to delete this?
+          </ModalBody>
+          <ModalFooter>
+            <Link to='/' className='link' onClick={() => this.props.delete_note(this.props.id)}>Delete</Link>
+            <button className='link' onClick={this.toggle}>No</button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    notes: state.notes,
+  };
+};
+
+export default connect(mapStateToProps, { delete_note })(DeleteNote);
