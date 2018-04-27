9cf8e8b0727809fd509b9eef1a340381bac26a8b
Mark Wong <fmarkwong@gmail.com>
2018-04-26 19:45:32 -0700

    Use Dragula for drag and drop

--- lambda-notes/src/components/notesList.js
+++ lambda-notes/src/components/notesList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import $ from 'jquery';
+import Dragula from 'react-dragula';
 import Note from './note';
 import { Input } from 'reactstrap';
 
@@ -12,29 +12,16 @@ export default class NotesList extends Component {
     };
   }
 
-  componentDidMount() {
-  // see https://github.com/gridstack/gridstack.js/issues/735#issuecomment-329888796
-  let options = {
-    cellHeight: 63,
-    auto: true
-  };
-    if ('gridstack' in $('.grid-stack')) $('.grid-stack').gridstack(options);
-  }
-
   handleSearchStringChange = e => {
     this.setState({ searchString: e.target.value });
   };
 
-  componentDidUpdate() {
-    console.log('component did update');
-    if ('gridstack' in $('.grid-stack')) $('.grid-stack').gridstack();
-  }
-
-  refreshGrid() {
-    console.log('DID REFRESH!!!!!!!!!!!!!')
-    // if ('gridstack' in $('.grid-stack')) $('.grid-stack').gridstack();
-    let grid = $('.grid-stack').gridstack();
-  }
+  dragulaDecorator = (componentBackingInstance) => {
+    if (componentBackingInstance) {
+      let options = { };
+      Dragula([componentBackingInstance], options);
+    }
+  };
 
   render() {
     let notesToDisplay;
@@ -43,28 +30,20 @@ export default class NotesList extends Component {
       notesToDisplay = this.props.notes.filter(note => note.title.toLowerCase().match(this.state.searchString));
     } else {
       notesToDisplay = this.props.notes
-      // console.log('zero length. props.notes is', notesToDisplay);
     }
     return (
       <div>
         <br /><br />
         <h3>Your Notes:</h3><br />
         <Input onChange={this.handleSearchStringChange} placeholder="Search Notes" /><br />
-        <div className="grid-stack">
-          { console.log("NOTES TO DISPLAY", notesToDisplay) }
-
-          { 
-            notesToDisplay.map((note, i) => { 
-              return ( 
-                <div key={i} className="grid-stack-item"
-                  data-gs-auto-position
-                  data-gs-width="4" data-gs-height="3">
-                  <div className="grid-stack-item-content"><Note key={i} { ...note } { ...this.props } /></div>
-                </div>
-              );
-            })
-          }
-          { this.refreshGrid() }
+        <div className="note-list container" ref={this.dragulaDecorator}>
+        { 
+          notesToDisplay.map((note, i) => { 
+            return ( 
+              <Note key={i} { ...note } { ...this.props } />
+            );
+          })
+        }
         </div>
       </div>
     );
--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'gridstack/dist/gridstack.min.css';
-import 'jquery/dist/jquery.min.js'
-import 'gridstack/dist/gridstack.jQueryUI.min.js';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'gridstack/dist/gridstack.min.css';
+// import 'jquery/dist/jquery.min.js'
+// import 'gridstack/dist/gridstack.jQueryUI.min.js';
 // import 'gridstack/dist/gridstack.min.js';
+import 'dragula/dist/dragula.min.css';
 
 
 import App from './App';
