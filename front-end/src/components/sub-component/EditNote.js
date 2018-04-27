6eee84ab7ceba50c3162b4cdee453e03e1ff3509
Henry K <mykim1891@hotmail.com>
2018-04-27 00:00:20 -0700

    minor CSS change in Edit Page

--- front-end/src/components/sub-component/EditNote.js
+++ front-end/src/components/sub-component/EditNote.js
@@ -21,36 +21,49 @@ class EditNote extends Component {
   render() {
     // console.log("EditNote:", this.props.secondProps.mainProps);
     return (
-      <div>
+      <div className="Note_Editor">
         <h1>Note Editor</h1>
-        <input
-          type="text"
-          name="title"
-          value={this.state.title}
-          onChange={e => this.setState({ [e.target.name]: e.target.value })}
-        />
-        <input
-          type="text"
-          name="content"
-          value={this.state.content}
-          onChange={e => this.setState({ [e.target.name]: e.target.value })}
-        />
-        <Link to="/">
-          <button
-            onClick={() => {
-              this.props.secondProps.mainProps.editNote(
-                {
-                  title: this.state.title,
-                  content: this.state.content
-                },
-                this.props.secondProps.match.params.id
-              );
-              this.setState({ title: "", content: "" });
-            }}
-          >
-            Save
-          </button>
-        </Link>
+        <div className="EditNoteContainer">
+          <div className="EditInputTitle">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Title"
+              name="title"
+              value={this.state.title}
+              onChange={e => this.setState({ [e.target.name]: e.target.value })}
+            />
+          </div>
+          <div className="EditInputContent">
+            <textarea
+              className="form-control"
+              type="text"
+              placeholder="Write your note here!"
+              name="content"
+              rows="10"
+              value={this.state.content}
+              onChange={e => this.setState({ [e.target.name]: e.target.value })}
+            />
+          </div>
+          <div className="EditSaveButton">
+            <Link to="/">
+              <button
+                onClick={() => {
+                  this.props.secondProps.mainProps.editNote(
+                    {
+                      title: this.state.title,
+                      content: this.state.content
+                    },
+                    this.props.secondProps.match.params.id
+                  );
+                  this.setState({ title: "", content: "" });
+                }}
+              >
+                Save
+              </button>
+            </Link>
+          </div>
+        </div>
       </div>
     );
   }
