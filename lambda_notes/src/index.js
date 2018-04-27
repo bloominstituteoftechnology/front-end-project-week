17c7409bea62da00685b56c02ca02df025f37f27
SyCoDe <SyCoDe@gmail.com>
2018-04-27 09:42:40 -0600

    integrated firebase hosting, made build version, successfully deployed

--- lambda_notes/src/index.js
+++ lambda_notes/src/index.js
@@ -4,12 +4,23 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
+import * as firebase from "firebase";
 
 import App from "./App";
 import reducer from "./reducers";
-import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+// import registerServiceWorker from "./registerServiceWorker";
+
+const config = {
+  apiKey: "AIzaSyA8wI_2iX5hYcQ38fuM_79BW4ZXlI6lav8",
+  authDomain: "frontend-project-wk.firebaseapp.com",
+  databaseURL: "https://frontend-project-wk.firebaseio.com",
+  projectId: "frontend-project-wk",
+  storageBucket: "frontend-project-wk.appspot.com",
+  messagingSenderId: "690328231241"
+};
+firebase.initializeApp(config);
 
 // Redux store
 const store = createStore(
@@ -24,4 +35,4 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-registerServiceWorker();
+// registerServiceWorker();
