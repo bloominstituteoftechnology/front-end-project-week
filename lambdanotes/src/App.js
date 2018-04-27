47f08083a35752618c407eaf16de24b09b855a36
sadrak105 <sadrak10@gmail.com>
2018-04-27 19:27:32 -0400

    changes

--- lambdanotes/src/App.js
+++ lambdanotes/src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
-import NoteList from './Notes/noteList';
-import notes from './Notes/notes';
-
+import CreateNote from './Notes/createNote';
+import EditNote from './Notes/editNote';
+import NotesList from './Notes/noteList';
+import SideNav from './Notes/sideNav';
+import NoteView from './Notes/viewNote';
+import axios from 'axios';
 class App extends Component {
   constructor() {
     super();
@@ -12,21 +16,55 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.setState({ notes });
+    this.getNotes();
   }
+
+  getNotes = () => {
+      axios
+        .get(`http://localhost:5000/notes`)
+        .then(response => {
+          // console.log('response:', response);
+          this.setState({ notes: response.data });
+        })
+        .catch(error => {
+          console.error("Error!", error);
+        });
+  };
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-        <h1 className="Name-header">Lambda Notes</h1>
-        <button className="Button-view">View Your Notes</button>
-        <button className="Button-create">+ Create New Note</button>
-        </header>
-        <div className="Notes-Body"><NoteList {...this.state} /></div>
+      <div className="Body__note">
+        <SideNav />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <NotesList
+                getNotes={() => this.componentDidMount()}
+                notes={this.state.notes}
+              />
+            )}/>
+          <Route
+            path="/note/:id"
+            render={({ match }) => (
+              <NoteView getNotes={this.getNotes} match={match} />
+            )}/>
+          <Route
+            path="/create"
+            render={() => (
+              <CreateNote
+                getNotes={() => this.componentDidMount()}                     
+              />
+            )}/>
+          <Route
+            path="/edit"
+            render={({ location }) => (
+              <EditNote getNotes={this.getNotes} location={location} />
+            )}/>
+          </Switch>
       </div>
-    );
+   );
   }
 }
-
 export default App;
--- lambdanotes/src/Notes/noteList.js
+++ lambdanotes/src/Notes/noteList.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { Component } from "react";
+import Note from "./noteCard";
 import './noteList.css';
 
-const NoteList = props => {
-    return (
-        <div className="Note-Wrapper">
-        <div className="Body-Title"><h4>Your Notes:</h4></div>
-            {props.notes.map(note => (
-                <div className="Note-Body" key={note.id}>
-                    <h4>{`${note.title}`}</h4>
-                    <p>{`${note.text}`}</p>
-                </div>
-            ))}
-        </div>
-    )
-}
+export default class NotesList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: []
+    };
+  }
 
-export default NoteList;
+  render() {
+    return (
+      <div className="List__body">
+        <h4 className="List__header">Your Notes:</h4>
+            <div className="List__view">
+          {this.props.notes.map(note => (
+            <div className="List__notes">
+              <Note key={note.id} note={note} />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
