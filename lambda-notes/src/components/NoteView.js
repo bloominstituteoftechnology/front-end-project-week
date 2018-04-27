114209ea9d11956c869e8ab49601b5710982a573
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2018-04-27 12:58:57 -0700

    added modal

--- lambda-notes/src/components/NoteView.js
+++ lambda-notes/src/components/NoteView.js
@@ -4,24 +4,56 @@ import { connect } from 'react-redux';
 import './NoteView.css';
 import { Link } from 'react-router-dom';
 
+
 class NoteView extends Component {
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+          modal: false
+        }
+    }
+
+
   componentDidMount() {
     const id = this.props.match.params.id;
     this.props.getNote(id);
   }
 
+
   render() {
-      console.log(this.props)
-      return (
-        <div className="noteView">
-            <div className="buttons">
-                <Link to='/'><button className="deleteButton" 
-                    onClick={() => {
-                    this.props.deleteNote(this.props.note.id)}}>delete</button></Link>
+      console.log(this.props.className)
+      let modal = null;
+   if (this.state.modal === true) {
+    modal = (          
+        <div className="ModalContainer">
+          <div className="ModalContainer__ContentContainer">
+            <h2>Are you sure you want to delete this?</h2>
+            <div className="ModalContainer__ButtonsContainer">
+              <Link to='/' className="link">
+                <button className="ModalContainer__Button ModalContainer__WrappedButton red"
+                  onClick={() => {
+                    this.props.deleteNote(this.props.note.id)
+                    this.setState({modal: !this.state.modal});
+                }}>
+                  Delete
+                </button>
+              </Link>
+              <button className="ModalContainer__Button" onClick={() => this.setState({modal: !this.state.modal})} >No</button>
             </div>
+          </div>
+        </div>);
+    }
+    return (
+
+        <div className="noteView">
+              {modal}
+              <div className="buttons">
+              <button className="deleteButton penis" 
+                    onClick={() => { this.setState({ modal: !this.state.modal })}}>delete</button>
+              </div>
             <div className="oneTitle">{this.props.note.title}</div>
             <div className="oneContent">{this.props.note.content}</div>
+        
         </div>
       )
   }
