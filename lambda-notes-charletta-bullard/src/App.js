a6f3182801cbf11cd3fb50411dbfb3726ba3f4fb
charletta <charlettabullard12@outlook.com>
2018-04-28 16:16:14 -0400

    deleted sidebar, edited app.js to be the holder of links, delete css files

--- lambda-notes-charletta-bullard/src/App.js
+++ lambda-notes-charletta-bullard/src/App.js
@@ -1,43 +1,54 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import SideBar from './components/pages/SideBar';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import NotesList from './components/pages/NotesList';
 import { getNotes } from './dummyData';
 import SingleNote from './components/pages/SingleNote';
 import CreateANote from './components/pages/CreateANote';
 import DeleteNote from './components/pages/DeleteNote';
 
-
 class App extends Component {
-  constructor () {
-    super()
+  constructor() {
+    super();
     this.state = {
       notes: getNotes(),
     };
   }
 
-  
   render() {
     return (
       <Router>
-        <div className='App'>
-          <Route path='/' component={SideBar} />
-          <Route
-            path='/noteslist'
-            render={state => <NotesList notes={this.state.notes} exact />}
-          />
-          <Route
-            render={routeProps => (
-              <SingleNote
-                {...routeProps}
-                {...this.props}
-                notes={this.state.notes}
-              />
-            )}
-            path='/note/:id'
-          />
-          <Route path='/createanote' component={CreateANote} exact />
-          <Route path='/delete/:id' component={DeleteNote} />
+        <div className="container">
+          <div className="navBar">
+            <h1>Lambda Notes</h1>
+            <NavLink to="/">
+              <button type="submit" className="navButton1">
+                View Your Notes
+              </button>
+            </NavLink>
+            <NavLink to="/CreateANote">
+              <button type="submit" className="navButton2">
+                +Create New Note
+              </button>
+            </NavLink>
+          </div>
+          <div className="pathContainer">
+            <Route
+              path="/"
+              render={state => <NotesList notes={this.state.notes} exact />}
+            />
+            <Route
+              render={routeProps => (
+                <SingleNote
+                  {...routeProps}
+                  {...this.props}
+                  notes={this.state.notes}
+                />
+              )}
+              path="/note/:id"
+            />
+            <Route path="/createanote" component={CreateANote} />
+            <Route path="/delete/:id" component={DeleteNote} />
+          </div>
         </div>
       </Router>
     );
@@ -45,12 +56,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
