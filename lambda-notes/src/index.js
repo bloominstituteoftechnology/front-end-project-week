9169b35bac18f7cf2f5c5568c005e777fa8c64c8
Carlos Andrade <carlos.andrade86@outlook.com>
2018-04-28 01:57:28 -0400

    refactor

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -1,35 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-    BrowserRouter as Router,
-    Route,
-
-  } from 'react-router-dom';
-
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from './reducers';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './components/App';
-import CreateNoteForm from './components/CreateNoteForm';
-import NoteView from './components/NoteView';
-import EditNote from './components/EditNote'
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-
-const store = createStore(reducers)
-
-ReactDOM.render(
- <Provider store={store}>   
-    <Router>
-        <div>    
-            <Route exact path='/' component={ App } />
-            <Route path='/create-note' component={ CreateNoteForm } />
-            <Route path='/note-view/:id' component={ NoteView } />
-            <Route path='/edit-note/:id' component={ EditNote } />
-        </div>    
-    </Router>
-</Provider>
-, document.getElementById('root'));
-
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
