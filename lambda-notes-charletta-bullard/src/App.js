9417cf3bec5b5e1f61eefc23fe7d6e90d7d89735
charletta <charlettabullard12@outlook.com>
2018-04-29 08:38:09 -0400

    Changed everything back to its original setting... Edit and delete not functional

--- lambda-notes-charletta-bullard/src/App.js
+++ lambda-notes-charletta-bullard/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import NotesList from './components/pages/NotesList';
 import { getNotes } from './dummyData';
+import Sidebar from './components/pages/Sidebar';
 import SingleNote from './components/pages/SingleNote';
 import CreateANote from './components/pages/CreateANote';
 import DeleteNote from './components/pages/DeleteNote';
@@ -17,38 +18,24 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div className="container">
-          <div className="navBar">
-            <h1>Lambda Notes</h1>
-            <NavLink to="/">
-              <button type="submit" className="navButton1">
-                View Your Notes
-              </button>
-            </NavLink>
-            <NavLink to="/CreateANote">
-              <button type="submit" className="navButton2">
-                +Create New Note
-              </button>
-            </NavLink>
-          </div>
-          <div className="pathContainer">
-            <Route
-              path="/"
-              render={state => <NotesList notes={this.state.notes} exact />}
-            />
-            <Route
-              render={routeProps => (
-                <SingleNote
-                  {...routeProps}
-                  {...this.props}
-                  notes={this.state.notes}
-                />
-              )}
-              path="/note/:id"
-            />
-            <Route path="/createanote" component={CreateANote} />
-            <Route path="/delete/:id" component={DeleteNote} />
-          </div>
+        <div className="App">
+          <Route path="/" component={Sidebar} />
+          <Route
+            path="/noteslist"
+            render={state => <NotesList notes={this.state.notes} exact />}
+          />
+          <Route
+            render={routeProps => (
+              <SingleNote
+                {...routeProps}
+                {...this.props}
+                notes={this.state.notes}
+              />
+            )}
+            path="/note/:id"
+          />
+          <Route path="/createanote" component={CreateANote} exact />
+          <Route path="/delete/:id" component={DeleteNote} />
         </div>
       </Router>
     );
