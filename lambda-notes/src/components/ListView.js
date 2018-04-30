019b6570761b92b4c541880a49440a89b2398102
crypto-jones <jeremyjones@Macintosh-58b035f2f989.local>
2018-04-30 15:17:48 -0700

    deployment test

--- lambda-notes/src/components/ListView.js
+++ lambda-notes/src/components/ListView.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { CSVLink } from 'react-csv';
-
 import 'bootstrap/dist/css/bootstrap.css';
 import './ListView.css';
 
