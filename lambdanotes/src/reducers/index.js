f367b5a938a04eb0b76742e2ccac46c4f5ffc7fa
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-04-30 16:37:18 -0700

    Trying with Firebase

--- lambdanotes/src/reducers/index.js
+++ lambdanotes/src/reducers/index.js
@@ -20,22 +20,24 @@ export default function notes(state = initialState, action) {
             };
 
         case EDIT_NOTE:
+            let copy = sate.notes.slice();
+            copy.filter(note => note.id !== action.id).push(action.note);
             return{
                 ...state,
-                notes:
+                notes: copy
             };
 
         case DEL_NOTE:
             return{
                 ...state,
-                notes:
+                notes: state.notes.slice().filter(note => note.id !== action.id)
             };
 
         case ERROR:
-            return{
-                ...state,
-                notes:
-            };
+            console.log("Error Occurred", action.error);
+            return { ...state, authed: false, error: action.error };
+                default:
+            return state;
 
         }
 }
