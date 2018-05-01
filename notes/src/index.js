71071c3da163795f0f4f1c718382e508d9e873c7
Alexander <alexander.botello@g.austincc.edu>
2018-04-30 22:04:05 -0500

    Connect react-router to app

--- notes/src/index.js
+++ notes/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>, document.getElementById('root'));
