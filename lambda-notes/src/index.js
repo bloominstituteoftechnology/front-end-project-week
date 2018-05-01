ad57b7a2bfcecc9ef6d95ba01c2f02ebcceb67a1
jnandez0586 <jnandez0586@gmail.com>
2018-04-30 21:57:54 -0500

    index.js is set up

---
+++ lambda-notes/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+  <Provider store = {store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
