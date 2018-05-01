5c32c8e445d0d449220110cd9e2ee8d5575d4664
zackhitch <zackhitch@users.noreply.github.com>
2018-05-01 12:23:16 -0700

    modified login - ZH

--- lambda_notes/src/Components/CreateNewNote/CreateNewNote.js
+++ lambda_notes/src/Components/CreateNewNote/CreateNewNote.js
@@ -1,23 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './CreateNewNote.css';
-
 import { Form, Input } from 'reactstrap';
+import axios from 'axios';
 
-const CreateNewNote = () => {
-  return (
-    <div className="newNoteMainDiv">
-      <h6 className="newNoteHeader">Create New Note:</h6>
-      <Form className="newNoteFormDiv">
-        <Input type="text" placeholder="Note Title" className="titleDiv" />
-        <Input
-          type="textarea"
-          placeholder="Note Content"
-          className="contentDiv"
-        />
-        <button className="saveNewNoteBtn">Save</button>
-      </Form>
-    </div>
-  );
-};
+class CreateNewNote extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      content: '',
+    };
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  createNote = event => {
+    event.preventDefault();
+    const title = this.state.title;
+    const content = this.state.content;
+    axios
+      .post('https://peaceful-gorge-48893.herokuapp.com/api/notes', {
+        title,
+        content,
+      })
+      .then()
+      .catch(err => {
+        console.log({ Error: `Unable to create note: ${err}` });
+      });
+  };
+
+  render() {
+    return (
+      <div className="newNoteMainDiv">
+        <h6 className="newNoteHeader">Create New Note:</h6>
+        <Form className="newNoteFormDiv" onSubmit={this.createNote}>
+          <Input
+            name="title"
+            type="text"
+            placeholder="Note Title"
+            className="titleDiv"
+            onChange={this.handleChange}
+          />
+          <Input
+            name="content"
+            type="textarea"
+            placeholder="Note Content"
+            className="contentDiv"
+            onChange={this.handleChange}
+          />
+          <button className="saveNewNoteBtn">Save</button>
+        </Form>
+      </div>
+    );
+  }
+}
 
 export default CreateNewNote;
