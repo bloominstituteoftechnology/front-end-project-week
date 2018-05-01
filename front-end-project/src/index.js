ea3d956bdd53a56b150c84e2588ca1ce4de00bd0
csstone12 <36163530+csstone12@users.noreply.github.com>
2018-05-01 12:21:44 -0400

    Stone Chris | Front End Project

--- front-end-project/src/index.js
+++ front-end-project/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
 import { BrowserRouter as Router, Route, NavLink, HashRouter } from 'react-router-dom'
 
 ReactDOM.render(
@@ -12,5 +12,3 @@ ReactDOM.render(
     
     document.getElementById('root')
 );
-
-registerServiceWorker();
