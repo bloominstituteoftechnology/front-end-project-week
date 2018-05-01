e9d0d455888ab3a29aeeb0ea883bc5b018bfc554
Stephanie Brooks <sbrooks84591@hotmail.com>
2018-04-30 23:04:19 -0500

    created component folders

--- lambda_notes/src/App.js
+++ lambda_notes/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
