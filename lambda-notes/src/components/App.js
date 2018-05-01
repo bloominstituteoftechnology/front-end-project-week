db3de89582a2efbf788a2908eec9f106ebbb1669
Jesse Reichel <31119775+KryoKorpz@users.noreply.github.com>
2018-04-30 20:59:51 -0700

    more boiler, file management, final commit

---
+++ lambda-notes/src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { 
+  BrowserRouter as Router, Route 
+} from 'react-router-dom';
+
+import '../css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ lambda-notes/src/reducers/index.js
@@ -0,0 +1,5 @@
+const rootReducer = () => {
+    return
+}
+
+export default rootReducer;
