f7bc185978adce38fdd42559842f84ccaac56807
Nick Coultas <nncoultas@yahoo.com>
2018-05-02 13:56:34 -0700

    changed root URL

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 axios.defaults.withCredentials = true;
-const ROOT_URL = 'http://localhost:5000';
+const ROOT_URL =
+  'mongodb://test:test@ds163769.mlab.com:63769/lambda-notes-project';
 
 export const NOTE_ERROR = 'NOTE_ERROR';
 export const UPDATE_SELECTED = 'UPDATE_SELECTED';
