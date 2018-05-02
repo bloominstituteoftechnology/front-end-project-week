c096a6d2b01351a01963b917088fa7d56fb39783
Yasin Shuman <email@yasin.io>
2018-05-01 21:27:30 -0700

    added notes and have them displaying but not very pretty. will put them in cards and style next

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
 import SideBar from "./components/SideBar/SideBar";
+import ListView from "./components/ListView/ListView";
 import "./App.css";
+import Route from "react-router-dom/Route";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [
+        { id: 2, title: "Note Title", text: "lorem ipsum", completed: false },
+        { id: 1, title: "Note Title", text: "lorem ipsum", completed: false },
+        { id: 3, title: "Note Title", text: "lorem ipsum", completed: false },
+        { id: 4, title: "Note Title", text: "lorem ipsum", completed: false },
+        { id: 5, title: "Note Title", text: "lorem ipsum", completed: false },
+        { id: 6, title: "Note Title", text: "lorem ipsum", completed: false },
+        { id: 7, title: "Note Title", text: "lorem ipsum", completed: false },
+        { id: 8, title: "Note Title", text: "lorem ipsum", completed: false },
+        { id: 9, title: "Note Title", text: "lorem ipsum", completed: false }
+      ]
+    };
+  }
+
   render = () => {
     return (
       <div className="App">
         <SideBar />
+        <Route
+          exact
+          path="/"
+          render={props => <ListView notes={this.state.notes} />}
+        />
       </div>
     );
   };
