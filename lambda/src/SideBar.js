793982b031d71082c21d8f7654d17f70434164d8
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-05-01 19:19:39 -0600

    still wip

--- lambda/src/SideBar.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-
-export default class SideBar extends Component {
-    shouldComponentUpdate = (nextProps, nextState) => false;
-
-}
-
--- lambda/src/reducersIndex.js
+++ lambda/src/reducersIndex.js
@@ -9,6 +9,12 @@ export default (notes=initialState, action) => {
     switch(action.type) {
         case ADD_NOTE:
         const addedState = update(notes, {$push: [action.note]});
-        return addedState
+        return addedState;
+        case MARK_COMPLETE:
+        const completed = notes[action.index].completed;
+        const completedState = update(notes, {[action.index]: {completed: {$set: !completed}}});
+        return completedState;
+        default:
+        
     }
 }
