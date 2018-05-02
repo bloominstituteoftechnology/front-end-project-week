6b1320803ce34387165043d896ccbba162094760
Charlie <ccariste@gmail.com>
2018-05-01 21:01:51 -0700

    CRC: Added buttons to toolbar.  Routing next

--- notetaker/src/Components/Toolbar.js
+++ notetaker/src/Components/Toolbar.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import "../App.css";
-import { Col, Button } from 'reactstrap';
+import { Col, Row, Button } from 'reactstrap';
+import Route from 'react-router-dom';
 
 const Toolbar = props => {
   return(
     <div className='Toolbar-container'>
       <h1>Lambda NoteTaker</h1>
+      <Row>
+        <Button>
+          View Your Notes
+        </Button>
+      </Row>
+      <Row>
+        <Button>
+          +Add New Note
+        </Button>
+      </Row>
+
     </div>
     )
 }
