db722ac24340c6d5ed90740c085325e1ae543dc4
Kaitlyn Schorr <kaitlynschorr@gmail.com>
2018-05-02 16:49:14 -0500

    Implemented redux persist

--- src/index.js
+++ src/index.js
@@ -2,19 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { createStore, applyMiddleware } from "redux";
 import reducer from "./reducers";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
 
-let store = createStore(reducer, applyMiddleware(thunk));
+const persistConfig = {
+  key: "root",
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+let store = createStore(persistedReducer, applyMiddleware(thunk));
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
-registerServiceWorker();
