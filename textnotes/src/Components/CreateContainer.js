1ecd3ce5dceda91b5b604fa7df1bf6c48e1ca840
unknown <alexisreyes90@gmail.com>
2018-05-02 21:48:05 -0400

    create new note page routing done, and css styling done

--- textnotes/src/Components/CreateContainer.js
+++ textnotes/src/Components/CreateContainer.js
@@ -5,8 +5,21 @@ import '../App.css';
 class CreateContainer extends Component {
     render() {
         return(
-            <div className="container">
-            <h1> new page </h1>;
+            <div className="createPage">
+                <h3 className="create-logo"> Create New Note: </h3>
+                <input className="title-text-box"
+                    type="text"
+                    name="name"
+                    placeholder="Note Title"
+                    />
+                    
+                <textarea className="content-box"
+                    type="text"
+                    name="name"
+                    placeholder="Note Content"
+                    />
+
+                <button className="save-button">Save</button>
             </div>
         );
     }
