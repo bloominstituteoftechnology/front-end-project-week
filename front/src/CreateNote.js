ef39c6268cd098f007594613ffd6f1898cfb353b
unknown <bonifacemtk15@yahoo.com>
2018-05-03 19:59:03 -0400

    updated

--- front/src/CreateNote.js
+++ front/src/CreateNote.js
@@ -1,72 +1,85 @@
-import React,{Component} from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 class CreateNote extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-       title:'',
-       body:''
-      };
-      this.handleChangeTitle=this.handleChangeTitle.bind(this);
-    this.handleChange=this.handleChange.bind(this);
-      this.handleSubmit=this.handleSubmit.bind(this);
-    }
-  
-    handleChangeTitle = (event) => {
-      this.setState({title: event.target.value});
-    }
-    handleChange = (event) => {
-        this.setState({body: event.target.value});
-      }
-     handleSubmit = (event) =>{
-event.preventDefault();
-const addNote ={
-    title: event.target.value,
-    body: event.target.value
-}
-axios.post('https://backend-project-week.herokuapp.com/api/note', {
-  title:this.state.title,
-  body:this.state.body,
-})
-.then(response => {
-  console.log(response, ' note saved' )
-})
-.catch(err => {
-  console.log(err, 'sorry, note not saved, try again ');
-});
-          this.state.notes.push(addNote);
-          this.setState({
-            title:'',
-            body:'' 
-          })
-      }
- 
-    
-    
-    render(){
-        
-      return (
-          <div className='container'>
-        <div className = 'button3'>
-        <h1>Lambda Notes</h1>
-       <button> <Link to='Data'> View Your Notes</Link></button><br/>
-       <button> <Link to='CreateNote'> + Create Your Notes</Link></button>
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      body: ""
+    };
+    this.handleChangeTitle = this.handleChangeTitle.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeTitle = event => {
+    this.setState({ title: event.target.value });
+  };
+  handleChange = event => {
+    this.setState({ body: event.target.value });
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+    const addNote = {
+      title: this.state.title,
+      body: this.state.body
+    };
+    axios
+      .post("https://backend-project-week.herokuapp.com/api/note", {
+        title: this.state.title,
+        body: this.state.body
+      })
+      .then(response => {
+        console.log(response, " note saved");
+      })
+      .catch(err => {
+        console.log(err, "sorry, note not saved, try again ");
+      });
+    this.setState({
+     notes:[...this.state.notes,{title,body}],
+      title: "",
+      body: ""
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="button3">
+          <h1>Lambda Notes</h1>
+          <button>
+            {" "}
+            <Link to="Data"> View Your Notes</Link>
+          </button>
+          <br />
+          <button>
+            {" "}
+            <Link to="CreateNote"> + Create Your Notes</Link>
+          </button>
         </div>
 
-        
-          <form>
+        <form>
           <h2>Create New Notes:</h2>
-            <input class = 'kin' type ='text' placeholder ='Enter your title' 
-            value = {this.state.title} onChange ={this.handleChangeTitle} /><br />
-              <textarea value={this.state.body} placeholder = 'Enter your notes'
-              onChange={this.handleChange} cols={60} rows={30} />
-            <button onClick={this.handleSubmit}>Save </button>
-          </form>
-          
-          </div>
-      );
-    }
+          <input
+            class="kin"
+            type="text"
+            placeholder="Enter your title"
+            value={this.state.title}
+            onChange={this.handleChangeTitle}
+          />
+          <br />
+          <textarea
+            value={this.state.body}
+            placeholder="Enter your notes"
+            onChange={this.handleChange}
+            cols={60}
+            rows={30}
+          />
+          <button onClick={this.handleSubmit}>Save </button>
+        </form>
+      </div>
+    );
   }
-  export default CreateNote;
-  
-  
+}
+export default CreateNote;
--- front/src/Login.js
+++ front/src/Login.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 class Login extends Component {
     constructor(props) {
       super(props);
