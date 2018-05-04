c1f3847a8ca0396922f2affa90be06c010e9ea0d
Sean Valdivia <irishseanvaldivia@gmail.com>
2018-05-04 13:46:09 -0500

    Debating on whether or not to delete notesButtons.js

--- notes/src/notesButtons.js
+++ notes/src/notesButtons.js
@@ -5,6 +5,8 @@ class Buttons extends Component {
     constructor(props) {
         super(props)
     }
+    // do I really need this component? Can't I just do it all in Notes.js??
+
     // the concept is to have a menu that is always at the top of my view
     // it will consist of four buttons: Create New, Delete Existing, View Existing, and Edit Existing
     // I don't think I have to call props or I'm not really sure, but I know that this component
