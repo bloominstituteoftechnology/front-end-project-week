a47d197a2a294d17c4c8a50ab3372ea68b1516f2
Sean Valdivia <irishseanvaldivia@gmail.com>
2018-05-04 11:22:37 -0500

    Working on notesButtons.js

--- notes/notesButtons.js
+++ notes/notesButtons.js
@@ -1,1 +1,14 @@
 import React from 'react';
+import { Component } from 'react';
+
+class Buttons extends Component {
+    constructor(props) {
+        super(props)
+    }
+    // the concept is to have a menu that is always at the top of my view
+    // it will consist of four buttons: Create New, Delete Existing, View Existing, and Edit Existing
+    // I don't think I have to call props or I'm not really sure, but I know that this component
+    // needs to be a class. This class should also handle onclick actions
+
+    
+}
