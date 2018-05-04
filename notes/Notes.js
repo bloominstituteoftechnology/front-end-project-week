78bcf88d673375f32098b0ad3b56fcd57499f1cb
Sean Valdivia <irishseanvaldivia@gmail.com>
2018-05-04 09:43:52 -0500

    Starting on Notes App.

---
+++ notes/Notes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from 'react';
+
+
+class Note extends Component {
+    constructor(props) {
+        super(props) 
+        this.state = {
+            
+        }
+    }
+}
