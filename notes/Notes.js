afa13d8d484963a808c182b7c790fe724da87659
Sean Valdivia <irishseanvaldivia@gmail.com>
2018-05-04 09:46:33 -0500

    Not sure where to start here...

--- notes/Notes.js
+++ notes/Notes.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Component } from 'react';
+import { connect } from 'react-redux';
 
 
 class Note extends Component {
     constructor(props) {
         super(props) 
         this.state = {
-            
+
         }
     }
 }
