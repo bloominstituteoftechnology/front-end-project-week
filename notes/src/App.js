665595a25d44e1a3016ef36a38c1116aad81ad8b
Sean Valdivia <irishseanvaldivia@gmail.com>
2018-05-04 11:46:03 -0500

    Still trying to swim.

--- notes/src/App.js
+++ notes/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
   render() {
     return (
@@ -13,8 +14,4 @@ class App extends Component {
 }
 // should I render my main default view in here?
 // where to really start...
-// this stuff is hard
-// please God give me the strength to not give up
-// I've never felt more stupid in my entire life.
-// It's like trying to think through caramel
 export default App;
