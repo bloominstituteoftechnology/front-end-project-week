35dcbddcf52f7f41dbb1f1c39ed7f99c29002585
Cesar Mejia Leiva <cmejia@gmail.com>
2018-05-06 16:45:27 -0700

    search bar done

--- src/components/ListNotes/ListNotes.js
+++ src/components/ListNotes/ListNotes.js
@@ -5,7 +5,7 @@ import { Modal, ModalBody, ModalFooter, Button } from 'reactstrap'
 import './ListNotes.css'
 
 class ListNotes extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       loggedIn: false,
@@ -13,7 +13,7 @@ class ListNotes extends Component {
     }
   }
 
-  componentWillMount () {
+  componentWillMount() {
     if (localStorage.getItem('authorization')) this.setState({ loggedIn: true })
   }
 
@@ -23,79 +23,83 @@ class ListNotes extends Component {
     })
   }
 
-  onSearch = (event) => {
+  onSearch = event => {
     this.setState({
       search: event.target.value
     })
   }
 
-  submitSearch = (event) => {
-    event.preventDefault()
-  }
-  render (props) {
-    let filteredNotes = this.props.notes
+  render(props) {
+    const { search } = this.state
+    const filteredNotes = this.props.notes.filter(note => {
+      return (
+        note.title.toLowerCase().indexOf(search.toLowerCase()) !== -1 ||
+        note.content.toLowerCase().indexOf(search.toLowerCase()) !== -1
+      )
+    })
 
     return (
-      <div className='ListNotes' >
-        <input
-          type='text'
-          placeholder='search'
-          onChange={this.onSearch}
-          onSubmit={this.submitSearch}
-        />
-        {
-          localStorage.getItem('authorization')
-            ? <section className='RegLog'>
-              <h6 className='loginName'>
+      <div className="ListNotes">
+        <form type="submit" className="submit-form">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="search"
+            value={this.state.search}
+            onChange={this.onSearch}
+          />
+        </form>
+        {localStorage.getItem('authorization')
+          ? <section className="RegLog">
+              <h6 className="loginName">
                 Logged in as: {this.props.username}
               </h6>
             </section>
-            : <div />
-        }
+          : <div />}
 
         <Modal
-          className='modal-modal'
+          className="modal-modal"
           isOpen={!this.state.loggedIn}
           toggle={this.toggle}
-          backdrop='static'
+          backdrop="static"
         >
-          <ModalBody className='modal-body'>
+          <ModalBody className="modal-body">
             Please Register/Login to access Lambda Notes.
           </ModalBody>
           <ModalFooter>
             <Button
-              color='success'
-              className='register-button'
+              color="success"
+              className="register-button"
               onClick={() => this.props.history.push('/register')}
             >
               Register
             </Button>
             <Button
-              color='primary'
+              color="primary"
               onClick={() => this.props.history.push('/login')}
-              className='login-button'
+              className="login-button"
             >
               Login
             </Button>
           </ModalFooter>
         </Modal>
 
-        <h2 className='list_h2'>Your Notes:</h2>
-        <div className='notes'>
+        <h2 className="list_h2">Your Notes:</h2>
+        <div className="notes">
           {filteredNotes.map(note =>
-            <div className='card-body' key={note._id}>
-              <h4 className='card-title'>
-                <Link className='card-link' to={`/view/${note._id}`}>
+            <div className="card-body" key={note._id}>
+              <h4 className="card-title">
+                <Link className="card-link" to={`/view/${note._id}`}>
                   {note.title}
                 </Link>
               </h4>
-              <span className='card-text'>
+              <span className="card-text">
                 {note.content}
               </span>
             </div>
           )}
         </div>
-      </div >
+      </div>
     )
   }
 }
