d1a1c91e26a022ea68654a9c71b915e1614d555e
sophiemullerc <32464200+sophiemullerc@users.noreply.github.com>
2018-05-06 21:13:18 -0700

    Added the Register Page

--- lambda_notes/src/Components/RegisterUser.js
+++ lambda_notes/src/Components/RegisterUser.js
@@ -3,32 +3,82 @@ import axios from 'axios';
 import './RegisterUser.css';
 import { Form, Input } from 'reactstrap';
 
-const CreateNewRegister = () => {
-    return (
-        <div className="newRegisterMainDiv">
-        <h6 className="newRegisterHeader">Register:</h6>
-        <Form className="newRegisterFormDiv">
-          <Input type="text" placeholder="Username" className="titleDiv" />
-          <Input
-            type="password"
-            placeholder="Password"
-            className="passwordDiv"
-          />
-          <button className="saveNewRegisterBtn">Register</button>
-        </Form>
-      </div>
-    );
- };
+class RegisterPage extends React.Component {
+  constructor(props) {
+      super(props);
 
-  axios.post('/register', {
-    username: 'sophiemullerc',
-    password: 'hola123'
-  })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-  });
- 
- export default CreateNewRegister;
+      this.state = {
+          user: {
+              username: '',
+              password: ''
+          }
+      };
+
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    const { user } = this.state;
+    this.setState({
+        user: {
+            ...user,
+            [name]: value
+        }
+    });
+  }
+
+  handleSubmit(event) {
+      event.preventDefault();
+
+      const { user } = this.state;
+
+      console.log(this.state);
+
+      if (!user.username || !user.password) {
+        console.log("UserName and/or Password are empty.")
+        return;
+      }
+
+      axios.post(
+        'https://sophiemullerc-notes.herokuapp.com/register', 
+        {
+          username: user.username,
+          password: user.password
+        }
+      )
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error.response.data);
+      });
+  }
+
+  render() {
+    return (
+      <div className="newRegisterMainDiv">
+      <h6 className="newRegisterHeader">Register:</h6>
+      <Form className="newRegisterFormDiv" onSubmit={this.handleSubmit}>
+        <Input 
+          type="text" 
+          placeholder="Username" 
+          className="titleDiv" 
+          name="username" 
+          onChange={this.handleChange} />
+        <Input
+          type="password"
+          placeholder="Password"
+          className="passwordDiv"
+          name="password"
+          onChange={this.handleChange}
+        />
+        <button className="saveNewRegisterBtn">Register</button>
+      </Form>
+    </div>
+  );
+  }
+}
+
+ export default RegisterPage;
