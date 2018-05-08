4088b1b2ec5ce04a3eeb9cf602f37ee3916acf13
Alexander <alexander.botello@g.austincc.edu>
2018-05-07 22:10:13 -0500

    Refactor Create to be class component

--- notes/src/Components/Create.js
+++ notes/src/Components/Create.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 
 import Form from './Form';
 
-export default () => {
-  let props = {
-    button: 'Add Note',
-    action: addNewNote,
+export default class Create extends Component {
+  state = {
+    redirect: false
+  } 
+  addNewNote = note => {
+    axios.post("http://localhost:5005/notes", note)
+      .then(() => this.setState({ redirect: true }))
+      .catch(error => console.log(error));
   }
-  return(
-    <div className="flex-container">
-      <div className="title">
-        <h2>Create New Note:</h2>
+  render() {
+    let items = {
+      button: 'Add Note',
+      action: this.addNewNote,
+    }
+    if (this.state.redirect) {
+      return <Redirect to="/" />
+    }
+    return(
+      <div className="flex-container">
+        <div className="title">
+          <h2>Create New Note:</h2>
+        </div>
+        <Form {...items}/>
       </div>
-      <Form {...props}/>
-    </div>
-  );
-}
-const addNewNote = note => {
-  axios.post("http://localhost:5005/notes", note)
-    .then(() => window.location = "/")
-    .catch(error => console.log(error));
+    );
+  }
 }
 
