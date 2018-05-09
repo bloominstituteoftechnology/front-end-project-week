61dd9938dd1fbc3a53691169833e1352a5d93e4f
Sean Valdivia <irishseanvaldivia@gmail.com>
2018-05-09 18:18:51 -0500

    Getting closer...

--- notes/src/CreateNote.js
+++ notes/src/CreateNote.js
@@ -4,9 +4,9 @@ import React from 'react';
 export const CreateNote = (props) => {
   return (
       <form onSubmit={props.handleSubmit}>
-        <input value={props.title} type="text" placeholder="title" name="title" onChange={props.handleChange} />
-        <textarea value={props.content} type="text" name="content" placeholder="Put note here!" onChange={props.handleChange}/>
-        <button>BatmanSubmit</button>
+        <input value={props.title} type="text" placeholder="Note Title" name="title" onChange={props.handleChange} />
+        <textarea value={props.content} type="text" name="content" placeholder="Note Content" onChange={props.handleChange}/>
+        <button>Save</button>
       </form>
   )
 }
