4dccd124a23086e79f8b8b547d187126491b6c02
Stephanie <sbowl001@fiu.edu>
2018-05-09 23:10:56 +0800

    still trying to figure out editview

--- notes/src/Components/EditView.js
+++ notes/src/Components/EditView.js
@@ -3,37 +3,68 @@ import axios from "axios";
 import {Link} from "react-router-dom";
 
 export default class AddFriend extends Component {
+constructor(props) {
+    super(props);
+    const { note} = props;
+
+this.state = {
+    title: note.title,
+    content: note.content,
+    id: note.id
+}
+}
+
+handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+handleSubmit = e => {
+     const { title, content, id } = this.state;
+     e.preventDefault();
+    this.updateNote({ title, content, id });
+    this.setState({ title: "", content: "" });
+}
+    updateNote = note => {
+  return dispatch => {
+    axios.put(`http://localhost:5000/notes/${note.id}`, note)
+      .then(response => dispatch({  notes: response.data }))
+      .catch(err => dispatch({
+         error: "ERROR UPDATING FRIEND"
+      }))
+    }
+}
  
-state = {
-    title: "",
-    content: "",
+// state = {
+//     title: "",
+//     content: "",
 
-};
+// };
 
-  componentDidMount() {
-    const id = this.props.match.params.id;
-    axios
-      .get(`http://localhost:5000/notes/${id}`)
-      .then(response => {
-        this.setState(() => ({ notes: response.data }));
-      })
-      .catch(error => {
-        console.error("error", error);
-      });
-  }
+//   componentDidMount() {
+//     const id = this.props.match.params.id;
+//     console.log("id", this.props.match.params.id);
+//     axios
+//       .get(`http://localhost:5000/notes/${id}`)
+//       .then(response => {
+//         this.setState(() => ({ notes: response.data }));
+//       })
+//       .catch(error => {
+//         console.error("error", error);
+//       });
+//   }
 
-  handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-  handleSubmit = event => {
-    event.preventDefault();
+//   handleChange = event => {
+//     this.setState({ [event.target.name]: event.target.value });
+//   };
+//   handleSubmit = event => {
+//     event.preventDefault();
 
-    axios
-      .post("http://localhost:5000/notes", this.state)
-      .then(response => console.log(response))
-      .then(() => (window.location.href = "/"))
-      .catch(error => console.log(error));
-  };
+//     axios
+//       .post("http://localhost:5000/notes", this.state)
+//       .then(response => console.log(response))
+//       .then(() => (window.location.href = "/"))
+//       .catch(error => console.log(error));
+//   };
 
   render() {
     return <div>
@@ -51,3 +82,4 @@ state = {
          </div>;
   }
 }
+    
