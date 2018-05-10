36725efa3a920840daaeb27b9a64dc30576911a5
Andrew Jarrett <ahrjarrett@gmail.com>
2018-05-10 15:45:47 -0600

    HOC not passing mapState & mapDispatch through?

--- client/src/components/Speak.js
+++ client/src/components/Speak.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
+import { compose } from 'redux'
 import { connect } from 'react-redux'
-import FA from 'react-fontawesome'
+
 import { startListening, stopListening, addToRegister, clearRegister } from '../actions/speech'
 import Mic from './Mic'
 
@@ -8,48 +9,58 @@ const head = arr => arr[0]
 
 // const defaultOptions = {
 //   interim: true,
-
 // }
 
+const withSpeech = Comp => {
+  return class WithSpeech extends Component {
+
+    componentDidMount = () => {
+      window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+      this.recognition = new window.SpeechRecognition()
+      this.recognition.interimResults = true
+    }
+
+    componentWillUnmount = () => {
+      this.recognition.removeEventListener('result', this.accumulateTranscript)
+    }
+
+    startListening = () => {
+      this.props.startListening()
+      this.recognition.addEventListener('result', this.accumulateTranscript)
+      this.recognition.start()
+    }
+
+    stopListening = () => {
+      this.props.stopListening()
+      this.recognition.stop()
+    }
+
+    accumulateTranscript = e => {
+      const transcript = Array.from(e.results)
+        .map(head)
+        .map(({ transcript }) => transcript)
+        .join('')
+
+      if (e.results[0].isFinal)
+        this.props.addToRegister(transcript)
+    }
+
+    render() {
+      return <Comp {...this.props} />
+    }
+  }
+}
+
 class Speak extends Component {
-  componentDidMount = () => {
-    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
-    this.recognition = new window.SpeechRecognition()
-    this.recognition.interimResults = true
-  }
-
-  componentWillUnmount = () => {
-    this.recognition.removeEventListener('result', this.accumulateTranscript)
-  }
-
-  startListening = () => {
-    this.props.startListening()
-    this.recognition.addEventListener('result', this.accumulateTranscript)
-    this.recognition.start()
-  }
-
-  stopListening = () => {
-    this.props.stopListening()
-    this.recognition.stop()
-  }
-
-  accumulateTranscript = e => {
-    const transcript = Array.from(e.results)
-      .map(head)
-      .map(({ transcript }) => transcript)
-      .join('')
-
-    if (e.results[0].isFinal)
-      this.props.addToRegister(transcript)
-  }
-
   render() {
     const { onSave } = this.props
     return (
       <div className="Speak">
         <Mic
-          startListening={this.startListening}
-          stopListening={this.stopListening}
+          startListening={this.props.startListening}
+          stopListening={this.props.stopListening}
+        // startListening={this.startListening}
+        // stopListening={this.stopListening}
         />
         <button onClick={() => onSave(this.props.register)}>Looks good?</button>
         <div className="transcript">
@@ -71,7 +82,7 @@ const mapDispatchToProps = {
   clearRegister,
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  withSpeech,
 )(Speak)
