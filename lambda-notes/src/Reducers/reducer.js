abb83bd8d38bbecf0db0802ecb42a0b3c9152b9e
Lisa Cee <three_nineteen@yahoo.com>
2018-05-10 10:34:53 -0700

    edited notes returned sorted by id

--- lambda-notes/src/Reducers/reducer.js
+++ lambda-notes/src/Reducers/reducer.js
@@ -4,7 +4,28 @@ let initialState =
 [{ title: 'Title', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a mattis tortor, at aliquet sem. Nulla elementum erat vel orci ullamcorper luctus. Duis porta justo posuere dui dignissim posuere. Vestibulum a lectus ornare velit posuere dictum. Nullam suscipit tempor neque, at accumsan urna tincidunt ut. In in tincidunt mauris. Suspendisse ac urna vitae massa lacinia convallis. Integer elit purus, ultrices a lorem vel, ullamcorper vulputate lectus. Vivamus tempus lobortis diam vel fermentum. Suspendisse sed ex at ex sodales placerat. In diam odio, sagittis sit amet gravida sit amet, interdum at urna.', id: 0 },
 { title: 'Note 2', content: 'Vestibulum accumsan dui quis odio ultricies pharetra. Quisque vitae mauris ut arcu vehicula sagittis sit amet sed mi. Nulla ac erat eu est viverra semper nec et urna. Cras feugiat venenatis sem, a gravida metus fringilla a. Fusce vel faucibus ipsum. Morbi ipsum felis, rutrum eu ultricies at, lobortis ut justo. Pellentesque vestibulum aliquet mi a hendrerit. Praesent rutrum ac turpis sed lacinia. Nunc metus arcu, imperdiet varius suscipit vel, sodales at sem. Integer mollis sapien ut tellus ultrices euismod. Nulla quis elementum lectus.', id: 1 }]
 
-const reducer = (notes = initialState, action) => {
+function compareValues(key, order = 'asc') {
+    return function (a, b) {
+        if (!a.hasOwnProperty(key) ||
+            !b.hasOwnProperty(key)) {
+            return 0;
+        }
+
+
+        let comparison = 0;
+        if (a[key] > b[key]) {
+            comparison = 1;
+        } else if (a[key] < b[key]) {
+            comparison = -1;
+        }
+        return (
+            (order === 'desc') ?
+                (comparison * -1) : comparison
+        );
+    };
+}
+
+const reducer = (notes = [], action) => {
     switch (action.type) {
         case ADD_NOTE:
             return notes.concat(action.payload);
@@ -13,7 +34,8 @@ const reducer = (notes = initialState, action) => {
         case EDIT_NOTE:
         console.log(action.id.id)
             notes.splice(action.id.id, 1);
-            return notes.concat(action.id);
+           
+           return (notes.concat(action.id)).sort(compareValues('id', 'asc'));
         default:
             return notes
     }
