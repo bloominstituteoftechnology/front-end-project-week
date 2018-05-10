fd92ce3e228343ffb5618e8897d4a0cee14f94fe
Sean Valdivia <irishseanvaldivia@gmail.com>
2018-05-09 19:05:24 -0500

    Gonna start on funcionality now.

--- notes/src/CreateNote.js
+++ notes/src/CreateNote.js
@@ -4,11 +4,14 @@ import './Form.css';
 
 export const CreateNote = (props) => {
   return (
+    <div className="NoteSpace">
+      <div>Create New Note</div>
       <form onSubmit={props.handleSubmit}>
         <input value={props.title} type="text" placeholder="Note Title" name="title" onChange={props.handleChange} />
-        <textarea value={props.content} type="text" name="content" placeholder="Note Content" onChange={props.handleChange}/>
+        <textarea value={props.content} type="text" name="content" placeholder="Note Content" onChange={props.handleChange} />
         <button>Save</button>
       </form>
+    </div>
   )
 }
 
