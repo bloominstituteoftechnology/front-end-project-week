818726da06e077074ce4388b2352f866a319e1da
Cesar Mejia Leiva <cmejia@gmail.com>
2018-05-11 21:41:25 -0700

    should fix text

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -48,7 +48,6 @@ class NavBar extends Component {
         >
           Logout
         </Link>
-        [![js-standard-style](https://cdn.rawgit.com/standard/standard/master/badge.svg)](http://standardjs.com)
       </div>
     )
   }
