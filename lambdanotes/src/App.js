7e06837cda02084373eaeab7bc0cfb76c2e906fd
Zach Campbell <beazlebubbles@gmail.com>
2018-05-21 10:53:25 -0600

    Imported bootstrap, Installed reactstrap properly, removed unintended package

--- lambdanotes/src/App.js
+++ lambdanotes/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Container>
+          <Row>
+            <Col className="left-side" xs="3">LEFT SIDE</Col>
+            <Col className="content" xs="9">CONTENT</Col>
+          </Row>
+        </Container>
       </div>
     );
   }
