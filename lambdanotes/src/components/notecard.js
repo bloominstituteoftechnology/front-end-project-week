c33f40db47b12b3b1ecb9dcd7334ea4d3c1748e3
Zach Campbell <beazlebubbles@gmail.com>
2018-05-21 11:57:20 -0600

    Created NoteCard component

---
+++ lambdanotes/src/components/notecard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NoteCard = (props) => {
+  return (
+    <div>THIS IS A NOTE CARD</div>
+  );
+}
+
+export default NoteCard;
--- lambdanotes/src/components/notelist.js
+++ lambdanotes/src/components/notelist.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import './notelist.css';
+import NoteCard from './notecard.js';
 
 const NoteList = (props) => {
   return (
-    <div>I AM A NOTE LIST</div>
+    <div className="notes-view">
+      <div className="heading">
+        <h5>Your Notes:</h5>
+      </div>
+      {
+        props.notes.map((note, index) => {
+          return (
+            <NoteCard key={index} note={note} />
+          );
+        })
+      }
+    </div>
   );
 }
 
