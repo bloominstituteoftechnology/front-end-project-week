6e1ba6dba3aa9e6d426e1e97011d3f058a984e6e
april <martinezapril56@yahoo.com>
2018-05-21 16:15:52 -0400

    made my components

--- notes/src/index.js
+++ notes/src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Create from './components/Create';
+import Delete from './component/Delete';
+import Edit from './component/Edit';
+import List from './component/list';
+import Note from './component/Note';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
