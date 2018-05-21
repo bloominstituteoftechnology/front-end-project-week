2abdb87edbe71e3ee02641a2cadac623e705e5ac
Brandon Hopper <BrandonHopper0@gmail.com>
2018-05-21 19:10:58 -0400

    -passed through props of Component and rendered cards with reactstrap

--- lambda/src/LambdaPages/ListView.js
+++ lambda/src/LambdaPages/ListView.js
@@ -1,13 +1,50 @@
 import React from 'react';
 import NavBar from '../components/Nav'
 
+import { Card, CardTitle, CardText, Row, Col } from 'reactstrap';
+
 //Stateless function that renders Navbar from components file
-const ListView = () => {
+const ListView = (props) => {
     return(
         <div>
             <NavBar />
+        <Row>
+            <Col sm="3">
+                <Card body>
+                    <CardTitle>NoteTitle</CardTitle>
+                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                </Card>
+            </Col>
+        </Row>  
+        <Row>
+            <Col sm="3">
+                <Card body>
+                    <CardTitle>NoteTitle</CardTitle>
+                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                </Card>
+            </Col>
+        </Row>
+        <Row>
+            <Col sm="3">
+                <Card body>
+                    <CardTitle>NoteTitle</CardTitle>
+                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                 </Card>
+            </Col>
+        </Row>  
         </div>
-    );
+        // <div>
+        // <Row>
+        //     <Col sm="3">
+        //         <Card body>
+        //             <CardTitle>NoteTitle</CardTitle>
+        //             <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+        //         </Card>
+        //     </Col>
+        // </Row>
+        // </div>
+               
+    )
 };
 
 export default ListView;
