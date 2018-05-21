52ee05bcde75ac0f2b97624eab8a66918b59a57e
Matthias Bakken <matthias.bakken@gmail.com>
2018-05-22 03:02:50 +0700

    imported dependencies to index.js and then set up render

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider>
+        <Router>
+            <App />
+        </Router>
+    </Provider>, 
+
+document.getElementById('root'));
 registerServiceWorker();
