1359be4cb58469f459cbfa2da3fdaa0900616245
Matthias Bakken <matthias.bakken@gmail.com>
2018-05-22 03:09:26 +0700

    added dependencies to app.js and removed react logo.svg

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        Hello
       </div>
     );
   }
