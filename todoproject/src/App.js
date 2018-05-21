21e1fa42e5f6cb9cc51cddfa3b83263cc8c9144a
Anne Courtney <annecourtney_1@yahoo.com>
2018-05-21 14:49:44 -0500

    created cards and layout for menu and homepage

--- todoproject/src/App.js
+++ todoproject/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import Menu from './components/menu';
+import Menu from './menu/menu';
+import Home from './home/home';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <container>
+      <Home />
       <Menu />
+ 
+      </container>
       </div>
     );
   }
---
+++ todoproject/src/home/home.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import './index.css';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button } from 'reactstrap';
+  
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+            <container1>
+            <h2>Your Notes:</h2>
+
+
+            <Card className="card">
+                <CardBody>
+                    <CardTitle className="cardtitle">Note Title</CardTitle>
+                    <CardText className="cardtext">Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                </CardBody>
+            </Card>
+
+
+            <Card className="card">
+                <CardBody>
+                    <CardTitle className="cardtitle">Note Title</CardTitle>
+                    <CardText className="cardtext">Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                </CardBody>
+            </Card>
+
+
+            <Card className="card">
+                <CardBody>
+                    <CardTitle className="cardtitle">Note Title</CardTitle>
+                    <CardText className="cardtext">Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                </CardBody>
+            </Card>
+
+            <Card className="card">
+                <CardBody>
+                    <CardTitle className="cardtitle">Note Title</CardTitle>
+                    <CardText className="cardtext">Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                </CardBody>
+            </Card>
+
+                        <Card className="card">
+                <CardBody>
+                    <CardTitle className="cardtitle">Note Title</CardTitle>
+                    <CardText className="cardtext">Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                </CardBody>
+            </Card>
+
+                        <Card className="card">
+                <CardBody>
+                    <CardTitle className="cardtitle">Note Title</CardTitle>
+                    <CardText className="cardtext">Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                </CardBody>
+            </Card>
+
+                        <Card className="card">
+                <CardBody>
+                    <CardTitle className="cardtitle">Note Title</CardTitle>
+                    <CardText className="cardtext">Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                </CardBody>
+            </Card>
+
+                        <Card className="card">
+                <CardBody>
+                    <CardTitle className="cardtitle">Note Title</CardTitle>
+                    <CardText className="cardtext">Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                </CardBody>
+            </Card>
+
+                        <Card className="card">
+                <CardBody>
+                    <CardTitle className="cardtitle">Note Title</CardTitle>
+                    <CardText className="cardtext">Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                </CardBody>
+            </Card>
+
+
+
+
+
+
+            </container1>
+            </div>
+        )
+    }
+};
+
+export default Home;
