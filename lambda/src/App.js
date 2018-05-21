c3918ed8b5979c487babbd252b7e2cb52fd385a0
Brandon Hopper <BrandonHopper0@gmail.com>
2018-05-21 16:24:08 -0400

    -restructured App.js -imported Navbar from components -render Navbar to App

--- lambda/src/App.js
+++ lambda/src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//importing NavBar component
+import NavBar from './components/Nav';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <NavBar />
       </div>
     );
   }
---
+++ lambda/src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+//statless componet for Navbar, which will be exported to App.js to render NavBar
+const NavBar = () => {
+    return(
+        <div>
+            <h1>Lambda Notes</h1>
+        </div>
+    );
+};
+
+export default NavBar;
