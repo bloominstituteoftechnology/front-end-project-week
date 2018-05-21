6adf72172dd443816e12a5be02568ab7696ef816
Anne Courtney <annecourtney_1@yahoo.com>
2018-05-21 12:59:28 -0500

    added static menu bar with buttons and title and css

---
+++ todoproject/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Menu from './components/menu';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Menu />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ todoproject/src/components/menu.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Menu extends Component {
+    render() {
+        return (
+            <div>
+            <h1>Lambda Notes</h1>
+            
+            <button className="button1">View Your Notes</button>
+            <button className="button2">+ Create New Note</button>
+            </div>
+        )
+    }
+};
+
+export default Menu;
