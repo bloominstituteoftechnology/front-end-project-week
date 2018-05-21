82ebddbe5321d11306bb6174d745f0a4f6355bff
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-05-21 14:01:59 -0700

    bootstrap links added

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import CreateNote from './components/CreateNote';
-import EditNote from "./components/EditNote";
-import Note from "./components/Note";
+import { Route, Link } from "react-router-dom";
+// import CreateNote from './components/CreateNote';
+// import EditNote from "./components/EditNote";
+// import Note from "./components/Note";
 import SideBar from "./components/SideBar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <SideBar />
+        <Route exact path='/' component={SideBar}/>
       </div>
     );
   }
