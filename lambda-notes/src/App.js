3df5abdeab8adae711b558cdc10c63b4f31b9d36
sagardesai90 <saagardesai@gmail.com>
2018-05-21 16:51:01 -0500

    getting the components to work

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react';
+import { Route }from "react-router-dom";
+import { Row, Col, Container, Input, Button } from "reactstrap";
 import logo from './logo.svg';
 import './App.css';
+import NoteList from './Components/NoteList';
+
+const placeHolder = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+const initialNotes = [
+  {id: 1,
+  title: "First Note",
+  content: placeHolder + placeHolder},
+  {id: 1,
+  title: "Second Note",
+  content: placeHolder + placeHolder},
+  {id: 1,
+  title: "Third Note",
+  content: placeHolder + placeHolder + placeHolder},
+  {id: 1,
+  title: "Fourth Note",
+  content: placeHolder + placeHolder + placeHolder + placeHolder}
+];
+
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      notes: initialNotes,
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Lambda Notes</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Container>
+          <Col sm="3">
+            <Route path="/" 
+              render={props => {
+                return <NoteList notes={this.state.notes} />;  
+              }}/>
+          </Col>
+        </Container>
       </div>
     );
   }
---
+++ lambda-notes/src/Components/NoteCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Card, CardHeader, CardBody, CardText} from 'reactstrap';
+
+const NoteCard = props => {
+    return (
+        <Card className="note-card">
+            <CardHeader>
+                <Link to={`note/${props.note.id}`}> {props.note.title} </Link>
+            </CardHeader>
+            <CardBody>
+                <CardText>{props.note.content}</CardText>
+            </CardBody>
+        </Card>
+    )
+}
+
+export default NoteCard;
