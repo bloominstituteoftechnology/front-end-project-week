8662e36fe08bdc038bdc29bf2eb0d3c8f0ec525b
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-05-21 13:22:06 -0700

    components creatged

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CreateNote from './components/CreateNote';
+import EditNote from "./components/EditNote";
+import Note from "./components/Note";
+import SideBar from "./components/SideBar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
