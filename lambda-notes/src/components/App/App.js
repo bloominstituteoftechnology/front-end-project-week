e034e31860fbed7f4de335bc43f4bcab34bc2bd6
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-05-21 16:53:15 -0400

    Nav component created

--- lambda-notes/src/components/App/App.js
+++ lambda-notes/src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from '../../img/logo.svg';
 import './App.css';
+import Nav from '../Nav/Nav';
 
 class App extends Component {
   render() {
@@ -10,9 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Nav />
       </div>
     );
   }
---
+++ lambda-notes/src/components/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+    render() { 
+        return (
+            <div>
+                <h1>Lambda Notes</h1>
+                <p><a href="">View Your Notes</a></p>
+                <p><a href="">+ Create New Note</a></p>
+            </div>
+        )
+    }
+}
+ 
+export default Nav;
