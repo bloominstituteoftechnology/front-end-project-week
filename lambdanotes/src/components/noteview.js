3c5d97ca68a3206b84873964c52456bf4400a3d0
Zach Campbell <beazlebubbles@gmail.com>
2018-05-21 16:08:27 -0600

    Filled in and styled individual view

--- lambdanotes/src/components/noteview.js
+++ lambdanotes/src/components/noteview.js
@@ -3,7 +3,13 @@ import './noteview.css';
 
 const NoteView = (props) => {
   return (
-    <div> TEST VIEW </div>
+    <div className="note-view">
+      <div className="note-view-links">
+        <div className="note-view-link">edit</div><div className="note-view-link">delete</div>
+      </div>
+      <h5 className="note-view-title">{props.note.title}</h5>
+      <p className="note-view-content">{props.note.content}</p>
+    </div>
   );
 }
 
