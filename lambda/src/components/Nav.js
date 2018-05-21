a7e09470929f97b3d306f78e19e461ec9035bdcf
Brandon Hopper <BrandonHopper0@gmail.com>
2018-05-21 19:11:53 -0400

    -added buttons to the nav component

--- lambda/src/components/Nav.js
+++ lambda/src/components/Nav.js
@@ -1,10 +1,15 @@
 import React from 'react';
+//importing button from reactstrap
+import { Button } from 'reactstrap';
+
 
 //statless componet for Navbar, which will be exported to App.js to render NavBar
 const NavBar = () => {
     return(
         <div>
             <h1>Lambda Notes</h1>
+            <Button color="secondary">View Your Note</Button>{' '}
+            <Button color="secondary">+ Create New Note</Button>{' '}
         </div>
     );
 };
