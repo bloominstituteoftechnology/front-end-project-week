dd5efcd17c387ddc4049e101f492eb7a7c260255
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-05-22 07:10:58 +0800

    Updated files

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import SideNavigation from './components/main/SideNavigation.js';
 
 class App extends Component {
   render() {
@@ -10,6 +12,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+
+      <SideNavigation />
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ lambda-notes/src/components/main/SideNavigation.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom"; 
+
+class SideNavigation extends Component {
+
+    render(props) {
+        return (
+            <div className="side-navigation">
+                <h1>Lambda Notes</h1>
+                <ul>
+                    <li className="side-nav-item">
+                        <Link to="/" style={{ textDecoration: "none" }}>
+                            <h4>View Notes</h4>
+                        </Link>
+                    </li>
+                    <li className="side-nav-item">
+                        <Link to="/create" style={{ textDecoration: "none" }}>
+                            <h4>+ Create New Note</h4>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+} 
+
+export default SideNavigation;
