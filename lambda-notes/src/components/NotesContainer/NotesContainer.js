2a0d2465b98f5f121c9c2a2bc7fb29e761cfaa44
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-21 11:49:06 -0500

    created NotesContainer, installed redux, thunk, & logger

---
+++ lambda-notes/src/components/NotesContainer/NotesContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class NotesContainer extends Component {
+  
+ render() {
+     return (
+         <div></div>
+     )
+ }
+}
+
+
+export default NotesContainer;
