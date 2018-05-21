9c9cba3896bda037e32c097775172148eb610cff
maribelcuales <maribelcuales@gmail.com>
2018-05-22 06:55:08 +0800

    added card template and change background color

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ListView from './components/ListView.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="App-header">          
+          <h1 className="App-title">Lambda Notes</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="App-intro">
+          Your Notes:
+        </h1>
+        <ListView />
+        
       </div>
     );
   }
---
+++ lambda-notes/src/components/listView.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import listView from './listView.css';
+import { Card, Button, CardImg, CardTitle, CardText, CardColumns,
+  CardSubtitle, CardBody } from 'reactstrap'; 
+
+const ListView = (props) => {
+  return (
+    <CardColumns className="cardColumns">
+      <Card className="card">        
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam finibus feugiat vestibulum. Ut molestie, ex at efficitur aliquet, dui justo venenatis augue, in dignissim lectus justo ut nulla. Sed consectetur eros vitae urna laoreet aliquam. Maecenas tincidunt tellus non egestas venenatis.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+      <Card className="card">        
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam finibus feugiat vestibulum. Ut molestie, ex at efficitur aliquet, dui justo venenatis augue, in dignissim lectus justo ut nulla. Sed consectetur eros vitae urna laoreet aliquam. Maecenas tincidunt tellus non egestas venenatis.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+      <Card className="card">        
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam finibus feugiat vestibulum. Ut molestie, ex at efficitur aliquet, dui justo venenatis augue, in dignissim lectus justo ut nulla. Sed consectetur eros vitae urna laoreet aliquam. Maecenas tincidunt tellus non egestas venenatis.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>      
+      <Card className="card">        
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam finibus feugiat vestibulum. Ut molestie, ex at efficitur aliquet, dui justo venenatis augue, in dignissim lectus justo ut nulla. Sed consectetur eros vitae urna laoreet aliquam. Maecenas tincidunt tellus non egestas venenatis.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+      <Card className="card">        
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam finibus feugiat vestibulum. Ut molestie, ex at efficitur aliquet, dui justo venenatis augue, in dignissim lectus justo ut nulla. Sed consectetur eros vitae urna laoreet aliquam. Maecenas tincidunt tellus non egestas venenatis.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+      <Card className="card">        
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam finibus feugiat vestibulum. Ut molestie, ex at efficitur aliquet, dui justo venenatis augue, in dignissim lectus justo ut nulla. Sed consectetur eros vitae urna laoreet aliquam. Maecenas tincidunt tellus non egestas venenatis.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>      
+      <Card className="card">        
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam finibus feugiat vestibulum. Ut molestie, ex at efficitur aliquet, dui justo venenatis augue, in dignissim lectus justo ut nulla. Sed consectetur eros vitae urna laoreet aliquam. Maecenas tincidunt tellus non egestas venenatis.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+      <Card className="card">        
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam finibus feugiat vestibulum. Ut molestie, ex at efficitur aliquet, dui justo venenatis augue, in dignissim lectus justo ut nulla. Sed consectetur eros vitae urna laoreet aliquam. Maecenas tincidunt tellus non egestas venenatis.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+      <Card className="card">        
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam finibus feugiat vestibulum. Ut molestie, ex at efficitur aliquet, dui justo venenatis augue, in dignissim lectus justo ut nulla. Sed consectetur eros vitae urna laoreet aliquam. Maecenas tincidunt tellus non egestas venenatis.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>      
+    </CardColumns>
+    
+
+    
+  );
+};
+
+export default ListView;
