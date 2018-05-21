7e1f9410c6a337ac1d55912fa234c52cb34da41a
Brandon Hopper <BrandonHopper0@gmail.com>
2018-05-21 16:08:07 -0400

    -installed react-router-dom and wrapped app with BrowserRouter

--- lambda/src/index.js
+++ lambda/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+//importing Router
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
 import App from './App';
 //importing Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+//Wrapping App with Router
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>
+    ,document.getElementById('root'));
 registerServiceWorker();
