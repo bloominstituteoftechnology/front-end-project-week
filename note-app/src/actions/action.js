1363fd526edaa4fb5297c0ae102607b8fc594769
maxwashington <mnwashington18@gmail.com>
2018-05-21 13:24:38 -0500

    Actions.js -Create addNote updateNote, addTag, deleteNote, checkUpdate functions

---
+++ note-app/src/actions/action.js
@@ -0,0 +1,55 @@
+export const ADDNOTE = "ADDNOTE";
+export const DELETENOTE = "DELETENOTE";
+export const UPDATENOTE = "UPDATENOTE";
+export const CHECKUPDATE = "CHECKUPDATE";
+export const ADDTAG = "ADDTAG";
+export const REORDERSTATE = "REORDERSTATE";
+
+let notes = [];
+let titles = [];
+
+export const addNote = note => {
+  return {
+    type: ADDNOTE,
+    payload: note
+  };
+};
+
+export const updateNote = (noteTitle, noteBody, i) => {
+  return {
+    type: UPDATENOTE,
+    title: noteTitle,
+    payload: noteBody,
+    index: i
+  };
+};
+
+export const addTag = (tag, i) => {
+    return {
+        type: ADDTAG,
+        payload: tag,
+        index: i
+    }
+}
+
+export const deleteNote = (note) => {
+    return {
+        type: DELETENOTE,
+        payload: note
+    }
+}
+
+export const checkUpdate = (bool, i) => {
+    return {
+        type: CHECKUPDATE,
+        payload: bool,
+        index: i
+    }
+}
+
+export const reorderState = (completeObject) => {
+    return {
+        type: REORDERSTATE,
+        payload: completeObject,
+    }
+}
