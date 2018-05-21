a69bf7640ba16f104e47ad4d0a94348025e6cc4c
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-05-21 10:23:42 -0700

    set up components directory and import/exports in index.js

---
+++ notes/src/components/index.js
@@ -0,0 +1,7 @@
+import List from './List';
+import Note from './Note.js';
+import NewNote from './NewNote';
+import EditNote from './EditNote';
+import DeleteNote from './DeleteNote';
+
+export default { List, Note, NewNote, EditNote, DeleteNote };
