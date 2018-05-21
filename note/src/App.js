cf6d8c7cfc1ff3f6c194802f6cfb5069f9cd2998
kammagik <kammagik@yahoo.com>
2018-05-21 11:40:41 -0700

    added bootstrap

--- note/src/App.js
+++ note/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {
