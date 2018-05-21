75d578f0c0128158f83e2c5aa9c2aa07fc452868
dlinz7 <linzdiegoa@gmail.com>
2018-05-21 16:18:34 -0500

    Created basic notecard component with title/body

---
+++ notes/src/components/NoteCard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Card, CardText, CardBody, CardTitle, Button } from 'reactstrap';
+
+
+
+const NoteCard = (props) => {
+    return (
+        <div>
+            <Card>
+                <CardBody>
+                    <CardTitle> Note Title </CardTitle>
+                    <CardText>fjkl;sajkfld;safjdksla;fdjakfld;ajfdklsa;fjdksalf;djaskfdl;asfjdksl;a</CardText>
+                </CardBody>
+            </Card>
+        </div>
+
+    );
+};
+
+
+export default NoteCard;
