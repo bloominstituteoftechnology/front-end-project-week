2b99c9af2e2d3ca154d2a34d4a2cc8c7761ed4f3
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-05-21 12:13:39 -0700

    built NewNote component barebones

--- notes/src/components/NewNote.js
+++ notes/src/components/NewNote.js
@@ -1,1 +1,26 @@
 import React, { Component } from 'react';
+
+class Newnote extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div style={{ display:'flex', flexWrap:'wrap' }}>
+                <h3>Create New Note:</h3>
+                <div className="note-title">
+                    <input placeholder="Note Title"/>
+                </div>
+                <div>
+                    <input style={{ width:'500px', height:'500px' }} placeholder="Note Content"/>
+                </div>
+                <div>
+                    <button>Save</button>
+                </div>
+            </div>
+         )
+    }
+}
+ 
+export default Newnote;
