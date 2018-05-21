61643d2c4a98aef7129f60ddc7bcf34818d234d5
bbenefield89 <bsquared18@gmail.com>
2018-05-21 18:42:20 -0400

    created reusable component `Form` this component original comes up `NewNote` comp

---
+++ client/src/components/misc/Form.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import TextField from '../misc/TextField';
+import Button from '../misc/Button';
+
+const Form = props => {
+  return (
+    <form className='new-note-wrapper__form' onSubmit={ e => e.preventDefault() } >
+      <div>
+        <TextField
+          variant='input'
+          inputName='title'
+          inputType='text'
+          inputVal={ props.title }
+          inputOnChange={ props.setInputVal }
+        />
+      </div>
+
+      <div>
+        <TextField
+          textareaName='content'
+          textareaContent={ props.content }
+          textareaOnChange={ props.setInputVal }
+        />
+      </div>
+
+      <div className='form__submit-wrapper'>
+        <Button
+          buttonClassName='submit-wrapper__submit-button'
+          buttonContent='Save'
+          buttonType='submit'
+          buttonOnClick={ props.buttonOnClick }
+        />
+      </div>
+    </form>
+  )
+}
+ 
+export default Form;
