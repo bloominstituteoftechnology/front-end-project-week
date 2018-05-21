d6d095af72e2eafa9045bd55d358149816bb3645
Xang Thao <xangthao24@yahoo.com>
2018-05-21 10:57:35 -0700

    added react-redux and configure index.js

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(() => {}, applyMiddleware(thunk, logger));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
