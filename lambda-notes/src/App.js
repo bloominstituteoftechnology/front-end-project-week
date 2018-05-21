728f2218a9d74190ce72c15c5abd90742fec1dbe
Matthias Bakken <matthias.bakken@gmail.com>
2018-05-22 03:58:07 +0700

    setup app.js layout for routing and design.

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
+import NoteList from './components/NoteList';
+import CreateNote from './components/CreateNote';
+import EditNote from './components/EditNote';
+import SideBar from './components/SideBar';
 import './App.css';
+import { EDIT_NOTE } from './actions';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello
+        <div className="sidebar">
+          <SideBar />
+        </div>
+        <div className="container">
+          <div className="home-page">
+            <Route exact path="/" component={NoteList} />
+          </div>
+          <div className="create-note">
+            <Route path="/addNote" component={CreateNote} />
+          </div>
+          <div className="edit-note">
+            <Route path="/editNote" component={EditNote} />
+          </div>
+        </div>
       </div>
     );
   }
