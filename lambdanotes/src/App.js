2a0c087fe67df0269e65ca844e60b2c71f4ace05
Bradiowave <divebombwalker@gmail.com>
2018-05-21 13:24:12 -0700

    connected google fonts Roboto and Raleway

--- lambdanotes/src/App.js
+++ lambdanotes/src/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello party people
+        <h1>Hello party people</h1>
+        <p>how is it going?</p>
       </div>
     );
   }
