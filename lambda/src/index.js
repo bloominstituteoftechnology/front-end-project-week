6181fe3f58182f9f3d872142cfed7cb51d17ada5
Brandon Hopper <BrandonHopper0@gmail.com>
2018-05-21 12:58:23 -0400

    -imported Bootstrap

---
+++ lambda/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+//importing Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
