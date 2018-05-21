0ce118af7295585775c12d063aef31565ba25de1
Zach Campbell <beazlebubbles@gmail.com>
2018-05-21 11:18:09 -0600

    Implemented some styling on outermost component Added content area component

--- lambdanotes/src/App.js
+++ lambdanotes/src/App.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import './App.css';
+import ContentArea from './components/contentarea.js';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [{
+        title: "Placeholder Note Title",
+        content: "Placeholder note content",
+        id: 0
+      }],
+      appState: "list",
+      editId: null
+    };
+  }
+
+
   render() {
     return (
       <div className="App">
         <Container>
           <Row>
-            <Col className="left-side" xs="3">LEFT SIDE</Col>
-            <Col className="content" xs="9">CONTENT</Col>
+            <Col className="left-side" xs="3">
+              PLACEHOLDER
+            </Col>
+            <Col className="content" xs="9">
+              <ContentArea />
+            </Col>
           </Row>
         </Container>
       </div>
---
+++ lambdanotes/src/components/contentarea.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ContentArea = (props) => {
+  return (
+    <div>TESTING</div>
+  );
+}
+
+export default ContentArea;
