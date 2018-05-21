39e7e8b17c850d4c82672b0d1d0e471891884ddb
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-05-21 11:26:36 -0700

    set up the layout for the list view

---
+++ notes/src/components/List.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+export default class List extends Component {
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col xs="3" className="sidebar1">
+            <h1>Lambda Notes</h1>
+            <button>View Your Notes</button>
+            <button>+ Create New Note</button>
+          </Col>
+          <Col xs="9" className="note-list">
+            <h3>Your Notes:</h3>
+                <Row>
+                    <Col xs="4">
+                        <button>
+                            <h1>Note Title</h1>
+                            <p>Morbi pharetra sem vel elit pulvinar interdum. Cras aliquam accumsan nunc.</p>
+                        </button>
+                    </Col>
+                    <Col xs="4">
+                        <button>
+                            <h1>Note Title</h1>
+                            <p>Morbi pharetra sem vel elit pulvinar interdum. Cras aliquam accumsan nunc.</p>
+                        </button>
+                    </Col>
+                    <Col xs="4">
+                        <button>
+                            <h1>Note Title</h1>
+                            <p>Morbi pharetra sem vel elit pulvinar interdum. Cras aliquam accumsan nunc.</p>
+                        </button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs="4">
+                        <button>
+                            <h1>Note Title</h1>
+                            <p>Morbi pharetra sem vel elit pulvinar interdum. Cras aliquam accumsan nunc.</p>
+                        </button>
+                    </Col>
+                    <Col xs="4">
+                        <button>
+                            <h1>Note Title</h1>
+                            <p>Morbi pharetra sem vel elit pulvinar interdum. Cras aliquam accumsan nunc.</p>
+                        </button>
+                    </Col>
+                    <Col xs="4">
+                        <button>
+                            <h1>Note Title</h1>
+                            <p>Morbi pharetra sem vel elit pulvinar interdum. Cras aliquam accumsan nunc.</p>
+                        </button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs="4">
+                        <button>
+                            <h1>Note Title</h1>
+                            <p>Morbi pharetra sem vel elit pulvinar interdum. Cras aliquam accumsan nunc.</p>
+                        </button>
+                    </Col>
+                    <Col xs="4">
+                        <button>
+                            <h1>Note Title</h1>
+                            <p>Morbi pharetra sem vel elit pulvinar interdum. Cras aliquam accumsan nunc.</p>
+                        </button>
+                    </Col>
+                    <Col xs="4">
+                        <button>
+                            <h1>Note Title</h1>
+                            <p>Morbi pharetra sem vel elit pulvinar interdum. Cras aliquam accumsan nunc.</p>
+                        </button>
+                    </Col>
+                </Row>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
--- notes/src/components/index.js
+++ notes/src/components/index.js
@@ -4,4 +4,4 @@ import NewNote from './NewNote';
 import EditNote from './EditNote';
 import DeleteNote from './DeleteNote';
 
-export default { List, Note, NewNote, EditNote, DeleteNote };
+export { List, Note, NewNote, EditNote, DeleteNote };
