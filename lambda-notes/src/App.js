524f5c8ab1fac6c2e16287b34b007f86e7e7a98c
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-05-22 07:48:22 -0700

    main content moved to separate file Cards and routed in App.js

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,83 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Route, Link } from "react-router-dom";
-// import CreateNote from './components/CreateNote';
+import CreateNote from './components/CreateNote';
 // import EditNote from "./components/EditNote";
 // import Note from "./components/Note";
 import SideBar from "./components/SideBar";
+import Cards from "./components/Cards";
+
 
 class App extends Component {
   render() {
     return <div className="container-fluid custom-container">
-          <div class="row custom-row">
-            <Route exact path="/" component={SideBar} />
-
-            <div className='col-sm-9 cards'>
-            <h4 className='your-notes'>Your notes:</h4>
-              <div class="card mb-sm-4 col-sm-3">
-                <div class="card-header no-bg">
-                  <h5 class="d-sm-inline">Note Title</h5>
-                </div>
-                <div class="list-group list-group-flush">
-                  <p className="mt-sm-2">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
-                  </p>
-                </div>
-              </div>
-
-
-              <div class="card mb-sm-4  col-sm-3">
-                <div class="card-header no-bg">
-                  <h5 class="d-sm-inline">Note Title</h5>
-                </div>
-                <div class="list-group list-group-flush">
-                  <p className="mt-sm-2">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
-                  </p>
-                </div>
-              </div>
-              <div class="card mb-sm-4  col-sm-3">
-                <div class="card-header no-bg">
-                  <h5 class="d-sm-inline">Note Title</h5>
-                </div>
-                <div class="list-group list-group-flush">
-                  <p className="mt-sm-2">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
-                  </p>
-                </div>
-              </div>
-              <div class="card mb-sm-4  col-sm-3">
-                <div class="card-header no-bg">
-                  <h5 class="d-sm-inline">Note Title</h5>
-                </div>
-                <div class="list-group list-group-flush">
-                  <p className="mt-sm-2">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
-                  </p>
-                </div>
-              </div>
-          </div>
-          </div>
+        <div class="row custom-row">
+          <Route path="/" component={SideBar} />
+          <Route exact path="/" component={Cards} />
+          <Route exact path="/create" component={CreateNote} />
+        </div>
       </div>;
   }
 }
