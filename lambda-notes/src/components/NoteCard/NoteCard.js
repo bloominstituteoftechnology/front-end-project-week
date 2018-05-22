b1a079d2b5fc0c3e0614d73c628ce9a293c273c0
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-22 13:04:55 -0400

    Added React Router Link to NoteCard - Routes to Note view

--- lambda-notes/src/components/NoteCard/NoteCard.js
+++ lambda-notes/src/components/NoteCard/NoteCard.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Card, CardBody, CardTitle, CardText } from 'reactstrap';
 
 const NoteCard = props => {
   const notePreview = `${props.note.value.slice(0, 100)}...`;
   return (
     <React.Fragment>
-      <Card className="note-preview-card">
-        <CardBody>
-          <CardTitle>
-            {props.note.title}
-          </CardTitle>
-          <CardText>
-            {notePreview}
-          </CardText>
-        </CardBody>
-      </Card>
+      <Link to={`/note/${props.note.id}`}>
+        <Card className="note-preview-card">
+          <CardBody>
+            <CardTitle>
+              {props.note.title}
+            </CardTitle>
+            <CardText>
+              {notePreview}
+            </CardText>
+          </CardBody>
+        </Card>
+      </Link>
     </React.Fragment>
   );
 }
