cdbf73be0011f017c764016ae40003dd020147b8
Bradiowave <divebombwalker@gmail.com>
2018-05-22 10:15:27 -0700

    refactored CreateNote.css to be non-conflicting

--- lambdanotes/src/components/CreateNote/CreateNote.js
+++ lambdanotes/src/components/CreateNote/CreateNote.js
@@ -6,13 +6,13 @@ const CreateNote = () => {
     return (
         <div className='CreateNoteContainer'>
             <div>
-                <h3 className='header'>Create New Note:</h3>
+                <h3 className='createNoteHeader'>Create New Note:</h3>
             </div>
-            <form className='form'>
-                <input type='text' className='inputTitle' name='title' placeholder='Note Title' />
-                <textarea type='text' className='inputContent' name='content' placeholder='Note Content'></textarea>
-                <Link to='/note' className='link'>
-                    <input className='saveButton' type='button' value='Save' />
+            <form className='createNoteForm'>
+                <input type='text' className='createNoteInputTitle' name='title' placeholder='Note Title' />
+                <textarea type='text' className='createNoteInputContent' name='content' placeholder='Note Content'></textarea>
+                <Link to='/note' className='createNoteLink'>
+                    <input className='createNoteSaveButton' type='button' value='Save' />
                 </Link>
             </form>
         </div>
