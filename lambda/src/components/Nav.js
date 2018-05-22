44ba0b7ebe3c4bcc7da02b3f31308b8541e0d665
Brandon Hopper <BrandonHopper0@gmail.com>
2018-05-21 20:10:04 -0400

    Linked buttons to paths

--- lambda/src/components/Nav.js
+++ lambda/src/components/Nav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 //importing button from reactstrap
 import { Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 //statless componet for Navbar, which will be exported to App.js to render NavBar
@@ -8,8 +9,9 @@ const NavBar = () => {
     return(
         <div>
             <h1>Lambda Notes</h1>
-            <Button color="secondary">View Your Note</Button>{' '}
-            <Button color="secondary">+ Create New Note</Button>{' '}
+            <Link to="/"><Button color="secondary">View Your Note</Button>{' '}</Link>
+            <Link to="/note"> <Button color="secondary">+ Create New Note</Button>{' '}</Link>
+           
         </div>
     );
 };
