e9f14dc7e55da56be267ea098c04301b0e345a2c
Lavell Burton <taylormade800@gmail.com>
2018-05-22 15:52:03 -0700

    updating files

--- notes/src/App.js
+++ notes/src/App.js
@@ -5,6 +5,26 @@ import Board from './components/board';
 import Sidebar from './components/sidebar';
 
 class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     newNote: ''
+  //   };
+  //   this.addNote = addNote.bind(this);
+  //   this.updateNewNote = this.updateNewNote.bind(this);
+  // }
+  // addNote(event) {
+  //   event.preventDefault();
+  //   this.props.addNote({
+  //     value: this.state.newNote,
+  //     complete: false
+  //   });
+  //   this.setState({ newNote: '' });
+  // }
+  // updateNewNote(event) {
+  //   this.setState({ newNote: event.target.value });
+  // }
+
   render() {
     return (
       <div className="App">
@@ -20,5 +40,11 @@ class App extends Component {
     );
   }
 }
+const mapStateToProps = (state) => {
+  return {
+    notes: state.notes
+  }
+}
 
 export default App;
+// export default connect(mapStateToProps, {addNote})(App);
---
+++ notes/src/components/sidebars.js
@@ -0,0 +1,22 @@
+// import React from 'react';
+// import { Nav, NavItem, NavLink } from 'reactstrap';
+
+// export default class Example extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <p>List Based</p>
+//         <Nav vertical>
+//           <NavItem>
+//             <NavLink href="#">Link</NavLink>
+//           </NavItem>
+//           <NavItem>
+//             <NavLink href="#">Link</NavLink>
+//                 </NavItem>
+//             </Nav>
+//           </div>      
+//           );
+//         }
+//     }
+
+// moving buttons from there to here. rearranging the files
