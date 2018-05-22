d22aed83aa9b0731ec7c34fba077d1cd604b2256
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-22 02:29:25 -0400

    Built Routes component - Routing to views

--- lambda-notes/src/components/Routes/Routes.js
+++ lambda-notes/src/components/Routes/Routes.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import List from '../../views/List';
+import CreateNew from '../../views/CreateNew';
+import Edit from '../../views/Edit';
+import Note from '../../views/Note';
 
 const Routes = () => {
   return (
     <div>
+      <Route exact path="/" component={List} />
+      <Route path="/create-new" component={CreateNew} />
+      <Route path="/edit" component={Edit} />
+      <Route path="/note/:id" component={Note} />
     </div>
   );
 }
- 
+
 export default Routes;
