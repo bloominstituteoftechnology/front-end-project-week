c1236a154c28c466d6d1c9631570011a5efd9553
jaklundun <govi.escudero@gmail.com>
2018-05-21 19:15:27 -0700

    Set up +Create New Note button functionality on SideBar component to route to appropriate URL path.

--- lambda-notes/src/components/SideBar.js
+++ lambda-notes/src/components/SideBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './SideBar.css'
+import { Link } from 'react-router-dom';
 import { Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle, Button, Row, Col, CardColumns } from 'reactstrap';
 
@@ -11,7 +12,7 @@ const SideBar = () => {
             <Button color="info" className="side-bar-button">View Your Notes</Button>
             <br/>
             <br/>
-            <Button color="info" className="side-bar-button">+Create New Note</Button>
+            <Link to="/createNote"><Button color="info" className="side-bar-button">+Create New Note</Button></Link>
         </div>
     )
 }
