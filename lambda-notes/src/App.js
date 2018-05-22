940470bf7a4e25830ea4781b5cc011eac059b3e6
Marco A Guzman <marcoguzman16@gmail.com>
2018-05-22 13:47:34 -0700

    adding components

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { }
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 class App extends Component {
-  componentDidMount() {
-
-   }
 
   render() {
     return (
@@ -19,12 +16,22 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-
+    <Router>
       <div className="Notes-sidebar" style="width:25%">
         <h3 className="Notes-bar-item">Lambda Notes</h3>
-        <a href="#" className="Notes-bar-item notes-button">View Your Notes</a>
-        <a href="#" className="Notes-bar-item notes-button">'+' Create New Note</a>
-      </div>
+        <ul>
+        <li><Link to="/notes" className="Notes-bar-item notes-button">View Your Notes</Link></li>
+        <li><Link to="/newnote" className="Notes-bar-item notes-button">'+' Create New Note</Link></li>
+        </ul>  
+          <hr/>
+
+          <Route exact path="/" component={Home} /> 
+          <Route path="/notes" component={Notes} />
+          <Route path="/newnote" component={NewNotes} />  
+          
+        </div>
+        </div>
+      </Router>
 
     );
   }
