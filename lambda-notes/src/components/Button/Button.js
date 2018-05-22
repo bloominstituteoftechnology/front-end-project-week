ef95c0e13c36a3eb0da79ca2305cd3a4e3c7dfdb
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-22 03:36:08 -0400

    Changed class name on Button component to not pick up Boostrap button styles

--- lambda-notes/src/components/Button/Button.js
+++ lambda-notes/src/components/Button/Button.js
@@ -6,8 +6,8 @@ const Button = props => {
     <React.Fragment>
       {
         (props.link !== null) 
-          ? <Link className={`btn ${props.type}`} to={props.link}>{props.title}</Link>
-          : <button className={`btn ${props.type}`}>{props.title}</button>
+          ? <Link className={`button ${props.type}`} to={props.link}>{props.title}</Link>
+          : <button className={`button ${props.type}`}>{props.title}</button>
       }
     </React.Fragment>
   );
