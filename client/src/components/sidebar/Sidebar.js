40693214639f01bf53cb9c96b781fdda2c761788
bbenefield89 <bsquared18@gmail.com>
2018-05-22 15:58:21 -0400

    style(Sidebar): CSS

--- client/src/components/sidebar/Sidebar.js
+++ client/src/components/sidebar/Sidebar.js
@@ -4,19 +4,22 @@ import { Link } from 'react-router-dom';
 // components
 import Button from '../misc/Button';
 
+// style
+import './Sidebar.css';
+
 const Sidebar = () => {
   return (
-    <nav>
-      <div className='logo'>
-        <h1>Lambda Notes</h1>
+    <nav className='navigation'>
+      <div className='navigation__logo'>
+        <h1 className='logo__h1'>Lambda Notes</h1>
       </div>
 
-      <ul>
+      <ul className='navigation__button-container'>
         <li>
           <Link to='/'>
             <Button
               buttonContent='View Your Notes'
-              className='nav__view-notes-button'
+              buttonClassName='button-container__view-notes-button'
             />
           </Link>
         </li>
@@ -25,7 +28,7 @@ const Sidebar = () => {
           <Link to='/note/new'>
             <Button
               buttonContent='+ Create new Note'
-              className='nav_create-new-note-button'
+              buttonClassName='button-container_create-new-note-button'
             />
           </Link>
         </li>
