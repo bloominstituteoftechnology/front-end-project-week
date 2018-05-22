7e414637c178ce3ff40988e625c7bae08e6621bb
Rachelle De Jesus Pestanas <rachelle.pestanas@gmail.com>
2018-05-22 15:10:53 -0500

    imported BrowserRouter as Router from 'react-router-dom''

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
