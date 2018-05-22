0d9f9bfd942be5e661510f454e4102c4fa6e2ecc
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-22 17:08:29 -0400

    Finished building NoteForm component -- will need to adjust to work with both edit and new note

--- lambda-notes/src/components/NoteForm/NoteForm.js
+++ lambda-notes/src/components/NoteForm/NoteForm.js
@@ -4,11 +4,34 @@ import Button from '../Button';
 class NoteForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = { 
+      title: '',
+      contents: ''
+    }
+  }
+  handleChange= event => {
+    this.setState({ [event.target.name]:event.target.value });
   }
   render() { 
-    return ( 
-      <Button type="primary" title={this.props.action} link="/" />
+    return (
+      <div>
+        <form>
+          <input 
+            className="form-control"
+            type="text"
+            name="title"
+            placeholder="Note Title"
+            onChange={this.handleChange}
+          />
+          <textarea
+            className="form-control"
+            name="contents"
+            placeholder="Note Content"
+            onChange={this.handleChange}
+          />
+          <Button type="primary" title={this.props.action} link="/" />
+        </form>
+      </div>
     )
   }
 }
