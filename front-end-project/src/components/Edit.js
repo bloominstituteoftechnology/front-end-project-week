a40aa111b996855f3e71cfd6a8b303a727273986
AmyShackles <amyshackles@gmail.com>
2018-05-22 12:51:14 -0700

    Edit page now looks like the edit page should.

--- front-end-project/src/components/Edit.js
+++ front-end-project/src/components/Edit.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import '../styles/Edit.css'
 
 const Edit = props => {
     return (
-        <div>
-            Edit Page
-        </div>
+        <div className="main-text">
+            <h3 style={{textAlign: "left"}}> Edit Note: </h3>
+            <textarea rows="1" cols="60" placeholder="Note Title">
+            </textarea>
+            <textarea rows="15" cols="110" placeholder="Note Content">
+            </textarea>
+            <NavLink to="/"> <button className="saveButton"> Update </button></NavLink>
+            </div>
     )
 }
 
