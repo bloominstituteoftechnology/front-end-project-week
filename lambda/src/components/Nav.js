373fba15daf0d2f43dee034851609c24fe7790ef
Brandon Hopper <BrandonHopper0@gmail.com>
2018-05-22 12:13:28 -0400

    -importing CSS to Form

--- lambda/src/components/Nav.js
+++ lambda/src/components/Nav.js
@@ -1,18 +1,25 @@
 import React from 'react';
-//importing button from reactstrap
-import { Button } from 'reactstrap';
+//importing button and Container from reactstrap
+import { Button, Container, Col } from 'reactstrap';
+//importing Link to from react-router-dom
 import { Link } from 'react-router-dom';
 
+//importing Nav CSS
+import './Nav.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 //statless componet for Navbar, which will be exported to App.js to render NavBar
 const NavBar = () => {
     return(
-        <div>
+      <Container className="container">
+        <Col>
+        <div className="Nav">
             <h1>Lambda Notes</h1>
-            <Link to="/"><Button color="secondary">View Your Note</Button>{' '}</Link>
-            <Link to="/note"> <Button color="secondary">+ Create New Note</Button>{' '}</Link>
-           
+            <Link to="/"><Button color="info">View Your Note</Button>{' '}</Link>
+            <Link to="/note"> <Button color="info">+ Create New Note</Button>{' '}</Link>  
         </div>
+        </Col>
+      </Container>
     );
 };
 
