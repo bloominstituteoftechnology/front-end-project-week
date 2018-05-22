2e91c1a6711d86c8d1bf4cf67d1afcd6ab822896
Zach Campbell <beazlebubbles@gmail.com>
2018-05-21 18:00:24 -0600

    Completed visual aspects of delete modal

--- lambdanotes/src/components/noteview.js
+++ lambdanotes/src/components/noteview.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './noteview.css';
 import { Modal, ModalBody } from 'reactstrap';
+import LambdaButton from './lambdabutton.js';
 
 
 const NoteView = (props) => {
@@ -15,8 +16,12 @@ const NoteView = (props) => {
       <h5 className="note-view-title">{props.note.title}</h5>
       <p className="note-view-content">{props.note.content}</p>
       <Modal isOpen={deleting} className="my-modal">
-        <ModalBody>
+        <ModalBody className="delete-modal">
           Are you sure you want to delete this?
+          <div className="delete-buttons">
+            <LambdaButton color="red" text="Delete" />
+            <LambdaButton color="green" text="No" />
+          </div>
         </ModalBody>
       </Modal>
     </div>
