f1868d7ddf02d28f062ef206be3430d9a2dd54dd
MacBookPro <macbookpro@MacBookPros-MacBook-Pro.local>
2018-05-22 15:51:30 -0500

    displayNotes.js partialy styled and linked

--- notes/src/components/displayNotes.js
+++ notes/src/components/displayNotes.js
@@ -1,10 +1,41 @@
 import React,  { Component } from 'react';
-import  NavButtons  from './navButtons';
-const DisplayNotes = () => {
-  return <h1>display notes</h1>;
-  <div>
-        {/*<NavButtons />*/}
-    </div>
-};
+//import  NavButtons  from './navButtons';
+import { Route, Link } from 'react-router-dom';
+import './displayNotes.css';
 
-export default DisplayNotes;
+
+class DisplayNotes extends Component {
+    constructor() {
+      super();
+      };
+    
+  
+    render() {
+      return (
+      <body>  
+        <div className="container">
+            <div className="sideBar">
+                <div className="sideBarTitle">
+                    <h1 className="sideBarTitle1">Lambda</h1>
+                    <h1 className="sideBarTitle2">Notes</h1>
+                </div>
+                <Link to="/">
+                <button type="button" className="viewNotesButton">
+                View Your Notes
+                </button>
+                </Link>
+                <Link to="/createNote">
+                <button type="button" className="createNoteButton">
+                + Create New Note
+                </button>
+                </Link>
+           </div>
+        
+         <div className="mainBody">
+            <h4 className="mainBodyTitle">Your Notes:</h4>
+        </div>
+        </div>
+      </body>
+ )} }    
+  
+  export default DisplayNotes;  
