0f6707c557ac30e503b1de327a8f6ab94a12f4ee
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-22 16:12:33 -0500

    created deleteNote action, succesfully able to delete and direct back to list view

--- lambda-notes/src/actions/index.js
+++ lambda-notes/src/actions/index.js
@@ -1,5 +1,6 @@
 export const FETCH_NOTES = 'FETCH_NOTES';
 export const ADD_NOTE = 'ADD_NOTE';
+export const DELETE_NOTE = 'DELETE_NOTE';
 
 export const fetchNotes = () => {
     return {
@@ -11,5 +12,12 @@ export const addNote = (newNote) => {
     return {
         type: ADD_NOTE,
         payload: newNote
+    }
+}
+
+export const deleteNote = id => {
+    return {
+        type: DELETE_NOTE,
+        payload: id
     }
 }
--- lambda-notes/src/components/NoteView/NoteView.js
+++ lambda-notes/src/components/NoteView/NoteView.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-
+import { deleteNote } from '../../actions';
 
 import './NoteView.css';
 import Button from '../Button/Button';
@@ -35,25 +35,28 @@ class NoteView extends Component {
                 <Link  onClick={this.toggle} className="link" to={`/note/${this.matchedNote.id}/delete`}>delete</Link>
                 <Link  onClick={this.toggle} className="link" to={`/note/${this.matchedNote.id}/edit`}>edit</Link>
             </div>
+
             <div className="noteView-title">{this.matchedNote.title}</div>
+
             <div className="noteView-body">{this.matchedNote.body}</div>
 
-            {/*Modal*/}
+            {/*MODAL*/}
             <div>
-        {/* <Button color="danger" onClick={this.toggle}>{this.props.buttonLabel}</Button> */}
-        <div>
-        <Modal className="modal" isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-          {/* <ModalHeader toggle={this.toggle}>Modal title</ModalHeader> */}
-          <ModalBody className="modal-body">
-             Are you sure you want to delete this?
-          </ModalBody>
-          <ModalFooter className="modal-footer">
-            <Button className="red-btn" text="Delete" onClick={this.toggle}></Button>{' '}
-            <Button text="No" onClick={this.toggle}></Button>
-          </ModalFooter>
-        </Modal>
-        </div>
-      </div>
+                <div>
+                    <Modal className="modal" isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                    
+                        <ModalBody className="modal-body">
+                        Are you sure you want to delete this?
+                        </ModalBody>
+
+                        <ModalFooter className="modal-footer">
+                            <Link style={{textDecoration: "none"}} to="/"><Button className="red-btn" text="Delete" onClick={() => this.props.deleteNote(this.matchedNote.id)}></Button>{' '}</Link>
+                            <Button text="No" onClick={this.toggle}></Button>
+                        </ModalFooter>
+
+                    </Modal>
+                </div>
+            </div>
         </div>
     )
   }
@@ -71,7 +74,7 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps)(NoteView);
+export default connect(mapStateToProps, { deleteNote })(NoteView);
 
 
 
