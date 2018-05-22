e6ef35e0b29115c1d76668120747c927263ffff3
FireInjun <Fireinjun291@gmail.com>
2018-05-22 17:02:09 -0400

    Added additional folders
    and files. To better represent what the app will be when complete. Also added Bootstrap and Reactstrap (the correct way) and will be removing the URL CDN shortly after this commit. I thought long and hard about it and I decided that I can't make this app without utilizing redux from the beginning. It would be disappointing to myself and my instructors. I can do better than that and this deserves my best.

--- lambdanotes/src/index.js
+++ lambdanotes/src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import reducer from './reducers'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(reducer)
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
--- projectdetails/scratchpad.js
+++ projectdetails/scratchpad.js
@@ -1,1 +1,50 @@
 //Here is my local useful snippets repository. 
+
+
+//NoteCard reactstrap 
+
+import React from 'react';
+import { Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+
+const Example = (props) => {
+    return (
+    <Row>
+        <Col sm="6">
+        <Card body>
+            <CardTitle>Special Title Treatment</CardTitle>
+            <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+            <Button>Go somewhere</Button>
+        </Card>
+        </Col>
+        <Col sm="6">
+        <Card body>
+            <CardTitle>Special Title Treatment</CardTitle>
+            <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+            <Button>Go somewhere</Button>
+        </Card>
+        </Col>
+    </Row>
+    );
+};
+
+export default Example;
+
+
+
+//Pretty sure I need to wrap the app like this example, on the index.js file.
+
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import reducer from './reducers'
+
+const store = createStore(reducer)
+
+render(
+    <Provider store={store}>
+    <App />
+    </Provider>,
+    document.getElementById('root')
+)
