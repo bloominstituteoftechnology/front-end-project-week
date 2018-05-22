5865b60c3ac7d3b34a540cbd33a9a2149a3a35df
Bradiowave <divebombwalker@gmail.com>
2018-05-22 10:57:03 -0700

    refactored ListView so links fit card boundary

--- lambdanotes/src/components/ListView/ListView.js
+++ lambdanotes/src/components/ListView/ListView.js
@@ -7,60 +7,78 @@ const ListView = () => {
         <div className='listViewContainer'>
             <h3 className='listViewHeader'>Your Notes:</h3>
             <div className='noteCardsCollection'>
-                <Link to='/note' className='noteCardLink'>
-                    <div className='noteCard'>
-                        <h6 className='noteTitle'>Note Title</h6>
-                        <p className='noteContent'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, eﬃcitur mollis …</p>
-                    </div>
-                </Link>
-                <Link to='/note' className='noteCardLink'>
-                    <div className='noteCard'>
-                        <h6 className='noteTitle'>Note Title</h6>
-                        <p className='noteContent'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, eﬃcitur mollis …</p>
-                    </div>
-                </Link>
-                <Link to='/note' className='noteCardLink'>
-                    <div className='noteCard'>
-                        <h6 className='noteTitle'>Note Title</h6>
-                        <p className='noteContent'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, eﬃcitur mollis …</p>
-                    </div>
-                </Link>
-                <Link to='/note' className='noteCardLink'>
-                    <div className='noteCard'>
-                        <h6 className='noteTitle'>Note Title</h6>
-                        <p className='noteContent'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, eﬃcitur mollis …</p>
-                    </div>
-                </Link>
-                <Link to='/note' className='noteCardLink'>
-                    <div className='noteCard'>
-                        <h6 className='noteTitle'>Note Title</h6>
-                        <p className='noteContent'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, eﬃcitur mollis …</p>
-                    </div>
-                </Link>
-                <Link to='/note' className='noteCardLink'>
-                    <div className='noteCard'>
-                        <h6 className='noteTitle'>Note Title</h6>
-                        <p className='noteContent'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, eﬃcitur mollis …</p>
-                    </div>
-                </Link>
-                <Link to='/note' className='noteCardLink'>
-                    <div className='noteCard'>
-                        <h6 className='noteTitle'>Note Title</h6>
-                        <p className='noteContent'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, eﬃcitur mollis …</p>
-                    </div>
-                </Link>
-                <Link to='/note' className='noteCardLink'>
-                    <div className='noteCard'>
-                        <h6 className='noteTitle'>Note Title</h6>
-                        <p className='noteContent'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, eﬃcitur mollis …</p>
-                    </div>
-                </Link>
-                <Link to='/note' className='noteCardLink'>
-                    <div className='noteCard'>
-                        <h6 className='noteTitle'>Note Title</h6>
-                        <p className='noteContent'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, eﬃcitur mollis …</p>
-                    </div>
-                </Link>
+                <div className='entireNoteCardLink'>
+                    <Link to='/note' className='noteCardLink'>
+                        <div className='noteCard'>
+                            <h6 className='noteTitle'>Note Title</h6>
+                            <p className='noteContent'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, eﬃcitur mollis …</p>
+                        </div>
+                    </Link>
+                </div>
+                <div className='entireNoteCardLink'>
+                    <Link to='/note' className='noteCardLink'>
+                        <div className='noteCard'>
+                            <h6 className='noteTitle'>Note Title</h6>
+                            <p className='noteContent'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, eﬃcitur mollis …</p>
+                        </div>
+                    </Link>
+                </div>
+                <div className='entireNoteCardLink'>
+                    <Link to='/note' className='noteCardLink'>
+                        <div className='noteCard'>
+                            <h6 className='noteTitle'>Note Title</h6>
+                            <p className='noteContent'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, eﬃcitur mollis …</p>
+                        </div>
+                    </Link>
+                </div>
+                <div className='entireNoteCardLink'>
+                    <Link to='/note' className='noteCardLink'>
+                        <div className='noteCard'>
+                            <h6 className='noteTitle'>Note Title</h6>
+                            <p className='noteContent'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, eﬃcitur mollis …</p>
+                        </div>
+                    </Link>
+                </div>
+                <div className='entireNoteCardLink'>
+                    <Link to='/note' className='noteCardLink'>
+                        <div className='noteCard'>
+                            <h6 className='noteTitle'>Note Title</h6>
+                            <p className='noteContent'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, eﬃcitur mollis …</p>
+                        </div>
+                    </Link>
+                </div>
+                <div className='entireNoteCardLink'>
+                    <Link to='/note' className='noteCardLink'>
+                        <div className='noteCard'>
+                            <h6 className='noteTitle'>Note Title</h6>
+                            <p className='noteContent'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, eﬃcitur mollis …</p>
+                        </div>
+                    </Link>
+                </div>
+                <div className='entireNoteCardLink'>
+                    <Link to='/note' className='noteCardLink'>
+                        <div className='noteCard'>
+                            <h6 className='noteTitle'>Note Title</h6>
+                            <p className='noteContent'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, eﬃcitur mollis …</p>
+                        </div>
+                    </Link>
+                </div>
+                <div className='entireNoteCardLink'>
+                    <Link to='/note' className='noteCardLink'>
+                        <div className='noteCard'>
+                            <h6 className='noteTitle'>Note Title</h6>
+                            <p className='noteContent'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, eﬃcitur mollis …</p>
+                        </div>
+                    </Link>
+                </div>
+                <div className='entireNoteCardLink'>
+                    <Link to='/note' className='noteCardLink'>
+                        <div className='noteCard'>
+                            <h6 className='noteTitle'>Note Title</h6>
+                            <p className='noteContent'>Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, eﬃcitur mollis …</p>
+                        </div>
+                    </Link>
+                </div>                
             </div>
         </div>
     )
