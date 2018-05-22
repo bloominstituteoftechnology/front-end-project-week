75dc661d9144f933b17de4e566f42479e2a923ec
Jorge Valdivia <emortalix@welcometoforever.io>
2018-05-22 15:20:33 -0400

    ACTUALLY fixed the App issue + added basic styling .css files

--- lambdanotes/src/index.js
+++ lambdanotes/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import App from './components/App/App';
+import Note from './components/Notes/Notes';
+import EditNote from './components/EditNote/EditNote';
+import CreateNewNote from './components/CreateNewNote/CreateNewNote';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
