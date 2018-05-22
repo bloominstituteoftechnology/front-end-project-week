e87f9f51e27d9a2c3dc89ab7ea36941124e7744a
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-22 13:02:17 -0400

    Built NoteCard component

--- lambda-notes/src/components/NoteCard/NoteCard.js
+++ lambda-notes/src/components/NoteCard/NoteCard.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Card, CardBody, CardTitle, CardText } from 'reactstrap';
 
-const NoteCard = () => {
+const NoteCard = props => {
+  const notePreview = `${props.note.value.slice(0, 100)}...`;
   return (
-    <div>Note Card</div>
+    <React.Fragment>
+      <Card className="note-preview-card">
+        <CardBody>
+          <CardTitle>
+            {props.note.title}
+          </CardTitle>
+          <CardText>
+            {notePreview}
+          </CardText>
+        </CardBody>
+      </Card>
+    </React.Fragment>
   );
 }
 
