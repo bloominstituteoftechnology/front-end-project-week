74cddd670d6ef60266dc61679c946a30eacd0b41
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-22 13:44:52 -0500

    created and styled NoteView

--- lambda-notes/src/components/NoteView/NoteView.js
+++ lambda-notes/src/components/NoteView/NoteView.js
@@ -4,14 +4,23 @@ import { connect } from 'react-redux';
 import './NoteView.css';
 
 
-const NoteView = (props) => {
+const NoteView = props => {
+
+    let matchedNote = props.notes.filter(note => {return note.id == props.match.params.id})[0] // [0] is the one element in the filtered Array that has a matched id
+    // console.log(matchedNote)
     return (
         <div className="noteView-container">
-        <div className=".noteView-header">Note Name</div>
-        {props.notes.filter(note => {return note.id === props.match.params.id})}
+            <div className="noteView-title">{matchedNote.title}</div>
+            <div className="noteView-body">{matchedNote.body}</div>
         </div>
     )
-}
+} 
+  
+
+    
+       
+    
+  
 
 
 
