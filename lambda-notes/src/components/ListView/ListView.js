cf679b6491ca29b2253819d4b1150f1ef81b2717
jesuarva <jean.ariza@jesuarva.com>
2018-05-23 00:18:02 +0200

    DEFINE NoteCard_ component starting structure.
    This component is the one that renders the Note snipped card.

--- lambda-notes/src/components/ListView/ListView.js
+++ lambda-notes/src/components/ListView/ListView.js
@@ -1,22 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
-import NoteCard_ from '../NoteCard_/NoteCard_';
+import NoteCard_ from "../NoteCard_/NoteCard_";
 
 class ListView extends Component {
   render() {
-    const {cards} = this.props;
-    const cardsToDisplay = cards.map((card, index) => <NoteCard_ key={card.id} card={card} />)
+    const { cards } = this.props;
+    const cardsToDisplay = cards.map((card, index) => (
+      <NoteCard_ key={card.id} card={card} />
+    ));
+
     return (
-      <div className="container custom-list-view-container" >
+      <div className="d-flex custom-list-view-container">
         {cardsToDisplay}
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     cards: state.data
-  }
-}
+  };
+};
 export default connect(mapStateToProps, {})(ListView);
--- lambda-notes/src/components/NoteCard_/NoteCard_.js
+++ lambda-notes/src/components/NoteCard_/NoteCard_.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { Card, CardTitle, CardText, Col } from "reactstrap";
 
 const NoteCard_ = props => {
   const { card } = props;
-  return <div>{card.title}</div>;
+  return (
+    <Card className="custom-NoteCard" body>
+      <CardTitle>{card.title}</CardTitle>
+      <CardText>{card.shortContent}</CardText>
+    </Card>
+  );
 };
 
 export default NoteCard_;
