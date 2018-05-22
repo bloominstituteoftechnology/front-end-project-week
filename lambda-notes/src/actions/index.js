eeb99018b64863b287df6895cb2e5d133903ea95
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-05-22 13:41:17 -0700

    actions payload changed so each note is created with an id

--- lambda-notes/src/actions/index.js
+++ lambda-notes/src/actions/index.js
@@ -2,26 +2,26 @@ export const FETCH_NOTES = "FETCH_NOTES";
 export const CREATE_NOTE = "CREATE_NOTE";
 export const DELETE_NOTE = "DELETE_NOTE";
 
-let id = 2;
+let noteId = 2;
 
 export const fetchNotes = () => {
   return {
     type: FETCH_NOTES,
     payload: [
-      { id: 1, title: "laundry", context: false },
-      { id: 2, title: "work out", context: false }
+      { id: 1, title: "redux", context: 'Redux is ...' },
+      { id: 2, title: "tips for front-end development", context: 'Students who...' }
     ]
   };
 };
 
 export const createNote = note => {
-  id += 1;
+  noteId += 1;
   return {
     type: CREATE_NOTE,
     payload: {
-      id: id,
-      title: note,
-      context: false
+      id: noteId,
+      title: note.title,
+      context: note.context
     }
   };
 };
