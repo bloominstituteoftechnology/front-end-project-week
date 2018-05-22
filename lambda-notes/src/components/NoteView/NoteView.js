0ae94b6c32af545e9892be1f6d616b46f5f3b9b0
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-22 14:04:36 -0500

    created edit & delete links in NoteView & added more styles

--- lambda-notes/src/components/NoteView/NoteView.js
+++ lambda-notes/src/components/NoteView/NoteView.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import './NoteView.css';
 
@@ -7,9 +8,14 @@ import './NoteView.css';
 const NoteView = props => {
 
     let matchedNote = props.notes.filter(note => {return note.id == props.match.params.id})[0] // [0] is the one element in the filtered Array that has a matched id
-    // console.log(matchedNote)
+    
+  
     return (
         <div className="noteView-container">
+            <div className="links-container">
+                <Link className="link" to={`/note/${matchedNote.id}/delete`}>delete</Link>
+                <Link className="link" to={`/note/${matchedNote.id}/edit`}>edit</Link>
+            </div>
             <div className="noteView-title">{matchedNote.title}</div>
             <div className="noteView-body">{matchedNote.body}</div>
         </div>
