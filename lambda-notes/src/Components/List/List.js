7aeacc34cc85f0b3b8bba966d790d4b54884ef0f
CMVNK <Kopecky12112@gmail.com>
2018-05-22 01:42:17 -0700

    generated some dummydata so I can make cards a reusable component
    modified List component

--- lambda-notes/src/Components/List/List.js
+++ lambda-notes/src/Components/List/List.js
@@ -3,29 +3,48 @@ import Sidebar from '../Sidebar/Sidebar';
 import './List.css';
 import { Card, CardTitle, CardText } from 'reactstrap';
 import { Link } from 'react-router-dom'
+import DummyData from '../DummyData';
 
 class List extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {
+            DummyData: [],
+          }
     }
+
+    
     render() { 
         return (
             <div className = "list-wrapper">
                 <Sidebar />
+                
                 <div className = "card-container">
-                    <h1 className = "card-container-header">Your notes:</h1>
-                    <Card className="card">
-                        <CardTitle className = "card-title"><Link to = "/list/number">Special Title Treatment</Link></CardTitle>
+                    <h1 className = "card-container-header">Your notes:</h1><FakeData />
+
                     
-                        <CardText className = "card-body">With supporting text below as a natural lead-in to additional content.</CardText>
-                    </Card>
+                    
                     
                 </div>
                 
             </div>
           )
     }
+    
 }
+const FakeData = (props) => {
+    return(
+        //map here over cards from DummyData.js
+        <Card className="card">
+            <CardTitle className = "card-title"><Link to = "/list/number">{DummyData.note_title}</Link></CardTitle>
+        
+            <CardText className = "card-body">{DummyData.note_body}</CardText>
+        </Card>
+
+    
+    
+    )
+}
+
  
 export default List;
