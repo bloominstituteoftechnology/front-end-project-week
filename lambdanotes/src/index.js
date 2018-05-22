49b58f310a00b1fdbb58a904bafbc83340dcb7d7
Jorge Valdivia <emortalix@welcometoforever.io>
2018-05-22 15:23:09 -0400

    Notes.css styling

--- lambdanotes/src/index.js
+++ lambdanotes/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import Note from './components/Notes/Notes';
+import Notes from './components/Notes/Notes';
 import EditNote from './components/EditNote/EditNote';
 import CreateNewNote from './components/CreateNewNote/CreateNewNote';
 import registerServiceWorker from './registerServiceWorker';
