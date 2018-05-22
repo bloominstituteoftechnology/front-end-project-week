815bcd2d77d46b005eb01545635f49cf4b8705e1
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-05-22 13:45:18 -0700

    store created by notesReducer

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -5,12 +5,16 @@ import App from './App';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { notesReducer } from "./reducers";
 
+const store = createStore(notesReducer);
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
 
