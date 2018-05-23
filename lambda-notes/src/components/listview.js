40c1d93f00168b81a293c5495a1ee441893bcdbb
Tramane Hall <tramanedhall@gmail.com>
2018-05-23 15:39:58 -0400

    Coded ListView

---
+++ lambda-notes/src/components/listview.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Card, Button, CardImg, CardTitle, CardText, CardColumns,
+  CardSubtitle, CardBody } from 'reactstrap'; 
+import {cardComponent, notecardTitle, notecardContents, notecardLine} from './ListViewNotecard.css';
+import SideNav from './components/SideNav.js';
+
+const ListView = (props) => {
+    return (
+        <div>
+            <SideNav />
+                <CardColumns className="cardColumns">
+                    <Card className="cardComponent">
+                        <CardTitle className="notecardTitle">Card Title</CardTitle>
+                        <notecardLine />
+                        <CardText className="notecardContents">Incididunt ad eiusmod esse qui reprehenderit duis nostrud ullamco fugiat amet quis voluptate. Et fugiat adipisicing sit non mollit. Sunt pariatur aliquip id amet proident consequat do sunt in sunt adipisicing laborum aliqua. Ut irure voluptate laborum commodo laborum enim sint qui laborum adipisicing ad. Incididunt aliqua nisi eu culpa labore esse cillum. Nostrud irure est duis veniam est laborum reprehenderit reprehenderit nisi. Proident eiusmod veniam officia mollit exercitation dolore magna velit irure qui elit.</CardText>
+                    </Card>
+                </CardColumns>
+        
+                <CardColumns className="cardColumns">
+                    <Card className="cardComponent">
+                        <CardTitle className="notecardTitle">Card Title</CardTitle>
+                        <notecardLine />
+                        <CardText className="notecardContents">Incididunt ad eiusmod esse qui reprehenderit duis nostrud ullamco fugiat amet quis voluptate. Et fugiat adipisicing sit non mollit. Sunt pariatur aliquip id amet proident consequat do sunt in sunt adipisicing laborum aliqua. Ut irure voluptate laborum commodo laborum enim sint qui laborum adipisicing ad. Incididunt aliqua nisi eu culpa labore esse cillum. Nostrud irure est duis veniam est laborum reprehenderit reprehenderit nisi. Proident eiusmod veniam officia mollit exercitation dolore magna velit irure qui elit.</CardText>                    </Card>
+                </CardColumns>
+    
+                <CardColumns className="cardColumns">
+                    <Card className="cardComponent">
+                        <CardTitle className="notecardTitle">Card Title</CardTitle>
+                        <notecardLine />
+                        <CardText className="notecardContents">Incididunt ad eiusmod esse qui reprehenderit duis nostrud ullamco fugiat amet quis voluptate. Et fugiat adipisicing sit non mollit. Sunt pariatur aliquip id amet proident consequat do sunt in sunt adipisicing laborum aliqua. Ut irure voluptate laborum commodo laborum enim sint qui laborum adipisicing ad. Incididunt aliqua nisi eu culpa labore esse cillum. Nostrud irure est duis veniam est laborum reprehenderit reprehenderit nisi. Proident eiusmod veniam officia mollit exercitation dolore magna velit irure qui elit.</CardText>                    </Card>
+                </CardColumns>
+        
+                <CardColumns className="cardColumns">
+                    <Card className="cardComponent">
+                        <CardTitle className="notecardTitle">Card Title</CardTitle>
+                        <notecardLine />
+                        <CardText className="notecardContents">Incididunt ad eiusmod esse qui reprehenderit duis nostrud ullamco fugiat amet quis voluptate. Et fugiat adipisicing sit non mollit. Sunt pariatur aliquip id amet proident consequat do sunt in sunt adipisicing laborum aliqua. Ut irure voluptate laborum commodo laborum enim sint qui laborum adipisicing ad. Incididunt aliqua nisi eu culpa labore esse cillum. Nostrud irure est duis veniam est laborum reprehenderit reprehenderit nisi. Proident eiusmod veniam officia mollit exercitation dolore magna velit irure qui elit.</CardText>                    </Card>
+                </CardColumns>
+        
+                <CardColumns className="cardColumns">
+                    <Card className="cardComponent">
+                        <CardTitle className="notecardTitle">Card Title</CardTitle>
+                        <notecardLine />
+                        <CardText className="notecardContents">Incididunt ad eiusmod esse qui reprehenderit duis nostrud ullamco fugiat amet quis voluptate. Et fugiat adipisicing sit non mollit. Sunt pariatur aliquip id amet proident consequat do sunt in sunt adipisicing laborum aliqua. Ut irure voluptate laborum commodo laborum enim sint qui laborum adipisicing ad. Incididunt aliqua nisi eu culpa labore esse cillum. Nostrud irure est duis veniam est laborum reprehenderit reprehenderit nisi. Proident eiusmod veniam officia mollit exercitation dolore magna velit irure qui elit.</CardText>                    </Card>
+                </CardColumns>
+        
+                <CardColumns className="cardColumns">
+                    <Card className="cardComponent">
+                        <CardTitle className="notecardTitle">Card Title</CardTitle>
+                        <notecardLine />
+                        <CardText className="notecardContents">Incididunt ad eiusmod esse qui reprehenderit duis nostrud ullamco fugiat amet quis voluptate. Et fugiat adipisicing sit non mollit. Sunt pariatur aliquip id amet proident consequat do sunt in sunt adipisicing laborum aliqua. Ut irure voluptate laborum commodo laborum enim sint qui laborum adipisicing ad. Incididunt aliqua nisi eu culpa labore esse cillum. Nostrud irure est duis veniam est laborum reprehenderit reprehenderit nisi. Proident eiusmod veniam officia mollit exercitation dolore magna velit irure qui elit.</CardText>                    </Card>
+                </CardColumns>
+        
+                <CardColumns className="cardColumns">
+                    <Card className="cardComponent">
+                        <CardTitle className="notecardTitle">Card Title</CardTitle>
+                        <notecardLine />
+                        <CardText className="notecardContents">Incididunt ad eiusmod esse qui reprehenderit duis nostrud ullamco fugiat amet quis voluptate. Et fugiat adipisicing sit non mollit. Sunt pariatur aliquip id amet proident consequat do sunt in sunt adipisicing laborum aliqua. Ut irure voluptate laborum commodo laborum enim sint qui laborum adipisicing ad. Incididunt aliqua nisi eu culpa labore esse cillum. Nostrud irure est duis veniam est laborum reprehenderit reprehenderit nisi. Proident eiusmod veniam officia mollit exercitation dolore magna velit irure qui elit.</CardText>                    </Card>
+                </CardColumns>
+       
+                <CardColumns className="cardColumns">
+                    <Card className="cardComponent">
+                        <CardTitle className="notecardTitle">Card Title</CardTitle>
+                        <notecardLine />
+                        <CardText className="notecardContents">Incididunt ad eiusmod esse qui reprehenderit duis nostrud ullamco fugiat amet quis voluptate. Et fugiat adipisicing sit non mollit. Sunt pariatur aliquip id amet proident consequat do sunt in sunt adipisicing laborum aliqua. Ut irure voluptate laborum commodo laborum enim sint qui laborum adipisicing ad. Incididunt aliqua nisi eu culpa labore esse cillum. Nostrud irure est duis veniam est laborum reprehenderit reprehenderit nisi. Proident eiusmod veniam officia mollit exercitation dolore magna velit irure qui elit.</CardText>                    </Card>
+                </CardColumns>
+    
+                <CardColumns className="cardColumns">
+                    <Card className="cardComponent">
+                        <CardTitle className="notecardTitle">Card Title</CardTitle>
+                        <notecardLine />
+                        <CardText className="notecardContents">Incididunt ad eiusmod esse qui reprehenderit duis nostrud ullamco fugiat amet quis voluptate. Et fugiat adipisicing sit non mollit. Sunt pariatur aliquip id amet proident consequat do sunt in sunt adipisicing laborum aliqua. Ut irure voluptate laborum commodo laborum enim sint qui laborum adipisicing ad. Incididunt aliqua nisi eu culpa labore esse cillum. Nostrud irure est duis veniam est laborum reprehenderit reprehenderit nisi. Proident eiusmod veniam officia mollit exercitation dolore magna velit irure qui elit.</CardText>                    </Card>
+                </CardColumns>
+        </div>
+    );
+};
+
+export default ListView;
