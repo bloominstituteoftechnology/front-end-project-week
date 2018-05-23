35c62a462e84e58119c01733661e83c408e2fdfa
Bee C <b.choeung@gmail.com>
2018-05-23 13:11:05 -0400

    add note area

--- project/src/noteView.js
+++ project/src/noteView.js
@@ -5,16 +5,24 @@ const noteView = () => {
         <div>
             <container className='NVcontainer'>
                 <div className='NVsidebar'>
-                    <div className='heading-container'>
-                        <p className='heading-font'><strong>Lambda Notes</strong>
-                    </p>
-                    </div>
-                    
-                    <div>
-                        <button className='button'>View Your Notes</button>
+                    <div className='NVtitleContainer'>
+                        <p className='NVtitle'><strong>Lambda Notes</strong></p>
                     </div>
                     <div>
-                        <button className='DL button'>+ Create New Notes</button>
+                        <button className='NVbutton'>View Your Notes</button>
+                    </div>
+                    <div>
+                        <button className='NVbutton'>+ Create New Notes</button>
+                    </div>
+                </div>
+                <div className="NVnote">
+                    <div>
+                        <p>
+                            Lion munchkin. Kitty manx so lynx. Bobcat devonshire rex american bobtail. Cougar devonshire rex cougar singapura mouser or lion. Kitty bengal donskoy lynx but tomcat. Lynx havana brown bombay persian. Savannah devonshire rex sphynx so leopard yet kitty tomcat. Savannah tom yet siberian singapura and persian but leopard for ragdoll. Cornish rex bobcat or maine coon. Kitty himalayan lion and norwegian forest singapura yet kitten yet kitten. American shorthair. Cornish rex devonshire rex but savannah leopard american shorthair. Himalayan grimalkin for tabby maine coon balinese . Tom bengal or devonshire rex. American shorthair cougar and burmese and russian blue and burmese and bengal thai. Bombay devonshire rex british shorthair yet siamese norwegian forest lion. Donskoy tomcat russian blue. Havana brown leopard so tabby so thai lion. Burmese munchkin british shorthair. American shorthair jaguar american shorthair. Norwegian forest tom and grimalkin so bobcat or leopard and siberian so egyptian mau. Ragdoll sphynx or cheetah, tom. Puma birman but devonshire rex yet ocicat and norwegian forest bengal but balinese . Cornish rex tiger grimalkin. 
+                        </p>
+                        <p>
+                        Burmese himalayan but devonshire rex and american bobtail havana brown lynx yet manx. Birman jaguar american shorthair singapura munchkin bombay mouser. Jaguar tomcat yet tomcat. Lion balinese malkin. Tabby savannah so cornish rex ocicat so egyptian mau. Russian blue. Maine coon panther tomcat for tomcat yet cougar. Munchkin puma munchkin and tabby. Cornish rex grimalkin. Norwegian forest kitten. Abyssinian himalayan yet jaguar kitten kitten havana brown but tiger. Ragdoll scottish fold so american bobtail or lion maine coon donskoy. British shorthair. Ocelot tomcat for devonshire rex so siberian. 
+                        </p>
                     </div>
                 </div>
             </container>
