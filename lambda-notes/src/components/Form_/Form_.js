7b2503f26bf95f7daf2b0c8344bce60531f1ccba
jesuarva <jean.ariza@jesuarva.com>
2018-05-23 22:42:24 +0200

    PASS Form_ to a 'Controlled component'

--- lambda-notes/src/components/Form_/Form_.js
+++ lambda-notes/src/components/Form_/Form_.js
@@ -1,24 +1,48 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import {addingItem} from '../../actions/index';
 import Button_ from '../Button_/Button_';
 
 class Form_ extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      content: ''
+    }
+  }
+  
+
+  componentDidMount() {
+    const url = this.props.match.url;
+
+  }
+  handleInput = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  
   render() {
     return (
       <form className="custom-form" >
         <div className="form-group">
           <input
-            type="text"
             className="custom-input"
-            id="exampleFormControlInput1"
+            type="text"
+            name="title"
             placeholder="Note Title"
+            onChange={this.handleInput}
+            value={this.state.title}
           />
         </div>
         <div className="form-group">
           <textarea
             className="form-control custom-text-area"
-            id="exampleFormControlTextarea1"
+            name="content"
             rows="13"
             placeholder="Note Content"
+            onChange={this.handleInput}
+            value={this.state.content}
           />
         </div>
         <Button_ text="Save" />
@@ -26,5 +50,11 @@ class Form_ extends Component {
     );
   }
 }
-
-export default Form_;
+const mapStateToProps = state => {
+  return {
+    notes: state.data
+  };
+};
+export default withRouter(
+  connect(mapStateToProps, { addingItem })(Form_)
+);
