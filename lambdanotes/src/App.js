5af78920ab1bdeadfb194af95ef2db53919129de
FireInjun <Fireinjun291@gmail.com>
2018-05-23 08:38:55 -0400

    Lots of files changed
    and rearranged and added to, kind of  a housekeeping commit.

--- lambdanotes/src/App.js
+++ lambdanotes/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      <div className="App-Box">
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -13,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+      </div>
       </div>
     );
   }
