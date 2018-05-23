4ff53289aa7249fea6524357c0a5275545058535
Bee C <b.choeung@gmail.com>
2018-05-23 13:55:54 -0400

    completed Note View

--- project/src/noteView.js
+++ project/src/noteView.js
@@ -15,8 +15,16 @@ const noteView = () => {
                         <button className='NVbutton'>+ Create New Notes</button>
                     </div>
                 </div>
-                <div className="NVnote">
-                    <div>
+
+                <div className="NVnoteContainer">
+                    <div className="NVheader">
+                        <p className="NVedit">edit</p>
+                        <p className="NVdelete">delete</p>
+                    </div>
+                    <div className="NVnoteName">
+                        <p><strong>Note Name</strong></p>
+                    </div>
+                    <div className="NVparagraph">
                         <p>
                             Lion munchkin. Kitty manx so lynx. Bobcat devonshire rex american bobtail. Cougar devonshire rex cougar singapura mouser or lion. Kitty bengal donskoy lynx but tomcat. Lynx havana brown bombay persian. Savannah devonshire rex sphynx so leopard yet kitty tomcat. Savannah tom yet siberian singapura and persian but leopard for ragdoll. Cornish rex bobcat or maine coon. Kitty himalayan lion and norwegian forest singapura yet kitten yet kitten. American shorthair. Cornish rex devonshire rex but savannah leopard american shorthair. Himalayan grimalkin for tabby maine coon balinese . Tom bengal or devonshire rex. American shorthair cougar and burmese and russian blue and burmese and bengal thai. Bombay devonshire rex british shorthair yet siamese norwegian forest lion. Donskoy tomcat russian blue. Havana brown leopard so tabby so thai lion. Burmese munchkin british shorthair. American shorthair jaguar american shorthair. Norwegian forest tom and grimalkin so bobcat or leopard and siberian so egyptian mau. Ragdoll sphynx or cheetah, tom. Puma birman but devonshire rex yet ocicat and norwegian forest bengal but balinese . Cornish rex tiger grimalkin. 
                         </p>
