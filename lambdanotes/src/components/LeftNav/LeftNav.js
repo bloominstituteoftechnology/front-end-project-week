f875b9c7c2132005ddd8c786a39525dddecfe6d1
Moises Dobarganes <designerexpert@gmail.com>
2018-05-23 14:43:42 -0400

    Latest Styling Changes

--- lambdanotes/src/components/LeftNav/LeftNav.js
+++ lambdanotes/src/components/LeftNav/LeftNav.js
@@ -7,7 +7,7 @@ class LeftNav extends Component {
             <div className='navWrap'>
                 <h1 className='navHeader'>Lambda Notes</h1>
                 <button className='navButton'><h3>View Your Notes</h3></button>
-                <button className='navButton'><h3>Create New Note</h3></button>
+                <button className='navButton'><h3>+ Create New Note</h3></button>
             </div>
         );
     }
