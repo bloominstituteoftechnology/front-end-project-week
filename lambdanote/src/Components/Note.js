fddf567ac54e232e5ef0ac10327a1ed4554448f4
JSNavey <jsnavey@gmail.com>
2018-05-23 18:20:16 -0400

    notelist stlying but still in column

--- lambdanote/src/Components/Note.js
+++ lambdanote/src/Components/Note.js
@@ -2,18 +2,17 @@ import React from 'react';
 import { Card, CardBody, CardTitle, CardText } from 'reactstrap';
 import { Container, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import './Note.css';
 
 const Note = props => {
     return (    
-        <Card className = 'note'>
-            <CardBody>
-                <Link to={`/note/${props.note.id}`}>
-                <CardTitle>{props.note.title}</CardTitle>
-                </Link>
-                <hr />
-                <CardText>{props.note.text}</CardText>
-            </CardBody>
-        </Card>
+        <div className = 'note'>
+            <Link to={`/note/${props.note.id}`}>
+            <CardTitle>{props.note.title}</CardTitle>
+            </Link>
+            <hr />
+            <CardText>{props.note.text}</CardText>
+         </div>
     )
 }
 
--- lambdanote/src/Components/NoteList.js
+++ lambdanote/src/Components/NoteList.js
@@ -3,16 +3,15 @@ import Note from './Note';
 import { Link } from 'react-router-dom';
 import { Container, Row, Button } from 'reactstrap';
 
+
 const NoteList = (props) => {
     return (
          <div>
-            <div className='note-container'>
-            <h2>Your Notes:</h2>
-            <Container fluid>
-            <Row>
-                {props.notes.map(note => <Note key={note.id} note={note} />)}
-            </Row>
-            </Container>
+            <div >
+            <h3 className='notelist-header'>Your Notes:</h3>
+            <div>            
+                {props.notes.map(note => <Note key={note.id} note={note} />)}        
+            </div>
             </div>
         </div>
     )
