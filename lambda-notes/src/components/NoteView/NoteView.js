bf1bf132da3a69135ca0ff801ac68c7d738748f2
jesuarva <jean.ariza@jesuarva.com>
2018-05-23 21:53:29 +0200

    IMPLEMENT modal on delete.

--- lambda-notes/src/components/NoteView/NoteView.js
+++ lambda-notes/src/components/NoteView/NoteView.js
@@ -3,19 +3,39 @@ import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { updatingItem, deletingItem } from "../../actions/index";
 
+import { Modal, ModalHeader, ModalFooter, Button } from "reactstrap";
+
 class NoteView extends Component {
-  delete = (e) => {
-    const index = this.props.match.params.index;
-    const id = this.props.notes[index].id;
-    this.props.deletingItem(index, id);
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false
+    };
+
+    this.toggle = this.toggle.bind(this);
   }
 
-  render() {
-    // const id = this.props.match.params.id;
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
+  delete = e => {
     const index = this.props.match.params.index;
-    // const note = this.props.notes.filter(note => note.id == id)[0];
     const note = this.props.notes[index];
-    // console.log(note);
+    const id = note.id;
+    this.props.deletingItem(index, id);
+    this.toggle();
+    setTimeout(
+      () => {alert(`${note.title} deleted`)},
+      500
+    );
+  };
+
+  render() {
+    const index = this.props.match.params.index;
+    const note = this.props.notes[index];
     const content = note.content
       .split("\n")
       .map((paragraph, index) => <p key={index}>{paragraph}</p>);
@@ -23,11 +43,31 @@ class NoteView extends Component {
     return (
       <React.Fragment>
         <div className="d-flex position-absolute edition">
-          <div onClick={this.delete} >delete</div>
+          <div onClick={this.toggle}>delete</div>
           <div>edit</div>
         </div>
-        <h5 className="text-capitalize">{note.id}-{note.title}</h5>
+        <h5 className="text-capitalize">
+          {note.id}-{note.title}
+        </h5>
         <div className="">{content}</div>
+
+        <Modal
+          isOpen={this.state.modal}
+          toggle={this.toggle}
+          className={this.props.className}
+        >
+          <ModalHeader toggle={this.toggle}>
+            Are you sure you want to delete this?
+            <div className="d-fles">
+              <Button color="danger" onClick={this.delete}>
+                Delete
+              </Button>{" "}
+              <Button color="secondary" onClick={this.toggle}>
+                No
+              </Button>
+            </div>
+          </ModalHeader>
+        </Modal>
       </React.Fragment>
     );
   }
