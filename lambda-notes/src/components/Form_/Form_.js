e3dc1ddc07cd538901fcce140d507e746088f519
jesuarva <jean.ariza@jesuarva.com>
2018-05-23 22:24:49 +0200

    IMPLEMENT Form_ component

--- lambda-notes/src/components/Form_/Form_.js
+++ lambda-notes/src/components/Form_/Form_.js
@@ -1,11 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Button_ from '../Button_/Button_';
 
 class Form_ extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <form className="custom-form" >
+        <div className="form-group">
+          <input
+            type="text"
+            className="custom-input"
+            id="exampleFormControlInput1"
+            placeholder="Note Title"
+          />
+        </div>
+        <div className="form-group">
+          <textarea
+            className="form-control custom-text-area"
+            id="exampleFormControlTextarea1"
+            rows="13"
+            placeholder="Note Content"
+          />
+        </div>
+        <Button_ text="Save" />
+      </form>
     );
   }
 }
