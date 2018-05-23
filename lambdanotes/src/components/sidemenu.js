bb33ae8ab6da88b2091f712748e39f2c1e6fa92f
Zach Campbell <beazlebubbles@gmail.com>
2018-05-23 16:16:42 -0600

    Made all the side buttons conditional

--- lambdanotes/src/components/sidemenu.js
+++ lambdanotes/src/components/sidemenu.js
@@ -3,16 +3,19 @@ import './sidemenu.css';
 import LambdaButton from './lambdabutton.js';
 
 const SideMenu = (props) => {
+
   let buttonArray = [];
-  buttonArray.push(<LambdaButton text="View Your Notes" color="green" myFunc={props.listMethod} />);
-  buttonArray.push(<LambdaButton text="+ Create New Note" color="green" myFunc={props.createMethod} />);
+
+  if (props.appState !== "list") buttonArray.push(<LambdaButton text="View Your Notes" color="green" myFunc={props.listMethod} />);
+
+  if (props.appState !== "create") buttonArray.push(<LambdaButton text="+ Create New Note" color="green" myFunc={props.createMethod} />);
+
   if (props.appState === "list") buttonArray.push(<LambdaButton text="Alphabetize Notes" color="green" myFunc={props.alphabetizeNotes} />);
+
   return (
     <div className="side-menu">
       <h1 className="lambda-title">Lambda Notes</h1>
-
       {buttonArray}
-
     </div>
   );
 }
