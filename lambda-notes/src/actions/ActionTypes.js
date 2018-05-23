6fe7fe96921d3e63906e37420381ba9e13f3b7a8
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-23 15:27:09 -0400

    Determine action types

---
+++ lambda-notes/src/actions/ActionTypes.js
@@ -0,0 +1,11 @@
+export const FETCHING_NOTES = 'FETHCING_NOTES';
+export const NOTES_FETCHED = 'NOTES_FETCHED';
+export const FETCHING_NOTE = 'FETCHING_NOTE';
+export const NOTE_FETCHED = 'NOTE_FETCHED';
+export const ADDING_NOTE = 'ADDING_NOTE';
+export const NOTE_ADDED = 'NOTE_ADDED';
+export const UPDATING_NOTE = 'UPDATING_NOTE';
+export const NOTE_UPDATED = 'NOTE_UPDATED';
+export const DELETING_NOTE = 'DELETING_NOTE';
+export const NOTE_DELETED = 'NOTE_DELETED';
+export const ERROR = 'ERROR';
