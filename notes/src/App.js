a8de7f54664bc44cfcd4639549e2fea89525414a
april <martinezapril56@yahoo.com>
2018-05-23 13:36:45 -0400

    i am still trying

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,98 +1,14 @@
 import React, { Component } from 'react';
-import { Route } from "react-router-dom";
-import { Row, Col, Container, Input, Button } from "reactstrap";
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import NoteList from './components/NoteList';
-
-const placeHolder = "/";
-
-const initialNotes = [
-  {
-    id: 1,
-    title: "First Note",
-    content: placeHolder + placeHolder
-  },
-  {
-    id: 1,
-    title: "Second Note",
-    content: placeHolder + placeHolder
-  },
-  {
-    id: 1,
-    title: "Third Note",
-    content: placeHolder + placeHolder + placeHolder
-  },
-  {
-    id: 1,
-    title: "Fourth Note",
-    content: placeHolder + placeHolder + placeHolder + placeHolder
-  }
-];
-
+import Listview from './components/ListView';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      notes: initialNotes,
-    };
-  }
-
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          
-          <h1 className="App-title">Lambda Notes</h1>
-        </header>
-        <p className="App-intro">
-      
-        </p>
-        <Container>
-          <Col sm="3">
-            <Route path="/"
-              render={props => {
-                return <NoteList notes={this.state.notes} />;
-              }} />
-          </Col>
-          <Col sm="3">
-            <Route path="/"
-              render={props => {
-                return <NoteList notes={this.state.notes} />;
-              }} />
-          </Col>
-          <Col sm="3">
-            <Route path="/"
-              render={props => {
-                return <NoteList notes={this.state.notes} />;
-              }} />
-          </Col>
-          <Col sm="3">
-            <Route path="/"
-              render={props => {
-                return <NoteList notes={this.state.notes} />;
-              }} />
-          </Col>
-          <Col sm="3">
-            <Route path="/"
-              render={props => {
-                return <NoteList notes={this.state.notes} />;
-              }} />
-          </Col>
-          <Col sm="3">
-            <Route path="/"
-              render={props => {
-                return <NoteList notes={this.state.notes} />;
-              }} />
-          </Col>
-          <Col sm="3">
-            <Route path="/"
-              render={props => {
-                return <NoteList notes={this.state.notes} />;
-              }} />
-          </Col>
-        </Container>
+
+      <div className="container-fluid">
+        < Listview />
       </div>
     );
   }
--- notes/src/index.js
+++ notes/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
+import App from './App';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
-registerServiceWorker();
+
+
+
+render(
+    <Router>
+        <App />
+
+    </Router>,
+
+    document.getElementById('root')
+);
