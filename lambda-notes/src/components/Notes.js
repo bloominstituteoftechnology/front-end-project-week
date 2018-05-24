dd52df65c934974638df2de447db12fec14453b8
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-05-24 12:42:36 -0700

    export csv implemented

---
+++ lambda-notes/src/components/Notes.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import "../App.css";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+
+
+class Notes extends Component {
+  
+
+  render() {
+    return (
+
+    <div className="col-sm-9 cards">
+        <h4 className="your-notes">Your notes:</h4>
+        <div className='notes-list'>
+          {this.props.notes.map(note => {
+              return (
+              <Link to={`notes/${note.id}`} key={note.id}><div className="card mb-sm-4 col-sm-3" key={note.id}>
+                  <div className="card-head no-bg">
+                      <h5 className="d-sm-inline">#{note.id} {note.title}</h5>
+                  </div>
+                  <div className="list-group list-group-flush">
+                      <p className="mt-sm-2">
+                      {note.content}
+                      </p>
+                  </div>
+              </div></Link>
+              )
+          })}
+        </div>
+
+        
+      </div>
+    )
+  }
+};
+
+const mapStateToProps = state => {
+  return {
+    notes: state
+  };
+};
+
+
+export default connect(mapStateToProps, { })(Notes);
