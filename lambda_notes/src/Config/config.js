2751156b8cefb488ce896d0a9c755e9f5d7fc4fd
Herbert Hodgson <herberthodgson@hotmail.com>
2018-05-24 19:29:07 -0400

    Delete config.js

--- lambda_notes/src/Config/config.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-  apiKey: "AIzaSyBhP5cwYA1QQTp0q8L-TwooK7xNY4BfqSk",
-  authDomain: "lambda-notes-f389f.firebaseapp.com",
-  databaseURL: "https://lambda-notes-f389f.firebaseio.com",
-  projectId: "lambda-notes-f389f",
-  storageBucket: "lambda-notes-f389f.appspot.com",
-  messagingSenderId: "430000687844"
-};
