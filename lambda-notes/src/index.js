b0768615a48cd5fec788c2814cc6e9c29b867676
udykas <udykas@gmail.com>
2018-05-24 14:08:17 -0400

    Started set up for redux

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -5,6 +5,14 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
 ReactDOM.render(
     <Router>
         <App />
---
+++ lambda-notes/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import { noteReducer } from './NoteReducer.js';
+
+export default combineReducers({
+  noteReducer
+});
