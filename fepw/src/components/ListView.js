d709a7b17852e3a6bf37aedf6d0ed766e577d96e
kelly <kelfro@fastmail.com>
2018-05-24 13:35:12 -0700

    MVP with styling done
    I finally figured out how to get rid of the text decoration on hover in my list view. The only thing I couldn't get was removing the border radius from the delete modal, but that is not a big thing so I think I'm just going to let it go. Going to look at some of the stretch items to see if any of them are doable in a day.

--- fepw/src/components/ListView.js
+++ fepw/src/components/ListView.js
@@ -12,7 +12,7 @@ class ListView extends Component {
 				</div>
 				<br />
 				<div className='cards'>
-					{ this.props.cards.map( (card, index) => <div><Link to='/NoteView'><NoteCard className='eachcard' key={index} updateSelectedNotecard={this.props.updateSelectedNotecard} { ...card} /><br /></Link></div>)}
+					{ this.props.cards.map( (card, index) => <div><Link to='/NoteView' style={{ textDecoration: 'none' }}><NoteCard className='eachcard' key={index} updateSelectedNotecard={this.props.updateSelectedNotecard} { ...card} /><br /></Link></div>)}
 				</div>
 			</div>
 		)
@@ -22,4 +22,5 @@ class ListView extends Component {
 export default ListView;
 
 // 21May - Yeah, I have no idea what I'm doing with this yet. At some point we have to map through all the cards and display them but I'm not sure if I'll end up doing that in this component or elsewhere. It's here for right now.
-// 22May - Right now I'm using this to display a list of all my notecards. I suppose that eventually we want that list to display more as blocks rather than a vertical list but one thing at a time.
+	// 22May - Right now I'm using this to display a list of all my notecards. I suppose that eventually we want that list to display more as blocks rather than a vertical list but one thing at a time.
+	// 24May - Figured out how to get rid of text decoration whenever I hover over a card - you have to style the link because that is where the 'a' element is hiding.
