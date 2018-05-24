1e3fb70172fbedf49e9a5836434875c93f146126
Bee C <b.choeung@gmail.com>
2018-05-24 16:02:23 -0400

    added clickable links from buttons on home page

--- project/src/displayList.js
+++ project/src/displayList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import { Card, CardText, CardTitle, CardBody, Table } from 'reactstrap';
   
 const catIpsum = 'Plop down in the middle where everybody walks play time, and run in circles, or eat too much then proceed to regurgitate all over living room carpet while humans eat dinner yet pounce on unsuspecting person and drool or stare out the window. Lick master\'s hand at first then bite because im moody touch water with paw then recoil in horror, scratch me there, elevator butt stare out the window ignore the human until she needs to get up, then climb on her lap and sprawl walk on car leaving trail of paw prints on hood and windshield. With tail in the air. Nap all day relentlessly pursues moth lick arm hair so lie in the sink all day. Mice kitty kitty that box? i can fit in that box.';
@@ -15,10 +16,14 @@ class displayList extends Component {
                         </div>
                         
                         <div>
-                            <button className='button'>View Your Notes</button>
+                            <button className='button'>
+                                <NavLink className='button' to='/noteView'>View Your Notes</NavLink>
+                            </button>
                         </div>
                         <div>
-                            <button className='DL button'>+ Create New Notes</button>
+                            <button className='DL button'>
+                                <NavLink className='button' to='/editView'>+ Create New Notes</NavLink>
+                            </button>
                         </div>
                     </div>
 
