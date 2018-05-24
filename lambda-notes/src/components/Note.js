9311758b15a4e1e237db988481374974540d8ad4
Chris Honda <christopher.honda92@gmail.com>
2018-05-23 22:01:20 -0700

    Also imported App.css into all components;

--- lambda-notes/src/components/Note.js
+++ lambda-notes/src/components/Note.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Container, Card, CardBody, CardTitle, CardText, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import './App.css';
+
 
 const Note = props => {
     return (
