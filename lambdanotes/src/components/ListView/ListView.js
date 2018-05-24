cacf30b584fae1a1f61dc0b6ce33a90363474386
Bradiowave <divebombwalker@gmail.com>
2018-05-24 15:49:03 -0700

    added markdown support to listView

--- lambdanotes/src/components/ListView/ListView.js
+++ lambdanotes/src/components/ListView/ListView.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './ListView.css';
+import marked from 'marked';
 
-const ListView = (props) => {
-    return (
-        <div className='listViewContainer'>
-            <h3 className='listViewHeader'>Your Notes:</h3>
-            <div className='noteCardsCollection'>
-                {props.notesList.map(note => (
-                    <div onClick={() => props.setCurrentNote(note._id)} key={note._id} className='entireNoteCardLink'>
-                        <Link to={`/${note._id}`} className='noteCardLink'>
-                            <div className='noteCard'>
-                                <h6 className='noteTitle'>{note.title}</h6>
-                                <span className='noteContent'>{note.textBody}</span>
-                            </div>
-                        </Link>
-                    </div>
-                ))}
+class ListView extends Component {
 
+    getMarkdownText(text) {
+        var rawMarkup = marked(`${text}`, {sanitize: true});
+        return { __html: rawMarkup };
+    }
+
+    render() {
+        return (
+            <div className='listViewContainer'>
+                <h3 className='listViewHeader'>Your Notes:</h3>
+                <div className='noteCardsCollection'>
+                    {this.props.notesList.map(note => (
+                        <div onClick={() => this.props.setCurrentNote(note._id)} key={note._id} className='entireNoteCardLink'>
+                            <Link to={`/${note._id}`} className='noteCardLink'>
+                                <div className='noteCard'>
+                                    <h6 dangerouslySetInnerHTML={this.getMarkdownText(note.title)} className='noteTitle'></h6>
+                                    <span dangerouslySetInnerHTML={this.getMarkdownText(note.textBody)} className='noteContent'></span>
+                                </div>
+                            </Link>
+                        </div>
+                    ))}
+
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default ListView;
