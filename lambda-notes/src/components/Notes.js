9a67d8a512e9eb1a4c1c73a604ec45857211d9e5
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-05-24 16:32:14 -0700

    dragging works

--- lambda-notes/src/components/Notes.js
+++ lambda-notes/src/components/Notes.js
@@ -8,30 +8,25 @@ class Notes extends Component {
   
 
   render() {
-    return (
-
-    <div className="col-sm-9 cards">
+    return <div className="col-sm-9 cards">
         <h4 className="your-notes">Your notes:</h4>
-        <div className='notes-list'>
+        <div className="notes-list" id="sortable">
           {this.props.notes.map(note => {
-              return (
-              <Link to={`notes/${note.id}`} key={note.id}><div className="card mb-sm-4 col-sm-3" key={note.id}>
+            return <Link to={`notes/${note.id}`} key={note.id}>
+                <div className="card mb-sm-4 col-sm-3 ui-state-default" key={note.id}>
                   <div className="card-head no-bg">
-                      <h5 className="d-sm-inline">#{note.id} {note.title}</h5>
+                    <h5 className="d-sm-inline">
+                      #{note.id} {note.title}
+                    </h5>
                   </div>
                   <div className="list-group list-group-flush">
-                      <p className="mt-sm-2">
-                      {note.content}
-                      </p>
+                    <p className="mt-sm-2">{note.content}</p>
                   </div>
-              </div></Link>
-              )
-          })}
+                </div>
+              </Link>;
+          }).reverse()}
         </div>
-
-        
-      </div>
-    )
+      </div>;
   }
 };
 
