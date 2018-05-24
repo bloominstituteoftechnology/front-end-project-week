56d5f30068b726cde04963fbaa13e16805395000
Bradiowave <divebombwalker@gmail.com>
2018-05-24 15:26:03 -0700

    added markdown support for noteView

--- lambdanotes/src/components/ViewNote/ViewNote.js
+++ lambdanotes/src/components/ViewNote/ViewNote.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './ViewNote.css';
 import { Link } from 'react-router-dom';
 import DeleteNote from '../DeleteNote/DeleteNote.js';
+import marked from 'marked';
 
-const ViewNote = (props) => {
-    return (
-        <div>
-            <div className='optionsContainer'>
-                <div className='entireEditLink'>
-                    <Link to={`/${props.currentNote._id}/edit`}>
-                        <p className='edit'>edit</p>
-                    </Link>
+class ViewNote extends Component {
+
+    getMarkdownText(text) {
+        var rawMarkup = marked(`${text}`, {sanitize: true});
+        return { __html: rawMarkup };
+    }
+
+    render() {
+        return (
+            <div>
+                <div className='optionsContainer'>
+                    <div className='entireEditLink'>
+                        <Link to={`/${this.props.currentNote._id}/edit`}>
+                            <p className='edit'>edit</p>
+                        </Link>
+                    </div>
+                    <DeleteNote currentNoteID={this.props.currentNote._id} />
                 </div>
-                <DeleteNote currentNoteID={props.currentNote._id} />
+                <div className='entireNote'>
+                    <h3 dangerouslySetInnerHTML={this.getMarkdownText(this.props.currentNote.title)} className='viewNoteHeader'>{}</h3>
+                    <span dangerouslySetInnerHTML={this.getMarkdownText(this.props.currentNote.textBody)} className='viewNoteContent'></span>
+                </div>
             </div>
-            <div className='entireNote'>
-                <h3 className='viewNoteHeader'>{props.currentNote.title}</h3>
-                <span className='viewNoteContent'>{props.currentNote.textBody}</span>
-            </div>
-        </div>
-                
-    )
+        )
+    }
 }
 
 export default ViewNote;
