5a94c4c1b3ad33941151a58f775b06fbbf928e66
JSNavey <jsnavey@gmail.com>
2018-05-24 18:30:24 -0400

    fixed the createForm component to be aboe to add content
    still cannot edit or delete

--- lambdanote/src/Components/CreateForm.js
+++ lambdanote/src/Components/CreateForm.js
@@ -7,7 +7,7 @@ export default class CreateForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            id: '',
+            id: Date.now(),
             title: '',
             text: ''
         }    
