7ca64e14979e69714a47c6a99988f09237055b8b
AmyShackles <amyshackles@gmail.com>
2018-05-24 13:32:08 -0700

    Clicking on individual notes works now.

--- front-end-project/src/components/NoteView.js
+++ front-end-project/src/components/NoteView.js
@@ -5,6 +5,8 @@ import { connect } from 'react-redux'
 import '../styles/NoteView.css'
 
 const NoteView = props => {
+const individualNote = props.notes.filter(note => { return note.id.toString() === props.match.params.id.toString()})
+console.log(individualNote[0].title)
     return (
         <div>
             {props.deleting ?
@@ -20,16 +22,12 @@ const NoteView = props => {
                     <NavLink to="/edit">edit</NavLink>
                     <span onClick={props.toggleModal} className="delete"> delete </span></div>
                 <div className="row yourNotes" onClick={props.toggleModalBack}>
-                    <h3 style={{ textAlign: "left" }} className="noteName"> Note Name </h3>
+                    
+                    <h3 style={{ textAlign: "left" }} className="noteName"> {individualNote[0].title.title} </h3>
                 </div>
                 <div onClick={props.toggleModalBack}>
                     <div className="note">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipscing elit.  Mauris egestas mauris sed nibh vulpatate, ac pharetra lacus aliquam.  Duis malesuada justo a iaculis sagittis.  Vestibulum ultrices ultricies arcu sit amet pharetra.  Duis et lectus arcu.  Morbi ornare dignissim dui, eu accumsan sapien lacinia et.  Maecenas ultricies, ante in accumsan tempor, tortor diam vulputate elit, id finibus enim magna in massa. Suspendisse vel accumsan nisi.  Vivamus elementum auctor ligula, at tempor nisl rutrum in.  Sed in eros luctus ligula porta efficitur eu non nibh.  Aliquam tellus ipsum, semper id cursus vel, posuere in dolor.  Phasellus maximus lacinia dolor eget laoreet.
-                    </p>
-                        <p>
-                            Morbi pellentesque euismod venenatis.  Nulla ut nibh nunc.  Phasellus diam metus, blandit ac purus a, efficitur mollis velit.  ut egestas, ante at lobortis ullamcorper, neque odio fringilla odio, non mattis elit lacus ut velit.  Sed feugiat nibh vel molestie sollicitudin.  Duis tincidunt portitor sem, sit amet ultricies lacus pelientesque vel.  Aenean quis enim placerat, posuere orci ac, condimentum tellus.  Vivamus vitae sodales quam, eget ultricies lorem.  Nam pellenteque massa nisl, at pellentesque nisi faucibus vitae.  Curabitur sit amet turpis quam.  Duis eget metus elementum, solicitudin dui sed, accumsan dui.  Donec ut est orci.  Nunc fringilla purus sit amet posuere volutpat.  Fusce vitae lectus is neque facilisis laoreet eget non odio.  Praesent sed mauris porta, volutpat ante hendrerit, ultrices nisl.
-                </p>
+                        <p>{individualNote[0].title.content}</p>
                     </div>
                 </div>
             </div>
