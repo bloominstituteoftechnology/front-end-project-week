2170c5c72f2ef88d69b5e59fc59e871be0d5660f
Matthias Bakken <matthias.bakken@gmail.com>
2018-05-25 05:42:44 +0700

    added drag and drop sort functionality

--- lambda-notes/src/components/NoteList.js
+++ lambda-notes/src/components/NoteList.js
@@ -11,14 +11,14 @@ class NoteList extends Component {
     render() {
         console.log(this.props.notes)
         return (
-            <div className="notelist">
+            <div className="notelist" id="sortable">
                 <h1>Your Notes:</h1>
                 {this.props.notes.map(note => {
                     return (
                         <Link 
                         style={{textDecoration: "none", color: "black"}} 
                         key={note.id} to={`/note/${note.id}`} 
-                        className="note-link note">
+                            className="note-link note ui-state-default">
                         <div>
                             <h4>{note.title}</h4>
                             <p>{note.body}</p>
