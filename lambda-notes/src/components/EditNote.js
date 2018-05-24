772b729b01371b2d0c3d9bdd71184f0ae46c538a
Chris Honda <christopher.honda92@gmail.com>
2018-05-24 10:38:15 -0700

    Changed EditNote to stateful component and added axios

--- lambda-notes/src/components/EditNote.js
+++ lambda-notes/src/components/EditNote.js
@@ -1,41 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import '../App.css';
 
 
-const EditNote = () => {
-    return (
-        <div className='edit-note-container'>
-            <div>
-                <h4 className='edit-note-header'>
-                    Edit Note:
-                </h4>
-            </div>
-            <form className='edit-note-form'>
-                <input 
-                    type='text'
-                    className='edit-note-title'
-                    name='title'
-                    placeholder='Note Title'
-                />
-                <textarea
-                    type='text'
-                    className='edit-note-content'
-                    name='content'
-                    placeholder='Note Content'
-                ></textarea>
-                <div className='link'>
-                    <Link to='/note'>
-                        <input
-                            className='edit-note-save-btn'
-                            type='button'
-                            value='Update'
-                        />
-                    </Link>
+import './EditNote.css';
+
+class EditNote extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: props.currentNote.title,
+            textBody: props.currentNote.textBody
+        }
+    }
+
+    handleTitleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    handleUpdateNote = () => {
+        console.log('handleUpdateNote Fired')
+        axios
+            .put(`https://killer-notes.herokuapp.com/note/edit/${this.props.currentNote._id}`, this.state)
+                .then(res => {
+                    console.log(res);
+                    window.location.reload();                
+                })
+                .catch(err => {console.log(err)})
+    }
+
+    render() {
+        return (
+            <div className='editNoteContainer'>
+                <div>
+                    <h3 className='editNoteHeader'>Edit Note:</h3>
                 </div>
-            </form>
-        </div>
-    )
+                <form className='editNoteForm'>
+                    <input 
+                        onChange={this.handleTitleChange} 
+                        type='text' 
+                        className='edit-note-title' 
+                        name='title'
+                        value={this.state.title}
+                        placeholder='Note Title'
+                    />
+                    <textarea
+                        onChange={this.handleTitleChange}
+                        type='text' 
+                        className='edit-note-content' 
+                        name='textBody'
+                        value={this.state.textBody}
+                        placeholder='Note Content'>
+                    </textarea>
+                    <div className='save-btn-link'>
+                        <Link to={`/`}>
+                            <input onClick={this.handleUpdateNote} className='editNoteSaveButton' type='button' value='Update' />
+                        </Link>
+                    </div>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default EditNote;
