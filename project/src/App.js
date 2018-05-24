4219eae06420a60ba9a779dc586fdcf8b9cdfc63
Bee C <b.choeung@gmail.com>
2018-05-24 11:59:23 -0400

    all paths are correctly displaying

--- project/src/App.js
+++ project/src/App.js
@@ -7,20 +7,24 @@ import './editView.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 
-// import displayList from './displayList';
-// import createNew from './createNew';
-// import noteView from './noteView';
+import displayList from './displayList';
+import createNew from './createNew';
+import noteView from './noteView';
 import editView from './editView';
+import Error from './Error';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Router>
-          {/*<Route exact path="/displayList" component={displayList} />*/}
-          {/*<Route path="/createNew" component={createNew} />*/}
-          {/*<Route path="/noteView" component={noteView} />*/}
-          <Route path="/" component={editView} />
+          <div>
+            <Route exact path="/" component={displayList} />
+            <Route path="/createNew" component={createNew} />
+            <Route path="/noteView" component={noteView} />
+            <Route path="/editView" component={editView} />
+            <Route component={Error} />          
+            </div>
         </Router>
       </div>
     );
