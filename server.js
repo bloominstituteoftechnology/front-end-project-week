1042271271d61a34b78a54743fba26a543b66365
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-05-25 12:11:45 -0700

    create backend with leveldb

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const port = 5000;
 const app = express();
+const db = require('./db.js')
 
 let nextId = 10;
 
@@ -68,54 +69,50 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/notes', (req, res) => {
-  setTimeout(() => {
-    res.send(notes);
-  }, 1000);
+  db.list((data)=>{
+    nextId = data.length;
+    res.send(data);
+  })
+
 });
 
 app.get('/note/:id', (req, res) => {
-  const note = notes.find(f => f.id == req.params.id);
+  db.get(req.params.id, (err, data)=>{
+    if (data && !err) {
+      res.status(200).json(data);
+    } else {
+      res.status(404).send({ msg: 'Note not found' });
+    }
+  })
 
-  if (note) {
-    res.status(200).json(note);
-  } else {
-    res.status(404).send({ msg: 'Note not found' });
-  }
 });
 
 app.post('/notes', (req, res) => {
   const note = { id: getNextId(), ...req.body };
-
-  notes = [...notes, note];
-
-  res.send(notes);
+  db.put(note, ()=>{
+    db.list((data)=>{
+      nextId = data.length;
+      res.send(data);
+    })
+  })
 });
 
 app.put('/note/:id', (req, res) => {
-  const { id } = req.params;
-
-  const noteIndex = notes.findIndex(f => f.id == id);
-
-  if (noteIndex > -1) {
-    const note = { ...notes[noteIndex], ...req.body };
-
-    notes = [
-      ...notes.slice(0, noteIndex),
-      note,
-      ...notes.slice(noteIndex + 1),
-    ];
-    res.send(notes);
-  } else {
-    res.status(404).send({ msg: 'Note not found' });
-  }
+  const note = { id: getNextId(), ...req.body };
+  db.put(note, ()=>{
+    db.list((data)=>{
+      nextId = data.length;
+      res.send(data);
+    })
+  })
 });
 
 app.delete('/note/:id', (req, res) => {
   const { id } = req.params;
 
-  notes = notes.filter(f => f.id !== Number(id));
-
-  res.send(notes);
+  db.del(id, ()=>{
+    res.status(200).send();
+  })
 });
 
 function getNextId() {
