0ba1fdaea1ceccac2edbe04bff557f4c0950d9ac
Brandon Hopper <BrandonHopper0@gmail.com>
2018-05-25 10:00:46 -0400

    passed attribute to style download button with reactstrap

---
+++ lambda/src/LambdaPages/ExcelNote.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import ReactExport from "react-data-export";
+
+import { Link } from 'react-router-dom';
+import { Button } from 'reactstrap';
+
+const ExcelFile = ReactExport.ExcelFile;
+const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
+const ExcelColumn = ReactExport.ExcelFile.ExcelColumn;
+
+const dataSet1 = [
+    {
+        note: "Note 1",
+        item: 30001,
+        completed: true
+    },
+    {
+        note: "Note 3",
+        item: 30002,
+        completed: true
+    },
+    {
+        note: "Note 3",
+        item: 30003,
+        completed: true
+    },
+    {
+        note: "Note 4",
+        item: 30004,
+        completed: true
+    }
+];
+
+
+class ExcelNote extends Component {
+    render() {
+        return (
+            <ExcelFile element={<Button color="info" >Download Data</Button>}>
+                <ExcelSheet data={dataSet1} name="Notes">
+                    <ExcelColumn label="Note" value="note"/>
+                    <ExcelColumn label="Item" value="amount"/>
+                    <ExcelColumn label="Completed"
+                                 value={(col) => col.is_completed ? "Completed" : ""}/>
+                </ExcelSheet>
+            </ExcelFile>
+        );
+    }
+}
+
+export default ExcelNote;
--- lambda/src/components/Nav.js
+++ lambda/src/components/Nav.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 //importing Nav CSS
 import './Nav.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ExcelNote from '../LambdaPages/ExcelNote';
 
 //statless componet for Navbar, which will be exported to App.js to render NavBar
 const NavBar = () => {
@@ -14,7 +15,8 @@ const NavBar = () => {
           <div className="Nav">
             <h1>Lambda Notes</h1>
             <Link to="/"><Button className="button" color="info">View Your Note</Button>{' '}</Link>
-            <Link to="/note"> <Button className="button1" color="info">+Create New Note</Button>{' '}</Link>  
+            <Link to="/note"> <Button className="button1" color="info">+Create New Note</Button>{' '}</Link> 
+            <ExcelNote/> 
           </div>
             
     );
