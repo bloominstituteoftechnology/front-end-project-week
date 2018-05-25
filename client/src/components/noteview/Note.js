d279a20dcd87b0971b1f3a52180f4d8d587f01e2
bbenefield89 <bsquared18@gmail.com>
2018-05-25 12:15:26 -0400

    feat(App): now holds `showDeleteModal` state. This is a boolean that determines if the delete modal is visible or not feat(Note): contains the delete modal style(Note): css

--- client/src/components/noteview/Note.js
+++ client/src/components/noteview/Note.js
@@ -10,37 +10,50 @@ import './Note.css';
 const Note = props => {
   if (props.match) {
     return (
-      props.noteList.filter(note => {
-        return note.id === props.match.params.id;
-      })
-        .map(note => {
-          const { id, title, content } = note;
-          return (
-            <div key={ Date.now() } className='note-wrapper'>
-              <div className='note-wrapper__note-action-wrapper'>
-                <Link to={ `edit/${ id }` } className='note-action-wrapper__note-edit-link'>
-                  <Button
-                    buttonClassName='note-edit-link__edit-button'
-                    buttonContent={ 'Edit' }
-                  />
-                </Link>
-
-                <Link to='/'>
-                  <Button
-                    buttonClassName='note-action-wrapper__delete-button'
-                    buttonContent='Delete'
-                    buttonOnClick={ () => props.handleDeleteNote(id) }
-                  />
-                </Link>
-              </div>
-              
-              <div className='note-wrapper__note-content-wrapper'>
-                <h2 className='note-content-wrapper__h2'>{ title }</h2>
-                <p className='note-content-wrapper__p'>{ content }</p>
-              </div>
-            </div>
-          )
-        })
+      <React.Fragment>
+        {
+          props.noteList.filter(note => {
+            return note.id === props.match.params.id;
+          })
+            .map(note => {
+              const { id, title, content } = note;
+              return (
+                <div key={ Date.now() } className='note-wrapper'>
+                  <div className='note-wrapper__note-action-wrapper'>
+                    <Link to={ `edit/${ id }` } className='note-action-wrapper__note-edit-link'>
+                      <Button
+                        buttonClassName='note-edit-link__edit-button'
+                        buttonContent={ 'Edit' }
+                      />
+                    </Link>
+                    
+                      <Button
+                        buttonClassName='note-action-wrapper__delete-button'
+                        buttonContent='Delete'
+                        buttonOnClick={ () => props.setShowDeleteModal() }
+                      />
+                  </div>
+                  <div className='note-wrapper__note-content-wrapper'>
+                    <h2 className='note-content-wrapper__h2'>{ title }</h2>
+                    <p className='note-content-wrapper__p'>{ content }</p>
+                  </div>
+                </div>
+              )
+            })
+        }
+        
+        <div className={'note-action-wrapper__delete-modal-wrapper' + (props.showDeleteModal ? ' note-action-wrapper__delete-modal-wrapper--show' : ' null')}>
+          <div>
+            <h2>Are you sure you want to delete this?</h2>
+          </div>
+          <div className='delete-modal-wrapper__button-wrapper'>
+            <Link className='button-wrapper__delete-note-link' to='/'>
+              <button className='delete-note-link__button' onClick={ () => props.handleDeleteNote(props.noteList[0].id) }>DELETE</button>
+            </Link>
+            <button className='button-wrapper__no-button' onClick={ () => props.setShowDeleteModal() }>No</button>
+          </div>
+        </div>
+      </React.Fragment>
     )
   }
   
