3facd42415f3fa1b6c8b986d8445f5af402ec2f3
Sam Park <sam.park.email@gmail.com>
2018-05-25 11:46:33 -0400

    Added clickable complete task button to NoteList

--- lambda-notes/src/Components/NoteCard.js
+++ lambda-notes/src/Components/NoteCard.js
@@ -1,35 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Card, CardBody, CardTitle, CardText, CardHeader } from "reactstrap";
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardText,
+  CardHeader,
+  Input
+} from "reactstrap";
 import { connect } from "react-redux";
 import "./NoteCss.css";
 
-const NoteCard = props => {
-  console.log("NoteCard");
+class NoteCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      completed: ""
+    };
+  }
 
-  return (
-    <Link to="/Note" style={{ textDecoration: "none", color: "black" }}>
-      <Card className="noteCard">
-        <CardBody>
-          <CardHeader style={{ padding: "0px", backgroundColor: "white" }}>
-            <CardTitle style={{ fontFamily: "Roboto", fontWeight: "bold" }}>
-              Note Title
-            </CardTitle>
-          </CardHeader>
-          <CardText
-            style={{
-              paddingTop: "10px",
-              fontFamily: "Raleway",
-              fontWeight: "medium"
-            }}
-          >
-            Basketball ipsum dolor sit amet free throw basketball basket bank
-            shot, outlet power forward flop zone defense Duncan. ...
-          </CardText>
-        </CardBody>
-      </Card>
-    </Link>
-  );
-};
+  completeHandler = () => {
+    if (this.state.completed === "") {
+      this.setState({ completed: "line-through" });
+    } else if (this.state.completed === "line-through") {
+      this.setState({ completed: "" });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+          <Card className="noteCard">
+            <CardBody>
+        <Input type="checkbox" onClick={this.completeHandler} style={{ marginLeft: '-15px'}} />
+        <Link to="/Note" style={{ textDecoration: "none", color: "black" }}>
+              <CardHeader style={{ padding: "0px", backgroundColor: "white" }}>
+                <CardTitle
+                  style={{
+                    fontFamily: "Roboto",
+                    fontWeight: "bold",
+                    textDecoration: `${this.state.completed}`
+                  }}
+                >
+                  Note Title
+                </CardTitle>
+              </CardHeader>
+              <CardText
+                style={{
+                  textDecoration: `${this.state.completed}`,
+                  paddingTop: "10px",
+                  fontFamily: "Raleway",
+                  fontWeight: "medium"
+                }}
+              >
+                Basketball ipsum dolor sit amet free throw basketball basket
+                bank shot, outlet power forward flop zone defense Duncan. ...
+              </CardText>
+        </Link>
+            </CardBody>
+          </Card>
+      </div>
+    );
+  }
+}
 
 export default NoteCard;
