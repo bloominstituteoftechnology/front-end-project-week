d24001ce3ac56b7f8071e3f0c5a606648f1f0afd
bbenefield89 <bsquared18@gmail.com>
2018-05-25 16:06:24 -0400

    feat(NoteView): conditional render checks if `noteList` is an empty array, if so render h1 text

--- client/src/components/noteview/NoteView.js
+++ client/src/components/noteview/NoteView.js
@@ -7,22 +7,29 @@ import Note from './Note';
 import './NoteView.css';
 
 const NoteView = ({ noteList, setSelectedNote }) => {
+  if (noteList.length > 0) {
+    return (
+      <div className='noteview__note-container'>
+        {
+          noteList.map(note => {
+            const { id, title, content } = note;
+            return (
+              <Note
+                key={ id }
+                ID={ id }
+                title={ title }
+                content={ content }
+                setSelectedNote={ setSelectedNote }
+              />
+            )
+          })
+        }
+      </div>
+    )
+  }
   return (
-    <div className='noteview__note-container'>
-      {
-        noteList.map(note => {
-          const { id, title, content } = note;
-          return (
-            <Note
-              key={ id }
-              ID={ id }
-              title={ title }
-              content={ content }
-              setSelectedNote={ setSelectedNote }
-            />
-          )
-        })
-      }
+    <div className='note-container-empty'>
+      <h1>Congratulations, you're up to date with all of your tasks!</h1>
     </div>
   )
 }
