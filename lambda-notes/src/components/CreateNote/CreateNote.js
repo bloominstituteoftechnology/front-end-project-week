900b5c109c6af5d11aa19bcb0ea483cf002ae442
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-05-24 20:54:09 -0400

    I had set a maximum limit of characters for the note title and the note content. I also implemented a popover component from reactstrap to alert users when they have reached the character limits.

--- lambda-notes/src/components/CreateNote/CreateNote.js
+++ lambda-notes/src/components/CreateNote/CreateNote.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Popover, PopoverBody } from "reactstrap";
+import { Redirect } from "react-router-dom";
 import "./CreateNote.css";
 import Navigation from "../Navigation/Navigation";
 
@@ -8,12 +9,40 @@ class CreateNote extends Component {
         super(props);
         this.state = {
             title: "",
-            text: ""
+            text: "",
+            redirect: false,
+            titleLimit: 20,
+            textLimit: 1000
         }
+        this.titlePopOverOpen = false;
+        this.textPopOverOpen = false;
+    }
+
+
+    handleOnSubmit = () => {
+        this.props.addNote({ title: this.state.title, text: this.state.text })
+        this.setState({ redirect: true })
     }
 
     handleOnChange = (event) => {
         this.setState({ [event.target.name]: event.target.value })
+    }
+
+    toggleTitleLimit = () => {
+        this.titlePopOverOpen = !this.titlePopOverOpen;
+    }
+
+    toggleTitleOut = () => {
+        this.setState({ titleLimit: null })
+        this.titlePopOverOpen = false;
+    }
+
+    toggleTextLimit = () => {
+        this.textPopOverOpen = !this.textPopOverOpen;
+    }
+
+    toggleTextOut = () => {
+        this.setState({ textLimit: null })
     }
 
     render() {
@@ -22,9 +51,20 @@ class CreateNote extends Component {
                 <Navigation />
                 <div className="createNoteCardContainer">
                     <h4>Create New Note:</h4>
-                    <input type="text" name="title" placeholder="Note Title" value={this.state.title} onChange={this.handleOnChange} />
-                    <textarea name="text" rows="15" cols="50" maxLength="1000" placeholder="Note Content" value={this.state.text} onChange={this.handleOnChange}></textarea>
-                    <Link to="/"><button onClick={() => this.props.addNote({ title: this.state.title, text: this.state.text })}>Save</button></Link>
+                    <form onSubmit={this.handleOnSubmit}>
+                        <input id="titlePopOver" type="text" name="title" maxLength="20" placeholder="Note Title" value={this.state.title} onChange={this.handleOnChange} required />
+                        <textarea id="textPopOver" name="text" rows="15" cols="50" maxLength="1000" placeholder="Note Content" value={this.state.text} onChange={this.handleOnChange} required></textarea>
+                        <input type="submit" value="Save" className="submit" />
+                    </form>
+                    {this.state.redirect ? (<Redirect to="/" />) : null}
+                    {this.state.title.length === this.state.titleLimit ? this.toggleTitleLimit() : this.titlePopOverOpen === false}
+                    {this.state.text.length === this.state.textLimit ? this.toggleTextLimit() : this.textPopOverOpen = false}
+                    <Popover placement="bottom" isOpen={this.titlePopOverOpen} target="titlePopOver" toggle={this.toggleTitleOut}>
+                        <PopoverBody><div className="exclamation">!</div> You have reached the maximum limit of characters you can type for the note title.</PopoverBody>
+                    </Popover>
+                    <Popover placement="bottom" isOpen={this.textPopOverOpen} target="textPopOver" toggle={this.toggleTextOut}>
+                        <PopoverBody><div className="exclamation">!</div> You have reached the maximum limit of characters you can type for the note content.</PopoverBody>
+                    </Popover>
                 </div>
             </div>
         )
--- lambda-notes/src/components/EditNote/EditNoteCard.js
+++ lambda-notes/src/components/EditNote/EditNoteCard.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Popover, PopoverBody } from "reactstrap";
+import { Redirect } from "react-router-dom";
 import "./EditNote.css";
 
 class EditNoteCard extends Component {
@@ -8,20 +9,59 @@ class EditNoteCard extends Component {
         this.state = {
             title: props.note.title,
             text: props.note.text,
+            redirect: false,
+            titleLimit: 20,
+            textLimit: 1000
         }
+        this.titlePopOverOpen = false;
+        this.textPopOverOpen = false;
+    }
+
+    handleOnSubmit = () => {
+        this.props.modifyNote(this.props.id, { title: this.state.title, text: this.state.text })
+        this.setState({ redirect: true })
     }
 
     handleOnChange = (event) => {
         this.setState({ [event.target.name]: event.target.value })
     }
 
+    toggleTitleLimit = () => {
+        this.titlePopOverOpen = !this.titlePopOverOpen;
+    }
+
+    toggleTitleOut = () => {
+        this.setState({ titleLimit: null })
+        this.titlePopOverOpen = false;
+    }
+
+    toggleTextLimit = () => {
+        this.textPopOverOpen = !this.titlePopOverOpen;
+    }
+
+    toggleTextOut = () => {
+        this.setState({ textLimit: null })
+        this.textPopOverOpen = !this.textPopOverOpen;
+    }
+
     render() {
         return (
             <div className="editNoteCardContainer">
                 <h4>Edit Note:</h4>
-                <input type="text" name="title" value={this.state.title} onChange={this.handleOnChange} />
-                <textarea name="text" rows="15" cols="50" maxLength="1000" value={this.state.text} onChange={this.handleOnChange} />
-                <Link to="/"><button onClick={() => this.props.modifyNote(this.props.id, { title: this.state.title, text: this.state.text })}>Update</button></Link>
+                <form onSubmit={this.handleOnSubmit}>
+                    <input id="titlePopOver" type="text" name="title" maxLength="20" value={this.state.title} onChange={this.handleOnChange} required />
+                    <textarea id="textPopOver" name="text" rows="15" cols="50" maxLength="1000" value={this.state.text} onChange={this.handleOnChange} required />
+                    <input type="submit" value="Update" className="submit" />
+                </form>
+                {this.state.redirect ? (<Redirect to="/" />) : null}
+                {this.state.title.length === this.state.titleLimit ? this.toggleTitleLimit() : this.titlePopOverOpen = false}
+                {this.state.text.length === this.state.textLimit ? this.toggleTextLimit() : this.textPopOverOpen = false}
+                <Popover placement="bottom" isOpen={this.titlePopOverOpen} target="titlePopOver" toggle={this.toggleTitleOut}>
+                    <PopoverBody><div className="exclamation">!</div> You have reached the maximum limit of characters you can type for the note title.</PopoverBody>
+                </Popover>
+                <Popover placement="bottom" isOpen={this.textPopOverOpen} target="textPopOver" toggle={this.toggleTextOut}>
+                    <PopoverBody><div className="exclamation">!</div> You have reached the maximum limit of characters you can type for the note content.</PopoverBody>
+                </Popover>
             </div>
         )
     }
