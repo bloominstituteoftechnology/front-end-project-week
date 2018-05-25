bed9e2c3fbf910a6f259a39555b7cbe33627c260
NandoTheessen <nando.theessen@gmail.com>
2018-05-25 20:47:19 +0200

    uninstalled package re-base, added styling for the sign-in page

--- notes/src/components/SignIn.js
+++ notes/src/components/SignIn.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { signIn } from "./Actions/";
 import PropTypes from "prop-types";
+import img from '../google.jpg'
 
 class Signin extends Component {
     static contextTypes = {
@@ -18,16 +19,15 @@ class Signin extends Component {
 
     render() {
         return (
-            <div className="row social-signin-container">
+            <div className="sign-in">
                 <div className="col s10 offset-s1 center-align">
-                    <img alt="Sign in" id="sign-in" src="/img/user.png" />
-                    <h4 id="sign-in-header">Sign In to start</h4>
+
+                    <h4 className="sign-in-header">Sign In to start</h4>
                     <a href="#" className="social-signin" onClick={() => {
                         this.props.signIn()
                     }}>
-                        <i className="fa fa-google social-signin-icon" />
-                        Sign In With Google
-          </a>
+                        <img alt="Sign in" id="sign-in" src={img} />
+                    </a>
                 </div>
             </div>
         );
