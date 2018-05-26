14ade89f92236d736dcfd761db6a46ea7b1d166e
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-26 15:57:10 -0500

    Note.css somehow was wiped clean, copy pasted code from github back into file

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -5,14 +5,13 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { notesReducer } from './reducers';
 
-const store = createStore(notesReducer, applyMiddleware(thunk, logger))
+const store = createStore(notesReducer, applyMiddleware(logger))
 
 ReactDOM.render(<Provider store={store}><Router><App /></Router></Provider>, document.getElementById('root'));
 registerServiceWorker();
