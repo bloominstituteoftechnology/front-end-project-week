92aa5e429840fd2e8925b6e0e7c6edec33fd8d4f
2940cristian <2940cristian@gmail.com>
2018-05-28 11:09:41 -0700

    Pushing to heroku

--- Server.js
+++ Server.js
@@ -2,18 +2,30 @@
 const express = require('express');
 const server = express();
 const mongoose = require('mongoose');
-
+const Note = require('./Database/Notes/Note')
 //Database
 let uri = 'mongodb://2940cristian:dance360@ds235840.mlab.com:35840/noteslambda';
 mongoose.connect(uri, function(err, success) {
     if(err) throw err;
     console.log("Connected to DB")
 })
+
 //Middleware
 server.use(express.json());
 
 
 //Routers
 server.get("/", (req,res) => {res.send('API RUNNING')})
+
+server.post("/notes", (req, res) => {
+    const newNote = req.body;
+    const note = new Note(newNote);
+
+    note.save().then(note => {
+        res.status(200).json(note);
+    }).catch(err => {
+        res.status(500).json(err)
+    })
+})
 
 server.listen(5000, () => {console.log("Listening on port 5000")})
