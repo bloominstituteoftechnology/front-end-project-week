89aade2e5a661abd9f4f0d855278fb8bd30873c5
2940cristian <2940cristian@gmail.com>
2018-05-28 09:57:39 -0700

    Connected db to node server

--- Server.js
+++ Server.js
@@ -1,9 +1,19 @@
+//Modules
 const express = require('express');
 const server = express();
+const mongoose = require('mongoose');
 
-
+//Database
+let uri = 'mongodb://2940cristian:dance360@ds235840.mlab.com:35840/noteslambda';
+mongoose.connect(uri, function(err, success) {
+    if(err) throw err;
+    console.log("Connected to DB")
+})
+//Middleware
 server.use(express.json());
 
+
+//Routers
 server.get("/", (req,res) => {res.send('API RUNNING')})
 
 server.listen(5000, () => {console.log("Listening on port 5000")})
