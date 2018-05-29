e9191e84760b993603b19dbe49731415358f5e72
2940cristian <2940cristian@gmail.com>
2018-05-29 10:43:34 -0700

    Protected home/ listview component

--- lambda-notes/src/Components/ListView/ListView.js
+++ lambda-notes/src/Components/ListView/ListView.js
@@ -15,13 +15,23 @@ class ListView extends Component {
         }
     }
 
-    componentDidMount() {
+    componentWillMount() {
         this.fetchData()
     }
 
     fetchData() {
         const token = localStorage.getItem('token')
-        axios.get('https://noteslambda.herokuapp.com/notes')
+        const authtoken = `${token}`;
+
+        const requestOptions = {
+            headers: {
+                Authorization: authtoken,
+            }
+        }
+
+
+
+        axios.get('https://noteslambda.herokuapp.com/notes', requestOptions)
         .then(response => {
             this.setState({
                 notes: response.data
@@ -29,36 +39,48 @@ class ListView extends Component {
             console.log(this.state.notes)
         }).catch(err => {
             console.log(err)
+            this.props.history.push('/')
         })
     }
     
 
     render() {
         return (
-            <div className = "body">
-                <SideBar/>
-                <div className = "sideBar_pop notes">
-                    <div className = "list">
-                        <h2>Your Notes:</h2>
-                    </div>
+            <div>
 
-                    <div>
-                        {this.state.notes.length < 1 ? (
-                            <div>Please Make A note</div>
-                        ) : (
-                            <div className="row">
-                            {this.state.notes.map(note => {
-                                return(
-                                    <Link to="/home" key={note._id} className="card">
-                                        <h1 className="underline">{note.title}</h1>
-                                        <p>{note.content}</p>
-                                    </Link>
-                                )
-                            })}
-                            </div>
-                        )}
+            {this.state.notes < 1 ? (
+                <div></div>
+            ) : (
+                <div className = "body">
+                    <SideBar/>
+                    <div className = "sideBar_pop notes">
+                        <div className = "list">
+                            <h2>Your Notes:</h2>
+                        </div>
+
+                        <div>
+                            {this.state.notes.length < 1 ? (
+                                <div>Please Make A note</div>
+                            ) : (
+                                <div className="row">
+                                {this.state.notes.map(note => {
+                                    return(
+                                        <Link to="/home" key={note._id} className="card">
+                                            <h1 className="underline">{note.title}</h1>
+                                            <p>{note.content}</p>
+                                        </Link>
+                                    )
+                                })}
+                                </div>
+                            )}
+                        </div>
                     </div>
                 </div>
+            )}
+
+
+
+
             </div>
         )
     }
