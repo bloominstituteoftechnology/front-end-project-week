ccaed1c8d9bd3ddba1b96d03c9e538a1b16346ad
Jared Cuffe <jcuffe@gmail.com>
2018-05-30 16:04:56 -0700

    Typo :(

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ActionButton = ({ type, text, onClick }) => {
   return (
-    <div className={`action-button ${type}`} onClick={onClick}>{text}</Wrapper>
+    <div className={`action-button ${type}`} onClick={onClick}>{text}</div>
   )
 }
 
