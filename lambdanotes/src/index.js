ace3401a342215af39baa761395bf3ba3588a0da
Rachelle De Jesus Pestanas <rachelle.pestanas@gmail.com>
2018-05-30 18:02:19 -0500

    wrapped Application in Provider & Router

--- lambdanotes/src/index.js
+++ lambdanotes/src/index.js
@@ -1,8 +1,26 @@
+// React Imports
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Component Imports
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+// Router Imports
+import { BrowserRouter as Router } from 'react-router-dom';
+// Redux Imports
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+const dummyReducer = (state = [], action) => state;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(dummyReducer);
+
+ReactDOM.render(
+    (
+        <Provider store={store}>
+            <Router>
+                <App />
+            </Router>
+        </Provider>
+    )
+    , document.getElementById('root'));
 registerServiceWorker();
