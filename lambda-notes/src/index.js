8497c46acf3f2559ae0d03cb77159968af541d72
Rachelle De Jesus Pestanas <rachelle.pestanas@gmail.com>
2018-05-30 17:23:03 -0500

    initial commit: created a React Application using CRA || installed Redux, React-Redux, React-Router-DOM

--- lambda-notes/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
-registerServiceWorker();
