d96810ba72626c4946a1c64f2809f6bbb57fb38d
Rachelle De Jesus Pestanas <rachelle.pestanas@gmail.com>
2018-05-31 17:37:30 -0500

    Styled Navigation Bar & Buttons

--- lambda-notes/src/components/LeftNav/LeftNav.js
+++ lambda-notes/src/components/LeftNav/LeftNav.js
@@ -4,7 +4,13 @@ class LeftNav extends Component {
     render() {
         return (
             <div className='nav_container'>
-                LEFT NAV PLACEHOLDER
+                <div>
+                    <h1 className='nav_header'>Lambda Notes</h1>
+                </div>
+                <div className='nav_buttons_container'>
+                    <div className='nav_button'>View Your Notes</div>
+                    <div className='nav_button'>+ Create New Note</div>
+                </div>
             </div>
         );
     }
