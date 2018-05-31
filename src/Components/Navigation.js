e96efcfb39320f7e31b256d7119f835f6581c137
Shawn Stewart <s.stewart0220@gmail.com>
2018-05-31 14:28:04 -0400

    Name change

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -6,7 +6,7 @@ import './Navigation.css';
 export const Navigation = () => {
   return(
     <div className='Navigation'>
-      <NavLink to='/' className='Link'><h1 className='Navigation__Heading'>Dope Notes</h1></NavLink>
+      <NavLink to='/' className='Link'><h1 className='Navigation__Heading'>Dope<br />Notes</h1></NavLink>
       <div className='Navigation__Buttons'>
         <NavLink to='/notes'><button className='Navigation__Button'>View Your Notes</button></NavLink>
         <NavLink to='/addnote'><button className='Navigation__Button'>+ Create New Note</button></NavLink>
