7d749bd04b9b383fe82c002e9bd3742f0b1d0036
Jared Cuffe <jcuffe@gmail.com>
2018-05-31 15:42:47 -0700

    Register/Login and fetch all notes implemented

--- src/state/actions.js
+++ src/state/actions.js
@@ -1,16 +1,68 @@
-export const FETCH_REQUEST = 'fetch request'
-export const FETCH_FAILURE = 'fetch failure'
+import axios from 'axios'
+import {
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAILURE,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAILURE,
+  USER_SET_TOKEN,
+  USER_NOTES_FETCH_SUCCESS,
+  FETCH_REQUEST,
+  FETCH_FAILURE,
+  CLEAR_FLASH_MESSAGE
+} from './actionTypes'
 
-export const USER_REGISTER_REQUEST = 'user register request'
-export const USER_REGISTER_SUCCESS = 'user register success'
-export const USER_LOGIN_REQUEST = 'user login request'
-export const USER_LOGIN_SUCCESS = 'user login success'
-export const USER_LOGOUT_REQUEST = 'user logout request'
-export const USER_LOGOUT_SUCCESS = 'user logout success'
-export const USER_SET_TOKEN = 'user set token'
+const api = axios.create({
+  baseURL: 'https://lambda-backend-project.herokuapp.com/api',
+})
 
-export const USER_NOTES_FETCH_SUCCESS = 'user notes fetch success'
-export const NOTE_FETCH_SUCCESS = 'note fetch success'
-export const NOTE_CREATE_SUCCESS = 'note create success'
-export const NOTE_UPDATE_SUCCESS = 'note update success'
-export const NOTE_DELETE_SUCCESS = 'note delete success'
+export const registerUser = (username, password) => {
+  return async dispatch => {
+    dispatch(userRegisterRequest({ username, password }))
+    try {
+      const { data } = await api.post('/user/register', { username, password })
+      dispatch(userRegisterSuccess(data.user))
+      dispatch(userSetToken(data.token))
+    } catch (err) {
+      dispatch(userRegisterFailure(err.response.data))
+    }
+  }
+}
+
+export const loginUser = (username, password) => async dispatch => {
+  dispatch(userLoginRequest({ username, password }))
+  try {
+    const { data } = await api.post('/user/login', { username, password })
+    dispatch(userLoginSuccess(data.user))
+    dispatch(userSetToken(data.token))
+  } catch (err) {
+    dispatch(userLoginFailure(err.response.data))
+  }
+}
+
+export const fetchUserNotes = (userId) => async (dispatch, getState) => {
+  dispatch(fetchRequest())
+  try {
+    const { data } = await api.get('/note').set('token', getState().sessionToken)
+    dispatch(userNotesFetchSuccess(data.notes))
+  } catch (err) {
+    dispatch(fetchFailure(err.response.data))
+  }
+}
+
+export const userRegisterRequest = (user) => ({ type: USER_REGISTER_REQUEST, payload: user })
+export const userRegisterSuccess = (user) => ({ type: USER_REGISTER_SUCCESS, payload: user })
+export const userRegisterFailure = (err) => ({ type: USER_REGISTER_FAILURE, payload: err })
+
+export const userLoginRequest = (user) => ({ type: USER_LOGIN_REQUEST, payload: user })
+export const userLoginSuccess = (user) => ({ type: USER_LOGIN_SUCCESS, payload: user })
+export const userLoginFailure = (err) => ({ type: USER_LOGIN_FAILURE, payload: err })
+export const userSetToken = (token) => ({ type: USER_SET_TOKEN, payload: token })
+
+export const fetchRequest = () => ({ type: FETCH_REQUEST })
+export const fetchFailure = (err) => ({ type: FETCH_FAILURE, payload: err })
+
+export const userNotesFetchSuccess = (notes) => ({ type: USER_NOTES_FETCH_SUCCESS, payload: notes })
+
+export const clearFlashMessage = () => ({ type: CLEAR_FLASH_MESSAGE })
