14c2aa1b6a72b1417a82461419e1426af1f05064
Jared Cuffe <jcuffe@gmail.com>
2018-05-31 15:43:51 -0700

    Ungraceful gating of interaction based on login status

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,37 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Route } from 'react-router'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router'
 import Sidebar from './Sidebar'
+import Flash from './Flash'
+import Authentication from './Authentication'
 import NoteList from './note/NoteList'
 import NoteDetail from './note/NoteDetail'
 import NoteCreate from './note/NoteCreate'
 import NoteEdit from './note/NoteEdit'
 
-const App = () => (
+const App = ({ sessionToken }) => (
   <div className="app">
     <Sidebar />
     <div className="main-container">
-      <Route exact path='/' component={NoteList} />
-      <Route path='/create' component={NoteCreate} />
-      <Route path='/edit/:id' component={NoteEdit} />
-      <Route path='/show/:id' component={NoteDetail} />
+    <Flash />
+    { 
+      sessionToken
+        ? <div>
+            <Route exact path='/' component={NoteList} />
+            <Route path='/create' component={NoteCreate} />
+            <Route path='/edit/:id' component={NoteEdit} />
+            <Route path='/show/:id' component={NoteDetail} />
+          </div>
+        : <Authentication />
+    }
     </div>
   </div>
 )
 
-export default App
+const mapStateToProps = ({ sessionToken }) => ({ sessionToken })
+export default compose(
+  withRouter,
+  connect(mapStateToProps)
+)(App)
