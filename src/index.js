8c57317c077c105a646ec810cf946c2cebf1ab0f
Jared Cuffe <jcuffe@gmail.com>
2018-05-31 15:40:17 -0700

    Applying logging middleware

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 import rootReducer from './state/reducers.js'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import './index.css'
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, applyMiddleware(thunk, logger))
 
 render(
   <BrowserRouter>
