3c68e6583515f1e30cd846a98b76bb7ed70161a0
Jared Cuffe <jcuffe@gmail.com>
2018-05-31 18:09:45 -0700

    Implemented modals using react-portal library

--- src/components/note/NoteDetail.js
+++ src/components/note/NoteDetail.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 // import { withRouter } from 'react-router'
+import { PortalWithState } from 'react-portal'
 import Markdown from './Markdown'
 import LinkButton from '../buttons/LinkButton'
 import ActionButton from '../buttons/ActionButton'
-import DeleteModal from './DeleteModal'
 import { fetchNote, deleteNote } from '../../state/actions'
 
-class Note extends Component {
-  state = {
-    modalHidden: true
-  }
-
+class NoteDetail extends Component {
   componentDidMount() {
     this.props.fetchNote(this.props.match.params.id)
   }
@@ -21,37 +17,54 @@ class Note extends Component {
     this.props.history.push('')
   }
 
-  toggleModal = () => this.setState({ modalHidden: !this.state.modalHidden })
 
-  render() {
-    const { note, match } = this.props
-    return (
-      <div className="note-detail-view">
-        <DeleteModal 
-          hidden={this.state.modalHidden} 
-          deleteNote={this.deleteNote}
-          hideSelf={this.toggleModal} />
-        <div className="controls">
-          <LinkButton 
-            to={`/edit/${match.params.id}`}
-            text='Edit' />
-          <ActionButton
-            type='delete'
-            text='Delete'
-            onClick={this.toggleModal} />
-        </div>
-        <div className="note">
-          <h1>{note.title}</h1>
-          <Markdown 
-            noteId={match.params.id}
-            markdown={note.content} />
+  render = () => (
+    <div className="note-detail-view">
+      <div className="controls">
+        <LinkButton 
+          to={`/edit/${this.props.note._id}`}
+          text='Edit' />
+        <PortalWithState closeOnOutsideClick closeOnEsc>
+          {DeleteModal(this.deleteNote)}
+        </PortalWithState>
+      </div>
+      <Note {...this.props.note} />
+    </div>
+  )
+}
+
+const Note = ({ _id, title, content }) => (
+  <div className="note">
+    <h1>{title}</h1>
+    <Markdown 
+      noteid={_id}
+      markdown={content} />
+  </div>
+)
+
+const DeleteModal = deleteNote => ({ openPortal, closePortal, isOpen, portal }) => (
+  <div>
+    <ActionButton type='delete' text='Delete' onClick={openPortal} />
+    {portal(
+      <div className="modal-mat">
+        <div className="modal-dialog">
+          <h5>Are you sure you want to delete this?</h5>
+          <div className="controls">
+            <ActionButton 
+              type='delete'
+              text='Delete'
+              onClick={deleteNote} />
+            <ActionButton
+              text='No'
+              onClick={closePortal} />
+          </div>
         </div>
       </div>
-    )
-  }
-}
+    )}
+  </div>
+)
 
 const stateToProps = ({ currentNote }) => ({ note: currentNote })
 const dispatchToProps = { fetchNote, deleteNote }
 
-export default connect(stateToProps, dispatchToProps)(Note)
+export default connect(stateToProps, dispatchToProps)(NoteDetail)
