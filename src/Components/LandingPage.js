db275fec5976a613b43d5418f84cdaa0a0d9ea84
Shawn Stewart <s.stewart0220@gmail.com>
2018-05-31 20:16:05 -0400

    Styling for mobile

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -6,7 +6,7 @@ import './LandingPage.css';
 export const LandingPage = () => {
   return(
     <div className='Content__Wrapper'>
-      <h3 className='Content__Heading Greeting'>Welcome back to Super Duper Dope Notes</h3>
+      <h3 className='Content__Heading Greeting'>Welcome back to Lambda Notes</h3>
       <p className='Content Greeting__Message'>The best place to keep your notes!</p>
     </div>
   );
