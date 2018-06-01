114fa33a41393709de94f71fcc3d5672d9673c3d
Rachelle De Jesus Pestanas <rachelle.pestanas@gmail.com>
2018-06-01 09:35:03 -0500

    DeleteNote Component Complete

--- lambda-notes/src/components/DeleteNote/DeleteNote.js
+++ lambda-notes/src/components/DeleteNote/DeleteNote.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
+import { deleteNote } from '../../actions';
+import { connect } from 'react-redux';
+import './index.css';
 
 class DeleteNote extends Component {
+
+    handleDelete = () => {
+        this.props.deleteNote(this.props.toDelete);
+        this.props.history.push('/');
+    }
+    
     render() {
+        console.log('Delete Note Props:', this.props)
+        let toggle = this.props.toggle;
         return (
-            <div>
-                
+            <div className={toggle ? 'delete_wrapper' : 'hidden'}>
+                <div>
+                    <h4>Are you sure you want to delete this?</h4>
+                </div>
+                <div className='delete_buttons_wrapper'>
+                    <div
+                        className='button button--danger'
+                        onClick={this.handleDelete}
+                    >
+                        DELETE
+                    </div>
+                    <div
+                        className='button'
+                        onClick={this.props.showModal}
+                    >
+                        NO
+                    </div>
+                </div>
             </div>
         );
     }
 }
 
-export default DeleteNote;
+export default connect(null, {deleteNote})(DeleteNote);
