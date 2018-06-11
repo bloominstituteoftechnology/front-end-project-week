4f044f83db7d1ce4ca2065bf6892888a79b663fd
kammagik <kammagik@yahoo.com>
2018-06-10 22:53:01 -0700

    added notes into array

--- lambdanotes/src/components/NoteList/NoteList.js
+++ lambdanotes/src/components/NoteList/NoteList.js
@@ -3,59 +3,54 @@ import './index.css';
 
 class NoteList extends Component {
     render() {
+        const notesElements = [<div className = 'note'>
+        <div>
+            <h4>Note Title</h4>
+        <br></br>
+             <p>Red hair crookshanks bludger Marauder’s Map Prongs sunshine daisies butter mellow Ludo Bagman. Beaters gobbledegook N.E.W.T.</p>
+        </div>
+     </div>,
+     <div className='note'>
+        <div>
+            <h4>Note Title</h4>
+        <br></br>
+             <p>Red hair crookshanks bludger Marauder’s Map Prongs sunshine daisies butter mellow Ludo Bagman. Beaters gobbledegook N.E.W.T.</p>
+        </div>
+     </div>,
+     <div className='note'>
+        <div>
+            <h4>Note Title</h4>
+        <br></br>
+             <p>Red hair crookshanks bludger Marauder’s Map Prongs sunshine daisies butter mellow Ludo Bagman. Beaters gobbledegook N.E.W.T.</p>
+        </div>
+     </div>,
+     <div className='note'>
+        <div>
+            <h4>Note Title</h4>
+        <br></br>
+             <p>Red hair crookshanks bludger Marauder’s Map Prongs sunshine daisies butter mellow Ludo Bagman. Beaters gobbledegook N.E.W.T.</p>
+        </div>
+     </div>,
+     <div className='note'>
+        <div>
+            <h4>Note Title</h4>
+        <br></br>
+             <p>Red hair crookshanks bludger Marauder’s Map Prongs sunshine daisies butter mellow Ludo Bagman. Beaters gobbledegook N.E.W.T.</p>
+        </div>
+     </div>,
+     <div className='note'>
+        <div>
+            <h4>Note Title</h4>
+        <br></br>
+             <p>Red hair crookshanks bludger Marauder’s Map Prongs sunshine daisies butter mellow Ludo Bagman. Beaters gobbledegook N.E.W.T.</p>
+        </div>
+     </div>,
+     ]
         return (
                 <div classname = 'notesList_container'>
                 <div><h3>Your Notes</h3></div>
                 <div className='notesList'>
-                <div className = 'note'>
-                <div>
-                    <h4>Note Title</h4>
-                <br></br>
-                     <p>Red hair crookshanks bludger Marauder’s Map Prongs sunshine daisies butter mellow Ludo Bagman. Beaters gobbledegook N.E.W.T.</p>
-                </div>
-             </div>
-             <div className='note'>
-                <div>
-                    <h4>notes</h4>
-                <br></br>
-                     <p>Red hair crookshanks bludger Marauder’s Map Prongs sunshine daisies butter mellow Ludo Bagman. Beaters gobbledegook N.E.W.T.</p>
-                </div>
-             </div>
-             <div className='note'>
-                <div>
-                    <h4>notes</h4>
-                <br></br>
-                     <p>Red hair crookshanks bludger Marauder’s Map Prongs sunshine daisies butter mellow Ludo Bagman. Beaters gobbledegook N.E.W.T.</p>
-                </div>
-             </div>
-             <div className='note'>
-                <div>
-                    <h4>notes</h4>
-                <br></br>
-                     <p>Red hair crookshanks bludger Marauder’s Map Prongs sunshine daisies butter mellow Ludo Bagman. Beaters gobbledegook N.E.W.T.</p>
-                </div>
-             </div>
-             <div className='note'>
-                <div>
-                    <h4>notes</h4>
-                <br></br>
-                     <p>Red hair crookshanks bludger Marauder’s Map Prongs sunshine daisies butter mellow Ludo Bagman. Beaters gobbledegook N.E.W.T.</p>
-                </div>
-             </div>
-             <div className='note'>
-                <div>
-                    <h4>notes</h4>
-                <br></br>
-                     <p>Red hair crookshanks bludger Marauder’s Map Prongs sunshine daisies butter mellow Ludo Bagman. Beaters gobbledegook N.E.W.T.</p>
-                </div>
-             </div>
-             <div className='note'>
-                <div>
-                    <h4>notes</h4>
-                <br></br>
-                     <p>Red hair crookshanks bludger Marauder’s Map Prongs sunshine daisies butter mellow Ludo Bagman. Beaters gobbledegook N.E.W.T.</p>
-                </div>
-             </div>
+                {notesElements}
             </div>
             </div>
         );
