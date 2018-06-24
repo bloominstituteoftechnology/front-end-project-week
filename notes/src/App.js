d6d754ee53ea5762d3b884fb335032b3daede280
MacBook <LonnieDavis@MacBookPros-MacBook-Pro.local>
2018-06-24 13:22:41 -0500

    CSS Changes
    css changes include colors with transparency and background image

--- notes/src/App.js
+++ notes/src/App.js
@@ -7,7 +7,7 @@ import MainContent from './components/MainContent/MainContent';
 class App extends Component {
   render() {
     return (
-      <div className='app_container'>
+      <div className='container'>
         <LeftNav/>
         <MainContent/>
       </div>
