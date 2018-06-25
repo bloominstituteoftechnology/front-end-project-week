16140b9fc3a6dce967ec5e0df7888a8ddacb38dc
Wladimir Fraga <wladimir917@gmail.com>
2018-06-25 11:34:26 -0400

    Delete dev.js

--- lambda-notes/src/config/dev.js
+++
@@ -1,8 +0,0 @@
-export const FirebaseConfig = {
-    apiKey: "AIzaSyCC3kKcO2uV12dSC4zwox_H5E1AyJOAVIY",
-    authDomain: "lambda-notes-8ecdf.firebaseapp.com",
-    databaseURL: "https://lambda-notes-8ecdf.firebaseio.com"
-  };
-
-  //Don/t load this to the git
-  
