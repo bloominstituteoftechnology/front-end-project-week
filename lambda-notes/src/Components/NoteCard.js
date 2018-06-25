03138a1678cef36a6f35007f64cd86b6940a839e
sagardesai90 <saagardesai@gmail.com>
2018-06-25 16:17:06 -0500

    getting started

--- lambda-notes/src/Components/NoteCard.js
+++ lambda-notes/src/Components/NoteCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Card, CardHeader, CardBody, CardText } from 'reactstrap';
 import "./NoteCard.css";
-import Markdown from 'markdown-to-jsx';
+// import Markdown from 'markdown-to-jsx';
 
 const NoteCard = (props) => {
     return (
@@ -11,7 +11,7 @@ const NoteCard = (props) => {
                 <Link to={`note/${props.note.id}`}>{props.note.title} </Link>
             </CardHeader>
             <CardBody className="card-body">
-                <Markdown>{props.note.content}</Markdown>
+                {props.note.content}
             </CardBody>
         </Card>
     )
--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -5,5 +5,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root')
+);
 registerServiceWorker();
