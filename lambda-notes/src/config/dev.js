6d4f31769fd00167a8e9fd5ad2b7d047ac4e684d
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-06-25 11:33:19 -0400

    update before start next project

--- lambda-notes/src/config/dev.js
+++ lambda-notes/src/config/dev.js
@@ -3,3 +3,6 @@ export const FirebaseConfig = {
     authDomain: "lambda-notes-8ecdf.firebaseapp.com",
     databaseURL: "https://lambda-notes-8ecdf.firebaseio.com"
   };
+
+  //Don/t load this to the git
+  
