ccb60fbd31bd3eaa5d30126a20ee0117fca9efc6
A99 <A99@99s-macbook-pro.home>
2018-06-27 00:07:14 +0200

    Add auth UI logic & signout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Route, Switch, withRouter } from "react-router-dom";
-import { Navigation, NotesList, AddNote, Note } from "./components";
+import { Navigation, NotesList, AddNote, Note, Login, Register } from "./components";
 import { connect } from "react-redux";
 
 class App extends Component {
 
+  signoutHandler = () => {
+    localStorage.removeItem('jwt');
+    this.props.history.push('/login');
+  }
+
   render() {
     return (
       <div className="App">
-        <Navigation />
-        <Switch>
-          <Route exact path="/" component={NotesList}/>
-          <Route path="/addnote" component={AddNote}/>
-          <Route path="/:id" component={Note}/>
-        </Switch>
+        {localStorage.getItem('jwt') ? (
+          <div>
+            <Navigation />
+            <button onClick={this.signoutHandler}>Sign Out</button>
+            <Switch>
+              <Route exact path="/" component={NotesList}/>
+              <Route path="/addnote" component={AddNote}/>
+              <Route path="/:id" component={Note}/>
+              <Route path="/login" component={Login}/>
+              <Route path="/register" component={Register}/>
+            </Switch>
+          </div>
+        ) : (
+          <Route exact path="/" render={() => <div><Link to="/login">Login</Link></div>}/>
+        )}
       </div>
     );
   }
