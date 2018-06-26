a86930dd0b2a0b7ac7009a471def65b134e45fb4
A99 <A99@99s-macbook-pro.home>
2018-06-27 00:08:28 +0200

    Add API calls for notes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
-// export const FETCHING_NOTES = 'FETCHING_NOTES';
-// export const NOTES_FETCHED = 'NOTES_FETCHED';
+export const FETCHING_NOTES = 'FETCHING_NOTES';
+export const NOTES_FETCHED = 'NOTES_FETCHED';
 
 export const ADDING_NOTE = 'ADDING_NOTE';
 export const NOTE_ADDED = 'NOTE_ADDED';
@@ -13,28 +13,58 @@ export const NOTE_DELETED = 'NOTE_DELETED';
 export const ERROR = 'ERROR';
 
 
-// export const fetchNotes = () => {
-    
-// }
+export const fetchNotes = id => {
+    const getNotes = axios.get(`https://notesmtm.herokuapp.com/api/users/${id}/notes`);
+    return function(dispatch) {
+        dispatch({ type: FETCHING_NOTES });
+        getNotes
+            .then(response => {
+                dispatch({ type: NOTES_FETCHED, payload: response.data });
+            })
+            .catch(err => {
+                dispatch({ type: ERROR, payload: err.message });
+            });
+    }
+}
 
 export const addNote = note => {
+    const postNote = axios.post('https://notesmtm.herokuapp.com/api/notes/', note);
     return function(dispatch) {
         dispatch({ type: ADDING_NOTE });
-        dispatch({ type: NOTE_ADDED, payload: note });  
+        postNote
+            .then(response => {
+                dispatch({ type: NOTE_ADDED, payload: response.data });  
+            })
+            .catch(err => {
+                dispatch({ type: ERROR, payload: err.message });
+            });
     }
 };
 
-
-export const updateNote = note => {
+export const updateNote = (id, note) => {
+    const putNote = axios.put(`https://notesmtm.herokuapp.com/api/notes/${id}`, note);
     return function(dispatch) {
         dispatch({ type: UPDATING_NOTE });
-        dispatch({ type: NOTE_UPDATED, payload: note });
+        putNote
+            .then(response => {
+                dispatch({ type: NOTE_UPDATED, payload: response.data });
+            })
+            .catch(err => {
+                dispatch({ type: ERROR, payload: err.message });
+            });
     }
 };
 
 export const deleteNote = id => {
+        const delNote = axios.put(`https://notesmtm.herokuapp.com/api/notes/${id}`);
     return function(dispatch) {
         dispatch({ type: DELETING_NOTE });
-        dispatch({ type: NOTE_DELETED, payload: id });
+        delNote
+            .then(response => {
+                dispatch({ type: NOTE_DELETED, payload: response.data });
+            })
+            .catch(err => {
+                dispatch({ type: ERROR, payload: err.message });
+            });
     }
 };
