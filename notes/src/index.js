8cbe23a2475a7940d3e7a0836945ec7309774968
A99 <A99@99s-macbook-pro.home>
2018-06-26 19:44:28 +0200

    Add service worker

--- notes/src/index.js
+++ notes/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
@@ -12,3 +13,4 @@ import rootReducer from "./reducers";
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(<Provider store={store}><Router><App /></Router></Provider>, document.getElementById('root'));
+registerServiceWorker();
