6050b4e2aa287a5478a5bcd9922656af41173dfb
A99 <A99@99s-macbook-pro.home>
2018-06-26 23:23:37 +0200

    Add export Login/Register

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
-export { default as Navigation} from "./Navigation.js";
-export { default as NotesList} from "./NotesList.js";
-export { default as Note} from "./Note.js";
-export { default as AddNote} from "./AddNote.js";
+export { default as Navigation } from "./Navigation.js";
+export { default as NotesList } from "./NotesList.js";
+export { default as Note } from "./Note.js";
+export { default as AddNote } from "./AddNote.js";
+export { default as Login } from './auth/Login.js';
+export { default as Register } from './auth/Register.js';
