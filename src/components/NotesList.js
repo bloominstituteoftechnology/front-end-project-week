5b042c8e291c08665614170414eca0bb7dde5567
A99 <A99@99s-macbook-pro.home>
2018-06-27 22:40:25 +0200

    Convert component to class & add fetch action

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,29 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import { fetchNotes } from "../actions";
 
-const NotesList = props => {
-    return (
-        <div className="notes-list">
-        <h2>Your Notes:</h2>
-            {props.notes.map(note => {
-                return (
-                    <div key={note._id}>
-                        <Link to={`/${note._id}`}>
-                            <div className="note-card">
-                                <h3>{note.title}</h3>
-                                <p className="note-card-text">{note.body}</p>
-                            </div>
-                        </Link>
-                    </div>
-                );
-            })}
-        </div>    
-    );
+class NotesList extends React.Component {
+    
+    componentDidMount() {
+        this.props.fetchNotes(this.props.id);
+    }
+
+    render() {
+        return (
+            <div className="notes-list">
+            <h2>Your Notes:</h2>
+                {this.props.notes.map(note => {
+                    return (
+                        <div key={note._id}>
+                            <Link to={`/${note._id}`}>
+                                <div className="note-card">
+                                    <h3>{note.title}</h3>
+                                    <p className="note-card-text">{note.body}</p>
+                                </div>
+                            </Link>
+                        </div>
+                    );
+                })}
+            </div>    
+        );
+    }
 }
 
 const mapStateToProps = state => {
     return state;
 }
 
-export default connect(mapStateToProps)(NotesList);
+export default connect(mapStateToProps, { fetchNotes })(NotesList);
