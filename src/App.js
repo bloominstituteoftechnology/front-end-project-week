068f1ac11673d9cee1862f66a2ac160e27407aec
A99 <A99@99s-macbook-pro.home>
2018-06-27 03:33:38 +0200

    Add setAccount method and jsonwebtoken verification

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
+import jwt from "jsonwebtoken";
 import { Route, withRouter, Link } from "react-router-dom";
 import { Navigation, NotesList, AddNote, Note, Login, Register } from "./components";
 import { connect } from "react-redux";
+import { setAccount } from "./actions";
 
 class App extends Component {
+
+  componentDidMount() {
+    const token = localStorage.getItem('jwt');
+    if (token) {
+      jwt.verify(token, process.env.REACT_APP_JWT_SECRET, (err, decoded) => {
+        if (err) {
+          console.log(err);
+        } else {
+          const { email } = decoded;
+          this.props.setAccount(email);
+        }
+      })
+    }
+  }
 
   signoutHandler = () => {
     localStorage.removeItem('jwt');
@@ -35,4 +51,4 @@ const mapStateToProps = state => {
   return state;
 }
 
-export default withRouter(connect(mapStateToProps)(App));
+export default withRouter(connect(mapStateToProps, { setAccount })(App));
