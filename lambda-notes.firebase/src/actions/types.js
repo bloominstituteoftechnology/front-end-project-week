74e8f82199f8df05cf7aac69f624a89a37e4393a
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-06-27 17:40:02 -0400

    Create new branch firebase

--- lambda-notes.firebase/src/actions/types.js
+++
@@ -1,4 +0,0 @@
-export const FETCHING_NOTES = 'FETCHING_NOTES'
-export const FETCHED_NOTES = 'FETCHED_NOTES'
-
-export const ERROR = 'ERROR'
--- lambda-notes.firebase/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-
-import data from "./dataReducer";
-
-export default combineReducers({
-  data
-});
