995d9bd044500aa6d0a38d9bc365544d8de42dc4
jesuarva <jesuarva.developer@gmail.com>
2018-06-27 23:51:43 +0200

    CONNECT TO API: Implement GET all notes.

--- lambda-notes/src/actions/index.js
+++ lambda-notes/src/actions/index.js
@@ -1,20 +1,20 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const FETCHING_ITEMS = "FETCHING_ITEMS";
-export const FETCHED_ITEMS = "FETCHED_ITEM";
-export const ADDING_ITEM = "ADDING_ITEM";
-export const ADDED_ITEM = "ADDED_ITEM";
-export const UPDATING_ITEM = "UPDATING_ITEM";
-export const UPDATED_ITEM = "UPDATED_ITEM";
-export const DELETING_ITEM = "DELETING_ITEM";
-export const DELETED_ITEM = "DELETED_ITEM";
-export const ERROR = "ERROR";
+export const FETCHING_ITEMS = 'FETCHING_ITEMS';
+export const FETCHED_ITEMS = 'FETCHED_ITEM';
+export const ADDING_ITEM = 'ADDING_ITEM';
+export const ADDED_ITEM = 'ADDED_ITEM';
+export const UPDATING_ITEM = 'UPDATING_ITEM';
+export const UPDATED_ITEM = 'UPDATED_ITEM';
+export const DELETING_ITEM = 'DELETING_ITEM';
+export const DELETED_ITEM = 'DELETED_ITEM';
+export const ERROR = 'ERROR';
 
-const URL = "http://localhost:5000/notes";
+const URL = 'https://jesuarva-lambda-notes.herokuapp.com/api/notes';
 const errorAction = error => {
   return {
     type: ERROR,
-    message: error.message
+    message: error.message,
   };
 };
 
@@ -22,18 +22,18 @@ export const fetchingItems = () => {
   const fetch = axios.get(URL);
   return dispatch => {
     dispatch({
-      type: FETCHING_ITEMS
+      type: FETCHING_ITEMS,
     });
     fetch
       .then(response => {
-        console.log("response.data", response.data);
+        console.log('response.data', response.data);
         dispatch({
           type: FETCHED_ITEMS,
-          data: response.data
+          data: response.data['Document(s) in database'],
         });
       })
       .catch(e => {
-        console.log("error", e);
+        console.log('error', e);
         dispatch(errorAction(e));
       });
   };
@@ -44,7 +44,7 @@ export const addingItem = newItem => {
   return dispatch => {
     dispatch({
       type: ADDING_ITEM,
-      newItem: newItem
+      newItem: newItem,
     });
     addItem
       .then(response => {
@@ -53,23 +53,23 @@ export const addingItem = newItem => {
         dispatch({
           type: ADDED_ITEM,
           allItems: response.data,
-          newItem: newItem //TO REVIEW WITH REAL EXAMPLES
+          newItem: newItem, //TO REVIEW WITH REAL EXAMPLES
         });
       })
       .catch(e => {
-        console.log("error", e);
+        console.log('error', e);
         dispatch(errorAction(e));
       });
   };
 };
 export const updatingItem = (index, id, content) => {
-  console.log("PUT",index,id,content);
+  console.log('PUT', index, id, content);
   const updateItem = axios.put(`${URL}/${id}`, content);
   return dispatch => {
     dispatch({
       type: UPDATING_ITEM,
       toUpdate: id,
-      content
+      content,
     });
     updateItem
       .then(response => {
@@ -77,12 +77,12 @@ export const updatingItem = (index, id, content) => {
         dispatch({
           type: UPDATED_ITEM,
           index,
-          content: {...content, id},  // Previous code missed the id in the 'content' note -> this make imposible to update one more time the same note. In teh server side there were no problem but in Redux, because of how it is implemented the state update, the note get updates with out id.
-          allItems: response.data // 
+          content: { ...content, id }, // Previous code missed the id in the 'content' note -> this make imposible to update one more time the same note. In teh server side there were no problem but in Redux, because of how it is implemented the state update, the note get updates with out id.
+          allItems: response.data, //
         });
       })
       .catch(e => {
-        console.log("error", e);
+        console.log('error', e);
         dispatch(errorAction(e));
       });
   };
@@ -93,22 +93,21 @@ export const deletingItem = (index, id) => {
     dispatch({
       type: DELETING_ITEM,
       index,
-      id
+      id,
     });
     deleteItem
       .then(response => {
-        console.log("DELETE response.data", response.data);
+        console.log('DELETE response.data', response.data);
         dispatch({
           type: DELETED_ITEM,
           allItems: response.data,
           index,
-          id
+          id,
         });
       })
       .catch(e => {
-        console.log("error", e);
+        console.log('error', e);
         dispatch(errorAction(e));
       });
   };
 };
- 
