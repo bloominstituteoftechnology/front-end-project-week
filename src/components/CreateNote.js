01be391243e8034d665e8f4e3ef1c78510159ae3
honda0306 <christopher.honda92@gmail.com>
2018-06-27 16:15:48 -0700

    Trying different route for axios call

--- src/components/CreateNote.js
+++ src/components/CreateNote.js
@@ -19,7 +19,7 @@ class CreateNote extends Component {
     handleSaveNote = () => {
         console.log('handleCreateNote Fired')
         axios
-            .post(`https://lambdanotesbyhonda.herokuapp.com/note/create`, this.state)
+            .post(`https://lambdanotesbyhonda.herokuapp.com/notes/create`, this.state)
                 .then(res => {
                     console.log(res);
                     window.location.reload();                
