24aba0f390e2629a3e267647ba109e2ea7374c59
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-06-28 13:33:26 -0700

    debugging react router

--- src/components/NoteThumbnail.js
+++ src/components/NoteThumbnail.js
@@ -6,14 +6,14 @@ import ReactMarkdown from 'react-markdown';
 const NoteThumbnail = (props) => {
     
     return (
-        <Link to={`/note/${props.note._id}`} className="note-link">
-            <Card className="note-thumbnail">
+        // <Link to={`/note/${props.note._id}`} className="note-link">
+            <Card className="note-thumbnail" onClick={() => this.props.history.push(`/note/${props.note._id}`)}>
                 <CardBody>
                     <CardTitle className="note-title heading">{props.note.title}</CardTitle>
                     <ReactMarkdown source={props.note.content} className="thumbnail-content"/>
                 </CardBody>
             </Card>
-        </Link>
+        // </Link>
     )
 }
  
