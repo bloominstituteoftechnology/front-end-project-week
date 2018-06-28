b06e9dcacdd05fe3ce9e17d50098c57aba84f18c
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-06-28 13:08:35 -0700

    debugging react router

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router';
 import { connect } from 'react-redux';
 import { fetchNote, removeNote } from '../actions';
 import { Container } from 'reactstrap';
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router';
 import { fetchNotes } from '../actions';
 import NoteThumbnail from './NoteThumbnail';
 import { Container, Row } from 'reactstrap';
