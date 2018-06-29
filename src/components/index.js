5da05c952eedf694e13fdebdddd91731194e083d
A99 <A99@99s-macbook-pro.home>
2018-06-29 03:00:38 +0200

    Add export of Landing component

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Note } from "./Note.js";
 export { default as AddNote } from "./AddNote.js";
 export { default as Login } from './auth/Login.js';
 export { default as Register } from './auth/Register.js';
+export { default as Landing } from './Landing.js';
