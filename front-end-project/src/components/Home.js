10cb4436143367f8cf78ce3b679f9404dd07e5f4
AmyShackles <amyshackles@gmail.com>
2018-06-28 19:06:49 -0700

    Changed home

--- front-end-project/src/components/Home.js
+++ front-end-project/src/components/Home.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
+import { NavLink } from 'react-router-dom'
 import { Grid } from "@material-ui/core";
 
 const Home = props => {
     return (
-    <div>
-        <h1> Welcome to Lambda Take Note! </h1>
-        <NavLink to="/login"> <button/> </NavLink>
-        <NavLink to="/register"> <button/> </NavLink>
+    <div style={{ padding: 20 }}>
+        <Grid container spacing={40}>
+            <h1> Welcome to Lambda Take Note! </h1>
+            <NavLink to="/login"> <button> Click to Log in </button> </NavLink>
+            <NavLink to="/register"> <button> Click to Register </button> </NavLink>
+        </Grid>
     </div>
     )
 }
+
+export default Home
