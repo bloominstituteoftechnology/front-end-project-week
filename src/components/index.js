4c278417da8efe24f50fa684be3088645bb8f007
A99 <A99@99s-macbook-pro.home>
2018-06-29 22:42:46 +0200

    Export NoMatch component

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export { default as AddNote } from "./AddNote.js";
 export { default as Login } from './auth/Login.js';
 export { default as Register } from './auth/Register.js';
 export { default as Landing } from './Landing.js';
+export { default as NoMatch } from './NoMatch.js';
