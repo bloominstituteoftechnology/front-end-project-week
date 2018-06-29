1f40e6a1e4ba9b08ef0aec5e84d067a0da3965b1
A99 <A99@99s-macbook-pro.home>
2018-06-29 22:40:40 +0200

    Add compDidMount & Switch router control

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Route, withRouter } from "react-router-dom";
-import { Navigation, NotesList, AddNote, Note, Landing } from "./components";
+import { Route, Switch, withRouter } from "react-router-dom";
+import { Navigation, NotesList, AddNote, Note, Landing, NoMatch } from "./components";
 import { connect } from "react-redux";
-import { resetStore } from "./actions";
+import { resetStore, loggedIn } from "./actions";
 
 class App extends Component {
 
+  componentDidMount() {
+    const token = localStorage.getItem('jwt');
+    if (token) {
+      this.props.loggedIn();
+      this.props.history.push('/notes');
+    }
+  }
 
   signoutHandler = () => {
     localStorage.removeItem('jwt');
@@ -17,19 +24,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {localStorage.getItem('jwt') ? (
-          <div>
-            <Navigation signoutHandler={this.signoutHandler}/>
-          </div>
-        ) : (
-          <Route exact path="/" render={() => 
-          <div>
-            <Landing />
-          </div>}/>
-        )}
-        <Route exact path="/notes" component={NotesList}/>
-        <Route path="/addnote" component={AddNote}/>
-        <Route path="/note/:id" component={Note}/>
+        {this.props.userLoggedIn ? (
+          <Navigation signoutHandler={this.signoutHandler}/>
+        ) : (null)}
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/notes" component={NotesList}/>
+          <Route path="/addnote" component={AddNote}/>
+          <Route path="/note/:id" component={Note}/>
+          <Route component={NoMatch}/>
+        </Switch>
       </div>
     );
   }
@@ -39,4 +43,4 @@ const mapStateToProps = state => {
   return state;
 }
 
-export default withRouter(connect(mapStateToProps, { resetStore })(App));
+export default withRouter(connect(mapStateToProps, { resetStore, loggedIn })(App));
