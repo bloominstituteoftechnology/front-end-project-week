103ee19e6e4b9ce99daf9ed0b0d16776996371c8
A99 <A99@99s-macbook-pro.home>
2018-07-01 00:53:38 +0200

    Export Menu component & updated name

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,9 @@
-export { default as Navigation } from "./Navigation.js";
+export { default as Sidebar } from "./utils/Sidebar.js";
 export { default as NotesList } from "./NotesList.js";
 export { default as Note } from "./Note.js";
 export { default as AddNote } from "./AddNote.js";
 export { default as Login } from './auth/Login.js';
 export { default as Register } from './auth/Register.js';
-export { default as Landing } from './Landing.js';
-export { default as NoMatch } from './NoMatch.js';
+export { default as Landing } from './auth/Landing.js';
+export { default as NoMatch } from './utils/NoMatch.js';
+export { default as Menu } from './utils/Menu.js';
