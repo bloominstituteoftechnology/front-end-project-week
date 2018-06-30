bec1792c6e32220be566064713ee9f2e1697e658
A99 <A99@99s-macbook-pro.home>
2018-07-01 00:53:04 +0200

    Change file architecture

---
+++ src/components/utils/NoMatch.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NoMatch = () => {
+    return (
+        <div>
+            <h3>404!</h3>
+            <h3>Oops! We don't have anything here.</h3>
+            <Link to="/">Home</Link>
+        </div>
+    );
+}
+
+export default NoMatch;
