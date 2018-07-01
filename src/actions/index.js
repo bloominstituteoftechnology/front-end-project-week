6ca6c39d7ffd1420daab3d4d7654f7b21b30dd62
MacBook <LonnieDavis@MacBookPros-MacBook-Pro.local>
2018-07-01 12:39:59 -0500

    test

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,7 +7,7 @@ export const FETCHING_NOTES = 'FETCHING_NOTES';
 export const POST_NOTE = 'POST_NOTE';
 
 //const URL = 'http://localhost:5001/api/note';
-const URL = 'https://create-notes.herokuapp.com/api/note';
+const URL = process.env.URL;
 export const deleteNote = (id) => {
     return {
         type: DELETE_NOTE,
