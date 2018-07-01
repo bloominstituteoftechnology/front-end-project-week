3a86eee2d445e4a3900302711b61bc42c7b4b27b
MacBook <LonnieDavis@MacBookPros-MacBook-Pro.local>
2018-07-01 18:36:11 -0500

    test

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,7 @@ export const CREATE_NOTE = 'CREATE_NOTE';
 export const FETCH_NOTES = 'FETCH_NOTES';
 export const FETCHING_NOTES = 'FETCHING_NOTES';
 export const POST_NOTE = 'POST_NOTE';
-require('dotenv').config();
+//require('dotenv').config();
 
 const URL = process.env.REACT_APP_API;
 
