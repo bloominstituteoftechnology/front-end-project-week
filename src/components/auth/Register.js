e0e2c7a3395869421f49ca2c7a07ff3a1cc03e7b
A99 <A99@99s-macbook-pro.home>
2018-07-03 01:24:10 +0200

    Add UI error handling & tooltip

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -9,7 +9,9 @@ class Register extends React.Component {
         email: '',
         password: '',
         firstName: '',
-        lastName: ''
+        lastName: '',
+        error: null,
+        tooltip: false
     }
 
     onChange = e => {
@@ -17,24 +19,56 @@ class Register extends React.Component {
     }
 
     handleSubmit = e => {
+        const { password, email } = this.state;
         e.preventDefault();
-        axios.post(`${process.env.REACT_APP_API_AUTH}/register`, this.state)
-            .then(response => {
-                localStorage.setItem('jwt', response.data.token);
-                this.props.loggedIn();
-                this.props.history.push('/notes');
-            })
-            .catch(err => console.log(err.message));
+        if (!email.includes('@' && '.') || email.length < 6) {
+            this.setState({ error: 'Please provide a valid email address.'});
+        } else if (password.length < 8) {
+            this.setState({ error: `Password must be at least 8 characters long.`});
+        } else {
+            axios.post(`${process.env.REACT_APP_API_AUTH}/register`, this.state)
+                .then(response => {
+                    localStorage.setItem('jwt', response.data.token);
+                    this.props.loggedIn();
+                    this.props.history.push('/notes');
+                })
+                .catch(err => {
+                    this.setState({ error: `Error processing your request. Please try again.`})
+                });
+        }
+    }
+
+    showTooltip = () => {
+        this.setState({ tooltip: true });
+    }
+
+    hideTooltip = () => {
+        this.setState({ tooltip: false });
+    }
+
+    passwordChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+        if (e.target.value.length >= 8) this.setState({ tooltip: false });
     }
 
     render() {
-        const { email, password, firstName, lastName } = this.state;
+        const { email, password, firstName, lastName, error, tooltip } = this.state;
         return (
             <div className="register-form">
                 <form>
+                    <div className="message">
+                        <p className="error">{error}</p>
+                    </div>
                     <h3>Register</h3>
                     <input type="email" name="email" value={email} placeholder="email" onChange={this.onChange}/>
-                    <input type="password" name="password" value={password} placeholder="password" onChange={this.onChange}/>
+                    <div>
+                        {tooltip ? (
+                            <div className="message tooltip">
+                                <p>8 characters minimum</p>
+                            </div>
+                        ) : (null)}
+                        <input type="password" name="password" value={password} placeholder="password" onChange={this.passwordChange} onFocus={this.showTooltip} onBlur={this.hideTooltip}/>
+                    </div>
                     <input type="text" name="firstName" value={firstName} placeholder="First Name" onChange={this.onChange}/>
                     <input type="text" name="lastName" value={lastName} placeholder="Last Name" onChange={this.onChange}/>
                     <button onClick={this.handleSubmit}>Sign Up</button>
