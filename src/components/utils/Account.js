358f7a6bc0d66e65f5c2185011d05459554b8b43
A99 <A99@99s-macbook-pro.home>
2018-07-03 21:05:59 +0200

    Add tooltips  & error message logic

--- src/components/utils/Account.js
+++ src/components/utils/Account.js
@@ -13,7 +13,9 @@ class Account extends React.Component {
         lastName: '',
         success: '',
         modal: false,
-        error: ''
+        error: '',
+        tooltip1: false,
+        tooltip2: false
     }
 
     componentDidMount() {
@@ -41,14 +43,25 @@ class Account extends React.Component {
         e.preventDefault();
         const token = localStorage.getItem('jwt');
         const requestOptions = { headers: { Authorization: token } };
-        if (this.state.password) {
-            if (this.state.password !== this.state.repeatpassword) {
+        const { email, firstName, lastName, password, repeatpassword, currentpassword } = this.state;
+        if (password.length > 0) {
+            if (password.length < 8 || currentpassword.length < 8) {
+                this.setState({
+                    success: null,
+                    error: 'Passwords must be 8 characters long.'
+                });
+            } else if (password !== repeatpassword) {
                 this.setState({
                     success: null,
                     error: 'New passwords do not match.'
-                })
+                });
+            } else if (currentpassword === password ) {
+                this.setState({
+                    success: null,
+                    error: 'New password can not match current password.'
+                });
             } else {
-                axios.post(`${process.env.REACT_APP_API_AUTH}/login`, { email: this.state.email, password: this.state.currentpassword })
+                axios.post(`${process.env.REACT_APP_API_AUTH}/login`, { email: this.state.email, password: currentpassword })
                     .then(response => {
                             localStorage.removeItem('jwt');
                             localStorage.setItem('jwt', response.data.token);
@@ -74,7 +87,7 @@ class Account extends React.Component {
                     })
             }
         } else {
-            axios.put(`${process.env.REACT_APP_API_USERS}/${this.props.id}`, this.state , requestOptions)
+            axios.put(`${process.env.REACT_APP_API_USERS}/${this.props.id}`, {email, firstName, lastName} , requestOptions)
                 .then(response => {
                     this.setState({ 
                         error: null,
@@ -108,11 +121,40 @@ class Account extends React.Component {
             });
         }
 
+    showTooltip1 = () => {
+        this.setState({ tooltip1: true });
+    }
+
+    hideTooltip1 = () => {
+        this.setState({ tooltip1: false });
+    }
+
+    passwordChange1 = e => {
+        this.setState({ [e.target.name]: e.target.value });
+        if (e.target.value.length >= 8) this.setState({ tooltip1: false });
+        else this.setState({ tooltip1: true });
+    }
+
+    showTooltip2 = () => {
+        this.setState({ tooltip2: true });
+    }
+
+    hideTooltip2 = () => {
+        this.setState({ tooltip2: false });
+    }
+
+    passwordChange2 = e => {
+        this.setState({ [e.target.name]: e.target.value });
+        if (e.target.value.length >= 8) this.setState({ tooltip2: false });
+        else this.setState({ tooltip2: true });
+    }
+
 
     render() {
-        const { email, currentpassword, password, repeatpassword, firstName, lastName, success, modal, error } = this.state;
+        const { email, currentpassword, password, repeatpassword, firstName, lastName, success, modal, error, tooltip1, tooltip2 } = this.state;
         return (
             <div className="account-page">
+
                     <div className="message">
                         {success ? (
                             <p className="success">{success}</p>
@@ -122,6 +164,7 @@ class Account extends React.Component {
                             <p className="error">{error}</p>
                         ) : (null)}
                     </div>
+
                 <form className="account-form">
                     <h3>Account Details</h3>
                     <label htmlFor="email">Email:</label>
@@ -130,15 +173,32 @@ class Account extends React.Component {
                     <input type="text" name="firstName" value={firstName} placeholder="First Name" onChange={this.onChange}/>
                     <label htmlFor="lastName">Last Name:</label>
                     <input type="text" name="lastName" value={lastName} placeholder="Last Name" onChange={this.onChange}/>
+
                     <h3>Update Password</h3>
                     <input type="password" name="currentpassword" value={currentpassword} placeholder="Current Password" onChange={this.onChange}/>
-                    <input type="password" name="password" value={password} placeholder="New Password" onChange={this.onChange}/>
-                    <input type="password" name="repeatpassword" value={repeatpassword} placeholder="Repeat New Password" onChange={this.onChange}/>
+                    <div>
+                        {tooltip1 ? (
+                            <div className="message tooltip">
+                                <p>8 characters minimum</p>
+                            </div>
+                        ) : (null)}
+                        <input type="password" name="password" value={password} placeholder="New Password" onChange={this.passwordChange1} onFocus={this.showTooltip1} onBlur={this.hideTooltip1}/>
+                    </div>
+                    <div>
+                        {tooltip2 ? (
+                            <div className="message tooltip">
+                                <p>8 characters minimum</p>
+                            </div>
+                        ) : (null)}
+                        <input type="password" name="repeatpassword" value={repeatpassword} placeholder="Repeat New Password" onChange={this.passwordChange2} onFocus={this.showTooltip2} onBlur={this.hideTooltip2}/>
+                    </div>
+
                     <div>
                         <a className="button delete" onClick={() => this.toggleModal()}>Delete Account</a>
                         <a className="button" onClick={this.handleUpdate}>Save</a>
                     </div>
                 </form>
+
                 {modal ? (
                         <div>
                             <div className="modal-background"></div>
@@ -154,6 +214,7 @@ class Account extends React.Component {
                             </div>
                         </div>
                     ) : (null)}
+
             </div>
         );
     }
