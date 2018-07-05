ed5490c1f37dba3f3e744c5bfb23648137b1917e
A99 <A99@99s-macbook-pro.home>
2018-07-05 15:07:24 +0200

    Add verification to JWT on App mount & updated log in/out actions

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
+import jwt from "jsonwebtoken";
 import './App.css';
 import { Route, Switch, withRouter } from "react-router-dom";
 import { Sidebar, Menu, NotesList, AddNote, Note, Landing, Account, NoMatch, Deleted } from "./components";
 import { connect } from "react-redux";
-import { loggedIn } from "./actions";
+import { logIn, logOut, resetStore, setAccount, fetchNotes } from "./actions";
 
 class App extends Component {
 
   componentDidMount() {
     const token = localStorage.getItem('jwt');
     if (token) {
-      this.props.loggedIn();
-      this.props.history.push('/notes');
+      jwt.verify(token, process.env.REACT_APP_JWT_SECRET, (err, decoded) => {
+        if (err) {
+          this.props.resetStore();
+          this.props.logOut();
+          localStorage.removeItem('jwt');
+        }
+        if (decoded) {
+          this.props.logIn();
+          this.props.history.push('/notes');
+        }
+      });
     }
   }
 
@@ -45,4 +55,4 @@ const mapStateToProps = state => {
   return state;
 }
 
-export default withRouter(connect(mapStateToProps, { loggedIn })(App));
+export default withRouter(connect(mapStateToProps, { logIn, logOut, resetStore, setAccount, fetchNotes })(App));
