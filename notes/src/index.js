96d4cc2a09e166e2a3ea938456551ac72dfe2d39
nchen0 <nicky.chen0@gmail.com>
2018-07-16 14:47:26 -0500

    project setup

--- notes/src/index.js
+++ notes/src/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import {
+  createStore,
+  applyMiddleware,
+  compose
+} from "redux";
+import { notesReducer } from "./reducers/reducer";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const composeEnhancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  notesReducer,
+  composeEnhancers(applyMiddleware(thunk, logger))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
---
+++ notes/src/reducers/reducer.js
@@ -0,0 +1,6 @@
+export const notesReducer = (state = [], action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
