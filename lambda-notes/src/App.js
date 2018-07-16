3d29283dde1fbd30b037489cc7c9f6e640b8d8e6
Randy Calderon <randycweb@gmail.com>
2018-07-16 11:12:10 -0700

    Set up folders

---
+++ lambda-notes/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import styled from 'styled-components';
+
+const MainContainer = styled.div`
+  display: flex;
+`;
+
+class App extends Component {
+  render() {
+    return (
+    <MainContainer>
+      </MainContainer>
+    );
+  }
+}
+
+export default App;
