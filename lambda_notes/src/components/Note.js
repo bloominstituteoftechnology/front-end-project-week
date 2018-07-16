e442c5c68249d7b6e72b900b9044cc80a79eda3c
TangledTessellations <mcevoya@oregonstate.edu>
2018-07-16 12:01:31 -0700

    Build rough structure for Note.js

--- lambda_notes/src/components/Note.js
+++ lambda_notes/src/components/Note.js
@@ -1,10 +1,107 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-const Note = props => {
-    console.log(props.match);
-    return(
-        <div>{props.match.params.id}</div>
-    );
+const StyledNote = styled.div`
+    display: flex;
+    flex-flow: column;
+    background: #F3F3F3;
+    width: 100%;
+    padding: 2%;
+    align-items: flex-end;
+`;
+
+const StyledButtons = styled.div`
+    display: flex;
+    align-self: flex-end;
+    width: 100%;
+`;
+
+const StyledLink = styled(Link)`
+    text-decoration: none;
+    color: black;
+`;
+
+class Note extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            notes: [{
+                tags: [],
+                title: "Learn Redux1",
+                textBody: "You should really learn Redux, Here's a note with a lot of text.Here's a note with a lot of text.Here's a note with a lot of text.Here's a note with a lot of text.Here's a note with a lot of text. Here's a note with a lot of text.Here's a note with a lot of text.Here's a note with a lot of text.",
+                id: 1,
+            }, 
+            {
+              tags: [],
+              title: "Learn Redux2",
+              textBody: "You should really learn Redux",
+              id: 2,
+            }, 
+            {
+              tags: [],
+              title: "Learn Redux3",
+              textBody: "You should really learn Redux",
+              id: 3,
+            }, 
+            {
+              tags: [],
+              title: "Learn Redux4",
+              textBody: "You should really learn Redux",
+              id: 4,
+            }, 
+            {
+              tags: [],
+              title: "Learn Redux5",
+              textBody: "You should really learn Redux",
+              id: 5,
+            }],
+            currentNote: {
+                tags: [],
+                title: "Learn Redux0",
+                textBody: "You should really learn Redux, Here's a note with a lot of text.Here's a note with a lot of text.Here's a note with a lot of text.Here's a note with a lot of text.Here's a note with a lot of text. Here's a note with a lot of text.Here's a note with a lot of text.Here's a note with a lot of text.",
+                id: 1,
+            },
+            match: props.match,
+            id: 1,
+        }
+    }
+
+    componentDidMount(){
+        console.log("ComponentDidMount");
+        const id = this.state.match.params.id;
+        this.setState({id: id});
+        this.getNote(id);
+    }
+
+    componentWillReceiveProps(newProps){
+        console.log("componentWillREeiveProps");
+        if(this.state.id !== newProps.match.params.id){
+            this.getNote(newProps.match.params.id);
+        }
+    }
+
+    getNote = id => {
+        const note = this.state.notes.filter(note => note.id === Number(id));
+        console.log("Note", note);
+        this.setState({currentNote: note[0]});
+
+    }
+
+    render(){
+        console.log("currentNote: ", this.state.currentNote);
+        return(
+            <StyledNote>
+                <StyledButtons>
+                    <StyledLink to={'./'}>edit</StyledLink>
+                    <StyledLink to={'./'}>delete</StyledLink>
+                </StyledButtons>
+                <h4>{this.state.currentNote.title}</h4>
+                <p>{this.state.currentNote.textBody}</p>
+            </StyledNote>
+        );
+    }
+
 }
 
 export default Note;
