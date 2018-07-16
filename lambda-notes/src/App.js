cd7d9dc6e2360a9dd18ee18a1bb37c3b79db8703
snowcoding <vimalshah77@gmail.com>
2018-07-16 10:08:50 -0700

    Basic Project Setup
    - Removed Boilerplate CRA code
    - Set up container CSS
    - Added styled components
    - Added reactstrap
    - Added Google Fonts
    - Set up MainContainer

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
+
+const MainContainer = styled.div`
+  border: 1px solid red;
+  max-width: 900px;
+  width: 100%;
+  margin: 0 auto;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MainContainer>Hello</MainContainer>
     );
   }
 }
