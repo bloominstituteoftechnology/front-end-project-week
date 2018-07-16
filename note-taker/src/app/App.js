9deb6bfe0260fadc88593a69de70be4b51170326
Simon <Simon_Freelance@hotmail.com>
2018-07-16 16:17:24 -0400

    Started importing and adding components to app and styling it

--- note-taker/src/app/App.js
+++ note-taker/src/app/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import { Route } from "react-router-dom";
+import SidebarContainer from "./common/Sidebar/SidebarContainer";
+import HomeContainer from "./home/HomeContainer";
+import './styles/custom-props.css';
+const AppContainer = styled.div`
+  display: flex;
 
-
+`;
+const MainContentContainer = styled.div`
+  background: lightblue;
+  width: 100%;
+  position: absolute;
+  left: var(--sidebar-width);
+`;
 const App = () => {
-    return (
-      <div className="App">
-        
-      </div>
-    );
-}
+  return (
+    <AppContainer>
+      <SidebarContainer />
+      <MainContentContainer>
+        <Route exact path="/" component={HomeContainer} />
+      </MainContentContainer>
+    </AppContainer>
+  );
+};
 
 export default App;
