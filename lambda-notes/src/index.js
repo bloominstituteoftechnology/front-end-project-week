8c549e21d59fe19b321a22ab4831506866203eb4
DirupT <iamcooled@gmail.com>
2018-07-16 10:49:38 -0500

    set up basic file structure

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
