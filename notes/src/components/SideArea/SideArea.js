7bd77629e94f2b7e7b9db4930a84398265baafae
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-07-16 09:15:50 -0700

    divided area into two fields

---
+++ notes/src/components/SideArea/SideArea.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class SideArea extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div className='sideArea'>
+                hi from side Area
+            </div> );
+    }
+}
+ 
+export default SideArea;
