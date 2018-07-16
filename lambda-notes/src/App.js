83a0166797ba3b4499838224501734c8588375be
Allen Hai <allenhai03@gmail.com>
2018-07-16 14:46:42 -0500

    add state to hold notes in App component

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -4,20 +4,42 @@ import NoteCardsContainer from './components/NoteCardsContainer'
 import NoteCard from './components/NoteCard'
 
 class App extends Component {
+ 
+  state = { 
+    notes: [
+      {
+        id: 0,
+        title: 'React',
+        text: 'React is a UI Library developed by Facebook'
+      },
+      {
+        id: 1,
+        title: 'Redux',
+        text: 'Redux makes managing complex state a breeze'
+      },
+      {
+        id: 2,
+        title: 'Complacency',
+        text: 'We have to keep pushing ourselves'
+      }
+    ] 
+  }
+  
   render() {
+
+    const { notes } = this.state
+
     return (
       <div style={{display: 'flex'}}>
         <Sidebar />
         <NoteCardsContainer>
-          <NoteCard />
-          <NoteCard />
-          <NoteCard />
-          <NoteCard />
-          <NoteCard />
-          <NoteCard />
-          <NoteCard />
-          <NoteCard />
-          <NoteCard />
+          {notes.map((note, index) =>
+            <NoteCard 
+              key={note.id} 
+              title={note.title}
+              text={note.text}
+            />
+          )}
         </NoteCardsContainer>
       </div>
     );
