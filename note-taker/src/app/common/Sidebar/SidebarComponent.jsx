143098afb1c1d4805e1c74061b19eb8c3fe7ed34
Simon <Simon_Freelance@hotmail.com>
2018-07-16 15:42:26 -0400

    Sidebar styling completed with links.

--- note-taker/src/app/common/Sidebar/SidebarComponent.jsx
+++ note-taker/src/app/common/Sidebar/SidebarComponent.jsx
@@ -1,12 +1,49 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import "../../styles/custom-props.css";
+import GeneralBtn from "../GeneralBtn/GeneralBtn";
+import { Link } from "react-router-dom";
+const SidebarWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  padding: 20px;
+  width: 300px;
+  background: var(--sidebar-bg);
+`;
+const Header = styled.h1`
+  font-weight: 700;
+  font-family: "Roboto", sans-serif;
+  font-size: 5rem;
+  margin-bottom: 40px;
+`;
 
+const ButtonsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  & > a {
+    margin-bottom: 20px;
+  }
+`;
+const UnstyledLink = styled(Link)``;
 
+const SidebarComponent = props => {
+  return (
+    <SidebarWrapper>
+      <Header>Lambda Notes</Header>
+      <ButtonsContainer>
+        <Link to="/">
+          <GeneralBtn>View your notes</GeneralBtn>
+        </Link>
+        <Link to = '/create-new'>
+          <GeneralBtn> + Create new note</GeneralBtn>
+        </Link>
+      </ButtonsContainer>
+    </SidebarWrapper>
+  );
+};
 
-const SidebarComponent = (props) => {
-    return ( 
-        <div>s</div>
-     );
-}
- 
 export default SidebarComponent;
