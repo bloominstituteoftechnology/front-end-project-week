eaf84c55a00242088a2f7d7d604a3871b6cc71ae
Vu Cao <cdhvu@yahoo.com>
2018-07-16 12:05:10 -0700

    Created Note.js and added some new packages

--- lambda_notes/src/App.js
+++ lambda_notes/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
-
 import NoteList from './components/NoteList';
-
+  
 class App extends Component {
   render() {
     return (
--- lambda_notes/src/components/Note.js
+++ lambda_notes/src/components/Note.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 const Note = props => {
     return (
         <div>
             <h1>{props.note.title}</h1>
             <p>{props.note.textBody}</p>
         </div>
-      );
+    );
 }
- 
+
+Note.propTypes = PropTypes.shape({
+    "tags": PropTypes.string.isRequired,
+    "_id": PropTypes.string.isRequired,
+    "title": PropTypes.string.isRequired,
+    "textBody": PropTypes.string.isRequired,
+    "__v": PropTypes.number.isRequired,
+}).isRequired
+
 export default Note;
