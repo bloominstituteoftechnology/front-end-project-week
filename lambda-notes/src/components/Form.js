1f7b191497d31607b1ccedb635f15abf5e5161c9
Gabriel Duquette <gabrielduquette@gmail.com>
2018-07-16 13:45:32 -0400

    view an existing note

---
+++ lambda-notes/src/components/Form.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+
+const Form = props => {
+  return (
+    <div>
+      <form>
+        <input />
+        <input />
+      </form>
+    </div>
+  );
+}
+
+export default Form;
