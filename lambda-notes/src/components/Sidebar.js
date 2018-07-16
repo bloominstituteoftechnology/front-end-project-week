c214712af1cf44201bd73ec46c4399d3fa74e779
Gabriel Duquette <gabrielduquette@gmail.com>
2018-07-16 13:22:06 -0400

    add URL to README

--- lambda-notes/src/components/Sidebar.js
+++ lambda-notes/src/components/Sidebar.js
@@ -4,7 +4,10 @@ import { Link } from 'react-router-dom';
 
 const Sidebar = props => {
   return (
-    <div>
+    <div className="sidebar-container">
+      <h1>Lambda Notes</h1>
+      <button>View Your Notes</button>
+      <button>+ Create New Note</button>
     </div>
   )
 }
