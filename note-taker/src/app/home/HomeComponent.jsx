7cacbe81a551b650e3cb57830f5f965edf112a56
Simon <Simon_Freelance@hotmail.com>
2018-07-16 15:59:36 -0400

    Made the starting home components

---
+++ note-taker/src/app/home/HomeComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HomeComponent = (props) => {
+    return (
+        <div>Home</div>
+    );
+}
+ 
+export default HomeComponent;
---
+++ note-taker/src/app/home/HomeContainer.js
@@ -0,0 +1,6 @@
+import { connect } from 'react-redux';
+import HomeComponent from './HomeComponent';
+
+const HomeContainer = connect(null)(HomeComponent);
+
+export default HomeContainer;
