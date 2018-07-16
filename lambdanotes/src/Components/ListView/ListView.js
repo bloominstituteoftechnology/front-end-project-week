b5b93d87cc3c0fcde7b34f93a905c12fe5365941
Joe Stanfield <jlstan06@hotmail.com>
2018-07-16 13:17:24 -0400

    initial pull request and setup

---
+++ lambdanotes/src/Components/ListView/ListView.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ListView extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  
+            <div>Testing Notes</div>
+        );
+    }
+}
+ 
+export default ListView;
