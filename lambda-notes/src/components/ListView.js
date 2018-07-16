872e63b4b8c9d60e0f0609bdb5c99e6f4bb916fc
snowcoding <vimalshah77@gmail.com>
2018-07-16 12:10:25 -0700

    ## MVP Features:
    - [x] Display a list of notes

--- lambda-notes/src/components/ListView.js
+++ lambda-notes/src/components/ListView.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const NoteCardContainer = styled.div`
+  display:flex;
+  flex-wrap:wrap;
+`
+
+const NoteCard = styled.div`
+  background-color: white;
+  border: 1px solid #979797;
+  margin-right: 10px;
+  margin-top: 20px;
+  width: 190px;
+  height: 200px;
+  overflow: hidden;
+`
 
 class ListView extends Component {
   render() {
     return (
-      <div>
-        {this.props.notes.map ( note => <h2 key={Math.random()}>{note.title}</h2>) }
-      </div>
+      <NoteCardContainer>
+        {this.props.notes.map ( note => <NoteCard key={Math.random()}><h2>{note.title}</h2><p>{note.textBody}</p></NoteCard>) }
+      </NoteCardContainer>
     );
   }
 }
