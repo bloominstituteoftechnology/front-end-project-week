bcc3409be95669f6c19da17ff5c9315634e34a93
DirupT <iamcooled@gmail.com>
2018-07-16 12:58:50 -0500

    updated styling of notes card

--- lambda-notes/src/components/NotesContainer/NotesCard.js
+++ lambda-notes/src/components/NotesContainer/NotesCard.js
@@ -4,6 +4,7 @@ const NotesCard = () => {
     return (
         <div className='notes-card'>
             <h3 className='note-title'>Note Title</h3>
+            <p className='note-paragraph'>Morbi pellentesqueeuismod venenatis.Nulla ut nibh nunc.Phasellus diam metus,blandit ac purus a,efficitur mollis Morbi pellentesqueeuismod venenatis.Nulla ut nibh nunc.Phasellus diam metus,blandit ac purus a,efficitur mollis</p>
         </div>
     );
 }
