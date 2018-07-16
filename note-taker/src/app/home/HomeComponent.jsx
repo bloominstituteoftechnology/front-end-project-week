a7ab7b4033a6d361f312af3da2df2856d1369f65
Simon <Simon_Freelance@hotmail.com>
2018-07-16 17:26:11 -0400

    Started the column system and the HomeComponent

--- note-taker/src/app/home/HomeComponent.jsx
+++ note-taker/src/app/home/HomeComponent.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
+import HeadingTitle from '../common/Titles/HeadingTitle';
+import 'bulma/css/bulma.css';
+import '../styles/custom-props.css';
+const HomeWrapper = styled.div`
 
+`;
+
+
+
+const CardTitle = styled.div.attrs({
+    className: 'card-header-title is-8'
+})`
+    text-transform: 'capitalize';
+    border-bottom: 2px solid var(--main-border-color);
+    padding-bottom: 1rem;
+`;
 const HomeComponent = (props) => {
     return (
-        <div>Home</div>
+        <HomeWrapper>
+            <HeadingTitle>Your Notes:</HeadingTitle>
+            <div className = 'columns is-multiline'>
+                <div className = 'column is-one-third'>
+                
+                </div> {/*column*/}
+            </div> {/*columns*/}
+        </HomeWrapper>
     );
 }
  
