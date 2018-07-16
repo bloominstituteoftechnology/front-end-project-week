8c2100435b1de76e47c72264d501b9f812281f75
Alec <afroenygam@gmail.com>
2018-07-16 17:14:57 -0400

    POST functionality updated

--- notes/src/components/CreateNote.js
+++ notes/src/components/CreateNote.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../App.css';
 import {NavLink} from 'react-router-dom';
+import axios from 'axios';
 
 class CreateNote extends React.Component {
   constructor(props) {
@@ -14,8 +15,20 @@ class CreateNote extends React.Component {
   handleChange = event => {
     this.setState({[event.target.name]: event.target.value})
   }
+
+  saveNote = event => {
+    const newNote = {title: this.state.title, textBody: this.state.content}
+    event.preventDefault();
+    axios.post('https://killer-notes.herokuapp.com/note/create', newNote)
+    .then(response => {
+      console.log(response)
+    })
+    .catch(err => {
+      console.log("Error is:", err);
+    });
+  }
+
   render() {
-    console.log(this.state);
     return (
     <div className="note-list">
       <div className="list-sidebar">
@@ -25,7 +38,7 @@ class CreateNote extends React.Component {
       </div>
       <div className="right-bar">
         <h3 className="note-list-header">Create New Note: </h3>
-        <form className="testing-form">
+        <form className="testing-form" onSubmit={this.saveNote}>
         <input className="title-input" name="title" placeholder="note title" value={this.state.title} onChange={this.handleChange}/>
         <br/>
         <input name="content" className="content-input" placeholder="Note Content" value={this.state.content} onChange={this.handleChange}/>
