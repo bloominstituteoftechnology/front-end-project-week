05d4f791ca0ce8636c094dde99779b680f39eb5e
Simon <Simon_Freelance@hotmail.com>
2018-07-16 15:08:14 -0400

    Added store to Provider component

--- note-taker/src/index.js
+++ note-taker/src/index.js
@@ -13,7 +13,7 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 ReactDOM.render(
-  <Provider>
+  <Provider store = {store}>
     <Router>
       <App />
     </Router>
