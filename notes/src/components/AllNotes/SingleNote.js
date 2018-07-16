e707834e4bf5303c976b562438eeed653b670a14
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-07-16 10:32:02 -0700

    displayed notes

---
+++ notes/src/components/AllNotes/SingleNote.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class SingleNote extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        console.log('this props in sindle note: ', this.props)
+        return ( 
+            
+                <div className='singleNote'>
+                    <Link to={`note/${this.props.item.id}`} className='linksNotes'>
+                    <h4>{this.props.item.title}</h4>
+                    <hr />
+                    <p>{this.props.item.text}</p>
+                    </Link>
+                </div>
+            
+         );
+    }
+}
+ 
+export default SingleNote;
