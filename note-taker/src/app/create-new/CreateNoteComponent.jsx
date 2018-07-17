64d583b69c908a01734e0853717a1ea90f024622
Simon <Simon_Freelance@hotmail.com>
2018-07-17 13:28:23 -0400

    Prevented form default and linked button to route to home

--- note-taker/src/app/create-new/CreateNoteComponent.jsx
+++ note-taker/src/app/create-new/CreateNoteComponent.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "bulma/css/bulma.css";
 import HeadingTitle from "../common/Titles/HeadingTitle";
 import GeneralBtn from "../common/GeneralBtn/GeneralBtn";
-
+import { Link } from 'react-router-dom';
 const CreateNoteComponent = props => {
   return (
     <div>
       <HeadingTitle>Create new note:</HeadingTitle>
-      <form>
+      <form onSubmit = {e => {
+          e.preventDefault();
+          }}>
         <div className="field">
           <div className="control">
             <input
@@ -20,7 +22,9 @@ const CreateNoteComponent = props => {
         <div className = "field">
         <textarea className="textarea" placeholder="Note Content" rows="10"></textarea>
         </div>
+        <Link to = '/'>
         <GeneralBtn width = "15rem">Save</GeneralBtn>
+        </Link>
       </form>
     </div>
   );
