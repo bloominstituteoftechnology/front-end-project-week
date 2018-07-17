18aaf552feb9cd8c0ef8670886f700603c55d152
Sukhada Gholba <sukhadagholba@gmail.com>
2018-07-17 16:00:16 -0700

    added reactstrap modal to delete button

--- lambda-notes/src/components/EachNote/EachNoteBody.js
+++ lambda-notes/src/components/EachNote/EachNoteBody.js
@@ -4,15 +4,26 @@ import {Link} from 'react-router-dom';
 import { connect } from 'react-redux';
 import {fetchingSingleNote, deleteNoteAction} from "../../actions";
 import LambdaLeftDiv from '../ViewNotes/LambdaLeftDiv';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 class EachNoteBody extends React.Component {
 
 constructor(props){
         super(props);
         this.state= {
+		modal: false
         };
 
-}
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
+
 
 componentDidMount() {
         const id = this.props.match.params.id;
@@ -32,7 +43,19 @@ render() {
                 <LambdaLeftDiv />
 		<div className="note-card-container">
 		<div>{this.props.deleted ? (<h1>Note Deleted..</h1>) :(null)}</div>
-		<button className="delete-btn" onClick={this.deleteNote}>delete</button>
+
+        <Button className="delete-btn"  onClick={this.toggle}>delete</Button>
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+          <ModalHeader toggle={this.toggle}>Are You Sure You Want To Delete This</ModalHeader>
+         <ModalFooter> 
+            <Button className="delete-modal-btn" onClick={this.deleteNote}>Delete</Button>
+            <Button className="cancel-modal-btn" onClick={this.toggle}>No</Button>
+	</ModalFooter>
+        </Modal>
+
+
+
+
 		<h3 className="single-note-title">{this.props.single.title}</h3>
 		<div className="single-note-container">{this.props.single.textBody}</div>
 		</div>
