d7587c11eedefdd51fdb0ff050bd2a3effcd14ff
Ari <ari@Nubias-iMac.local>
2018-07-17 10:34:46 -0700

    created Edit Note View

--- lambdanotes/src/components/EditNote/EditNote.js
+++ lambdanotes/src/components/EditNote/EditNote.js
@@ -5,7 +5,20 @@ export default class EditNote extends Component {
   render() {
     return (
       <div>
-        Edit Note Place Holder
+        <h2 className="mb-4">Edit note:</h2>
+        <input
+          type="text" 
+          className="mb-2 w-50"
+          placeholder="Note Title"
+        /><br />
+        <textarea 
+          className="mb-2 w-75"
+          placeholder="Note Content"
+          rows="15"
+        /><br />
+        <button type="submit" class="btn btn-info btn-large px-5">
+          Update
+        </button>
       </div>
     )
   }
