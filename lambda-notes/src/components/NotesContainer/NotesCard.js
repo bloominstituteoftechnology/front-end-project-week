7b211dcc48ece34ea7a89aa191a0a44b0c74cd95
DirupT <iamcooled@gmail.com>
2018-07-17 09:22:02 -0500

    updated styling / added ellipsis on note cards

--- lambda-notes/src/components/NotesContainer/NotesCard.js
+++ lambda-notes/src/components/NotesContainer/NotesCard.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import LinesEllipsis from 'react-lines-ellipsis'
 
 const NotesCard = props => {
     return (
         <Link className='notes-card-link' to={`/notes/${props.note._id}`}>
-        
+
             <div className='notes-card'>
                 <h3 className='note-title'>{props.note.title}</h3>
-                <p className='note-paragraph'>{props.note.textBody}</p>
+                <LinesEllipsis
+                    className='note-paragraph'
+                    text={props.note.textBody}
+                    maxLine='6'
+                    ellipsis=' ...'
+                    trimRight
+                    basedOn='words'
+                />
             </div>
 
         </Link>
