af06cf6892f31f31d9396b258bdc883df5b45ec7
snowcoding <vimalshah77@gmail.com>
2018-07-17 16:27:55 -0700

    Styling the View Note UI

--- lambda-notes/src/components/ViewNote.js
+++ lambda-notes/src/components/ViewNote.js
@@ -1,21 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
+const Header = styled.div`
+  font-family: 'Roboto', sans-serif;
+  font-size: 2rem;
+  margin-top: 16px;
+  margin-bottom: 25px;
+`
+
+const EditDelSpan = styled.span`
+  padding-left:1%;
+`
+
+const EditDelDiv = styled.div`
+  font-family: 'Roboto', sans-serif;
+  text-align:right;
+  padding-right: 12px;
+  margin-top: 16px;
+  font-size: 1.5rem;
+`
+
+const TextBody = styled.div`
+  font-family: 'Roboto', sans-serif;
+  font-size: 1.4rem;
+  line-height: 25px;
+`
 const ViewNote = (props) => {
   // (props) ? console.log(props.match.params.id) : null;
 
   return (
     <div>
-      <div>
-        <span> <Link to={`/edit-note/${props.id}`}>edit </Link> </span>
-         <span> delete </span>
-      </div>
-      <div>
+      <EditDelDiv>
+        <EditDelSpan> <Link to={`/edit-note/${props.id}`}>edit </Link> </EditDelSpan>
+         <EditDelSpan> delete </EditDelSpan>
+      </EditDelDiv>
+      <Header>
         {props.title}
-      </div>
-      <div>
+      </Header>
+      <TextBody>
         {props.textBody}
-      </div>
+      </TextBody>
     </div>
   );
 };
