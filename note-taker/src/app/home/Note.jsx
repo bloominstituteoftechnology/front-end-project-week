0e732a95ffb1baf4e0cd31238f4816e258f5fc60
Simon <Simon_Freelance@hotmail.com>
2018-07-17 11:28:51 -0400

    Started the Notes components

---
+++ note-taker/src/app/home/Note.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+const Note = (props) => {
+    return <div>note</div>;
+}
+ 
+export default Note;
---
+++ note-taker/src/app/home/NotesList.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Note from './Note';
+
+const NotesList = (props) => {
+    return <Note />
+}
+ 
+export default NotesList;
