81ef1280d529a8e153a6d5742271c73f4ced5468
Simon <Simon_Freelance@hotmail.com>
2018-07-17 11:17:40 -0400

    Applied redux-thunk middleware

--- note-taker/src/index.js
+++ note-taker/src/index.js
@@ -4,13 +4,14 @@ import "./index.css";
 import App from "./app/App";
 import registerServiceWorker from "./registerServiceWorker";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
 import { BrowserRouter as Router } from "react-router-dom";
-
+import thunk from 'redux-thunk';
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
 );
 ReactDOM.render(
   <Provider store = {store}>
