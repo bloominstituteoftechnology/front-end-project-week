daf026cf7dea50ef3c59542a846cbc9af296c8b7
chainchompa <chainchompa@gmail.com>
2018-07-17 15:01:44 -0400

    styled add note page

--- lambda-notes/src/components/AddNote.js
+++ lambda-notes/src/components/AddNote.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 const AddNote = props => {
   return (
-    <div>
+    <div className='form'>
     <h2>Create New Note:</h2>
     <form>
       <input name='newTitle' type='text' value={props.title} onChange={props.handleInputChange} placeholder='Note Title'/>
       <textarea name='newTextBody' type='text' value={props.body} onChange={props.handleInputChange} placeholder='Note Content'/>
-      <Link style={{ textDecoration: 'none' }} to={`/`}><div onClick={props.handleAddNote}>save</div></Link>
+      <Link style={{ textDecoration: 'none' }} to={`/`}><div className='save' onClick={props.handleAddNote}>Save</div></Link>
       </form>
     </div>
   )
