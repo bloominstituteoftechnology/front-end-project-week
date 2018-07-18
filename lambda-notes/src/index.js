65dba627853e56b0243a777cba6da048581756aa
cmok4290 <cmok4290@gmail.com>
2018-07-18 11:06:52 -0400

    added connected-react-router

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -1,12 +1,54 @@
+import { AppContainer } from 'react-hot-loader';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { createBrowserHistory } from 'history';
+import { routerMiddleware, connectRouter } from 'connected-react-router';
+import { Provider } from 'react-redux';
+import {BrowserRouter as Router} from 'react-router-dom';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import rootReducer from './reducers';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
-import {BrowserRouter as Router} from 'react-router-dom';
 
-ReactDOM.render(
-        <Router>
-            <App />
-        </Router>,
-    document.getElementById('root')
+import './index.css';
+
+const history = createBrowserHistory();
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    connectRouter(history)(rootReducer),
+    composeEnhancer(
+        applyMiddleware(
+            routerMiddleware(history),
+            thunk,
+            logger
+        ),
+    ),
 );
+
+const render = () => {
+    ReactDOM.render(
+        <AppContainer>
+            <Provider store={store}>
+                <App history={history} />
+            </Provider>
+        </AppContainer>,
+        document.getElementById('root')
+    );
+};
+
+render();
+
+// Hot reloading
+if(module.hot) {
+    // Reload components
+    module.hot.accept('./components/App', () => {
+        render()
+    });
+
+    // Reload reducers
+    module.hot.accept('./reducers', () => {
+        store.replaceReducer(connectRouter(history)(rootReducer))
+    });
+}
