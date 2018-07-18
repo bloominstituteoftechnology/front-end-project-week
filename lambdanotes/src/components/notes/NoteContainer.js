7cfb5423d5b823e4201ca489ae31440fcfe4a5d1
Mariana Cerda <raymond_grc@yahoo.com>
2018-07-18 14:02:23 -0500

    Delete notesconatiner constructor

--- lambdanotes/src/components/notes/NoteContainer.js
+++ lambdanotes/src/components/notes/NoteContainer.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import Notes from "./Notes"
 
 class NoteContainer extends Component {
-  constructor(props){
-    super(props);
-  }
   render(){
     return(
       <main>
