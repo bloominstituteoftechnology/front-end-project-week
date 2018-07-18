5e98ac49fec1a7dd86fe31ea4c90e01c4468d27a
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-07-18 11:02:44 -0400

    Added delete modal with react-modal

--- lambda-notes/src/components/Note.js
+++ lambda-notes/src/components/Note.js
@@ -2,6 +2,18 @@ import React, { Component } from "react";
 import axios from "axios";
 import CreateNote from "./CreateNote";
 import { Redirect } from "react-router-dom";
+import Modal from "react-modal";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)"
+  }
+};
 
 class Note extends Component {
   constructor(props) {
@@ -11,8 +23,26 @@ class Note extends Component {
       title: "",
       textBody: "",
       edit: false,
-      deleted: false
+      deleted: false,
+      modalIsOpen: false
     };
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({ modalIsOpen: true });
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = "#f00";
+  }
+
+  closeModal() {
+    this.setState({ modalIsOpen: false });
   }
 
   componentDidMount() {
@@ -62,12 +92,27 @@ class Note extends Component {
       <div className="note">
         <div className="editdeletebuttons">
           <a onClick={this.editNote}>edit</a>
-          <a onClick={this.deleteNote}>delete</a>
+          <a onClick={this.openModal}>delete</a>
         </div>
         <h3 className="">{this.state.title}</h3>
         <div>
           <div className="">{this.state.textBody}</div>
         </div>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <div>
+            <h2 ref={subtitle => (this.subtitle = subtitle)}>
+              Are you sure you want to delete this?
+            </h2>
+            <button onClick={this.deleteNote}>Delete</button>
+            <button onClick={this.closeModal}>No</button>
+          </div>
+        </Modal>
       </div>
     );
   }
