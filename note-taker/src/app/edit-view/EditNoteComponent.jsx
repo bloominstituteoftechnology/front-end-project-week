5d7aed25734741b64c2dd9e8f9440c87e4fabc3c
Simon <Simon_Freelance@hotmail.com>
2018-07-18 13:33:15 -0400

    The values for the title and body are filled in the form

--- note-taker/src/app/edit-view/EditNoteComponent.jsx
+++ note-taker/src/app/edit-view/EditNoteComponent.jsx
@@ -1,7 +1,65 @@
-import React from 'react';
+import React from "react";
+import HeadingTitle from "../common/Titles/HeadingTitle";
+import GeneralBtn from "../common/GeneralBtn/GeneralBtn";
+import { Link } from "react-router-dom";
+class EditNoteComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "",
+      textBody: ""
+    };
+  }
+  componentDidMount() {
+      const id = this.props.match.params.id;
+      this.props.fetchNote(id);
+      this.setState({ title: this.props.title, textBody: this.props.textBody });
+  }
+  changeHandler = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  render() {
+    const id = this.props.match.params.id;
 
-const EditNoteComponent = (props) => {
-    return ( <div>edit</div> );
+    return (
+      <div>
+        <HeadingTitle>Edit Note:</HeadingTitle>
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+          }}
+        >
+          <div className="field">
+            <div className="control">
+              <input
+                className="input is-large"
+                type="text"
+                name="title"
+                value={this.state.title}
+                placeholder="Note Title"
+                onChange={this.changeHandler}
+              />
+            </div>
+          </div>
+          <div className="field">
+            <textarea
+              className="textarea"
+              name="textBody"
+              value={this.state.textBody}
+              placeholder="Note Content"
+              rows="10"
+              onChange={this.changeHandler}
+            />
+          </div>
+          <Link to={`/note/get/${id}`}>
+            <GeneralBtn width="15rem">
+              Update
+            </GeneralBtn>
+          </Link>
+        </form>
+      </div>
+    );
+  }
 }
- 
+
 export default EditNoteComponent;
--- note-taker/src/app/edit-view/EditNoteContainer.js
+++ note-taker/src/app/edit-view/EditNoteContainer.js
@@ -1,14 +1,18 @@
 import { connect } from 'react-redux';
 import EditNoteComponent from './EditNoteComponent';
-
+import { noteViewOperations } from '../note-view/duck';
 const mapStateToProps = (state) => {
+    const { title, textBody } = state.noteView.note;
     return {
-
+        title,
+        textBody
     }
 }
 const mapDispatchToProps = (dispatch) => {
-    return {
+    const fetchNote = (id) => dispatch(noteViewOperations.fetchNote(id));
 
+    return {
+        fetchNote
     }
 }
 const EditNoteContainer = connect(mapStateToProps, mapDispatchToProps)(EditNoteComponent);
