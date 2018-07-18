b5268799872f0f789ec606ea298782915e9b77c2
Gabriel Duquette <gabrielduquette@gmail.com>
2018-07-18 13:40:22 -0400

    tweak sidebar centering

--- lambda-notes/src/components/Sidebar.js
+++ lambda-notes/src/components/Sidebar.js
@@ -5,9 +5,11 @@ import { Link } from 'react-router-dom';
 const Sidebar = props => {
   return (
     <div className="sidebar-container">
-      <h1>Lambda<br/>Notes</h1>
-      <Link to="/"><button>View Your Notes</button></Link>
-      <Link to="/notes/add"><button>+ Create New Note</button></Link>
+      <div className="sidebar">
+        <h1>Lambda<br/>Notes</h1>
+        <Link to="/"><button>View Your Notes</button></Link>
+        <Link to="/notes/add"><button>+ Create New Note</button></Link>
+      </div>
     </div>
   )
 }
