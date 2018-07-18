aa9f36a33f5bdd9a7fad19917f6d4095b4d8e836
cmok4290 <cmok4290@gmail.com>
2018-07-18 13:44:18 -0400

    refactored CreateView

--- lambda-notes/src/components/CreateView.js
+++ lambda-notes/src/components/CreateView.js
@@ -1,36 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { submitReq } from '../actions';
 import logo from '../logo.svg';
 
-const CreateView = props => {
-    if(!props.fetched) return <img src={logo} className="App-logo" alt="logo" style={{margin: "auto", height: "50%"}}/>;
+class CreateView extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            textBody: ''
+        }
+    }
 
-    return (
-        <div className="CreateView-container">
-            <div className="CreateView-header">
-                <h2>Create New Note:</h2>
+    componentDidMount() {
+        this.setState({title: '', textBody: ''});
+    }
+
+    handleChange = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        if(this.state.title === '' && this.state.textBody === '') return null;
+
+        const note = {
+            title: this.state.title,
+            textBody: this.state.textBody
+        }
+        this.props.submitReq(note);
+    }
+
+    render() {
+        // if(!props.fetched) return <img src={logo} className="App-logo" alt="logo" style={{margin: "auto", height: "50%"}}/>;
+
+        return (
+            <div className="CreateView-container">
+                <div className="CreateView-header">
+                    <h2>Create New Note:</h2>
+                </div>
+                <form className="CreateView-content" onSubmit={this.handleSubmit}>
+                    <input
+                        name="title"
+                        placeholder="Note Title"
+                        value={this.state.title}
+                        onChange={this.handleChange}
+                        />
+                    <textarea
+                        name="textBody"
+                        placeholder="Note Content"
+                        value={this.state.textBody}
+                        onChange={this.handleChange}
+                        />
+                </form>
+                <div className="CreateView-buttons">
+                    <Link to="/"><button onClick={this.handleSubmit}>Save</button></Link>
+                </div>
             </div>
-            <form className="CreateView-content" onSubmit={props.handleSubmit}>
-                <input
-                    name="title"
-                    placeholder="Note Title"
-                    value={props.title}
-                    onChange={props.handleChange}
-                    />
-                <textarea
-                    name="textBody"
-                    placeholder="Note Content"
-                    value={props.textBody}
-                    onChange={props.handleChange}
-                    />
-            </form>
-            <div className="CreateView-buttons">
-                <Link to="/"><button onClick={props.handleSubmit}>Save</button></Link>
-            </div>
-        </div>
-    );
+        );
+    }
 };
 
-export default CreateView;
+const mapStateToProps = ({ notes }) => {
+    return {};
+}
+
+export default connect(mapStateToProps, { submitReq })(CreateView);
