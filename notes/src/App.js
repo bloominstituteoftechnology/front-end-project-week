6d34953449a315b3415d6924909c1d14cd86de38
nchen0 <nicky.chen0@gmail.com>
2018-07-18 18:56:09 -0500

    added login page

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,45 +1,7 @@
 import React, { Component } from "react";
-import Notes from "./components/Notes";
-import Menu from "./components/Menu";
-import { Route, withRouter } from "react-router-dom";
-import Create from "./components/Create";
-import "./App.css";
-import ViewNote from "./components/ViewNote";
-import EditNote from "./components/EditNote";
-import { fetchNotes } from "./actions/action";
-import { connect } from "react-redux";
+import PostApp from "./PostApp";
+import Authenticate from "./Authentication/Authenticate";
 
-class App extends Component {
-  componentDidMount() {
-    this.props.fetchNotes(
-      "https://killer-notes.herokuapp.com/note/get/all"
-    );
-  }
+const App = () => <PostApp />;
 
-  render() {
-    return (
-      <div className="container">
-        <React.Fragment>
-          <Menu />
-        </React.Fragment>
-        <React.Fragment>
-          <Route exact path="/" component={Notes} />
-        </React.Fragment>
-        <Route exact path="/create" component={Create} />
-        <Route
-          exact
-          path="/note/:id"
-          component={ViewNote}
-        />
-        <Route path="/edit" component={EditNote} />
-      </div>
-    );
-  }
-}
-
-export default withRouter(
-  connect(
-    null,
-    { fetchNotes }
-  )(App)
-);
+export default Authenticate(App);
---
+++ notes/src/PostApp.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import Notes from "./components/Notes";
+import Menu from "./components/Menu";
+import { Route, withRouter } from "react-router-dom";
+import Create from "./components/Create";
+import "./App.css";
+import ViewNote from "./components/ViewNote";
+import EditNote from "./components/EditNote";
+import { fetchNotes } from "./actions/action";
+import { connect } from "react-redux";
+
+class PostApp extends Component {
+  componentDidMount() {
+    this.props.fetchNotes(
+      "https://killer-notes.herokuapp.com/note/get/all"
+    );
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <React.Fragment>
+          <Menu />
+        </React.Fragment>
+        <React.Fragment>
+          <Route exact path="/" component={Notes} />
+        </React.Fragment>
+        <Route exact path="/create" component={Create} />
+        <Route
+          exact
+          path="/note/:id"
+          component={ViewNote}
+        />
+        <Route path="/edit" component={EditNote} />
+      </div>
+    );
+  }
+}
+
+export default withRouter(
+  connect(
+    null,
+    { fetchNotes }
+  )(PostApp)
+);
