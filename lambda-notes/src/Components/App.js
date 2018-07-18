d6f5f55d01169e8f500c335f2c5762cd188e4b5e
Jameson <jamesonbrown@neolivity.com>
2018-07-18 14:45:08 -0600

    Added axios.put to update a note

--- lambda-notes/src/Components/App.js
+++ lambda-notes/src/Components/App.js
@@ -19,7 +19,7 @@ class App extends Component {
       deleting: false
     }
   }
-// New Functions
+
   componentDidMount() {
     axios
       .get('http://localhost:3333/notes')
@@ -33,28 +33,6 @@ class App extends Component {
   toggleDeleting = () => {
     this.setState({ deleting: !this.state.deleting })
   }
-// Old Functions
-  handleSetCurrent = note => {
-    this.setState({ currentNote: note })
-  }
-  handleInputChange = e => this.setState({[e.target.name]: e.target.value});
-  handleEditTitle= e => {
-    this.setState({currentNote: {id: this.state.currentNote.id, title: e.target.value, content: this.state.currentNote.content}})
-  }
-  handleEditContent= e => {
-    this.setState({currentNote: {id: this.state.currentNote.id, title: this.state.currentNote.title, content: e.target.value}})
-  }
-  
-  handleEditNote = id => {
-    const notes = this.state.notes.slice();
-    for (let i=0; i<notes.length; i++) {
-      if (notes[i].id === Number(id)) {
-        notes[i] = { id: this.state.currentNote.id, title: this.state.currentNote.title, content: this.state.currentNote.content, };
-      }
-    }
-    this.setState({notes, currentNote: {}});
-    alert('NOTE UPDATED: Click \'View Your Notes\' in side banner to see all notes.')
-  }
 
   render() {
     return (
@@ -63,7 +41,7 @@ class App extends Component {
         <Route exact path='/' render={() => <ListView notes={this.state.notes} />} />
         <Route path='/create' render={() => <CreateNote contentValue={this.state.contentValue} titleValue={this.state.titleValue} handleSetData={this.handleSetData} />} />
         <Route path='/view/:id' render={(props) => <ViewNote {...props} notes={this.state.notes} toggleDeleting={this.toggleDeleting} />} />
-        <Route path='/edit/:id' render={(props) => <EditNote {...props} notes={this.state.notes} currentNote={this.state.currentNote} handleSetCurrent={this.handleSetCurrent} handleEditNote={this.handleEditNote} handleEditTitle={this.handleEditTitle} handleEditContent={this.handleEditContent} />} />
+        <Route path='/edit/:id' render={(props) => <EditNote {...props} notes={this.state.notes} currentNote={this.state.currentNote} handleSetData={this.handleSetData} handleEditNote={this.handleEditNote} handleEditTitle={this.state.handleEditTitle} handleEditContent={this.state.handleEditContent} handleSetCurrent={this.handleSetCurrent} />} />
         {this.state.deleting ? (<Route path='/view/:id' render={props => (<DeleteNote {...props} toggleDeleting={this.toggleDeleting} handleSetData={this.handleSetData} handleDeleteNote={this.handleDeleteNote}/>)}/>) : null}
       </div>  
     );
--- lambda-notes/src/Components/EditNote.js
+++ lambda-notes/src/Components/EditNote.js
@@ -1,34 +1,58 @@
 import React from 'react';
+import axios from 'axios';
 
 class EditNote extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             id: null,
-            note: []
+            notes: []
         }
     }
     componentDidMount() {
         const id = this.props.match.params.id;
-        let note = this.props.notes.filter(note => note.id === Number(id))
-        this.props.handleSetCurrent(note[0]);
+        axios.get(`http://localhost:3333/notes/`)
+            .then(response => {
+                console.log('ONE GET: ', response)
+                const note = response.data.filter(note => note.id === Number(id))
+                const currentNote = note[0]
+                this.setState({ id: Number(id), notes: currentNote })
+            })
+            .catch(err => {console.log(err)})
     }
-    editCompleted = () => {this.props.handleEditNote(this.props.match.params.id)}
+    editNote = () => {
+        const id = this.state.id;
+        axios.put(`http://localhost:3333/notes/${id}`, { title: this.state.notes.title, content: this.state.notes.content })
+            .then(response => {
+                console.log('PUT RESPONSE: ', response)
+                this.setState({ id: null, note: [] })
+                this.props.handleSetData(response.data)
+                alert('NOTE UPDATED: Click \'View Your Notes\' in side banner to see all notes.')
+            })
+            .catch(err => {console.log(err)})
+    }
+    handleEditTitle= e => {
+        this.setState({notes: {id: this.state.notes.id, title: e.target.value, content: this.state.notes.content}})
+    }
+    handleEditContent= e => {
+        this.setState({notes: {id: this.state.notes.id, title: this.state.notes.title, content: e.target.value}})
+    }
     render() {
         return (
             <form className="edit-note">
                 <h1>Edit New Note:</h1>
                 <input
                     name='titleValue'
-                    value={this.props.currentNote.title}
-                    onChange={this.props.handleEditTitle}
+                    value={this.state.notes.title}
+                    onChange={this.handleEditTitle}
                 />
                 <textarea
                     name='contentValue'
-                    value={this.props.currentNote.content}
-                    onChange={this.props.handleEditContent}
+                    type='text'
+                    value={this.state.notes.content}
+                    onChange={this.handleEditContent}
                 />
-                <div onClick={this.editCompleted} className="custom-button">
+                <div onClick={this.editNote} className="custom-button">
                     Update
                 </div>
             </form>
