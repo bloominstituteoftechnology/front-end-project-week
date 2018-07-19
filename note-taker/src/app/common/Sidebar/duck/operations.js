cc2ac0a959a4b2a734edb7fe019c6c87921b0555
Simon <Simon_Freelance@hotmail.com>
2018-07-18 21:58:32 -0400

    Refactored the toggleTheme function to use module pattern - Clean up the global scope

--- note-taker/src/app/common/Sidebar/duck/operations.js
+++ note-taker/src/app/common/Sidebar/duck/operations.js
@@ -2,31 +2,33 @@ import Creators from "./actions";
 import "../../../styles/custom-props.css";
 
 const toggleThemeAction = Creators.toggleTheme;
-const root = document.querySelector(":root");
-const rootStyle = getComputedStyle(root);
 
-const mainFontColor = rootStyle.getPropertyValue("--main-font-color");
-const noteBg = rootStyle.getPropertyValue("--note-bg");
-const sidebarBg = rootStyle.getPropertyValue("--sidebar-bg");
-const mainBg = rootStyle.getPropertyValue("--main-bg");
+const toggleTheme = (function() {
+  const root = document.querySelector(":root");
+  const rootStyle = getComputedStyle(root);
 
-const toggleTheme = isDark => {
-  if (isDark === false) {
-    root.style.setProperty("--main-font-color", "white");
-    root.style.setProperty("--note-bg", "#4A4A4A");
-    root.style.setProperty("--sidebar-bg", "#4A4A4A");
-    root.style.setProperty("--main-bg", "#181818");
-  } else {
-    root.style.setProperty("--main-font-color", mainFontColor);
-    root.style.setProperty("--note-bg", noteBg);
-    root.style.setProperty("--sidebar-bg", sidebarBg);
-    root.style.setProperty("--main-bg", mainBg);
-  }
+  const mainFontColor = rootStyle.getPropertyValue("--main-font-color");
+  const noteBg = rootStyle.getPropertyValue("--note-bg");
+  const sidebarBg = rootStyle.getPropertyValue("--sidebar-bg");
+  const mainBg = rootStyle.getPropertyValue("--main-bg");
 
-  return dispatch => {
-    dispatch(toggleThemeAction());
+  return function(isDark) {
+    if (isDark === false) {
+      root.style.setProperty("--main-font-color", "white");
+      root.style.setProperty("--note-bg", "#4A4A4A");
+      root.style.setProperty("--sidebar-bg", "#4A4A4A");
+      root.style.setProperty("--main-bg", "#181818");
+    } else {
+      root.style.setProperty("--main-font-color", mainFontColor);
+      root.style.setProperty("--note-bg", noteBg);
+      root.style.setProperty("--sidebar-bg", sidebarBg);
+      root.style.setProperty("--main-bg", mainBg);
+    }
+    return function(dispatch) {
+      dispatch(toggleThemeAction());
+    };
   };
-};
+})();
 
 export default {
   toggleTheme
