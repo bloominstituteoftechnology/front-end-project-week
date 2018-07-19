ec924b019dcd8950d66e86538b85d0cc98f516a8
Simon <Simon_Freelance@hotmail.com>
2018-07-18 20:34:37 -0400

    Fixed the background for all components

--- note-taker/src/app/App.js
+++ note-taker/src/app/App.js
@@ -3,21 +3,21 @@ import styled from "styled-components";
 import { Route } from "react-router-dom";
 import SidebarContainer from "./common/Sidebar/SidebarContainer";
 import HomeContainer from "./home/HomeContainer";
-import './styles/custom-props.css';
+import "./styles/custom-props.css";
 import CreateNoteContainer from "./create-new/CreateNoteContainer";
 import NoteContainer from "./note-view/NoteContainer";
 import EditNoteContainer from "./edit-view/EditNoteContainer";
 const AppContainer = styled.div`
   display: flex;
-  color: var(--main-font-color); 
-
-`;
-const MainContentContainer = styled.div`
+  color: var(--main-font-color);
   background: var(--main-bg);
+  height: 100vw;
+  `;
+const MainContentContainer = styled.div`
   position: absolute;
+  background: var(--main-bg);
   left: var(--sidebar-width);
   right: 0;
-  height: 100vw;
   padding: 60px 25px;
 `;
 const App = () => {
@@ -27,8 +27,8 @@ const App = () => {
       <MainContentContainer>
         <Route exact path="/" component={HomeContainer} />
         <Route exact path="/create-new" component={CreateNoteContainer} />
-        <Route exact path="/note/get/:id" component ={NoteContainer} />
-        <Route exact path="/:id/edit-view" component = {EditNoteContainer}/>
+        <Route exact path="/note/get/:id" component={NoteContainer} />
+        <Route exact path="/:id/edit-view" component={EditNoteContainer} />
       </MainContentContainer>
     </AppContainer>
   );
