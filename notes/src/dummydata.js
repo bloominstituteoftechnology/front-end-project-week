871f6cf71dc417a9f2ae67c2c14f517149bd4cdd
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-07-19 11:03:03 -0700

    changed pointer

--- notes/src/dummydata.js
+++
@@ -1,9 +0,0 @@
-[
-    {
-    "tags": [],
-    "_id": "5b087507ebfb8700149af806",
-    "title": "Please do not delete this note.",
-    "textBody": "Don't delete it! This will be a base note. Many people will see this note.",
-    "__v": 0
-    }
-    ]
