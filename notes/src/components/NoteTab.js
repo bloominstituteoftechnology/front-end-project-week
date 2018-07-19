1c00214d5e35f99c78bc52be7465e6b01d5981fc
Alec <afroenygam@gmail.com>
2018-07-19 13:02:57 -0400

    Text truncation on tab note added

--- notes/src/components/NoteTab.js
+++ notes/src/components/NoteTab.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../App.css';
+import LinesEllipsis from 'react-lines-ellipsis';
 
 /*Class responsible for displaying Note tabs in list view*/
 class NoteTab extends React.Component {
@@ -14,7 +15,14 @@ class NoteTab extends React.Component {
     return (
       <div id="note-tabz" onClick={this.viewNote}>
       <div id="note-tab-header">{this.props.note.title}</div>
-      <p id="note-tab-body">{this.props.note.textBody.length > 55 ? this.props.note.textBody.slice(0,55) + " (...)" : this.props.note.textBody}</p>
+      {/*<p id="note-tab-body">{this.props.note.textBody.length > 55 ? this.props.note.textBody.slice(0,55) + " (...)" : this.props.note.textBody}</p>*/}
+      <LinesEllipsis
+        text={this.props.note.textBody}
+        maxLine='4'
+        ellipsis=' (...)'
+        trimRight
+        basedOn='letters'
+         />
       </div>
     )
   }
