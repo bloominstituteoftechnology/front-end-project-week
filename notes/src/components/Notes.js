95d8728731320e24d4e6ac4e7de7be7a87d3311e
nchen0 <nicky.chen0@gmail.com>
2018-07-19 13:34:12 -0500

    adding search

--- notes/src/components/Notes.js
+++ notes/src/components/Notes.js
@@ -2,22 +2,33 @@ import React from "react";
 import { connect } from "react-redux";
 import Note from "./Note";
 
-const Notes = props => {
-  return (
-    <div className="notesList">
-      <p>Log Out</p>
-      <h2>Your Notes: </h2>
-      <form className="notesSearch">
-        <input placeholder="Search Note..." />
-      </form>
-      <div className="cards">
-        {props.notes.map(note => {
-          return <Note key={Math.random()} note={note} />;
-        })}
+class Notes extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  logout = () => {
+    localStorage.clear();
+    window.location.reload();
+  };
+  render() {
+    return (
+      <div className="notesList">
+        <p onClick={this.logout} className="logout-button">
+          Log Out
+        </p>
+        <h2>Your Notes: </h2>
+        <form className="notesSearch">
+          <input placeholder="Search Note..." />
+        </form>
+        <div className="cards">
+          {this.props.notes.map(note => {
+            return <Note key={Math.random()} note={note} />;
+          })}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 const mapStateToProps = state => {
   return {
