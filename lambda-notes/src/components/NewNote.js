7e506a06492871d4253d7f0866187741ecf6a0f7
markdavidwilliams <markwilliams.aus@gmail.com>
2018-07-19 00:32:11 -0500

    added tag input on note create note and handled empty form input

--- lambda-notes/src/components/NewNote.js
+++ lambda-notes/src/components/NewNote.js
@@ -9,6 +9,7 @@ class NewNote extends Component {
     state = {
         title: '',
         body: '',
+        tags: '',
     }
 
     handeInputChange = event => {
@@ -16,9 +17,23 @@ class NewNote extends Component {
     }
 
     handleCreateNote = URL => {
+        let tags = [];
+        if(!this.state.title || !this.state.body) {
+            return;
+        }
+        else if (this.state.tags === '' || this.state.tags === ' ' || this.state.tags === [] || this.state.tags === [''] || this.state.tags === [' ']) {
+            tags = [];
+        }
+        else if (this.state.tags.constructor === Array) {
+            tags = this.state.tags;
+        }
+        else {
+            tags = this.state.tags.split(',');
+        }
         const note = {
             title: this.state.title,
             textBody: this.state.body,
+            tags: tags,
         }
         this.props.createNote(URL, note);
     }
@@ -30,6 +45,7 @@ class NewNote extends Component {
                 <div className='note-form'>
                     <input name='title' className='title' placeholder='Note Title' value={this.state.title} onChange={this.handeInputChange} />
                     <textarea name='body' className='body' placeholder='Note Content' value={this.state.body} onChange={this.handeInputChange}></textarea>
+                    <input name='tags' className='title' placeholder='Tags' value={this.state.tags} onChange={this.handeInputChange} />
                     <button className='save-btn' type='submit' onClick={() => this.handleCreateNote(URL)}>Save</button>
                 </div>
             </div>
