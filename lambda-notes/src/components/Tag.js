d079c2d5a9a567f5058b51e90c6de78cf59c31b8
markdavidwilliams <markwilliams.aus@gmail.com>
2018-07-19 22:30:49 -0500

    preparing for deployment

--- lambda-notes/src/components/Tag.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import '../styles/Tag.css';
-
-// returns to ViewNote how to display each Tag
-const Tag = props => {
-    return <p className='tag'>{props.tag},</p>
-}
-
-export default Tag;
