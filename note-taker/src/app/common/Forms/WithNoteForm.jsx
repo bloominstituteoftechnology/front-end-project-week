44f398f59cbce78f32ef515c675ef4f3fa03d8d5
Simon <Simon_Freelance@hotmail.com>
2018-07-20 15:25:11 -0400

    Made a HOC for common forms

---
+++ note-taker/src/app/common/Forms/WithNoteForm.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import "bulma/css/bulma.css";
+import { Link } from "react-router-dom";
+function withNoteForm(WrappedBtn) {
+  return class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        title: "",
+        textBody: ""
+      };
+    }
+   
+    componentDidMount() {
+      const title = this.props.title === undefined ? '' : this.props.title;
+      const textBody = this.props.textBody === undefined ? '' : this.props.textBody;
+      this.setState({ title: title, textBody: textBody})
+    }
+    componentWillReceiveProps(nextProps) {
+      console.log('nextProps');
+      if(nextProps.title !== this.props.title && nextProps.textBody !== this.props.textBody) {
+        this.setState({ title: nextProps.title, textBody: nextProps.textBody });
+      }
+    }
+    changeHandler = e => {
+      this.setState({ [e.target.name]: e.target.value });
+    };
+
+    render() {
+      return (
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+          }}
+        >
+          <div className="field">
+            <div className="control">
+              <input
+                className="input is-large"
+                type="text"
+                name="title"
+                value={this.state.title}
+                placeholder="Note Title"
+                onChange={this.changeHandler}
+              />
+            </div>
+          </div>
+          <div className="field">
+            <textarea
+              className="textarea"
+              name="textBody"
+              value={this.state.textBody}
+              placeholder="Note Content"
+              rows="10"
+              onChange={this.changeHandler}
+            />
+          </div>
+          <Link to={this.props.routeTo}>
+            <WrappedBtn width="15rem" onClick = {() => {
+                this.props.btnHandlers(this.props.id, {
+                  tags: [],
+                  title: this.state.title,
+                  textBody: this.state.textBody
+                });
+              }}>{this.props.btnText}</WrappedBtn>
+          </Link>
+        </form>
+      );
+    }
+  };
+}
+
+export default withNoteForm;
