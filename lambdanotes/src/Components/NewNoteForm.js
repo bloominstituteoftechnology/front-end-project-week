d98f1caa9e0fde0b6f3278513654bf3e76a61149
Katie Gorbell <katiegorbell@gmail.com>
2018-07-23 13:17:06 -0500

    building out form

--- lambdanotes/src/Components/NewNoteForm.js
+++ lambdanotes/src/Components/NewNoteForm.js
@@ -2,10 +2,42 @@ import React, { Component } from 'react';
 
 
 class NewNoteForm extends Component {
+    constructor() {
+        super();
+        this.state = {
+            title: '',
+            content: ''
+        };
+    }
+
+    updateInputChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    addNote = () => {
+
+    }
+
     render() {
         return (
-            <div>
-            </div>
+            <form>
+                <h3>Create New Note:</h3>
+                <input 
+                    type='text'
+                    placeholder='Note Title'
+                    name='title'
+                    value={this.state.title}
+                    onChange={this.updateInputChange} 
+                />
+                <input
+                    type='text'
+                    placeholder='Note Content'
+                    name='content'
+                    value={this.state.content}
+                    onChange={this.updateInputChange}
+                />
+                <button onSubmit={this.addNote}>Save</button>
+            </form>
         )
     }
 }
