70ea12d5d95f7806a168e7afadb93a90e55b91d7
M.Real <mreal126@protonmail.com>
2018-07-23 19:15:34 -0400

    took a littlittle break, reconsidering approach as I think I can do it making more use of React/Redux?

---
+++ notes/src/Components/NewNoteForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Sidebar.css';
+
+const NewNote = () => {
+    return (
+        <div>
+            </div>
+    )
+
+}
+
+export default NewNote;
---
+++ notes/src/Extras.js
@@ -0,0 +1,4 @@
+{/* import { BrowserRouter as Router } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';*/}
