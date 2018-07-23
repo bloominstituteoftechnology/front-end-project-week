ddba3676fec43791cbc6aa3deb763befc40b2127
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-07-23 15:39:08 -0600

    Trello URL in README.md

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -4,14 +4,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import thunk from 'redux-thunk';
-// import logger from 'redux-logger';
+import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 const store = createStore(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk // logger)
-  ));  
+    applyMiddleware(thunk, logger)
+  );  
 
 
 ReactDOM.render(
