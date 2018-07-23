d05e0645a533cd2dd383b71fafc0c6168b3a4c0b
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-07-23 14:54:15 -0600

    index.js and new files

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk, logger)
+  );  
+
+
+ReactDOM.render(
+    <Router>
+        <Provider store={store}>
+        <App />
+        </Provider>
+    </Router>
+, document.getElementById('root'));
+
