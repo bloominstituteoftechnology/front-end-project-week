cecf9427b063420d04f51b3f64d4ccc406e6fedc
Kairi <kairijb@gmail.com>
2018-07-23 17:58:28 -0500

    Began working on sidebar

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,18 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SideBar from './components/Sidebar';
+
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      notes: [
+        {
+          title: 'Lorem',
+          note: 'Ipsum',
+          id: Date.now()
+        }
+      ],
+      title: '',
+      note: '',
+    }
+  }
+
+  change = e => {
+    this.setState({ [e.target.name]: e.target.value });  
+  }
+
+  submit = e => {
+    e.preventDefault()
+    this.state.notes.push({
+      title: this.state.title,
+      note: this.state.note,
+      id: Date.now()
+    })
+    this.setState({title: '', note: ''})
+  }
+
+
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <SideBar />
+        {this.state.notes.map((note, index) => {
+          return <div key={index}>{note.title} <hr /> {note.note}</div>
+        })}
+
+        <form onSubmit={this.submit}>
+        <input
+        type='text'
+        name='title'
+        value={this.state.title}
+        placeholder='title'
+        onChange={this.change}
+        />
+
+        <input
+        type='text'
+        name='note'
+        value={this.state.note}
+        placeholder='note'
+        onChange={this.change}
+        />
+
+        <button
+        type='submit'>
+        ADD NOTE
+        </button>
+        </form>
+        
       </div>
     );
   }
---
+++ lambda-notes/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SideBar = () => {
+    return ( 
+        <div>
+        <h1>
+        Lambda Notes
+        </h1>
+        </div>
+     );
+}
+ 
+export default SideBar;
