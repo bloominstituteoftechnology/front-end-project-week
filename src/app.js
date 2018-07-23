c8c7a8bc8cd0fd94dc25789f4f494da12f3a5635
Terrance <tbarnett2020@gmail.com>
2018-07-23 12:52:03 -0400

    Terrance Danckert CS11 (front-end-project-week)

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp'
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+
+ReactDOM.render(
+  <p>BOILER PLATE</p>,
+  document.getElementById('app')
+)
