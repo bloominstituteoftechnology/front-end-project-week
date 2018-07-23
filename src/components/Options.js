c5fdc966a6bdb797e057f641dc8b1ea171c10580
Terrance <tbarnett2020@gmail.com>
2018-07-23 16:55:38 -0400

    Notes showing**

--- src/components/Options.js
+++ src/components/Options.js
@@ -13,13 +13,13 @@ import Option from './Option'
 // This stateless function replaced code right below it
 const Options = (props) => {
   return (
-    <div>
+    <div className= "main-widget">
     <div className="widget-header">
-    <h3 className="widget-header__title">Your Notes</h3>
+{/*   <h3 className="widget-header__title">Your Notes</h3>
     <button
     className="button button--link"
-     onClick={props.handleDeleteOptions}>Remove All</button>
-     </div>
+      onClick={props.handleDeleteOptions}>Remove All</button>
+ */}  </div>
        {props.options.length === 0 && <p className="widget-message">Please add a note to get started!</p>}
         {
         props.options.map((option, index) => {
