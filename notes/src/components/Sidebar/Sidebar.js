d7fc3523bfc49daa4e3fd45653211e5e8287fff8
JacobLayton <jlayjlay3@gmail.com>
2018-07-23 16:36:08 -0700

    Sidebar styled

--- notes/src/components/Sidebar/Sidebar.js
+++ notes/src/components/Sidebar/Sidebar.js
@@ -5,8 +5,8 @@ const Sidebar = () => {
     return (
         <div className='sidebar'>
             <div className='header'>
-                <p>Lambda</p>
-                <p>Notes</p>
+                <h1>Lambda</h1>
+                <h1 className="notes">Notes</h1>
             </div>
             <div className='buttons'>
                 <button className='sidebar-button'>View Your Notes</button>
