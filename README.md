7000a5a60c2b1d5c1c312393b5bf8f7f5cb63538
Terrance <tbarnett2020@gmail.com>
2018-07-23 14:56:43 -0400

    Added Babel, && Webpack

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ You are to treat this week as if you are working at a company and the instructor
 
 The main objective of this week is to develop the MVP feature set listed below using react and any other technologies you have learned here at Lambda School. There are design files in this repository you should use as a creative guide.
 
-## Git Commits 
+## Git Commits
 
 - You are required to showcase progress with at least 1 commit a day. This will let your project manager know where you are and if you need help. This also allows the client to get progress reports from the company in a real world setting.
 
---
+++ src/components/AddNote.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+//  Challeneg setup form with text and sumbit button
+// wire up onSubmit
+// handleAddNote -> fetch the value typed -> if value, then alert
+export default class AddNote extends React.Component {
+  state = {
+    error: undefined
+  };
+    handleAddNote = (event) => {
+      event.preventDefault();
+      // "Event.target" is the form element
+      // then uses 'Elements" to go into the "input" by its name which is 'option" then we grab its value property
+      const option = event.target.elements.option.value.trim();
+      const error = this.props.handleAddNote(option);
+      // if theres an option then do something which is alert that option
+  
+      // This is the shorthand version of the code below it
+      this.setState(() => ({error: error}))
+      /*this.setState(() => {
+        return { error: error }
+      })
+      */
+     if (!error) {
+      event.target.elements.option.value = '';
+     }
+    };
+    render() {
+        return (
+            <div>
+            {this.state.error && <p className="add-option-error">{this.state.error}</p>}
+            {/* this calls the handleAddNote method from above */}
+              <form className="add-option" onSubmit={this.handleAddNote}>
+                <input className="add-option__input" type="text" name="option" />
+                <button className="button">Add Option</button>
+              </form>
+            </div>
+        );
+    }
+  }
