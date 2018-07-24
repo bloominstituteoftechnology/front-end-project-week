2ece46a00090adbcd5690aa5c72b5b1751c6667f
sean chavez <seanchavez@seanchavezsMBP.home>
2018-07-23 17:30:47 -0700

    Create files for components; add react-router and set up Routes

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Route path="/" component={DisplayNotes} />
+          <Route path="/:id" component={Note} />
+          <Route path="/create" component={CreateNote} />
+          <Route path="/edit" component={EditNote} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
