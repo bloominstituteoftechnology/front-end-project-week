1bb528cf837c480d83821515644618e74f161aa6
M.Real <mreal126@protonmail.com>
2018-07-23 20:28:49 -0400

    New note page touched up a bit, still needs functionality. Need to test if styled components is properly installed and find out what it can help with.

--- notes/src/index.js
+++ notes/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 registerServiceWorker();
 
