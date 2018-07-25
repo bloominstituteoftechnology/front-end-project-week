dbaa946553fc5c8ba06052ee0b56b254cf2b09af
Lisa Canini <three_nineteen@yahoo.com>
2018-07-24 18:58:07 -0700

    button links wip

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -6,8 +6,8 @@ const Sidebar = () => {
     return (
         <div className='sidebar'>
             <h6>Lambda Notes</h6>
-            <Link to={'/notes'}><button className='sidebar-button'>View Your Notes</button></Link>
-            <Link to={'/notes/create'}><button className='sidebar-button'>+ Create New Note</button></Link>
+            <Link to={'/'}><button className='sidebar-button'>View Your Notes</button></Link>
+            <Link to={'/create'}><button className='sidebar-button'>+ Create New Note</button></Link>
         </div>
     )
 }
