1a5228333b1abd3dad245bd84f725848746fc81d
David Layman <davidlayman85@gmail.com>
2018-07-24 20:16:03 -0400

    cards display

--- project/src/App.js
+++ project/src/App.js
@@ -7,22 +7,29 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+	  <div className="posts">
+	  { this.props.posts.map(post => {
+		  return(
+		<div key={Math.random(Date.now())} className="card">
+			<div className="title">
+			{post.title}
+			</div>
+			<div className="content">
+			{post.content.length > 199 ? `${post.content.slice(0,199)}`+'...' : post.content}
+			</div>
+		</div>
+		
+	  )})	}
       </div>
+	  </div>
     );
   }
 }
 
 const stateToProps = (state) => {
 return {
-	
+	posts:state.posts
 	};
 };
 
-export default App;
+export default connect(stateToProps)(App);
