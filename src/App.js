ce2caae0d43ba22412822cb0c9e8b668b97620c6
Lisa Canini <three_nineteen@yahoo.com>
2018-07-24 17:23:43 -0700

    restructured files for deployment

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import CreateNote from './Components/CreateNote';
+import ListView from './Components/ListView';
+import NoteView from './Components/NoteView';
+import EditView from './Components/EditView';
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className='app'>
+        <Route exact path='/notes' component={ ListView } />
+        <Route path='/notes/create' component={ CreateNote } />
+        <Route exact path='/notes/:id' component={ NoteView } />
+        <Route path='/notes/:id/edit' component={ EditView } />
+      </div>
+    );
+  }
+}
+
+export default App;
