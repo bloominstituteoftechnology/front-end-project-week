9ff2a49cc12485e9e6723bcbfb096ebaae1f98b8
Lisa Canini <three_nineteen@yahoo.com>
2018-07-24 18:59:33 -0700

    button links wip

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import "../css/Sidebar.css";
+import CreateNote from './CreateNote';
 
 const Sidebar = () => {
     return (
         <div className='sidebar'>
             <h6>Lambda Notes</h6>
             <Link to={'/'}><button className='sidebar-button'>View Your Notes</button></Link>
-            <Link to={'/create'}><button className='sidebar-button'>+ Create New Note</button></Link>
+            <Link to={CreateNote}><button className='sidebar-button'>+ Create New Note</button></Link>
         </div>
     )
 }
