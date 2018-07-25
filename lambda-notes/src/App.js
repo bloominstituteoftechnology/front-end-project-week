d781a0830417d089b5d95ee4b633809274282e07
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-07-25 11:00:46 -0600

    Added routes

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
 import ViewList from './components/ViewList/ViewList';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+import AddNote from './components/AddNote/AddNote';
+
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Lambda Notes</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <Route exact path='/notes' component={ViewList} />
+        <Route exact path='/Add-Note' component={AddNote} />
+      
       </div>
     );
   }
 }
 
-export default App;
+//export default App;
