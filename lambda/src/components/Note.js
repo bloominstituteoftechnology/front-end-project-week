be6308c3cd543414b0f0c94733a55cba9ceb669a
Sean <sean47900@gmail.com>
2018-08-20 16:30:50 -0500

    little more styling

--- lambda/src/components/Note.js
+++ lambda/src/components/Note.js
@@ -13,11 +13,11 @@ justify-content: center;
 const Card = styled.div`
 border: 1px solid black;
 background-color: white; 
-width: 250px; 
-height: 150px;  
+width: 200px; 
+height: 190px;  
 flex-direction: column;
 padding: 10px; 
-margin: 20px; 
+margin: 10px; 
 display: flex;  
 `
 const Title = styled.div`
