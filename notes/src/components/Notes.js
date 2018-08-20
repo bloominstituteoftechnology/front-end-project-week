ac7b1e1927b004764e9d514b8dcc9898f824ca45
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-08-20 14:51:38 -0700

    implement draggable notes

--- notes/src/components/Notes.js
+++ notes/src/components/Notes.js
@@ -1,6 +1,37 @@
 import React, { Component } from 'react'
 import Note from './Note';
 import { Input } from 'reactstrap';
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+};
+
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+    // some basic styles to make the items look a bit nicer
+    userSelect: "none",
+    padding: grid * 2,
+    margin: `0 0 ${grid}px 0`,
+
+    // change background colour if dragging
+    background: isDragging ? "#636e72" : "transparent",
+
+    // styles we need to apply on draggables
+    ...draggableStyle
+});
+
+const getListStyle = isDraggingOver => ({
+    background: isDraggingOver ? "#b2bec3" : "#dfe6e9",
+    padding: grid,
+    width: `60%`,
+    margin: `1rem auto`
+});
 
 class Notes extends Component {
     constructor(props){
@@ -12,7 +43,7 @@ class Notes extends Component {
 
     componentDidMount = () => {
         this.props.fetchNotes();
-        setTimeout(() => this.initializeData(), 1000);
+        setTimeout(() => this.initializeData(), 500);
     }
 
     searchInput = (event) => {
@@ -28,20 +59,67 @@ class Notes extends Component {
         })
     }
 
+    onDragEnd = (result) => {
+        // dropped outside the list
+        if (!result.destination) {
+            return;
+        }
+
+        const shownNotes = reorder(
+            this.state.shownNotes,
+            result.source.index,
+            result.destination.index
+        );
+
+        this.setState({
+            shownNotes
+        });
+    }
+
     render() {
         if (this.props.fetchingNotes){
             return (<div></div>)
         }
         return (
-            <div>
-                <Input type="text" onChange={this.searchInput} />
-                {this.state.shownNotes.map(note => {
+            <div className="home">
+                <Input type="text" onChange={this.searchInput} placeholder="Search" className="search-bar"/>
+                <DragDropContext onDragEnd={this.onDragEnd}>
+                    <Droppable droppableId="droppable" className="note-container">
+                        {(provided, snapshot) => (
+                            <div
+                                ref={provided.innerRef}
+                                style={getListStyle(snapshot.isDraggingOver)}
+                            >
+                                {this.state.shownNotes.map((note, index) => (
+                                    <Draggable key={note._id} draggableId={note._id} index={index}>
+                                        {(provided, snapshot) => (
+                                            <div
+                                                ref={provided.innerRef}
+                                                {...provided.draggableProps}
+                                                {...provided.dragHandleProps}
+                                                style={getItemStyle(
+                                                    snapshot.isDragging,
+                                                    provided.draggableProps.style
+                                                )}
+                                            >
+                                                <Note id={note._id} title={note.title} content={note.textBody} tags={note.tags} /> 
+                                            </div>
+                                        )}
+                                    </Draggable>
+                                ))}
+                                {provided.placeholder}
+                            </div>
+                        )}
+                    </Droppable>
+                </DragDropContext>
+
+                {/* {this.state.shownNotes.map(note => {
                     return (
                         <div className="note" key={note._id}>
                             <Note id={note._id} title={note.title} content={note.textBody} tags={note.tags} /> 
                         </div>
                     )
-                })}
+                })} */}
             </div>
         )
     }
