972f7b31ed82dcd835bf1f60dce34eb9029ce986
codejoncode <jonathanjholloway@gmail.com>
2018-08-20 11:39:31 -0500

    deleted unneccessary files created new folders

---
+++ note-app/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h3>Starting note taking app</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
