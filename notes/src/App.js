d4c2e7963596234ee02c155c6a928ec01c993b70
johnoro <officialjohnorourke@gmail.com>
2018-08-20 11:36:02 -0400

    Used CRA and cleaned it up a bit.

---
+++ notes/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
