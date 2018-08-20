3c56bc6372eb3bffa3da2e6853fd54551cd8d66c
johnoro <officialjohnorourke@gmail.com>
2018-08-20 12:45:48 -0400

    Completed note creation functionality.

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Note from './components/Note';
+import AddNote from './components/AddNote';
 
 class App extends Component {
   dummyText = 'Morbi pellentesque euismod venenatis. Nulla ut nibh nunc. Phasellus diam metus, blandit ac purus a, efficitur mollis.';
@@ -19,14 +20,40 @@ class App extends Component {
         id: 2,
         title: 'Note title',
         text: this.dummyText
-      },
-    ]
+      }
+    ],
+    title: '',
+    text: ''
+  };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    const { notes, title, text } = this.state;
+    this.setState({
+      notes: [...notes, {
+        id: notes.length,
+        title,
+        text
+      }],
+      title: '',
+      text: ''
+    });
   };
 
   render() {
     return (
       <div className="App">
-        {this.state.notes.map(note => <Note note={note} />)}
+        <AddNote 
+          onChange={this.onChange} 
+          onSubmit={this.onSubmit} 
+          title={this.state.title} 
+          text={this.state.text} 
+        />
+        {this.state.notes.map(note => <Note note={note} key={note.id} />)}
       </div>
     );
   }
---
+++ notes/src/components/AddNote.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const AddNote = props => (
+  <div>
+    <form onSubmit={props.onSubmit}>
+      <input
+        name="title"
+        placeholder="Note Title"
+        onChange={props.onChange}
+        value={props.title}
+      />
+      <textarea
+        name="text"
+        placeholder="Note Content"
+        onChange={props.onChange}
+        value={props.text}
+      />
+      <button>Save</button>
+    </form>
+  </div>
+);
+
+export default AddNote;
