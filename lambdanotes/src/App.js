ec2cffc63843d7600d7a70bb7837323a2157d287
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-08-20 11:10:28 -0500

    Major changes - Added dependencies and create different components for the app views.

--- lambdanotes/src/App.js
+++ lambdanotes/src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Lambda Notes</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        This is app.js
       </div>
     );
   }
