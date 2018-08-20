815d5ade1f70b4afa6f32c96006c36b4a8aa6df6
Trevor <trevorfehrman@gmail.com>
2018-08-20 12:19:01 -0700

    Reconverted components to class components

--- notes/src/Components/Note.js
+++ notes/src/Components/Note.js
@@ -1,13 +1,28 @@
 import React from "react";
 
-const Note = props => {
-	const note = props.notes.find(note => note.id == props.match.params.id);
-	return (
-		<div>
-			<h4>{note.title}</h4>
-			<p>{note.text}</p>
-		</div>
-	);
-};
+class Note extends React.Component {
+	state = { display: false };
+	render() {
+		const note = this.props.notes.find(
+			note => note.id == this.props.match.params.id,
+		);
+		return (
+			<div>
+				<h4>{note.title}</h4>
+				<p>{note.text}</p>
+			</div>
+		);
+	}
+}
 
 export default Note;
+
+// const Note = props => {
+//     const note = props.notes.find(note => note.id == props.match.params.id);
+//     return (
+//         <div>
+//             <h4>{note.title}</h4>
+//             <p>{note.text}</p>
+//         </div>
+//     );
+// };
--- notes/src/Components/Notes.js
+++ notes/src/Components/Notes.js
@@ -1,14 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Notes = ({ notes }) => (
-	<div>
-		{notes.map(note => (
-			<Link to={`/notes/${note.id}`}>
-				<h4>{note.title}</h4>
-				<p>{note.text}</p>
-			</Link>
-		))}
-	</div>
-);
+class Notes extends React.Component {
+	state = { display: false };
+	render() {
+		return (
+			<div>
+				{this.props.notes.map(note => (
+					<Link to={`/notes/${note.id}`}>
+						<h4>{note.title}</h4>
+						<p>{note.text}</p>
+					</Link>
+				))}
+			</div>
+		);
+	}
+}
 export default Notes;
+
+// const Notes = ({ notes }) => (
+// 	<div>
+// 		{notes.map(note => (
+// 			<Link to={`/notes/${note.id}`}>
+// 				<h4>{note.title}</h4>
+// 				<p>{note.text}</p>
+// 			</Link>
+// 		))}
+// 	</div>
+// );
+// export default Notes;
