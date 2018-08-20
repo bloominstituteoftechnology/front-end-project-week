33d3f2ba4437e28181f4f8422e773cb64ef4f2b5
codejoncode <jonathanjholloway@gmail.com>
2018-08-20 14:19:34 -0500

    the place holder is off and margin / overall width is about 11px bigger than i would like but it is good enough to more forward on other task.

--- note-app/src/components/create-note.js
+++ note-app/src/components/create-note.js
@@ -7,10 +7,13 @@ class CreateNote extends Component {
         return (
 
             <div className ="create-note-form">
-                <h3>Create new Note:</h3>
-                <input type="text" placeholder = "Note Title"/>
-                <input type="text" placeholder = "Note Content"/>
+                <h3>Create New Note:</h3>
+                <input className = "input-title" type="text" placeholder = "Note Title"/> 
+                <br/>
+                <input className = "input-content" type="text" placeholder = "Note Content"/>
+                <br/>
                 <button className = "btn-side-bar">Save</button>
+                {/* Would like to fix the placeholder inside of the input-content also to the right margin is off by 11px */}
             </div>
 
         )
