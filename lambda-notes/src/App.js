a16e8f94eabf418c884f9f12ab103666a0ec731a
Kairi <kairijb@gmail.com>
2018-08-20 12:36:40 -0500

    started side nav and added react router

---
+++ lambda-notes/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import SideNav from './components/SideBar/SideNav';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SideNav />
+        
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ lambda-notes/src/components/SideBar/SideNav.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Side = styled.div `
+
+
+`
+
+
+const SideNav = () => {
+    return ( <Side>
+        
+        <h1> Lambda <br /> Notes</h1>
+        <button>View Your Notes</button>
+        <br />
+        <button>+Create New Notes</button>
+        
+        </Side> );
+}
+ 
+export default SideNav;
