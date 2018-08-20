081e6f2d005cd3d37e4739aebdfa5a2479da2a9e
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-08-20 11:36:49 -0500

    Setting up
    Setting up the home page with all of the notes chowing up, just doing a little bit of styling so that I can make all of the pages look and come up the same.

--- lambda-todo/src/App.js
+++ lambda-todo/src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Nav from './Home/Nav Links/Nav'; 
+import NotesHolder from './Home/HomePage/NotesHolder'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <div className="nav-bar">
+      <Nav />
+      </div>
+      <div className="pages">
+      <NotesHolder />
+      </div>
       </div>
     );
   }
---
+++ lambda-todo/src/Home/HomePage/NotesHolder.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import './NotesHolder.css'; 
+
+class NotesHolder extends Component{
+    render(){
+        return(
+            <div class="notes-holder">
+            </div>
+        )
+    }
+}
+export default NotesHolder; 
+
