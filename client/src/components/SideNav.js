13864e127295d8c3513ffa3c4f2a791aea5262cc
Ashwin <aswinjeganath@gmail.com>
2018-08-21 01:03:36 +0530

    Build the side nav

---
+++ client/src/components/SideNav.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledSideNav = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 250px;
+  background-color: #ddd;
+  padding: 1.5rem;
+  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.3);
+`;
+
+const PrimaryHeading = styled.h1`
+  font-size: 3.6rem;
+  margin-bottom: 2rem;
+`;
+
+const BlockButton = styled.button`
+  width: 100%;
+  padding: 1rem 0;
+  text-align: center;
+  font-size: inherit;
+  font-weight: bold;
+  color: #fafafa;
+  background-color: #26a69a;
+  border: none;
+  display: block;
+  cursor: pointer;
+
+  &:not(:last-child) {
+    margin-bottom: 1rem;
+  }
+`;
+
+const SideNav = () => {
+  return (
+    <StyledSideNav>
+      <PrimaryHeading>
+        Lambda <br /> Notes
+      </PrimaryHeading>
+
+      <BlockButton>View Your Notes</BlockButton>
+
+      <BlockButton>+ Create a new note</BlockButton>
+    </StyledSideNav>
+  );
+};
+
+export default SideNav;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { injectGlobal } from 'styled-components';
 
 import App from './App';
 
+injectGlobal`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-size: 1.6rem;
+    font-family: 'Roboto', sans-serif;
+    background-color: #eee;
+    color: #434343;
+    margin-left: 250px; /* taking into account the sidenav */
+  }
+`;
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
