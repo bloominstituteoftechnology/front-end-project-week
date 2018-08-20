10350cb2f2d700e10e702660552d195c5ef8ffd2
Sean <sean47900@gmail.com>
2018-08-20 12:25:38 -0500

    note content

---
+++ lambda/src/components/Notes.js
@@ -0,0 +1,54 @@
+export const notes = [
+    {
+        id: 0,
+        title: 'Note Title',
+        content: "Convenire comprehensam, cu novum movet prompta pri, moderatius definitiones per ex. Has sale novum erroribus ei, mel quando bonorum gubergren eu. Usu elit tritani accusam ea, ei regione fabulas complectitur sit. At labores nominavi perfecto sit. Te quo incorrupte scriptorem. Dolorem aliquando definitiones mea ea, dictas vocent ei nec, vis libris erroribus id."
+    },
+
+    {
+        id: 1,
+        title: 'Note Title',
+        content: "Convenire comprehensam, cu novum movet prompta pri, moderatius definitiones per ex. Has sale novum erroribus ei, mel quando bonorum gubergren eu. Usu elit tritani accusam ea, ei regione fabulas complectitur sit. At labores nominavi perfecto sit. Te quo incorrupte scriptorem. Dolorem aliquando definitiones mea ea, dictas vocent ei nec, vis libris erroribus id."
+    },
+
+    {
+        id: 2,
+        title: 'Note Title',
+        content: "Convenire comprehensam, cu novum movet prompta pri, moderatius definitiones per ex. Has sale novum erroribus ei, mel quando bonorum gubergren eu. Usu elit tritani accusam ea, ei regione fabulas complectitur sit. At labores nominavi perfecto sit. Te quo incorrupte scriptorem. Dolorem aliquando definitiones mea ea, dictas vocent ei nec, vis libris erroribus id."
+    },
+
+    {
+        id: 3,
+        title: 'Note Title',
+        content: "Convenire comprehensam, cu novum movet prompta pri, moderatius definitiones per ex. Has sale novum erroribus ei, mel quando bonorum gubergren eu. Usu elit tritani accusam ea, ei regione fabulas complectitur sit. At labores nominavi perfecto sit. Te quo incorrupte scriptorem. Dolorem aliquando definitiones mea ea, dictas vocent ei nec, vis libris erroribus id."
+    },
+
+    {
+        id: 4,
+        title: 'Note Title',
+        content: "Convenire comprehensam, cu novum movet prompta pri, moderatius definitiones per ex. Has sale novum erroribus ei, mel quando bonorum gubergren eu. Usu elit tritani accusam ea, ei regione fabulas complectitur sit. At labores nominavi perfecto sit. Te quo incorrupte scriptorem. Dolorem aliquando definitiones mea ea, dictas vocent ei nec, vis libris erroribus id."
+    },
+
+    {
+        id: 5,
+        title: 'Note Title',
+        content: "Convenire comprehensam, cu novum movet prompta pri, moderatius definitiones per ex. Has sale novum erroribus ei, mel quando bonorum gubergren eu. Usu elit tritani accusam ea, ei regione fabulas complectitur sit. At labores nominavi perfecto sit. Te quo incorrupte scriptorem. Dolorem aliquando definitiones mea ea, dictas vocent ei nec, vis libris erroribus id."
+    },
+
+    {
+        id: 6,
+        title: 'Note Title',
+        content: "Convenire comprehensam, cu novum movet prompta pri, moderatius definitiones per ex. Has sale novum erroribus ei, mel quando bonorum gubergren eu. Usu elit tritani accusam ea, ei regione fabulas complectitur sit. At labores nominavi perfecto sit. Te quo incorrupte scriptorem. Dolorem aliquando definitiones mea ea, dictas vocent ei nec, vis libris erroribus id."
+    },
+
+    {
+        id: 7,
+        title: 'Note Title',
+        content: "Convenire comprehensam, cu novum movet prompta pri, moderatius definitiones per ex. Has sale novum erroribus ei, mel quando bonorum gubergren eu. Usu elit tritani accusam ea, ei regione fabulas complectitur sit. At labores nominavi perfecto sit. Te quo incorrupte scriptorem. Dolorem aliquando definitiones mea ea, dictas vocent ei nec, vis libris erroribus id."
+    },
+    {
+        id: 8,
+        title: 'Note Title',
+        content: "Convenire comprehensam, cu novum movet prompta pri, moderatius definitiones per ex. Has sale novum erroribus ei, mel quando bonorum gubergren eu. Usu elit tritani accusam ea, ei regione fabulas complectitur sit. At labores nominavi perfecto sit. Te quo incorrupte scriptorem. Dolorem aliquando definitiones mea ea, dictas vocent ei nec, vis libris erroribus id."
+    },
+]
