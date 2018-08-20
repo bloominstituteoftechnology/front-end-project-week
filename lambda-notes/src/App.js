a976ec02091562ff3926df7e7e82fc1f1d30009b
Jurgen Kela <jurgenkela@Jurgens-MacBook-Pro.local>
2018-08-20 19:01:07 -0400

    Navbar completed and Note display

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom';
 
-// import './App.css';
+import Note from "./components/Note";
+
+import './App.css';
 
 class App extends Component {
+  state={
+    notes: [
+      {
+        id: 0,
+        title: 'Note Title',
+        text: 'This is what I have to do',
+      },
+      {
+        id: 1,
+        title: 'Note Title',
+        text: 'More of what I have to do',
+      }
+    ]
+  };
+
+  addNote = props => {
+    this.setState(prevState => ({ note: [...prevState.notes, props] }));
+  }
 
 
   render() {
     return (
-      <div className="App">
+      <div className='App'>
+        <div className="nav-bar">
+          <h1>Lambda Notes</h1>
+          <Link to='/notes'>View your notes</Link>
+          <Link to='/add'>+Create New Note</Link>
+        </div>
+
+        <Route exact path='/' render={() => ( 
+          <div>Home Page</div> )}
+        />
+        <Route exact path='/add' render={() => ( 
+          <div>Add a Note</div> )}
+        />
+        <Route exact path='/notes' render={() => ( 
+          <Notes notes={this.state.notes} /> )}
+        />
+        <Route exact path='/notes/:id' render={props => (
+          <Note match={props.match} notes={this.state.notes}/>)}
+        />
 
       </div>
     );
