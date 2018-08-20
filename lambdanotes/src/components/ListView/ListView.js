d6b0529f6688479abf5906c5a56c98dc61138e40
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-08-20 14:40:28 -0500

    Displaying cards info.

--- lambdanotes/src/components/ListView/ListView.js
+++ lambdanotes/src/components/ListView/ListView.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './ListView.css';
 
 import {
@@ -8,17 +9,51 @@ import {
     NavLink
   } from "react-router-dom";
 
+import NoteCard from './NoteCard';
+
 class ListView extends Component {
     constructor() {
         super();
+        this.state = {
+            noteList: [],
+        };
     }
+
+    componentDidMount() {
+        const endpoint = 'https://killer-notes.herokuapp.com/note/get/all';
+    
+        axios
+        .get(endpoint)
+        .then(response => {
+          this.setState({ noteList: response.data });
+        })
+        .catch(error => {
+          console.log('Error: ', error);
+        });
+    }
+
     render() {
         return (
             <div className="list-container">
                 this is list-container
+                <ul>
+          {this.state.noteList.map(note => {
+            return (
+              <NoteCard
+                tags={note.tags}
+                title={note.title}
+                textBody={note.textBody}
+              />
+            );
+          })}
+        </ul>
             </div>
         );
     }
 }
+
+ListView.defaultProps = {
+    noteList: [],
+};
 
 export default ListView;
---
+++ lambdanotes/src/components/ListView/NoteCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const NoteCard = props => {
+  return (
+    <div className="note-card">
+      <h3>{props.tags}</h3>
+      <p>{props.title}</p>
+      <p>{props.textBody}</p>
+    </div>
+  );
+};
+
+// NoteCard.defaultProps = {
+//   tags: '',
+//   title: '',
+//   textBody: ''
+// };
+
+export default NoteCard;
