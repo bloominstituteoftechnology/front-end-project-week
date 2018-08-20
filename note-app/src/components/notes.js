734abef7584b867b3d825b1c9feb8b93602081a3
codejoncode <jonathanjholloway@gmail.com>
2018-08-20 15:53:34 -0500

    created some notes so that I can render them to the screen. Note component needed.

---
+++ note-app/src/components/notes.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+
+class ViewAllNotes extends Component {
+    constructor (props){
+        super(props)
+        this.state = {
+
+        }
+    }
+    
+    render() {
+
+        return (
+            <div className = "create-note-form">
+              <h3>Your Notes:</h3>
+
+
+            </div>
+        )
+        
+    }
+}
+
+export default ViewAllNotes
