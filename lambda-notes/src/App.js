fe871bcb709ff22d0e927e13fb14288440207e83
Charlie Winslow <cwinslow22@yahoo.com>
2018-08-20 12:41:50 -0500

    added some dummy data and started working on the vertical nav bar

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -2,13 +2,46 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      notes: [
+        {
+          id: 0,
+          title: 'note 0',
+          note: this.ipsumNote
+        },
+        {
+          id: 1,
+          title: 'note 1',
+          note: this.ipsumNote
+        },
+        {
+          id: 2,
+          title: 'note 2',
+          note: this.ipsumNote
+        },
+        {
+          id: 3,
+          title: 'note 3',
+          note: this.ipsumNote
+        }
+      ]
+    }
+  }
+
+  ipsumNote = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultrices nulla sit amet arcu tempus, non dapibus nibh tincidunt. Nam a ullamcorper arcu. Curabitur efficitur sem eget libero tincidunt, ac pharetra sem cursus.'
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Lambda Notes</h1>
+          <button>View Your Notes</button>
+          <button>+ Create New Note</button>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
