682da2bc231c93a712beb2b6ad8bdd3fd22348fb
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-08-20 15:07:43 -0500

    Create New Note
    No functionality yet, but I am done with the basic styling of the page

--- lambda-todo/src/New Note/NewNotePage.js
+++ lambda-todo/src/New Note/NewNotePage.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 import './NewNotePage.css'; 
+import {NavLink} from 'react-router-dom'; 
 
 class NewNotePage extends Component{
     render(){
         return(
             <div className="new-note">
             <h1>Create New Note</h1>
-            <div>
+            <div className="new-note-inputs">
+                <input
+                placeholder="Title"
+                type="text"
+                className="title-input"
+                />
+                <input 
+                placeholder="Content"
+                type="text"
+                className="content-input"
+                />
+                <NavLink className="nav-btn"to='/'>
+                Add New Note
+                </NavLink>
             </div>
             </div>
         )
