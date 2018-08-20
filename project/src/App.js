fdfba9829f2683f034a31fbfa67fa0d6860b0c7f
jaymzsocool <james.mcraejr@gmail.com>
2018-08-20 19:04:31 -0400

    componets for sidebar, note list, and note view laid out. need further styling.

--- project/src/App.js
+++ project/src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './components/Sidebar';
+import NoteList from './components/NoteList';
+import { notes } from './data'
+import { Route } from 'react-router-dom'
+import NoteView from './components/NoteView'
 
 class App extends Component {
+  constructor(){
+    super(),
+    this.state={
+        notes: notes
+    }
+}
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route path = '/' component = {Sidebar} />
+        <Route exact path = '/' render = {props => <NoteList {...props} notes = {this.state.notes} />} />
+        <Route exact path = '/:id' render = {props => <NoteView {...props} notes = {this.state.notes} />} />
       </div>
     );
   }
---
+++ project/src/components/NoteView.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+import { Link } from 'react-router-dom'
+
+class NoteView extends Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div className = "note-page">
+                <div className = "note-links">
+                    <Link to = "/edit"><span>edit</span></Link><span>delete</span>
+                </div>
+                <h2 className = "note-title">{this.props.notes[this.props.match.params.id].title}</h2>
+                <p className = "note-content">{this.props.notes[this.props.match.params.id].note}</p>
+            </div>
+        )
+    }
+} 
+
+export default NoteView
