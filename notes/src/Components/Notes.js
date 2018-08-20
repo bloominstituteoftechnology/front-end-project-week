a1f03a342dcd066a1ffe6798c86f19248ac3a131
Trevor <trevorfehrman@gmail.com>
2018-08-20 12:13:42 -0700

    Dynamic link bug resolved

--- notes/src/Components/Notes.js
+++ notes/src/Components/Notes.js
@@ -1,23 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-class Notes extends React.Component {
-	state = { display: false };
-	render() {
-		return (
-			<div>
-				<h3>Your Notes:</h3>
-				{this.props.notes.map(note => (
-					<Link to={`/notes/${note.id}`}>
-						<div>
-							<h4>{note.title}</h4>
-							<p>{note.text}</p>
-						</div>
-					</Link>
-				))}
-			</div>
-		);
-	}
-}
-
+const Notes = ({ notes }) => (
+	<div>
+		{notes.map(note => (
+			<Link to={`/notes/${note.id}`}>
+				<h4>{note.title}</h4>
+				<p>{note.text}</p>
+			</Link>
+		))}
+	</div>
+);
 export default Notes;
