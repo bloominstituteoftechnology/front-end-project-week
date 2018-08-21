0461ca91b1e91f4402dfb600dce090eafc8ce24c
jordan-massingill <jordan_massingill@hotmail.com>
2018-08-20 20:18:21 -0500

    Completed add note and view individual note functionalities

--- notes/src/components/new-note-page/NewNote.js
+++ notes/src/components/new-note-page/NewNote.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export const NewNote = (props) => {
+
+export default function NewNote(props) {
   return (
       <div>
-        <form onSubmit={props.addNote}>
+        <form onSubmit={(event) => {event.preventDefault(); props.addNote(props); props.history.push('/')}}>
           <input name='newTitle' type='text' value={props.title} placeholder='new note title...' onChange={props.change} /><br/>
           <input name='newNote' type='text' value={props.note} placeholder='new note text...' onChange={props.change} /><br/>
           <button type='submit'>Submit Note</button>
--- notes/src/components/single-note-page/NotePage.js
+++ notes/src/components/single-note-page/NotePage.js
@@ -3,6 +3,7 @@ import { Note } from './Note';
 import { EditNote } from '../edit-note-page/EditNote';
 
 export const NotePage = (props) => {
+  console.log(props);
   const note1 = props.notes.find(note => note.id.toString() === props.match.params.id);
   return (
     note1.editing ? <EditNote title={props.title} note={props.note} /> : <Note note={note1} />
