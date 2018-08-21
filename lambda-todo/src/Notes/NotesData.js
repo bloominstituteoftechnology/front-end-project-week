edb84a73357954070f1c2577011eca990344dddc
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-08-21 17:53:27 -0500

    Added some more notes to the NotesData

--- lambda-todo/src/Notes/NotesData.js
+++ lambda-todo/src/Notes/NotesData.js
@@ -46,6 +46,30 @@ let Notes = [
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
     content:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+    id: 7,
+    title: "Garden Work",
+    cardcontent: 
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+    id: 8,
+    title: "Coding Notes",
+    cardcontent: 
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+    id: 9,
+    title: "Walk the Dog",
+    cardcontent: 
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   }
 ];
 export default Notes;
