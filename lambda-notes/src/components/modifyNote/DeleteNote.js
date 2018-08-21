f9bae6443f41286fa5036bb43e523536192a6872
szincone <sawyerzincone@gmail.com>
2018-08-21 13:26:05 -0600

    added react-modal boilerplate

--- lambda-notes/src/components/modifyNote/DeleteNote.js
+++ lambda-notes/src/components/modifyNote/DeleteNote.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-export const DeleteNote = props => {
-  console.log("DeleteNote Props", props);
-  return (
-    <div className="deleteNoteDiv">
-      <Link to={"/"}>
-        <button>Home</button>
-      </Link>
-    </div>
-  );
-};
+export class DeleteNote extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false
+    };
+    // got to bind here to make 'this' work in callback
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  handleOpenModal() {
+    this.setState({ showModal: true });
+  }
+
+  handleCloseModal() {
+    this.setState({ showModal: false });
+  }
+
+  render() {
+    return (
+      <div className="deleteNoteDiv">
+        <button onClick={this.handleOpenModal}>Delete</button>
+        <ReactModal
+          isOpen={this.state.showModal}
+          contentLabel="Minimal Modal Example"
+        >
+          <button onClick={this.handleCloseModal}>Close Modal</button>
+        </ReactModal>
+      </div>
+    );
+  }
+}
