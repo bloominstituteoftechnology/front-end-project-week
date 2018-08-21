659b8bd18f099221beefcbdc6964ea2a83614394
codejoncode <jonathanjholloway@gmail.com>
2018-08-20 23:03:36 -0500

    the buttons now have lighter opacity on delete form set up with a conditional. The other links are also disabled during this time you can only click from the modal.

--- note-app/src/components/side-bar.js
+++ note-app/src/components/side-bar.js
@@ -12,10 +12,20 @@ class SideBar extends Component {
     }
 
     render(){
+
+        if (this.props.name){
+            return (
+                <div className ="side-bar">
+                  <h1 className = {this.props.name ? "application-title deleting": "application-title" }>Lambda Notes</h1>
+                  <button className="btn-side-bar deleting">View Your Notes</button>
+                  <button className="btn-side-bar deleting">+ Create New Note</button>
+               </div>
+            )
+        }
         
         return (
             <div className ="side-bar">
-                <h1 className = {this.props.name ? "application-title deleting": "application-title" }>Lambda Notes</h1>
+                <h1 className = "application-title">Lambda Notes</h1>
                 <Link to = "/"><button className="btn-side-bar">View Your Notes</button></Link>
                 <Link to="create-note"> <button className="btn-side-bar">+ Create New Note</button> </Link>
             </div>
