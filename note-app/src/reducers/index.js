d6cbdfbb17c61004fc12fdfd5e8f24f2151f1421
codejoncode <jonathanjholloway@gmail.com>
2018-08-21 17:03:42 -0500

    created the reducer

--- note-app/src/reducers/index.js
+++ note-app/src/reducers/index.js
@@ -6,6 +6,52 @@ export const DELETING_NOTE = 'DELETING_NOTE';
 export const DELETED_NOTE = 'DELETED_NOTE';
 export const GETTING_NOTES = 'GETTING_NOTES';
 export const GOT_NOTES = 'GOT_NOTES';
+export const SELECTING_NOTE = 'SELECTING_NOTE';
+export const SELECTED_NOTE = 'SELECTED_NOTE'; 
+// export const SELECTED_NOTE = 'SELECTED_NOTE'; 
 export const ERROR = 'ERROR'; 
+
+const initialState = {
+    notes: [],
+    addingNote: false,
+    updatingNote: false,
+    deletingNote: false,
+    gettingNotes: false, 
+    selecting: false, 
+    error: null,
+    select: null,
+    nextId: null,
+
+}
+
+const reducerNote = (state = initialState, action) => {
+  switch(action.type){
+    case ADDING_NOTE:
+      return {...state, addingNote: true}
+    case ADDED_NOTE:
+      return {...state, addingNote: false, notes: [...notes, action.payload]}
+    case UPDATING_NOTE:
+      return {...state, updatingNote: true}
+    case UPDATED_NOTE:
+      return {...state, updatingNote: false, notes: [...notes, action.payload]}
+    case DELETING_NOTE:
+      return {...state, deletingNote: true}
+    case DELETED_NOTE:
+      return {...state, deletingNote: false, notes: [...state, action.payload]}
+    case SELECTING_NOTE:
+      return {...state, selecting: true}
+    case SELECTED_NOTE:
+      return {...state, selecting: false, select: action.payload }
+    case GETTING_NOTES:
+      return {...state, gettingNotes: true }
+    case GOT_NOTES:
+      return {...state, gettingNotes: false, notes: action.payload}
+    case ERROR:
+      return {...state, gettingNotes: false, addingNote: false, updatingNote: false, deletingNote: false, selecting: false, error: action.error }
+    default:
+      return state
+  }
+
+}
 
 
