28c446288bfe235b6954171bbb1c2e71b0bf9dde
codejoncode <jonathanjholloway@gmail.com>
2018-08-21 16:34:24 -0500

    Actions completed

--- note-app/src/actions/index.js
+++ note-app/src/actions/index.js
@@ -1,6 +1,6 @@
-import {GETTING_NOTES, ADDING_NOTE, UPDATING_NOTE, DELETING_NOTE, GOT_NOTES} from '../reducers'
+import {GETTING_NOTES, ADDING_NOTE, UPDATING_NOTE, DELETING_NOTE, GOT_NOTES, ADDED_NOTE, UPDATED_NOTE, DELETED_NOTE} from '../reducers'
 import {store} from '../index'
-
+import axios from 'axios' 
 
 
 export const fetchingNotes = () => {
@@ -17,20 +17,57 @@ export const fetchingNotes = () => {
 }
 
 export const addingNote = note => {
-    store.dispatch({
-      type: ADDING_NOTE,
-      payload: note
-  })
+    const promise = axios.post('http://localhost:8080/notes', noteobj)
+    return (dispatch) => {
+      dispatch({type:ADDING_NOTE})
+      promise.then( response => {
+          dispatch({type:ADDED_NOTE, payload: response.data})
+      })
+      .catch(error => {
+          dispatch({type:ERROR})
+      })
+    }
 }
+
 export const updatingNote = (id, note) => {
-    store.dispatch({
-      type: UPDATING_NOTE,
-      paylaod: {id, note}
-  })
+    const promise = axios.put(`http://localhost:8080/notes/${id}`, note)
+    return dispatch => {
+      dispatch({type:UPDATING_NOTE})
+      promise.then( response => {
+        dispatch({type: UPDATED_NOTE, payload:response.data})
+        // may have to call on the fetcher 
+      })
+      .catch(error => {
+          dispatch({type:ERROR, error})
+      })
+    }
+    
+    
 }
+
+deleteNote  = (index) => {
+    const promise = axios.delete(`http://localhost:8080/notes/${index}`)
+    promise
+    .then(response => {
+      console.log(response)
+      this.fetchNotes()
+    })
+    .catch(error => {
+      console.log(error, index) 
+    })
+
+  }
+
 export const deletingNote = id => {
-    store.dispatch({
-      type: DELETING_NOTE,
-      payload: id
-  })
+    const promise = axios.delete(`http://localhost:8080/notes/${id}`)
+    return dispatch => {
+        dispatch({type:DELETING_NOTE})
+        promise.then(response => {
+            dispatch({type:DELETED_NOTE, payload: response.data})
+        })
+        .catch(error => {
+            dispatch({type:ERROR, error})
+        })
+        
+    }
 }
