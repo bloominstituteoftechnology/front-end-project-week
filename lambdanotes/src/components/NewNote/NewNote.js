b0cf39e8b50cf38e1c865f678b2e52f3dc3bd2d7
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-08-21 10:48:46 -0500

    Create-New-Note Functionality working properly.

--- lambdanotes/src/components/NewNote/NewNote.js
+++ lambdanotes/src/components/NewNote/NewNote.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './NewNote.css';
+import axios from 'axios';
 
 import {
     BrowserRouter as Router,
@@ -9,25 +10,60 @@ import {
   } from "react-router-dom";
 
 class NewNote extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+        this.state = {
+            newnote: [],
+            tag: '',
+            title: '',
+            textBody: '',
+        };
     }
+
+    addNote = event => {
+        event.preventDefault();
+       
+        const newNote = {
+            tag: this.state.tag,
+            title: this.state.title,
+            textBody: this.state.textBody,
+        }
+        axios
+        .post('https://killer-notes.herokuapp.com/note/create', newNote)
+        .then(response => {
+            this.setState({ newnote: response.data })
+        })
+        .catch(error => {
+            console.log('Error: ', error)
+        })
+    }
+
+    handleInputChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
+
     render() {
         return (
         <div className="newnote-container">
             <h3 className="newnote-title">Create New Note:</h3>
-            <form className="newnote-form">
+            <form onSubmit={this.addNote} className="newnote-form">
                 <input 
+                    onChange={this.handleInputChange}
+                    value={this.state.title}
+                    name="title"
                     placeholder="Note Title" 
                     type="text"
                     className="note-title-input"
                 />
                 <input 
+                    onChange={this.handleInputChange}
+                    value={this.state.textBody}
+                    name="textBody"
                     placeholder="Note Content" 
                     type="text"
                     className="note-content-input"
                 />
-                <button>Save</button>
+                <button type="submit">Save</button>
             </form>
         </div>
         );
