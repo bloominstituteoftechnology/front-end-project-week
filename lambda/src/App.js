e910190b021bce52125b4ab691d646b36592f454
Sean <sean47900@gmail.com>
2018-08-20 20:11:13 -0500

    still working on funcionality

--- lambda/src/App.js
+++ lambda/src/App.js
@@ -3,36 +3,49 @@ import './App.css';
 import {Route, Link} from 'react-router-dom';
 import SideBar from './components/sidebar'
 import NoteView from './components/NoteView';
-import NewNote from './components/NewNote'
+import NewNote from './components/NewNote';
+import {notes} from './components/Notes';
 
+let id = 10;
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-  
+      notes:notes,
       title: '',
       content: '',
-    }}
+      id: id,
+      
+    }} 
+     handleChange = e =>{
+      this.setState({[e.target.name]: e.target.value});
+    }
      addNote = e => {
       e.preventDefault();
       let notes = this.state.notes.slice();
-      notes.push({title: this.state.title, content: this.state.content});
+
+      notes.push({
+        id: this.state.id,
+        title: this.state.title,
+        content: this.state.content,
+        });
       this.setState({notes, title:'', content:''})
       console.log(this.state.notes);
     }
-    handleChange = e =>{
-      this.setState({[e.target.name]: e.target.value});
-    }
+  
    
   render() {
     return (
       <div className="App">
      
       <SideBar/> 
-       <Route exact path="/" render={props => <NoteView {...props}/>}/>
+
+       <Route exact path="/" render={props => <NoteView {...props} notes={this.state.notes}/>}/>
+
        <Route exact path="/new" render={props => <NewNote {...props}title={this.state.title}
        content={this.state.content}handleChange={this.handleChange}addNote={this.addNote} />}/>
+
       </div>
     );
   }
