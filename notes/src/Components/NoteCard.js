797d1389c2e7aa70d62806f0bdfce1c831c80ad7
Trevor <trevorfehrman@gmail.com>
2018-08-21 19:57:18 -0700

    Code a bit DRYer

---
+++ notes/src/Components/NoteCard.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NoteCard = ({ note }) => {
+	return (
+		<Link key={note._id} className="Notes" to={`/notes/${note._id}`}>
+			<div key={note._id}>
+				<h4 className="Notes__title">
+					{note.title.length > 20
+						? note.title.substring(0, 20) + "..."
+						: note.title}
+				</h4>
+				{note.tags.map(tag => (
+					<span className="Notes__tag">{tag}</span>
+				))}
+				<p className="Notes__text">
+					{note.textBody.length > 170
+						? note.textBody.substring(0, 170) + "..."
+						: note.textBody}
+				</p>
+			</div>
+		</Link>
+	);
+};
+
+export default NoteCard;
--- notes/src/Components/Notes.js
+++ notes/src/Components/Notes.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
+import NoteCard from "./NoteCard";
 import Tags from "./Tags";
 
 class Notes extends React.Component {
@@ -10,27 +10,7 @@ class Notes extends React.Component {
 				<h1 className="Notes__header">Your Notes:</h1>
 				<Tags notes={this.props.notes} />
 				{this.props.notes.map(note => (
-					<Link
-						key={note._id}
-						className="Notes"
-						to={`/notes/${note._id}`}
-					>
-						<div key={note._id}>
-							<h4 className="Notes__title">
-								{note.title.length > 20
-									? note.title.substring(0, 20) + "..."
-									: note.title}
-							</h4>
-							{note.tags.map(tag => (
-								<span className="Notes__tag">{tag}</span>
-							))}
-							<p className="Notes__text">
-								{note.textBody.length > 170
-									? note.textBody.substring(0, 170) + "..."
-									: note.textBody}
-							</p>
-						</div>
-					</Link>
+					<NoteCard note={note} />
 				))}
 			</div>
 		);
