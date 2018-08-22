7d76a40a979920106ffaad3fa44f43d701b8122e
codejoncode <jonathanjholloway@gmail.com>
2018-08-22 15:53:32 -0500

    at this point you can delete search terms in order to lower the results by clicking the x next to the search term

--- note-app/src/components/Search.js
+++ note-app/src/components/Search.js
@@ -20,19 +20,38 @@ class Search extends React.Component {
         console.log('clicked')
         const searchTerm = this.state.search.slice()
         const searches = this.state.searches.slice()
-        searches.push(searchTerm)
+        if(searchTerm.length){
+            searches.push(searchTerm)
+        }
         const results = this.props.notes
-        const display = results.filter(result => result.tags.includes(searchTerm) || result.title.includes(searchTerm) || result.textBody.includes(searchTerm))
+        const display = []
+        searches.forEach(searchTerm => {
+            console.log(searchTerm)
+            results.forEach(result => {
+                console.log(result)
+            if(result.tags.includes(searchTerm) || result.title.includes(searchTerm) || result.textBody.includes(searchTerm)){
+                if(!display.includes(result.title)){
+                display.push(result.title)
+                console.log("pushed it")
+                }
+                }
+            })
+        })
         console.log(display)
         console.log(this.state.search)
         console.log(this.state.dataIn)
         this.setState({results: display, search:'', dataIn: true, searches})
+        
     }
+
     handleKeywordClick = event => {
         console.log(event.target.innerHTML)
         let searches = this.state.searches.slice();
         searches = searches.filter(search => search !== event.target.innerHTML) 
+        console.log(searches)
         this.setState({ searches })
+        console.log("after delete")
+        return setTimeout(() => {this.handleSearch()},300)
     }
 
 
@@ -43,6 +62,7 @@ class Search extends React.Component {
             notes = this.state.results.slice()
         }
         const searches = this.state.searches.slice()
+        console.log(notes)
         return (
             <div className="main-container">
               <SideBar />
@@ -53,7 +73,7 @@ class Search extends React.Component {
                     <h5>keywords: </h5>
                     {searches.map((search,i)=> <i onClick ={this.handleKeywordClick} key ={i} className="far fa-times-circle">{search}</i>)}
                   </div>
-                  {notes.map(note => <div className = "title-search" key = {note.id}>{note.title}</div>)}
+                  {notes.map((note,i) => <div className = "title-search" key={i}>{note}</div>)}
               </div>
             </div>
         )
