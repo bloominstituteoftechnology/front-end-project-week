641301d7579dfdc9c0d9a032c3377f481faab216
szincone <sawyerzincone@gmail.com>
2018-08-22 16:40:06 -0600

    inputchange updating title and description on NewNote

--- lambda-notes/src/components/notes/NewNote.js
+++ lambda-notes/src/components/notes/NewNote.js
@@ -4,17 +4,33 @@ class NewNote extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      allNotes: props.notes
+      allNotes: props.notes,
+      title: "",
+      description: ""
     };
   }
 
+  inputChangeHandler = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
   render() {
+    console.log("TITLE", this.state.title);
+    console.log("DESCRIPTION", this.state.description);
     return (
       <div>
         <h2>New Note:</h2>
         <form className="newNoteForm">
-          <input placehold="Note Title" />
-          <input placehold="Note Content" />
+          <input
+            placehold="Note Title"
+            name="title"
+            onChange={this.inputChangeHandler}
+          />
+          <input
+            placehold="Note Content"
+            name="description"
+            onChange={this.inputChangeHandler}
+          />
           <button>Update</button>
         </form>
       </div>
