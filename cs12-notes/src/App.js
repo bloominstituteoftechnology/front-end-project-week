91c3d6b478dcf11ac868d4c7a52748ff99d419fa
kyle <kauzlarich.kyle@gmail.com>
2018-08-22 11:44:37 -0700

    Added on Got the background going and all notes showing still need to refactor for that and non working nav bar for a place holder

--- cs12-notes/src/App.js
+++ cs12-notes/src/App.js
@@ -1,20 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+
+
+const DisplayNotes = (props) => {
+  console.log(props.dev[0]);
+  return (
+    <div className="DisplayNotes_Container">
+      {props.dev.map(notes =>
+        <div key={notes._id} className="DisplayNotes-Card">
+          <h1>{notes.title}</h1>
+          <h4>{notes.textBody}</h4>
+        </div>
+      )}
+    </div>
+  )
+}
+
+
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      dev: []
+    }
+  }
+  componentDidMount() {
+    axios
+    .get('https://killer-notes.herokuapp.com/note/get/all')
+    .then(responce => {
+      this.setState( { dev: responce.data } );
+    }).catch(err => console.log(err));
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    if (this.state.div === []) {
+      return (
+        <h1>Loading...</h1>
+      )
+    } else {
+      return (
+        <div className="container"> {/* container div */}
+
+          <div className="App_Top"> {/* top div */} 
+          <div className="App_Top-Button">
+            <span>View Notes</span>
+          </div>
+          <div className="App_Top-Button">
+            <span>New Note</span>
+          </div>
+          </div> {/* top div end */}
+
+          <div className="App_Bottom"> {/* bottom div */} 
+            <DisplayNotes dev={this.state.dev} test="hello"/>
+          </div> {/* bottom div end */}
+
+        </div> /* container end */
+      );
+    }
   }
 }
 
