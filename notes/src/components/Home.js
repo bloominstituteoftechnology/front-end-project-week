4875d9626d8bd90f67b52f24d929d27f1eb8b23a
David Pok <david.pok@gmail.com>
2018-08-21 20:49:17 -0700

    fixes some styling issues

--- notes/src/components/Home.js
+++ notes/src/components/Home.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
+import "./components.css";
 
 export default class Home extends Component {
   render() {
-    return <h1>Welcome to my shitty Notes app!</h1>;
+    return (
+      <div className="home-page">
+        <h1>Welcome to my shitty Notes app!</h1>
+      </div>
+    );
   }
 }
