4e612c5ddd189a2d5ed2d2afb6f28d7da35d94be
William <kwontaewoongah@gmail.com>
2018-08-21 21:34:30 -0700

    implemented searchbar

--- notes/src/components/notes/index.js
+++ notes/src/components/notes/index.js
@@ -23,20 +23,45 @@ min-height: 100vh;
 width: 75%;
 `
 class Notes extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            filteredNotes:'',
+            filterparam:''
+        }
+    }
     componentDidMount() {
         localStorage.setItem('location',this.props.location.pathname);
         this.props.getNotes();
     }
+    inputChange=(e)=>{
+        this.setState({[e.target.name]:e.target.value},()=>{
+            let filteredNotes=this.props.notes.slice();
+            filteredNotes=filteredNotes.filter(e=>e.textBody.includes(this.state.filterparam)||e.title.includes(this.state.filterparam));
+            return this.setState({filteredNotes:filteredNotes},()=>console.log(this.state.filteredNotes));
+        });
+    }
     render() {
+        if (!this.props.fetchingNotes) {
         return(
             <NotesPage>
-            <NotesHeading>Your Notes:</NotesHeading>
-            <NotesList>
-        {!this.props.fetchingNotes?this.props.notes.map((e,i)=><Note key={i} data={e}></Note>):null}
-            </NotesList>
+                <input type='text' name='filterparam' value={this.state.value} onChange={this.inputChange}/>
+                <NotesHeading>Your Notes:</NotesHeading>
+                <NotesList>
+                    {this.state.filterparam.length===0?
+                    this.props.notes.map((e,i)=><Note key={i} data={e}></Note>):
+                    this.state.filteredNotes?this.state.filteredNotes.map((e,i)=><Note key={i} data={e}></Note>):null
+                        }
+                </NotesList>
             </NotesPage>
         )
     }
+ else {
+    return (
+        <div></div>
+    );
+}
+}
 }
 const mapStateToProps=state=>{
     return{
