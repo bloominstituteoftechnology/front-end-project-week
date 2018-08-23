4ce85dbfd3f821606696336d41cc5dbbbeaf87fb
William <kwontaewoongah@gmail.com>
2018-08-22 18:13:57 -0700

    app now supports markdown for textBody

--- notes/src/components/notes/note.js
+++ notes/src/components/notes/note.js
@@ -8,13 +8,10 @@ width: 30%;
 margin-right: 3%;
 background-color: white;
 border: 1px solid #ddd;
-height: 220px;
+height: 230px;
 margin-bottom: 20px;
 text-align: left;
-
 h3,p{
-    margin-left: 10%;
-    margin-right: 10%;
     color:#000;
     word-wrap: break-word;
 }
@@ -23,28 +20,34 @@ h3{
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
-    width: 80%;
 }
 p{
     font-size: 16px;
 }
 a{
     text-decoration: none;
+    color: #000;
 }
 `
+const Content=styled.div`
+    width: 80%;
+    margin: 0 auto;
+    `
 const Rule=styled.div`
-width: 80%;
+width: 100%;
 margin: 0 auto;
 height: 2px;
 background-color:#c6c6c6;
 `
 const Note=(props)=><NoteContainer>
     <Link to={`/notes/${props.data._id}`}>
+    <Content>
     <h3>{props.data.title}</h3>
     <Rule/>
     {props.data.textBody.length<100?
     <ReactMarkdown source={props.data.textBody}/>:
-    <ReactMarkdown source={props.data.textBody.slice(0,100)+'...'}/>}
+    <ReactMarkdown source={props.data.textBody.slice(0,101)+'...'}/>}
+    </Content>
     </Link>
     </NoteContainer>
 export default Note;
