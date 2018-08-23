2863f7a983a1d7c63ff9f1b467bf84b7c3fe279f
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-08-23 10:15:50 -0700

    Delete functionality working - app doesn't refresh when a note is deleted

--- lambdanotes/src/components/DeleteNote/DeleteNote.js
+++ lambdanotes/src/components/DeleteNote/DeleteNote.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
+import './DeleteModal.css';
+
 class DeleteModal extends React.Component {
   constructor(props) {
     super(props);
@@ -20,15 +22,14 @@ class DeleteModal extends React.Component {
   render() {
     return (
       <div>
-        <Button color="danger" onClick={this.toggle}>x</Button>
-        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-          <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
-          <ModalBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <Button className="toggle-button" color="danger" onClick={this.toggle}>x</Button>
+        <Modal className="custom-modal" isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+          <ModalBody className="modal-text">
+            Are you sure you want to delete this?
           </ModalBody>
-          <ModalFooter>
-            <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
-            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+          <ModalFooter className="modal-footer">
+            <Button className="custom-button" color="danger" onClick={() => this.props.deleteNote(this.props.note)}>Delete</Button>{' '}
+            <Button className="custom-button" color="info" onClick={this.toggle}>No</Button>
           </ModalFooter>
         </Modal>
       </div>
--- lambdanotes/src/components/NoteView/NoteView.js
+++ lambdanotes/src/components/NoteView/NoteView.js
@@ -51,7 +51,7 @@ class NoteView extends React.Component {
             <NoteViewContainer>
                 <LinksContainer>
                     <Link to={`note-view/${this.state.note.id}/edit`} className="link">edit</Link>
-                    <DeleteModal>delete</DeleteModal>
+                    <DeleteModal note={this.state.note} deleteNote={this.props.deleteNote}>delete</DeleteModal>
                 </LinksContainer>
                 <div className="body-container">
                     <Title>{this.state.note.title}</Title>
