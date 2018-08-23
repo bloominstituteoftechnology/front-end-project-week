b414fbda6ddd2f6dc4bc5c720bdb9575ef8dc17c
CLN <cln@Sags-MacBook-Pro.local>
2018-08-23 16:25:28 -0400

    adding heroku url,.

--- src/App.js
+++ src/App.js
@@ -46,8 +46,9 @@ display: flex;
 }
 }
 componentDidMount() {
+  const apiURL = process.env.REACT_APP_API
   axios
-  .get('http://localhost:6500/api/notes')
+  .get(apiURL)
   .then(response => {
     console.log("Get Response",response);
     this.setState({notes: response.data });
