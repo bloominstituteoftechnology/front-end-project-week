75a9e82f149bb23cf57da583c3fe10404ea6a20e
DirupT <iamcooled@gmail.com>
2018-08-23 15:47:55 -0500

    removed loading bar

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import notes from './notes';
 import auth from './auth';
-import { loadingBarReducer } from 'react-redux-loading-bar'
 
 const rootReducer = combineReducers({
     notes,
-    auth,
-    loadingBar: loadingBarReducer,
+    auth
 });
 
 export default rootReducer;
