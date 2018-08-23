46b84eaa27634a260f0d5927718e22d530820910
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-08-23 14:04:39 -0500

    update to edit functionality to add original note to form

--- lambda-notes/src/components/EditNote.js
+++ lambda-notes/src/components/EditNote.js
@@ -19,23 +19,23 @@ class EditNote extends React.Component {
             note => note.id === Number(noteID)
         );
         if (!note) return;
-        this.setState({ note });
-    }Í
+        this.setState({ note, title: note.title, content: note.content });
+    }
 
 
     handleSubmit = (event) => {
         event.preventDefault();
         const updatedNote = {
-          id: this.state.note.id,
-          title: this.state.title,
-          content: this.state.content
+            id: this.state.note.id,
+            title: this.state.title,
+            content: this.state.content
         }
         this.props.editNote(updatedNote);
-      }
+    }
 
-      handleInputChange = event => {
+    handleInputChange = event => {
         this.setState({ [event.target.name]: event.target.value });
-      };
+    };
 
 
     render() {
@@ -49,10 +49,10 @@ class EditNote extends React.Component {
                 <Form>
                     <Row>
                         <Col sm="7">
-                            <Input onChange={this.handleInputChange} type="text" name="title" placeholder="Note Title" style={{ marginTop: 30 + 'px', border: "2px solid lightgray"  }} value={this.state.title} />
+                            <Input onChange={this.handleInputChange} type="text" name="title" placeholder="Note Title" style={{ marginTop: 30 + 'px', border: "2px solid lightgray" }} value={this.state.title} />
                         </Col>
                     </Row>
-                    <Input onChange={this.handleInputChange} type="textarea" name="content" placeholder="Note Content" rows="15" style={{ marginTop: 15 + 'px', border: "2px solid lightgray"  }} value={this.state.content} />
+                    <Input onChange={this.handleInputChange} type="textarea" name="content" placeholder="Note Content" rows="15" style={{ marginTop: 15 + 'px', border: "2px solid lightgray" }} value={this.state.content} />
                     <Row>
                         <Col sm="4">
                             <Button onClick={this.handleSubmit} onMouseUp={() => this.props.history.goBack()} color="info" style={{ marginTop: 15 + 'px' }} block>Update</Button>
