11ec0edd291a75256ebce40304d125ba6cd88e89
szincone <sawyerzincone@gmail.com>
2018-08-23 13:17:40 -0600

    created my cutoff text if statement and got it working on allnotes

--- lambda-notes/src/components/notes/AllNotes.js
+++ lambda-notes/src/components/notes/AllNotes.js
@@ -22,6 +22,16 @@ class AllNotes extends Component {
         <h2 className="yourNotesTitle">Your Notes:</h2>
         <div className="allNotesDiv">
           {this.state.allNotes.map(note => {
+            {
+              console.log("Note Title", note.title);
+            }
+            {
+              if (note.description.length > 5) {
+                note.description = note.description.substring(0, 5) + "...";
+              } else {
+                note.description = note.description;
+              }
+            }
             return (
               <Link to={`/notes/${note.id}`} className="noteCard" key={note.id}>
                 <h2>{note.title}</h2>
