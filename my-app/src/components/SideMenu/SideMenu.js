dbdc51c01461da515fc8e6cc80602372d63c0a3c
bri79319 <bri79319@gmail.com>
2018-08-27 14:41:32 -0400

    styling buttons for side menu

--- my-app/src/components/SideMenu/SideMenu.js
+++ my-app/src/components/SideMenu/SideMenu.js
@@ -4,8 +4,19 @@ import "./SideMenu.css";
 const SideMenu = () => {
   return (
     <div className="side-menu">
-      <div className="lambda-notes">
-        <h1>Lambda<br/>Notes</h1>
+      <div className="lambda">
+        <h1>Lambda</h1>
+      </div>
+      <div className="notes">
+          <h1>Notes</h1>
+      </div>
+      <div className="buttons">
+        <div className="view-notes">
+            <span>View Your Notes</span>
+        </div>
+        <div className="create-note">
+            <span>+ Create New Note</span>
+        </div>
       </div>
     </div>  
   );
