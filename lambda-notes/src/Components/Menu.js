ed143d1b8dc48c5c7a8ed936b8960b0eede1e5ec
JoCatherine Griggs <jocafrin@gmail.com>
2018-08-27 17:06:42 -0400

    menu created

--- lambda-notes/src/Components/Menu.js
+++ lambda-notes/src/Components/Menu.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {Link} from 'react-router-dom';
 
-class Menu extends Component {
-    render() {
-        return (
-            <div>
 
-            </div>
-        );
-    }
-}
+const Menu = () => {
+    return (
+        <div className='menu'>
+            <h1>Lambda Notes</h1>
+            <Link className='link' to='/'>View Your Notes</Link>
+            <Link className='link' to='/'>Create New Note</Link>
+        </div>
+    );
+};
 
 export default Menu;
 
