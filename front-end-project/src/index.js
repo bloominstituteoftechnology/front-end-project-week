c3a23eecdb2a6b7a3df8ab50535337c635a594c9
Noah-Amar <ne.amar@outlook.com>
2018-08-27 17:50:04 -0500

    getting set up, index.js

---
+++ front-end-project/src/index.js
@@ -0,0 +1,25 @@
+//React Imports
+import React from "react";
+import ReactDOM from "react-dom";
+//Components Imports
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+//Browser Imports
+import { BrowserRouter as Router } from "react-router-dom";
+//Redux Imports
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+const dummyReducer = (state = [], action) => state;
+
+const store = createStore(dummyReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
+registerServiceWorker();
