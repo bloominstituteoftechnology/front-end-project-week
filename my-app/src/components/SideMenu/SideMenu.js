9ae0b325674ebade3d525b522d3b222f47c6773a
bri79319 <bri79319@gmail.com>
2018-08-27 12:10:42 -0400

    constructing sidemenu component

---
+++ my-app/src/components/SideMenu/SideMenu.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SideMenu = () => {
+  return (
+    <div className="side-menu">
+      <button>View Your Notes</button>
+      <button>+ Create New Note</button>
+    </div>
+  );
+};
+
+export default SideMenu;
