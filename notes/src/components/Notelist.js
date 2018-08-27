8d11bb9d3db7170fd763e9b5d46cd790e3661448
keegn <keegan.t.burkett@gmail.com>
2018-08-27 13:20:18 -0500

    linked logo to/

--- notes/src/components/Notelist.js
+++ notes/src/components/Notelist.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import './Sidebar.css'
+import styled from 'styled-components'
 
 import Notecard from './Notecard'
 
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: auto;
+    grid-gap: 20px;
+    margin: 0 20px;
+    min-width: 200px;
+`
 class Notelist extends Component {
   state = {
     notes: this.props.notes
   }
   render() {
     return (
-      <div>
+      <Grid>
         {this.props.notes.map(note => {
-            return <div><Notecard key={note.id} note={note} /></div>
+            return <Notecard key={note.id} note={note} />
         })}
-      </div>
+      </Grid>
     );
   }
 }
--- notes/src/components/Sidebar.js
+++ notes/src/components/Sidebar.js
@@ -1,20 +1,23 @@
 import React from "react";
 import "./Sidebar.css";
 //we will need this for link to our /notes page and /add page - each button will be wrapped in a <Link to="/notes">button here etc
-// import Link from 'react-router-dom'
+import { Link }  from "react-router-dom";
 
 const Sidebar = () => {
   return (
-      <div className="Sidebar">
-        <div className="Sidebar_header">
-          <h1>
-            Lambda <br /> Notes
-          </h1>
-        </div>
-
+    <div className="Sidebar">
+      <div className="Sidebar_header">
+      <Link to="/">
+        <h1>
+          Lambda <br /> Notes
+        </h1>
+        </Link>
+      </div>
+      <Link to="/notes">
         <button className="Button">View Your Notes</button>
+      </Link>
 
-        <button className="Button">+ Create New Note</button>
+      <button className="Button">+ Create New Note</button>
     </div>
   );
 };
