9fcd27c2381d1c6bd6a5a16b1e5f42e260e94325
bri79319 <bri79319@gmail.com>
2018-08-27 13:27:42 -0400

    setting up side menu css and cards for the notes

--- my-app/src/components/SideMenu/SideMenu.js
+++ my-app/src/components/SideMenu/SideMenu.js
@@ -1,14 +1,13 @@
 import React from "react";
+import "./SideMenu.css";
 
 const SideMenu = () => {
   return (
     <div className="side-menu">
-      <div>
-        <h1>Lambda Notes</h1>
+      <div className="lambda-notes">
+        <h1>Lambda<br/>Notes</h1>
       </div>
-      <button>View Your Notes</button>
-      <button>+ Create New Note</button>
-    </div>
+    </div>  
   );
 };
 
