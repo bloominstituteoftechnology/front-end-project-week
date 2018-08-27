45c18f990c7b7ee2d05e5827876514240b216f04
vanceleon <vanceleon44@gmail.com>
2018-08-27 14:09:29 -0400

    app.js build out commit before adding less to application

--- notebook/src/App.js
+++ notebook/src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route} from 'react-router-dom';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      notes: [],
+
+    }
+  }
+
+    
+
+
   render() {
     return (
+      //Route NoteList
+      //Route Note Form
+      //Build Sidebar
+      
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="side-bar-container">
+          <aside className="sidebar-left">
+            Left Sidebar
+          </aside>
+        </div>
+        <div className="notes-container">
+          Body note content will go here
+        </div>
+        <Route>
+
+        </Route>
+      
       </div>
     );
   }
