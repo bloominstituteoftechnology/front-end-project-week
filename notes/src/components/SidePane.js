ceb590e3153bd94bea013070d130b3b112696fb3
AI2EJAY <JHammett1314@gmail.com>
2018-08-27 14:38:19 -0500

    Navbar styling good enough for now. Moving to displaying data

--- notes/src/components/SidePane.js
+++ notes/src/components/SidePane.js
@@ -11,49 +11,57 @@ const NavContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   border-right: 1px solid grey;
+  margin-bottom: 0px;
+  padding: 0px;
+  position: relative;
+  left: 0;
   h1 {
     font-size: 36px;
     font-weight: bold;
-    margin-left: 5%;
+    margin-left: 12%;
     height: 40px;
+    margin-top: 5px;
+
   }
 `;
 const Buttons = styled.div`
-
-
   width: 80%;
   color: white;
-  text-align: center;
   margin: 10px;
+  margin: 0 auto;
+  margin-top: -10%;
+  position: absolute;
+  top: 110px;
+  right: 30px;
+
 `;
 
 const SideButton = styled.div`
   background-color: #5eb3bb;
   height: 40px;
-  width: 80%;
   text-align: center;
   margin: 10px;
   align-content: center;
   display: flex;
   justify-content: center;
-  a{
+  
+  a {
     text-decoration: none;
     color: white;
     font-weight: bold;
     align-self: center;
-    
   }
 `;
 const SidePane = () => {
   return (
     <NavContainer>
-      <h1>Lambda<br/>Notes</h1>
+        <h1>Lambda<br/>Notes</h1>
       <Buttons>
         <SideButton>
-          <a href='#'>View Your Notes</a>
+          <a href="#">View Your Notes</a>
         </SideButton>
         <SideButton>
-          <a href='#'>+ Create New Note</a>
+          <a href="#">+ Create New Note</a>
         </SideButton>
       </Buttons>
     </NavContainer>
