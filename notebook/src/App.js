8084da204f1f590513a4484a6d6d4c6ba16e33c0
vanceleon <vanceleon44@gmail.com>
2018-08-27 18:43:27 -0400

    server and less setup

--- notebook/src/App.js
+++ notebook/src/App.js
@@ -5,14 +5,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="welcome">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="testing">
+          working in less react
       </div>
     );
   }
