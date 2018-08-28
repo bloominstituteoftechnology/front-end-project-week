9c20bf723857946669562d25e682e3fae4ba2464
cubsfan46350 <cubsfan.46350@msn.com>
2018-08-28 10:16:22 -0500

    added main content component

---
+++ lambdanotes/src/Components/MainContent/MainContent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './mainContent.css';
+
+
+class MainContent extends Component {
+    render() {
+        return(
+            <div>
+                Main Content
+            </div>
+        );
+    }
+}
+
+export default MainContent;
