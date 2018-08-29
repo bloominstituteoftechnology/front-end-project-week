4148a0ce0ea71f7c48db3a07bbba007fc595d103
cubsfan46350 <cubsfan.46350@msn.com>
2018-08-29 14:50:47 -0500

    added API to project

--- lambdanotes/src/App.js
+++ lambdanotes/src/App.js
@@ -2,7 +2,29 @@ import React, { Component } from 'react';
 import SideBar from './Components/NavBar/SideBar';
 import MainContent from './Components/MainContent/MainContent';
 import './index.css';
+import axios from 'axios';
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      notesArray: [],
+      newNote: {
+        id: 1,
+        title: '',
+        textBody: ''
+      }
+    }
+  }
+  componentDidMount() {
+    axios
+      .get('https://killer-notes.herokuapp.com')
+        .then(response => {
+          this.setState({ notesArray: response.data })
+        })
+        .catch(err => {
+          console.log('Error', err)
+        });
+  }
   render() {
     return (
       <div className="appContainer">
