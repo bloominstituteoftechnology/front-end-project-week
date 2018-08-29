00d12697a1ca822e966a23d4a9eafe15fbedd340
AI2EJAY <JHammett1314@gmail.com>
2018-08-29 12:10:02 -0500

    Create note styling finished.
    -Working onsave to localStorage functionality

--- notes/src/components/CreateNote.js
+++ notes/src/components/CreateNote.js
@@ -19,46 +19,90 @@ const StyledForm = styled.form`
       font-weight: bold;
       margin-bottom:4px;
   }
-
-  .test {
-    width: 200px;
-    margin-bottom: 20px;
+    .input {
+        padding-left: 10px; 
+        border-radius: 5px;
+        border: 2px solid #D1D0D1;
+        ::placeholder {
+            font-weight: bold;
+            font-size: 14px;
+        }
+        :focus {
+            border: 2px solid darkgrey;
+            outline: none;
+        }
+    }
+  .input-title {
+    width: 280px;
+    margin-bottom: 10px;
+    height: 40px;
+    }
   }
+  .input-content {
+    padding-top: 15px;
+    padding-left: 15px;
+}
 
+`;
 
+const Button = styled.div`
+  background-color: #5eb3bb;
+  height: 40px;
+  text-align: center;
+  margin-left: 2px;
+  margin-top: 10px;
+ 
+  display: flex;
+  color: white;
+  font-weight: bold;
+  width: 160px;
+  justify-content: center;
+  align-items: center;
+  a {
+    text-decoration: none;
+    color: white;
+  }
 `;
 
 class CreateNote extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      comment: 
-      {
+      comment: {
         title: "",
         // tags: "",
         body: ""
       }
-    
     };
   }
-  handleInput = (event) => {
-
-    const {name, value} = event.target 
-    this.setState((prevState) => ({ comment: {...prevState.comment, [name]: value }}));
-
-  }
+  handleInput = event => {
+    const { name, value } = event.target;
+    this.setState(prevState => ({
+      comment: { ...prevState.comment, [name]: value }
+    }));
+  };
 
   render() {
     return (
       <StyledViewWrapper>
-        <h2>Create a note:</h2>
+        <h2>Create New Note:</h2>
 
         <StyledForm>
-          <h3>Title</h3>
-          <input name="title" onChange={this.handleInput} className="test"  />
-          <h3>Text Body</h3>
-          
-          <textarea name="body" rows="15" cols="60" onChange={this.handleInput} class="longInput" />
+          <input
+            name="title"
+            placeholder="Note Title"
+            onChange={this.handleInput}
+            className="input input-title"
+          />
+          <textarea
+            name="body"
+            rows="15"
+            cols="60"
+            placeholder="Note Content"
+            onChange={this.handleInput}
+            className="input input-content"
+          />
+          <Button>Save</Button>
         </StyledForm>
       </StyledViewWrapper>
     );
