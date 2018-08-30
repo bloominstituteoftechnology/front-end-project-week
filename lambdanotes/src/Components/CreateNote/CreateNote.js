2dba857edc7c24bcb9431f3db44e7f8d5afe7172
Sania Parekh <sparekh@Sanias-MacBook.local>
2018-08-30 17:47:21 -0500

    create new note

--- lambdanotes/src/Components/CreateNote/CreateNote.js
+++ lambdanotes/src/Components/CreateNote/CreateNote.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import './index.css';
 
 
 class CreateNote extends React.Component {
     render() {
         return (
-         <div>
-
+         <div className='noteView-Container'>
+           <div className='noteView-topContent'>
+             <h3 className='content-header'>
+                Create New Note:
+             </h3>
+           </div>
+        <div className='createNote-form'>
+          <input type='text' className='createNote-title' placeholder='Note Title'/>
+          <textarea className='createNote-body' placeholder='Note Content' rows='20'/>
+          <div className='createNote-btn' ><button>Save</button></div>
+        </div>
          </div>
         );
     }
