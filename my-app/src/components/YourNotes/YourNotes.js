d93379b3e0056cd1ad6bd9f67e9ad361815b476c
bri79319 <bri79319@gmail.com>
2018-08-30 18:13:36 -0400

    changes to styling

--- my-app/src/components/YourNotes/YourNotes.js
+++ my-app/src/components/YourNotes/YourNotes.js
@@ -2,14 +2,12 @@ import React, { Component } from "react";
 import NoteCard from "./NoteCard";
 import "./YourNotes.css";
 import SideMenu from "../SideMenu/SideMenu";
-import { Link } from "react-router-dom";
 
 class YourNotes extends Component {
   render() {
     return (
       <div>
         <SideMenu />
-        <h2>Your Notes</h2>
         <div className="cards-container">
           {this.props.notes.map(note => {
             return (
