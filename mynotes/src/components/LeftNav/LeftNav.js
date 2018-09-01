d14c0f916961af782d21d830d7c07a9e3f354c1e
bri79319 <bri79319@gmail.com>
2018-09-01 11:54:17 -0400

    styling the left side nav

--- mynotes/src/components/LeftNav/LeftNav.js
+++ mynotes/src/components/LeftNav/LeftNav.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
-import './index.css';
+import React, { Component } from "react";
+import "./index.css";
 
 class LeftNav extends Component {
-    render() {
-        return (
-            <div className='nav_container'>
-                Left
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="nav_container">
+        <div>
+          <h1 className="nav_header">Lambda Notes</h1>
+        </div>
+        <div className='nav_buttons_container'>
+            <div className='nav_button'>View Your Notes</div>
+            <div className='nav_button'>+ Create New Notes</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default LeftNav;
