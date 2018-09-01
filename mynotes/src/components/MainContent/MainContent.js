80330f3c9abd69d4b6df17a36a25b5ed7e9e5f5c
bri79319 <bri79319@gmail.com>
2018-09-01 12:22:27 -0400

    working on notes and styling of notes

--- mynotes/src/components/MainContent/MainContent.js
+++ mynotes/src/components/MainContent/MainContent.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './index.css';
+import NotesList from '../NotesList/NotesList.js';
 
 class MainContent extends Component {
     render() {
         return (
             <div className='main_container'>
-                Main
+                <NotesList />
             </div>
         );
     }
--- mynotes/src/components/NotesList/NotesList.js
+++ mynotes/src/components/NotesList/NotesList.js
@@ -1,14 +1,25 @@
-import React, { Component } from 'react';
-import './index.css';
+import React, { Component } from "react";
+import "./index.css";
 
 class NotesList extends Component {
-    render() {
-        return (
+  render() {
+    return (
+      <div className="notesList_container">
+        <div>
+          <h3 className='content_header'>Your Notes:</h3>
+        </div>
+        <div className='notesList'>
+          <div className="note">
             <div>
-                
+              <h4>Note Title</h4>
+              <hr />
+              <p>stuff</p>
             </div>
-        );
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default NotesList;
