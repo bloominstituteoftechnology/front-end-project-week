ad9ecf01bc08fc6c51fe7cb39a9ffb4398464d2c
Noah-Amar <ne.amar@outlook.com>
2018-09-01 19:05:50 -0500

    Edit Note component complete

--- front-end-project/src/components/MainContent/MainContent.js
+++ front-end-project/src/components/MainContent/MainContent.js
@@ -4,13 +4,14 @@ import '../NotesList/NotesList';
 import NotesList from '../NotesList/NotesList';
 import NoteView from '../NoteView/NoteView';
 import CreateNote from '../CreateNote/CreateNote';
+import EditNote from '../EditNote/EditNote';
 
 class MainContent extends Component {
     state = {  }
     render() { 
         return ( 
             <div className='main_container'>
-            <CreateNote />
+            <EditNote />
             </div>
          );
     }
