dd4e4038ec3d7c8a2f3b83cb387f46250e1ee4a9
Synch3 <jonsimo@aol.com>
2018-09-04 11:42:28 -0400

    Started styling, display now hidden

--- lambdanotes/src/components/Navbar.js
+++ lambdanotes/src/components/Navbar.js
@@ -3,9 +3,25 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 let NavBarContainer = styled.div `
+    background-color: #aaa;
+	background-color: #ccc;
+	border: 1px solid darkgray;
+	height: 100vh;
+	width: 300px;
+	margin-right: 40px;
+	display: flex;
+	flex-direction: column;
+	align-content: flex-start;
+	align-items: center;
 `
 
 let NavButton = styled.button `
+    width: 200px;
+	height: 40px;
+	background-color: #00b6bb;
+	color: white;
+	text-align: center;
+	margin-top: 20px;
 `
 
 export const Navbar = (props) => {
