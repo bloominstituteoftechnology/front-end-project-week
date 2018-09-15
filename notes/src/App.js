545bf596c9ee81002419edd8134e64b3f7d32e07
Alec <afroenygam@gmail.com>
2018-09-15 18:07:22 -0400

    minor change

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
+import {Route} from 'react-router-dom';
 import NotesList from './components/NotesList';
 import CreateNote from './components/CreateNote';
-import {Route} from 'react-router-dom';
 import ViewNote from './components/ViewNote';
 import Register from './components/Register';
 
