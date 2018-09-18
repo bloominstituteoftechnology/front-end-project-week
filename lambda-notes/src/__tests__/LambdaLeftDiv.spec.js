f789b1902b0bead84459e58212e4276fbd760e57
Sukhada Gholba <sukhadagholba@gmail.com>
2018-09-18 14:18:09 -0700

    created tests to check if the header and two divs with specified class names are rendered

--- lambda-notes/src/__tests__/LambdaLeftDiv.spec.js
+++ lambda-notes/src/__tests__/LambdaLeftDiv.spec.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import {Link} from 'react-router-dom';
 
 import LambdaLeftDiv from '../components/ViewNotes/LambdaLeftDiv';
 
@@ -24,4 +25,25 @@ it('should render a div', () => {
         expect(div.length).toBe(1);
 
 });
+
+
+it('should render the Header Component', () => {
+
+        const app = shallow(<LambdaLeftDiv />);
+
+        const header = app.find('.lambda-notes-header');
+        expect(header.text()).toEqual('Lambda Notes');
+
+});
+
+
+it('should render two divs', () => {
+
+        const app = shallow(<LambdaLeftDiv />);
+        
+        const div = app.find('.teal-div');
+        expect(div.length).toBe(2);
+        
+});
+
 });
