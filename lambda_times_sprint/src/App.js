3057e9b2a8899bdd8790060e4755bf71ff608c59
mparedes003 <michelle.g.paredes@gmail.com>
2018-09-24 10:47:23 -0700

    NotesList now displays on screen. Made NavLink and Route for NotesList. Styled .App and .notes-section just so screen view reflects layout on design file  a little better.

--- lambda_times_sprint/src/App.js
+++ lambda_times_sprint/src/App.js
@@ -1,22 +1,49 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { Route, NavLink } from 'react-router-dom';
+import axios from 'axios';
 import NotesList from './components/NotesList';
 
 class App extends Component {
   state = {
-    tags: [],
+    notes: [],
     title: '',
     textBody: ''
   }
+
+  componentDidMount() {
+    axios.get('https://killer-notes.herokuapp.com/note/get/all')
+      .then(response => {
+        this.setState({ notes: response.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="sidebar">
           <h1 className="App-title">Lambda Notes</h1>
+          <ul className="navbar">
+          <li>
+            <NavLink exact to="/" activeClassName="activeNavButton">
+              View Your Notes
+            </NavLink>
+          </li>
+        </ul>
         </header>
-        <div>
-          <NotesList />
+        <div className="notes-section">
+        <h2>Your Notes:</h2>
+        <Route
+          exact
+          path="/"
+          render={props => (
+            <NotesList {...props} notesList={this.state.notes} />
+          )}
+        />
 
         </div>
 
