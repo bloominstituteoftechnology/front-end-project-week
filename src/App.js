c8a7bddeaba41bac234b0ef08ec53fc71762a0e0
Timothy Hoang <timh1203@gmail.com>
2018-09-24 11:47:57 -0400

    Add create react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Lambda Notes App</h1>
+      </div>
+    )
+  }
+}
+
+export default App
