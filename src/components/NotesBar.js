78410d9d5f8c83a522c583f98a02fd7b8c858649
Timothy Hoang <timh1203@gmail.com>
2018-09-24 12:15:20 -0400

    Add basic layout for Notesbar

--- src/components/NotesBar.js
+++ src/components/NotesBar.js
@@ -5,11 +5,14 @@ const NotesBar = () => {
   return (
     <Div1 className="NotesBar">
       <H2>NotesBar Functional Component</H2>
+      <Button>My Notes</Button>
+      <Button>New Notes</Button>
     </Div1>
   )
 }
 
 const Div1 = styled.div``
 const H2 = styled.h2``
+const Button = styled.button``
 
 export default NotesBar
