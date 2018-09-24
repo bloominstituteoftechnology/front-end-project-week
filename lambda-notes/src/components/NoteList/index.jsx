9cd0ec9da0dd3b16e0eabf8d51a428e14a27cc22
decagondev <decagondevelopment@gmail.com>
2018-09-24 21:12:57 +0100

    (MVP NoteList) : add content to render and include Link from react-router-dom

--- lambda-notes/src/components/NoteList/index.jsx
+++ lambda-notes/src/components/NoteList/index.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React from "react";
+
+import { Link } from 'react-router-dom';
 
 const NoteList = props => {
   if (!props.notes) {
-    return (
-      <div>Notes are loading...</div>
-    )
+    return <div>Notes are loading...</div>;
   }
+  // if the notes do exist then we can slice them and reverse them so that newest note is shown first
+  const notes = props.notes.slice().reverse();
+
   return (
-    <div>
-      NoteList component
+    <div className="main-container">
+      <h2>Your Notes:</h2>
+      <div className="note-previews-container">
+        {notes.map(note => {
+          return (
+            <Link to={`/notes/${note._id}`} key={Math.random()}>
+              <div className="note-preview-container">
+                <h3>{note.title}</h3>
+                <p>{note.textBody}</p>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default NoteList;
