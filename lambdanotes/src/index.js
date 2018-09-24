0f00e0ef50fbc38ccb49ff730d5abf045c5fdd9e
Max <maxkajiwara@gmail.com>
2018-09-24 12:29:52 -0700

    Build src/index.js

--- lambdanotes/src/index.js
+++ lambdanotes/src/index.js
@@ -1,8 +1,21 @@
+// Dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { BrowserRouter as Router } from 'react-router-dom';
+// Service Worker
 import registerServiceWorker from './registerServiceWorker';
+// Reducers
+import rootReducer from './reducers';
+// Components
+import App from './components/App';
+// Styles
+import './index.css';
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
 	<Provider store={store}>
