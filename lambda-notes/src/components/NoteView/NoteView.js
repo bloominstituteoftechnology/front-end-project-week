ae86799ce7662bd459f1cf468d5116192ae0654a
alejandrok93 <alejandrok93@gmail.com>
2018-09-24 14:46:14 -0700

    Styling for buttons

--- lambda-notes/src/components/NoteView/NoteView.js
+++ lambda-notes/src/components/NoteView/NoteView.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./NoteView.css";
 
 const NoteView = props => {
   console.log(props);
   console.log(props.note);
   return (
     <div className="note-view">
+      <Link to={`/notes/${props.note.id}/edit`}>Edit</Link>
+      <Link to={`/notes/${props.note.id}/delete`}>Delete</Link>
       <h1>{props.note.title}</h1>
       <p>{props.note.content}</p>
     </div>
