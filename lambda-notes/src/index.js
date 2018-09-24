d9929e9c11b8bf07e4244dfaeee435a44b6a5638
Clint Kunz <clintkunz@gmail.com>
2018-09-24 10:22:00 -0600

    added component structure and wrapped App with Router

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -4,5 +4,12 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Router
+import {BrowserRoute as Router} from 'react-router-dom';
+
+ReactDOM.render(
+<Router>
+    <App />
+</Router>
+, document.getElementById('root'));
 registerServiceWorker();
