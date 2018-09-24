838a1eb322d6da5b9c4829d51274ad7d63eb8d40
Michael Agard <mdagard@gmail.com>
2018-09-24 12:19:46 -0400

    add imports and wrap <App> in Provider

--- lambdanotes/src/index.js
+++ lambdanotes/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import './reset.css';
+import rootReducer from './reducers';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
