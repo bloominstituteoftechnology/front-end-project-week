95f3fcf0f10f446079b6cb86691e4d31ed972ef8
Caleb Kirkwood <calebkirkwood@calebs-mbp.attlocal.net>
2018-09-24 13:15:15 -0400

    Set up nav. (Nav styled, Routes to different views)

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,14 +1,72 @@
 import React, { Component } from 'react';
+import { Route, NavLink, withRouter } from 'react-router-dom';
+import styled from 'styled-components';
+
 import './styles/App.css';
+import NotesView from './views/NotesView';
+import AddNoteView from './views/AddNoteView';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>hello</h1>
-      </div>
+      <Container>
+        <NavBar>
+          <NavTitle>Lambda Notes</NavTitle>
+          <StyledNavLink exact to="/" activeClassName="activeNavButton">
+            View Your Notes
+          </StyledNavLink>
+          <StyledNavLink to="/addnote" activeClassName="activeNavButton">
+            + Create New Note
+          </StyledNavLink>
+        </NavBar>
+        <Content>
+          <Route exact path="/" component={NotesView} />
+          <Route path="/addnote" component={AddNoteView} />
+        </Content>
+      </Container>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  margin: auto;
+`;
+
+const NavBar = styled.nav`
+  background: #d3d2d3;
+  height: 100vh;
+  width: 20%;
+  position: sticky;
+  display: flex;
+  flex-direction: column;
+  padding: 12px;
+  border: 1px solid #c3c2c3;
+`;
+
+const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+  background: #59b5bb;
+  color: white;
+  border: 1px solid #9cb1b3;
+  height: 40px;
+  text-align: center;
+  line-height: 40px;
+  font-size: 1.2rem;
+  margin: 20px 0;
+`;
+
+const NavTitle = styled.h1`
+  font-size: 2.2rem;
+  margin-bottom: 10px;
+`;
+
+const Content = styled.div`
+  border: 1px solid #c3c2c3;
+  width: 80%;
+  height: 100vh;
+  background: #f2f1f2;
+`;
---
+++ notes/src/views/NotesView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotesView extends Component {
+  render() {
+    return (
+      <div>
+        <h1> NotesView</h1>
+      </div>
+    );
+  }
+}
+
+export default NotesView;
