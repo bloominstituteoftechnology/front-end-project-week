adedca6caae997662c8d8bf74f0546c9ac488945
Max <maxkajiwara@gmail.com>
2018-09-24 12:12:21 -0700

    Add redux-thunk and redux-logger, set up file structure

--- lambdanotes/src/index.js
+++ lambdanotes/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>
+		<Router>
+			<App />
+		</Router>
+	</Provider>,
+	document.getElementById('root')
+);
 registerServiceWorker();
