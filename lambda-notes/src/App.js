65017637a20d3043e12e73cb276a5b9f213158d3
Arkadiusz Kusiak <akusiak23@gmail.com>
2018-09-24 17:09:17 -0500

    GET

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as actions from './actions';
+import Notes from  './components/notes/Notes'
+import { connect } from 'react-redux';
+import { withRouter, Route } from 'react-router-dom';
 
 class App extends Component {
+  componentDidMount = () => {
+    this.props.fetchNotes();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Test</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <h1 className="App-title">Lambda Notes</h1>
+          <Route 
+            path="/"
+            render={props => (
+              <Notes 
+                {...props} 
+                notes={this.props.notes}
+              />
+            )}
+          />
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    notes: state.notes,
+    fetchingNotes: state.fetchingNotes,
+    fetchedNotes: state.fetchedNotes,
+    updatingNote: state.updatingNote,
+    updatedNote: state.updatedNote,
+    error: state.error
+  }
+}
+
+export default withRouter(connect(mapStateToProps, actions)(App));
--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
