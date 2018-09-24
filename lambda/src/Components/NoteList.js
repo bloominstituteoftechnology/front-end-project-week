af622d8c7d106dec0d7e967118fbab0d29108c0a
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-09-24 12:18:31 -0500

    NoteList created

--- lambda/src/Components/NoteList.js
+++ lambda/src/Components/NoteList.js
@@ -1,7 +1,19 @@
-import React, { Component } from 'react';
-class NoteList extends Component {
-  render() {
-    return <div />;
-  }
-}
+import React from 'react';
+
+const NoteList = props => {
+  return (
+    <div className="note-container">
+      <h2>Your Notes:</h2>
+      <div className="all-notes">
+        {props.notes.map(note => (
+          <div className="note">
+            <p className="title">{note.title}</p>
+            <p>{note.text}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
 export default NoteList;
