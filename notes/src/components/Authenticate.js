fa7db15a6e79be02ada89488a70834a7d88274b9
Grant Reighard <grant.reighard@gmail.com>
2018-09-24 17:41:31 -0400

    Got Google Login working.

--- notes/src/components/Authenticate.js
+++ notes/src/components/Authenticate.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { GoogleLogin } from 'react-google-login';
 
 const Authenticate = App =>
     class extends React.Component {
@@ -9,6 +10,12 @@ const Authenticate = App =>
             }
         }
 
+        responseGoogle = (response) => {
+            this.props.handleLogin(response.googleId)
+            console.log(response);
+            window.location.reload();
+        }
+
         handleChange = (event) => {
             this.setState({username: event.target.value})
         }
@@ -16,8 +23,15 @@ const Authenticate = App =>
         render() {
             return (
                 <div>
+                    <h1>Please log in</h1>
+                    <GoogleLogin
+                    clientId="82812314384-pd0mig537qa6b5o6pdn79nb31i7k7k23.apps.googleusercontent.com"
+                    buttonText="Log in with Google"
+                    onSuccess={this.responseGoogle}
+                    onFailure={this.responseGoogle}
+                    />
                     <form className="loginForm" onSubmit={() => {this.props.handleLogin(this.state.username)}}>
-                        <h1>Please log in</h1>
+                        
                         <input name="username" type="text" placeholder="username" value={this.state.username} onChange={this.handleChange}/>
                         <input name="password" type="password" placeholder="password" />
                         <button type="submit">Log in</button>
--- notes/src/components/Sidebar.js
+++ notes/src/components/Sidebar.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { GoogleLogout } from 'react-google-login';
+
+const logout = () => {
+    localStorage.removeItem('username');
+    console.log('logout');
+    window.location.reload();
+}
 
 class Sidebar extends Component {
     constructor(props){
@@ -16,6 +23,10 @@ class Sidebar extends Component {
                 <h1>Lambda Notes</h1>
                 <div className="button"><Link to="/" onClick={() => this.forceUpdate()}>View Your Notes</Link></div>
                 <div className="button"><Link to="/create">+ Create New Note</Link></div>
+                <GoogleLogout
+                buttonText="Log out"
+                onLogoutSuccess={logout}>
+                </GoogleLogout>
             </div>
         )
     }
