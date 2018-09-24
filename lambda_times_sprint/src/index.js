30360c4f3281ed79e274b419fa3bc4bc3dd04be6
mparedes003 <michelle.g.paredes@gmail.com>
2018-09-24 10:01:08 -0700

    Wrapped App in Router in index.js

--- lambda_times_sprint/src/index.js
+++ lambda_times_sprint/src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { BrowserRouter as Router } from 'react-router-dom';
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>, document.getElementById('root'));
+
