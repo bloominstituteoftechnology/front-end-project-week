d95e20043ea3f8e213382b411874733485d9929b
Andrew <ghobrial.andrew@gmail.com>
2018-09-24 16:27:24 -0400

    note GET bug fixed, now working on adding new note

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -1,8 +1,22 @@
+// import './styles/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { notesReducer } from "./reducers";
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(notesReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
