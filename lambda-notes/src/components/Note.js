195e0344da30f5189c5c39b4cc87f36e6a2c4092
unknown <cornielleandres@gmail.com>
2018-09-23 22:47:06 -0400

    Style delete modal.

--- lambda-notes/src/components/Note.js
+++ lambda-notes/src/components/Note.js
@@ -7,7 +7,7 @@ const Note = props => {
 		<div className = 'note'>
 			<h3><Link to = { `/note/${ props.index }` }>{ props.note.noteTitle.length > 11 ? props.note.noteTitle.slice(0, 11) + '...' : props.note.noteTitle }</Link></h3>
 			<hr />
-			<p>{ props.note.noteContent }</p>
+			<p>{ props.note.noteContent.length > 85 ? props.note.noteContent.slice(0, 85) + '...' : props.note.noteContent }</p>
 		</div>
 	);
 }
--- lambda-notes/src/views/NoteView/index.js
+++ lambda-notes/src/views/NoteView/index.js
@@ -2,21 +2,52 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NoteView = props => {
-	return(
-		<div className = 'main-content'>
-			<div className = 'note-view'>
-				<div className = 'edit-delete-wrapper'>
-					<span><Link to = { `/edit/${ props.id }` }>edit</Link></span>
-					<span>delete</span>
+// Styles
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Modal, ModalBody } from 'reactstrap';
+
+class NoteView extends React.Component {
+	state = {
+		deleteModal: false,
+	};
+
+	toggleDeleteModal = () => {
+		this.setState({ deleteModal: !this.state.deleteModal });
+	}
+
+	render() {
+		return(
+			<div className = 'main-content'>
+				<div className = 'note-view'>
+					<div className = 'edit-delete-wrapper'>
+						<span><Link to = { `/edit/${ this.props.id }` }>edit</Link></span>
+						<span onClick = { this.toggleDeleteModal }>delete</span>
+						
+						<div>
+							<Modal 
+								centered={ true } 
+								isOpen={ this.state.deleteModal } 
+								toggle={ this.toggleDeleteModal } 
+							>
+								<ModalBody>
+									<p>Are you sure you want to delete this?</p>
+
+									<div className = 'modal-btns'>
+										<div className = 'btn save-btn delete-btn'>Delete</div>
+										<div className = 'btn save-btn no-btn'>No</div>
+									</div>
+								</ModalBody>
+							</Modal>
+						</div>
+					</div>
+
+					<h2>{ this.props.note.noteTitle }</h2>
+
+					{ this.props.note.noteContent.split('\n').map((str, i) => <p key = { i }>{ str }</p>) }
 				</div>
-
-				<h2>{ props.note.noteTitle }</h2>
-
-				{ props.note.noteContent.split('\n').map((str, i) => <p key = { i }>{ str }</p>) }
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default NoteView;
