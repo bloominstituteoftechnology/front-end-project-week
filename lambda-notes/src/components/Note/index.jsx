a78fa753b3367eb1353cb3559a7dd97b209130d3
decagondev <decagondevelopment@gmail.com>
2018-09-24 22:00:03 +0100

    (MVP : Note) : add state

--- lambda-notes/src/components/Note/index.jsx
+++ lambda-notes/src/components/Note/index.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 
 export default class Note extends Component {
+  state = {
+    note: null,
+    title: "",
+    textBody: ""
+  }
   render() {
     return (
       <div>
