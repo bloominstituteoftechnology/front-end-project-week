d2b356b1edd557c99e02f532cb717ea3f52455ef
decagondev <decagondevelopment@gmail.com>
2018-09-24 17:50:13 +0100

    (MVP - SideBar) : add SideBar component to App container for testing UI render

--- lambda-notes/src/components/App/index.jsx
+++ lambda-notes/src/components/App/index.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './index.css';
 
+import SideBar from '../SideBar';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Lambda Notes
+        <SideBar />
       </div>
     );
   }
