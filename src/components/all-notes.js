ce28cb98c27475f462e262f76db8ff4aa09c8e10
mkerbleski <mkerbles@gmail.com>
2018-09-24 12:18:18 -0600

    added back all code from backup

---
+++ src/components/all-notes.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import NotePreview from './note-preview.js';
+
+const AllNotesDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 15px;
+  background-color: #F3F3F3;
+  .sort {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+    margin: 5px 0 0 15px;
+    button {
+      height: 25px;
+      margin: 0 10px;
+    }
+  }
+  .all-notes {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-around;
+
+    .note-link{
+      text-decoration: none;
+    }
+
+  }
+`;
+
+export default class AllNotes extends Component {
+  constructor(props){
+    super(props);
+    console.log(this.props)
+    this.state = {
+      stateNotes: false,
+      notes: [],
+    }
+  }
+
+
+
+  render(props) {
+    // console.log(this.props)
+    // console.log(this.state.notes)
+
+
+    return (
+      <AllNotesDiv>
+        <h3>Your Notes:</h3>
+        <div className="sort">
+          <h4>Sort by:</h4>
+            <button onClick={this.props.sortByLetter}>A->Z</button>
+            <button onClick={this.props.sortById}>Id</button>
+        </div>
+
+        <div className="all-notes">
+          {this.props.notes.map((note, index) => {
+            // let url = decodeURI(note.title)
+            // would be cool to use title instead
+            //how to do that?
+            //------------------------------??----------------------
+            return (
+              <NotePreview
+                onDrop={this.props.onDrop}
+                key={index}
+                index={index}
+                note={note}
+              />)
+          })}
+        </div>
+      </AllNotesDiv>
+    );
+  }
+}
---
+++ src/components/edit-form.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+
+const EditFormDiv = styled.div`
+  form {
+    ${'' /* border: 1px solid blue; */}
+    background-color: #F3F3F3;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .input {
+      margin: 10px;
+      padding: 10px;
+      color: gray;
+    }
+    #title{
+      width: 30%;
+    }
+    #body{
+      height: 100px;
+      text-emphasis: wrap;
+      max-width: 90%;
+      font-family: 'Roberto';
+      line-height: 20px;
+    }
+    .menu-item{
+        ${'' /* border: 1px solid red; */}
+        width: 30%;
+        padding: 15px;
+        margin: 5px;
+        text-align: center;
+        text-decoration: none;
+        color: white;
+        background-color: #2AC0C4;
+        font-weight: bold;
+        border: 1px solid lightgray;
+      }
+  }
+`;
+
+export default class EditForm extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      tags: this.props.note.tags,
+      title: this.props.note.title,
+      textBody: this.props.note.textBody,
+      _id: this.props.note._id,
+      newTag: '',
+    }
+  }
+
+  sendToApp = (e) => {
+    this.props.editNote(this.state)
+  }
+
+  inputHandler = (e) => {
+    e.preventDefault();
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  addTag = (e) => {
+    e.preventDefault();
+    let thing = this.state.tags.slice();
+    thing.push(this.state.newTag)
+    this.setState({
+      newTag: '',
+      tags: thing,
+    })
+  }
+
+  render() {
+    return (
+      <EditFormDiv>
+        <form onSubmit={this.sendToApp}>
+
+          <input
+            className="input"
+            id="title"
+            onChange={this.inputHandler}
+            name='title'
+            value={this.state.title}
+            placeholder="Note Title"
+          >{this.value}</input>
+
+          <textarea
+            className="input"
+            id="body"
+            name='textBody'
+            onChange={this.inputHandler}
+            value={this.state.textBody}
+            placeholder="Note Content"
+          >{this.value}</textarea>
+
+
+          <input
+            onSubmit={this.addTag}
+            className="input"
+            onChange={this.inputHandler}
+            value={this.state.newTag}
+            id="tag"
+            name='newTag'
+            placeholder={`${this.state.tags}`}
+          >{this.value}</input>
+          <button onClick={this.addTag}>add tag</button>
+
+
+          <Link
+            className="menu-item"
+            onClick={this.sendToApp}
+            to={`/all-notes/${this.state._id}`}
+          >Save</Link>
+
+        </form>
+      </EditFormDiv>
+    );
+  }
+}
