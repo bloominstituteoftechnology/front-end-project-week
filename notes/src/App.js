aa7f2f3c04e8a1de5cc1c7de213b10f31297f97d
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-09-24 17:46:54 -0400

    set up react and react router envrionments, router added to App.js

---
+++ notes/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Route } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Notes App - Placeholder</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
