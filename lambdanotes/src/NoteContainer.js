2a2b52afdf5e1c0b7153990cee4755c51e417ebe
PatrickTheCodeGuy <patrick.jthompson93@gmail.com>
2018-09-24 20:01:49 -0700

    delete implemented

--- lambdanotes/src/NoteContainer.js
+++ lambdanotes/src/NoteContainer.js
@@ -8,18 +8,23 @@ import OneNote from "./components/NotesContainer/OneNote";
 function NoteContainer(props) {
 	const noteID = parseInt(props.match.params.id, 10);
 
-	console.log(noteID);
-	console.log(props.notes);
-	const thisNote = props.notes.find(note => note.id === noteID);
-	console.log(thisNote);
+	let thisNote = props.notes.find(note => note.id === noteID);
 
+	function handleDelete() {
+		console.log(props);
+		let newNotes = props.notes.filter(note => note.id != thisNote.id);
+		props.getNotes(newNotes);
+		props.history.push("/");
+	}
 	return (
 		<div className="App">
 			<SidebarContainer />
 			<div className="newNote">
 				<div className="edit-delete">
 					<button className="button">edit</button>
-					<button className="button">delete</button>
+					<button onClick={props => handleDelete(props)} className="button">
+						delete
+					</button>
 				</div>
 				<OneNote note={thisNote} title={thisNote.title} body={thisNote.body} />
 			</div>
