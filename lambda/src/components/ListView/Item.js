67985da90d2c3052430320218b63d7aad8c99655
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-09-25 10:44:34 -0500

    Added Basic Dragging Functionality

--- lambda/src/components/ListView/Item.js
+++ lambda/src/components/ListView/Item.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { NoteItem, H1, NoteTitle, P, H3, H4 } from '../StyledComponents';
-
-function Item(props) {
-  return (
-    <NoteItem onClick={props.onClick}>
-      <NoteTitle>
-        <H4>{props.title}</H4>
-      </NoteTitle>
-      <P>{props.textBody}</P>
-    </NoteItem>
-  );
+// import { Draggable } from 'react-draggable'
+import ReactDOM from 'react-dom';
+import Draggable from 'react-draggable';
+class Item extends React.Component {
+  render() {
+    const dragHandlers = { onStart: this.onStart, onStop: this.onStop };
+    return (
+      <Draggable {...dragHandlers}>
+        <NoteItem onClick={this.props.onClick}>
+          <NoteTitle>
+            <H4>{this.props.title}</H4>
+          </NoteTitle>
+          <P>{this.props.textBody}</P>
+        </NoteItem>
+      </Draggable>
+    );
+  }
 }
 
 export default Item;
