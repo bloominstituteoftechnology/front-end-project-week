6be9b928bdfc41fb929b8cd24dfa523410936940
Max <maxkajiwara@gmail.com>
2018-09-24 20:00:46 -0700

    Fix Link for real this time

--- lambdanotes/src/components/Note.js
+++ lambdanotes/src/components/Note.js
@@ -7,7 +7,7 @@ import '../styles/Note.css';
 const Note = props => {
 	return (
 		<li className="NoteListPage">
-			<Link to={`/${props.id}`} className="note-link">
+			<Link to={`/${props.note._id}`} className="note-link">
 				<h4 className="note-title">{props.note.title}</h4>
 				{/* todo: clamp preview text */}
 				<p className="note-preview">{props.note.textbody}</p>
