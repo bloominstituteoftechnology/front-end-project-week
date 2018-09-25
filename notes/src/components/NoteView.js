a067d4957e4c4fcb46215a4fc3a929605d558c07
Carey <careycbaldwin@gmail.com>
2018-09-25 16:24:00 -0400

    noteview component is working now
    had to delete filteredNotes method from when I was using local data

--- notes/src/components/NoteView.js
+++ notes/src/components/NoteView.js
@@ -9,15 +9,14 @@ class NoteView extends Component {
         super(props);
         console.log('logging props in Noteview', this.props)
         this.state = {
-            notes: [],
-            note: {}
+            note: {},
         }
 }
     
     componentDidMount() {
         const id = this.props.match.params.id;
         console.log('id match',this.props.match.params)
-        this.setState({ id: Number(id), notes: this.state.notes})
+        //this.setState({ id: Number(id), notes: this.props.notes})
     
         axios.get(`http://localhost:9000/api/notes/${id}`)
         .then(response => {
@@ -30,34 +29,25 @@ class NoteView extends Component {
         })
     }
 
-    filteredNotes = note => {
-        if(note.id === this.state.note.id) {
-            console.log('note in filtered notes',this.state.note)
-            return(
-                <div className = "note-view"
-                key = {note.id}>
-                <Link to={`/edit/${note.id}`}>
-                <button>
-                Edit
-                </button>
-                </Link>
-                
-                <Link to = {`/delete/${note.id}`}>
-                <button onClick={this.props.toggleDeleteNote}>
-                Delete
-                </button>
-                </Link>
-
-                <h1>{this.state.note.name}</h1>
-                <p>{this.state.note.content}</p>
-                </div>
-            )
-        }
-    }
-
     render() {
+        const {note} = this.state;
         return(
-            <div>{this.filteredNotes}
+            <div className = "note-view"
+            key = {note.id}>
+            <Link to={`/edit/${note.id}`}>
+            <button>
+            Edit
+            </button>
+            </Link>
+            
+            <Link to = {`/delete/${note.id}`}>
+            <button onClick={this.props.toggleDeleteNote}>
+            Delete
+            </button>
+            </Link>
+
+            <h1>{note.name}</h1>
+            <p>{note.content}</p>
             </div>
         )
     }
