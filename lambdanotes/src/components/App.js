ba00479e66a14deddccffa6171c1a563331dac4e
Michael Agard <mdagard@gmail.com>
2018-09-25 14:12:38 -0400

    add route path for single note

--- lambdanotes/src/components/App.js
+++ lambdanotes/src/components/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
+
+import { Route } from 'react-router-dom';
+
+import styled from 'styled-components';
+
 import SideBarNav from './Nav/SideBarNav';
 import NotesPage from './notes/NotesPage';
+import NoteView from './notes/NoteView';
+
+const AppWrapper = styled.div`
+  display: flex;
+`;
 
 const App = () => {
   return (
-    <div className="App">
-      {/* <SideBarNav /> */}
-      <NotesPage />
-      App Page
-    </div>
+    <AppWrapper className="App">
+      <SideBarNav />
+      <Route path="/note/:id" component={NoteView} />
+      <Route exact path="/" component={NotesPage} />
+    </AppWrapper>
   );
 };
 export default App;
