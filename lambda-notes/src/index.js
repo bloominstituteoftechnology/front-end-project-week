562a86cb84814f5941507077908a7ccaf15df9d5
BrianARuff <thelonewolf198927@gmail.com>
2018-09-25 16:30:47 -0400

    organized main index.js imports

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+
+import rootReducer from './reducers';
+
+// Auxillary Packages
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './reducers';
+import { BrowserRouter as Router } from 'react-router-dom';
 
+// Components
+import App from './App';
+
+// CSS
+import './index.css';
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk, logger)));
 
