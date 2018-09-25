e7e4ead292c41f8084c0b252b0c618e158c3c39a
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-09-25 16:42:02 -0400

    Created Add/edit note form component

--- notes/src/components/CreateEditNote.js
+++ notes/src/components/CreateEditNote.js
@@ -4,7 +4,7 @@ export default function CreateEditNote(props) {
   function handleSubmit(event) {
     event.preventDefault();
 
-    if (props.Updating) {
+    if (props.updatingNote) {
       props.handleUpdateNote();
     } else {
       props.handleAddNewNote(event);
@@ -13,77 +13,27 @@ export default function CreateEditNote(props) {
 
   return (
     <div>
-      <h2>{props.isUpdating ? "Update Avenger" : "Add New Avenger"}</h2>
+      <h2>{props.updatingNote ? "Update Note" : "Add A New Note"}</h2>
       <form>
-        <div className="group">
+        <div>
           <input
             type="text"
-            value={props.avenger.name}
+            placeholder="Note Title"
+            value={props.note.title}
             name="name"
             onChange={props.handleChange}
           />
-          <span className="highlight" />
-          <span className="bar" />
-          <label>Name</label>
         </div>
-        <div className="group">
+        <div>
           <input
             type="text"
-            value={props.avenger.nickname}
-            name="nickname"
+            placeholder="Note Text"
+            value={props.note.textBody}
+            name="textBody"
             onChange={props.handleChange}
           />
-          <span className="highlight" />
-          <span className="bar" />
-          <label>Nickname</label>
         </div>
-        <div className="group">
-          <input
-            type="text"
-            value={props.avenger.description}
-            name="description"
-            onChange={props.handleChange}
-          />
-          <span className="highlight" />
-          <span className="bar" />
-          <label>Description</label>
-        </div>
-        <div className="group">
-          <input
-            type="text"
-            value={props.avenger.thumbnail}
-            name="thumbnail"
-            onChange={props.handleChange}
-          />
-          <span className="highlight" />
-          <span className="bar" />
-          <label>Thumbnail</label>
-        </div>
-        <div className="group">
-          <input
-            type="text"
-            value={props.avenger.img}
-            name="img"
-            onChange={props.handleChange}
-          />
-          <span className="highlight" />
-          <span className="bar" />
-          <label>Img</label>
-        </div>
-        <div className="group">
-          <input
-            type="text"
-            value={props.avenger.movies.join(", ")}
-            name="movies"
-            onChange={props.handleChange}
-          />
-          <span className="highlight" />
-          <span className="bar" />
-          <label>Movies (comma delimited list)</label>
-        </div>
-        <button className="material-button-raised" onClick={handleSubmit}>
-          Submit
-        </button>
+        <button onClick={handleSubmit}>Submit</button>
       </form>
     </div>
   );
