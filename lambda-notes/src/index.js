47644f058eac46d58489c8e205136a1540769a2b
Clint Kunz <clintkunz@gmail.com>
2018-09-25 14:43:33 -0600

    installed redux and set up index.js

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -3,13 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import  { noteReducer } from './reducers/index';
 
 //Router
 import { BrowserRouter as Router } from 'react-router-dom';
 
+const store = createStore(
+    noteReducer, // this is the most basic reducer. A function that returns and object. Replace it.
+    applyMiddleware(thunk, logger)
+  );
+
 ReactDOM.render(
-<Router>
-    <App />
-</Router>
+<Provider store={store}>
+    <Router>
+        <App />
+    </Router>
+</Provider>
 , document.getElementById('root'));
 registerServiceWorker();
