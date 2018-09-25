b10adc25e2c4c511b2f0a3503ba870ab03b20dd0
decagondev <decagondevelopment@gmail.com>
2018-09-25 16:14:28 +0100

    (STRETCH : REDUX) : create reducers skeleton

---
+++ lambda-notes/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+// getNotes action creator TODO: fill in logic
+export const getNotes = () => {}
+
+// addNotes action creator TODO: fill in logic
+export const addNote = () => {}
+
+// getNote action creator TODO: fill in logic
+export const getNote = () => {}
+
+// editNote action creator TODO: fill in logic
+export const editNote = editedNote => {}
+
+// deleteNote action creator TODO: fill in logic
+export const deleteNote = id => {}
