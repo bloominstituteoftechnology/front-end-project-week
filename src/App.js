8976dd98e2577e49a3e854b10054873cccb1edd7
Charles Cortinas <crcortinas@gmail.com>
2018-09-25 18:32:38 -0500

    Hosted Backend
    Confugured app to work with back end project hosted seperately

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Route } from 'react-router-dom';
-import axios from 'axios';
+import React, { Component } from "react";
+import "./App.css";
+import { Route } from "react-router-dom";
+import axios from "axios";
 
-import Menu from './Menu';
-import Notes from './Notes';
-import AddNote from './AddNote';
-import Note from './Note';
-import EditNote from './EditNote';
+import Menu from "./Menu";
+import Notes from "./Notes";
+import AddNote from "./AddNote";
+import Note from "./Note";
+import EditNote from "./EditNote";
 
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.url = "https://charles-note.herokuapp.com/api/notes/";
+
     this.state = {
       notes: [],
-      title: '',
-      textBody: '',
-    }
+      title: "",
+      textBody: ""
+    };
   }
 
   componentDidMount() {
@@ -24,92 +27,128 @@ class App extends Component {
   }
 
   getNotes = () => {
-    axios.get('https://killer-notes.herokuapp.com/note/get/all')
-      .then((res) => {
-        console.log(res.data);
+    axios
+      .get(this.url)
+      .then(res => {
         this.setState({
           notes: res.data
-        })
+        });
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
-      })
-  }
+      });
+  };
 
-  changeHandler = (event) => {
+  changeHandler = event => {
     this.setState({
       [event.target.name]: event.target.value
     });
     console.log(this.state.textBody);
-  }
+  };
 
-  submitHandler = (event) => {
+  submitHandler = event => {
     event.preventDefault();
     const newNote = {
       title: this.state.title,
-      textBody: this.state.text,
+      contents: this.state.text
     };
-    axios.post('https://killer-notes.herokuapp.com/note/create',newNote)
-      .then((res) => {
+    axios
+      .post(this.url, newNote)
+      .then(res => {
         console.log(res);
       })
-      .catch((err) => {
-        console.log(err);
+      .then(() => {
+        this.setState({
+          title: "",
+          textBody: ""
+        });
+        this.getNotes();
       })
-    this.setState({
-      title: '',
-      textBody: '',
-    });
-    this.getNotes();
-  }
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
-  editHandler = (event) => {
+  editHandler = event => {
     event.preventDefault();
     const id = event.target.id;
-    let title = '';
-    let text = '';
-    
+
     const editedNote = {
       title: this.state.title,
-      textBody: this.state.textBody,
+      contents: this.state.textBody
     };
-    const url = "http://killer-notes.herokuapp.com/note/edit/" + id;
-    axios.put(url, editedNote)
-      .then((res) => {
+    const url = this.url + id;
+    axios
+      .put(url, editedNote)
+      .then(res => {
         console.log(res);
         this.getNotes();
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
-      })
+      });
     this.setState({
-      title: '',
-      textBody: ''
+      title: "",
+      textBody: ""
     });
-  }
+  };
 
   deleteHandler = (e, id) => {
     e.preventDefault();
-    const url = "http://killer-notes.herokuapp.com/note/delete/" + id;
-    axios.delete(url)
-      .then((res) => {
+    const url = this.url + id;
+    axios
+      .delete(url)
+      .then(res => {
         console.log(res);
         this.getNotes();
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
-      })
-  }
+      });
+  };
 
   render() {
     return (
       <div className="App">
-        <div className='body'>
-          <Route path='/' component={ Menu } />
-          <Route exact path='/' render={props => <Notes {...props} notes={this.state.notes} />} />
-          <Route path='/add' render={props => <AddNote {...props} state={this.state} changeHandler={this.changeHandler} submitHandler={this.submitHandler} />} />
-          <Route path='/view/:id' render={props => <Note {...props} notes={this.state.notes} deleteHandler={this.deleteHandler} />} />
-          <Route path='/edit/:id' render={props => <EditNote {...props} state={this.state} changeHandler={this.changeHandler} editHandler={this.editHandler} />} />
+        <div className="body">
+          <Route path="/" component={Menu} />
+          <Route
+            exact
+            path="/"
+            render={props => <Notes {...props} notes={this.state.notes} />}
+          />
+          <Route
+            path="/add"
+            render={props => (
+              <AddNote
+                {...props}
+                state={this.state}
+                changeHandler={this.changeHandler}
+                submitHandler={this.submitHandler}
+              />
+            )}
+          />
+          <Route
+            path="/view/:id"
+            render={props => (
+              <Note
+                {...props}
+                notes={this.state.notes}
+                deleteHandler={this.deleteHandler}
+              />
+            )}
+          />
+          <Route
+            path="/edit/:id"
+            render={props => (
+              <EditNote
+                {...props}
+                state={this.state}
+                changeHandler={this.changeHandler}
+                editHandler={this.editHandler}
+              />
+            )}
+          />
         </div>
       </div>
     );
--- src/Notes.js
+++ src/Notes.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { CSVLink } from 'react-csv';
+import React from "react";
+import { Link } from "react-router-dom";
+import { CSVLink } from "react-csv";
 
 const Notes = props => {
   const headers = [
-    {label: 'ID', key: 'id'},
-    {label: 'Title', key: 'title'},
-    {label: 'Content', key: 'text'}
-  ]
+    { label: "ID", key: "id" },
+    { label: "Title", key: "title" },
+    { label: "Content", key: "text" }
+  ];
+
+  console.log("Notes.js notes ", props.notes);
   return (
-    <div className='notes-container'>
+    <div className="notes-container">
       <h4>Your Notes:</h4>
-      <div className='notes'>
+      <div className="notes">
         {props.notes.map(note => {
-          let preview = note.textBody;
-          if (note.textBody.length > 100) {
-            preview = note.textBody.slice(0,85) + '...';
+          let preview = note.contents;
+          if (note.contents.length > 100) {
+            preview = note.contents.slice(0, 85) + "...";
           }
           return (
-            <div className='note' key={note._id}>
-              <Link to={`/view/${note._id}`}>
+            <div className="note" key={note.id}>
+              <Link to={`/view/${note.id}`}>
                 <h5>{note.title}</h5>
                 <p>{preview}</p>
               </Link>
@@ -27,9 +29,16 @@ const Notes = props => {
           );
         })}
       </div>
-      <CSVLink className='download-link' data={props.notes} filename={'lambda-notes.csv'} headers={headers}>Download Notes in CSV Format</CSVLink>
+      <CSVLink
+        className="download-link"
+        data={props.notes}
+        filename={"lambda-notes.csv"}
+        headers={headers}
+      >
+        Download Notes in CSV Format
+      </CSVLink>
     </div>
   );
-}
+};
 
 export default Notes;
