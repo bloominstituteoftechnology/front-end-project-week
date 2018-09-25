1ba7233c13b89cb03eeb9f4fb231575faca3c71e
Trevor <trevorfehrman@gmail.com>
2018-09-25 11:02:06 -0700

    Removed Drag and Drop, feature wasn't useful

--- notes/src/Components/Notes.js
+++ notes/src/Components/Notes.js
@@ -7,48 +7,10 @@ import Tags from "./Tags";
 class Notes extends React.Component {
 	state = {
 		searchTerm: "",
-		notes: this.props.notes,
-	};
-
-	onDragStart = (event, id) => {
-		event.dataTransfer.setData("id", id);
-	};
-
-	onDragOver = event => {
-		event.preventDefault();
-	};
-
-	onDrop = (ev, cat) => {
-		let id = ev.dataTransfer.getData("id");
-		let notes = this.props.notes.filter(note => {
-			if (note.id == id) {
-				note.category = cat;
-			}
-			return note;
-		});
-		this.setState({
-			...this.state,
-			notes,
-		});
 	};
 
 	handleSearchInput = searchTerm => this.setState({ searchTerm });
 	render() {
-		let notes = {
-			wip: [],
-			complete: [],
-		};
-
-		this.props.notes.forEach(note => {
-			if (note.category) {
-				notes[note.category].push(
-					<NoteCard onDragStart={this.onDragStart} note={note} />,
-				);
-			} else {
-				notes.wip.push(note);
-			}
-		});
-
 		return (
 			<div className="NotesWrapper">
 				<h1 className="Notes__header">Your Notes:</h1>
@@ -59,28 +21,14 @@ class Notes extends React.Component {
 					onChange={e => this.handleSearchInput(e.target.value)}
 				/>
 				<Tags notes={this.props.notes} />
-				<div
-					onDragOver={e => this.onDragOver(e)}
-					className="Notes__wip"
-				>
+				<div className="Notes__wip">
 					{this.props.notes
 						.filter(note =>
 							fuzzysearch(this.state.searchTerm, note.title),
 						)
 						.map(note => (
-							<NoteCard
-								onDragStart={this.onDragStart}
-								note={note}
-							/>
+							<NoteCard note={note} />
 						))}
-				</div>
-				<div
-					className="NotesWrapper"
-					onDragOver={e => this.onDragOver(e)}
-					onDrop={e => this.onDrop(e, "complete")}
-				>
-					<h1 className="Notes__header">Completed:</h1>
-					{notes.complete}
 				</div>
 			</div>
 		);
