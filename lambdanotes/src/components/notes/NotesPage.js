4cb043a90b6ff3e678e0d17da41fc3a498bcaa43
Michael Agard <mdagard@gmail.com>
2018-09-25 14:13:55 -0400

    add router for individual notes

--- lambdanotes/src/components/notes/NotesPage.js
+++ lambdanotes/src/components/notes/NotesPage.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
+import NoteList from './NoteList';
 import { fetchNotes } from '../../actions';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
+
+const NotesPagerapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  border: 1px solid #c8c7c8;
+  font-size: 20px;
+  background: #f2f1f2;
+  justify-content: space-around;
+`;
+
 class NotesPage extends Component {
   componentDidMount() {
     this.props.fetchNotes();
   }
   render() {
-    return <div className="NotesPage">NotesPage</div>;
+    // console.log(this.props.notes);
+    return (
+      <NotesPagerapper>
+        {this.props.notes.map(note => {
+          return (
+            <NoteList
+              tags={note.tags}
+              title={note.title}
+              textBody={note.textBody}
+              key={note._id}
+              id={note._id}
+            />
+          );
+        })}
+      </NotesPagerapper>
+    );
   }
 }
 
