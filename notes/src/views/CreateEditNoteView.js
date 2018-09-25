b0e36ca3eb9bce6483c59eefb48b42a55f7222a0
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-09-25 17:00:13 -0400

    Created stateful view for add/edit note form

--- notes/src/views/CreateEditNoteView.js
+++ notes/src/views/CreateEditNoteView.js
@@ -1,6 +1,67 @@
-import React from 'react';
-import CreateNote from '../components/CreateNote';
+import React from "react";
+import CreateEditNote from "../components/CreateEditNote";
 
-export default function () {
-    return <CreateNote />
+import React from "react";
+import { connect } from "react-redux";
+
+import { addNewNote, updateNote } from "../store/actions";
+
+class CreateEditNoteView extends React.Component {
+  state = {
+    note: {
+      title: "",
+      textBody: ""
+    },
+    updatingNote: false
+  };
+
+  componentDidMount() {
+    console.log(this.props);
+    if (this.props.noteToUpdate) {
+      this.setState({ updatingNote: true, note: this.props.noteToUpdate });
+    }
+  }
+
+  handleChange = event => {
+    this.setState({
+      note: {
+        ...this.state.note,
+        [event.target.name]: event.target.value
+      }
+    });
+  };
+
+  handleAddNote = event => {
+    event.preventDefault();
+    console.log("handle add new note");
+    this.props.addNewNote(this.state.note);
+    this.props.history.push("/notes");
+  };
+
+  handleUpdateNote = () => {
+    this.props.updateNote(this.state.note);
+    this.props.history.push("/notes");
+  };
+
+  render() {
+    return (
+      <CreateEditNote
+        {...this.props}
+        note={this.state.note}
+        handleAddNote={this.handleAddNote}
+        handleChange={this.handleChange}
+        handleUpdateNote={this.handleUpdateNote}
+        updatingNote={this.state.updatingNote}
+      />
+    );
+  }
 }
+
+const mapStateToProps = state => ({
+  noteToUpdate: state.noteToUpdate
+});
+
+export default connect(
+  mapStateToProps,
+  { addNewNote, updateNote }
+)(CreateEditNoteView);
