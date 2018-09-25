8efb2f4e0ecb995d839d7d63eba14439013123ed
decagondev <decagondevelopment@gmail.com>
2018-09-25 21:22:07 +0100

    (STRETCH : SEARCH) : add serch input to the sidebar component

--- lambda-notes/src/components/SideBar/index.jsx
+++ lambda-notes/src/components/SideBar/index.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../components.css';
 import './index.css';
-const SideBar = () => {
+const SideBar = props => {
   return (
     <div>
       <div className="sidebar-wrapper">
         <h1>Lambda<br />Notes</h1>
         <Link to="/"><button>View Your Notes</button></Link>
         <Link to="/notes/add"><button>+ Create Ne Note</button></Link>
+        <input
+          type="text"
+          name="search"
+          maxLength="20"
+          placeholder="search..."
+          value={props.search}
+          onChange={props.handleSearchChange}
+        />
       </div>
     </div>
   );
