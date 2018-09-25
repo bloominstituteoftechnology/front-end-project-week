865b66b6d453200cd69c8c05f6175327d361b3a8
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-09-25 16:30:48 -0400

    Added cases for adding and updating notes to the reducer

--- notes/src/store/reducers/notesReducer.js
+++ notes/src/store/reducers/notesReducer.js
@@ -4,13 +4,23 @@ import {
   NOTES_FETCH_ERROR,
   NOTES_DELETE_START,
   NOTES_DELETE_COMPLETE,
-  NOTES_DELETE_ERROR
+  NOTES_DELETE_ERROR,
+  NOTES_ADD_START,
+  NOTES_ADD_COMPLETE,
+  NOTES_ADD_ERROR,
+  NOTES_UPDATE_START,
+  NOTES_UPDATE_COMPLETE,
+  NOTES_UPDATE_ERROR
 } from "../actions";
 
 const initialState = {
   notes: [],
+
   fetchingNotes: false,
   deletingNotes: false,
+  addingNote: false,
+  updatingNote: false,
+
   error: null
 };
 
@@ -28,6 +38,18 @@ export const notesReducer = (state = initialState, action) => {
     case NOTES_DELETE_COMPLETE:
       return { ...state, deletingNote: false, notes: action.payload };
     case NOTES_DELETE_ERROR:
+      console.log(action.payload);
+    case NOTES_ADD_START:
+      return { ...state, addingNote: true };
+    case NOTES_ADD_COMPLETE:
+      return { ...state, addingNote: false, notes: action.payload };
+    case NOTES_ADD_ERROR:
+      console.log(action.payload);
+    case NOTES_UPDATE_START:
+      return { ...state, updatingNote: true };
+    case NOTES_UPDATE_COMPLETE:
+      return { ...state, updatingNote: false, notes: action.payload };
+    case NOTES_UPDATE_ERROR:
       console.log(action.payload);
     default:
       return state;
