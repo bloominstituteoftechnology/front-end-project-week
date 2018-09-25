40a6f0fcc1179ed81431a9df3ad80b36293ee598
bri79319 <bri79319@gmail.com>
2018-09-24 22:39:43 -0400

    making changes to front end

--- mynotes/src/actions/index.js
+++ mynotes/src/actions/index.js
@@ -1,8 +1,13 @@
+import axios from "axios";
 export const DELETE_NOTE = "DELETE_NOTE";
 export const EDIT_NOTE = "EDIT_NOTE";
 export const CREATE_NOTE = "CREATE_NOTE";
+export const FETCH_NOTE = "FETCH_NOTE";
+export const FETCH_NOTE_ID = "FETCH_NOTE_ID";
+
 
 export const deleteNote = id => {
+  axios.delete("https://killer-notes.herokuapp.com/note/delete/id")
   return {
     type: DELETE_NOTE,
     payload: id
