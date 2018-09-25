d269e3ea92541ef1cefbcf7374c34c0098e0ed9d
decagondev <decagondevelopment@gmail.com>
2018-09-25 16:26:56 +0100

    (STRETCH : REDUX) : import thunk and logger

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -7,5 +7,7 @@ import App from './components/App';
 // redux imports
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'react-thunk';
+import logger from 'react-logger';
 
 ReactDOM.render(<Router><Provider><App /></Provider></Router>, document.getElementById('root'));
