7a810e03d9a9d84b889c78489810ef0ac3deac18
Charlie Winslow <cwinslow22@yahoo.com>
2018-09-25 16:27:21 -0500

    started working on add search functionality

---
+++ lambda-notes/src/components/searchBar.js
@@ -0,0 +1,11 @@
+import React, {  } from 'react';
+
+export const SearchNotes = props => {
+
+    return (
+        <form>
+            <input placeholder='Search Notes' />
+        </form>
+    )
+}
+
