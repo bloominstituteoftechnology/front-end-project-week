e39d898e729a9179d4d9b4367b5505e9c4821a94
BrianARuff <thelonewolf198927@gmail.com>
2018-09-25 16:18:32 -0400

    remove 100vh to right side container

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -19,9 +19,7 @@ class App extends Component {
           <div className="col-md-3 p-5 border-left action-container">
             <ActionPanel />
           </div>
-          <div className={`col-md-9 p-5 border right-side ${
-            window.location.href.length > 22 ? 'vh-100' : null
-          }`}>
+          <div className={`col-md-9 p-5 border right-side`}>
             <Route exact path="/" component={ListView} />
             <Route exact path="/newNote" component={NewNoteForm} />
             <Route exact path="/:title/:id" component={GotOne} />
