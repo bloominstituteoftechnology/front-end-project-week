778416c579e0373c854915742d05eb75d387f4f8
m-j-hacker <mjhacker@gmail.com>
2018-09-25 12:29:42 -0500

    Began implementing _id for url navigation

--- notes/src/components/NotesList.js
+++ notes/src/components/NotesList.js
@@ -6,7 +6,9 @@ export default function NotesList(props) {
     return (
         <div className="notes-container">
         {props.notes.map((note, index) => (
-            <div className="note-card">
+            <div 
+            className="note-card"
+            onClick={() => props.history.push(`/notes/${note._id}`)}>
             <h2>{note.title}</h2>
             <p>{note.textBody}</p>
             </div>
