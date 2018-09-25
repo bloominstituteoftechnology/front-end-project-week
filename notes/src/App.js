bc6fd958ecad4b162b01f4fcafaa170676d541c6
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-09-25 15:50:23 -0400

    added event handling and note submission functions to App.js

--- notes/src/App.js
+++ notes/src/App.js
@@ -17,7 +17,24 @@ class App extends Component {
       id: null
     };
   }
-  
+
+  inputHandler = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  publishNote = () => {
+    let single = this.state.notes[this.state.notes.length - 1].id;
+    this.setState({ id: single }, () => {
+      let notes = this.state.notes.slice();
+      let id = this.state.id;
+      if (this.state.singletitle !== '' || this.state.singlebody !== '') { /* checking to make sure that at least one field has input */
+        id++;
+        notes.push({ id: id, title: this.state.singletitle, text: this.state.singlebody });
+        this.setState({ notes, singletitle: '', singlebody: '', id }); /* reset state to allow for new note input */
+      }
+    });
+  };
+
   render() {
     return (
       <div className="App">
