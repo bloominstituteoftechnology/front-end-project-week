f87779fc15f0f49f41aab1f3c608f81e03092116
Max <maxkajiwara@gmail.com>
2018-09-26 14:17:02 -0700

    Style App component

--- lambdanotes/src/components/App.js
+++ lambdanotes/src/components/App.js
@@ -1,6 +1,8 @@
 // Dependencies
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import styled from 'styled-components';
+
 // Components
 import SideBar from './SideBar';
 import NoteListPage from './NoteListPage';
@@ -10,16 +12,25 @@ import NewNotePage from './NewNotePage';
 // Styles
 import '../styles/App.css';
 
+const AppContainer = styled.div`
+	display: flex;
+	max-width: 888px;
+	min-height: 888px;
+	background: #f3f3f3;
+	border: 1px solid #9a9a9a;
+	margin: 0 auto;
+`;
+
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
+			<AppContainer>
 				<SideBar />
 				<Route exact path="/" component={NoteListPage} />
 				<Route path={`/note/:id`} component={NotePage} />
 				<Route path="/new" component={NewNotePage} />
 				{/* <Route path="/edit" component={EditNote} /> */}
-			</div>
+			</AppContainer>
 		);
 	}
 }
