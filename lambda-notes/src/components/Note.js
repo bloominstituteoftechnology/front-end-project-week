17031ab7f88185c0ec2fd79d17e565288e812b4f
unknown <cornielleandres@gmail.com>
2018-09-26 13:32:11 -0400

    Implement exact search functionality.

--- lambda-notes/src/components/Note.js
+++ lambda-notes/src/components/Note.js
@@ -2,19 +2,30 @@
 import React from 'react';
 
 const Note = props => {
+	const { exactInput, note, history } = props;
 	const allowedTags = ['grin-beam', 'angry', 'grin-squint-tears', 'frown'];
 	const tags = [];
 
 	for (let i = 0; i < allowedTags.length; i++) {
-		if (props.note.tags && props.note.tags.includes(allowedTags[i])) tags.push(allowedTags[i]);
+		if (note.tags && note.tags.includes(allowedTags[i])) tags.push(allowedTags[i]);
 	}
 
 	return(
 		<div className = 'note-wrapper'>
-			<div onClick = { () => props.history.push(`/note/${ props.note._id }`) } className = 'note'>
-				<h3>{ props.note.title.length > 11 ? props.note.title.slice(0, 11) + '...' : props.note.title }</h3>
+			<div onClick = { () => history.push(`/note/${ note._id }`) } className = 'note'>
+				{ 
+					exactInput && note.title.includes(exactInput) ? (note.title.length > 11 ? <p><span className = 'highlight'>{ note.title.substr(note.title.indexOf(exactInput)).slice(0, exactInput.length) }</span>{ note.title.substr(note.title.indexOf(exactInput)).slice(exactInput.length, 11) + '...' }</p> : <p><span className = 'highlight'>{ note.title.substr(note.title.indexOf(exactInput)).slice(0, exactInput.length) }</span>{ note.title.substr(note.title.indexOf(exactInput)).slice(exactInput.length) }</p>) : 
+
+					(note.title.length > 11 ? <p>{ note.title.slice(0, 11) + '...' }</p> : <p>{ note.title }</p>)
+				}
+
 				<hr />
-				<p>{ props.note.textBody.length > 85 ? props.note.textBody.slice(0, 85) + '...' : props.note.textBody }</p>
+
+				{ 
+					exactInput && note.textBody.includes(exactInput) ? (note.textBody.length > 85 ? <p><span className = 'highlight'>{ note.textBody.substr(note.textBody.indexOf(exactInput)).slice(0, exactInput.length) }</span>{ note.textBody.substr(note.textBody.indexOf(exactInput)).slice(exactInput.length, 85) + '...' }</p> : <p><span className = 'highlight'>{ note.textBody.substr(note.textBody.indexOf(exactInput)).slice(0, exactInput.length) }</span>{ note.textBody.substr(note.textBody.indexOf(exactInput)).slice(exactInput.length) }</p>) : 
+
+					(note.textBody.length > 85 ? <p>{ note.textBody.slice(0, 85) + '...' }</p> : <p>{ note.textBody }</p>)
+				}
 			</div>
 
 			{ tags.length > 0 && 
--- lambda-notes/src/views/ListView/index.js
+++ lambda-notes/src/views/ListView/index.js
@@ -4,16 +4,98 @@ import React from 'react';
 // Components
 import { Note } from '../../components';
 
-const ListView = props => {
-	return(
-		<div className = 'main-content'>
-			<div className = 'list'>
-				<h2>{ props.username }'s Notes:</h2>
+class ListView extends React.Component {
+	state = {
+		search: {
+			exactSearch: false,
+			fuzzySearch: false,
+		},
+		input: {
+			exactInput: '',
+			fuzzyInput: '',
+		}
+	};
 
-				{ props.notes.map((note, i) => <Note history = { props.history } key = { i } note = { note } />) }
+	toggleSearch = e => {
+		e.preventDefault();
+		const newSearch = { ...this.state.search };
+
+		for (let key in newSearch) {
+			if (key === e.target.name) newSearch[key] = !newSearch[key];
+			else newSearch[key] = false;
+		}
+
+		this.setState({
+			...this.state,
+			search: newSearch,
+			input: { ...this.state.input },
+		});
+	}
+
+	handleInputChange = e => {
+		e.preventDefault();
+		const newInput = { ...this.state.input };
+
+		for (let key in newInput) {
+			if (key === e.target.name) newInput[key] = e.target.value;
+			else newInput[key] = '';
+		}
+
+		this.setState({
+			...this.state,
+			search: { ...this.state.search },
+			input: newInput,
+		});
+
+
+	}
+
+	render() {
+		return(
+			<div className = 'main-content'>
+				<div className = 'list'>
+					<div className = 'search-wrapper'>
+						<button name = 'exactSearch' onClick = { e => this.toggleSearch(e) }>Exact search</button>
+
+						<button name = 'fuzzySearch' onClick = { e => this.toggleSearch(e) }>Fuzzy search</button>
+
+						{ this.state.search.exactSearch && 
+							<div className = 'search'>
+								<input 
+									name = 'exactInput' 
+									placeholder = 'Search for exact terms...' 
+									value = { this.state.input.exactInput } 
+									onChange = { e => this.handleInputChange(e) } 
+								/>
+							</div> 
+						}
+
+						{ this.state.search.fuzzySearch && 
+							<div className = 'search'>
+								<input 
+									name = 'fuzzyInput' 
+									placeholder = 'Search for fuzzy terms...' 
+									value = { this.state.input.fuzzyInput } 
+									onChange = { e => this.handleInputChange(e) } 
+								/>
+							</div> 
+						}
+					</div>
+
+					<h2>{ this.props.username }'s Notes:</h2>
+
+					{ 
+						(this.state.search.exactSearch && (this.props.notes.filter((note) => {
+							if ((note.title.indexOf(this.state.input.exactInput) !== -1) || (note.textBody.indexOf(this.state.input.exactInput) !== -1)) return true;
+							else return false;
+						}).map((note, i) => <Note history = { this.props.history } key = { i } exactInput = { this.state.input.exactInput } note = { note } />)) ) || 
+
+						(this.props.notes.map((note, i) => <Note history = { this.props.history } key = { i } note = { note } />)) 
+					}
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default ListView;
