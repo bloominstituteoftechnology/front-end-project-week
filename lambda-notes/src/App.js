4978fb6514b53d6b5cd274c638a640a742397525
Sukhada Gholba <sukhadagholba@gmail.com>
2018-09-26 16:07:33 -0700

    added Login page, making axios call to register/login and setting token in localStorage

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -3,8 +3,10 @@ import ViewNotesContainer from './components/ViewNotes/ViewNotesContainer';
 import CreateNoteContainer from './components/CreateNotes/CreateNoteContainer';
 import EachNoteBody from './components/EachNote/EachNoteBody';
 import EditNoteBody from './components/EditNote/EditNoteBody';
+import Login from './components/Login/Login';
 import './App.css';
 import {Route} from 'react-router-dom';
+import styled from 'styled-components';
 
 class App extends Component {
 constructor(props){
@@ -18,7 +20,8 @@ super(props);
 render() {
     return (
       <div className="App">
-      	<Route exact  path="/" component={ViewNotesContainer} />
+	 <Route exact  path="/" component={Login} />   
+      	<Route exact  path="/notes" component={ViewNotesContainer} />
         <Route exact path="/add" render={(props) => <CreateNoteContainer {...props} />} />	   
 	<Route exact path="/note/:id" render={(props) => <EachNoteBody {...props} />} />
 	<Route exact path="/editnote/:id" render={(props) => <EditNoteBody {...props} />} />
--- lambda-notes/src/components/ViewNotes/LambdaLeftDiv.js
+++ lambda-notes/src/components/ViewNotes/LambdaLeftDiv.js
@@ -6,7 +6,7 @@ const LambdaLeftDiv = () => {
 	return(
 		<div className="left-div">
 		<h1 className="lambda-notes-header">Lambda Notes</h1>	
-		<Link style={{ textDecoration: 'none' }} to="/"><div className="teal-div">View Your Notes</div></Link>
+		<Link style={{ textDecoration: 'none' }} to="/notes"><div className="teal-div">View Your Notes</div></Link>
 		<Link  style={{ textDecoration: 'none' }} to="/add"><div className="teal-div">+ Create New Note</div></Link>
 		</div>
 	);
