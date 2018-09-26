3995ac2bbe5264c4c60843ea62c304eae14f07e2
Max <maxkajiwara@gmail.com>
2018-09-26 15:05:57 -0700

    Style Note component

--- lambdanotes/src/components/Note.js
+++ lambdanotes/src/components/Note.js
@@ -1,18 +1,49 @@
 // Dependencies
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 // Styles
 import '../styles/Note.css';
 
+const NoteLink = styled(Link)`
+	width: 192px;
+	height: 197px;
+	text-decoration: none;
+	color: black;
+	padding: 10px 20px 0 16px;
+	background: white;
+	border: 1px solid #a5a5a5;
+	margin: 0 16px 24px 0;
+`;
+
+const NoteTitle = styled.h3`
+	white-space: nowrap;
+	overflow: hidden;
+	text-overflow: ellipsis;
+	font-size: 1.8rem;
+	font-weight: bold;
+	padding-bottom: 6px;
+	border-bottom: 1px solid #969696;
+	margin-bottom: 6px;
+`;
+
+const NoteBody = styled.p`
+	font-size: 1.3rem;
+	line-height: 2.2rem;
+`;
+
+const truncate = (content, length) => {
+	return content.length > length
+		? `${content.slice(0, length - 3)} …`
+		: content;
+};
+
 const Note = props => {
 	return (
-		<li className="NoteListPage">
-			<Link to={`/note/${props.note._id}`} className="note-link">
-				<h3 className="note-title">{props.note.title}</h3>
-				{/* todo: clamp preview text */}
-				<p className="note-preview">{props.note.textBody}</p>
-			</Link>
-		</li>
+		<NoteLink to={`/note/${props.note._id}`}>
+			<NoteTitle>{props.note.title}</NoteTitle>
+			<NoteBody>{truncate(props.note.textBody, 140)}</NoteBody>
+		</NoteLink>
 	);
 };
 
