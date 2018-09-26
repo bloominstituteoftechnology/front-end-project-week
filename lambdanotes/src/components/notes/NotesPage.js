76842d66aa0e5703687aaadd7bcd733e2dd52bdc
Michael Agard <mdagard@gmail.com>
2018-09-25 23:00:23 -0400

    handle text overflow

--- lambdanotes/src/components/notes/NotesPage.js
+++ lambdanotes/src/components/notes/NotesPage.js
@@ -4,36 +4,48 @@ import { fetchNotes } from '../../actions';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 
-const NotesPagerapper = styled.div`
+const NotesWrapper = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  border: 1px solid #c8c7c8;
+  /* border: 1px solid #c8c7c8; */
   font-size: 20px;
   background: #f2f1f2;
-  justify-content: space-around;
+  /* justify-content: space-evenly; */
 `;
-
+const NotesPageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const NoteHeaderWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 40px;
+  background: #f2f1f2;
+  padding: 70px 0px 35px 35px;
+`;
 class NotesPage extends Component {
   componentDidMount() {
     this.props.fetchNotes();
   }
   render() {
-    // console.log(this.props.notes);
     return (
-      <NotesPagerapper>
-        {this.props.notes.map(note => {
-          return (
-            <NoteList
-              tags={note.tags}
-              title={note.title}
-              textBody={note.textBody}
-              key={note._id}
-              id={note._id}
-            />
-          );
-        })}
-      </NotesPagerapper>
+      <NotesPageWrapper>
+        <NoteHeaderWrapper>Your Notes:</NoteHeaderWrapper>
+        <NotesWrapper>
+          {this.props.notes.map(note => {
+            return (
+              <NoteList
+                tags={note.tags}
+                title={note.title}
+                textBody={note.textBody}
+                key={note._id}
+                id={note._id}
+              />
+            );
+          })}
+        </NotesWrapper>
+      </NotesPageWrapper>
     );
   }
 }
