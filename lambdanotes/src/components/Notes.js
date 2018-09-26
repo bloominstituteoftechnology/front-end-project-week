1a446822b33b0d44d91c75970fbf0664ef6a532d
Synch3 <jonsimo@aol.com>
2018-09-26 18:01:45 -0400

    Two endpoints working with backend

--- lambdanotes/src/components/Notes.js
+++ lambdanotes/src/components/Notes.js
@@ -1,32 +1,45 @@
 import React from "react";
-import { Link } from 'react-router-dom';
-import dummyData from './dummydata';
+import { Link } from "react-router-dom";
+import dummyData from "./dummydata";
+import axios from "axios";
 
-
+const URL = "http://localhost:9000/api/notes";
 
 class Notes extends React.Component {
-    constructor(props) {
-        super(props);
-          this.state = {
-          notes: this.props.notes,
-		  title: '',
-		  text: '',
-          };
-    }  
+  constructor(props) {
+    super(props);
+    this.state = {
+      response: []
+    };
+  }
 
-	render() {
-		console.log(this.state.notes[0].id)
-		return (
-			<div>
-				<h3>Your Notes:</h3>
-				{this.state.notes.map(note => (
-					<Link to={`/notes/${note.id}`}>
-						<h4>{note.title}</h4>
-						<p>{note.text}</p>
-					</Link>
-				))}
-			</div>
-		);
-	}
+  componentDidMount() {
+    this.callApi();
+  }
+
+  callApi = () => {
+    axios
+      .get(`${URL}`)
+      .then(res => this.setState({ response: res.data }))
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    console.log(this.state.response);
+    return (
+      <div>
+        <h3>Your Notes:</h3>
+        {this.state.response.map(note => {
+          console.log("line33", note);
+          return (
+            <Link key={note.id} to={`/notes/${note.id}`}>
+              <h4>{note.title}</h4>
+              <p>{note.body}</p>
+            </Link>
+          );
+        })}
+      </div>
+    );
+  }
 }
 export default Notes;
