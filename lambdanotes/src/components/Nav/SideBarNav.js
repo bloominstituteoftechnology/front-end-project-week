7aeb7856351e09fe73e12aa2b949b6b7c56ddd68
Michael Agard <mdagard@gmail.com>
2018-09-25 20:03:35 -0400

    comment out NotesPage

--- lambdanotes/src/components/Nav/SideBarNav.js
+++ lambdanotes/src/components/Nav/SideBarNav.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-import NotesPage from '../notes/NotesPage';
+// import NotesPage from '../notes/NotesPage';
 
 const SideBarNavWrapper = styled.div`
   display: flex;
