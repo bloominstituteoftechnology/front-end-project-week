bd917292d06746fe418e34801898090a0021079b
Michael Agard <mdagard@gmail.com>
2018-09-25 22:59:52 -0400

    move sc to bottom

--- lambdanotes/src/components/notes/NoteList.js
+++ lambdanotes/src/components/notes/NoteList.js
@@ -2,27 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-const NoteCardWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  border: 1px solid #c8c7c8;
-  font-size: 18px;
-  width: 295px;
-  height: 240px;
-  background: white;
-  margin-bottom: 25px;
-  padding: 5px 15px 20px 15px;
-`;
-
-const NoteTitleWrapper = styled.h3`
-  font-size: 24px;
-  font-weight: bold;
-  border-bottom: solid 1px black;
-  padding-bottom: 5px;
-  margin-bottom: 10px;
-`;
-
 const NoteList = props => {
   return (
     <Link to={`/note/${props.id}`}>
@@ -37,3 +16,28 @@ const NoteList = props => {
 };
 
 export default NoteList;
+
+const NoteCardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  border: 1px solid #c8c7c8;
+  font-size: 18px;
+  width: 295px;
+  height: 240px;
+  background: white;
+  margin: 0px 0px 35px 35px;
+  padding: 5px 15px 20px 15px;
+  overflow: hidden;
+`;
+
+const NoteTitleWrapper = styled.h3`
+  font-size: 24px;
+  font-weight: bold;
+  border-bottom: solid 1px black;
+  padding-bottom: 5px;
+  margin-bottom: 10px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
