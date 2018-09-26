ecebc29ea85ff5754f6922b7a25030e940e402ea
Michael Agard <mdagard@gmail.com>
2018-09-25 23:23:10 -0400

    relocate sc to bottom

--- lambdanotes/src/components/notes/NotesPage.js
+++ lambdanotes/src/components/notes/NotesPage.js
@@ -4,6 +4,50 @@ import { fetchNotes } from '../../actions';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 
+class NotesPage extends Component {
+  componentDidMount() {
+    this.props.fetchNotes();
+  }
+  render() {
+    return (
+      <React.Fragment>
+        {this.props.fetching ? (
+          <p>Reticulating Splines...</p>
+        ) : (
+          <NotesPageWrapper>
+            <NoteHeaderWrapper>Your Notes:</NoteHeaderWrapper>
+            <NotesWrapper>
+              {this.props.notes.map(note => {
+                return (
+                  <NoteList
+                    tags={note.tags}
+                    title={note.title}
+                    textBody={note.textBody}
+                    key={note._id}
+                    id={note._id}
+                  />
+                );
+              })}
+            </NotesWrapper>
+          </NotesPageWrapper>
+        )}
+        {this.props.error ? <p>{this.props.error}</p> : null}
+      </React.Fragment>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  notes: state.notes,
+  fetching: state.fetching,
+  error: state.error
+});
+
+export default connect(
+  mapStateToProps,
+  { fetchNotes }
+)(NotesPage);
+
 const NotesWrapper = styled.div`
   display: flex;
   flex-direction: row;
@@ -24,39 +68,3 @@ const NoteHeaderWrapper = styled.div`
   background: #f2f1f2;
   padding: 70px 0px 35px 35px;
 `;
-class NotesPage extends Component {
-  componentDidMount() {
-    this.props.fetchNotes();
-  }
-  render() {
-    return (
-      <NotesPageWrapper>
-        <NoteHeaderWrapper>Your Notes:</NoteHeaderWrapper>
-        <NotesWrapper>
-          {this.props.notes.map(note => {
-            return (
-              <NoteList
-                tags={note.tags}
-                title={note.title}
-                textBody={note.textBody}
-                key={note._id}
-                id={note._id}
-              />
-            );
-          })}
-        </NotesWrapper>
-      </NotesPageWrapper>
-    );
-  }
-}
-
-const mapStateToProps = state => ({
-  notes: state.notes,
-  fetching: state.fetching,
-  error: state.error
-});
-
-export default connect(
-  mapStateToProps,
-  { fetchNotes }
-)(NotesPage);
