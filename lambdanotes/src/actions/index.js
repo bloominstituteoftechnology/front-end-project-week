fa4789e02999413ef2fbebdb539fcb04a69f19d6
Michael Agard <mdagard@gmail.com>
2018-09-25 23:21:47 -0400

    add note submission functionality.

--- lambdanotes/src/actions/index.js
+++ lambdanotes/src/actions/index.js
@@ -4,10 +4,12 @@ export const FETCHING_NOTES = 'FETCHING_NOTES';
 export const FETCHED_NOTES = 'FETCHED_NOTES';
 export const FETCH_NOTES_ERROR = 'ERRORS';
 // export const DELETE_NOTE = 'DELETE_NOTE';
-// export const ADD_NOTE = 'ADD_NOTE';
 export const FETCH_NOTE = 'FETCH_NOTE';
 export const FETCHED_NOTE = 'FETCHED_NOTE';
 export const FETCH_NOTE_ERROR = 'FETCH_NOTE_ERROR';
+export const NOTE_ADD = 'NOTE_ADD';
+export const NOTE_ADDED = 'NOTE_ADDED';
+export const NOTE_ADD_ERROR = 'NOTE_ADD_ERROR';
 
 export const fetchNotes = () => {
   return dispatch => {
@@ -39,17 +41,17 @@ export const fetchNote = id => {
   };
 };
 
-// export const addNote = note => {
-//   return dispatch => {
-//     dispatch({ type: ADD_NOTE });
-//     axios
-//     .post('https://killer-notes.herokuapp.com/note/create', note)
+export const addNote = note => {
+  return dispatch => {
+    dispatch({ type: NOTE_ADD });
+    axios
+      .post('https://killer-notes.herokuapp.com/note/create', note)
 
-//     .then(response => {
-//       dispatch({ type: FETCHED_NOTES, payload: response.data});
-//       // console.log(response.data);
-//     })
+      .then(response => {
+        dispatch({ type: NOTE_ADDED, payload: response.data });
+        // console.log(response.data);
+      })
 
-//     .catch(error => dispatch({ type: ERRORS, error: error}));
-//   };
-// };
+      .catch(error => dispatch({ type: NOTE_ADD_ERROR, error: error }));
+  };
+};
