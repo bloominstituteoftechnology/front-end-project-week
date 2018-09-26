7f85d18702c1dfeb269c42f33efc1efb448e4501
Michael Agard <mdagard@gmail.com>
2018-09-25 22:58:32 -0400

    empty line removed

--- lambdanotes/src/index.js
+++ lambdanotes/src/index.js
@@ -8,7 +8,6 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from './reducers';
-
 import App from './components/App';
 
 import './reset.css';
