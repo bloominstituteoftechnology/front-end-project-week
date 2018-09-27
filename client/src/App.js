1fff8b3275b563f5fb0e04c78d2eba0e0ecb269b
Ashwin <aswinjeganath@gmail.com>
2018-09-27 16:09:23 +0530

    Add page transitions

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import SideNav from './components/SideNav';
 import styled from 'styled-components';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, withRouter } from 'react-router-dom';
 import ListContainer from './containers/ListContainer';
 import NoteContainer from './containers/NoteContainer';
 import AddNoteContainer from './containers/AddNoteContainer';
@@ -11,6 +11,7 @@ import SortingOptions from './containers/SortingOptions';
 import LoggedIn from './containers/LoggedIn';
 import Register from './components/Register';
 import Login from './components/Login';
+import Fade from './components/Fade';
 
 const MainContent = styled.div`
   padding: 4rem;
@@ -22,24 +23,32 @@ class App extends Component {
       <div>
         <LoggedIn>{props => <SideNav {...props} />}</LoggedIn>
         <MainContent>
-          <Switch>
-            <Route exact path="/" component={Login} />
-            <Route exact path="/login" component={Register} />
-            <Route
-              exact
-              path="/notes"
-              render={() => (
-                <React.Fragment>
-                  <SearchBar />
-                  <SortingOptions />
-                  <ListContainer />
-                </React.Fragment>
-              )}
-            />
-            <Route exact path="/notes/new" component={AddNoteContainer} />
-            <Route exact path="/notes/:id/edit" component={EditNoteContainer} />
-            <Route exact path="/notes/:id" component={NoteContainer} />
-          </Switch>
+          <Fade>
+            {({ location }) => (
+              <Switch location={location}>
+                <Route exact path="/" render={() => <Login />} />
+                <Route exact path="/register" component={Register} />
+                <Route
+                  exact
+                  path="/notes"
+                  render={() => (
+                    <React.Fragment>
+                      <SearchBar />
+                      <SortingOptions />
+                      <ListContainer />
+                    </React.Fragment>
+                  )}
+                />
+                <Route exact path="/notes/new" component={AddNoteContainer} />
+                <Route
+                  exact
+                  path="/notes/:id/edit"
+                  component={EditNoteContainer}
+                />
+                <Route exact path="/notes/:id" component={NoteContainer} />
+              </Switch>
+            )}
+          </Fade>
         </MainContent>
       </div>
     );
---
+++ client/src/components/Fade.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import styled, { keyframes } from 'styled-components';
+
+const Fader = styled.div`
+  opacity: ${props => (props.fadeIn ? 1 : 0)};
+  transform: translateY(${props => (props.fadeIn ? 0 : 50)}px);
+  transition: all 0.3s;
+`;
+
+class Fade extends Component {
+  state = { in: false, location: this.props.location };
+
+  componentDidMount() {
+    setTimeout(() => this.setState({ in: true }), 0);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.location !== prevProps.location) {
+      this.setState({ in: false }, () =>
+        setTimeout(
+          () => this.setState({ in: true, location: this.props.location }),
+          300,
+        ),
+      );
+      console.log(this.props.location, prevProps.location);
+    }
+  }
+
+  render() {
+    return (
+      <Fader fadeIn={this.state.in}>
+        {this.props.children({ location: this.state.location })}
+      </Fader>
+    );
+  }
+}
+
+export default withRouter(Fade);
