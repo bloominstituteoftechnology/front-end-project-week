2da02e6cfa417b8a1911df4710c371972b69e223
Max <maxkajiwara@gmail.com>
2018-09-26 20:01:15 -0700

    Create shared styled-components

--- lambdanotes/src/styles/SharedStyles.js
+++ lambdanotes/src/styles/SharedStyles.js
@@ -33,3 +33,67 @@ export const StatusMessage = styled.h4`
 			color: red;
 		`};
 `;
+
+export const StyledInput = styled.input`
+	width: 358px;
+	height: 43px;
+	padding: 12px;
+	border: 1px solid #9a9a9a;
+	border-radius: 3px;
+
+	&::placeholder {
+		font-size: 1.2rem;
+	}
+`;
+
+export const StyledTextArea = styled.textarea`
+	font-family: inherit;
+	width: 100%;
+	height: 348px;
+	padding: 22px;
+	border: 1px solid #9a9a9a;
+	border-radius: 3px;
+	margin: 14px 0 12px;
+	resize: none;
+
+	&::placeholder {
+		font-size: 1.2rem;
+	}
+`;
+
+// WHY CAN'T I MAKE TAG AGNOSTIC STYLED-COMPONENTS
+export const SubmitInput = styled.input`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 191px;
+	height: 43px;
+	font-size: 1.5rem;
+	font-weight: bold;
+	text-decoration: none;
+	color: white;
+	background: #24b8bd;
+	border: 1px solid #969696;
+	cursor: pointer;
+`;
+// I'M SO WET RIGHT NOW
+export const SubmitButton = styled.button`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 191px;
+	height: 43px;
+	font-size: 1.5rem;
+	font-weight: bold;
+	text-decoration: none;
+	color: white;
+	background: #24b8bd;
+	border: 1px solid #969696;
+	cursor: pointer;
+
+	${props =>
+		props.red &&
+		css`
+			background: #ca001a;
+		`};
+`;
