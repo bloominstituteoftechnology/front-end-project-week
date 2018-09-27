fdfae5bafa40f6aca63ba3a2fc14bf3877922a1c
duckets615 <33557104+duckets615@users.noreply.github.com>
2018-09-27 14:02:01 -0400

    mvp I mvp II mvp III

---
+++ src/views/NotesListView.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import NotesList from '../components/NotesList/NotesList';
+
+class NotesListView extends React.Component {
+    componentDidMount() {
+        this.props.notesList;
+    }
+
+    render() {
+        return (
+            <div>
+            <NotesList {...this.props}/>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+    notesList: state.notes
+});
+
+export default connect(mapStateToProps)(NotesListView);
