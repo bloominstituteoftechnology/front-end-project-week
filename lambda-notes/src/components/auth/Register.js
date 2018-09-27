ae0a3548e2b74f5ac1cd8c65a1f75ccc9c66deee
sca4cs <shannon.atkinson.ca@gmail.com>
2018-09-27 10:03:04 -0400

    added some styling to login page

--- lambda-notes/src/components/auth/Register.js
+++ lambda-notes/src/components/auth/Register.js
@@ -30,31 +30,33 @@ class Register extends Component {
 
   render() {
     return (
-      <div className="outer-div">
-        <div className="link-buttons">
-            <Link to={`/`}>Login</Link>
-            <Link to={`/signup`}>Sign Up</Link>
+        <div className="login-page">
+            <div className="outer-div">
+            <div className="link-buttons">
+                <Link to={`/`}>Login</Link>
+                <Link to={`/signup`}>Sign Up</Link>
+            </div>
+            <div className="inner-div">
+                <form className="form-div" onSubmit={this.submitHandler}>
+                    <input
+                    value={this.state.username}
+                    onChange={this.inputChangeHandler}
+                    type="text"
+                    placeholder="Username"
+                    name="username"
+                    />
+                    <input
+                    value={this.state.password}
+                    onChange={this.inputChangeHandler}
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    />
+                    <button type="submit">Create Account</button>
+                </form>
+            </div>
+            </div>
         </div>
-        <div className="inner-div">
-            <form className="form-div" onSubmit={this.submitHandler}>
-                <input
-                value={this.state.username}
-                onChange={this.inputChangeHandler}
-                type="text"
-                placeholder="Username"
-                name="username"
-                />
-                <input
-                value={this.state.password}
-                onChange={this.inputChangeHandler}
-                type="password"
-                placeholder="Password"
-                name="password"
-                />
-                <button type="submit">Create Account</button>
-            </form>
-        </div>
-      </div>
     );
   }
 
