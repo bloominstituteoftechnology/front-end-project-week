0e2d661dd6dc50845a4947dea9d901174d45c1b1
BrianARuff <thelonewolf198927@gmail.com>
2018-09-27 16:45:42 -0400

    max width on cards fixed

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 // Auxillary Packages
-import { connect } from 'react-redux';
-import { updateNote } from './actions';
-import { withRouter } from 'react-router-dom';
+import { connect } from "react-redux";
+import { updateNote } from "./actions";
+import { withRouter } from "react-router-dom";
 
 // Purgatorial Packages
-import { Route } from 'react-router-dom';
+import { Route } from "react-router-dom";
 
 // Components
-import ListView from './components/ListView';
-import ActionPanel from './components/ActionPanel';
-import NewNoteForm from './components/NewNoteForm';
-import GotOne from './components/GotOne';
-import EditNoteForm from './components/EditNoteForm';
+import ListView from "./components/ListView";
+import ActionPanel from "./components/ActionPanel";
+import NewNoteForm from "./components/NewNoteForm";
+import GotOne from "./components/GotOne";
+import EditNoteForm from "./components/EditNoteForm";
 
 // CSS
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <div className="row m-3" style={this.props.notes.length < 1 ? {height: '100vh'} : {height: '100%'}}>
+        <div
+          className="row m-3"
+          style={
+            this.props.notes.length > 1
+              ? { height: "100%" }
+              : { height: "100vh" }
+          }
+        >
           <div className="col-md-3 p-5 border-left action-container left-side">
             <ActionPanel />
           </div>
@@ -30,7 +37,11 @@ class App extends Component {
             <Route exact path="/" component={ListView} />
             <Route exact path="/newNote" component={NewNoteForm} />
             <Route exact path="/:title/:id" component={GotOne} />
-            <Route exact path="/editForm/:title/:id" render={() => <EditNoteForm updateNote={ this.props.updateNote } />} />
+            <Route
+              exact
+              path="/editForm/:title/:id"
+              render={() => <EditNoteForm updateNote={this.props.updateNote} />}
+            />
           </div>
         </div>
       </div>
@@ -42,8 +53,13 @@ const mapStateToProps = state => {
   return {
     updated: state.updated,
     isFetching: state.isFetching,
-    notes: state.notes,
-  }
-}
+    notes: state.notes
+  };
+};
 
-export default withRouter(connect(mapStateToProps, { updateNote })(App));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    { updateNote }
+  )(App)
+);
