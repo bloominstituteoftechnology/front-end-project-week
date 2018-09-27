0af5397679ea08fca1d3e525ce4e4960554f0397
Michael Agard <mdagard@gmail.com>
2018-09-27 12:02:53 -0400

    add style

--- lambdanotes/src/components/Nav/SideBarNav.js
+++ lambdanotes/src/components/Nav/SideBarNav.js
@@ -8,12 +8,38 @@ const SideBarNavWrapper = styled.div`
   display: flex;
   flex-direction: column;
   min-width: 245px;
+  padding: 20px 0px 0px 20px;
+`;
+const HeaderWrapper = styled.h1`
+  font-size: 48px;
+  margin-bottom: 35px;
+`;
+const SideBarButton = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 210px;
+  height: 47px;
+  border: 1px solid grey;
+  background: #24b8bd;
+  font-size: 18px;
+  text-decoration: none;
+  margin-bottom: 15px;
 `;
 const SideBarNav = () => {
   return (
     <SideBarNavWrapper className="SideBarNav">
-      <Link to="/">View Your Notes</Link>
-      <Link to="/addnote">Add Note</Link>
+      <HeaderWrapper>Lambda Notes</HeaderWrapper>
+      <SideBarButton>
+        <Link to="/" style={{ textDecoration: 'none' }}>
+          View Your Notes
+        </Link>
+      </SideBarButton>
+      <SideBarButton>
+        <Link to="/addnote" style={{ textDecoration: 'none' }}>
+          Add Note
+        </Link>
+      </SideBarButton>
     </SideBarNavWrapper>
   );
 };
