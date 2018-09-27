6ddd4253b8a3b9063b55488cfef1bfced0305a2d
Luisan <luisangelsc@gmail.com>
2018-09-27 13:34:10 -0600

    every 9 notes has its own class

--- notes/src/components/NotesList.js
+++ notes/src/components/NotesList.js
@@ -3,29 +3,58 @@ import React, { Component } from 'react';
 import Note from './Note';
 // import { Route } from 'react-router-dom'
 
+let pages = 0
+var divs = []
+let i = 0
+var whatevs = []
+let filteredNoteList= ''
+
 class NoteList extends Component {
 
+
+
   render() {
-    console.log('NoteList Component came first:', this.props.noteList);
+    divs = []
+    filteredNoteList = this.props.noteList.filter(item =>
+      (JSON.stringify(item.title)+JSON.stringify(item.textBody))
+      .toLowerCase().includes(this.props.filter.toLowerCase()))
+    pages =
+    parseInt((
+      this.props.noteList.filter(item =>
+        (JSON.stringify(item.title)+JSON.stringify(item.textBody))
+        .toLowerCase().includes(this.props.filter.toLowerCase())).length-1
+      )/9)+1
+      console.log(pages);
+      whatevs = Array.from(Array(pages).keys())
+      console.log('whatevs:', whatevs);
+      console.log('props:', this.props);
+      console.log('FilteredNoteList:', filteredNoteList);
+    // for(i=0; i<pages; i++){
+    //   // divs.includes(`<div className="note-list" id="page${i}">`) ? '' :
+    //   divs.push(`<div className="note-list" id="page${i}">`)
+    // }
+    console.log(divs);
+    // console.log('NoteList Component came first:', this.props.noteList);
     return this.props
-    ?  (
+    ?
+    (
       <div className="note-list">
-          {this.props.noteList.filter(item =>
-            (JSON.stringify(item.title)+JSON.stringify(item.textBody))
-            .toLowerCase().includes(this.props.filter.toLowerCase()))
-            .map(note => {
-              console.log('NoteList _id:', note._id);
-            return (
-                <React.Fragment key={note._id}>
-                <Note
-                  match={this.props.match}
-                  note={note}
-                  // key={note._id}
-                />
-              </React.Fragment>
-              // } />
-            );
-          })}
+
+          {
+            whatevs.map(item => {
+              return (<div className="note-list" id={`page${item}`}>{filteredNoteList.slice((item*9),(item+1)*9)
+              .map(note => {
+                return (
+                  <Note
+                    match={this.props.match}
+                    note={note}
+                    key={note._id}
+                  />
+                );
+              })
+            }</div>)
+            })
+        }
       </div>
     )
     : (<h2>API Error...</h2>)
