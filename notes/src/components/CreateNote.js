f56e160950eb1c9148e115474999c0d9101113ca
Grant Reighard <grant.reighard@gmail.com>
2018-09-27 14:54:10 -0400

    Removed gitignore files.

--- notes/src/components/CreateNote.js
+++ notes/src/components/CreateNote.js
@@ -15,6 +15,10 @@ class CreateNote extends Component {
         this.setState({[event.target.name]: event.target.value})
     }
 
+    onDrop = (picture) => {
+        this.setState({pictures: this.state.pictures.concat(picture)});
+    }
+
     render() {
         return (
             <div className="createNote">
@@ -22,7 +26,7 @@ class CreateNote extends Component {
                 <form className="createForm" onSubmit={(ev) => {ev.preventDefault(); this.props.submit(this.state.tags, this.state.title, this.state.textBody); this.setState({title: '', tags: '', textBody: ''})}} >
                     <input type="text" name="title" className="createFormTitleInput" placeholder="Note Title" value={this.state.title} onChange={this.changeHandler} />
                     <input type="text" name="tags" className="createFormTitleInput" placeholder="Note Tags" value={this.state.tags} onChange={this.changeHandler} />
-                    <textarea type="text" name="textBody" className="createFormTextInput" placeholder="Note Text" value={this.state.textBody} onChange={this.changeHandler} />
+                    <textarea type="text" name="textBody" className="createFormTextInput" placeholder="Note Text" value={this.state.textBody} onChange={this.changeHandler} />                    
                     <button className="button saveButton" type="submit">Save</button>
                 </form>
             </div>
