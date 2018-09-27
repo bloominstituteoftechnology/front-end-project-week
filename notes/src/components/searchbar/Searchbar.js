c7ec0215bee9c6f501885c8aa1b5cf79af2f6209
jordan-massingill <jordan_massingill@hotmail.com>
2018-09-27 12:15:33 -0500

    Added searchbar component
    Added search component
    Need to create functionality

---
+++ notes/src/components/searchbar/Searchbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Input = styled.input `
+  height: 25px;
+  width: 180px;
+  text-align: center;
+`;
+
+export const Searchbar = (props) => {
+  <form onSubmit={props.searchSubmit}>
+  <Input onChange={props.change} placeholder="Search..." name='searchValue' value={props.searchValue} />
+  </form>
+}
