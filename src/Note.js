f2aadad175be1fddbf6007fefe849987524ff8b2
Charles Cortinas <crcortinas@gmail.com>
2018-09-27 16:10:12 -0500

    fixed syntax errors

--- src/Note.js
+++ src/Note.js
@@ -1,52 +1,56 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
 class Note extends React.Component {
   constructor(props) {
     super(props);
     const id = props.match.params.id;
-    console.log('id ', id);
+    console.log("id ", id);
     const note = props.notes.find(note => {
-      return note._id === id;
+      return note.id === id;
     });
-    console.log('note ', note);
+    console.log("note ", note);
     this.state = {
       id: id,
       note: note,
       deleteHandler: props.deleteHandler,
       showModal: false
-    }
+    };
   }
 
   deleteHandler = e => {
     this.state.deleteHandler(e, this.state.id);
-    this.props.history.push('/');
-  }
+    this.props.history.push("/");
+  };
 
   toggle = () => {
     this.setState({ showModal: !this.state.showModal });
-  }
+  };
 
   render() {
     return (
-      <div className='note-view'>
-        <div className='note-links'>
-          <Link to={`/edit/${this.state.id}`}><p>edit</p></Link>
+      <div className="note-view">
+        <div className="note-links">
+          <Link to={`/edit/${this.state.id}`}>
+            <p>edit</p>
+          </Link>
           <p onClick={this.toggle}>delete</p>
         </div>
-        <div className='note-content'>
+        <div className="note-content">
           <h5>{this.state.note.title}</h5>
-          <p>{this.state.note.textBody}</p>
+          <p>{this.state.note.contents}</p>
         </div>
         <Modal isOpen={this.state.showModal} toggle={this.toggle}>
           <ModalHeader toggle={this.toggle}>Delete Note</ModalHeader>
-          <ModalBody>
-            Are you sure you want to delete this note?
-          </ModalBody>
+          <ModalBody>Are you sure you want to delete this note?</ModalBody>
           <ModalFooter>
-            <Button color="danger" onClick={this.deleteHandler}>Delete</Button>{' '}
-            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+            <Button color="danger" onClick={this.deleteHandler}>
+              Delete
+            </Button>{" "}
+            <Button color="secondary" onClick={this.toggle}>
+              Cancel
+            </Button>
           </ModalFooter>
         </Modal>
       </div>
