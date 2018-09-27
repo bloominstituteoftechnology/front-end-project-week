515bf69b14941f9564628b009329b0c83e9affcd
Keiran <keirankozlowski@gmail.com>
2018-09-27 13:42:38 -0400

    Removed some extraneous styles.

--- lambda-notes/src/components/SingleNoteCard.js
+++ lambda-notes/src/components/SingleNoteCard.js
@@ -21,42 +21,6 @@ class SingleNoteCard extends Component {
         this.props.fetchSingleNote(_id);
     }
 
-    // return(
-    //     <Fragment>
-    // <div className="single-note">
-    //     <div className="edit-delete">
-    //         <a className="plain-button" onClick={event => {
-    //             event.preventDefault();
-    //             console.log("clicked open update");
-    //             this.props.openUpdateForm(event, this.state.note._id)
-    //         }}>edit</a>
-    //         <a className="plain-button" onClick={event => {
-    //             this.props.showModal(event, this.state.note._id)
-    //         }}>delete</a>
-
-    //     </div>
-    //     <div className="note-info-wrapper">
-    //         <h1>{this.state.note.title}</h1>
-    //         <p>{this.state.note.textBody}</p>
-
-    //     </div>
-    // </div>
-
-    // <Modal show={this.props.show}>
-
-    //     <div className="modal-content">
-    //         <p>Are you sure you want to delete this note?</p>
-    //         <div className="modalButtons">
-    //             <button className="blueButton" onClick={this.props.showModal}>No</button>
-    //             <button className="redButton" onClick={this.deleteNote}>Delete</button>
-    //         </div>
-    //     </div>
-
-    // </Modal>
-
-    //     </Fragment >
-    // )
-
     render() {
         return (
             <div className="page-container">
@@ -64,10 +28,10 @@ class SingleNoteCard extends Component {
                 <div className="section-container">
                     <div className="buttons">
                         <Link to={{ pathname: `/edit/${this.props.note._id}` }}>
-                            <p>edit</p>
+                            <button>edit</button>
                         </Link>
                         {/* <Link to={{ pathname: `/delete/${this.props.note._id}` }} > */}
-                            <p onClick={() => this.props.deleteNote(this.props.note._id)}>Delete Note</p>
+                            <button onClick={() => this.props.deleteNote(this.props.note._id)}>Delete Note</button>
                         {/* </Link> */}
                     </div>
                     <div className="single-note">
