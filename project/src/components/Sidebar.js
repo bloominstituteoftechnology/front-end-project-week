9412d00ff9d16f700b4c3eefb791f76b5c457516
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-09-27 15:53:39 -0400

    Clean Up

--- project/src/components/Sidebar.js
+++ project/src/components/Sidebar.js
@@ -2,9 +2,8 @@ import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 
 class Sidebar extends Component {
-    constructor(){
-        super()
-    }
+    
+    
     render(){
         return(
             <div className = "sb-wrap">
