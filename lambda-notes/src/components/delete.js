eebf96b27d21f1d5940758805382497183540312
Akshay Gadkari <akshay.gadkari@gmail.com>
2018-09-27 14:49:35 -0700

    completed modal functionality

--- lambda-notes/src/components/delete.js
+++ lambda-notes/src/components/delete.js
@@ -10,60 +10,29 @@ class Delete extends Component {
     }
 
     componentDidMount() {
-	console.log(this.props);
-	const note = this.props.notes.find(note => `${note.id}` === this.props.match.params.id);
-	if (note) {
-	    this.setState({
-		title: note.title,
-		text: note.text
-	    });
-	} else {
-	    axios
-		.get(`http://localhost:3333/notes/`, {title: this.state.title, text: this.state.text, id: this.state.id})
-		.then(response => {
-		    const myNotes = response.data;
-		    const note = myNotes.find(note => `${note.id}` === this.props.match.params.id);
-		    if (note) {
-			this.setState({
-			    title: note.title,
-			    text: note.text
-			});
-		    }})
-		.catch(err => {
-		    console.log(err);
-		});
-	}
-    }
-
-    handleInputChange = e => {
-	this.setState({ [e.target.name]: e.target.value });
-    };
-
-    handleSubmit = event => {
-	event.preventDefault();
-	// const note = this.props.notes.find(note => `${note.id}` === this.props.match.params.id);
-	const myNote2 = {
-	    id: this.state.id,
-	    title: this.state.title,
-	    text: this.state.text
-	};
-
-	axios
-	    .put(`http://localhost:3333/notes/${this.props.match.params.id}`, myNote2)
-	    .then(response => {
-		this.setState({ myNote2: response.data });
-		console.log(myNote2);
-	    })
-	    .catch(err => {
-		console.log(err);
-	    });
-
-	// this.props.history.push('');
-	window.location.reload();
-	console.log(this.props.match.params.id);
-	console.log(this.state.title);
-	console.log(this.state.text);
-	console.log(myNote2);
+    	console.log(this.props);
+    	const note = this.props.notes.find(note => `${note.id}` === this.props.match.params.id);
+    	if (note) {
+    	    this.setState({
+    		title: note.title,
+    		text: note.text
+    	    });
+    	} else {
+    	    axios
+    		.get(`http://localhost:3333/notes/`, {title: this.state.title, text: this.state.text, id: this.state.id})
+    		.then(response => {
+    		    const myNotes = response.data;
+    		    const note = myNotes.find(note => `${note.id}` === this.props.match.params.id);
+    		    if (note) {
+    			this.setState({
+    			    title: note.title,
+    			    text: note.text
+    			});
+    		    }})
+    		.catch(err => {
+    		    console.log(err);
+    		});
+    	}
     }
 
     deleteNote = e => {
@@ -83,13 +52,11 @@ class Delete extends Component {
 	return (
             <div>
               <div className="oneNoteExpanded">
-		<form onSubmit={this.handleSubmit}>
 		  <h1>{this.state.title}</h1>
 		  <br/>
                   <p>{this.state.text}</p>
 		  <br/>
                   <button onClick={this.deleteNote}>Delete</button>
-		</form>
 	      </div>
 	    </div>
 	);
--- lambda-notes/src/components/expand.js
+++ lambda-notes/src/components/expand.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 // import './note.css';
 import Note from './note.js';
 import Modal from './delete.js';
+// import './expand.css';
+import styled from 'styled-components';
+import axios from 'axios';
 
 import {
     BrowserRouter as Router,
@@ -9,6 +12,27 @@ import {
     Link,
     NavLink
 } from "react-router-dom";
+
+const P = styled.p`
+    left: 150px;
+    top: 150px;
+`;
+
+const Div = styled.div`
+    background: white;
+    width: 250px;
+    margin: 100px auto 0 auto;
+    padding: 10px;
+    top: 150px;
+`;
+
+const Section = styled.section`
+    display: none;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    margin: auto;
+`;
 
 // class Expand extends Component {
 //     constructor(props) {
@@ -49,35 +73,69 @@ import {
 //     }
 // }
 
-const Expand = props => {
-    console.log(props);
-
-    // toggleModal = () => {
-    // 	console.log('clicked!');
-    // 	this.setState({
-    // 	    isOpen: !this.state.isOpen
-    // 	});
-    // };
+class Expand extends Component {
+    constructor(props) {
+	super(props);
+	this.state = {
+	    myNotes: []
+	};
+    }
     
-    return (
-        <div className="oneNote" key={props.id}>
-	  <Link
-	    key={props.location.state.note.id}
-	    to={{pathname:`/notes/${props.location.state.note.id}/edit`,
-	    state:{note: props.location.state.note}}}>
-	    Edit
-	  </Link>
-	  <Link
-	    key={props.location.state.note.id}
-	    to={{pathname:`/notes/${props.location.state.note.id}/delete`,
-	    state:{note: props.location.state.note}}}>
-	    Delete
-	  </Link>
-	  <h2>{props.location.state.note.title}</h2>
-	  <p>{props.location.state.note.text}</p>
-	</div>
-    );
+    openModal = () => {
+    	console.log('clicked!');
+	document.getElementById("modal").style.display = "block";
+	document.getElementById("modal").style.backgroundColor = 'rgba(0,0,3,0.4)';
+    };
+
+    closeModal = () => {
+	document.getElementById("modal").style.display = "none";
+    };
+
+    deleteNote = e => {
+	e.preventDefault();
+	console.log(this.props.match.params.id);
+	axios
+	    .delete(`http://localhost:3333/notes/${this.props.match.params.id}`)
+	    .then(res => {
+		this.setState({ myNote2: res.data });
+	    })
+	    .catch(err => {
+		console.log(err);
+	    });
+	console.log('delete clicked');
+	window.location.reload();
+    };
+    
+    render() {
+	return (
+            <div id='mydiv' className="oneNote" key={this.props.id}>
+	      <Link
+		key={this.props.location.state.note.id}
+		to={{pathname:`/notes/${this.props.location.state.note.id}/edit`,
+		state:{note: this.props.location.state.note}}}>
+		Edit
+	      </Link>
+              <button onClick={this.openModal}>Delete</button>
+              <Section id="modal">
+		<Div>
+		  <P>Do you want to delete this note?</P>
+		  <button  onClick={this.deleteNote}>Delete</button>
+		  <button onClick={this.closeModal}>No</button>
+		</Div>
+              </Section>
+	      <h2>{this.props.location.state.note.title}</h2>
+	      <p>{this.props.location.state.note.text}</p>
+	    </div>
+	);
+    }
 };
+
+//     <Link
+// key={this.props.location.state.note.id}
+// to={{pathname:`/notes/${this.props.location.state.note.id}/delete`,
+//      state:{note: this.props.location.state.note}}}>
+//     Delete
+// </Link>
 
 //<Link key={note.id} to={{pathname:`/notes/${note.id}/edit`, state:{note}}}>Edit</Link>
 //<Link to={`${this.context.location.pathname}/picture/`}>Edit</Link>
