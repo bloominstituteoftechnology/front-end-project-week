ae6468afa915f0ec5b084555ab92800237467c10
Keiran <keirankozlowski@gmail.com>
2018-09-27 13:08:37 -0400

    Minor fix

--- lambda-notes/src/components/SingleNoteCard.js
+++ lambda-notes/src/components/SingleNoteCard.js
@@ -21,60 +21,25 @@ class SingleNoteCard extends Component {
         this.props.fetchSingleNote(_id);
     }
 
-    // return(
-    //     <Fragment>
-    // <div className="single-note">
-    //     <div className="edit-delete">
-    //         <a className="plain-button" onClick={event => {
-    //             event.preventDefault();
-    //             console.log("clicked open update");
-    //             this.props.openUpdateForm(event, this.state.note._id)
-    //         }}>edit</a>
-    //         <a className="plain-button" onClick={event => {
-    //             this.props.showModal(event, this.state.note._id)
-    //         }}>delete</a>
 
-    //     </div>
-    //     <div className="note-info-wrapper">
-    //         <h1>{this.state.note.title}</h1>
-    //         <p>{this.state.note.textBody}</p>
-
-    //     </div>
-    // </div>
-
-    // <Modal show={this.props.show}>
-
-    //     <div className="modal-content">
-    //         <p>Are you sure you want to delete this note?</p>
-    //         <div className="modalButtons">
-    //             <button className="blueButton" onClick={this.props.showModal}>No</button>
-    //             <button className="redButton" onClick={this.deleteNote}>Delete</button>
-    //         </div>
-    //     </div>
-
-    // </Modal>
-           
-    //     </Fragment >
-    // )
-
-render() {
-    console.log(this.props);
-    return (
-        <div className="page-container">
-            <Sidebar />
-            <div className="section-container">
-                <div className="buttons">
-                    <Link to={{ pathname: `/edit/${this.props.note._id}` }}>
-                        <a>edit</a>
-                    </Link>
-                    <a onClick={() => this.props.deleteNote(this.props.note._id)}>Delete Note</a>
+    render() {
+        console.log(this.props);
+        return (
+            <div className="page-container">
+                <Sidebar />
+                <div className="section-container">
+                    <div className="buttons">
+                        <Link to={{ pathname: `/edit/${this.props.note._id}` }}>
+                            <a>edit</a>
+                        </Link>
+                        <a onClick={() => this.props.deleteNote(this.props.note._id)}>Delete Note</a>
+                    </div>
+                    <h1>{this.props.note.title}</h1>
+                    <p>{this.props.note.textBody}</p>
                 </div>
-                <h1>{this.props.note.title}</h1>
-                <p>{this.props.note.textBody}</p>
             </div>
-        </div>
-    )
-}
+        )
+    }
 }
 
 const mapStateToProps = state => {
