39802c953eb171a7fe8111ed9a2bcd7c506077bc
Michael Agard <mdagard@gmail.com>
2018-09-27 12:03:04 -0400

    add style removed tag field

--- lambdanotes/src/components/notes/NoteForm.js
+++ lambdanotes/src/components/notes/NoteForm.js
@@ -3,20 +3,44 @@ import { addNote } from '../../actions';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 
+const NoteFormWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const FormWrapper = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+const TitleInput = styled.input`
+  padding: 5px;
+  width: 394px;
+  height: 47px;
+`;
+const TextBodyInput = styled.textarea`
+  padding: 5px;
+  width: 669px;
+  height: 383px;
+`;
+const SaveButton = styled.input`
+  width: 210px;
+  height: 46px;
+  border: 1px solid grey;
+`;
 class NoteForm extends Component {
   state = {
     tags: '',
     title: '',
     textBody: ''
   };
+
   submitHandler = event => {
     event.preventDefault();
     this.props.addNote(this.state);
-    this.setState({
-      tags: '',
-      title: '',
-      textBody: ''
-    });
+    // this.setState({
+    //   tags: '',
+    //   title: '',
+    //   textBody: ''
+    // });
     setTimeout(() => {
       this.props.history.push('/');
     }, 1000);
@@ -27,30 +51,32 @@ class NoteForm extends Component {
   };
   render() {
     return (
-      <form onSubmit={this.submitHandler}>
-        <input
+      <NoteFormWrapper className="note-form-wrapper">
+        <FormWrapper onSubmit={this.submitHandler}>
+          {/* <input
           type="text"
           value={this.state.tags}
           name="tags"
           onChange={this.changeHandler}
           required
-        />
-        <input
-          type="text"
-          value={this.state.title}
-          name="title"
-          onChange={this.changeHandler}
-          required
-        />
-        <input
-          type="text"
-          value={this.state.textBody}
-          name="textBody"
-          onChange={this.changeHandler}
-          required
-        />
-        <input type="submit" value="Add New Note" />
-      </form>
+        /> */}
+          <TitleInput
+            type="text"
+            value={this.state.title}
+            name="title"
+            onChange={this.changeHandler}
+            required
+          />
+          <TextBodyInput
+            // type="text"
+            value={this.state.textBody}
+            name="textBody"
+            onChange={this.changeHandler}
+            required
+          />
+          <SaveButton type="submit" value="Add New Note" />
+        </FormWrapper>
+      </NoteFormWrapper>
     );
   }
 }
