0de0f37613786549475e2dd25c17bed26a64d057
Sasha <taylork85@gmail.com>
2018-09-29 14:34:14 -0500

    got single note

--- lambda-notes/src/components/NoteView.js
+++ lambda-notes/src/components/NoteView.js
@@ -4,15 +4,12 @@ import NoteForm from './NoteForm';
 import { Link, Route } from "react-router-dom";
 
 const NoteView = (props) => {
-   const note = props.notes.find(
-     note => note._id === props.match.params.noteId);//?
+   const note = props.notes.find(note => note._id === props.match.params.id);//?
 console.log(note);
 
   return (
     <Fragment>
-           {/* <Note note={note} noteTitle={props.note.title} noteBody={props.note.textBody} /> */ }
-          <h2>{props.note.title}</h2> 
-          <h4></h4>
+            <Note note={note} noteTitle={props.note.title} noteBody={props.note.textBody} /> 
 
     </Fragment>
   );
