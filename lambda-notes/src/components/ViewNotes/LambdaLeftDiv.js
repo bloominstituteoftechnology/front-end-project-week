8ad6d9d0afd3e053fc09355f332073bec4478332
Sukhada Gholba <sukhadagholba@gmail.com>
2018-09-29 09:44:32 -0700

    added reset handler

--- lambda-notes/src/components/ViewNotes/LambdaLeftDiv.js
+++ lambda-notes/src/components/ViewNotes/LambdaLeftDiv.js
@@ -7,7 +7,7 @@ const LambdaLeftDiv = (props) => {
 		<div className="left-div">
 		<h1 className="lambda-notes-header">Lambda Notes</h1>
 		<Link  onClick={props.viewNotesHandler}  style={{ textDecoration: 'none' }} to="/notes"><div className="teal-div">View Your Notes</div></Link>
-		<Link  style={{ textDecoration: 'none' }} to="/add"><div className="teal-div">+ Create New Note</div></Link>
+		<Link  onClick={props.createNotesReset} style={{ textDecoration: 'none' }} to="/add"><div className="teal-div">+ Create New Note</div></Link>
 		</div>
 	);
 };
