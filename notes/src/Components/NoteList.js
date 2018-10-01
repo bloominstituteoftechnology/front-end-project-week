92645135e52b76833c36c6db61e16078d9a04cf1
Marious Davis <mlancelot88@gmail.com>
2018-10-01 14:11:30 -0500

    established NoteList for project

--- notes/src/Components/NoteList.js
+++ notes/src/Components/NoteList.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React from "react";
 
-class NoteList extends Compnent {
-    render()
-    return ()
-}
+const NoteList = props => {
+  return (
+    <div className="note-container">
+      <h2>Your Notes:</h2>
+      <div className="all-notes">
+        {props.notes.map(note => (
+          <div className="note">
+            <p className="title">{note.title}</p>
+            <p>{note.text}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default NoteList;
