ce017fb6228c1daf8216bf3a8001c86a9c735e58
lyang9 <lyang9@buffalo.edu>
2018-10-01 14:12:06 -0400

    Called constructor and createdexisting notes

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -3,6 +3,23 @@ import './App.css';
 import NavBar from './Components/NavBar';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [
+        {
+          id: 0,
+          title: 'Cookbook',
+          content: 'How to make pasta'
+        },
+        {
+          id: 1,
+          title: 'Photography',
+          content: 'Cameras used to take pics'
+        }
+      ]
+    };
+  }
   render() {
     return (
       <div className="App">
