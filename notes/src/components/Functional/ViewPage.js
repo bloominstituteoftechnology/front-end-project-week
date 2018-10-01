31421acb8a64a7bc8a6d2374cb23a600743db01c
Matthew Wright <matthewbwright@gmail.com>
2018-10-01 15:06:03 -0700

    sets up modal

--- notes/src/components/Functional/ViewPage.js
+++ notes/src/components/Functional/ViewPage.js
@@ -6,54 +6,83 @@ import Modal from '../Views/Modal';
 import Todo from './Todo';
 
 class ViewPage extends React.Component {
-  state= {
+  state = {
     note: {},
     showModal: false
-  }
+  };
 
   filterProps = () => {
     this.props.notes.forEach(note => {
       if (this.props.match.params.id === note._id) {
-        this.setState({ note: note })
+        this.setState({ note: note });
       }
-    })
+    });
+  };
+
+  componentDidMount() {
+    this.props.getData();
   }
 
-
-componentDidMount() {
-  this.props.getData();
-}
-
-componentDidUpdate(prevProps) {
-  if (this.props.success !== prevProps.success) {
-    this.filterProps();
+  componentDidUpdate(prevProps) {
+    if (this.props.success !== prevProps.success) {
+      this.filterProps();
+    }
   }
-}
 
-toggleModal = event => {
-  event.preventDefault();
-  this.setState(prevState => ({ showModal: !prevState.showModal }));
-}
+  toggleModal = event => {
+    event.preventDefault();
+    this.setState(prevState => ({ showModal: !prevState.showModal }));
+  };
 
-deleteClick = event => {
-  event.preventDefault();
-  this.props.deleteNote(this.state.note._id);
-  this.props.history.goBack();
-}
+  deleteClick = event => {
+    event.preventDefault();
+    this.props.deleteNote(this.state.note._id);
+    this.props.history.goBack();
+  };
 
-editClick = event => {
-  event.preventDefault();
-  this.props.history.push(`/noteform/${this.state.note._id}`)
+  editClick = event => {
+    event.preventDefault();
+    this.props.history.push(`/noteform/${this.state.note._id}`);
+  };
+
+  render() {
+    const description =
+      this.state.note.textBody === undefined
+        ? []
+        : this.state.note.textBody.split('\n');
+    return (
+      <ViewContainer>
+        <Button onClick={this.editClick} className="edit">
+          Edit Note
+        </Button>
+        <Button onClick={this.toggleModal} className="delete">
+          Delete Note
+        </Button>
+        <div>
+          <h3>{this.state.note.title}</h3>
+          {description.map((text, index) => {
+            return <p key={index}>{text}</p>;
+          })}
+        </div>
+        <Todo />
+        <Modal
+          showModal={this.state.showModal}
+          toggleModal={this.state.toggleModal}
+          deleteClick={this.state.deleteClick}
+        />
+      </ViewContainer>
+    );
+  }
 }
 
 const mapStateToProps = state => {
   return {
     notes: state.notes,
     success: state.success
-  }
-}
+  };
+};
 
 export default connect(
   mapStateToProps,
-    { deleteNote, getData }
+  { deleteNote, getData }
 )(ViewPage);
--- notes/src/components/Views/Modal.js
+++ notes/src/components/Views/Modal.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import '../Style/styleModal';
 
 function Modal(props) {
-  return <div />;
+  return (
+    <div>
+      <div>
+        <h5>Are you sure you want to delete this note?</h5>
+        <button onClick={event => props.deleteClick(event)}>Delete</button>
+        <button onClick={props.toggleModal}>No</button>
+      </div>
+    </div>
+  );
 }
 
 export default Modal;
