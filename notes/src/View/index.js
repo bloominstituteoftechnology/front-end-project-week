90987eb7715bc6b738be8beaea9378ed58c5d4b4
A2i <abdulfatah.a.ahmad@gmx.com>
2018-10-01 15:27:24 -0400

    Added NotesList component and mounted on the render() method of App.js

--- notes/src/View/index.js
+++ notes/src/View/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './container/App';
+import App from '../container/App';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
