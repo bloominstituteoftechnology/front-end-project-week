f3c334f16993089fa4bf7120e0f78076fdd75f1c
lyang9 <lyang9@buffalo.edu>
2018-10-01 13:22:05 -0400

    Used CSS in JS for styling

--- lambda-notes/src/Components/NavBar.js
+++ lambda-notes/src/Components/NavBar.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const NavbarContainer = styled.div`
+  border: 1px solid gray;
+  width: 300px;
+  height: 100vh;
+  background: #D3D2D3;
+`;
+
+const NavButton = styled.button`
+  width: 180px;
+  height: 40px;
+  margin-bottom: 25px;
+  border: 1px solid gray;
+  background: #24b8bd;
+  color: white;
+  font-weight: bold;
+  font-size: 14px;
+`;
 
 const NavBar = props => {
   return (
-    <div className='Navbar-container'>
+    <NavbarContainer>
       <h1>Lambda Notes</h1>
-      <button>View Your Notes</button>
-      <button>+ Create New Note</button>
-    </div>
+      <NavButton>View Your Notes</NavButton>
+      <NavButton>+ Create New Note</NavButton>
+    </NavbarContainer>
   );
 }
 
