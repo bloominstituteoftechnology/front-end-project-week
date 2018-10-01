0d3cbd9d893c163411fdb060501c51f35b81c9db
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-10-01 11:24:53 -0500

    basic folder layout with all discrepancies added.

---
+++ notes/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to Notes</h1>
+        <h4>Enter Below</h4>
+        <button>Enter</button>
+      </div>
+    );
+  }
+}
+
+export default App;
