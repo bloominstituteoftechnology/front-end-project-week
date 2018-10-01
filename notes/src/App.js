d6554753f530060eba791888028dd956c5af19c3
A2i <abdulfatah.a.ahmad@gmx.com>
2018-10-01 13:28:27 -0400

    Deleted un-needed files

--- notes/src/App.js
+++ notes/src/App.js
@@ -5,15 +5,13 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <React.Fragment>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">List View</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <hr />
+       
+      </React.Fragment>
     );
   }
 }
