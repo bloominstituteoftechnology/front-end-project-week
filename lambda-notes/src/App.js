f00437bcf6d6ef4d6533c048c9c560a272fea482
Brandon White <bwhite06@stac.edu>
2018-10-01 15:43:34 -0400

    Working on create note page
    -must add more styling later

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -3,18 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 import LambdaNav from './components/LambdaNav.js';
 import HomePage from './components/Homepage/HomePage.js';
+import axios from 'axios';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
 
+  constructor() {
+  super();
+      this.state = {
+        notes: [
 
+        ]
+      };
+    }
+
+
+  componentDidMount(){
+  const URL = 'https://killer-notes.herokuapp.com/note'
+  axios
+  .get(`${URL}/get/all`)
+  .then(response => {
+    this.setState( ()  => ({notes:response.data}));
+  })
+  .catch(err => {
+      console.error('Server Error', err);
+  })
+  }
 
 
 
   render() {
     return (
       <div className="App">
-        <LambdaNav />
-        <HomePage />
+        <Route path= "/" component ={LambdaNav} />
+        <Route exact path = '/' render ={props => <HomePage {...props} notes = {this.state} />}/>
+
       </div>
     );
   }
--- lambda-notes/src/components/Homepage/HomePage.js
+++ lambda-notes/src/components/Homepage/HomePage.js
@@ -1,40 +1,19 @@
 import React from 'react';
 
 class HomePage extends React.Component {
-  constructor() {
-  super();
-      this.state = {
-        notes: [
+  constructor(props){
+super(props);
 
-        ]
-      };
-    }
-
-
-componentDidMount(){
-  this.setState({
-  notes:[{  id:0,
-    Title: "note title",
-    Content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vel maximus elit, at sodales sem. Suspendisse at facilisis lacus. Vivamus orci purus, pellentesque et faucibus sed, cursus ut augue. Phasellus pellentesque orci a lacinia scelerisque. Quisque elementum aliquet mauris, quis facilisis risus commodo a. Donec bibendum nisl nec lacus semper scelerisque. Aliquam rhoncus erat urna. Donec euismod ultricies magna, vitae interdum ante vehicula non. Morbi eget nulla sollicitudin, euismod augue vitae, hendrerit dui. Maecenas mattis elit quis facilisis vulputate. Etiam quis ornare diam.",
-  }]})
-
-}
+  }
 
 
   render() {
     return (
     <div>
-      <h2>Your Notes:</h2>
-      <hr/>
-      {this.state.notes.map(note => {
-        return(
-        <div key ={note.id} className='NoteContainer'>
-        <h3>{note.Title}</h3>
-        <hr/>
-        <p> {note.Content}</p>
-        </div>
-        )
-      })}
+      <form>
+
+        
+      </form>
     </div>
   )
   }
