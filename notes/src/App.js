e4b4357e2f4925415d06210a188b00f1a6b63386
Marious Davis <mlancelot88@gmail.com>
2018-10-01 13:48:41 -0500

    setup React Router and empty Components

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Menu from "./Components/Menu";
+import NewNote from "./Components/NewNote";
+import NoteList from "./Components/NoteList";
+import { Route } from "react-router-dom";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      notes: [],
+      notetitle: "",
+      notebdy: "",
+      id: null
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Menu />
+        <Route
+          exact
+          path="/"
+          render={props => <NoteList {...props} notes={this.state.notes} />}
+        />
       </div>
     );
   }
---
+++ notes/src/Components/NoteList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class NoteList extends Compnent {
+    render()
+    return ()
+}
+
+export default NoteList;
