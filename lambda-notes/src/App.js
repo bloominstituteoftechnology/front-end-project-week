a1bf57afdf36a3e808a3c4890b7af1402b14c578
lyang9 <lyang9@buffalo.edu>
2018-10-01 18:15:09 -0400

    Imported axios and componentDidMount

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,29 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
+import axios from 'axios';
+
 import NavBar from './Components/NavBar';
+import Notes from './Components/Notes';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      notes: [
-        {
-          id: 0,
-          title: 'Cookbook',
-          content: 'How to make pasta'
-        },
-        {
-          id: 1,
-          title: 'Photography',
-          content: 'Cameras used to take pics'
-        }
-      ]
+      notes: []
     };
   }
+
+  componentDidMount() {
+    axios 
+      .get('https://killer-notes.herokuapp.com/note/get/all')
+      .then(response => {
+        this.setState({notes: response.data});
+      })
+      .catch(error => (
+        console.log('Server Error', error)
+      ));
+  }
+
   render() {
     return (
       <div className="App">
         <NavBar />
+        <Route 
+          exact path='/' 
+          render={props => <Notes {...props} notes={this.state.notes} />}
+        />
       </div>
     );
   }
