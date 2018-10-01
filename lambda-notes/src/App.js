bb0dc35c3366624dd2a69b36b7f44abecb89f65a
Brandon White <bwhite06@stac.edu>
2018-10-01 14:12:25 -0400

    Working on Homepage
    -got notes to display off my data

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import LambdaNav from './components/LambdaNav.js'
+import LambdaNav from './components/LambdaNav.js';
+import HomePage from './components/Homepage/HomePage.js';
+
 class App extends Component {
+
+
+
+
+
   render() {
     return (
       <div className="App">
-        <LambdaNav/>
+        <LambdaNav />
+        <HomePage />
       </div>
     );
   }
