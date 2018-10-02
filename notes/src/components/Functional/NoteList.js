eb28dfa48873553629ac4a9751259aa90981d48a
Matthew Wright <matthewbwright@gmail.com>
2018-10-02 09:06:38 -0700

    finishes NoteList.js

--- notes/src/components/Functional/NoteList.js
+++ notes/src/components/Functional/NoteList.js
@@ -5,9 +5,25 @@ import { connect } from 'react-redux';
 import { getData } from '../../Store/actions';
 import Note from '../Views/Note';
 
+import { NotesContainer, NoteAlignment } from '../style/styleNote';
+
 class NoteList extends React.Component {
   componentDidMount() {
     this.props.getData();
+  }
+  render() {
+    return (
+      <NotesContainer>
+        <h2>Notes:</h2>
+        <NoteAlignment>
+          {this.props.notes.map(note => (
+            <Link to={`note._id}`} key={note._id}>
+              <Note note={note} />
+            </Link>
+          ))}
+        </NoteAlignment>
+      </NotesContainer>
+    );
   }
 }
 
