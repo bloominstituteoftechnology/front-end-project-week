4a44722eb426656cc3a5f42ead6ac9bd09549afc
lyang9 <lyang9@buffalo.edu>
2018-10-01 20:25:08 -0400

    Imported Link

--- lambda-notes/src/Components/Notes.js
+++ lambda-notes/src/Components/Notes.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Notes = props => {
   return (
-    <div className='Notes-container'>
+    <div className='notes-container'>
       <h2>Your Notes:</h2>
       <div className='notes-display'>
-        {props.notes.map(note => (
-          <div
-            key={note.id}
-            title={note.title}
-            textBody={note.textBody}
-          />
-        ))}
+        {props.notes.map(note => {
+          return (
+            <Link to={`/note/${note._id}`}>
+              <div 
+                key={note._id}
+              >
+                <h3>{note.title}</h3>
+                <p>{note.textBody}</p>
+              </div>
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
