0a409d92d34f64cfbdea8b011f9534ca779e6dba
lyang9 <lyang9@buffalo.edu>
2018-10-02 01:19:00 -0400

    Changed to using class Component

--- lambda-notes/src/Components/NoteForm.js
+++ lambda-notes/src/Components/NoteForm.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const NoteForm = props => {
-  return (
-    <div>
-      <h2>Create New Note:</h2>
-      <form>
-        <input
-          name='title'
-          placeholder='Note Title'
-          value={props.state.title}
-        />
-        <input
-          name='textBody'
-          placeholder='Note Content'
-          value={props.state.textBody}
-        />
-        <button type='submit'>Save</button>
-      </form>
-    </div>
-  );
+class NoteForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      textBody: ''
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Create New Note:</h2>
+        <form>
+          <input
+            name='title'
+            placeholder='Note Title'
+            value={this.state.title}
+          />
+          <input
+            name='textBody'
+            placeholder='Note Content'
+            value={this.state.textBody}
+          />
+          <button type='submit'>Save</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default NoteForm;
