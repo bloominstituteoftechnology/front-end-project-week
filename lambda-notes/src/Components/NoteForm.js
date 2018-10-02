035da06d8651329eeec2618452cbbf57af3f08e7
lyang9 <lyang9@buffalo.edu>
2018-10-02 00:05:14 -0400

    Created new note form

---
+++ lambda-notes/src/Components/NoteForm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const NoteForm = props => {
+  return (
+    <div>
+      <h2>Create New Note:</h2>
+      <form>
+        <input
+          name='title'
+          placeholder='Note Title'
+          value={props.state.title}
+        />
+        <input
+          name='textBody'
+          placeholder='Note Content'
+          value={props.state.textBody}
+        />
+        <button>Save</button>
+      </form>
+    </div>
+  );
+}
+
+export default NoteForm;
