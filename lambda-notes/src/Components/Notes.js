6bb2b9adbb8597f2a610ccd3964a7398f9277c68
lyang9 <lyang9@buffalo.edu>
2018-10-01 22:57:43 -0400

    Imported styled-components and styling

--- lambda-notes/src/Components/Notes.js
+++ lambda-notes/src/Components/Notes.js
@@ -1,25 +1,79 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NotesContainer = styled.div`
+  border: 1px solid black;
+  margin-left: 300px;
+  margin-top: -610px;
+`;
+
+const Heading = styled.h2`
+  display: flex;
+  margin-left: 30px;
+`;
+
+const NotesDisplay = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+const NoteCard = styled.div`
+  border: 1px solid gray;
+  width: 225px;
+  height: 200px;
+  margin-left: 60px;
+  margin-top: 30px;
+`;
+
+const NoteTitle = styled.h3`
+  border-bottom: 1px solid gray;
+  display: flex;
+  margin-left: 10px;
+  margin-right: 10px;
+  text-decoration: none;
+  color: black;
+`;
+
+const NoteBody = styled.p`
+  display: flex;
+  margin-left: 10px;
+  margin-right: 10px;
+  text-decoration: none;
+  color: gray;
+`;
 
 const Notes = props => {
   return (
-    <div className='notes-container'>
-      <h2>Your Notes:</h2>
-      <div className='notes-display'>
+    <NotesContainer>
+      <Heading>Your Notes:</Heading>
+      <NotesDisplay>
         {props.notes.map(note => {
           return (
             <Link to={`/note/${note._id}`}>
-              <div 
+              <NoteCard
                 key={note._id}
+                style={{ textDecoration: "none" }}
               >
-                <h3>{note.title}</h3>
-                <p>{note.textBody}</p>
-              </div>
+                <NoteTitle>
+                  {note.title.length >= 15
+                    ? note.title.substring(0, 15) + '...'
+                    : note.title
+                  }
+                </NoteTitle>
+                <NoteBody>
+                  {note.textBody.length >= 100
+                    ? note.textBody.substring(0, 100) + '...'
+                    : note.title
+                  }
+                </NoteBody>
+              </NoteCard>
             </Link>
           );
         })}
-      </div>
-    </div>
+      </NotesDisplay>
+    </NotesContainer>
   );
 }
 
