43b634b44a45b7b1104f4062abbc3190a7d5a6a7
MichelleOk <umbralcompendium@gmail.com>
2018-10-03 01:06:46 -0600

    w.i.p

--- lambdanotes/src/components/Notes.js
+++ lambdanotes/src/components/Notes.js
@@ -3,7 +3,7 @@ import React from 'react';
 
  const Notes = props => {
      console.log("note props check", props);
-     const note = props.notesList.find(note => note.id === props.match.params.noteId);
+     const note = props.note.notesList.find(note => note.id === props.match.params.noteId);
 
      function clickHandler(url) {
          props.history.push(url);
