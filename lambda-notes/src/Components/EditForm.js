20639b2d00ee67e6427f59ec83c430e2062d0dcd
lyang9 <lyang9@buffalo.edu>
2018-10-03 16:34:08 -0400

    Refactored updateNote axios.put

--- lambda-notes/src/Components/EditForm.js
+++ lambda-notes/src/Components/EditForm.js
@@ -22,39 +22,59 @@ class EditForm extends Component {
       title: this.state.title,
       textBody: this.state.textBody
     };
-    this.props.updateNote(updatedNote);
-    this.props.history.push('/');
-  }
-
-  updatedNote = note => {
-    const id = this.props.match.params.id;
     axios
-      .put(`https://killer-notes.herokuapp.com/note/edit/${id}`, note)
+      .put(`https://killer-notes.herokuapp.com/note/edit/${this.state.id}`, updatedNote)
       .then(response => {
-        this.setState({notes: response.data});
+        this.setState({notes: response.data, updatedNote});
       })
-      .catch(error => (
+      .catch(error => {
         console.log('Server Error', error)
-      ));
-  }  
+      });
+      this.props.history.push('/')
+  };
+
+  // updateNote = event => {
+  //   event.preventDefault();
+  //   const updatedNote = {
+  //     id: this.state.id,
+  //     title: this.state.title,
+  //     textBody: this.state.textBody
+  //   };
+  //   this.props.updateNote(updatedNote);
+  //   this.props.history.push('/');
+  // }
+
+  // updatedNote = note => {
+  //   const id = this.props.match.params.id;
+  //   axios
+  //     .put(`https://killer-notes.herokuapp.com/note/edit/${id}`, note)
+  //     .then(response => {
+  //       this.setState({notes: response.data});
+  //     })
+  //     .catch(error => (
+  //       console.log('Server Error', error)
+  //     ));
+  // }  
 
   render() {
     return (
       <div className='edit-container'>
         <h2>Edit Note:</h2>
-        <input 
-          name='title'
-          placeholder='Note Title'
-          value={this.state.title}
-          onChange={this.handleInputChange}
-        />
-        <input
-          name='textBody'
-          placeholder='Note Content'
-          value={this.state.textBody}
-          onChange={this.handleInputChange}
-        />
-        <button onClick={this.updateNote}>Update</button>
+        <form>
+          <input 
+            name='title'
+            placeholder='Note Title'
+            value={this.state.title}
+            onChange={this.handleInputChange}
+          />
+          <input
+            name='textBody'
+            placeholder='Note Content'
+            value={this.state.textBody}
+            onChange={this.handleInputChange}
+          />
+          <button onClick={this.updateNote}>Update</button>
+        </form>
       </div>
     );
   }
