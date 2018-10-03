060b776d3f546c3fb2c7b7c1141de95e4954d357
lyang9 <lyang9@buffalo.edu>
2018-10-03 14:53:17 -0400

    Deleted ModalHeader on import

--- lambda-notes/src/Components/DeleteModal.js
+++ lambda-notes/src/Components/DeleteModal.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Button, Modal, ModalBody, ModalFooter } from 'reactstrap';
 
 class DeleteModal extends Component {
   constructor(props) {
