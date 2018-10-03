8eff621d42e5ee80549b81b6f75dccd2975ee0a5
A2i <abdulfatah.a.ahmad@gmx.com>
2018-10-03 15:38:35 -0400

    Linked Delete & NoteDeleteModal.js
    #COMMENT
    
    Wrapped the delete button in NotePage.js with <Link> with route pointing to NoteDeleteModal

--- notes/src/components/NoteDeleteModal.js
+++ notes/src/components/NoteDeleteModal.js
@@ -8,7 +8,25 @@ class NoteDeleteModal extends Component {
 
     componentDidMount() {
         console.log(this.props.match.params);
+        axios
+      .put(`https://killer-notes.herokuapp.com/note/delete/${this.props.match.params.id}`, newNote)
+        .then(res => {
+        
+          this.setState({
+            title: res.data.title,
+            textBody: res.data.textBody
+          });
+        })
+        .catch(err => {
+          console.error(`Error: ${err}`)
+        })
+
+    this.setState({
+      title: '',
+      textBody: ''
+    });
     }
+
     render() { 
         return (  
         <h1>Delete</h1>
