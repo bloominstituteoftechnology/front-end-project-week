03b21fb3749c3273bcdd810ca8304af437030321
Ray Lomeli <raylom96@gmail.com>
2018-10-03 18:13:57 -0500

    updated styling for Noteview

--- my-app/src/components/NoteView.js
+++ my-app/src/components/NoteView.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import axios from "axios";
+import styled from "styled-components";
 
 class NoteView extends React.Component {
   constructor(props) {
@@ -32,19 +33,46 @@ class NoteView extends React.Component {
   };
 
   render() {
+    console.log(" render is running ");
     return (
-      <Fragment>
-        <div>
-          <div>
-            <button>Edit</button>
-            <button onSubmit={this.handleDelete}>Delete</button>
-          </div>
+      <NoteViewWrap>
+        <LinkWrap>
+          <Link>Edit</Link>
+          <Link onSubmit={this.handleDelete}>Delete</Link>
+        </LinkWrap>
+        <ContentWrap>
           <h1>{this.state.note.title}</h1>
           <p>{this.state.note.textBody}</p>
-        </div>
-      </Fragment>
+        </ContentWrap>
+      </NoteViewWrap>
     );
   }
 }
+
+const NoteViewWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 10px 5%;
+  height: 500px;
+  margin-left: 275px;
+`;
+
+const LinkWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+const Link = styled.a`
+  padding: 6px 1.5%;
+  font-weight: bold;
+  font-size: 0.85rem;
+  color: black;
+`;
+
+const ContentWrap = styled.div`
+  margin-top: 20px;
+  line-height: 24px;
+`;
 
 export default NoteView;
