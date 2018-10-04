1631f802d24f386708f8122f8465ba741fa9f684
JustinSHong <jhong2009@gmail.com>
2018-10-03 22:55:40 -0700

    fixed material import issues

--- LambdaNotes/src/components/SingleTodo.js
+++ LambdaNotes/src/components/SingleTodo.js
@@ -6,9 +6,9 @@ import { toggleTodo, deleteTodo, archiveTodo } from "../actions/index";
 // material components
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import IconMenu from "material-ui/IconMenu";
+import Menu from "@material-ui/core/Menu";
 import MenuItem from "material-ui/MenuItem";
-import IconButton from "material-ui/IconButton";
+import IconButton from "@material-ui/core/IconButton";
 import MoreVertIcon from "material-ui/svg-icons/navigation/more-vert";
 // styles
 import "../styles/SingleTodos.css";
@@ -60,7 +60,7 @@ class SingleTodo extends React.Component {
                         )}
                     </p>
                     <div className="row SingleTodo_footer">
-                        <IconMenu
+                        <Menu
                             iconButtonElement={
                                 <IconButton>
                                     <MoreVertIcon className="SingleTodo__menu" />
@@ -94,7 +94,7 @@ class SingleTodo extends React.Component {
                                     this.props.handleClick("Archived item");
                                 }}
                             />
-                        </IconMenu>
+                        </Menu>
                     </div>
                 </CardContent>
             </Card>
