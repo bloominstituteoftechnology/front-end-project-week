ec92ea6197d89a4779c107edc3639fc340e6b3d0
verydecent <wnjhwng@gmail.com>
2018-10-05 11:58:25 -0700

    Individul note request testing, setting to state to use the Note data.

--- notes-app/src/components/NotePage.js
+++ notes-app/src/components/NotePage.js
@@ -1,37 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import '../App.css';
 
-function NotePage(props) {
-    console.log(props)
-    const note = props.notes.find(note => note._id === props.match.params.id);
-    console.log(note._id)
-    // deleteHandler = id => {
+class NotePage extends Component {
+    constructor() {
+        super();
+        this.state = {
+            notes: [],
+            note: {
+                id: '',
+                title: '',
+                textBody: '',
+            }
+        }
+    }
+    componentDidMount() {
+        const noteId = this.props.match.params.id;
+        this.getRequestById(noteId)
+    }
+    getRequestById = (id) => {
+        axios.get(`https://killer-notes.herokuapp.com/note/get/${id}`)
+        .then(response => console.log(response))
+    }
 
-    // }
-    // function deleteHandler() {
-    //     props.deleteNote(note._id)
-    //     props.history.push('/notes')
-    // }
-    return (
-        <div className="notepage-div">
-            <div className="edit-delete">
-                <Link to="/noteform">
-                    <div onClick={() => 
-                        props.goToUpdateNoteForm(note._id)}> edit
-                    </div> 
-                </Link>
-                &nbsp;&nbsp;
-                <Link to="/notes">
-                    <div onClick={() => {
-                        props.deleteNote(note._id)}}> delete
-                    </div>
-                </Link>
-            </div>   
-            <h1 className="notepage-header">{note.title}</h1>
-            <p className="notepage-paragraph">{note.textBody}</p>
-        </div>
-    )
+    render() {
+        return (
+            <div className="notepage-div">
+                <div className="edit-delete">
+                    <Link to="/noteform">
+                        <div onClick={() => 
+                            console.log('edit clicking')}> edit
+                        </div> 
+                    </Link>
+                    &nbsp;&nbsp;
+                    <Link to="/notes">
+                        <div onClick={() => {
+                            console.log('delete clicking')}}> delete
+                        </div>
+                    </Link>
+                </div>   
+                <h1 className="notepage-header">{this.state.note.title}</h1>
+                <p className="notepage-paragraph">{this.state.note.textBody}</p>
+            </div>
+        )
+    }
 }
+
 
 export default NotePage;
