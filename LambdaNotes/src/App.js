9fa41e34d5a03ba2755bc2dfca3f028cf5967976
JustinSHong <jhong2009@gmail.com>
2018-10-06 23:24:53 -0700

    removed unneeded divs and bootstrap rows

--- LambdaNotes/src/App.js
+++ LambdaNotes/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Route } from "react-router-dom";
 // styles
 import "./styles/App.css";
-import "./bootstrap/css/bootstrap.css";
 import "./styles/animation.css";
 // components
 import CreateTodoForm from "./components/CreateTodoForm";
@@ -12,29 +11,21 @@ import EditTodoForm from "./components/EditTodoForm";
 import VisibilityTodos from "./components/VisibilityTodos";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App fade">
-        <div className="row App__container">
-          <div className="col-sm-12 col-md-12 col-lg-4 ControlPanel">
-            <ControlPanel />
-          </div>
-          <div className="col-md-8">
-            <Route exact path="/" component={VisibilityTodos} />
-          </div>
-          <div className="col-md-8 CreateTodoForm">
-            <Route exact path="/addTodo" component={CreateTodoForm} />
-          </div>
-          <div className="col-md-8">
-            <Route exact path="/todo/:id" component={Todo} />
-          </div>
-          <div className="col-md-8 CreateTodoForm">
-            <Route exact path="/editTodo" component={EditTodoForm} />
-          </div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App fade">
+                <div className="ControlPanel">
+                    <ControlPanel />
+                </div>
+                <div className="TodoContainer">
+                    <Route exact path="/" component={VisibilityTodos} />
+                    <Route exact path="/addTodo" component={CreateTodoForm} />
+                    <Route exact path="/todo/:id" component={Todo} />
+                    <Route exact path="/editTodo" component={EditTodoForm} />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
