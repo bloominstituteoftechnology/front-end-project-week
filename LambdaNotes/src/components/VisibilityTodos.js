cdb4afc5baf42a85c4e1634da2d18ae451b9e315
JustinSHong <jhong2009@gmail.com>
2018-10-07 15:15:31 -0700

    added SHOW_ARCHIVED_TODOS case to filter todos whose text matches search text given

--- LambdaNotes/src/components/VisibilityTodos.js
+++ LambdaNotes/src/components/VisibilityTodos.js
@@ -5,36 +5,42 @@ import { VisibilityFilters } from "../actions/index.js";
 import Todos from "./Todos";
 
 // container component that will filter todos list according to store's filter status
-const getVisibleTodos = (todos, filter) => {
-	switch (filter) {
-		// no filter applied
-		case "":
-		case VisibilityFilters.SHOW_ALL_TODOS:
-			return todos;
-		// only render finished tasks (isComplete: true)
-		case VisibilityFilters.SHOW_COMPLETED_TODOS:
-			return todos.filter(todo => {
-				return todo.isComplete;
-			});
-		// only return unfinished tasks (isComplete: false)
-		case VisibilityFilters.SHOW_ACTIVE_TODOS:
-			return todos.filter(todo => {
-				return !todo.isComplete;
-			});
-		case VisibilityFilters.SHOW_ARCHIVED_TODOS:
-			return todos.filter(todo => {
-				return todo.archive;
-			});
-		default:
-			return todos;
-	}
+const getVisibleTodos = (todos, filter, target) => {
+    switch (filter) {
+        // no filter applied
+        case "":
+        case VisibilityFilters.SHOW_ALL_TODOS:
+            return todos;
+        // only return finished tasks (isComplete: true)
+        case VisibilityFilters.SHOW_COMPLETED_TODOS:
+            return todos.filter(todo => {
+                return todo.isComplete;
+            });
+        // only return unfinished tasks (isComplete: false)
+        case VisibilityFilters.SHOW_ACTIVE_TODOS:
+            return todos.filter(todo => {
+                return !todo.isComplete;
+            });
+        // only return archived tasks
+        case VisibilityFilters.SHOW_ARCHIVED_TODOS:
+            return todos.filter(todo => {
+                return todo.archive;
+            });
+        // only show searched tasks
+        case VisibilityFilters.SHOW_SEARCHED_TODOS:
+            return todos.filter(todo => {
+                return todo.text === target;
+            });
+        default:
+            return todos;
+    }
 };
 
 const mapStateToProps = state => {
-	// filtered todos are set as Todo component's props
-	return {
-		todos: getVisibleTodos(state.todos, state.filter)
-	};
+    // filtered todos are set as Todo component's props
+    return {
+        todos: getVisibleTodos(state.todos, state.filter, state.searchText)
+    };
 };
 
 const VisibilityTodos = connect(mapStateToProps)(Todos);
