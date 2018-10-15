e21ec6f004039d2d7ee61d4c1d89b68c11fd9b14
David Loveday <davemech@yahoo.com>
2018-10-15 16:35:08 -0700

    more updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,10 @@ const Home = () => {
 
     return (
           <div>
-          {localStorage.getItem("token")? (<div className="main-wrapper"> {`Welcome, ${localStorage.getItem("name").split(" ")[0]}! `}</div>) : (
+          {localStorage.getItem("token") ? ( 
+          <div className="main-wrapper"> <div className="loggged-on">
+          {`Welcome back, ${localStorage.getItem("name").split(" ")[0]}! You are currently logged on. `}
+          </div></div>) : (
            <div className="main-wrapper">
             <h3>Welcome to Lambda Notes</h3>
              <p>Please Sign In or Sign Up to begin making notes.</p>            
@@ -14,5 +17,4 @@ const Home = () => {
           }</div>
     )
 }
-
 export default Home;
