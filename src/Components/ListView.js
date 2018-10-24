d48d79ad1a6bbf7f336cac22647295c3c09b26d4
Jameson13B <jamesonbrown@neolivity.com>
2018-10-24 13:26:36 -0600

    Switched to comparing with the length of the array of notes

--- src/Components/ListView.js
+++ src/Components/ListView.js
@@ -13,7 +13,7 @@ const ListView = (props) => {
                 </div>    
             </div>
             <ul className='note-list'>
-                {props.notes[0] === null ? props.notes.map(note => {
+                {props.notes.length === 0 ? props.notes.map(note => {
                     return (
                         <Link to={`/view/${note.id}`} key={Math.random()} className='note'>
                             <h1>{note.title.substring(0, 20)}</h1>
