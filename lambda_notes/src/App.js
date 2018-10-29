3875520f50e0ffa0381f54c9756f2f5ffffb607e
Steven O'Neill <oneillsteven957@gmail.com>
2018-10-29 12:53:14 -0500

    Started working on app js

--- lambda_notes/src/App.js
+++ lambda_notes/src/App.js
@@ -3,6 +3,75 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      notes: [],
+      title: '',
+      text: '',
+      activeNote: null
+    }
+  }
+
+  componentDidMount() {
+    const endpoint = 'https://fe-notes.herokuapp.com/note/get/all';
+
+    axios
+      .get(endpoint)
+      .then(response => {
+        this.setState({notes: response.data });
+      })
+      .catch(error => {
+        console.log('Error: ', error);
+      })
+  }
+
+  getItemById = id => {
+    axios
+      .get(`https://fe-notes.herokuapp.com/note/get/id/${id}`)
+      .then(response => this.setState({ activeNote: response.data }))
+      .catch(error => console.log('Error: ', error));
+  };
+
+  handleTitleChange = event => {
+    this.setState({ title: event.target.value });
+  };
+
+  handleTextChange = event => {
+    this.setState({ text: event.target.value });
+  };
+
+  handleSubmit = (e) => {
+    const newNote = {
+      tile: this.state.title,
+      text: this.state.text,
+    };
+    this.setState({ title: '', text: ''});
+    axios
+      .post('https://fe-notes.herokuapp.com/note/create', newNote)
+      .then(response => {
+        this.setState({
+          title: '',
+          text: '',
+          notes: response.data
+        });
+      })
+      .catch(error => {
+        console.log('Error: ', error);
+      });
+  };
+
+  deleteItem = (event, id) => {
+    event.preventDefault();
+    axios
+      .delete(`https://fe-notes.herokuapp.com/note/get/id/${id}`)
+      .then(response => {
+        this.setState({ notes: response.data });
+      })
+      .catch(error => console.log(error));
+  };
+
+
   render() {
     return (
       <div className="App">
