9814f86ec33988c1ed717d56bf284b5b319b82f3
TRIF3X <thewizardofcrypto@gmail.com>
2018-10-29 14:56:31 -0500

    made nav links functional

--- todo-project/src/CreateNote/form.js
+++ todo-project/src/CreateNote/form.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './form.css'
 
 function form(props) {
     return (
--- todo-project/src/NavContainer/index.js
+++ todo-project/src/NavContainer/index.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import './index.css'
+
 
 export default class NavContainer extends Component {
   render() {
     return (
       <div className='noteButtonContainer'>
             <p className='titleNav'>Lambda <br />Notes</p>
-          <p className='button' >View Your Notes</p>
-          <Route exact path='/create-note' ><p className='button' >+Create New Note</p></Route>
+            <div className='navDiv'> 
+            <NavLink exact to="/" className='button'>View Your Notes</NavLink>  
+          <NavLink exact to="/create-note" className='button'>+Create New Note</NavLink>   
+          </div>
       </div>
     )
   }
