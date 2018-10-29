47322376b8edecf055d0fde30d164ec416626080
Carlos Lantigua <clantigua954@gmail.com>
2018-10-29 15:49:01 -0400

    finally got the add note to work

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -10,11 +10,7 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			notes: [],
-			newNote: {
-				noteTitle: '',
-				noteContent: ''
-			}
+			notes: []
 		};
 	}
 
@@ -28,46 +24,12 @@ class App extends React.Component {
 			.catch((err) => console.log(err));
 	}
 
-	changeHandler = (e) => {
-		e.preventDefault();
-		this.setState({
-			[e.target.name]: e.target.value
-		});
-	};
-	saveNote = () => {
-		// if (this.state.noteTitle === 0 || this.state.noteContent === 0) {
-		// 	alert('Please fill out both fields');
-		// } else {
-		axios
-			.post('https://killer-notes.herokuapp.com/note/create', this.state.newNote)
-			.then((res) => {
-				this.setState({
-					notes: res.data,
-					noteTitle: '',
-					noteContent: ''
-				});
-			})
-			.catch((err) => console.log(err));
-		// }
-	};
-
 	render() {
 		return (
 			<StyledContainer>
 				<Route path="/" render={(props) => <SidebarContainer {...props} />} />
 				<Route exact path="/" render={(props) => <NoteContainer {...props} notes={this.state.notes} />} />
-				<Route
-					exact
-					path="/create-new-note"
-					render={(props) => (
-						<NewNote
-							{...props}
-							value={this.state.newNote}
-							changeHandler={this.changeHandler}
-							saveNote={this.saveNote}
-						/>
-					)}
-				/>
+				<Route exact path="/create-new-note" render={(props) => <NewNote {...props} />} />
 			</StyledContainer>
 		);
 	}
--- lambda-notes/src/Components/Maincontent/NewNote.js
+++ lambda-notes/src/Components/Maincontent/NewNote.js
@@ -1,28 +1,66 @@
 import React from 'react';
+import axios from 'axios';
 
-const NewNote = (props) => {
-	return (
-		<div>
-			<h1>Create New Note:</h1>
-			<form action="submit">
-				<input
-					type="text"
-					name="noteTitle"
-					value={props.noteTitle}
-					onChange={props.changeHandler}
-					placeholder="Note Title"
-				/>
-				<input
-					type="text"
-					name="noteContent"
-					value={props.noteTitle}
-					onChange={props.changeHandler}
-					placeholder="Note Title"
-				/>
-				<button>Save</button>
-			</form>
-		</div>
-	);
-};
+class NewNote extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			noteTitle: '',
+			noteContent: ''
+		};
+	}
+
+	changeHandler = (e) => {
+		e.preventDefault();
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	};
+
+	saveNote = (e) => {
+		const { noteTitle, noteContent } = this.state;
+		e.preventDefault();
+		if (this.state.noteTitle === '' || this.state.noteContent === '') {
+			alert('Please fill out both fields');
+		} else {
+			axios
+				.post('https://killer-notes.herokuapp.com/note/create', { title: noteTitle, textBody: noteContent })
+				.then((res) => {
+					console.log(res);
+					this.setState({
+						notes: res.data,
+						noteTitle: '',
+						noteContent: ''
+					});
+				})
+				.catch((err) => console.log(err));
+		}
+	};
+
+	render() {
+		return (
+			<div>
+				<h1>Create New Note:</h1>
+				<form onSubmit={this.saveNote}>
+					<input
+						type="text"
+						name="noteTitle"
+						value={this.state.noteTitle}
+						onChange={this.changeHandler}
+						placeholder="Note Title"
+					/>
+					<input
+						type="text"
+						name="noteContent"
+						value={this.state.noteContent}
+						onChange={this.changeHandler}
+						placeholder="Note Title"
+					/>
+					<button>Save</button>
+				</form>
+			</div>
+		);
+	}
+}
 
 export default NewNote;
