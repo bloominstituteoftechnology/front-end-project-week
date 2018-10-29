299b04f58db6aaa6740554bca60d3d52f14de173
bummings <eabummings@gmail.com>
2018-10-29 13:58:59 -0400

    wiring up axios get request to API

--- lamba-notes/src/App.js
+++ lamba-notes/src/App.js
@@ -4,6 +4,22 @@ import axios from "axios";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://fe-notes.herokuapp.com/note/get/all")
+      .then(response => this.setState({ notes: response.data }))
+      .then(console.log(this.state.notes))
+      .catch(error => console.log(error));
+  }
+
   render() {
     return (
       <div className="App">
