7c7516a28c8e3b29f7eb6feabf2b15451b3e2c1a
Drewsup123c <drewsup123c@gmail.com>
2018-10-29 14:05:51 -0600

    changed notecard div to a Link tag

--- notes/src/App.js
+++ notes/src/App.js
@@ -59,7 +59,7 @@ class App extends Component {
         onChangeHandler ={this.onChangeHandler}
         />} />
         {/* Edit Card Route */}
-        <Route path='/edit' render={() => <EditNote /> } />
+        <Route path='/edit/$id' render={() => <EditNote notes={this.state.notes} /> } />
       </div>
     );
   }
--- notes/src/components/NoteCard.js
+++ notes/src/components/NoteCard.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NoteCard = props => {
     return(
-        <div className="note-card">
+        
+        <Link to={`/edit/${props.id}`} className="note-card">
             <h2 className="card-title">{props.title}</h2>
             <p>{props.textBody}</p>
-        </div>
+        </Link>
+        
     );
 }
 
