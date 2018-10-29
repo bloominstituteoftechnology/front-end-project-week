56d9c7ecc678dcc52f725b36722fbe8addbe0d70
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-10-29 09:44:35 -0700

    side menu

---
+++ lambda-notes/src/components/Menu.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <div className="menu">
+      <h1>
+        Lambda <br />
+        Notes
+      </h1>
+      <button className="menu-btn">View Your Notes</button>
+      <button className="menu-btn">+ Create New Note</button>
+    </div>
+  );
+};
+
+export default Menu;
