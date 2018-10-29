8211546e1d4dc85f7d12e84fc036c2b9953d4c1b
KHollobaugh <khollobaugh@gmail.com>
2018-10-29 11:55:34 -0400

    set up index.js and app.js

--- lambda-notes/src/components/App.js
+++ lambda-notes/src/components/App.js
@@ -1,28 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { getNotes } from '../actions';
+import { connect } from 'react-redux';
+import Note from './Note';
+import NoteForm from './NoteForm';
+import NotesView from './NotesView';
+import SideBar from './SideBar';
+import { Route } from 'react-router-dom';
+
 
 class App extends Component {
+  componentDidMount() {
+    this.props.getNotes();
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <SideBar />
+
+      <Route
+      exact path = '/'
+      render = {props =>(
+        <NotesView {...props}
+        notes = {this.props.smurfs} />
+      )}/>
+
+      <Route
+      path = 'newnote'
+      render = {props =>(
+        <NoteForm {...props}
+        notes = {this.props.notes} />
+      )}/>
+
+      <Route
+      path = 'viewnote'
+      render = {props =>(
+        <Note {...props}
+        notes = {this.props.notes}/>
+      )}/>
+
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  const { noteReducer } = state;
+  return {
+    notes: noteReducer.notes,
+    gettingNotes: noteReducer.gettingNotes,
+    error: noteReducer.error
+  }
+}
+
+export default connect(mapStateToProps, { getNotes })(App);
--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router} from 'react-router-dom';
+import './css/index.css';
+import App from './components/App';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+<Provider store={store}>
+<Router>
+<App />    
+</Router>
+</Provider>
+, document.getElementById('root'));
+
+
