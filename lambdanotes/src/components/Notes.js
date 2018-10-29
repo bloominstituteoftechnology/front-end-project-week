770e6328c5e70a7d680e7e85ff72f098b3000044
Matthew Wright <matthewbwright@gmail.com>
2018-10-29 12:36:30 -0700

    adds styling for the note list

--- lambdanotes/src/components/Notes.js
+++ lambdanotes/src/components/Notes.js
@@ -5,15 +5,22 @@ import './ComponentStyle.css';
 const Notes = props => {
   return (
     <Fragment>
-      <div>Your Notes:</div>
-      {props.notes.map(note => {
-        return (
-          <div key={note.id}>
-            <div>{note.title}</div>
-            <div>{note.textBody}</div>
-          </div>
-        );
-      })}
+      <div className="sidebar">
+        <h1 className="sidebar-title">Lambda Notes</h1>
+        <button className="button">View Your Notes</button>
+        <button className="button">+ Create New Note</button>
+      </div>
+      <div className="note-container">
+        <h1 className="notes-title">Your Notes:</h1>
+        {props.notes.map(note => {
+          return (
+            <div key={note.id} className="note-card">
+              <h1 className="note-title">{note.title}</h1>
+              <p className="note-text">{note.textBody}</p>
+            </div>
+          );
+        })}
+      </div>
     </Fragment>
   );
 };
--- lambdanotes/src/components/NotesList.js
+++ lambdanotes/src/components/NotesList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import Notes from './Note';
+import Notes from './Notes';
 
 import './ComponentStyle.css';
 
