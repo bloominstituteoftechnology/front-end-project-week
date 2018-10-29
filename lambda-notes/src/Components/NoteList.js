d1bce73a234c4d8f1cb6cafd7e3bbf69d6884eaf
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-10-29 13:08:20 -0500

    Re did my formatting

---
+++ lambda-notes/src/Components/NoteList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+
+const NoteList = props => {
+  return (
+    <div className="note-container">
+      <h2>Your Notes:</h2>
+      <div className="all-notes">
+        {props.notes.map(note => (
+          <Link className="notelink" to={`/notes/${note.id}`}>
+            <div className="note">
+              <div className="note-contents">
+                <p className="title">{note.title}</p>
+                <p>{note.content}</p>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default NoteList;
