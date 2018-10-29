0c57face8813d9b3b82f764bfe414738b15a09c1
Stublink <chrisrey95@gmail.com>
2018-10-29 12:38:07 -0400

    Show list of notes + local api

--- notes/src/components/App.js
+++ notes/src/components/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getNotes } from '../actions';
 
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.getNotes();
+  }
+
   render() {
     return (
-      <div>
-        Notes
+      <div className={'appContainer'}>
+        <div className={'toolBar'} ></div>
+        <div className={'Notes'}>
+        {
+          !this.props.notes.length 
+            ? (<h2>Loading Notes ...</h2>)
+            : this.props.notes.map(note => {
+                return (<div key={note.key}>{note.textBody}</div>);
+              })
+        }
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  const { notes } = state;
+  return { notes };
+}
+
+export default connect(mapStateToProps, { getNotes })(App);
---
+++ server.js
@@ -0,0 +1,109 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const port = 3333;
+
+const server = express();
+server.use(bodyParser.json());
+server.use(cors());
+
+const sendUserError = (msg, res) => {
+  res.status(422);
+  res.json({ Error: msg });
+  return;
+};
+
+let notes = [
+    {
+        "tags": ["tag", "otherTag"],
+        "title": "Note Title",
+        "textBody": "Note Body 1",
+        "_id": "0",
+    },
+    {
+      "tags": ["tag", "otherTag"],
+      "title": "Note Title",
+      "textBody": "Note Body 2",
+      "_id": "1",
+    },
+];
+server.get('/notes', (req, res) => {
+  res.json(notes);
+});
+let smurfId = 2;
+
+server.get('/notes/:id', (req, res) => {
+  const id = req.params.id;
+  findById(id)
+    .then(data => {
+      res.status(200).json(data)
+    })
+    .catch(err => console.log(err))
+})
+
+findById = (id) => {
+  return new Promise((res, rej) => {
+    let index = notes.findIndex(smurf => Number(smurf.id) === Number(id))
+    res(notes[index]);
+  })
+}
+
+
+server.post('/notes', (req, res) => {
+  const { name, age, height } = req.body;
+  const newSmurf = { name, age, height, id: smurfId };
+  if (!name || !age || !height) {
+    return sendUserError(
+      'Ya gone did smurfed! Name/Age/Height are all required to create a smurf in the smurf DB.',
+      res
+    );
+  }
+  const findSmurfByName = smurf => {
+    return smurf.name === name;
+  };
+  if (notes.find(findSmurfByName)) {
+    return sendUserError(
+      `Ya gone did smurfed! ${name} already exists in the smurf DB.`,
+      res
+    );
+  }
+
+  notes.push(newSmurf);
+  smurfId++;
+  res.json(notes);
+});
+
+server.put('/notes/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, age, height } = req.body;
+  const findSmurfById = smurf => {
+    return smurf.id == id;
+  };
+  const foundSmurf = notes.find(findSmurfById);
+  if (!foundSmurf) {
+    return sendUserError('No Smurf found by that ID', res);
+  } else {
+    if (name) foundSmurf.name = name;
+    if (age) foundSmurf.age = age;
+    if (height) foundSmurf.height = height;
+    res.json(notes);
+  }
+});
+
+server.delete('/notes/:id', (req, res) => {
+  const { id } = req.params;
+  const foundSmurf = notes.find(smurf => smurf.id == id);
+
+  if (foundSmurf) {
+    const SmurfRemoved = { ...foundSmurf };
+    notes = notes.filter(smurf => smurf.id != id);
+    res.status(200).json(notes);
+  } else {
+    sendUserError('No smurf by that ID exists in the smurf DB', res);
+  }
+});
+
+server.listen(port, err => {
+  if (err) console.log(err);
+  console.log(`server is listening on port ${port}`);
+});
