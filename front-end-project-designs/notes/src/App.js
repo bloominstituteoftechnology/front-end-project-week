149209f95d5d02e6c467ed4d0df61c5ebdb6fcfa
Lajawanti <lajawanti.dhake@gmail.com>
2018-10-29 12:58:06 -0400

    axios.get Done

--- front-end-project-designs/notes/src/App.js
+++ front-end-project-designs/notes/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import NotesSideBar from './Components/NotesSideBar';
+import NotesDisplay from './Components/NotesDisplay';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className = "app-div">
         
           <h1>Hello...............</h1>
           <NotesSideBar />
+          <NotesDisplay />
         
       </div>
     );
---
+++ front-end-project-designs/notes/src/Components/NotesDisplay.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import axios from 'axios';
+import SingleNote from './SingleNote';
+
+
+
+
+class NotesDisplay extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            notes : []
+        }
+    }//Constructor end..
+
+    //ComponentDidMount to fetch data from API
+    componentDidMount() {
+        axios
+            .get('https://killer-notes.herokuapp.com/note/get/all')
+            .then(response => {
+                    console.log(response.data);
+                    this.setState({ notes : response.data})
+             })
+            .catch(error => console.log(error))
+    }
+
+    render() {
+        console.log("State from API .. ", this.state.notes);
+        return (
+            <div>
+                <h1>NotesDisplay</h1>
+
+            </div>
+        )
+    }
+}
+
+export default NotesDisplay;
