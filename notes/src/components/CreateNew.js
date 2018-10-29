3a6a04691d80aa0229e90889fdc8c646d3ec098c
Drewsup123c <drewsup123c@gmail.com>
2018-10-29 11:52:11 -0600

    styled create new note page

--- notes/src/components/CreateNew.js
+++ notes/src/components/CreateNew.js
@@ -5,8 +5,22 @@ const CreateNew = (props) => {
         <div className="create-new">
             <h2>Create New Note:</h2>
             <form>
-                <input name="title" type="text" placeholder="Note Title" />
-                <input name="content" type="text" placeholder="Note Content" />
+                <input 
+                className="form-title"
+                name="title" 
+                type="text" 
+                placeholder="Note Title" 
+                />
+                <input 
+                className="form-content"
+                name="content" 
+                type="text" 
+                placeholder="Note Content" 
+                />
+                <input 
+                className="form-save"
+                type="submit" 
+                value="Save" />
             </form>
         </div>
     );
