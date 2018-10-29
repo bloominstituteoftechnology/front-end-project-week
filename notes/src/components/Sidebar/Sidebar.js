649ab01ef567d844b9485f0307b828244cc93750
Unknown <zachary.arney@gmail.com>
2018-10-29 17:28:22 -0500

    Added links in Sidebar to List and CreateNote

--- notes/src/components/Sidebar/Sidebar.js
+++ notes/src/components/Sidebar/Sidebar.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Sidebar() {
   return (
     <div>
       <h1>Lambda Notes</h1>
+      <Link to="/">
+        <div>View Your Notes</div>
+      </Link>
+      <Link to="/create">
+        <div>+ Create New Note</div>
+      </Link>
     </div>
   );
 }
