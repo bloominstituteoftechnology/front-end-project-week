62b7660c21e56d729ce0057935558522c73c71ba
sean-one <basement365@gmail.com>
2018-10-29 16:05:46 -0700

    working on main layout

---
+++ notes/src/components/HeadNav/HeadNav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//styling
+import './HeadNav.css';
+
+const HeadNav = () => {
+    return (
+        <div className='headNav'>
+            <h1>Lambda Notes</h1>
+        </div>
+    );
+}
+ 
+export default HeadNav;
---
+++ notes/src/components/MainView/MainView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//styling
+import './MainView.css';
+
+const MainView = () => {
+    return (
+        <div className='mainView'>
+            <h1>Main Section</h1>
+        </div>
+    );
+}
+ 
+export default MainView;
