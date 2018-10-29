47ba55c47c4742100331220f180d8bd4c2e143e0
AM_Myrick <amyrick.820@gmail.com>
2018-10-29 16:39:12 -0400

    added create functionality

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from "react-router-dom";
 import Navigation from "./Views/Navigation";
 import AllNotes from "./Views/AllNotes";
+import NewNote from "./Components/NewNote"
 import './App.css';
 
 export default class App extends Component {
@@ -10,6 +11,7 @@ export default class App extends Component {
       <div className="App">
         <Route path="/" component={Navigation} />
         <Route exact path="/" component={AllNotes} />
+        <Route exact path="/add-note" component={NewNote} />
       </div>
     )
   }
