3adea847ee891ab403fe804f2238853117ed5f82
Brandon Strellis <bstrellis@gmail.com>
2018-10-29 14:24:25 -0400

    Added POST req to AddNoteForm. Successfully posted req.

--- lambda-notes/src/components/AddNoteForm.js
+++ lambda-notes/src/components/AddNoteForm.js
@@ -1,21 +1,37 @@
 import React from 'react';
+import axios from 'axios';
 
 class AddNoteForm extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             titleText: '',
             bodyText: ''
         }
     }
 
+    changeHandler = event => {
+        event.preventDefault();
+        this.setState({ [event.target.name]: event.target.value});
+    }
+
+    saveAndRedirect = () => {
+        axios
+            .post('https://fe-notes.herokuapp.com/note/create', 
+                {title: this.state.titleText, 
+                textBody: this.state.bodyText})
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
+        
+    }
+
     render() {
         return(
             <div className='addNoteForm'>
                 <h2>Create New Note:</h2>
-                <input placeholder='Note Title' name='titleText' value={this.state.titleText} />
-                <input placeholder='Note Content' name='bodyText' value={this.state.bodyText} />
-                <div>Save</div>
+                <input placeholder='Note Title' name='titleText' value={this.state.titleText} onChange={this.changeHandler}/>
+                <input placeholder='Note Content' name='bodyText' value={this.state.bodyText} onChange={this.changeHandler}/>
+                <div onClick={this.saveAndRedirect}>Save</div>
             </div>
         )
     }
