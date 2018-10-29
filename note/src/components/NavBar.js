279a62294a3e51430ac723040a0d007da379e97a
Yanrong Xu <xuyanrong@icloud.com>
2018-10-29 14:19:08 -0400

    style the navBar

--- note/src/components/NavBar.js
+++ note/src/components/NavBar.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const NavBarContainer = styled.div
+`
+    border: 1px solid gray;
+    width: 20%;
+    height: 100vh;
+    background: #D3D2D3;
+`;
+
+const NavButton = styled.button
+`
+
+    width: 90%;
+    height: 40px;
+    margin-bottom: 25px;
+    border: 1px solid gray;
+    background: #24B8BD;
+    color: white;
+    font-weight: bold;
+    font-size: 14px;
+`;
 
 const NavBar = props => {
     return (
-        <div className='navBar-container'>
+        <NavBarContainer>
             <h1>Lambda Notes</h1>
-            <button>View your Notes</button>
-            <button>+ Create New Note</button>
-        </div>
+            <NavButton>View your Notes</NavButton>
+            <NavButton>+ Create New Note</NavButton>
+        </NavBarContainer>
     )
 }
 
