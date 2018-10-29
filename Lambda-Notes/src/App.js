20455f62e029d38535c176c6db1b28a6b9f112ef
unknown <brandongrych@gmail.com>
2018-10-29 12:12:57 -0500

    basic component tree setup

--- Lambda-Notes/src/App.js
+++ Lambda-Notes/src/App.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = 
+    {
+    }
+  }
+  componentDidMount(){
+    
+  }
+
   render() {
     return (
       <div className="App">
-      <h1>Notes</h1>
+        <h1>Notes</h1>
       </div>
     );
   }
