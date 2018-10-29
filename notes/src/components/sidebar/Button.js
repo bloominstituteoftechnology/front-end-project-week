582cc4db85beeaab6e78ebdc6771ef730157b0b7
Drewsup123c <drewsup123c@gmail.com>
2018-10-29 10:33:23 -0600

    finished styled button

--- notes/src/components/sidebar/Button.js
+++ notes/src/components/sidebar/Button.js
@@ -8,6 +8,13 @@ background-color : #24B8BD;
 border : 2px solid #88A6A7;
 height:50px;
 width: 150px;
+:hover{
+    cursor:pointer;
+    background-color:lightblue;
+    border: 3px solid #24B8BD;
+    transition:0.3s ease;
+    
+}
 `
 
 export default Button;
