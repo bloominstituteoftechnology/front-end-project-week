72474564efb7f79319024057eb2a46faea79e738
Chiboba <briangfelix@gmail.com>
2018-10-29 15:14:31 -0700

    More Styling

--- notes/src/components/CreateNote.js
+++ notes/src/components/CreateNote.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 const CreateNote = (props) => {
     return (
+        <div className ='createNoteContainer'>
         <form onSubmit={props.postHandler}>
             <h2>Create New Note:</h2>
-            <input onChange={props.changeHandler} name='title' placeholder='Note Title'></input>
-            <input onChange={props.changeHandler} name='textBody' placeholder='Note Content'></input>
+            <input className ='titleInput' onChange={props.changeHandler} name='title' placeholder='Note Title'></input>
+            <input className ='textBodyInput' onChange={props.changeHandler} name='textBody' placeholder='Note Content'></input>
             <button onClick={props.postHandler}>Save</button>
         </form>
+        </div>
     );
 }
 
