38e0b9131ca7bfbd6884dd177d23dbf9bb406228
Lee Len Formento <leebatef@gmail.com>
2018-10-29 13:08:10 -0400

    Reducers

---
+++ notes/src/reducers/index.js
@@ -0,0 +1,27 @@
+import {FETCHING, FETCHED, SAVING, SAVED, ERROR} from '../actions';
+
+const initialState = {
+    notes: [],
+    fetching: false,
+    fetched: false,
+    saving: false,
+    saved: false,
+    error: null
+}
+
+export const notesReducer = ( state = initialState, action ) => {
+    switch(action.type) {
+        case FETCHING:
+        return {...state, fetching: true};
+        case FETCHED:
+        return{...state, fetching: false, fetched: true, notes: action.payload};
+        case SAVING:
+        return {...state, saving: true};
+        case SAVED:
+        return{...state, saving: false, saved: true};
+        case ERROR:
+        return {...state, fetching: false, saving: false,error: action.payload};
+        default:
+        return state;
+    }
+}
