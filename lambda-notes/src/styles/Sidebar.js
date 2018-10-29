05fff44a81f20c1fb59849e79f05bd24c5f90f78
Axolotl <samuelmachat@gmail.com>
2018-10-29 15:47:18 -0400

    style sidebar

--- lambda-notes/src/styles/Sidebar.js
+++ lambda-notes/src/styles/Sidebar.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  background: grey;
+  background: #d2d1d2;
   height: 100%;
+  min-width: 220px;
+  max-width: 220px;
   display: flex;
   flex-direction: column;
-  padding: 20px;
+  padding: 15px;
 
   > * {
     margin-bottom: 30px;
@@ -13,9 +15,21 @@ export const Container = styled.div`
 `
 
 export const Title = styled.h1`
-  font-size: 3rem;
+  font-weight: bold;
+  font-size: 3.4rem;
+  line-height: 3.2rem;
 `
 
 export const Button = styled.button`
-  
+  background: #00bbbe;
+  padding: 13px;
+  border: 0px;
+  color: white;
+  font-size: 1.6rem;
+  font-weight: bold;
+  cursor: pointer;
+
+  &:hover {
+    background: #007f82;
+  }
 `
