66e2d5973044620147a599dc8286a80031d260fb
unknown <brandongrych@gmail.com>
2018-10-29 14:15:04 -0500

    everything in working state, H1 from app.js displaying added styled components

--- Lambda-Notes/src/index.js
+++ Lambda-Notes/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import { BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 
 ReactDOM.render(
-<Router>
+<BrowserRouter>
     <App />
-</Router>, document.getElementById('root'));
+</BrowserRouter>, document.getElementById('root'));
