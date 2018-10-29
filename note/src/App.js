ba55265ece41bb7b6116443051ef34dfcbe8ab1e
Yanrong Xu <xuyanrong@icloud.com>
2018-10-29 14:26:06 -0400

    call constructor

--- note/src/App.js
+++ note/src/App.js
@@ -3,6 +3,13 @@ import './App.css';
 import NavBar from './components/NavBar'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      note: []
+    }
+  }
+
   render() {
     return (
       <div className="App">
