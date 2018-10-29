818b00eca522beabfa14dd75a7112d3277b8ca09
Drewsup123c <drewsup123c@gmail.com>
2018-10-29 10:03:42 -0600

    set up basic component tree, and wrapped App with router

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 
+
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = 
+    {
+
+    }
+  }
+
+  componentDidMount(){
+    //will fetch data from the api
+  }
+
   render() {
     return (
       <div className="App">
