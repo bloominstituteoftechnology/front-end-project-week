8d45bcc6c04ad2d920eaf95e19ca6f361ddc8065
Brandon Strellis <bstrellis@gmail.com>
2018-10-29 14:11:19 -0400

    Added Links on LeftBar

--- lambda-notes/src/components/LeftBar.js
+++ lambda-notes/src/components/LeftBar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './LeftBar.css';
+import { Link } from 'react-router-dom';
 
 const LeftBar = props => {
     return (
         <div className='leftBar'>
             <h1>Lambda</h1>
             <h1>Notes</h1>
-            <div>View Your Notes</div>
-            <div>+ Create New Note</div>
+            <Link to={'/'}>View Your Notes</Link>
+            <Link to={'/AddNoteForm'}>+ Create New Note</Link>
         </div>
     )
 }
