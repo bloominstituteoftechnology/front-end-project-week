53f717aa1a40f5b0d2b34cc2196e33b17548db57
Brandon Strellis <bstrellis@gmail.com>
2018-10-30 12:57:52 -0400

    Added html for modal to ExpandedNote.

--- lambda-notes/src/components/ExpandedNote.js
+++ lambda-notes/src/components/ExpandedNote.js
@@ -10,6 +10,15 @@ const ExpandedNote = props => {
             </div>
             <h1>{props.expandedNote.title}</h1>
             <p>{props.expandedNote.text}</p>
+            <div className='modal'>
+                <div className='modalMenu'>
+                    <p>Are you sure you want to delete this?</p>
+                    <div className='modalButtons'>
+                        <button className='button deleteButton'>Delete</button>
+                        <button className='button retreatButton'>Retreat</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
