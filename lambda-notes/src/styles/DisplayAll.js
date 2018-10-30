03036c9f8fff51031fdeacecb3899ba622c6a354
Axolotl <samuelmachat@gmail.com>
2018-10-30 15:54:43 -0400

    cleanup

--- lambda-notes/src/styles/DisplayAll.js
+++ lambda-notes/src/styles/DisplayAll.js
@@ -103,38 +103,3 @@ export const StyledCheckbox = styled.span`
       }
     `};
 `
-
-
-// -webkit-appearance: none;
-// background-color: #fafafa;
-// border: 1px solid #cacece;
-// box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05),
-//   inset 0px -15px 10px -12px rgba(0, 0, 0, 0.05);
-// padding: 9px;
-// border-radius: 3px;
-// display: inline-block;
-// position: relative;
-
-// &:active, &:checked:active {
-
-//   box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05),
-//     inset 0px 1px 3px rgba(0, 0, 0, 0.1);
-// }
-
-// $:checked {
-//   background-color: #e9ecee;
-//   border: 1px solid #adb8c0;
-//   box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05),
-//     inset 0px -15px 10px -12px rgba(0, 0, 0, 0.05),
-//     inset 15px 10px -12px rgba(255, 255, 255, 0.1);
-//   color: #99a1a7;
-// }
-
-// &:checked:after {
-//   content: '\2714';
-//   font-size: 14px;
-//   position: absolute;
-//   top: 0px;
-//   left: 3px;
-//   color: #99a1a7;
-// }
