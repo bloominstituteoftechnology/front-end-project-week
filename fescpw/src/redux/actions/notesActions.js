8535b063a887eab9944ed8e17b14130891d3f234
Tristen Neu <tristenneu@Tristens-iMac.local>
2018-10-30 15:34:04 -0500

    Changed Route Handlers

--- fescpw/src/redux/actions/notesActions.js
+++ fescpw/src/redux/actions/notesActions.js
@@ -21,7 +21,7 @@ export const noteFetch = noteID => dispatch => {
     dispatch({ type: NOTE_FETCH_IN_PROGRESS });
     axios
         // .get(`https://killer-notes.herokuapp.com/note/get/${noteID}`)
-        .get(`https://lambda-notes-v01.herokuapp.com/notes/${noteID}`)
+        .get(`https://clear-39769.herokuapp.com/notes/${noteID}`)
         .then((response) => {
             dispatch({
                 type: NOTE_FETCH_SUCCESS,
@@ -40,7 +40,7 @@ export const notesFetch = () => dispatch => {
     dispatch({ type: NOTES_FETCH_IN_PROGRESS });
     axios
         // .get("https://killer-notes.herokuapp.com/note/get/all")
-        .get("https://lambda-notes-v01.herokuapp.com/notes")
+        .get("https://clear-39769.herokuapp.com/notes")
         .then((response) => {
             dispatch({
                 type: NOTES_FETCH_SUCCESS,
@@ -59,7 +59,7 @@ export const notesCreate = note => dispatch => {
     dispatch({ type: NOTES_CREATE_IN_PROGRESS });
     axios
         // .post("https://killer-notes.herokuapp.com/note/create", note)
-        .post("https://lambda-notes-v01.herokuapp.com/notes", note)
+        .post("https://clear-39769.herokuapp.com/notes", note)
         .then((response) => {
             dispatch({
                 type: NOTES_CREATE_SUCCESS,
@@ -85,7 +85,7 @@ export const notesEdit = note => dispatch => {
     dispatch({ type: NOTES_EDIT_IN_PROGRESS });
     axios
         // .put(`https://killer-notes.herokuapp.com/note/edit/${note._id}`, note)
-        .put(`https://lambda-notes-v01.herokuapp.com/notes/${note.id}`, note)
+        .put(`https://clear-39769.herokuapp.com/notes/${note.id}`, note)
         .then((response) => {
             dispatch({
                 type: NOTES_EDIT_SUCCESS,
@@ -104,7 +104,7 @@ export const notesRemove = noteID => dispatch => {
     dispatch({ type: NOTES_REMOVE_IN_PROGRESS });
     axios
         // .delete(`https://killer-notes.herokuapp.com/note/delete/${noteID}`)
-        .delete(`https://lambda-notes-v01.herokuapp.com/notes/${noteID}`)
+        .delete(`https://clear-39769.herokuapp.com/notes/${noteID}`)
         .then((response) => {
             dispatch({
                 type: NOTES_REMOVE_SUCCESS,
