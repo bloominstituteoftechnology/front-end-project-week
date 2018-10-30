39a5219ea1d08b5b252e3b4fa50ae1f8f84be0ba
unknown <brandongrych@gmail.com>
2018-10-30 16:01:49 -0500

    style looking more like project files, used colordropper to color match

--- Lambda-Notes/src/Components/Sidebar/Button.js
+++ Lambda-Notes/src/Components/Sidebar/Button.js
@@ -5,7 +5,7 @@ const Button = styled.button `
 background-color : #24B8BD;
 border : 2px solid #88A6A7;
 height:50px;
-width: 150px;
+width: 220px;
 margin-bottom: 20px;
 :hover{
     cursor:pointer;
