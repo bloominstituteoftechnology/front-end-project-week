0c4e62882dbd31262cff6f430fda97ed68faf5ae
sean-one <basement365@gmail.com>
2018-10-29 22:19:14 -0700

    styled nav and header sidebar section

--- notes/src/components/HeadNav/HeadNav.js
+++ notes/src/components/HeadNav/HeadNav.js
@@ -7,6 +7,10 @@ const HeadNav = () => {
     return (
         <div className='headNav'>
             <h1>Lambda Notes</h1>
+            <div className='navSection'>
+                <div className='navButton'>View Your Notes</div>
+                <div className='navButton'>+ Create New Note</div>
+            </div>
         </div>
     );
 }
