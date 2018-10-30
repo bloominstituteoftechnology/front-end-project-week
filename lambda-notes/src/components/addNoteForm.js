7ba6e7968064c5e1a74430a00728a743e3b7f31f
Brad Mortensen <b.mort86@gmail.com>
2018-10-30 10:24:33 -0600

    going back to working on functinality of edit

--- lambda-notes/src/components/addNoteForm.js
+++ lambda-notes/src/components/addNoteForm.js
@@ -6,18 +6,20 @@ const AddNoteForm = props => {
     <form onSubmit={props.clickHandler} className='form'>
       <h3 className='add-header'>Create New Note:</h3>
       <input 
+        className='title'
         type='text'
         name='title' 
         value={props.data.title}
         placeholder='Note Title'
         onChange={props.inputChange}/>
       <input 
+        className='body'
         type='text'
         name='textBody' 
         value={props.data.textBody}
         placeholder='Note Content'
         onChange={props.inputChange}/>
-      <button onClick={props.clickHandler}>Submit</button>
+      <button className='save' onClick={props.clickHandler}>Save</button>
     </form> 
   );
 }
