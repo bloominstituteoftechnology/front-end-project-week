d7e49aa639cb30fe772a92dd9936bc1f37da2527
Matthew Wright <matthewbwright@gmail.com>
2018-10-30 11:07:05 -0700

    adds styling for individual note

--- lambdanotes/src/components/Note.js
+++ lambdanotes/src/components/Note.js
@@ -1,35 +1,38 @@
 import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import './ComponentStyle.css';
 
 function Note(props) {
   console.log(props);
   const note = props.notesList.find(
-    note => note._id === props.match.params.noteId
+    note => note._id == props.match.params.noteId
   );
   console.log(note);
   function handleDelete() {
     props.handleDeleteNote(note._id);
-    props.history.push('/note');
+    props.history.push(`/note`);
   }
 
   return (
     <Fragment>
-      <button
-        onClick={event => {
-          event.preventDefault();
-          props.goToUpdateNoteForm(note);
-        }}
-      >
-        Edit
-      </button>
-      <button onClick={handleDelete}>Delete</button>
-      <div>
-        <Link to={`/${note._id}`}>
-          <h1>{note.title}</h1>
-          <h4>({note.textBody})</h4>
-        </Link>
+      <div className="single-note">
+        <button
+          className="note-button"
+          onClick={event => {
+            event.preventDefault();
+            props.goToUpdateNoteForm(note);
+          }}
+        >
+          Edit
+        </button>
+        <button className="note-button" onClick={handleDelete}>
+          Delete
+        </button>
+        <div>
+          <h1 className="note-name">{note.title}</h1>
+          <h4 className="note-body">{note.textBody}</h4>
+        </div>
       </div>
     </Fragment>
   );
