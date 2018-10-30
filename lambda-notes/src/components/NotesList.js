f2763c6a14e8a71f235912df612761e83560be66
Brandon Strellis <bstrellis@gmail.com>
2018-10-30 17:04:26 -0400

    Finished rough css for notes list view.

--- lambda-notes/src/components/NotesList.js
+++ lambda-notes/src/components/NotesList.js
@@ -5,13 +5,16 @@ import './NotesList.css';
 const NotesList = props => {
     return (
         <div className='notesList'>
-            {props.notes.map(note => <Note key={note._id} 
-                                           id={note._id}
-                                           title={note.title} 
-                                           text={note.textBody}
-                                           passThisNote={props.passThisNote}
-                                           />)
-            }
+            <h2>Your Notes:</h2>
+            <div className='notesDisplay'>
+                {props.notes.map(note => <Note key={note._id} 
+                                            id={note._id}
+                                            title={note.title} 
+                                            text={note.textBody}
+                                            passThisNote={props.passThisNote}
+                                            />)
+                }
+            </div>
         </div>
     )
 }
