194f95a754688da4c84d48d817408438dd74d557
Daniel Ferrer <danfrrr13@gmail.com>
2018-10-30 11:22:15 -0400

    added some styling to the default view.

--- notes/src/components/Nav/Nav.js
+++ notes/src/components/Nav/Nav.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Nav.css';
 
 const Nav = props => {
     return (
         <div className="nav">
             <h1>Lambda Notes</h1>
-            <button>View Your Notes</button>
-            <button>+ Create New Note</button>
+            <button className="button">View Your Notes</button>
+            <button className="button">+ Create New Note</button>
         </div>
     )
 };
--- notes/src/components/Note/Note.js
+++ notes/src/components/Note/Note.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Note.css';
 
 const Note = props => {
     return (
-        <div className="Note">
+        <div className="note">
             <h3>{props.title}</h3>
             <p>{props.content}</p>
         </div>
