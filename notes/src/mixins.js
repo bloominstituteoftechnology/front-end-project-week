9d5d067b8d925215aaa579bd499722b3507c648c
Unknown <zachary.arney@gmail.com>
2018-10-30 23:32:39 -0500

    Mixins for styling

--- notes/src/mixins.js
+++ notes/src/mixins.js
@@ -1,11 +1,17 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const ViewContainer = styled.div`
   flex-grow: 1;
   height: 100vh;
   background: white;
   padding: 10px 5px;
+  border: 1px solid rgb(152, 152, 152);
   overflow-y: scroll;
+`;
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
 `;
 
 export const Button = styled.div`
@@ -18,4 +24,16 @@ export const Button = styled.div`
   padding: 15px 0;
   border: 1px solid gray;
   margin: 0 0 21px;
+`;
+
+export const StyledH3 = styled.h3`
+  font-size: 2.3rem;
+  color: rgb(74, 73, 74);
+`;
+
+export const StyledP = styled.p`
+  text-align: left;
+  font-size: 1.6rem;
+  color: rgb(0, 0, 0);
+  margin-top: 9px;
 `;
