30cec98a482dd220c4919aa6ffae633d735c1103
mparedes003 <michelle.g.paredes@gmail.com>
2018-10-31 11:56:13 -0700

    Added logout method to remove token data for localStorage and logout. Added logout li button. Minimally Styled

--- lambda_times_sprint/src/App.js
+++ lambda_times_sprint/src/App.js
@@ -46,16 +46,16 @@ class App extends Component {
     tags: [],
     title: '',
     textBody: '',
-};
+  };
 
-componentDidMount() {
-  this.fetchNotes();
-}
+  componentDidMount() {
+    this.fetchNotes();
+  }
 
-// allows us to get all the notes data from the API
-fetchNotes = ()=> {
-  // axios.get('https://killer-notes.herokuapp.com/note/get/all')
-  axios.get('http://localhost:9900/api/notes')
+  // allows us to get all the notes data from the API
+  fetchNotes = () => {
+    // axios.get('https://killer-notes.herokuapp.com/note/get/all')
+    axios.get('http://localhost:9900/api/notes')
       .then(response => {
         // console.log(response);
         this.setState({ tags: response.data });
@@ -63,91 +63,96 @@ fetchNotes = ()=> {
       .catch(err => {
         console.log(err);
       });
-}
+  }
 
-// allows us to add title and textBody info for new note created on state
+  // allows us to add title and textBody info for new note created on state
   handleInputChange = event => {
-      this.setState({ [event.target.name]: event.target.value });
-    }
+    this.setState({ [event.target.name]: event.target.value });
+  }
 
-// allows us to create a new note and post it to the API
+  // allows us to create a new note and post it to the API
   handleAddNewNote = event => {
     console.log('firing')
 
-    const note = {title: this.state.title, textBody: this.state.textBody};
+    const note = { title: this.state.title, textBody: this.state.textBody };
 
     // axios.post('https://killer-notes.herokuapp.com/note/create', note)
     axios.post('http://localhost:9900/api/notes', note)
-      .then(response => this.setState({ note: response.data,
-                                        title: response.data.title,
-                                        textBody: response.data.textBody
-                                      }))
+      .then(response => this.setState({
+        note: response.data,
+        title: response.data.title,
+        textBody: response.data.textBody
+      }))
   }
 
   render() {
     // console.log("PROCESS: ", process.env);
     if (this.isAuthenticated()) {
-    return (
-      <div className="App">
-        <header className="sidebar">
-          <h1 className="App-title">Lambda Notes</h1>
-          <ul className="navbar">
-          <li>
-            <NavLink exact to="/notes" activeClassName="activeNavButton">
-              View Your Notes
+      return (
+        <div className="App">
+          <header className="sidebar">
+            <h1 className="App-title">Lambda Notes</h1>
+            <ul className="navbar">
+              <li>
+                <NavLink exact to="/notes" activeClassName="activeNavButton">
+                  View Your Notes
             </NavLink>
-          </li>
-          <li>
-            <NavLink to="/note-form" activeClassName="activeNavButton">
-              +Create New Note
+              </li>
+              <li>
+                <NavLink to="/note-form" activeClassName="activeNavButton">
+                  +Create New Note
             </NavLink></li>
-        </ul>
-        </header>
-        <div className="notes-section">
-        <Route
-          exact
-          path="/notes"
-          render={props => (
-            <NotesList {...props} notesList={this.state.tags} />
-          )}
-        />
-        <Route
-          path="/note-form"
-          render={props => (
-          <NoteForm 
-            {...props} 
-            
-            title={this.state.title}
-            textBody={this.state.textBody}
-            handleAddNewNote={this.handleAddNewNote}
-            handleInputChange={this.handleInputChange} 
-          /> 
-          )}
-        />
-        <Route path="/notes/:id" render={props => 
-          <Note 
-            {...props}
-            fetchNotes={this.fetchNotes}
-          />
-          } 
-        />
+              <li className="logout"
+                onClick={this.logout}>
+                Log Out
+              </li>
+            </ul>
+          </header>
+          <div className="notes-section">
+            <Route
+              exact
+              path="/notes"
+              render={props => (
+                <NotesList {...props} notesList={this.state.tags} />
+              )}
+            />
+            <Route
+              path="/note-form"
+              render={props => (
+                <NoteForm
+                  {...props}
+
+                  title={this.state.title}
+                  textBody={this.state.textBody}
+                  handleAddNewNote={this.handleAddNewNote}
+                  handleInputChange={this.handleInputChange}
+                />
+              )}
+            />
+            <Route path="/notes/:id" render={props =>
+              <Note
+                {...props}
+                fetchNotes={this.fetchNotes}
+              />
+            }
+            />
+
+          </div>
 
         </div>
-
-      </div>
-    );
-  } else {
-    return (
-      <div className="please-login">
-      <h1 className="access-notes">Please Log In to Access Notes</h1>
-      <div className="login"
-          onClick={function() {
-            lock.show();
-          }}
+      );
+    } else {
+      return (
+        <div className="please-login">
+          <h1 className="access-notes">Please Log In to Access Notes</h1>
+          <div className="login"
+            onClick={function () {
+              lock.show();
+            }}
           >Log In</div>
-      </div>
-    )
-  }
+        </div>
+      )
+    }
   }
 
   // Check if authenticated
@@ -156,6 +161,15 @@ fetchNotes = ()=> {
     // Access Token's expiry time
     let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
     return new Date().getTime() < expiresAt;
+  }
+
+  // remove token data from localStorage
+  logout() {
+    // Clear Access Token and ID Token from local storage
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('expires_at');
+    window.location.reload();
+
   }
 }
 
