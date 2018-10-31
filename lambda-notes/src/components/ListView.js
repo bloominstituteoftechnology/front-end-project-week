f3dd27a68d5394290ea148fa7e730a367588639e
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-10-31 14:07:39 -0700

    filtered search

--- lambda-notes/src/components/ListView.js
+++ lambda-notes/src/components/ListView.js
@@ -2,20 +2,46 @@ import React from "react";
 import "../App.css";
 import NoteCard from "./NoteCard";
 
-const ListView = props => {
-  if (!props.notes.length) {
-    return <h2>Loading</h2>;
+class ListView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: ""
+    };
   }
-  return (
-    <div className="list-view">
-      <h1>Your Notes:</h1>
-      <div className="note-card-container">
-        {props.notes.map(note => {
-          return <NoteCard note={note} key={note._id} />;
-        })}
+
+  updateSearch = e => {
+    this.setState({search: e.target.value});
+  };
+
+  render() {
+    let filteredNotes = this.props.notes.filter(note => {
+      return (
+        note.title.toLowerCase().includes(this.state.search.toLowerCase()) ||
+        note.textBody.toLowerCase().includes(this.state.search.toLowerCase())
+      );
+    });
+
+    if (!this.props.notes.length) {
+      return <h2>Loading</h2>;
+    }
+    return (
+      <div className="list-view">
+        <input
+          type="search"
+          className="search"
+          onChange={this.updateSearch}
+          value={this.state.search}
+        />
+        <h1>Your Notes:</h1>
+        <div className="note-card-container">
+          {filteredNotes.map(note => {
+            return <NoteCard note={note} key={note._id} />;
+          })}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ListView;
