0566474ab8dcbd8f778e43b179ad09ce407f126e
Csillag61 <csillagok@gmail.com>
2018-10-31 15:35:57 -0400

    LogIn auth updated

--- src/components/Authentication/Authenticate.js
+++ src/components/Authentication/Authenticate.js
@@ -1,25 +1,53 @@
 import React from "react";
 import Login from "../Login/Login";
 
-const Authenticate = App =>
+const Authenticate = (App) =>
   class extends React.Component {
-    constructor(props) {
-      super(props);
+    constructor() {
+      super()
       this.state = {
-        loggedIn: false
-      };
-    }
-    componentDidMount() {
-      if (!localStorage.getItem("user")) {
-        this.setState({ loggedIn: false });
-      } else {
-        this.setState({ loggedIn: true });
+        logedIn: false,
+        userName: '',
+        password: ''
       }
     }
-    render() {
-      if (this.state.loggedIn) return <App />;
-        return <div className="loginPage"><Login /></div>;
-    }
-  };
 
+    componentDidMount() {
+      const userName = localStorage.getItem('userName')
+      if (this.state.logedIn === false && userName) {
+        this.setState({ logedIn: true })
+      }
+    }
+
+    loginUser(event) {
+      event.preventDefault()
+      console.log('Storing')
+      const userName = this.state.userName
+      const password = this.state.password
+      localStorage.setItem('userName', userName)
+      window.location.reload()
+      console.log(localStorage.getItem('userName'))
+    }
+
+    onUserInputChnage(event) {
+      this.setState({ userName: event.target.value })
+    }
+
+    onPasswordInputChange(event) {
+      this.setState({ password: event.target.value })
+    }
+
+    render() {
+      if (this.state.logedIn)
+        return (<App />)
+      else
+        return (
+          <Login onChangeFunction={{
+            userName: this.onUserInputChnage.bind(this),
+            password: this.onPasswordInputChange.bind(this),
+            loginUser: this.loginUser.bind(this)
+          }} />)
+
+    }
+  }
 export default Authenticate;
