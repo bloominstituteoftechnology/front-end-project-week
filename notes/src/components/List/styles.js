be8cdf025d9c71c45e023e1c582ce52b5aea69f8
Unknown <zachary.arney@gmail.com>
2018-10-31 16:13:49 -0500

    Adjusted List flexbox layout

--- notes/src/components/List/styles.js
+++ notes/src/components/List/styles.js
@@ -3,23 +3,20 @@ import { ViewContainer, StyledH3 } from '../../mixins';
 import { StyledLink } from '../../mixins';
 
 export const StyledView = styled(ViewContainer)`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-start;
-  background: rgb(243, 243, 243);
-  padding: 64px 30px 0;
-  `;
+padding-right: 0;
+`;
 
 export const ListTitle = styled(StyledH3)``;
 
 export const StyledListDiv = styled.div`
 display: flex;
 flex-flow: row wrap;
-justify-content: space-between;
+justify-content: flex-start;
+width: 100%;
 padding: 33px 1px;
 `;
 
 export const StyledNoteLink = styled(StyledLink)`
+  margin-right: 17.5px;
   margin-bottom: 27px;
 `;
