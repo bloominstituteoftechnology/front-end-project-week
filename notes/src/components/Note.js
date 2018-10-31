305e8028a3c128b54f8744665e63e69684a77b22
Mindysueasaurus <mymindyyeager@gmail.com>
2018-10-31 10:49:43 -0500

    starting work on delete functionality and modal

--- notes/src/components/Note.js
+++ notes/src/components/Note.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import axios from 'axios';
-
-
+import { Link , Route } from 'react-router-dom';
+import DeleteNote from './DeleteNote';
+import EditNote from './EditNote';
 
 
 
@@ -9,7 +10,8 @@ export default class Note extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      note: 0
+      note: '',
+      showModal: false
     }
   }
 
@@ -29,11 +31,33 @@ export default class Note extends React.Component {
       })
   }
 
+  showModal = () => {
+    this.setState({ show: true })
+  }
+
+  hideModal = () => {
+    this.setState({ show: false })
+  }
+
   render() {
       return (
         <div>
+        <Link to='/edit'>Edit</Link>
+        <Link to='/delete'>Delete</Link>
+        <Route path='/delete' render={ () => {
+          <Modal show={this.state.show} handleClose={this.hideModal}>
+          <h3>{this.state.note.title}</h3>
+          <p>{this.state.note.textBody}</p>
+          <button></button>
+          <button type='button' onClick={this.showModal}>Cancel</button>
+          </Modal>
+          
+        }} />
+        
+        <Route path='/edit' component={EditNote} />
         <h3>{this.state.note.title}</h3>
         <p>{this.state.note.textBody}</p>
+        
         </div>
       )
 }}
