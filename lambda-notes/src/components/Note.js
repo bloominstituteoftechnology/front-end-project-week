f951b55b728e7efe965331e4665ff073e80d11f2
DaftBeowulf <ajmboosh@gmail.com>
2018-10-30 20:00:15 -0500

    finished implementing edit functionality. Note component will dynamically render its default info or editing form

--- lambda-notes/src/components/Note.js
+++ lambda-notes/src/components/Note.js
@@ -5,41 +5,73 @@ class Note extends React.Component {
 constructor(props){
     super(props);
     this.state={
-        activeNote:[]
+        activeNote:[],
+        isEditing: false,
+        updatedNote: {
+            title:'',
+            textBody:''
+        }
     }
 }
     
 componentDidMount() {
     axios.get(`http://fe-notes.herokuapp.com/note/get/${this.props.match.params.id}`)
-    .then(
-        
-        response => 
+    .then(response => 
         this.setState({ 
       activeNote: response.data
     })
     )
+    .catch(error=>{console.log("The server failed to retrieve this note: ", error)})
 }
 
-getNoteById = () => {
-    axios.get(`http://fe-notes.herokuapp.com/note/get/${this.props.match.params.id}`)
-    .then(response => this.setState({
-      ...this.state, 
-      activeNote: [this.state.notes.filter(note=>note._id===response.data)]
-    }))
+toggleEditing = event => {
+event.preventDefault();
+this.setState({...this.state, isEditing:!this.state.isEditing})
+}
+
+changeHandler=(key, value) => {
+    this.setState({
+        ...this.state,
+        updatedNote:{
+        ...this.state.updatedNote,
+          [key]: value
+        }
+      })
+  }
+  
+  editNote= event => {
+      event.preventDefault();
+      axios.put(`http://fe-notes.herokuapp.com/note/edit/${this.state.activeNote._id}`, this.state.updatedNote)
+      .then(response=>{this.setState({...this.state, activeNote: response.data})})
+      .catch(error=>{console.log("We were unable to edit this note: ", error)})
+      this.toggleEditing(event);
   }
 
 render(){
 
+    if (!this.state.isEditing) {
  return(
  <div className="note-full-view">
  <div className="edit-delete-wrapper">
- <p className="edit-button">edit</p>
+ <p className="edit-button" onClick={this.toggleEditing}>edit</p>
  <p className="delete-button">delete</p>
  </div>
     <h3 className="full-note-title">{this.state.activeNote.title}</h3>
 <h4 className="full-note-text">{this.state.activeNote.textBody}</h4>
     </div>
  )
+} else {
+return (
+    <div className="edit-note-wrapper">
+    <h2>Edit Note:</h2>
+    <form className="edit-note-form" onSubmit={this.editNote}>
+        <input className="edit-note-title" type="text" placeholder="Note Title" name="title" value={this.state.updatedNote.title} onChange={event=>this.changeHandler(event.target.name, event.target.value)}></input>
+        <input className="edit-note-text" type="text" placeholder="Note Content" name="textBody" value={this.state.updatedNote.textBody} onChange={event=>this.changeHandler(event.target.name, event.target.value)}></input>
+    <button type="submit">Update</button>
+    </form>
+                </div>
+)
+}
 }
 }
 
