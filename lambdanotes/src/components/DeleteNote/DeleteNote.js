f2ce971520a5e5d544805630c4f7e68e16cc0485
Lucas Beemer <lucasbeemer@gmail.com>
2018-10-31 13:57:12 -0700

    delete note component completed

---
+++ lambdanotes/src/components/DeleteNote/DeleteNote.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./DeleteNote.css";
+
+class DeleteNote extends Component {
+  
+  render() {
+    let toggle = this.props.toggle;
+    return (
+      <div className={toggle ? "delete_wrapper" : "hidden"}>
+        <div>
+          <h4>Are you sure you want to delete this?</h4>
+        </div>
+        <div classname="modal_wrapper">
+          <div className="button_red" onClick={this.props.showModal}>DELETE</div>
+          <div className="button" onClick={this.props.showModal}>NO</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default DeleteNote;
