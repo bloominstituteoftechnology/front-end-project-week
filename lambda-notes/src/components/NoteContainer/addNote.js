f7ad5b2772a7997d2d1a2c2ca475e010a98f8495
Brian G <brian.gregorius@gmail.com>
2018-10-31 15:52:52 -0400

    styled edit note and add page

--- lambda-notes/src/components/NoteContainer/addNote.js
+++ lambda-notes/src/components/NoteContainer/addNote.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
+import './NoteEdit.css';
 
 
 
@@ -14,53 +14,52 @@ class AddNote extends Component {
         };
     }
 
-   
+
     handleInput = event => {
         this.setState({ [event.target.name]: event.target.value })
     }
 
     addNote = event => {
         event.preventDefault();
-        const newNote = { title: this.state.title, textBody: this.state.textBody}
+        const newNote = { title: this.state.title, textBody: this.state.textBody }
         axios
-          .post('https://fe-notes.herokuapp.com/note/create', newNote)
-          .then(response => {
-            console.log('ADDING', response);
-            this.props.changeState(response.data)
-          })
-          .catch(err =>
-            console.log(err));
-    
+            .post('https://fe-notes.herokuapp.com/note/create', newNote)
+            .then(response => {
+                console.log('ADDING', response);
+                this.props.changeState(response.data)
+            })
+            .catch(err =>
+                console.log(err));
+
         this.setState({
-          title: '',
-         textBody: '',
+            title: '',
+            textBody: '',
         });
 
         this.props.history.push('/');
-      }
+    }
 
     render() {
         return (
-            <div>
-                <h2>Create New Note:</h2>
-                <form onSubmit={this.addNote} className='new-note-form'>
+            <div className='add-note-wrapper'>
+                <form onSubmit={this.addNote} className='edit-note'>
+                    <h1 className='add-note-h1'>Create New Note:</h1>
                     <input
-                        className='title-input'
+                        className='edit-title'
                         placeholder='Note Title'
                         value={this.state.title}
                         name='title'
                         onChange={this.handleInput}
                     />
-                    <input
-                        className='textbody-input'
+                    <textarea
+                        className='edit-textBody'
                         placeholder='Note Content'
                         value={this.state.textBody}
                         name='textBody'
                         onChange={this.handleInput}
                     />
-                    <button type='submit' onClick={this.handleInput} className='save-note'>Save</button>
+                    <button type='submit' onClick={this.handleInput} className='save-edit-btn'>Save</button>
                 </form>
-
             </div>
         )
     }
--- lambda-notes/src/components/NoteContainer/editNote.js
+++ lambda-notes/src/components/NoteContainer/editNote.js
@@ -1,29 +1,32 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-
+// import axios from 'axios';
+// import { Link } from 'react-router-dom';
+import './NoteEdit.css';
 
 
 const EditNote = props => {
 
     return (
+        
         <form onSubmit={props.saveEdits} className='edit-note'>
+            <h1>Edit Note:</h1>
             <input
                 placeholder={props.notes.title}
                 name='editTitle'
                 type='text'
                 value={props.editTitle}
                 onChange={props.editHandler}
+                className='edit-title'
             />
-            <input
+            <textarea
                 placeholder={props.notes.textBody}
                 name='editTextBody'
                 type='text'
                 value={props.editTextBody}
                 onChange={props.editHandler}
+                className='edit-textBody'
             />
-
-            <button type='submit' onClick={props.openEditForm} onClick={props.editHandler}>Save</button>
+            <button type='submit' onClick={props.saveEdits} className='save-edit-btn'>Save</button>
 
         </form>
     )
