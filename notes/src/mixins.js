6a8eb15970eeb8a984db094acd8c4532c771ba72
Unknown <zachary.arney@gmail.com>
2018-10-31 16:13:00 -0500

    More mixins

--- notes/src/mixins.js
+++ notes/src/mixins.js
@@ -3,18 +3,24 @@ import { Link } from 'react-router-dom';
 
 export const ViewContainer = styled.div`
   flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
   height: 100vh;
-  background: white;
-  padding: 10px 5px;
+  background: rgb(243, 243, 243);
+  padding: 64px 30px 0;
   border: 1px solid rgb(152, 152, 152);
   overflow-y: scroll;
 `;
 
 export const StyledLink = styled(Link)`
+  cursor: pointer;
   text-decoration: none;
 `;
 
 export const Button = styled.div`
+  cursor: pointer;
   width: 214px;
   background: rgb(42, 192, 196);
   color: rgb(255, 255, 255);
@@ -29,11 +35,47 @@ export const Button = styled.div`
 export const StyledH3 = styled.h3`
   font-size: 2.3rem;
   color: rgb(74, 73, 74);
-`;
+  `;
 
 export const StyledP = styled.p`
   text-align: left;
   font-size: 1.6rem;
-  color: rgb(0, 0, 0);
+  color: rgb(74, 73, 74);
   margin-top: 9px;
+`;
+
+export const StyledForm = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  padding: 33px 0;
+`;
+
+export const StyledInput = styled.input`
+  max-width: 402px;
+  font-size: 1.6rem;
+  color: rgb(0, 0, 0);
+  ::placeholder {
+    color: rgb(152, 152, 152);
+  }
+  padding: 14px 12px;
+  border: 1px solid rgb(152, 152, 152);
+  border-radius: 2px;
+  margin-bottom: 16px;
+  `;
+
+export const StyledTextarea = styled.textarea`
+  width: 100%;
+  height: 56vh;
+  resize: none;
+  font-size: 1.6rem;
+  color: rgb(0, 0, 0);
+  ::placeholder {
+    color: rgb(152, 152, 152);
+  }
+  padding: 14px 12px;
+  border: 1px solid rgb(152, 152, 152);
+  border-radius: 2px;
+  margin-bottom: 16px;
 `;
