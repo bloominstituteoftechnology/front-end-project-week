b2c640d1a21f42cdc4bcaacc8ed71435119a2a4d
lyang9 <lyang9@buffalo.edu>
2018-10-31 16:27:11 -0400

    Imported styled-components. Implemented isAuthentication

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
 import axios from 'axios';
+import styled from 'styled-components';
 
 import NavBar from './Components/NavBar';
 import Notes from './Components/Notes';
@@ -11,6 +12,39 @@ import EditForm from './Components/EditForm';
 // import DeleteModal from './Components/DeleteModal';
 
 import { Auth0Lock } from 'auth0-lock';
+
+const Form = styled.div`
+  border: 2px solid blue;
+  margin: 200px auto;
+  // margin: auto;
+  width: 400px;
+  height: 200px;
+  background: lightskyblue;
+  opacity: 0.6;
+`;
+
+const FormHeading = styled.h3`
+  margin: 20px auto;
+  border-bottom: 1px solid white;
+  width: 240px;
+  color: white;
+  font-size: 22px;
+`;
+
+const LoginBtn = styled.button`
+  border-radius: 5px;
+  border: 1px solid dodgerblue;
+  width: 100px;
+  height: 30px;
+  color: blue;
+  font-weight: bold;
+  font-size: 12px;
+  margin: 40px 150px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 var lock = new Auth0Lock(
   process.env.REACT_APP_CLIENT_ID,
@@ -76,52 +110,73 @@ class App extends Component {
 
   render() {
     console.log('PROCESS:', process.env);
-    return (
-      <div className="App">
-        <NavBar />
-        <Route 
-          exact path='/' 
-          render={props => (
-            <Notes 
-              {...props} 
-              notes={this.state.notes} 
-              
-            />
-          )}
-        />
-        <Route
-          path='/new'
-          render={props => (
-            <NoteForm 
-              {...props} 
-              addNote={this.addNote} 
-              fetchNotes={this.fetchNotes}
-            />
-          )}
-        />
-        <Route 
-          path='/note/:id'
-          render={props => (
-            <NoteView 
-              {...props} 
-              getNote={this.getNote} 
-              handleDeleteNote={this.handleDeleteNote}
-              fetchNotes={this.fetchNotes}
-            />
-          )}
-        />
-        <Route
-          path='/edit/:id'
-          render={props => (
-            <EditForm 
-              {...props} 
-              updateNote={this.updateNote}
-              fetchNotes={this.fetchNotes}
-            />
-          )}
-        />
-      </div>
-    );
+    if (this.isAuthenticated()) {
+      return (
+        <div className="App">
+          <NavBar />
+          <Route 
+            exact path='/' 
+            render={props => (
+              <Notes 
+                {...props} 
+                notes={this.state.notes} 
+                
+              />
+            )}
+          />
+          <Route
+            path='/new'
+            render={props => (
+              <NoteForm 
+                {...props} 
+                addNote={this.addNote} 
+                fetchNotes={this.fetchNotes}
+              />
+            )}
+          />
+          <Route 
+            path='/note/:id'
+            render={props => (
+              <NoteView 
+                {...props} 
+                getNote={this.getNote} 
+                handleDeleteNote={this.handleDeleteNote}
+                fetchNotes={this.fetchNotes}
+              />
+            )}
+          />
+          <Route
+            path='/edit/:id'
+            render={props => (
+              <EditForm 
+                {...props} 
+                updateNote={this.updateNote}
+                fetchNotes={this.fetchNotes}
+              />
+            )}
+          />
+        </div>
+      );
+    } else {
+      return (
+        <Form>
+          <FormHeading>Login to Your Account</FormHeading>
+          <LoginBtn 
+            id='btn-login'
+            onClick={function() {
+              lock.show();
+            }}
+          >
+            LOG IN
+          </LoginBtn>
+        </Form>
+      );
+    }
+  }
+  isAuthenticated() {
+    // check whether the current time is past the access Token's expire time
+    let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
   }
 }
 
