c227b048494bb371385c7b98572ee472376eb2b0
lyang9 <lyang9@buffalo.edu>
2018-10-31 00:43:30 -0400

    Added styling using styled-components

--- lambda-notes/src/Components/NavBar.js
+++ lambda-notes/src/Components/NavBar.js
@@ -13,7 +13,7 @@ const NavbarContainer = styled.div`
 `;
 
 const NavButton = styled.button`
-  width: 180px;
+  width: 240px;
   height: 40px;
   margin-bottom: 25px;
   border: 1px solid gray;
@@ -21,6 +21,58 @@ const NavButton = styled.button`
   color: white;
   font-weight: bold;
   font-size: 14px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const LogoutBtn = styled.button`
+  border-radius: 5px;
+  width: 90px;
+  height: 30px;
+  margin-bottom: 25px;
+  border: 1px solid limegreen;
+  color: green;
+  font-weight: bold;
+  font-size: 14px;
+  
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Form = styled.div`
+  border: 2px solid blue;
+  margin: 200px auto;
+  // margin: auto;
+  width: 400px;
+  height: 200px;
+  background: lightskyblue;
+  opacity: 0.6;
+`;
+
+const FormHeading = styled.h3`
+  margin: 20px auto;
+  border-bottom: 1px solid white;
+  width: 300px;
+  color: white;
+  font-size: 22px;
+`;
+
+const LoginBtn = styled.button`
+  border-radius: 5px;
+  border: 1px solid dodgerblue;
+  width: 100px;
+  height: 30px;
+  color: blue;
+  font-weight: bold;
+  font-size: 12px;
+  margin-top: 40px;
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 var lock = new Auth0Lock(
@@ -63,22 +115,22 @@ class NavBar extends Component {
             <NavButton>+ Create New Note</NavButton>
           </Link>
 
-          <div onClick={this.logout}>Log out</div>
+          <LogoutBtn onClick={this.logout}>Log out</LogoutBtn>
         </NavbarContainer>
       );
     } else {
       return (
-        <div>
-          <h3>Please click login to use Note App</h3>
-          <div 
+        <Form>
+          <FormHeading>Login to Your Account</FormHeading>
+          <LoginBtn 
             id='btn-login'
             onClick={function() {
               lock.show();
             }}
           >
             LOG IN
-          </div>
-        </div>
+          </LoginBtn>
+        </Form>
       );
     }
   }
@@ -86,6 +138,12 @@ class NavBar extends Component {
     // check whether the current time is past the access Token's expire time
     let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
     return new Date().getTime() < expiresAt;
+  }
+  logout() {
+    // clear Access Token and ID Token from local storage
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('expires_at');
+    window.location.reload();
   }
 }
 
