fd7740d2892f852aae5e50e246f3625f3132a2a8
Steven O'Neill <oneillsteven957@gmail.com>
2018-10-31 18:58:35 -0500

    Made a log out feature
    I think it is a pretty hack way to do it but I will figure out how to do it the right way later

--- lambda_notes/src/components/SideBar.js
+++ lambda_notes/src/components/SideBar.js
@@ -9,6 +9,9 @@ const SideBar = () => (
         </h1>
         <NavLink to='/'>View Your Notes</NavLink>
         <NavLink to='/createnote'>+ Create New Note</NavLink>
+        <button onClick={() => localStorage.clear(window.location.reload())}>
+            Log Out
+        </button>
     </div>
 )
 
