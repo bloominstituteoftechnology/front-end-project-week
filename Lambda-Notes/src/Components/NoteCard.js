438a291065701fb2989f9bb65b42859da70416f4
unknown <brandongrych@gmail.com>
2018-10-31 16:38:15 -0500

    dependencies installed for csv exporting, fixed small non user friendly issues

--- Lambda-Notes/src/Components/NoteCard.js
+++ Lambda-Notes/src/Components/NoteCard.js
@@ -4,6 +4,11 @@ const NoteCard = props => {
     return(
         
         <Link to={`/view/${props.id}`} className="note-card">
+        <form>
+            <input 
+            type="checkbox"
+            />
+          </form>
             <h2 className="card-title">{props.title}</h2>
             <p>{props.textBody}</p>
         </Link>
