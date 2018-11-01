1afd511eba5eaff41aaea669b70c40c8e57abf22
Brian G <brian.gregorius@gmail.com>
2018-11-01 12:37:17 -0400

    limited view of long titles on all notes page

--- lambda-notes/src/components/NoteContainer/noteCard.js
+++ lambda-notes/src/components/NoteContainer/noteCard.js
@@ -11,7 +11,7 @@ const NoteCard = props => {
     return (
         <div className='note-card'>
             <div className='notes'>
-                <h2>{props.note.title}</h2>
+                <h2>{props.note.title.substring(0,33)}</h2>
                 <p>{props.note.textBody}</p>
 
             </div>
