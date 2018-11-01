2af9be896272c654f271d5fd28991974137951b9
David Benavidez <benavidez_david13@yahoo.com>
2018-11-01 11:53:48 -0700

    commit

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware} from 'redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { notesReducer } from './reducers';
 import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
 
+const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 const store = createStore(
-    notesReducer,compose(applyMiddleware(thunk))
+    notesReducer,composeWithDevTools(applyMiddleware(thunk), reduxDevtools)
 );
+
 
 
 ReactDOM.render(
