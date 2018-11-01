5dc4f899ac041c8508014985fd90c5c1f9c14726
Timothy Hoang <timh1203@gmail.com>
2018-11-01 08:32:29 -0400

    Add stylings to sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,35 +1,58 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
+import { MdFormatListBulleted, MdAddCircleOutline } from 'react-icons/md'
 
 const SideBar = () => {
   return (
     <Div1 className="SideBar">
-      <H1>Lambda<br /> Notes</H1>
-      <Link to="/">
-        <Button>View Your Notes</Button>
-      </Link>
-      <Link to="/new">
-        <Button>+ Create New Note</Button>
-      </Link>
+      <H1>Lambda Notes</H1>
+      <Div2>
+        <Link to="/" title="My Notes">
+          <MdFormatListBulleted1 />
+        </Link>
+        <Link to="/new" title="New Note">
+          <MdAddCircleOutline1 />
+        </Link>
+      </Div2>
     </Div1>
   )
 }
 
 const Div1 = styled.div`
-  /* width: 25%;
-  border: 1px solid #B7B7B7;
-  text-align: center;
-  background: #D3D2D3;
-  min-height: 90vh; */
+  font-family: 'Goudy Bookletter 1911';
+  display: flex;
+  justify-content: space-between;
+`
+const Div2 = styled.div`
+  width: 20%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `
 const H1 = styled.h1`
-  /* text-align: left;
-  margin-left: 7%;
-  font-size: 3.5rem;
-  line-height: 0.9; */
-  font-family: 'Goudy Bookletter 1911';
 `
-const Button = styled.button``
+const MdFormatListBulleted1 = styled(MdFormatListBulleted)`
+  color: black;
+  font-size: 3rem;
+  &:hover {
+    background: black;
+    color: white;
+    border-radius: 3px;
+    border: none;
+    box-shadow: 0 0 20px lightgray;
+  }
+`
+const MdAddCircleOutline1 = styled(MdAddCircleOutline)`
+  color: black;
+  font-size: 3rem;
+  &:hover {
+    background: black;
+    color: white;
+    border-radius: 3px;
+    border: none;
+    box-shadow: 0 0 20px lightgray;
+  }
+`
 
 export default SideBar
