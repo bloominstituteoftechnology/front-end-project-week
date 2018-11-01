65c5f6f79978233453919eda3c9b5c8deee4417c
Riley-Brown <riley@riley.gg>
2018-11-01 16:42:42 -0400

    Added search bar for notes component

--- lambda-notes/src/components/notes/Notes.js
+++ lambda-notes/src/components/notes/Notes.js
@@ -6,7 +6,14 @@ import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import Spinner from "../layout/Spinner";
 class Notes extends Component {
-  state = {};
+  state = {
+    searchNoteFilter: ""
+  };
+  handleSearchNoteFilter = e => {
+    this.setState({
+      searchNoteFilter: e.target.value
+    });
+  };
   render() {
     const { notes } = this.props;
 
@@ -14,11 +21,26 @@ class Notes extends Component {
       const filteredNotes = this.props.notes.filter(
         note => note.userID === localStorage.getItem("userID")
       );
+
+      const filteredNotesPlusSearch = filteredNotes.filter(
+        note =>
+          note.title.toLowerCase().includes(this.state.searchNoteFilter) ||
+          note.textBody.toLowerCase().includes(this.state.searchNoteFilter)
+      );
       return (
         <React.Fragment>
+          <form className="form-search-filter">
+            <input
+              type="text"
+              placeholder="Search Notes"
+              value={this.state.searchNoteFilter}
+              name="searchNoteFilter"
+              onChange={this.handleSearchNoteFilter}
+            />
+          </form>
           <div className="notes-container">
             <h1 className="your-notes">Your Notes:</h1>
-            {filteredNotes.map(note => (
+            {filteredNotesPlusSearch.map(note => (
               <Note note={note} key={note.id} />
             ))}
           </div>
