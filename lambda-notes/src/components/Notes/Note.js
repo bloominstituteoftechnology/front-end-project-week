7b0562be565a6d3b8cf37e0281f5ce27df3e5a0b
mich-shrak <micah.shrak@gmail.com>
2018-11-01 16:30:18 -0700

    Updated FE

--- lambda-notes/src/components/Notes/Note.js
+++ lambda-notes/src/components/Notes/Note.js
@@ -1,31 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 // import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 // Individual Notes Listed
 class Note extends Component {
-	constructor() {
-		super();
+	constructor(props) {
+		super(props);
 		this.state = {
 			note: [],
 		};
 	}
 	componentDidMount() {
-		const noteId = this.props.match.params.id;
-		this.getRequestById(noteId);
-		console.log(noteId);
-	}
-
-	getRequestById = (id) => {
+		const id = this.props.match.params.id;
+		//this.getRequestById(noteId);
+		//console.log(noteId);
 		console.log(id);
 		axios
 			.get(`http://localhost:3300/api/notes/${id}`)
-			.then(response => {
+			.then((response) => {
 				console.log('response: ', response);
-				this.setState({note: response.data});
-			  })
+				this.setState({ note: response.data[0] });
+			})
 			.catch((error) => console.log(error));
-	};
+	}
+
+	/* getRequestById = (id) => {
+		console.log(id);
+		axios
+			.get(`http://localhost:3300/api/notes/${id}`)
+			.then((response) => {
+				console.log('response: ', response);
+				this.setState({ note: response.data });
+			})
+			.catch((error) => console.log(error));
+	}; */
 	// Update function
 	editNote = () => {
 		const id = this.props.match.params.id;
@@ -39,15 +47,17 @@ class Note extends Component {
 
 	render() {
 		return (
-			<div className="NoteViewDiv">
-				<h1 className="NoteViewHeader">{this.state.title}</h1>
+			<Fragment>
+				<div className="NoteViewDiv">
+					<h1 className="NoteViewHeader">{this.state.note.title}</h1>
 
-				<p className="NoteViewParagraph">{this.state.content}</p>
-				<div className="EditDelete">
-					<button onClick={() => this.editNote()}>Edit</button>
-					<button onClick={() => this.deleteNote()}>Delete</button>
+					<p className="NoteViewParagraph">{this.state.note.content}</p>
+					<div className="EditDelete">
+						<button onClick={() => this.editNote()}>Edit</button>
+						<button onClick={() => this.deleteNote()}>Delete</button>
+					</div>
 				</div>
-			</div>
+			</Fragment>
 		);
 	}
 }
