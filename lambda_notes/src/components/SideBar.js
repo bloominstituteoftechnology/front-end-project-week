f4ada6a66d7f855793ad229dcf1a1a6e98634e53
Steven O'Neill <oneillsteven957@gmail.com>
2018-11-01 14:32:04 -0500

    Changed the logout button
    Changed it so it removes the user from local storage rather than clearing the local storage

--- lambda_notes/src/components/SideBar.js
+++ lambda_notes/src/components/SideBar.js
@@ -2,17 +2,26 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Components.css';
 
-const SideBar = () => (
-    <div className="sidebar-container">
+const SideBar = () => {
+    const handleLogoutSubmit = props => {
+        const user = props.username;
+        localStorage.removeItem('user', user);
+        window.location.reload();
+      };
+
+    return (
+        <div className="sidebar-container">
         <h1>
             Lambda <br /> Notes
         </h1>
         <NavLink to='/'>View Your Notes</NavLink>
         <NavLink to='/createnote'>+ Create New Note</NavLink>
-        <button onClick={() => localStorage.clear(window.location.reload())}>
+        <button onClick={handleLogoutSubmit}>
             Log Out
         </button>
     </div>
-)
+    )
+    
+    }
 
 export default SideBar;
