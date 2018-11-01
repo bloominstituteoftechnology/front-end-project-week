59fe710ef1cb576e67159b7be4963c54c80589ee
pedro <pedroluismontesinosrivera@gmail.com>
2018-11-01 00:23:10 -0400

    fixed a bug in my NotePage component, when note page was re loaded it threw an error

--- lambda-notes/src/components/NotePage.js
+++ lambda-notes/src/components/NotePage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const NotePage = props => {
+    if(props.notes.length){
         let note = props.notes.find(note => `${note._id}` === props.match.params.id);
 
         const deleteNotes = event => {
@@ -12,7 +13,7 @@ const NotePage = props => {
         return (
             <div className="note-page">
                 {props.delete && (
-                    <div className="delete-modal">
+                    <div className="delete-modal"> 
                         <p>Are you sure you want to delete this?</p>
                         <div className="note-page-buttons">
                             <button className="delete-btn" onClick={deleteNotes}>Delete</button>
@@ -20,12 +21,17 @@ const NotePage = props => {
                         </div>
                     </div>
                 )}
-                <Link to={`/note/${note._id}/edit`} onClick={() => props.editNote(note.title, note.textBody)}>edit</Link>{" "}
-                <span onClick={props.deleteToggleOn}>delete</span>
+                <div className="note-page-buttons">
+                    <Link to={`/note/${note._id}/edit`} onClick={() => props.editNote(note.title, note.textBody)}>edit</Link>{" "}
+                    <span onClick={props.deleteToggleOn}>delete</span>
+                </div>
                 <h2>{note.title}</h2>
                 <p>{note.textBody}</p>
             </div>
         );
+    }else{
+        return <p>Loading...</p>
+    }
     
 };
 
