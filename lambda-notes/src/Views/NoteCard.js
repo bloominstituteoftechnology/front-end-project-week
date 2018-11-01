cb28d69107c4bfecebd2a9f4ce39a9cec778460d
AM_Myrick <amyrick.820@gmail.com>
2018-11-01 11:34:27 -0400

    creates length cutoff for note titles and textbody

--- lambda-notes/src/Views/NoteCard.js
+++ lambda-notes/src/Views/NoteCard.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./NoteCard.css"
 
 const NoteCard = props => {
     return (
         <Link to={`/note/${props.note._id}`}>
             <div className="notecard">
-                <h3>{props.note.title}</h3>
-                <p>{props.note.textBody}</p>
+                <h3>{props.note.title.length > 15 ?
+                        props.note.title.substring(0, 15) + "..." :
+                        props.note.title}</h3>
+                <p>{props.note.textBody.length > 75?
+                        props.note.textBody.substring(0, 75) + "..." :
+                        props.note.textBody}</p>
             </div>
         </Link>
     )
