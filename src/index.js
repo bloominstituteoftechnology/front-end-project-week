5b166328d5ab901d6e1f4e474049e988590fc5f0
David Benavidez <benavidez_david13@yahoo.com>
2018-11-01 11:59:56 -0700

    commit

--- src/index.js
+++ src/index.js
@@ -3,19 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware} from 'redux';
+import { createStore, applyMiddleware, compose} from 'redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { notesReducer } from './reducers';
 import thunk from 'redux-thunk';
-import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
 
-const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+let devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && 
+window.__REDUX_DEVTOOLS_EXTENSION__();
+if (process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'production') {
+    devTools = a => a;
+}
 
 const store = createStore(
-    notesReducer,composeWithDevTools(applyMiddleware(thunk), reduxDevtools)
+    notesReducer,compose(applyMiddleware(thunk), devTools)
 );
-
-
 
 ReactDOM.render(
     <Provider store={store}>
