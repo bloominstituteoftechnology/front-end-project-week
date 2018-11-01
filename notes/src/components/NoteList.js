5b67fc20d01144987251f31c20190b28e1ed5259
Drewsup123c <drewsup123c@gmail.com>
2018-11-01 13:33:36 -0600

    added styling onto the icons for a better look

--- notes/src/components/NoteList.js
+++ notes/src/components/NoteList.js
@@ -4,9 +4,13 @@ import NoteCard from './NoteCard'
 const NoteList = (props) => {
     return(
         <div className="note-list">
-            <i className="fas fa-file-export export" onClick={() => props.export()}>Export As CSV</i>
-            <i onClick={() => props.changeSize(true)} className="fas fa-th-large"></i>
-            <i onClick={() => props.changeSize(false)} className="fas fa-th"></i>
+            <div className="btn-section">
+                <i className="fas fa-file-export export" onClick={() => props.export()}>Export As CSV</i>
+                <div className="size-btns">
+                    <i onClick={() => props.changeSize(true)} className="fas fa-th-large"></i>
+                    <i onClick={() => props.changeSize(false)} className="fas fa-th"></i>
+                </div>
+            </div>
             {props.notes.map(note => {
                 return (<NoteCard 
                     size={props.size}
