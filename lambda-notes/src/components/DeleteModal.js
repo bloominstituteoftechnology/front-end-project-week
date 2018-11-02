9432948267f8c0721b6a2a33baf75f566255583f
Elvis Ibarra <ielvisd@gmail.com>
2018-11-01 17:42:24 -0700

    One More Modal Style

--- lambda-notes/src/components/DeleteModal.js
+++ lambda-notes/src/components/DeleteModal.js
@@ -25,7 +25,7 @@ const ModalViewContainer = styled.div`
 
 const ModalContainer = styled.div`
   background: white;
-  height: 35vh;
+  height: 45vh;
   font-size: calc(10px + 2vmin);
   color: black;
   width: 80%;
