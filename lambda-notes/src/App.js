2626bedafe93024d6e478f4d9964107ed0499f8e
Angelo Deleon <rudy209@comcast.net>
2018-11-05 13:42:58 -0800

    removed generic template info

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -7,18 +7,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Lambda Notes!!! Best Note taking app in the world!!
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
