566d69a5a53ac0f9656386f53556a259c0f36952
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2018-11-05 14:32:35 -0500

    More refactor

---
+++ lambda-notes/src/Components/NotesList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Note from './Note';
+
+const NotesList = props => (
+    <div className='notes-list'>
+      <h3 className='your-notes'>Your Notes</h3>
+      <div className='notes'>
+        {props.notes.map((note, index) => (
+          <Note note={note} key={index} />
+        ))}
+      </div>
+    </div>
+);
+
+export default NotesList;
