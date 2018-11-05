5d22276bbd678512857752a643ed1c884327d55c
Barbara Hunter <commandandcode@gmail.com>
2018-11-05 16:41:06 -0500

    working on it

---
+++ project/src/components/notebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Notebar = (props) => {
+  return (
+    <div> 
+      <h1>Lambda Notes</h1>
+       <button>View Your Notes</button>
+       <button>+ Create New Note</button>
+
+    </div>
+  )
+} 
+
+
+
+export default Notebar;
---
+++ project/src/components/notecontainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Notelist = (props) => {
+  return (
+    <div> 
+      <h3>Your Notes</h3>
+       <button>Note Component here</button>
+
+    </div>
+  )
+} 
+
+
+
+export default Notelist;
