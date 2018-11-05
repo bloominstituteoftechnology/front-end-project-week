5211d7b445d780976afaa30c0f8f1589adbb4026
Angelo Deleon <rudy209@comcast.net>
2018-11-05 15:59:38 -0800

    added imports to App.js

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import  { Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
