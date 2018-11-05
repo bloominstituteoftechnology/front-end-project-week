1b7c6eecbbc14ecbb32b46a8c29b3f367d25c737
Jake Thomas <jmaxt12@gmail.com>
2018-11-05 13:08:20 -0700

    Notes should be showing with minor styling

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,28 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [],
+      newNote: {
+        tag: '',
+        title: '',
+        textBody: '',
+      }
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://fe-notes.herokuapp.com/note/get/all')
+      .then(response => {
+        this.setState({ notes: response.data })
+      })
+      .catch(error => console.log('It\'s over! Turn back now!'))
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='container'>
+        <div className="App">
+          <div className="side-bar">
+            <div className='lambda-notes'>
+              <h1>Lambda Notes</h1>
+            </div>
+            <button className='button'>View Your Notes</button>
+            <button className='button'>+Create New Note</button>
+          </div>
+
+          <div className='main-view'>
+            <div className='title-cont'>
+              <h2>Your Notes: </h2>
+            </div>
+            <div className="cont-body">
+              {this.state.notes.map(note => (
+                <div className="note">
+
+                  <div className='note-title'>
+                    <h3>{note.title}</h3>
+                  </div>
+
+                  <div className="note-body">{note.textBody}</div>
+
+                </div>
+
+              )
+              )}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
-
 export default App;
