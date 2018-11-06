4fd3c031562e5ab989c0f119ebc5fb99c4270397
Angelo Deleon <rudy209@comcast.net>
2018-11-06 12:45:32 -0800

    installed react-router-dom and axios

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -4,6 +4,20 @@ import  { Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+     this.state = {
+      notes: []
+    };
+  }
+   componentDidMount() {
+    axios
+      .get("https://fe-notes.herokuapp.com/note/get/all")
+      .then(response => this.setState({ notes: response.data }))
+      .then(console.log(this.state.notes))
+      .catch(error => console.log(error));
+  }
+
   render() {
     return (
       <div className="App">
