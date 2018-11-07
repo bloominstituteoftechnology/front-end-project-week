47629f69d56eb2f31ae8490d0a1c7a8302a2b286
mikaelacurrier <mikaela.currier@gmail.com>
2018-11-07 18:22:53 -0500

    did the same to homepage.js

--- notes/src/components/HomePage.js
+++ notes/src/components/HomePage.js
@@ -2,21 +2,46 @@ import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import "./homePage.css";
 
-class HomePage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      notes: []
-    };
-  }
-  showNotes = event => {
-    event.preventDefault();
-    console.log("Show me the notes!");
-  }
+// class HomePage extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       notes: []
+//     };
+//   }
+//   showNotes = event => {
+//     event.preventDefault();
+//     console.log("Show me the notes!");
+//   }
   
-  render() {
-    return (
-      <div className="container">
+//   render() {
+//     return (
+//       <div className="container">
+//         <div className="sideBar">
+//           <NavLink to="/">
+//             <h1 className="lambdaTitle">Lambda Notes</h1>
+//           </NavLink>
+//           <div className="sideBarButtons">
+//             <NavLink to="/notes">
+//               <button>View Your Notes</button>
+//             </NavLink>
+//             <NavLink to="/createnote">
+//               <button>+ Create New Note</button>
+//             </NavLink>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default HomePage;
+
+
+
+const HomePage = () => {
+  return ( 
+    <div className="container">
         <div className="sideBar">
           <NavLink to="/">
             <h1 className="lambdaTitle">Lambda Notes</h1>
@@ -31,8 +56,7 @@ class HomePage extends Component {
           </div>
         </div>
       </div>
-    );
-  }
+   );
 }
-
+ 
 export default HomePage;
