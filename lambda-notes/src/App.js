a794772cc4ba4f3dcef71dd9d2c0b8ece15eb79f
Angelo Deleon <rudy209@comcast.net>
2018-11-07 14:20:50 -0800

    working on NoteForm to add new notes

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 // import  { Route } from 'react-router-dom';
-
 import Notes from './components/Notes';
 import './styles.css';
 
---
+++ lambda-notes/src/components/NoteForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import axios from 'axios';
+
+class NoteForm extends React.Component {
+  constructor(props){
+  super(); 
+  }
+}
+
+export default NoteForm;
