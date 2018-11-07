894b1f7c48b7822c353cffa2e0aa138c79c82705
ckirkwood94 <ckirkwood94@gmail.com>
2018-11-07 12:56:44 -0500

    Test precommit hook on NotesView

--- src/views/NotesView.js
+++ src/views/NotesView.js
@@ -8,86 +8,82 @@ import Search from '../components/Notes/Search';
 import NoteCard from '../components/Notes/NoteCard';
 
 class NotesView extends Component {
-  state = {
-    searchInput: '',
-  };
+	state = {
+		searchInput: '',
+	};
 
-  componentDidMount() {
-    if (this.props.notes.length === 0) {
-      this.props.fetchData();
-    }
-  }
+	componentDidMount() {
+		if (this.props.notes.length === 0) {
+			this.props.fetchData();
+		}
+	}
 
-  handleInput = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  };
+	handleInput = event => {
+		this.setState({
+			[event.target.name]: event.target.value,
+		});
+	};
 
-  handleSearch = (event) => {
-    event.preventDefault();
-    console.log('handleSearch');
-    this.state.searchInput === ''
-      ? this.props.fetchData()
-      : this.props.searchData(this.state.searchInput);
-  };
+	handleSearch = event => {
+		event.preventDefault();
+		console.log('handleSearch');
+		this.state.searchInput === ''
+			? this.props.fetchData()
+			: this.props.searchData(this.state.searchInput);
+	};
 
-  render() {
-    if (this.props.fetchingData) {
-      return <Title>Loading notes...</Title>;
-    }
-    return (
-      console.log(this.props.notes),
-      (
-        <Container>
-          <Search
-            handleInput={this.handleInput}
-            handleSearch={this.handleSearch}
-            {...this.state}
-          />
-          <Title> Your Notes:</Title>
-          <List>
-            {this.props.notes.length === 0 ? (
-              <Title>No notes to see! Go add some!</Title>
-            ) : (
-              this.props.notes.map((note) => {
-                return <NoteCard note={note} key={note.id} />;
-              })
-            )}
-          </List>
-        </Container>
-      )
-    );
-  }
+	render() {
+		if (this.props.fetchingData) {
+			return <Title>Loading notes...</Title>;
+		}
+		return (
+			console.log(this.props.notes),
+			(
+				<Container>
+					<Search handleInput={this.handleInput} handleSearch={this.handleSearch} {...this.state} />
+					<Title> Your Notes:</Title>
+					<List>
+						{this.props.notes.length === 0 ? (
+							<Title>No notes to see! Go add some!</Title>
+						) : (
+							this.props.notes.map(note => {
+								return <NoteCard note={note} key={note.id} />;
+							})
+						)}
+					</List>
+				</Container>
+			)
+		);
+	}
 }
 
-const mapStateToProps = (state) => {
-  // console.log(state);
-  return {
-    notes: state.notesReducer.notes,
-    fetchingData: state.notesReducer.fetchingData,
-  };
+const mapStateToProps = state => {
+	// console.log(state);
+	return {
+		notes: state.notesReducer.notes,
+		fetchingData: state.notesReducer.fetchingData,
+	};
 };
 
 export default connect(
-  mapStateToProps,
-  {
-    fetchData,
-    searchData,
-  }
+	mapStateToProps,
+	{
+		fetchData,
+		searchData,
+	}
 )(NotesView);
 
 const Container = styled.div`
-  width: 100%;
+	width: 100%;
 `;
 
 const List = styled.div`
-  width: 100%;
-  display: flex;
-  flex-wrap: wrap;
+	width: 100%;
+	display: flex;
+	flex-wrap: wrap;
 `;
 
 const Title = styled.h1`
-  font-size: 1.5rem;
-  font-weight: 600;
+	font-size: 1.5rem;
+	font-weight: 600;
 `;
