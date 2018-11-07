7c0d32b0a127556dfd1ae2011c1482f9fcfca270
ckirkwood94 <ckirkwood94@gmail.com>
2018-11-07 12:30:34 -0500

    Testing precommit hook

--- src/App.js
+++ src/App.js
@@ -7,38 +7,38 @@ import Nav from './components/Nav';
 import Routes from './components/Routes';
 
 class App extends Component {
-  componentDidMount() {
-    this.props.history.push('/');
-  }
+	componentDidMount() {
+		this.props.history.push('/');
+	}
 
-  render() {
-    console.log(this.props);
-    return (
-      <React.Fragment>
-        <Container>
-          <Nav />
-          <Routes />
-        </Container>
-        <BackGround />
-      </React.Fragment>
-    );
-  }
+	render() {
+		console.log(this.props);
+		return (
+			<React.Fragment>
+				<Container>
+					<Nav />
+					<Routes />
+				</Container>
+				<BackGround />
+			</React.Fragment>
+		);
+	}
 }
 
 export default withRouter(withRouter(App));
 
 const Container = styled.div`
-  display: flex;
-  width: 100%;
-  margin: auto;
+	display: flex;
+	width: 100%;
+	margin: auto;
 `;
 
 const BackGround = styled.div`
-  border: 1px solid #c3c2c3;
-  background: #f2f1f2;
-  position: fixed;
-  height: 100vh;
-  width: 100%;
-  z-index: -1;
-  top: 0;
+	border: 1px solid #c3c2c3;
+	background: #f2f1f2;
+	position: fixed;
+	height: 100vh;
+	width: 100%;
+	z-index: -1;
+	top: 0;
 `;
