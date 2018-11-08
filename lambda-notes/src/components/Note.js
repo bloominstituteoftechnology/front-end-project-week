8116377e9997228f28e97082b1bc29ea2f22b0a7
Angelo Deleon <rudy209@comcast.net>
2018-11-08 11:42:50 -0800

    working on adding notes functionality

---
+++ lambda-notes/src/components/Note.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import axios from 'axios';
+
+
+class Note extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      note: {};
+    
+    };
+  }
+
+
+
+
+
+}
+
+
+export default Note;
--- lambda-notes/src/components/NoteForm.js
+++ lambda-notes/src/components/NoteForm.js
@@ -3,8 +3,43 @@ import axios from 'axios';
 
 class NoteForm extends React.Component {
   constructor(props){
-  super(); 
+  super();
+  this.state = {
+    title: "",
+    textBody: ""
+    }; 
   }
+
+  addNote = event => {
+    event.preventDefault();
+     axios
+    .post('https://fe-notes.herokuapp.com/note/create', {
+      title: this.state.title,
+      textBody: this.state.textBody
+    })
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
+     this.setState({
+      title: "",
+      textBody: ""
+    });
+  };
+   handleInputChange = event => {
+    this.setState({ [e.target.title]: e.target.value })''
+  };
+   clickHandler = event => {
+    event.preventDefault();
+  };
+
+
+  render(){
+    return (
+      <div>
+        {/* ??? */}
+       </div>
+    )
+  }
+
 }
 
 export default NoteForm;
