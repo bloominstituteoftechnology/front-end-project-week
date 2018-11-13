a9a783647aa5be066e59d5dae724a69071be37a5
Colin Dismuke <colin.dismuke@gmail.com>
2018-11-12 23:14:34 -0600

    Connected to the Notes API

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,28 +1,90 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, withRouter } from 'react-router-dom';
+import axios from 'axios';
+
+import Navigation from './components/Navigation';
+import Notes from './components/Notes';
+import NoteCard from './components/NoteCard'
+import NewForm from './components/NewForm';
+import EditForm from './components/EditForm';
 
 class App extends Component {
+  constructor(props) {
+    super(props) 
+
+    this.state = {
+      notes: [],
+      title: '',
+      textBody: '',
+      updateTitle: '',
+      updateTextBody: '',
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://killer-notes.herokuapp.com/note/get/all')
+      .then(response => {
+        this.setState(() => ({ notes: response.data }));
+      })
+      .catch(error => {
+        console.error('Server Error', error);
+      });
+  }
+
+  addNote = (newNote) => {
+    axios
+        .post('https://killer-notes.herokuapp.com/note/create', {
+          title: this.state.title,
+          textBody: this.state.textBody
+        })
+        .catch(err => {
+          console.log(err)
+        })
+
+    this.setState({ title: '', textBody: '' })
+  }
+
+  deleteNote = (id) => {
+    axios
+        .delete(`https://killer-notes.herokuapp.com/note/delete/${id}`)
+        .then(response => {
+          console.log(response.data)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+
+    this.setState();
+    this.props.history.push('/')
+  }
+
+  editNote = (id) => {
+    axios
+        .put(`https://killer-notes.herokuapp.com/note/edit/${id}`, {
+          title: this.state.title,
+          textBody: this.state.textBody          
+        })
+        .catch(err => {
+          console.log(err)
+        })
+  }
+
+  updatedNote = (title, textBody) => {
+    this.setState({ updateTitle: title, updateTextBody: textBody })
+  }
+
+  inputHandler = event => {
+    this.setState({ [event.target.name] : event.target.value })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
