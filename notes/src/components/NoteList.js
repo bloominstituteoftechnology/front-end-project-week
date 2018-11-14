1e58ba0f9f02784c54aa25df2bb243e47dbf2695
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-11-14 15:04:16 -0700

    Cleaned up get request to the server

--- notes/src/components/NoteList.js
+++ notes/src/components/NoteList.js
@@ -8,18 +8,20 @@ class NoteList extends React.Component {
 
         this.state = {
             error: null,
+            loading: false,
             notes: []
         }
     }
 
     componentDidMount() {
         console.log('NoteList: componentDidMount()');
+        this.setState({...this.state, loading: true});
         axios.get('https://fe-notes.herokuapp.com/note/get/all')
             .then( response => {
-                this.setState({error: null, notes: response.data});
+                this.setState({error: null, loading: false, notes: response.data});
             })
             .catch( err => {
-                this.setState({...this.state, error: "Unable to retrieve notes from server"});
+                this.setState({error: "Unable to retrieve notes from server", loading: false, notes: []});
             })
     }
 
@@ -27,6 +29,7 @@ class NoteList extends React.Component {
         console.log('NoteList: render()');
         return (
             <div className="note-list">
+                { this.state.loading === true ? <h1>Loading...</h1>: null }
                 { this.state.error !== null ? <h1>{this.state.error}</h1> : null }
                 { this.state.notes.map(note => <Note note={note} key={note._id}/> ) }
             </div>
