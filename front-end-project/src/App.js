242c26347bfeaa134d2a75ed01dd82483e8d750c
BrendonKootz <brendonkootz@gmail.com>
2018-11-14 17:23:29 -0600

    Added dependancies

--- front-end-project/src/App.js
+++ front-end-project/src/App.js
@@ -1,28 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { connect } from 'react-redux';
+import { withRouter, Route } from 'react-router';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<h1>LAMBDA NOTES PROJECT</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
