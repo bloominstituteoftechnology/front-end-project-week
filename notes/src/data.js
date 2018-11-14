5dca2c62dd011182e99f0f8721e23eb6a7e8fafd
iepoch <staclem@live.com>
2018-11-13 18:21:52 -0700

    fixed the data id's as I wanted them numbers instead of strings

--- notes/src/data.js
+++ notes/src/data.js
@@ -1,13 +1,13 @@
 export const notes = [
     {
         tags: [''],
-        _id: '0',
+        id: 0,
         title: 'My first note',
         textBody: 'This is my first note'
     },
     {
         tags: [''],
-        _id: '1',
+        id: 1,
         title: 'My second note',
         textBody: 'This is my second note'
     }
