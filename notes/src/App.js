4c83cc16c24a70440a812d9962680caffb9d077f
Jess <jessdev83@gmail.com>
2018-11-13 21:45:00 -0500

    API Connect
    Connected app to external API, added needed dependencies, rough start to app in App.js file to display all notes in API to make sure connection worked.

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,25 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      notes: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://fe-notes.herokuapp.com/note/get/all")
+      .then(response => {
+        this.setState({ notes: response.data });
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.notes.map(note => {
+          return (
+            <div>
+              <h1>{note.title}</h1>
+              <p>{note.textBody}</p>
+            </div>
+          );
+        })}
       </div>
     );
   }
