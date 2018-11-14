d6d6136a9138d9d9a20d9a3af3f561537bff4e46
Alex Martin <atonymartin20@yahoo.com>
2018-11-13 21:14:53 -0600

    Set up dependencies and created react app

---
+++ todo/src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import './App.css';
+
+class App extends Component {
+  state = {
+    todos: [],
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
