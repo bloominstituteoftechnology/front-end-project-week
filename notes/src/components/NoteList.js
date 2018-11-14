08407a80d7ab47f6ca5834fe03d2684ca88ae31a
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-11-14 13:47:42 -0700

    React app running and displaying hello world

---
+++ notes/src/components/NoteList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class NoteList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>Hello World</div>
+        )
+    }
+}
+
+export default NoteList;
