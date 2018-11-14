2c8dba84c6eff35c6d3f29e2e7b2cdc8d5499b46
iepoch <staclem@live.com>
2018-11-13 18:20:08 -0700

    Added some more new files and created small data.js file to hold some notes

---
+++ notes/src/data.js
@@ -0,0 +1,14 @@
+export const notes = [
+    {
+        tags: [''],
+        _id: '0',
+        title: 'My first note',
+        textBody: 'This is my first note'
+    },
+    {
+        tags: [''],
+        _id: '1',
+        title: 'My second note',
+        textBody: 'This is my second note'
+    }
+]
