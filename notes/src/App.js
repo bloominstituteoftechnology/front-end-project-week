539a192fc1d124de587a702035963dac75748497
Jennifer King <jenbellpgh@gmail.com>
2018-11-13 22:49:52 -0500

    Setup App.js as main entry point for routes/links setup Components/Notes.js as main starting point, where "/" points to.

--- notes/src/App.js
+++ notes/src/App.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 
 import './App.css';
+import Notes from './Components/Notes';
+
+/* Main entry point for the Lambda Notes app.
+ * This will primarily contains the routes needed for our app, along with
+ * app links
+ */
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         Lambda Notes
+        <Route exact path="/" component={Notes} />
       </div>
     );
   }
