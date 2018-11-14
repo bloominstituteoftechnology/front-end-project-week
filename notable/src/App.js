35c8f0cb0c67db4a5915832f380707382a1d03e7
mglittleton <michaelglittleton@gmail.com>
2018-11-13 23:25:08 -0500

    Basic framework done for a Redux app
    Reducer and action file has been created. App and index don't do much, but are skeleton'd out. All basic libraries are installed and imported.

---
+++ notable/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import styled from 'styled-components';
+
+// something styled - need flex for sidebar and notes 
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Notable</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ notable/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+
+import App from "./App";
+import rootReducer from "./reducers";
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
