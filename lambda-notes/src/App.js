9c52e1927415d2b289401715fae63974d9ca8ba1
Dewayne87 <dewaynelindsay87@gmail.com>
2018-11-14 18:36:04 -0800

    added styling

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import { NavLink } from 'react-router-dom'
+import './index.css';
 import axios from 'axios';
+import { Route } from 'react-router-dom';
 import CreateNote from './components/CreateNote';
 import Notes from './components/Notes';
+import NoteCard from './components/NoteCard';
 
 class App extends Component {
   constructor(props) {
@@ -61,8 +64,19 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Notes notes={this.state.notes} />
+      <div className="app">
+        <div className="sidebar-links">
+          <h1>Lambda<br/>Notes</h1>
+          <NavLink className="link" to={'/'}>View Your Notes</NavLink><br/>
+          
+          <NavLink className="link" to={'/note-form'}>+ Create New Note</NavLink>
+          
+        </div>
+        <div className="content">
+          <Route exact path="/" render={(props) => <Notes {...props} notes={this.state.notes} />} />
+          <Route path="/note-form" render={(props) => <CreateNote {...props}  />} />
+          <Route path="/notes/:id" render={(props) => <NoteCard {...props} notes={this.state.notes}  />} />
+        </div>
       </div>
     );
   }
--- lambda-notes/src/components/Note.js
+++ lambda-notes/src/components/Note.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom'
 const Note = props => {
   return (
-    <NavLink className="link" to={`/notes/${props.id}`}>
+    <NavLink className="notelink" to={`/notes/${props.id}`}>
       <div className="note">
         <h3>{props.title}</h3>
         <p>{props.text}</p>
