58d1cd6a655cb2a757069a68a3381d266da3acb1
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-11-14 21:50:58 -0500

    refactored App added axios.get/all function

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
+import { instance } from './utils.js';
 
 import './App.css';
 import NotesList from './components/Notes/NotesList';
 
 class App extends Component {
-  state = {
-    notes: [
-      {
-        title: 'pizza',
-        textBody: 'please eat some good pizza kid'
-      },
-      {
-        title: 'cows',
-        textBody: 'good for beef tacos!!'
+  constructor() {
+    super();
+      this.state = {
+        notes: [
+          {
+            title: 'pizza',
+            textBody: 'please eat some good pizza kid'
+          },
+          {
+            title: 'cows',
+            textBody: 'good for beef tacos!!'
+          }
+        ]
       }
-    ]
   }
+
+  componentDidMount() {
+    instance.get('/get/all')
+      .then(response => {
+        console.log(response);
+        this.setState(() => ({ notes: response.data }));
+      })
+      .catch(err => {
+        console.log(err, 'Whoops');
+      });
+  }
+
   render() {
     return (
       <div className="App">
