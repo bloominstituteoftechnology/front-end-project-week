3bd4171aabf595c27315a44ce21c82d2b2c088dc
Mitchell Robles <mitchellr92@live.com>
2018-11-14 17:52:55 -0800

    added temporary title on top and contuing to style

--- notes/src/App.js
+++ notes/src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import NoteList from '../src/Components/NoteList';
 import Menu from '../src/Components/Menu';
+import Title from '../src/Components/Tite';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Menu />
-        <NoteList />
+      <div>
+        <Title />
+        <div className="App">
+          <Menu />
+          <NoteList />
+        </div>
       </div>
     );
   }
---
+++ notes/src/Components/Tite.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Title.css';
+
+const Title = (props) => {
+    return (
+        <h2 class='title'>
+            List View
+        </h2>
+    )
+}
+
+export default Title;
