ad21518d414dcc583355dca0238f685d3357a936
Rob Salzberg <robsalzberg@gmail.com>
2018-11-14 23:01:43 -0500

    POST a newly created note

--- lambda_notes/src/App.js
+++ lambda_notes/src/App.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import "./App.css";
 import { Route, NavLink } from "react-router-dom";
 import NoteList from "./components/notelist";
+import CreateNote from "./components/createnote";
 
 class App extends Component {
   constructor() {
@@ -29,26 +30,31 @@ class App extends Component {
   };
   render() {
     return (
-      <>
-      <div className='noteview'>
-      <div className="sidebar">
-        <h1>Lambda</h1>
-        <h1>Notes</h1>
-        <div className="navigation">
-          <NavLink className='sidebarbutton' to="/" onClick={this.getNotes}>
-            View Your Notes
-          </NavLink>
-          <button className='sidebarbutton'>+ Create New Note</button>
-        </div>
-      </div>
-      
-          <Route
-            path="/"
-            exact
-            render={props => <NoteList {...props} notes={this.state.notes} />}
-          />
+      <div className="noteview">
+        <div className="sidebar">
+          <h1>Lambda</h1>
+          <h1>Notes</h1>
+          <div className="navigation">
+            <NavLink className="sidebarbutton" to="/" onClick={this.getNotes}>
+              View Your Notes
+            </NavLink>
+            <NavLink className="sidebarbutton" to="/create">
+              + Create New Note
+            </NavLink>
           </div>
-          </>
+        </div>
+        <Route
+          path="/"
+          exact
+          render={props => <NoteList {...props} notes={this.state.notes} />}
+        />
+        <Route
+          path="/create"
+          render={props => (
+            <CreateNote {...props} createNewNote={this.createNewNote} />
+          )}
+        />
+      </div>
     );
   }
 }
---
+++ lambda_notes/src/components/createnote.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class CreateNote extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tag: [],
+      title: "",
+      body: ""
+    };
+  }
+
+  handleInput = e => {
+    e.preventDefault();
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const note = {
+      title: this.state.title,
+      textBody: this.state.body
+    };
+    console.log(note);
+    axios
+      .post("https://fe-notes.herokuapp.com/note/create", note)
+      .then(res => {
+        console.log(res);
+        this.setState({ title: "", body: "" });
+      })
+      .catch(() => alert("Error adding note"));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Create New Note:</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            required
+            placeholder="Note Title"
+            name="title"
+            value={this.state.title}
+            onChange={this.handleInput}
+          />
+          <input
+            required
+            placeholder="Note Text"
+            name="body"
+            value={this.state.body}
+            onChange={this.handleInput}
+          />
+          <button>Create Note</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default CreateNote;
