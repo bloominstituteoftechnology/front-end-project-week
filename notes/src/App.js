8f15139d77dcd433a00865994c4b49e49dfcf147
Griffin <griffinshuth@gmail.com>
2018-11-14 20:00:51 -0700

    added note components

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Note from './components/cards/Note';
 import './App.css';
 
 class App extends Component {
   render() {
+    
     return (
       <div className="App">
-        <header className="App-header">
-         <h1>HERE IS A NOTE</h1>
-         <h4>HERE IS ANOTHER NOTE!</h4>
-         <h5>Am I done with the Project yet?</h5>
-         <h6>I think this is good enough for me is this a 2 for the project?</h6>
-        </header>
+      <Note/>
       </div>
     );
   }
---
+++ notes/src/components/cards/Note.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class Note extends Component {
+    constructior(props) {
+        super(props);
+        this.state = {
+            note: null,
+        };
+    }
+
+
+    fetchNote = id => {
+        axios.get(`https://fe-notes.herokuapp.com/note/get/all`)
+            .then(response => {
+                this.setState(() => ({ note: response.data }));
+            })
+            .catch(errpr => {
+                console.error(PleaseWork);
+            })
+    }
+    render() {
+
+    }
+    return(
+
+
+    )
+}
+
+
+export default Note;
