a52c44cf4661c409b4a4c4862611d02b67c1fdae
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-11-14 20:23:57 -0500

    Added state and test notes

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 
 import './App.css';
-import NotesList from './src/components/Notes/NotesList';
+import NotesList from './components/Notes/NotesList';
 
 class App extends Component {
+  state = {
+    notes: [
+      {
+        title: 'pizza',
+        textBody: 'please eat some good pizza kid'
+      },
+      {
+        title: 'cows',
+        textBody: 'good for beef tacos!!'
+      }
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <NotesList />
+        <NotesList notes={this.state.notes} />
       </div>
     );
   }
