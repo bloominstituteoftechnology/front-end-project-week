df0001aa3ca1117d389b685fe0808366462942be
Matt <matt@Matts-MacBook-Pro.local>
2018-11-15 15:12:25 -0500

    Setup componentDidMount and handleInput functions

--- notes/src/App.js
+++ notes/src/App.js
@@ -9,6 +9,36 @@ import CreateNote from './components/CreateNote';
 import EditNote from './components/EditNote';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      notes: [],
+      title: '',
+      text: '',
+      updatedTitle: '',
+      updatedText: '',
+      delete: false,
+      filteredNotes: [],
+      searchTerm: ''
+    }
+  }
+
+  componentDidMount() {
+    let url = 'https://fe-notes.herokuapp.com/note/get/all';
+    axios
+      .get(url)
+      .then(res => {
+        this.setState({ notes: res.data })
+      })
+      .catch(err => {
+        console.log('Error: ', err)
+      })
+  }
+
+  handleInput = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
   render() {
     return (
       
