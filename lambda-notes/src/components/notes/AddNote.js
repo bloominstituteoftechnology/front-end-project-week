82d9a3f14d1290322d281d41e0ebc7fc1e83b7be
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-11-15 09:40:58 -0800

    New Note field created.

--- lambda-notes/src/components/notes/AddNote.js
+++ lambda-notes/src/components/notes/AddNote.js
@@ -3,8 +3,13 @@ import './Notes.css';
 
 const AddNote = () => {
     return (
-        <div>
-            This is the add note component!
+        <div className="notes-list">
+            <h2 className="your-notes">Create New Note:</h2>
+                <form className="input-form">
+                    <input type="text" placeholder="Note Title" />
+                    <textarea type="text" placeholder="Note Content" />
+                    <button className="submit-button">Save</button>
+                </form>
         </div>
     )
 }
