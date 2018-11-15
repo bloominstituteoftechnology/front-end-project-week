9ebf6ec33103d59661004c9e458c05902d8c79de
Denise Escobar <descobar1490@gmail.com>
2018-11-14 20:18:38 -0600

    Notes are mapping out and I added a css reset

--- lambda-notes/src/components/Note.js
+++ lambda-notes/src/components/Note.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const Note = () =>{
+const Note = (props) =>{
     return(
-        <div>
-            One single Note
+        <div className="noteCard">
+            <h1>{props.note.title}</h1>
+            <p>{props.note.textBody}</p>
         </div>
     )
 }
--- lambda-notes/src/components/Notes.js
+++ lambda-notes/src/components/Notes.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import Note from './Note'
 
 class Notes extends Component {
+    constructor(){
+        super();
+        this.state ={
+            notes: []
+        }
+    }
+
+    componentDidMount() {
+        axios
+            .get('https://fe-notes.herokuapp.com/note/get/all')
+            .then(response => {
+                this.setState(()=>({notes:response.data}));
+            })
+            .catch(error => {
+                console.log("error", error);
+            });
+    }
+
     render(){
         return(
             <div>
-                A list of Notes
+                {this.state.notes.map( item => {
+                    return(
+                        <Note note={item} key={item.id}/>
+                    )
+                })}
             </div>
         )
     }
