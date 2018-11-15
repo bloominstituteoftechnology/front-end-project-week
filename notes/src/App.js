a3534eef51d14abd872c897e7d0131c81f037c66
Jennifer King <jenbellpgh@gmail.com>
2018-11-14 22:58:09 -0500

    Added Note.js/.css for viewing single note. Makes axios call to pull in note data. Correctly navigates back and forth between notelist and individual notes

--- notes/src/App.js
+++ notes/src/App.js
@@ -4,6 +4,7 @@ import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Nav from './Components/Nav/Nav';
 import Notes from './Components/NoteList/Notes';
+import Note from './Components/Note/Note';
 
 /* Main entry point for the Lambda Notes app.
  * This will primarily contains the routes needed for our app, along with
@@ -14,9 +15,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route exact path="/" component={Nav} />
-        <Route path="/" component={Notes} />
-        <Route path="/note/:id" component="" />
+        <Route path="/" component={Nav} />
+        <Route exact path="/" component={Notes} />
+        <Route path="/note/:id" component={Note} />
         <Route path="/edit/:id" component="" />
         <Route path="/add" component="" />
       </div>
---
+++ notes/src/Components/Note/Note.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+import './Note.css';
+
+/* Wanted to pass the note as state from within the link, but it keeps
+ * returning 'undefined'. Would be nice to use Redux here to eliminate
+ * the new axios call. Will have to refactor later. */
+
+class Note extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      note: []
+    };
+  };
+
+  componentDidMount() {
+    axios.get(`https://fe-notes.herokuapp.com/note/get/${this.props.match.params.id}`)
+      .then( (response) => {
+        this.setState( () => ({ note: response.data }) )
+      })
+      .catch( (error) => console.error(error) );
+  };
+
+  render() {
+    return (
+      <div className="viewNote">
+        <div className="noteLinks">
+          <Link to={`/edit/${this.state.note.id}`}>edit</Link>
+          <Link to={`/delete/${this.state.note.id}`}>delete</Link>
+        </div>
+        <div className="viewNoteTitle">{this.state.note.title}</div>
+        <div className="viewNoteText">{this.state.note.textBody}</div>
+      </div>
+    );
+  }
+};
+
+export default Note;
