a19e82453a29c35b2da9dca5175110d19077d84c
Matt <matt@Matts-MacBook-Pro.local>
2018-11-15 15:04:17 -0500

    Added empty components to app. Figuring out basic structure. No redux

---
+++ notes/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+import { Route } from 'react-router-dom';
+import NotesList from './components/NotesList';
+import Note from '/components/Note';
+import SideBar from './components/SideBar';
+import CreateNote from './components/CreateNote';
+import EditNote from './components/EditNote';
+
+class App extends Component {
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default App;
