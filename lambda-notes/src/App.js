bbf24fefb02027e94e1d1785a0a89a75034fa183
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-11-14 22:13:32 -0500

    created a state and am setting the external notes api to that state

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Route} from 'react-router-dom'
+import axios from 'axios'
+
 import './App.css';
+import SideBar from './components/SideBar';
+import ListNotes from './components/listView/ListNotes';
+import CreateNote from './components/newView/CreateNote';
+
 
 class App extends Component {
+
+  state = {
+    notes: []
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://fe-notes.herokuapp.com/note/get/all')
+      .then( response => {
+        console.log(response)
+        this.setState({notes: response.data})
+      })
+      .catch(err => console.log(err))
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <SideBar />
+      </div>
+      <div>
+        <Route exact path='/' render={ props => <ListNotes {...props} notes={this.state.notes}/>} />
+        <Route path='/create-note' component={CreateNote} />
+      </div>
+      
       </div>
     );
   }
