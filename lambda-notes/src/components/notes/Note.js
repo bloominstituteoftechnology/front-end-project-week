d9707be77393c3a49bc9e629395e6c025401a563
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-11-14 18:49:34 -0800

    Something rendering for each note.

---
+++ lambda-notes/src/components/notes/Note.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Friend extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    };
+
+    render() {
+        return (
+            <div>I am a note!</div>
+        )
+    }
+}
--- lambda-notes/src/components/notes/Notes.js
+++ lambda-notes/src/components/notes/Notes.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
 import './Notes.css';
+import Note from './Note';
 
-const Notes = () => {
-    return (
-        <div className="notes-list">
-            <h2 className="your-notes">Your Notes:</h2>
-        </div>
-    );
+const URL = 'https://fe-notes.herokuapp.com/note';
+
+export default class Notes extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            notes: [],        
+        };
+    }
+
+    componentDidMount() {
+        axios
+            .get(`${URL}/get/all`)
+            .then(response => {
+                this.setState(() => ({ notes: response.data }))
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
+    render() {
+        return (
+            <div className="notes-list">
+                <h2 className="your-notes">Your Notes:</h2>
+                {this.state.notes.map(note => (
+                    <Note key={note._id} note={note} />
+                ))}
+            </div>
+        );
+    }
 };
 
-export default Notes;
