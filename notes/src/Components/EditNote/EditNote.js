8d00892b6384a86c9be4fd3d2023b85a6eeb1206
Jennifer King <jenbellpgh@gmail.com>
2018-11-15 23:32:30 -0500

    Fixed the bug with my update handler. I had to make the axios.put request using a temp variable before using setState.

--- notes/src/Components/EditNote/EditNote.js
+++ notes/src/Components/EditNote/EditNote.js
@@ -36,28 +36,25 @@ class EditNote extends Component {
 
   // Need a handler for submitting & updating through axios
   handleSubmit = (event) => {
-    //debugger;
     event.preventDefault();
-    console.log( "Initial state: ", this.state );
 
-    // Set state - had to use a log cb function here because otherwise, state wasn't being set before calling next code
-    this.setState( (prevState) => (
-      {note: {
-        ...prevState.note, 
-        title: prevState.newTitle,
-        textBody: prevState.newText
-      },
-      newTitle: "",
-      newText: "",
-      redirect: false
-    }), console.log( "new state:", this.state ));
-    debugger;
-    console.log( "state:", this.state );
+    // Setting variable to avoid setting/updating axios through state.
+    // Was having a major block using state.
+    const newNote = Object.assign({}, this.state.note, {
+      title: this.state.newTitle,
+      textBody: this.state.newText
+    });
+    
     // Send to axios
-//    axios.put(`https://fe-notes.herokuapp.com/note/edit/${this.props.match.params.id}`, this.state.note )
+    axios.put(`https://fe-notes.herokuapp.com/note/edit/${this.props.match.params.id}`, newNote )
 
-    // Redirect to note display
-//    this.setState( {redirect: true});
+    // Redirect to note display & reset fields
+    this.setState( (prevState) => ({
+      note: { ...prevState.note, title: prevState.newTitle, textBody: prevState.newText },
+      redirect: true,
+      newTitle: "",
+      newText: ""
+    }) );
   };
 
   /* Send props:
