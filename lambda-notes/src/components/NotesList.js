f5c05b00fee55ac0f2e9eda064d4b0f7d10a45a0
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2018-11-16 08:54:00 -0600

    added function to add ellipsis to end of notes that are too long.

--- lambda-notes/src/components/NotesList.js
+++ lambda-notes/src/components/NotesList.js
@@ -5,6 +5,17 @@ class NoteList extends Component {
   constructor(props) {
     super(props);
   }
+
+  truncateString = (str, num) => {
+    if (str.length > num && num > 3) {
+      return str.slice(0, num - 3) + '...';
+    } else if (str.length > num && num <= 3) {
+      return str.slice(0, num) + '...';
+    } else {
+      return str;
+    }
+  };
+
   render() {
     // console.log('props ', this.props.notes);
     return (
@@ -13,11 +24,13 @@ class NoteList extends Component {
         <div className="noteList">
           {this.props.notes.map(note => {
             // console.log(note);
+            const body = this.truncateString(note.textBody, 210);
             return (
               <Link to={`/view/${note._id}`} key={note._id} className="note">
                 <p className="noteTitle">{note.title}</p>
                 <hr />
-                <p className="noteBody">{note.textBody}</p>
+                <p className="noteBody">{body}</p>
+                {/* <p className="noteBody">{note.textBody}</p> */}
               </Link>
             );
           })}
