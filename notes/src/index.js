63290f47ae9764a92ac8bcb0ff46a229cf6090be
Louis <7.louislee@gmail.com>
2018-11-15 17:32:24 -0800

    scroll to top of the page with Route

--- notes/src/index.js
+++ notes/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 
 ReactDOM.render(
   <Router>
-    <App />
+    <ScrollToTop>
+      <App />
+    </ScrollToTop>
   </Router>,
   document.getElementById("root")
 );
