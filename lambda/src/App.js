b47613e1e6cd892ec15b230b61cf0d4d2d9313fd
Megan Fontenot <lambda_school_loaner_28@Lambda-School-Loaner-28s-MacBook-Pro.local>
2018-11-15 22:06:39 -0600

    react router added.

--- lambda/src/App.js
+++ lambda/src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 
+
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = 
+    {
+
+    }
+  }
+
+  componentDidMount(){
+    //will fetch data from the api
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Did I break anything?</h1>
+        <h1>What about now?</h1>
       </div>
     );
   }
