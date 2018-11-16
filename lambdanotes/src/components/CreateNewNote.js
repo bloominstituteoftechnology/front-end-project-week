12a283af6a0e8d0cf1cd9484cf9a33608ea88ab4
paulapivat <paul.apivat@gmail.com>
2018-11-16 12:17:12 +0700

    styling noteview and create note component done

--- lambdanotes/src/components/CreateNewNote.js
+++ lambdanotes/src/components/CreateNewNote.js
@@ -2,6 +2,14 @@ import React from 'react'
 
 import { Link } from 'react-router-dom'
 
+import styled from "styled-components"
+
+
+const StyledLink = styled(Link)`
+    text-decoration: none;
+    color: white;
+`
+
 class CreateNewNote extends React.Component {
     constructor(){
         super();
@@ -25,7 +33,7 @@ class CreateNewNote extends React.Component {
 
     render(){
         return (
-            <div className="createnote-container">
+            <div className="note-container">
             <h2> Create New Notes: </h2>
                 <form>
                     <input 
@@ -45,7 +53,7 @@ class CreateNewNote extends React.Component {
                         onChange={this.handleChange}
                     />
                 </form>
-                <div className="button save" onClick={this.handleSubmit}><Link to="/notes" >Save</Link></div>
+                <div className="button save" onClick={this.handleSubmit}><StyledLink to="/notes" >Save</StyledLink></div>
             </div>
         )
     }
--- lambdanotes/src/components/NoteView.js
+++ lambdanotes/src/components/NoteView.js
@@ -32,13 +32,20 @@ class NoteView extends React.Component {
 
     render(){
         return (
-            <div key={this.state.note._id}>
-                <h2>Title: {this.state.note.title} </h2>
-                <p>Text Body: {this.state.note.textBody}</p>
-                <p>ID: {this.state.note._id}</p>
-                <p>Double_Underscore_V: {this.state.note.__v}</p>
-                <div onClick={this.handleDelete}><Link to="/notes" style={{cursor: "pointer"}} > X </Link></div>
-                <div><Link to={`/edit/${this.state.note._id}`}> Edit </Link></div>
+            <div>
+                <div className="note-menu">
+                    <div><Link to={`/edit/${this.state.note._id}`}> edit </Link></div>
+                    <div onClick={this.handleDelete}><Link to="/notes" style={{cursor: "pointer"}} > delete </Link></div>     
+                </div>
+
+                <div className="note-container">
+                    <div key={this.state.note._id}>
+                        <h2>Note Name: {this.state.note.title} </h2>
+                        <p>Text Body: {this.state.note.textBody}</p>
+                        <p>ID: {this.state.note._id}</p>
+                        <p>Double_Underscore_V: {this.state.note.__v}</p>
+                    </div>
+                </div>
             </div>
         )
     } 
