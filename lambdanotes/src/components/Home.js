ea192d134bc1a3bc9c3028a3a76950b464bc39d6
paulapivat <paul.apivat@gmail.com>
2018-11-17 22:12:23 +0700

    add login form to home component

--- lambdanotes/src/components/Home.js
+++ lambdanotes/src/components/Home.js
@@ -1,13 +1,53 @@
 import React from 'react'
 import logo from '../logo.svg';
 
-const Home = () => {
-    return (
-        <div>
-            <img src={logo} className="App-logo" alt="logo" />
-            <h3> Enter </h3>
-        </div>
-    )
+class Home extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            username: '',
+            password: '',
+            loggedIn: false,
+        }
+    }
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    handleSubmit = event => {
+        const user = this.state.username;
+        const password = this.state.password;
+        localStorage.setItem('user', user);
+        localStorage.setItem('password', password);
+        window.location.reload();
+    }
+
+    render(){
+        return (
+            <div>
+                <img src={logo} className="App-logo" alt="logo" />
+                <form>
+                    <input 
+                        placeholder="username"
+                        type="text"
+                        name="username"
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                    />
+                    <input 
+                        placeholder="password"
+                        type="text"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
+                </form>
+                <button onClick={this.handleSubmit}>Login</button>
+            </div>
+        )
+    }
 }
+
 
 export default Home
