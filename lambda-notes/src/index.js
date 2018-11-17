cb4690e2dc2752dd3eed3d652cd244895bd7ed29
Emmanium <pradoe.ep@gmail.com>
2018-11-17 11:54:09 -0700

    started importing stuff to get started refactoring code using redux

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import { BrowserRouter as Router } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import rootReducer from './reducers/';
 
+const middleware = applyMiddleware(thunk, logger);
+const store = createStore(rootReducer, middleware);
 const root = document.getElementById('root');
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>, root);
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>, root);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
