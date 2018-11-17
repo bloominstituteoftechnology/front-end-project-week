5da97906c2429cd3cb93f6c12a42a9ff6927804c
paulapivat <paul.apivat@gmail.com>
2018-11-18 02:46:34 +0700

    still working on sort functionality

--- lambdanotes/src/components/NotesList.js
+++ lambdanotes/src/components/NotesList.js
@@ -36,22 +36,18 @@ class NotesList extends React.Component {
                         )
                     })}
                 </div>
-                <div>
+                
                     <h2> SORTED Notes: </h2>
-            
+                <div className="notebox-container">
                     {this.props.notes.map(note => {
-                        emptyArr.push(note.title)
-                        console.log(typeof emptyArr)
+                        emptyArr.push(note.title + ' ')
+                        let emptyArr2 = emptyArr.sort()
+                        //console.log(emptyArr.sort())
                         return(
-                            <div key={note._id}>
-                                {/* <div>
-                                    {note.title.toUpperCase()}
-                                </div> */}
-                                <div>
-                                    {emptyArr.sort()}
-                                </div>
+                            <div className="notebox" key={note._id}>
+                                {emptyArr2.shift()}
                             </div>
-                        )
+                        )  
                     })}
                 </div>
             </div>
@@ -68,10 +64,11 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps, { getNotes })(NotesList)
-
-    
-//}
-
 //export default NotesList
 
+
 {/* <Link to={`/notes/${note._id}`}>{note.title.toUpperCase()}</Link> */}
+
+// {emptyArr2.map(e => {
+//     return e
+// })}
