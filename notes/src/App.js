f60adfd7ece8529094647a6489deddd99c38196c
Matthew Meitl <darksamus6@gmail.com>
2018-11-17 10:42:47 -0600

    added simple note form(not functioning) and got comments to display and added some routes

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,22 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Route } from "react-router-dom";
-import logo from './logo.svg';
-import './App.css';
-import NoteForm from './components/NoteForm';
+import logo from "./logo.svg";
+import "./App.css";
+import NoteForm from "./components/NoteForm";
+import NoteList from "./components/NoteList";
+import axios from "axios";
+
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-
+      notes: [{ _id: null }],
+      newId: 0
     };
-
   }
 
+  componentDidMount() {
+    axios
+      .get("https://fe-notes.herokuapp.com/note/get/all")
+      .then(response => {
+        this.setState({ notes: response.data });
+      })
+      .catch(err => console.log(err));
+  }
+
+  noteSubmit = (title, content) => {
+    this.setState({
+      notes: [
+        ...this.state.notes,
+        { title: title, textBody: content, id: this.state.newId }
+      ],
+      newID: this.state.newId + 1
+    });
+  };
 
   render() {
     return (
       <div className="App">
-      <Route path="/noteform" component={NoteForm} />
+        <Route
+          path="/noteform"
+          render={props => (
+            <NoteForm
+              {...props}
+              notes={this.state.notes}
+              id={this.state.newId}
+              noteSubmit={this.state.noteSubmit}
+            />
+          )}
+        />
+        <NoteList notes={this.state.notes} />
       </div>
     );
   }
--- notes/src/components/NoteForm.js
+++ notes/src/components/NoteForm.js
@@ -1,29 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class NoteForm extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-  
-      };
-  
-    }
-  
-  
-    render() {
-      return (
-        <div className="formdiv">
-        <form>
-          <input type="text" onChange={this.props.value} />
-          <input type="submit" value="Submit" onClick={this.props.submit} />
-          
-        </form>
-        </div>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      notes: ""
+    };
   }
-  
 
+  inputHandler = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
 
+  submitHandler = e => {
+    e.preventDefault();
+  };
 
-
+  render() {
+    return (
+      <div className="formdiv">
+        <form>
+          <input
+            type="text"
+            onChange={this.inputHandler}
+            name="title"
+            placeholder="Title"
+          />
+          <input
+            type="text"
+            onChange={this.inputHandler}
+            name="notes"
+            placeholder="Notes"
+          />
+          <input type="submit" value="Submit" onClick={this.props.submit} />
+        </form>
+      </div>
+    );
+  }
+}
