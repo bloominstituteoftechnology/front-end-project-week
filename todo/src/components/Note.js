1e4348a97d3380120080cc1d657c283e58b00dea
tommycollison <tommy@collison.ie>
2018-11-17 11:01:05 -0800

    link working

--- todo/src/components/Note.js
+++ todo/src/components/Note.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+// Link goes here?
+
 const Note = props => {
     return (
+        <Link to={`/notes/${props.id}`}>
         <div className="Note">
                 <h3>{props.title}</h3>
                 <p>{props.textBody}</p>
         </div>
+        </Link>
 
     )
 }
