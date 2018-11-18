9b52ddd295e343578441f3cb926ddc0360098fb8
Chris Juelfs <CRJuelfs24@comcast.net>
2018-11-18 16:37:04 -0600

    only making the main page refresh properly after edits & deletes is left

--- lambda/src/sub-components/noteProfile.js
+++ lambda/src/sub-components/noteProfile.js
@@ -3,7 +3,7 @@ import React from 'react';
 import axios from 'axios';
 
 import { connect } from 'react-redux';
-import { saveNoteId } from '../Actions/notesActions';
+import { deleteNoteAction } from '../Actions/notesActions';
 
 import { NavLink, Route } from 'react-router-dom';
 
@@ -30,27 +30,106 @@ class NoteProfile extends React.Component {
                 console.log(error);
             })
         // console.log(this.state.note);
-        this.props.saveNoteId(this.props.match.params.id);
+        // this.props.saveNoteId(this.props.match.params.id);
+    }
+    openDeleteModal = e => {
+        const deleteModal = document.querySelector('.modal');
+        if (deleteModal.style.display !== 'block') {
+            deleteModal.style.display = 'block';
+        }
+        // console.log(e.target.style);
+        // if (e.target.style.display !== 'block') {
+        //     e.target.style.display = 'block';
+        // }
+    }
+    closeDeleteModal = e => {
+        const deleteModal = document.querySelector('.modal');
+        if (deleteModal.style.display !== '') {
+            deleteModal.style.display = '';
+        }
+    }
+    deleteNoteHandler = e => {
+        // e.preventDefault();
+        // if (window.confirm('Are you sure you want to delete this note?')) {
+        //     this.props.deleteNoteAction(this.props.match.params.id);
+        // } 
+        // else {
+        //     console.log('user cancelled note deletion.')
+        // }
 
+        this.props.deleteNoteAction(this.props.match.params.id);
+        const deleteModal = document.querySelector('.modal');
+        // window.location.assign('http://localhost:3000/');
+        setTimeout(window.location.assign('http://localhost:3000/'), 5000);
+
+        // // window.location.reload();
+        // this.props.history.goBack(2);
+        // window.location.reload();
+        
+        // const deleter = this.props.deleteNoteAction;
+        // const past = this.props.history;
+        // const functionOne = function(value, callback) {
+        //     return (
+        //         deleter(value),
+        //         callback()
+        //     )
+        // }
+        // const functionTwo = function(value, callback) {
+        //     return (
+        //         past.go(-2),
+        //         callback()
+        //     )
+        // }
+        // const functionThree = function(value) {
+        //     return (
+        //         window.location.reload()
+        //     )
+        // }
+        // functionOne(this.props.match.params.id, function() {
+        //     functionTwo(null, function() {
+        //         functionThree()
+        //     })
+        // })
+
+        // setTimeout(this.props.deleteNoteAction(this.props.match.params.id), 10);
+        // setTimeout(this.props.history.goBack(2), 10);
+        // setTimeout(window.location.reload, 10);
+
+        // console.log(this.props.history.location);
+        // this.props.history.goBack();
+        // window.location.reload();
     }
 
     render() {
         return (
             <div className='note-view'>
-                <div>
-                    <div>
-                        <NavLink to={`/note/${this.props.match.params.id}/update-note`} onClick={this.props.pageReload}>edit</NavLink>
+                <div className="modal">
+                    <div className="modal-content">
+                        <span className="close">Are you sure you want to delete this?</span>
+                        <div>
+                            <button className='delete-button' onClick={this.deleteNoteHandler}>Delete</button>
+                            <button className='no-button' onClick={this.closeDeleteModal}>No</button>
+                        </div>
                     </div>
                 </div>
-                <div className='notes-sidebar'>
-                    <h1 className='title'>Lambda Notes</h1>
-                    <NavLink to='/' className='navlink' exact activeClassName='navlink-selected'><button onClick={this.props.pageReload}>View Your Notes</button></NavLink>
-                    <NavLink to='/create-new-note' className='navlink' exact activeClassName='navlink-selected'><button onClick={this.props.pageReload}>+ Create New Note</button></NavLink>
+                <div className='note-view-allcontent'>
+                    <div className='notes-sidebar'>
+                        <h1 className='title'>Lambda Notes</h1>
+                        <NavLink to='/' className='navlink' exact activeClassName='navlink-selected'><button onClick={this.props.pageReload}>View Your Notes</button></NavLink>
+                        <NavLink to='/create-new-note' className='navlink' exact activeClassName='navlink-selected'><button onClick={this.props.pageReload}>+ Create New Note</button></NavLink>
+                    </div>
+                    <div key={this.props.match.params.id} id={this.props.match.params.id} className='specific-note'>
+                        <div className='edit-delete-box'>
+                            <div className='edit-delete-section'>
+                                <NavLink to={`/note/${this.props.match.params.id}/update-note`} className='edit-navlink' onClick={this.props.pageReload}>edit</NavLink>
+                                <span onClick={this.openDeleteModal}><u>delete</u></span>
+                            </div>
+                        </div>
+                        <h4 className='specific-note-title'>{this.state.note.title}</h4>
+                        <p className='specific-note-textbody'>{this.state.note.textBody}</p>
+                    </div>
                 </div>
-                <div key={this.props.match.params.id} id={this.props.match.params.id} className='specific-note'>
-                    <h4 className='specific-note-title'>{this.state.note.title}</h4>
-                    <p className='specific-note-textbody'>{this.state.note.textBody}</p>
-                </div>
+                
                 {/* <Route exact path='/notes/:id/update-note' render={ (props) => <UpdateNote {...props} pageReload={this.windowReload} />} /> */}
             </div>
         )
@@ -70,4 +149,4 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps, {saveNoteId})(NoteProfile);
+export default connect(mapStateToProps, {deleteNoteAction})(NoteProfile);
