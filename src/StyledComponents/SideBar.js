454e6f25f159d53866669c57bd852128cec143b7
Sumayyah Asgar <sasgar04@gmail.com>
2018-11-17 16:44:48 -0800

    testing commits on github

--- src/StyledComponents/SideBar.js
+++ src/StyledComponents/SideBar.js
@@ -15,7 +15,7 @@ export const H1 = styled.h1`
 
 export const Button = styled.button`
   width: 100%;
-  height: 60px;
+  height: 50px;
   background-color: #2bc1c4;
   color: white;
   font-weight: 900;
