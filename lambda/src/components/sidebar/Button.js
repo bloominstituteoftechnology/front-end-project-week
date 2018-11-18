c268d8a9b89a2672e5eeee960b78d5c472d74000
Megan Fontenot <lambda_school_loaner_28@Lambda-School-Loaner-28s-MacBook-Pro.local>
2018-11-18 03:02:29 -0600

    added note files and imported axios and styled-components.

---
+++ lambda/src/components/sidebar/Button.js
@@ -0,0 +1,13 @@
+// Button is a styled Component
+
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button `
+background-color : #24B8BD;
+border : 2px solid #88A6A7;
+height:50px;
+width: 150px;
+`
+
+export default Button;
