caa584b12e57456cf3815911a29c654bae6d92b0
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-11-19 20:35:37 -0500

    added Sidebar component

---
+++ lambda-notes/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Lambda Notes</h1>
+        <div>View Your Notes</div>
+        <div>+ Create New Note</div>
+      </div>
+    )
+  }
+}
