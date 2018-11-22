c24b9b1314d0e3e00f2199a1d15b309418e578b1
paulapivat <paul.apivat@gmail.com>
2018-11-22 22:42:51 +0700

    add search functionality and some styling

--- lambdanotes/src/components/DisplayData.js
+++ lambdanotes/src/components/DisplayData.js
@@ -1,18 +1,35 @@
 import React from 'react'
+import "../styles/DisplayData.css"
+
+import { Link } from 'react-router-dom'
 
 class DisplayData extends React.Component {
 
     render(){
         return (
-            <div>
-                {this.props.dummyData.map(data => (
-                    <div key={data._id}>
-                        <p>{data.title}</p>
+
+            <div className="noteslist-container">
+                    <div className="notebox-container">
+                        {this.props.dummyData.map(data => (
+                        <div>
+                            <div className="notebox" key={data._id}>
+                                <h2>
+                                Title: <Link to={`/notes/${data._id}`}>{data.title.toUpperCase()}</Link>
+                                </h2>
+                                <div className="contentbox">
+                                    <p> <strong>Content:</strong> {data.textBody.slice(0, 100) + (data.textBody.length > 100 ? "..." : "")}</p>
+                                </div>
+                                <p> <strong>ID:</strong> {data._id}</p>
+                            </div>   
+                        </div>
+                        ))}
                     </div>
-                ))}
             </div>
+
         )
     }
 }
 
 export default DisplayData
+
+
