f2071a478ca8cf0b4b7045d264b6b09cefab3213
paulapivat <paul.apivat@gmail.com>
2018-11-25 23:15:50 +0700

    implement conditional rendering using local state

--- lambdanotes/src/components/ReactSort.js
+++ lambdanotes/src/components/ReactSort.js
@@ -3,6 +3,10 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { getNotes } from '../actions/noteAction'
 
+import '../styles/ReactSort.css'   //add X to className, else entangle with App.css
+
+
+
 class ReactSort extends React.Component {
     constructor(){
         super();
@@ -10,6 +14,8 @@ class ReactSort extends React.Component {
             notes: [],
             sortAZ: false,
             sortUN: false,
+            sortZA: false,
+            sortID: false,
         }
     }
 
@@ -17,6 +23,7 @@ class ReactSort extends React.Component {
         this.props.getNotes()
     }
 
+    //A-Z 
     sortObjProperty = (list, key) => {
         const compare = (a, b) => {
             a = a[key];
@@ -31,30 +38,69 @@ class ReactSort extends React.Component {
         return list.sort(compare);
     }
 
+    //Z-A or ID
+    sortObjProperty2 = (list, key) => {
+        const compare = (a, b) => {
+            a = a[key];
+            b = b[key];
+            const type = (typeof(a) === 'string' || 
+                        typeof(b) === 'string') ? 'string' : 'number';
+            let result;
+            if (type === 'string') result = b.localeCompare(a);   //reverses the order Z-A
+            else result = b - a;                                  //reverses the order Z-A
+            return result;
+        }
+        return list.sort(compare);
+    }
 
+    //NEED separate sortClick for each "sort" variation
     sortClick = () => {
         this.setState(prevState => ({
-            sortAZ: !prevState.sortAZ
+            sortAZ: !prevState.sortAZ, sortUN: false, sortZA: false, sortID: false
         }));
     }
 
     sortClick2 = () => {
         this.setState(prevState => ({
-            sortUN: !prevState.sortUN
+            sortUN: !prevState.sortUN, sortAZ: false, sortZA: false, sortID: false
         }))
     }
 
+    sortClick3 = () => {
+        this.setState(prevState => ({
+            sortZA: !prevState.sortZA, sortUN: false, sortAZ: false, sortID: false
+        }))
+    }
+
+    sortClick4 = () => {
+        this.setState(prevState => ({
+            sortID: !prevState.sortID, sortUN: false, sortZA: false, sortAZ: false
+        }))
+    }
 
     render(){
         const emptyArr = [];
 
-        //A-Z
+        //A-Z or Z-A or ID
         {this.props.notes.map(note => {
             emptyArr.push(note)
         })}
 
         return (
             <div>
+                <div className="dropdownX" style={{float: "right"}}>
+                    <button className="dropbtnX">Dropdown</button>
+                    <div className="dropdown-contentX">
+                        <div onClick={this.sortClick} style={{cursor: "pointer"}}><h2>A-Z</h2></div>
+                        <div onClick={this.sortClick2} style={{cursor: "pointer"}}><h2>Unsorted</h2></div>
+                        <div onClick={this.sortClick3} style={{cursor: "pointer"}}><h2>Z-A</h2></div>
+                        <div onClick={this.sortClick4} style={{cursor: "pointer"}}><h2>ID</h2></div>
+                        <a href="#">Link 2</a>
+                        <a href="#">Link 3</a>
+                    </div>
+                </div>
+
+
 
                 <div>
                     ReactSort Testing: A-Z
@@ -92,12 +138,50 @@ class ReactSort extends React.Component {
                     </div>
                 </div> 
 
+                <div>
+                    ReactSort Testing: Z-A
+                    <div onClick={this.sortClick3}>
+                        <h1>{this.state.sortZA ? 'unsort' : 'z-a'}</h1>
+                        {this.state.sortZA && (
+                            <div>
+                                {this.sortObjProperty2(emptyArr, 'title').map(obj => {
+                                    return (
+                                        <div className="notebox" key={obj._id}>
+                                            Title: <h2>{obj.title.toUpperCase()}</h2>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        )}
+                    </div>
+                </div>
+
+                <div>
+                    ReactSort Testing: by ID
+                    <div onClick={this.sortClick4}>
+                        <h1>{this.state.sortID ? 'unsort' : 'ID'}</h1>
+                        {this.state.sortID && (
+                            <div>
+                                {this.sortObjProperty2(emptyArr, '_id').map(obj => {   //sortObjProperty2 same as Z-A
+                                    return (
+                                        <div className="notebox" key={obj._id}>
+                                            Title: <h2>{obj.title.toUpperCase()}</h2>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        )}
+                    </div>
+                </div>
 
 
             </div>
         )
     }
 }
+
+
+
 
 const mapStateToProps = state => {
     return {
