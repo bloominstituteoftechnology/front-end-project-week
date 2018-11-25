97d94f06645f836659e3d48c7e0c46c99c2f546b
cferrell2 <cferrell12182@gmail.com>
2018-11-25 13:52:12 -0500

    Add functionality to NoteCard.js- determine if note is too long to fit completely inside card and add ellipsis if too long

--- lambdanotes/src/components/Note.js
+++ lambdanotes/src/components/Note.js
@@ -15,7 +15,6 @@ class Note extends Component{
     render(){
 
         const {id} = this.props.match.params;
-        
         return (
             <div>
             <nav>
--- lambdanotes/src/components/NoteCard.js
+++ lambdanotes/src/components/NoteCard.js
@@ -3,12 +3,17 @@ import {Link} from 'react-router-dom'
 import './style.css'
 
 const NoteCard = ({note})=> {
+
+  //Determine if note is too long to fit inside card, and add ellipsis if so
+  let noteText = ''
+  noteText = (note.textBody.length > 400) ? note.textBody.slice(0,145).concat('',' ...') : note.textBody
+
   return (
     <Link to={`note/${note._id}`} style={{textDecoration:"none"}} className="noteCard">
       <div className="noteContent">
 
           <h3>{note.title}</h3>
-          <p>{note.textBody}</p>
+          <p>{noteText}</p>
       </div>
     </Link>
   )
