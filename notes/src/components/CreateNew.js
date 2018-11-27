e4b5452be61f2ec24cde0e6246abf1b86b9c1498
Matthew Meitl <darksamus6@gmail.com>
2018-11-26 19:57:00 -0600

    updated button text

--- notes/src/components/CreateNew.js
+++ notes/src/components/CreateNew.js
@@ -31,7 +31,7 @@ export default class CreateNew extends React.Component {
         <form onSubmit={this.submitHandler}>
           <input onChange={this.inputHandler} value={this.state.title} name="title" placeholder="Note Title" className='input' type='text'/>
           <input onChange={this.inputHandler} name="content" value={this.state.content} placeholder="Note Content" className="text-area" type='text'/> 
-          <button type="submit" className="buttonTwo">Save</button> 
+          <button type="submit" className="buttonTwo">Submit</button> 
         </form> 
       </div> 
     )
