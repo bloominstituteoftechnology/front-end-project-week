1a9889c550fc21cb415c217c9fce3ba45c04ab32
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-11-27 17:01:46 -0500

    Adding styled components for easier styling/readable code.

--- lambda-notes/src/components/NoteContainer/Note.js
+++ lambda-notes/src/components/NoteContainer/Note.js
@@ -1,14 +1,41 @@
-import React from 'react'
-import './NoteContainer.css'
+import React from 'react';
+// import './NoteContainer.css';
+import styled from 'styled-components' 
+
+const NoteContainer = styled.section `
+  display: flex ;
+  flex-direction: row ;
+  flex-wrap: wrap ;
+`
+const NoteStyling = styled.div ` 
+ border: 1px solid black ;
+`
+
+const NoteTitle = styled.div `
+ font-size: 1rem ;
+ font-family: 'Archivo Black';
+ overflow: hidden;
+ text-overflow: ellipsis;
+ width: 100px ;
+`
+const NoteBody = styled.article ` 
+ width: 150px ;
+ height: 100px ;
+ overflow: hidden;
+ text-overflow: ellipsis ;
+ font-family: 'Courier New', Courier, monospace ;
+ font-size: 0.8rem ;
+`
+
 const Note = props => {
   return (
     <div>
-     <section className="note_container">
-      <div className="note_styling">
-       <div className="note_title">{props.title}</div>
-        <article className="note_body">{props.textBody}</article>
-      </div>
-     </section>
+     <NoteContainer>
+      <NoteStyling>
+       <NoteTitle>{props.title}</NoteTitle>
+        <NoteBody className="note_body">{props.textBody}</NoteBody>
+      </NoteStyling>
+     </NoteContainer>
     </div>
   )
 }
