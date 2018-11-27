c532ab8e2bb45e6b3ab3f595dc829b3d4c65f7cb
cferrell2 <cferrell12182@gmail.com>
2018-11-26 20:43:06 -0500

    revise code to determine when to use ellipsis

--- lambdanotes/src/components/NoteCard.js
+++ lambdanotes/src/components/NoteCard.js
@@ -6,13 +6,15 @@ const NoteCard = ({note})=> {
 
   //Determine if note is too long to fit inside card, and add ellipsis if so
   let noteText = ''
-  noteText = (note.textBody.length > 400) ? note.textBody.slice(0,145).concat('',' ...') : note.textBody
-
+  console.log(note.textBody.length);
+  noteText = (note.textBody.length > 140) ? note.textBody.slice(0,140).concat('',' ...') : note.textBody
+  console.log(noteText);
   return (
     <Link to={`note/${note._id}`} style={{textDecoration:"none"}} className="noteCard">
       <div className="noteContent">
 
           <h3>{note.title}</h3>
+          {console.log('before display',noteText)}
           <p>{noteText}</p>
       </div>
     </Link>
