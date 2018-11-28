740caba25ffb3424d134d6e6c5aab853a8755ee9
cocoitali <cmburatto@gmail.com>
2018-11-27 20:27:24 -0700

    MVP (added state and container components to App, preparingaxios)

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+import SideBarContainer from "./components/sidebar/SideBarContainer";
+import NotesContainer from "./components/notes/NotesContainer";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      notes: [],
+      title: '',
+      txt: ''
+    };
+  }
+
   render() {
     return (
       <div className="App">
-      
+        <SideBarContainer />
+        <NotesContainer />
       </div>
     );
   }
