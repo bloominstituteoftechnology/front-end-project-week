04ceb37ab2b9a6bfd1c90ce619c9b55b4e7fea02
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2018-11-28 14:50:45 -0600

    Sidenav compenent added to app

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import NoteList from './components/noteViews/NoteList';
+
 import './App.css';
+
+import NoteList from './components/noteViews/NoteList';
+import SideNav from './components/SideNav';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <SideNav/>
       <NoteList/>
       </div>
     );
