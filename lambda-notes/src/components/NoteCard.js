1214332a7d106b68ac6c853896ff8c45ead4c870
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2018-11-28 17:08:18 -0600

    note cards stulyling plus note list are now spaced evenly and wrap inside the container

--- lambda-notes/src/components/NoteCard.js
+++ lambda-notes/src/components/NoteCard.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const NoteCard = props => {
   return (
-    <div className="main-container">
-      <div className="header-container">
+    <div className="card-main-container">
+      <div className="card-header-container">
         <h3>{props.title}</h3>
       </div>
       {/* just show preview for now, implement SUMMRY api when mvp is done */}
-      <div className="body-container">
+      <div className="card-body-container">
         <p className="text-body">{props.textBody}</p>
       </div>
     </div>
