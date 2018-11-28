5bba5fd5534ac8447a4e7cf78c64752371d13607
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2018-11-28 11:02:36 -0600

    note component done

---
+++ lambda-notes/src/components/note.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Note = props => {
+  return(
+    <div className="note-container">
+      <h2>{props.title}</h2>
+      <div className="body-container">
+        <p className="text-body">
+          {props.textBody}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Note;
