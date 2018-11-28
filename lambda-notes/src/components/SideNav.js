5566fea69aea896c84fad80294d805b6f3cf7b28
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2018-11-28 11:38:31 -0600

    note card basic structure done

---
+++ lambda-notes/src/components/SideNav.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+class SideNav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHidden: false,
+    }
+  }
+  render() { 
+    return (
+      <div className="main-container">
+        <div className="header-container">
+          <h2>NOTES APP</h2>
+          <i className="fas fa-arrow-left"></i>
+        </div>
+        <div className="buttons-search-container">
+          <div className="view-button">View Notes</div>
+          <div className="new-button">New Notes +</div>
+        </div>
+        <div className="search-container">
+          <input 
+            type="text"
+            placeholder="Search"
+            className="search-imput"
+            value={this.props.handleSearchValue}
+            onChange={this.props.handleOnChange} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SideNav;
