2e9992c3be1d839bc62cce7f890c07a8caf1c6cd
Brandon <brandon.g.elzy@gmail.com>
2018-11-28 20:11:42 -0600

    Brandon G. Elzy - Add circular progression

--- client/src/components/Notes/index.js
+++ client/src/components/Notes/index.js
@@ -2,33 +2,60 @@ import React, { Component } from 'react';
 import Note from './Note/index';
 import { getAllNotes } from '../actions/index';
 import { connect } from 'react-redux';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { withStyles } from '@material-ui/core/styles';
 
 const styles = {
-  display: 'flex',
-  margin: '100px auto 0px auto',
-  width: '90vw',
-  maxWidth: '1400px',
-  flexWrap: 'wrap',
-  justifyContent: 'center',
-  border: '1px solid black'
+  notes: {
+    display: 'flex',
+    margin: '100px auto 0px auto',
+    width: '90vw',
+    maxWidth: '1400px',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    // border: '1px solid black',
+  },
+  progress: {
+    marginTop: '150px'
+  },
 }
+
 
 class Notes extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      notesAreLoading: true,
+    }
+
   }
 
   componentDidMount() {
     this.props.getAllNotes()
+    setTimeout(() =>{
+      this.setState({ notesAreLoading: false });
+
+    }, 1000);
   }
 
   render() {
 
+    const { classes } = this.props;
+
     const notes = [...this.props.notes];
+    const loadNotes = () => {
+
+      if (this.props.notes.length > 0 && this.props.isFetching === false && this.state.notesAreLoading === false) {
+        return notes.map( (note, i) => <Note key={note._id} title={note.title} text={note.textBody}/>);
+      } else {
+        return <CircularProgress size={100} thickness={2.0} className={classes.progress} />
+      }
+    }
 
     return (
-      <div style={styles}>
-        {notes.map( (note, i) => <Note key={i} title={note.title} text={note.textBody}/>)}
+      <div style={styles.notes}>
+        {loadNotes()}
       </div>
     );
   }
@@ -49,4 +76,4 @@ const mapDispatchToProps = dispatch => {
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Notes);
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(Notes));
