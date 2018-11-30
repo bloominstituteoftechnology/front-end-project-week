762b669932e14f1145e3bbf052013386d2d1058c
cocoitali <cmburatto@gmail.com>
2018-11-29 20:28:24 -0700

    Added content to NoteView, not currently wired up)

--- notes/src/components/NoteView.js
+++ notes/src/components/NoteView.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from "react";
+
+import { NoteCardTitle, NoteCardText } from "./Styled";
 
 const NoteView = props => {
-    return (
-        <div />
-    )
-}
+  return (
+    <div>
+      <div>
+        <span>edit</span>
+        <span>delete</span>
+      </div>
+
+      <NoteCardTitle>{props.note.title}</NoteCardTitle>
+      <NoteCardText>{props.note.textBody}</NoteCardText>
+    </div>
+  );
+};
+
+NoteView.defaultProps = {
+  title: "",
+  textBody: ""
+};
 
 export default NoteView;
