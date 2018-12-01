4ea030222765f6e8f04f387ad6544d7cd72bce8f
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2018-12-01 14:47:37 -0500

    (MVP : SIDEBAR) - added Link to view notes

--- lambda-notes/src/components/Sidebar/index.js
+++ lambda-notes/src/components/Sidebar/index.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
     return (
-        <div>
-            Behold, the sidebar component
+        <div className="sidebar-container">
+            <h1>
+                Lambda <br />
+                Notes
+            </h1>
+            <Link to="/">
+                <button>
+                    View Your Notes
+                </button>
+            </Link>
         </div>
     );
 }
