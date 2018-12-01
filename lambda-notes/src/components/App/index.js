8a0e5b804e8bab8f19f21d6c9be3b8e6811daed2
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2018-12-01 14:59:41 -0500

    (MVP : INDEX) - added home and Form routes

--- lambda-notes/src/components/App/index.js
+++ lambda-notes/src/components/App/index.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Sidebar from "../Sidebar";
+import NoteList from "../NoteList";
+import Form from "../Form";
 import "./index.css";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                Lambda Notes
+                <Sidebar />
+                <Route exact path="/" component={NoteList} />
+                <Route path="notes/add" component={Form} />
             </div>
         )
     }
