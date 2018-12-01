77b333d64ec486e911f33b70ddcfc823c8fa661c
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2018-12-01 15:40:18 -0500

    (MVP : AXIOS) - imported axios and added REST functions in app/index.js

--- lambda-notes/src/components/App/index.js
+++ lambda-notes/src/components/App/index.js
@@ -3,9 +3,40 @@ import { Route } from "react-router-dom";
 import Sidebar from "../Sidebar";
 import NoteList from "../NoteList";
 import Form from "../Form";
+import axios from "axios";
 import "./index.css";
 
+
 class App extends Component {
+    state = {
+        notes: [],
+        title: "",
+        textBody: ""
+    }
+
+    componentDidMount() {
+        this.refetchNotes();
+    };
+
+    handleInputChange = event => {
+        this.setState(
+            { [event.target.name]: event.target.value }
+            );
+    };
+
+    handleFormSubmit() {}
+
+    refetchNotes() {
+        axios
+            .get("https://fe-notes.herokuapp.com/note/get/all")
+            .then(response => {
+                this.setState(
+                    {notes: response.data}
+                )
+            });
+            // .catch(error => console.log(error));
+    };
+
     render() {
         return (
             <div className="App">
