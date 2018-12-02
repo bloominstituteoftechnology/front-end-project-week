ae7f9c36e45391de32481014569b026e7bd71140
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2018-12-01 20:13:52 -0500

    (MVP : NOTELIST) - created file structure to render notes

--- lambda-notes/src/components/NoteList/index.js
+++ lambda-notes/src/components/NoteList/index.js
@@ -1,11 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const NoteList = () => {
+const NoteList = props => {
+    if (!props.notes) {
     return (
-        <div>
-            Reserved for NoteList component
+        <div>Notes are loading...</div>
+        )
+    }
+
+    const notes = props.notes;
+
+    return (
+        <div className="your-notes-container">
+            <h2>Your Notes:</h2>
+            <div className="notes-previews-container">
+                {notes.map(note => {
+                    return(
+                        <Link to={`/notes/${note.id}`} key={Math.random()}>
+                            <div className="note-preview-container">
+                                <h3>{note.title}</h3>
+                                <p>{note.textBody}</p>
+                            </div>
+                        </Link>
+                    );
+                })}
+            </div>
         </div>
     );
-}
+};
 
 export default NoteList;
