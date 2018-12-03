79d981091eb29dcc8e5a234e9a5c790a8f537ba8
Orlando Castillo <omaddoc@gmail.com>
2018-12-03 14:56:28 -0500

    functionality

--- lambda-notes/src/component/NoteList.js
+++ lambda-notes/src/component/NoteList.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import axios from 'axios';
 import { Button, Modal, ModalBody, ModalFooter } from 'reactstrap';
-class NoteView extends React.Component {
+
+class NoteList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       note: [],
       modal: false,
       editing: false,
-      editedText: ''
+      editedTitle: '',
+      editedTextBody: ''
     };
     this.toggle = this.toggle.bind(this);
   }
@@ -16,24 +18,7 @@ class NoteView extends React.Component {
     const { id } = this.props.match.params;
     this.getNote(id);
   }
-  toggle() {
-    this.setState({
-      modal: !this.state.modal
-    });
-  }
-  deleteNote = id => {
-    axios
-      .delete(`https://fe-notes.herokuapp.com/note/delete/${id}`)
-      .then(response => {
-        console.log('response', response);
-      })
-      .catch(error => console.log(error));
-  };
-  deleteHandler = event => {
-    event.preventDefault();
-    this.deleteNote(this.state.note._id);
-    this.props.history.push('/');
-  };
+
   getNote = id => {
     axios
       .get(`https://fe-notes.herokuapp.com/note/get/${id}`)
@@ -42,9 +27,71 @@ class NoteView extends React.Component {
       })
       .catch(error => console.log(error));
   };
+
+  deleteNote = id => {
+    axios
+      .delete(`https://fe-notes.herokuapp.com/note/delete/${id}`)
+      .then(response => {
+        console.log('response', response);
+      })
+      .catch(error => console.log(error));
+  };
+
+  editNote = id => {
+    axios
+      .put(`https://fe-notes.herokuapp.com/note/edit/${id}`, this.state.note)
+      .then(response => {
+        console.log('response', response);
+        this.setState({ note: response.data });
+      })
+      .catch(error => console.log(error));
+  };
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
+  deleteHandler = event => {
+    event.preventDefault();
+    this.deleteNote(this.state.note._id);
+    this.props.history.push('/');
+  };
+
+  changeHandler = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  editHandler = event => {
+    event.preventDefault();
+    this.setState({
+      editing: true,
+      editedTitle: this.state.note.title,
+      editedTextBody: this.state.note.textBody
+    });
+  };
+
+  saveHandler = event => {
+    event.preventDefault();
+    this.setState({
+      editing: false,
+      note: {
+        title: this.state.editedTitle,
+        textBody: this.state.editedTextBody,
+        _id: this.state.note._id,
+        tags: []
+      }
+    });
+    setTimeout(() => {
+      this.editNote(this.state.note._id);
+    }, 500);
+  };
+
   render() {
     let viewStyle = {};
     let editStyle = {};
+
     if (this.state.editing) {
       viewStyle.display = 'none';
     } else {
@@ -55,23 +102,32 @@ class NoteView extends React.Component {
         <div style={viewStyle} onDoubleClick={this.editHandler}>
           <h2>{this.state.note.title}</h2>
           <p>{this.state.note.textBody}</p>
+          <Button onClick={this.editHandler}>Edit</Button>
         </div>
-        <input
-          name="title"
-          type="text"
-          style={editStyle}
-          onKeyDown={this.editSubmitHandler}
-          onChange={this.ChangeHandler}
-          value={this.state.editedText}
-        />
-        <input
-          name="textBody"
-          type="textarea"
-          style={editStyle}
-          onKeyDown={this.editSubmitHandler}
-          onChange={this.ChangeHandler}
-          value={this.state.editedText}
-        />
+        <form onSubmit={this.saveHandler}>
+          <input
+            name="editedTitle"
+            type="text"
+            style={editStyle}
+            onKeyDown={this.submitHandler}
+            onChange={this.changeHandler}
+            value={this.state.editedTitle}
+          />
+
+          <input
+            name="editedTextBody"
+            type="textarea"
+            style={editStyle}
+            onKeyDown={this.submitHandler}
+            onChange={this.changeHandler}
+            value={this.state.editedTextBody}
+          />
+        </form>
+
+        <Button onClick={this.saveHandler} style={editStyle}>
+          Save
+        </Button>
+
         <div>
           <Button color="danger" onClick={this.toggle}>
             {this.props.buttonLabel}Delete
@@ -96,4 +152,5 @@ class NoteView extends React.Component {
     );
   }
 }
-export default NoteView;
+
+export default NoteList;
