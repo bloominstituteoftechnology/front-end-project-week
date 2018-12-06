41bd1eaff744a3826014183f47c8cccadd00b50a
ykrueng <yasirah.krueng@gmail.com>
2018-12-06 14:49:08 -0600

    set up Redux store and localstorage that subscribe to the store

--- notes/src/index.js
+++ notes/src/index.js
@@ -1,11 +1,28 @@
 // importing libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 
 // importing components
 import App from './App';
 
+// use local storage for data persistance
+const persistedData = JSON.parse(window.localStorage.getItem('redux-notes')) || [];
+
+// Redux Store
+// TODO: change first input to reducers
+const store = createStore(()=>{}, persistedData);
+
+// subscribe local storage to get updated data
+store.subscribe(() => {
+  localStorage.setItem('redux-notes', JSON.stringify(store.getState()));
+})
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.querySelector('#root')
 );
