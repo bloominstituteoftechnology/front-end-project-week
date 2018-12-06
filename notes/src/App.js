538c5afdc75de3cfe699c102170dd61cc7587cd7
ykrueng <yasirah.krueng@gmail.com>
2018-12-06 14:35:10 -0600

    initial commit

---
+++ notes/src/App.js
@@ -0,0 +1,12 @@
+// importing libraries
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>React - Notes</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ notes/src/index.js
@@ -0,0 +1,11 @@
+// importing libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// importing components
+import App from './App';
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
