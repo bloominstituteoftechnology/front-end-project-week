c155c60f7a4d3595e1fe22d9119b01e70c332d28
bummings <eabummings@gmail.com>
2018-12-07 17:47:42 -0500

    fixed nav reload issue

--- lamba-notes/src/components/Nav.js
+++ lamba-notes/src/components/Nav.js
@@ -1,16 +1,16 @@
-import React from "react";
-// import { NavLink } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Nav extends React.Component {
   render() {
     return (
-      <div className="sideBar">
-        <h1 className="sideBar__headline">
+      <div className='sideBar'>
+        <h1 className='sideBar__headline'>
           Lambda
           <br />
           Notes
         </h1>
-        <div className="link-section">
+        <div className='link-section'>
           {/* <NavLink exact to="/" activeClassName="link">
             <button class="btn">View Your Notes</button>
           </NavLink>
@@ -18,12 +18,21 @@ class Nav extends React.Component {
           <NavLink to="/submit" activeClassName="link">
             <button class="btn">+ Create New Note</button>
           </NavLink> */}
-          <a className="nav__btn" href="/">
+
+          <Link className='nav__btn' to={`/`}>
             View Your Notes
-          </a>
-          <a className="nav__btn" href="/submit">
+          </Link>
+
+          <Link className='nav__btn' to={`/submit`}>
             Create New Note
-          </a>
+          </Link>
+          {/* <a className="nav__btn" href="/">
+            View Your Notes
+          </a> */}
+
+          {/* <a className="nav__btn" href="/submit">
+            Create New Note
+          </a> */}
         </div>
       </div>
     );
