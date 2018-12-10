fb5f0e7ee230794d6787ae4edf706d874a2f4701
Kat Fries <katherynejf@gmail.com>
2018-12-10 15:28:16 -0500

    changed package.json

--- notes/src/components/SingleNote.js
+++ notes/src/components/SingleNote.js
@@ -3,10 +3,16 @@ import { Link } from 'react-router-dom';
 
 const SingleNote = props => {
   return (
-    <div className='single-note-container'>
-    <Link to={`/note/${props.note._id}`} className='note-title'>{props.note.title}</Link>
-    <Link to={`/note/${props.note._id}`} className='note-content'>{props.note.textBody}</Link>
-      {/* <li className='note-content'>{props.note.textBody}</li> */}
+    <div 
+      className='single-note-container'>
+      <Link 
+        to={`/note/${props.note._id}`} 
+        className='note-title'>{props.note.title}
+      </Link>
+      <Link 
+        to={`/note/${props.note._id}`} 
+        className='note-content'>{props.note.textBody}
+      </Link>
     </div> 
   )
 }
