7ab4b23e091a12a6e8fb40589bcd51212b4fb32f
Dennis Orbison <orbison.dennis@gmail.com>
2018-12-10 09:28:32 -0800

    fundamental layout created

---
+++ lambda-notes/src/Components/MainContent.js
@@ -0,0 +1,7 @@
+import React from 'react'; 
+
+const MainContent = props => {
+  return <h1 className='main-content'>Main Content</h1>
+}
+
+export default MainContent;
---
+++ lambda-notes/src/Components/SideBar.js
@@ -0,0 +1,9 @@
+import React from 'react'; 
+
+const SideBar = props => {
+  return (
+    <div className='side-bar'>Side Bar</div>
+  )
+}
+
+export default SideBar;
