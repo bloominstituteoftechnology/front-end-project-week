79dc094c29d5caf5242d5f181a11d2b61e68dad2
steelworx <eileencuevas62@gmail.com>
2018-12-10 18:21:22 -0500

    implemented serach bar

--- lambda-notes/src/components/NotesList.js
+++ lambda-notes/src/components/NotesList.js
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 const NotesContainer = styled.div`
+    width: 100%;
     padding: 3rem;
     margin-left: 22rem;
 
@@ -12,6 +13,24 @@ const NotesContainer = styled.div`
         margin: 3rem 0;
         font-size: 2rem;
         font-weight: bold;
+    }
+`
+
+const TopOfPage = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+const SearchInput = styled.input`
+    border: none;
+    border-bottom: 1px solid #AFAFAF;
+    background-color: #F2F1F2;
+    color: #494745;
+
+    &:focus {
+        outline: none;
     }
 `
 
@@ -28,19 +47,46 @@ const NotesLink = styled(Link)`
     }
 `
 
-const NotesList = props => {
-    return(
-        <NotesContainer>
-            <h2>Your Notes:</h2>
-            <YourNotes>
-                {props.notes.map(note => (
-                    <NotesLink key={note._id} exact to={`/note/${note._id}`}>
-                        <SmallNote  noteData={note} />
-                    </NotesLink>
-                ))}
-            </YourNotes>
-        </NotesContainer>
-    );
+class NotesList extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            searchText: '',
+        }
+    }
+
+    handlesChanges = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+    render(){
+        return(
+            <NotesContainer>
+                <TopOfPage>
+                    <h2>Your Notes:</h2>
+                    <form>
+                        <SearchInput 
+                            type='text'
+                            name='searchText'
+                            value={this.state.searchText}
+                            placeholder='&#8981; Search...'
+                            onChange={this.handlesChanges}
+                        />
+                    </form>
+                </TopOfPage>
+                <YourNotes>
+                    {this.props.notes
+                        .filter(note => 
+                            note.title.includes(this.state.searchText) || note.textBody.includes(this.state.searchText)
+                            )
+                        .map(note => (
+                        <NotesLink key={note._id} exact to={`/note/${note._id}`}>
+                            <SmallNote  noteData={note} />
+                        </NotesLink>
+                    ))}
+                </YourNotes>
+            </NotesContainer>
+        );
+    }
 } 
 
 export default NotesList;
