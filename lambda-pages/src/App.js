9a21b1faa665625ba9c469c60fe29a4116cf46fa
Angelo Deleon <rudy209@comcast.net>
2018-12-09 21:31:31 -0800

    Initial project Setup

---
+++ lambda-pages/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+// Components
+import CreateNoteView from './views/CreateNoteView';
+import EditNoteView from './views/EditNoteView';
+import NoteView from './views/NoteView';
+import NotesView from './views/NotesView';
+import Sidebar from './components/Sidebar';
+
+
+// Styles
+import './styles/App.css';
+ class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Sidebar />
+        {/* Routes */}
+        <Route path="/" component={NotesView} />
+        <Route path="/create-note" component={CreateNoteView} />
+        <Route path="/edit-note/:id" component={EditNoteView} />
+        <Route path="/note/:id" component={NoteView} />
+      </div>
+    );
+  }
+}
+ export default App;
---
+++ lambda-pages/src/views/NotesView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+ class NotesView extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Notes View</h1>
+      </div>
+    );
+  }
+}
+
+ export default NotesView;
