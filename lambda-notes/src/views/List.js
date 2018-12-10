14fc109f45d0922eb4459d1f95546997494b2058
FreitagDavid <freitagdavid@outlook.com>
2018-12-10 14:35:12 -0500

    views/List: Gonna break this down into some views

--- lambda-notes/src/views/List.js
+++ lambda-notes/src/views/List.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import NotesList from '../components/NotesList';
+import styleVars from '../helpers/styleVars';
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+    color: ${styleVars.pageHeaderColor};
+`;
+
 const List = props => {
     return (
         <>
-            <h1>Your Notes:</h1>
+            <H1>Your Notes:</H1>
             <NotesList notes={props.notes} />
         </>
     );
