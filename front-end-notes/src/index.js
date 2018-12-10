e50d42801a71db9e85b19a6f927f0bdd1fab150b
tryingtokeepup <kl.hawaii@gmail.com>
2018-12-10 14:28:41 -0800

    Took a while to review delete and edit functionality code. Managed it, going to implement in project now.

--- front-end-notes/src/index.js
+++ front-end-notes/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
