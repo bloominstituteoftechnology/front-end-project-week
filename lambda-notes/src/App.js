b1d9d80e1a743753f7367c3bd46c8be2f0a20d38
David Freitag <freitagdavid@outlook.com>
2018-12-10 11:43:58 -0500

    App: Basic note displaying complete

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -3,13 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import SideBar from './components/SideBar';
 import Content from './components/Content';
-
+import notes from './testNotes';
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <SideBar />
-                <Content />
+                <Content notes={notes} />
             </div>
         );
     }
