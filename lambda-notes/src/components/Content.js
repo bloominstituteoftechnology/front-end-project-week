9989d4978b87bb38ca21956cdcdc7149fd863b1a
FreitagDavid <freitagdavid@outlook.com>
2018-12-10 14:33:57 -0500

    components/Content: Basic Styling

--- lambda-notes/src/components/Content.js
+++ lambda-notes/src/components/Content.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import NotesList from './NotesList';
+import styled from 'styled-components';
+import styleVars from '../helpers/styleVars';
+
+const Div = styled.div`
+    width: ${100 - styleVars.sideBarWidth}%;
+    padding: 56px 0 0 32px;
+    background: ${styleVars.mainContentBG};
+`;
+
+const H1 = styled.h1`
+    font-size: 1.8rem;
+    font-weight: bold;
+    color: ${styleVars.pageHeaderColor};
+`;
+
 const Content = props => {
     console.log(props);
     return (
-        <>
-            <h1>Your Notes:</h1>
+        <Div>
+            <H1>Your Notes:</H1>
             <NotesList notes={props.notes} />
-        </>
+        </Div>
     );
 };
 
