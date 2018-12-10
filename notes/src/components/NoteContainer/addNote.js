2e227b449881c3e0542f82ce17af26490e19b0df
Brian G <brian.gregorius@gmail.com>
2018-12-10 14:45:09 -0500

    renamed notes

---
+++ notes/src/components/NoteContainer/addNote.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './NoteEdit.css';
+
+
+const url = process.env.REACT_APP_DB_URL; 
+
+
+class AddNote extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            textBody: '',
+        };
+    }
+
+
+    handleInput = event => {
+        this.setState({ [event.target.name]: event.target.value })
+    }
+
+    addNote = event => {
+        event.preventDefault();
+        const newNote = { title: this.state.title, textBody: this.state.textBody }
+        axios
+            .post(`${url}/note/create`, newNote)
+            .then(response => {
+                console.log('ADDING', response);
+                this.props.changeState(response.data)
+            })
+            .catch(err =>
+                console.log(err));
+
+        this.setState({
+            title: '',
+            textBody: '',
+        });
+
+        this.props.history.push('/');
+    }
+
+    render() {
+        return (
+            <div className='add-note-wrapper'>
+                <form onSubmit={this.addNote} className='edit-note'>
+                    <h1 className='add-note-h1'>Create New Note:</h1>
+                    <input
+                        className='edit-title'
+                        placeholder='Note Title'
+                        value={this.state.title}
+                        name='title'
+                        onChange={this.handleInput}
+                    />
+                    <textarea
+                        className='edit-textBody'
+                        placeholder='Note Content'
+                        value={this.state.textBody}
+                        name='textBody'
+                        onChange={this.handleInput}
+                    />
+                    <button type='submit' onClick={this.handleInput} className='save-edit-btn'>Save</button>
+                </form>
+            </div>
+        )
+    }
+
+
+}
+
+
+
+export default AddNote;
---
+++ notes/src/components/NoteContainer/noteCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './Notes.css';
+
+const NoteCard = props => {
+
+    return (
+        <div className='note-card'>
+            <div className='notes'>
+                <h2>{props.note.title.substring(0, 33)}</h2>
+                <p>{props.note.textBody}</p>
+            </div>
+        </div>
+    )
+}
+
+
+export default NoteCard;
