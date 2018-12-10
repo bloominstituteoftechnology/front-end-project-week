8b0517e046d9129ab062226a9eba149ad106e4b3
darenlarson <larson.daren@gmail.com>
2018-12-10 10:43:02 -0600

    Created folder structure and added code to src/index.js

--- notes/src/index.js
+++ notes/src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import {createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './index.css';
+
 import App from './App';
+import rootReducer from './reducers';
 
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+        
+    </Provider>
+    , document.getElementById('root')
+);
