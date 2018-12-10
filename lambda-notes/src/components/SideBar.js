2aee696e88f09139e4c5b05a9800b4c3338b8ece
FreitagDavid <freitagdavid@outlook.com>
2018-12-10 13:32:00 -0500

    components/SideBar: Header styling

--- lambda-notes/src/components/SideBar.js
+++ lambda-notes/src/components/SideBar.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+    width: 24.7%;
+    border-right: 2px solid grey;
+    padding: 28px 0 0 17px;
+    line-height: 2.5rem;
+`;
+
+const H1 = styled.h1`
+    margin: 0;
+    font-size: 3rem;
+`;
+
 const SideBar = props => {
     return (
-        <>
-            <h1>This is gonna be a sidebar</h1>
-        </>
+        <Div>
+            <H1>Lambda Notes</H1>
+        </Div>
     );
 };
 
