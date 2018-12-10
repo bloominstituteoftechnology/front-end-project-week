2d6872ce9c995704df689ebd9308b889869bb4d4
Dennis Orbison <orbison.dennis@gmail.com>
2018-12-10 12:39:40 -0800

    added more components

---
+++ lambda-notes/src/Components/CreateNoteFrom.js
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+const CreateNoteForm = props => {
+  return (
+    <div className={'create-note'}>
+      <h3>Create Note</h3> 
+    </div>
+  )
+}
+
+export default CreateNoteForm;
---
+++ lambda-notes/src/Components/EditNote.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+const EditNote = props => {
+  return (
+    <div className={'edit-note'}>
+      <h3>Edit Note</h3> 
+
+    </div>
+  )
+}
+
+export default EditNote;
