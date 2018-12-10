2647b9e7636311b9808a779abc6c31ccdedd1106
kevinmarker1101 <kmarker1101@icloud.com>
2018-12-10 11:35:11 -0600

    layout

---
+++ lambdanotes/src/components/SideNav/SideNav.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const SideNav = () => {
+    return (
+        <div className="sideNav">
+            <div className="sideNav__directory">
+                <div className="directory__title">Lambda Notes</div>
+                <div className="directory__link">
+                    <Link to="/notes">
+                        <button className="link__button">
+                            {" "}
+                            View Your Notes{" "}
+                        </button>
+                    </Link>
+                </div>
+                <div className="directory__link">
+                    <Link to="/newNote">
+                        <button className="link__button">
+                            {" "}
+                            + Create New Note{" "}
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SideNav;
