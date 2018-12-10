b7cd5e8068c3dd5020c1018afaab6e6dd849dd39
FreitagDavid <freitagdavid@outlook.com>
2018-12-10 14:34:33 -0500

    components/NotesList: Added styled div and styled it

--- lambda-notes/src/components/NotesList.js
+++ lambda-notes/src/components/NotesList.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import Note from './Note';
+import styled from 'styled-components';
+
+const Div = styled.div`
+    outline: 1px solid red;
+    width: 95%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin-top: 41px;
+    margin-left: 4px;
+`;
 
 const NotesList = props => {
     return (
-        <>
+        <Div>
             {props.notes.length === 0 ? (
                 <h1>Be patient we are loading the notes</h1>
             ) : (
                 props.notes.map(note => <Note key={note._id} note={note} />)
             )}
-        </>
+        </Div>
     );
 };
 
