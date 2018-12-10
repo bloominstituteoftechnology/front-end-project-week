7e5b1040db23e5a1e1341bad092b59e2b01ff4fc
David Freitag <freitagdavid@outlook.com>
2018-12-10 11:29:26 -0500

    components/SideBar: create

---
+++ lambda-notes/src/components/SideBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const SideBar = props => {
+    return (
+        <>
+            <h1>This is gonna be a sidebar</h1>
+        </>
+    );
+};
+
+export default SideBar;
