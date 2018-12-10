1da1d50484240fadb629e72778d36333dd898f83
David Freitag <freitagdavid@outlook.com>
2018-12-10 11:29:00 -0500

    components/NotesList: create

---
+++ lambda-notes/src/components/NotesList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotesList = props => {
+    return (
+        <>
+            <h1>This is gonna be a list of notes</h1>
+        </>
+    );
+};
+
+export default NotesList;
