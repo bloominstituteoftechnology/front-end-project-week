cb3e9e06be7e618db4db989ae8f07b83c5bbecd2
Trai Lynne <trailynne@gmail.com>
2018-12-10 16:18:05 -0700

    Defined Action Creator for fetching single note Used axios GET request in fetchUniqueData Exported action type

--- notes/src/actions/index.js
+++ notes/src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 // Action Type Exports
 export const FETCH_NOTES = 'FETCH_NOTES';
+export const FETCH_NOTE = 'FETCH_NOTE';
 export const FETCH_ALL_SUCCESS = 'FETCH_ALL_SUCCESS';
 export const FETCH_ONE_SUCCESS = 'FETCH_ONE_SUCCESS';
 export const FETCH_FAIL = 'FETCH_FAIL';
@@ -25,4 +26,20 @@ export const fetchAllData = () => dispatch => {
                 payload: err
             });
         });
+}
+
+export const fetchUniqueData = id => dispatch => {
+    dispatch({action: FETCH_NOTE});
+    axios
+        .get(`https://fe-notes.herokuapp.com/note/get/${id}`)
+        .then(resp => {
+            return dispatch({
+                type: FETCH_ONE_SUCCESS,
+                payload: resp.data
+            });
+        })
+        .catch(err => dispatch({
+            type: FETCH_FAIL,
+            payload: err
+        }));
 }
