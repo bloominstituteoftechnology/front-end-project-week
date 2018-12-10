8497b6e344641922be24d64edf66c803169fe27c
David Freitag <freitagdavid@outlook.com>
2018-12-10 11:28:29 -0500

    components/Content: create

---
+++ lambda-notes/src/components/Content.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Content = props => {
+    return (
+        <>
+            <h1>This is gonna be the main content</h1>
+        </>
+    );
+};
+
+export default Content;
