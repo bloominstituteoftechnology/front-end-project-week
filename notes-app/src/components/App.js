e9c2268e846a35724b6942e23a1c5215710cdb35
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2018-12-10 10:48:46 -0600

    component directories organized

---
+++ notes-app/src/components/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      notes: []
+    }
+  }
+
+  componentDidMount(){
+
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
