fea201124c34e276696ea5821ba45e451b78b1e1
Mr. Metelus <Mdmetelus@gmail.com>
2018-12-11 17:21:43 -0500

    "create new oned components made."

---
+++ lambda_notes/src/Components/CreateNewOnes.js
@@ -0,0 +1,38 @@
+
+
+
+import React, { Component } from "react";
+import styled from 'styled-components';
+import axios from 'axios';
+
+
+
+
+
+class CreateNewOnes extends Component {
+  constructor() {
+    super()
+    this.state = {
+      title: '',
+      textBody: ''
+
+
+    }
+  }
+
+ 
+
+  addNewNote = e => {
+    e.preventDefault();
+    
+  };
+
+  render() {
+    return (
+      
+    );
+  };
+}
+
+export default CreateNewOnes;
+
