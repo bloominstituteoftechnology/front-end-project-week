9dba6eb740b6bf9d14d6077478b01962240b54d4
FreitagDavid <freitagdavid@outlook.com>
2018-12-10 20:16:21 -0500

    helpers/styleVars: Added some button colors

--- lambda-notes/src/helpers/styleVars.js
+++ lambda-notes/src/helpers/styleVars.js
@@ -2,5 +2,7 @@ export default {
     sideBarWidth: 24.3,
     mainContentBG: '#f3f3f3',
     sideBarBG: '#D3D2D3',
-    pageHeaderColor: '#414141'
+    pageHeaderColor: '#414141',
+    buttonColor: '#2BC1C4',
+    buttonFontColor: 'white'
 };
