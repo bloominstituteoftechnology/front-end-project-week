8ee78f523746e1b367e9ca9be15b6ef8a1631164
FreitagDavid <freitagdavid@outlook.com>
2018-12-10 20:15:35 -0500

    components/Button: Styling

---
+++ lambda-notes/src/components/Button.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import styleVars from '../helpers/styleVars';
+
+const StyledButton = styled.button`
+    font-size: 1.4rem;
+    font-weight: bold;
+    border: none;
+    outline: rgba(0, 0, 0, 0.2) solid 2px;
+    background: ${styleVars.buttonColor};
+    color: ${styleVars.buttonFontColor};
+    padding: 15px;
+`;
+
+function Button(props) {
+    return (
+        <StyledButton className={props.className}>{props.text}</StyledButton>
+    );
+}
+
+export default Button;
