09090447195e2934c58b8b99d52e42b2b88f2817
Aman <amsingh714@gmail.com>
2018-12-11 10:05:13 -0500

    added string limits to note boxes

--- lambda-notes/src/Components/NoteBox/NoteBox.js
+++ lambda-notes/src/Components/NoteBox/NoteBox.js
@@ -2,11 +2,18 @@ import React from "react";
 import "./NoteBox.css";
 
 const NoteBox = props => {
+
+
+  let text = props.textBody.split("").splice(0, 249).join("").concat("..."); 
+  let title = props.title.split("").splice(0, 29).join("").concat("...");
+  
   return (
     <div className="note-box">
-      <h4>{props.title}</h4>
+      <h4>{props.title.length > 30 ? title : props.title}</h4>
       <div className="line" />
-      <p>{props.textBody}</p>
+      <p>
+      {props.textBody.length > 250 ? text : props.textBody}
+      </p>
     </div>
   );
 };
