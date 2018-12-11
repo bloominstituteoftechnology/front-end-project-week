9203f55321d27ee41dff5f6a237026c979070eaf
Wanda <doubleuevans@gmail.com>
2018-12-11 14:04:34 -0600

    updated App with edit, delete and handlers

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -27,6 +27,59 @@ class App extends Component {
       })  
   }
 
+  componentDidUpdate() {
+    axios
+      .get("https://fe-notes.herokuapp.com/note/get/all")
+      .then( response => {
+        this.setState(() => ({ notes: response.data }))
+      })
+      .catch(error => { 
+        console.log(error)
+      })
+  }
+
+  inputHandler = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  addNote = () => {
+    const newNote = {
+      title: this.state.title,
+      textBody: this.state.textBody
+    }
+    axios
+      .post('https://fe-notes.herokuapp.com/note/create', newNote)
+      .then(response => {
+        this.setState({ notes: response.data })
+      })
+      .catch( error => console.log(error))
+  }
+
+  editHandler = (title, textBody) => {
+    this.setState({ edittitle: title, edittextBody: textBody })
+  }
+
+  editNote = (id) => {
+    axios
+      .put(`https://fe-notes.herokuapp.com/note/edit/${id}`,
+      {
+        title: this.state.edittitle,
+        textBody: this.state.edittextBody,
+      })
+      .then(response => console.log(response.data))
+      .catch(error => console.log(error))
+    this.setState();
+  }
+    
+  toggleDeleteOn = () => {
+    this.setState({ isDeleting: true})
+  }
+
+  toggleDeleteOff = (event) => {
+    event.preventDefault();
+    this.setState({ isDeleting: false})
+  }
+
   deleteNote = (event, id) => {
     event.preventDefault();
     axios
