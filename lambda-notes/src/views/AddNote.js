6e9e7e8535171da770a9447025d8693238aa9d54
FreitagDavid <freitagdavid@outlook.com>
2018-12-10 21:37:08 -0500

    views/AddNote: Styling

--- lambda-notes/src/views/AddNote.js
+++ lambda-notes/src/views/AddNote.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import styleVars from '../helpers/styleVars';
+import NoteForm from '../components/NoteForm';
+
+const H1 = styled.h1`
+    font-size: 1.8rem;
+    font-weight: bold;
+    color: ${styleVars.pageHeaderColor};
+`;
+
+const Div = styled.div`
+    margin-left: 4px;
+`;
 
 export default class AddNote extends Component {
-    static propTypes = {
-        prop: PropTypes
-    };
-
     render() {
-        return <div />;
+        return (
+            <Div>
+                <H1>Create New Note:</H1>
+                <NoteForm />
+            </Div>
+        );
     }
 }
