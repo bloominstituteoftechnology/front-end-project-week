432dbb22e1a34ac7dd6c17d7291b623f17b95345
Brian G <brian.gregorius@gmail.com>
2018-12-11 16:44:42 -0500

    added an authenticate function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Authenticate from './components/authenticate/authenticate.js';
 import NotesContainer from './components/NoteContainer/notesContainer';
 import SideBar from './components/sidebar/sidebar';
 
@@ -17,4 +18,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Authenticate(App);
