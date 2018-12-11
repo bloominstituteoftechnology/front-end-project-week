916c944b26d10192eea6ecd459557abb4d536606
shawn <shawnantonucci2018@gmail.com>
2018-12-11 14:54:33 -0800

    created handle payment token

--- lambdaNotes/src/components/Payments.js
+++ lambdaNotes/src/components/Payments.js
@@ -1,21 +1,29 @@
 import React, { Component } from "react";
+import axios from 'axios';
 import StripeCheckout from "react-stripe-checkout";
-import {
-    Credits,
-  } from "../Styles";
+import { Credits } from "../Styles";
 
 class Payments extends Component {
+  handleToken = token => {
+    axios
+      .get("/api/stripe")
+      .then(res => {
+        this.setState(() => ({ user: res.data }));
+      })
+      .catch(err => console.log(err));
+  };
+
   render() {
     return (
-        <StripeCheckout
+      <StripeCheckout
         name="Emaily"
         description="$5 for 5 email credits"
         amount={500}
-        token={token => console.log(token)}
+        token={token => this.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-    >
+      >
         <Credits>Buy Credits</Credits>
-    </StripeCheckout>
+      </StripeCheckout>
     );
   }
 }
