350fc7c224f607ba7f8aa4f2343fbb9bf991cef7
FreitagDavid <freitagdavid@outlook.com>
2018-12-11 14:50:13 -0500

    components/Note: Minor styling

--- lambda-notes/src/components/Note.js
+++ lambda-notes/src/components/Note.js
@@ -1,40 +1,56 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-const Div = styled.div`
-    width: 31.3%;
-    height: 270px;
+const StyledLink = styled(Link)`
+    width: 31.6%;
+    height: 273px;
     border: 1px solid rgba(0, 0, 0, 0.2);
     margin-bottom: 35px;
     background: white;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    text-decoration: none;
+    color: black;
     h1 {
         display: block;
-        font-size: 1.5rem;
-        border-bottom: 2px solid grey;
-        width: 85%;
+        font-size: 1.64rem;
+        width: 81.1%;
         margin-left: auto;
         margin-right: auto;
         text-overflow: ellipsis;
         overflow: hidden;
         white-space: nowrap;
+        margin-top: 10px;
+    }
+    span {
+        border: 1px solid rgba(0, 0, 0, 0.5);
+        display: block;
+        margin-top: -13px;
+        margin-left: auto;
+        margin-right: auto;
+        width: 80%;
     }
     p {
         display: block;
         text-overflow: ellipsis;
-        width: 85%;
+        width: 81.2%;
         margin-left: auto;
         margin-right: auto;
-        font-size: 1.2rem;
+        font-size: 1.195rem;
         overflow: hidden;
+        margin-top: 9px;
+        font-weight: 300;
     }
 `;
 
 const Note = props => {
     return (
-        <Div>
+        <StyledLink key={props.note._id} to={`/notes/${props.note._id}`}>
             <h1>{props.note.title}</h1>
+            <span />
             <p>{props.note.textBody}</p>
-        </Div>
+        </StyledLink>
     );
 };
 
