adb1b72b5acd9d13de8d4023ad19deb2c4245fab
shawn <shawnantonucci2018@gmail.com>
2018-12-11 14:26:51 -0800

    added name and description for purchases

--- lambdaNotes/src/components/Payments.js
+++ lambdaNotes/src/components/Payments.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import StripeCheckout from 'react-stripe-checkout';
+import React, { Component } from "react";
+import StripeCheckout from "react-stripe-checkout";
 
 class Payments extends Component {
-    render() {
-        return (
-            <StripeCheckout
-                amount={500}
-                token={token => console.log(token)}
-                stripeKey={process.env.REACT_APP_STRIPE_KEY}
-            />
-        );
-    }
+  render() {
+    return (
+      <StripeCheckout
+        name="Emaily"
+        description="$5 for 5 email credits"
+        amount={500}
+        token={token => console.log(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      />
+    );
+  }
 }
 
 export default Payments;
 
-console.log('key', process.env.REACT_APP_STRIPE_KEY);
-console.log('Enviroment', process.env.NODE_ENV);
+console.log("key", process.env.REACT_APP_STRIPE_KEY);
+console.log("Enviroment", process.env.NODE_ENV);
