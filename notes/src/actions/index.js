7e645ab6375a08bff812bb072584c82e0dd560a3
Trai Lynne <trailynne@gmail.com>
2018-12-10 18:42:47 -0700

    Defined action creator for creating note

--- notes/src/actions/index.js
+++ notes/src/actions/index.js
@@ -2,6 +2,8 @@
 import axios from 'axios';
 
 // Action Type Exports
+export const CREATE_NOTE = 'CREATE_NOTE';
+export const CREATE_SUCCESS = 'CREATE_SUCCESS';
 export const FETCH_NOTES = 'FETCH_NOTES';
 export const FETCH_NOTE = 'FETCH_NOTE';
 export const FETCH_ALL_SUCCESS = 'FETCH_ALL_SUCCESS';
@@ -9,6 +11,27 @@ export const FETCH_ONE_SUCCESS = 'FETCH_ONE_SUCCESS';
 export const FETCH_FAIL = 'FETCH_FAIL';
 
 // Action Creators
+
+// C - Create
+export const createNote = newNote => dispatch => {
+    dispatch({type: CREATE_NOTE});
+    axios
+        .get(`https://fe-notes.herokuapp.com/note/create`, newNote)
+        .then(resp => {
+            dispatch({
+                type: CREATE_SUCCESS,
+                payload: resp
+            })
+        })
+        .catch(err => {
+            dispatch({
+                type: FETCH_FAIL,
+                payload: err
+            })
+        });
+}
+
+// R - Read
 export const fetchAllData = () => dispatch => {
     dispatch({type: FETCH_NOTES});
     
