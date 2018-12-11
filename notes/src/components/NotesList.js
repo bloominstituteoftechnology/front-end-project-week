4d786fe7838c70006ebb9e6ce5db2f57286a96d3
Thomas Claydon <tomclaydon102@gmail.com>
2018-12-11 17:05:53 -0500

    Completed Stretch Search Functionality

--- notes/src/components/NotesList.js
+++ notes/src/components/NotesList.js
@@ -1,29 +1,71 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 
-const NotesList = props => {
-  console.log("rendering NotesList component");
-  if (props.isFetching) {
-    return <h4>Loading items...</h4>;
+class NotesList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filteredNotes: []
+    };
   }
-  return (
-    <Fragment>
-      <h2>Your Notes:</h2>
-      <div className="notes-container">
-        {props.notes.map(note => {
-          return (
-            <div
-              className="note"
-              onClick={() => props.history.push(`/notes/${note._id}`)}
-              key={note.id}
-            >
-              <h3>{note.title}</h3>
-              <p>{note.textBody}</p>
-            </div>
-          );
-        })}
-      </div>
-    </Fragment>
-  );
-};
+
+  searchPostsHandler = e => {
+    const filteredNotes = this.props.notes.filter(note => {
+      if (note.title.includes(e.target.value)) {
+        return note;
+      }
+    });
+    this.setState({ filteredNotes: filteredNotes });
+  };
+
+  render() {
+    console.log("rendering NotesList component");
+    if (this.props.isFetching) {
+      return <h4>Loading items...</h4>;
+    }
+    return (
+      <Fragment>
+        <div className="searchContainer"> 
+            <input
+              type="text"
+              placeholder="Search Your Notes:"
+              onKeyDown={this.searchPostsHandler}
+            />
+        </div>
+        <h2>Your Notes:</h2>
+        <div className="notes-container">
+          {this.state.filteredNotes.length > 0
+            ? this.state.filteredNotes.map(note => {
+                return (
+                  <div
+                    className="note"
+                    onClick={() =>
+                      this.props.history.push(`/notes/${note._id}`)
+                    }
+                    key={note.id}
+                  >
+                    <h3>{note.title}</h3>
+                    <p>{note.textBody}</p>
+                  </div>
+                );
+              })
+            : this.props.notes.map(note => {
+                return (
+                  <div
+                    className="note"
+                    onClick={() =>
+                      this.props.history.push(`/notes/${note._id}`)
+                    }
+                    key={note.id}
+                  >
+                    <h3>{note.title}</h3>
+                    <p>{note.textBody}</p>
+                  </div>
+                );
+              })}
+        </div>
+      </Fragment>
+    );
+  }
+}
 
 export default NotesList;
