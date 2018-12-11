8e0ddd55910464c884ed70c8a8c1142f279431e3
jsmnglng <jsmn.glng@gmail.com>
2018-12-11 08:16:13 -0800

    Refactored styles on buttons 2

--- lambda_notes/src/components/Notes/DeleteModal.js
+++ lambda_notes/src/components/Notes/DeleteModal.js
@@ -2,6 +2,32 @@ import React from "react";
 import { ActionButton } from "./Styles";
 import "./Modal.css";
 
+const deleteButton = {
+  width: "35%",
+  margin: "15px",
+  border: "1px solid #b7b5b7",
+  padding: "15px",
+  backgroundColor: "#d0011b",
+  fontSize: "1.8rem",
+  fontWeight: "bold",
+  color: "#fff",
+  textDecoration: "none",
+  cursor: "pointer"
+};
+
+const closeButton = {
+  width: "35%",
+  margin: "15px",
+  border: "1px solid #b7b5b7",
+  padding: "15px",
+  backgroundColor: "#23b8bd",
+  fontSize: "1.8rem",
+  fontWeight: "bold",
+  color: "#fff",
+  textDecoration: "none",
+  cursor: "pointer"
+};
+
 const DeleteModal = props => {
   const showHideClassName = props.show
     ? "modal display-block"
@@ -11,15 +37,19 @@ const DeleteModal = props => {
     <div className={showHideClassName}>
       <section className="modal-main">
         <p>Are you sure you want to delete this?</p>
-        <ActionButton
-          style={{ backgroundColor: "#d0011b" }}
+        <button
+          style={deleteButton}
           type="button"
           onClick={e => props.deleteNote(e, props.noteID)}
         >
           Delete
-        </ActionButton>
+        </button>
 
-        <ActionButton type="button" onClick={props.hideModal}>
+        <ActionButton
+          style={closeButton}
+          type="button"
+          onClick={props.hideModal}
+        >
           No
         </ActionButton>
       </section>
