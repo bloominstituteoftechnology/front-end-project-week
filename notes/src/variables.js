b6d81f3164b9b4159e107147cc81fac141627f63
zacharyarney <zachary.arney@gmail.com>
2018-12-12 17:48:15 -0600

    styling

--- notes/src/variables.js
+++ notes/src/variables.js
@@ -1,7 +1,8 @@
 export const colors = {
   textColor: '#4E4E4E',
   titleColor: '#5B618A',
-  backgroundColor: '#F9F8EA',
+  backgroundColor: '#EFF1ED',
+  altBackgroundColor: '#F9F8EA',
   buttonColor: '#4C86A8',
   buttonNegColor: '#E0777D',
   borderColor: '#9DBDCF',
