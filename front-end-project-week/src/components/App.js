7d2200fa83df90f54cba07065b56cacebbc4cd45
jamespagedev <jamespagedev@outlook.com>
2018-12-12 12:38:28 -0700

    cleaned up deadcode

--- front-end-project-week/src/components/App.js
+++ front-end-project-week/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom';
 import styled from 'styled-components';
 import SideNav from './SideNav';
@@ -10,7 +10,7 @@ import EditNoteView from '../views/EditNoteView';
 /***************************************************************************************************
  ********************************************* Variables *******************************************
  **************************************************************************************************/
-const isTestServerOn = false;
+const isTestServerOn = true;
 const server = isTestServerOn
   ? 'http://localhost:3333'
   : 'https://fe-notes.herokuapp.com';
@@ -38,38 +38,36 @@ const DivWrapper = styled.div`
 /***************************************************************************************************
  ********************************************* Component *******************************************
  **************************************************************************************************/
-class App extends Component {
-  render() {
-    return (
-      <DivWrapper>
-        <SideNav
-          {...this.props}
-          viewNotesLink={`${urlLinks.home}${urlLinks.readNotes}`}
-          createNoteLink={`${urlLinks.home}${urlLinks.createNote}`}
-        />
-        <Route
-          exact
-          path={`${urlLinks.home}${urlLinks.readNotes}`}
-          render={props => <ListNotesView {...props} urlLinks={urlLinks} />}
-        />
-        <Route
-          exact
-          path={`${urlLinks.home}${urlLinks.readNoteId}`}
-          render={props => <NoteView {...props} urlLinks={urlLinks} />}
-        />
-        <Route
-          exact
-          path={`${urlLinks.home}${urlLinks.createNote}`}
-          render={props => <CreateNoteView {...props} urlLinks={urlLinks} />}
-        />
-        <Route
-          exact
-          path={`${urlLinks.home}${urlLinks.editNoteId}`}
-          render={props => <EditNoteView {...props} urlLinks={urlLinks} />}
-        />
-      </DivWrapper>
-    );
-  }
-}
+const App = props => {
+  return (
+    <DivWrapper>
+      <SideNav
+        {...props}
+        viewNotesLink={`${urlLinks.home}${urlLinks.readNotes}`}
+        createNoteLink={`${urlLinks.home}${urlLinks.createNote}`}
+      />
+      <Route
+        exact
+        path={`${urlLinks.home}${urlLinks.readNotes}`}
+        render={props => <ListNotesView {...props} urlLinks={urlLinks} />}
+      />
+      <Route
+        exact
+        path={`${urlLinks.home}${urlLinks.readNoteId}`}
+        render={props => <NoteView {...props} urlLinks={urlLinks} />}
+      />
+      <Route
+        exact
+        path={`${urlLinks.home}${urlLinks.createNote}`}
+        render={props => <CreateNoteView {...props} urlLinks={urlLinks} />}
+      />
+      <Route
+        exact
+        path={`${urlLinks.home}${urlLinks.editNoteId}`}
+        render={props => <EditNoteView {...props} urlLinks={urlLinks} />}
+      />
+    </DivWrapper>
+  );
+};
 
 export default App;
