2d9f9945787e22c6415652cda79b2f31836373ca
MEBonham <programmer@mckaybonham.com>
2018-12-12 13:53:38 -0700

    console logs response

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -47,6 +47,7 @@ class App extends Component {
       .post("https://infinite-cliffs-77240.herokuapp.com/api/notes", note)
       // .post("http://localhost:9000/api/notes", note)
       .then(response => {
+        console.log(response.data);
         history.push(`/note/${response.data.success}`);
         this.setState({
           notes: [
