8ab95ebf1dc140a1edbd18f31c8b12e921b7336b
Aman <amsingh714@gmail.com>
2018-12-12 15:02:56 -0500

    fixed long textbody string bug

--- lambda-notes/src/Components/NoteBox/NoteBox.js
+++ lambda-notes/src/Components/NoteBox/NoteBox.js
@@ -12,12 +12,14 @@ const NoteBox = props => {
     .splice(0, 29)
     .join("")
     .concat("...");
+  let word = props.textBody
+  let displayWord = word.split("").splice(0, 19).join("").concat("...");
 
   return (
     <div className="note-box">
       <h4>{props.title.length > 30 ? title : props.title}</h4>
       <div className="line" />
-      <p>{props.textBody.length > 200 ? text : props.textBody}</p>
+      <p>{word.length > 20 ? displayWord : props.textBody.length > 200 ? text : props.textBody}</p>
     </div>
   );
 };
