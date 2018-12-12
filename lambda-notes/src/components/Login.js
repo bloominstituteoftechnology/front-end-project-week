cf7a66001916cdb2ddf816cc75ce0657b106d7c0
DaftBeowulf <ajmboosh@gmail.com>
2018-12-12 16:07:12 -0600

    refactored Login to create and look for JWT after hitting register and login endpoints

--- lambda-notes/src/components/Login.js
+++ lambda-notes/src/components/Login.js
@@ -1,33 +1,86 @@
 import React from "react";
+import axios from "axios";
+
+const url = "https://fsw14-lambda-notes.herokuapp.com/api";
+
+const initialUser = {
+  username: "",
+  password: ""
+};
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: "",
-      password: ""
+      user: { ...initialUser },
+      isRegistering: false
     };
   }
 
-  changeHandler = (key, value) => {
+  changeHandler = event => {
+    const { name, value } = event.target;
     this.setState({
-      ...this.state,
-      [key]: value
+      user: { ...this.state.user, [name]: value }
     });
   };
 
-  login = () => {
-    if (this.state.username && this.state.password) {
-      localStorage.setItem("username", this.state.username);
-      this.setState({
-        username: "",
-        password: ""
-      });
+  login = event => {
+    event.preventDefault();
+    if (this.state.user.username && this.state.user.password) {
+      axios
+        .post(`${url}/login`, this.state.user)
+        .then(res => {
+          if (res.data) {
+            localStorage.setItem("note_token", res.data.token);
+            window.location.reload();
+          } else {
+            throw new Error();
+          }
+        })
+        .catch(err => {
+          console.log(err);
+          alert("Incorrect username or password");
+          this.setState({ user: initialUser });
+        });
     } else {
       alert(
         "Please fill out both username and password fields before submitting"
       );
+      this.setState({ user: initialUser });
     }
+  };
+
+  register = event => {
+    event.preventDefault();
+    if (this.state.user.username && this.state.user.password) {
+      axios
+        .post(`${url}/register`, this.state.user)
+        .then(res => {
+          if (res.data) {
+            console.log(res.data);
+            localStorage.setItem("note_token", res.data.token);
+            window.location.reload();
+          } else {
+            console.log(res);
+            throw new Error();
+          }
+        })
+        .catch(err => {
+          console.log(err);
+          alert("Registration failed, please try again");
+          this.setState({ user: initialUser });
+        });
+    } else {
+      alert(
+        "Please fill out both username and password fields before submitting"
+      );
+      this.setState({ user: initialUser });
+    }
+  };
+
+  toggleRegister = event => {
+    event.preventDefault();
+    this.setState({ isRegistering: !this.state.isRegistering });
   };
 
   render() {
@@ -35,28 +88,37 @@ class Login extends React.Component {
       <div className="login-page">
         <div className="login-content-wrapper">
           <h1>Lambda Notes</h1>
-          <p>Enter username and password to log in!</p>
-          <form className="login-form" onSubmit={this.login}>
+          <p>
+            {this.state.isRegistering
+              ? "Enter username and password to register!"
+              : "Enter username and password to log in!"}
+          </p>
+
+          <form
+            className="login-form"
+            onSubmit={this.state.isRegistering ? this.register : this.login}
+          >
             <input
               type="text"
               placeholder="Username"
               name="username"
-              value={this.state.username}
-              onChange={event =>
-                this.changeHandler(event.target.name, event.target.value)
-              }
+              value={this.state.user.username}
+              onChange={this.changeHandler}
             />
             <input
               type="password"
               placeholder="Password"
               name="password"
-              value={this.state.password}
-              onChange={event =>
-                this.changeHandler(event.target.name, event.target.value)
-              }
+              value={this.state.user.password}
+              onChange={this.changeHandler}
             />
-            <button>Log In</button>
+            <button>{this.state.isRegistering ? "Register" : "Log In"}</button>
           </form>
+          <button onClick={this.toggleRegister}>
+            {this.state.isRegistering
+              ? "Already registered? Click here to login!"
+              : "Not registered yet? Click here to sign up!"}
+          </button>
         </div>
       </div>
     );
