387792bc0950bb155dc9dd5e1af3bcf5eaa714d2
darenlarson <larson.daren@gmail.com>
2018-12-12 11:51:01 -0600

    Fixed issue where sorting notes by newest or oldest would break the search functionality. I changed the logic of the notes prop being passed down to the NotesListView component.

--- notes/src/App.js
+++ notes/src/App.js
@@ -23,7 +23,9 @@ const MainWrapper = styled.div`
 
 class App extends Component {
     state = {
-        filteredNotes: []
+        filteredNotes: [],
+        reversedNotes: [],
+        sortText: 'newest',
     }
 
   componentDidMount() {
@@ -39,12 +41,29 @@ class App extends Component {
     this.setState({ filteredNotes: searchNotes })
   }
 
+  sortNotes = event => {
+    let reversedArray = this.props.notes.reverse();
+    this.setState(prevState => ({ reversedNotes: reversedArray, sortText: (prevState.sortText === 'newest' ? 'oldest' : 'newest') }));
+  }
+
   render() {
     return (
       <AppWrapper>
         <Route path="/" render={props => <Sidebar {...props} searchNotes={this.searchNotes} />} />
           <MainWrapper>
-            <Route exact path="/" render={props => <NotesListView {...props} notes={this.state.filteredNotes.length > 0 ? this.state.filteredNotes : this.props.notes} /> } />
+            <Route 
+              exact path="/" 
+              render={props => 
+                <NotesListView 
+                  {...props}
+                  sortNotes={this.sortNotes}
+                  sortText ={this.state.sortText}
+                  // notes={this.state.filteredNotes.length > 0 ? this.state.filteredNotes : this.props.notes}
+                  notes={this.state.filteredNotes.length > 0 && this.state.filteredNotes.length != this.props.notes.length ? this.state.filteredNotes : (this.state.reversedNotes.length ? this.state.reversedNotes : this.props.notes)} 
+                /> 
+                }
+              />
+            
             <Route exact path="/note/:noteId" render={props => <Note {...props} notes={this.props.notes} />} />
             <Route path="/createnewnote" component={CreateNewNote} />
             <Route path="/note/edit/:noteId" render={props => <EditNote {...props} notes={this.props.notes} />} />
--- notes/src/views/NotesListView.js
+++ notes/src/views/NotesListView.js
@@ -8,6 +8,16 @@ const NotesHeader = styled.h2`
     font-size: 20px;
     margin: 50px 0 0px 20px;
 `;
+const SortContainer = styled.div`
+    display: flex;
+    margin: 20px 0 0 20px;
+`;
+const SortButton = styled.button`
+    margin-left: 5px;
+    border: none;
+    background: none;
+    font-size: 10px;
+`;
 
 class NotesListView extends React.Component {
 
@@ -19,6 +29,11 @@ class NotesListView extends React.Component {
         return (
             <div>
                 <NotesHeader>Your Notes:</NotesHeader>
+                <SortContainer>
+                    <p>sort: </p>
+                    <SortButton onClick={this.props.sortNotes}>{this.props.sortText}</SortButton>
+                    {/* <SortButton>oldest</SortButton> */}
+                </SortContainer>
                 <NotesList notes={this.props.notes} {...this.props} />
             </div>
         );
