9736d24cea3efbdc34a597e90b2252ca23e68365
MEBonham <programmer@mckaybonham.com>
2018-12-13 14:57:02 -0700

    Switch to Heroku database

--- lambda-notes/src/api-url.js
+++ lambda-notes/src/api-url.js
@@ -1,8 +1,8 @@
 
 // Local database:
-const url = "http://localhost:9000";
+// const url = "http://localhost:9000";
 
 // Heroku database:
-// const url = "https://infinite-cliffs-77240.herokuapp.com";
+const url = "https://infinite-cliffs-77240.herokuapp.com";
 
 module.exports = url;
