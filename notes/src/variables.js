c9c70e3c6897f38e88503e706afd18c6cc0d47f9
zacharyarney <zachary.arney@gmail.com>
2018-12-13 12:21:42 -0600

    styling--makes background color darker again

--- notes/src/variables.js
+++ notes/src/variables.js
@@ -1,8 +1,8 @@
 export const colors = {
   textColor: '#323232',
   titleColor: '#5B618A',
-  backgroundColor: '#F7F8F6',
-  altBackgroundColor: '#EFF1ED',
+  backgroundColor: '#EFF1ED',
+  altBackgroundColor: '#F7F8F6',
   buttonColor: '#4C86A8',
   buttonNegColor: '#E0777D',
   borderColor: '#9DBDCF',
