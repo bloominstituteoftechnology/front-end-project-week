e768d501cc63a672e430002eef65f7d0651f9461
ykrueng <yasirah.krueng@gmail.com>
2018-12-12 21:28:48 -0600

    refactor component

--- notes/src/components/NoteList/NotesList.js
+++ notes/src/components/NoteList/NotesList.js
@@ -3,10 +3,11 @@ import React from 'react';
 import styled from 'styled-components';
 import { Loader } from 'semantic-ui-react';
 
-import { truncate, sortNotes } from '../../helper/helper';
+import { sortNotes } from '../../helper/helper';
 
 // importing component
 import ToolBar from './ToolBar';
+import NoteCard from './NoteCard';
 
 // styled Notes List
 const NotesWrapper = styled.div`
@@ -26,26 +27,6 @@ const NotesWrapper = styled.div`
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
-
-    .note-item {
-      border: 1px solid #414141;
-      overflow-wrap:break-word;
-      margin: 2rem;
-      font-size: 1.6rem;
-      background-color: #FFFFFF;
-      padding: 1.5rem;
-      width: 25rem;
-      min-height: 25rem;
-      cursor: pointer;
-
-      h3 {
-        font-size: 2.2rem;
-        font-weigth: bold;
-        margin-bottom: 2rem;
-        padding-bottom: 1rem;
-        border-bottom: 1px solid #414141;
-      }
-    }
   }
 `;
 
@@ -54,6 +35,7 @@ class NotesList extends React.Component {
     searchText: '',
     sort: null,
     sortValue: 'oldest',
+    dragging: null,
   }
 
   onInputChange = (e) => {
@@ -62,8 +44,41 @@ class NotesList extends React.Component {
     })
   }
 
+  onDragLeave = (e) => {
+    e.target.style["border-right"] = "";
+    e.target.style["border-left"] = "";
+  }
+
+  onDragOver = (e) => {
+    e.preventDefault();
+    var bounding = e.target.getBoundingClientRect();
+    var offsetX = bounding.x + bounding.width / 2;
+    if (e.clientX - offsetX > 0) {
+      e.target.style["border-right"] = "solid 4px red";
+      e.target.style["border-left"] = "";
+    } else {
+      e.target.style["border-left"] = "solid 4px red";
+      e.target.style["border-right"] = "";
+    }
+  }
+
+  onDragStart = event => {
+    this.setState({ dragging: event.target })
+  }
+
+  onDrop = event => {
+    event.preventDefault();
+    if (event.target.style["border-right"] !== "") {
+      event.target.style["border-right"] = "";
+      this.props.dragDrop(event.target.id, this.state.dragging.id, true);
+    } else {
+      event.target.style["border-left"] = "";
+      this.props.dragDrop(event.target.id, this.state.dragging.id, false);
+    }
+  }
+
   render () {
-    const { notes, dragDrop, history, fetchingNotes } = this.props;
+    const { notes, history, fetchingNotes } = this.props;
     const { searchText, sort, sortValue } = this.state;
 
     let computedNotes;
@@ -75,8 +90,6 @@ class NotesList extends React.Component {
       computedNotes = sortNotes(computedNotes, sortValue);
 
     }
-
-    var dragging = null;
 
     return (
       <NotesWrapper>
@@ -92,39 +105,10 @@ class NotesList extends React.Component {
           />
         </div>
         <div
-          onDragOver={event => {
-            event.preventDefault();
-            var bounding = event.target.getBoundingClientRect();
-            var offsetX = bounding.x + bounding.width / 2;
-            if (event.clientX - offsetX > 0) {
-              event.target.style["border-right"] = "solid 4px red";
-              event.target.style["border-left"] = "";
-            } else {
-              event.target.style["border-left"] = "solid 4px red";
-              event.target.style["border-right"] = "";
-            }
-          }}
-          onDragLeave={event => {
-            event.target.style["border-right"] = "";
-            event.target.style["border-left"] = "";
-          }}
-          onDragStart={event => {
-            dragging = event.target;
-            console.log(event.target);
-            event.dataTransfer.setData("text/html", dragging);
-          }}
-          onDrop={event => {
-            event.preventDefault();
-            console.log(event);
-
-            if (event.target.style["border-right"] !== "") {
-              event.target.style["border-right"] = "";
-              dragDrop(event.target.id, dragging.id, true);
-            } else {
-              event.target.style["border-left"] = "";
-              dragDrop(event.target.id, dragging.id, false);
-            }
-          }}
+          onDragOver={this.onDragOver}
+          onDragLeave={this.onDragLeave}
+          onDragStart={this.onDragStart}
+          onDrop={this.onDrop}
           className="notes"
         >
           {
@@ -133,20 +117,7 @@ class NotesList extends React.Component {
             !notes[0] ?
                 <div>Something has gone teribbly wrong.</div> :
                 computedNotes
-                  .map(note => (
-                  <div
-                    draggable
-                    className="note-item"
-                    key={note._id}
-                    id={note._id}
-                    onClick = { () => {
-                      history.push(`/notes/${note._id}`)
-                    }}
-                  >
-                    <h3>{truncate(note.title, 15)}</h3>
-                    <p>{truncate(note.textBody, 200)}</p>
-                  </div>
-                ))
+                  .map(note => <NoteCard note={note} history={history} />)
           }
         </div>
       </NotesWrapper>
