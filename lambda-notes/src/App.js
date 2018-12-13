847c66cb9336b25ee3d6c5d4472f70ce13c8ed67
Hunter315 <hunter.mckay.smith@gmail.com>
2018-12-12 18:06:28 -0700

    cleaned up console.logs in app.js

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Route, Switch } from 'react-router-dom';
-import CreateNoteView from './components/CreateNoteView';
-import EditNoteView from './components/EditNoteView';
-import NoteView from './components/NoteView';
-import NotesListView from './components/NotesListView';
-import axios from 'axios';
+import React, { Component } from "react";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import CreateNoteView from "./components/CreateNoteView";
+import EditNoteView from "./components/EditNoteView";
+import NoteView from "./components/NoteView";
+import NotesListView from "./components/NotesListView";
+import axios from "axios";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      notes: [],
-      
-    }
+      notes: []
+    };
   }
   componentDidMount() {
     axios
@@ -30,20 +29,15 @@ class App extends Component {
 
   updateNotes = updatedNote => {
     const updatedNotes = this.state.notes.map(note => {
-      console.log("this is updateNoted before if",updatedNote);
       if (note.id === updatedNote.data.id) {
-        console.log("this is updateNoted",updatedNote);
-      
         return updatedNote.data;
       }
       return note;
     });
     this.setState({ notes: updatedNotes });
-    
   };
 
   updateDeleted = targetID => {
-    
     const updatedLists = this.state.notes.filter(note => {
       if (note.id === targetID) {
         return false;
@@ -54,42 +48,54 @@ class App extends Component {
   };
 
   addNewNotes = addedNote => {
-    
-    let newNotesList = [...this.state.notes, addedNote]
+    let newNotesList = [...this.state.notes, addedNote];
     this.setState({ notes: newNotesList });
   };
 
   render() {
     return (
       <div className="App">
-     
-      <Switch>
-      <Route exact path='/' 
-            render = {props =>
-               <NotesListView {...props}    
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={props => (
+              <NotesListView
+                {...props}
                 notes={this.state.notes}
                 updateNotes={this.updateNotes}
                 updateDeleted={this.updateDeleted}
-              //  componentDidMount={this.componentDidMount}
-                />} />
-      <Route path='/note/create' 
-          render = {props => 
-            <CreateNoteView {...props}
-              notes={this.state.notes}
-              updateNotes={this.updateNotes}
-              addNewNotes={this.addNewNotes} />} />
-      <Route path='/note/get/:id' 
-          render={props => 
-            <NoteView {...props}
-              notes={this.state.notes}
-              updateNotes={this.updateNotes}
-              updateDeleted={this.updateDeleted} />} />
-      <Route path='/note/edit/:id' render = {props => <EditNoteView {...props} />} />
-      </Switch>
-
-
-
-
+                //  componentDidMount={this.componentDidMount}
+              />
+            )}
+          />
+          <Route
+            path="/note/create"
+            render={props => (
+              <CreateNoteView
+                {...props}
+                notes={this.state.notes}
+                updateNotes={this.updateNotes}
+                addNewNotes={this.addNewNotes}
+              />
+            )}
+          />
+          <Route
+            path="/note/get/:id"
+            render={props => (
+              <NoteView
+                {...props}
+                notes={this.state.notes}
+                updateNotes={this.updateNotes}
+                updateDeleted={this.updateDeleted}
+              />
+            )}
+          />
+          <Route
+            path="/note/edit/:id"
+            render={props => <EditNoteView {...props} />}
+          />
+        </Switch>
       </div>
     );
   }
