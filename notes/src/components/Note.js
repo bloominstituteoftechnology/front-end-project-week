6b3e4e5a3ca29539db1b4b1bc5c3134b1bba0710
Thomas Claydon <tomclaydon102@gmail.com>
2018-12-12 19:06:29 -0500

    added additional markdown capability

--- notes/src/components/Note.js
+++ notes/src/components/Note.js
@@ -2,18 +2,28 @@ import React, { Fragment } from "react";
 import { connect } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { getNotesById, deleteNote, getNotes } from "../actions";
+
 import "../App.css";
+
+const ReactMarkdown = require("react-markdown/with-html");
+
 class Note extends React.Component {
   constructor() {
     super();
     this.state = {
-      delete: false
+      delete: false,
+      markdown: "regular"
     };
   }
 
   componentDidMount() {
     this.props.getNotesById(this.props.match.params.noteId);
   }
+
+  sortHandlerMarkdown = e => {
+    console.log(e.target.value);
+    this.setState({ ...this.state, markdown: e.target.value });
+  };
 
   render() {
     if (this.props.isFetching) {
@@ -36,7 +46,9 @@ class Note extends React.Component {
                 Delete
               </button>
               <button
-                onClick={() => this.setState({ delete: !this.state.delete })}
+                onClick={() =>
+                  this.setState({ ...this.state, delete: !this.state.delete })
+                }
                 className="delete-btn-no"
               >
                 No
@@ -47,25 +59,51 @@ class Note extends React.Component {
         <div className="note-wrapper">
           <nav className="sub-nav">
             <div className="sub-nav-links">
-              <NavLink
-                exact
-                to={`/edit-note/${this.props.note._id}`}
-              >
+              <NavLink exact to={`/edit-note/${this.props.note._id}`}>
                 <button className="sub-nav-btn sub-nav-links-fix">edit</button>
               </NavLink>
-
-                <button
-                  className="sub-nav-btn"
-                  onClick={() => this.setState({ delete: !this.state.delete })}
-                >
-                  delete
-                </button>
-
+              <button
+                className="sub-nav-btn"
+                onClick={() =>
+                  this.setState({ ...this.state, delete: !this.state.delete })
+                }
+              >
+                delete
+              </button>
             </div>
           </nav>
+          <div className="sort-markdown">
+            <select onChange={this.sortHandlerMarkdown}>
+              <option value="" selected>
+                Mark Down?
+              </option>
+              <option value="regular">Regular</option>
+              <option value="markdown">Markdown</option>
+            </select>
+          </div>
           <div className="note-body">
-            <h2>{this.props.note.title}</h2>
-            <p>{this.props.note.textBody}</p>
+            <Fragment>
+              {this.state.markdown === "markdown" ? (
+                <ReactMarkdown
+                  className="markdown-title"
+                  source={this.props.note.title}
+                  escapeHtml={false}
+                />
+              ) : (
+                <h2>{this.props.note.title}</h2>
+              )}
+            </Fragment>
+            <Fragment>
+              {this.state.markdown === "markdown" ? (
+                <ReactMarkdown
+                  className="markdown-content"
+                  source={this.props.note.textBody}
+                  escapeHtml={false}
+                />
+              ) : (
+                <p>{this.props.note.textBody}</p>
+              )}
+            </Fragment>
           </div>
         </div>
       </Fragment>
