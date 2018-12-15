3636cb7ffbd38bfbd447721d58bea699858dd475
zacharyarney <zachary.arney@gmail.com>
2018-12-15 13:59:55 -0600

    styling--removes max-width of 1000px in App styles

--- notes/src/styles.js
+++ notes/src/styles.js
@@ -4,7 +4,7 @@ export const StyledContainer = styled.div`
   display: flex;
   flex-direction: row;
   text-align: center;
-  max-width: 1000px;
+  //max-width: 1000px;
   margin: 0 auto;
   @media (max-width: 940px) {
     flex-direction: column;
