a30a3758510a94d9e2ece367a3ac39974c27309f
chrisgins <christiansgina@gmail.com>
2018-12-15 11:43:28 -0700

    App.js add navbar that doesn't exist yet- errors abound. Fix on the way.

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,28 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { getNotes, updateForm, addNote, deleteNote } from "./Actions";
+import { withRouter } from "react-router";
+import { Route } from "react-router-dom";
+import ViewNotes from "./View/viewNotes";
+import NewList from "./View/newList";
+
 
 class App extends Component {
+  componentDidMount() {
+    this.props.getNotes();
+  }
+
   render() {
+   
+    if (this.props.gettingNotes) {
+      return <h2>Loading: Hang Tight.</h2>;
+    }
+    if (this.props.error) {
+      return <h1>Houston, we have a problem!</h1>;
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="Page-Container">
+        <NavBar />
+        <div className="Main-View">
+          <div className="Note-Container">
+            <Route
+              exact
+              path="/notes"
+              render={props => <NewList {...props} notes={this.props.notes} />}
+            />
+            <Route
+              
+              path="/createNote"
+              render={props => (
+                <createNewNote {...props} notes={this.props.notes} />
+              )}
+            />
+            <Route
+              
+              path="/notes/:_id"
+              render={props => (
+                <ViewNotes
+                  {...props}
+                  notes={this.props.notes}
+                  deleteNote={this.props.deleteNote}
+                  updateForm={this.props.updateForm}
+                  getNotes={this.props.getNotes}
+                />
+              )}
+            />
+          </div>
+        </div>
       </div>
     );
   }
 }
+const mapStateToProps = state => ({
+  notes: state.notes,
+  gettingNotes: state.gettingNotes,
+  populate: state.populate
+});
 
 export default App;
