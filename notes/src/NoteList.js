852068ec8cd39bf8d24be576674842e471814d3a
1@$@1github <asaofdiamonds@gmail.com>
2018-12-17 11:35:44 -0700

    created intial notelist so it connects with app.js

---
+++ notes/src/NoteList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NoteList (props) {
+    return (
+        <div className='note-list'>
+            I am the note list
+        </div>
+    );
+}
+
+export default NoteList;
