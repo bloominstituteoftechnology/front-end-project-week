38d16d4b4456c4332bf249ecb494c526b35a2f48
1@$@1github <asaofdiamonds@gmail.com>
2018-12-17 16:26:19 -0700

    started note.js, sidebar nav is next

---
+++ notes/src/components/addNote.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+class AddNote extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            textBody: ''
+        };
+    }
+
+    createNote = event => {
+        event.preventDefault();
+        const newNote = {
+            title: this.state.title,
+            textBody: this.state.textBody
+        }
+        axios
+        .post('https://fe-notes.herokuapp.com/note/create', newNote)
+        .then(response => {
+            console.log(response.data);
+            this.props.updateNote(response.data);
+            this.props.history.push('/')
+        })
+        .catch(err => console.log(err));
+    }
+
+    handleInputChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+    render() {
+        return (
+            <div className='add-note'>
+                <form onSubmit={this.createNote}>
+                <h2>Create New Note:</h2>
+                <input 
+                    type='text'
+                    name='title'
+                    placeholder='Note Title'
+                    value={this.state.title}
+                    onChange={this.handleInputChange}
+                    required
+                />
+                <textarea 
+                    name='textBody'                    
+                    placeholder='Note Content'
+                    value={this.state.textBody}
+                    onChange={this.handleInputChange}
+                    required
+                />
+                <button className='btn' type='submit'>Save</button>
+                </form>
+            </div>
+        );
+    }
+
+    
+}
+
+export default AddNote;
