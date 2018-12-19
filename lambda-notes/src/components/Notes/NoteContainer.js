933e5e3d581f650600299ad34d6c694349df5b7f
Nolan Allen <nolan.r.allen@gmail.com>
2018-12-18 18:36:47 -0600

    2nd commit; got notes to display on screen.

---
+++ lambda-notes/src/components/Notes/NoteContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Note from './Note';
+import './Notes.css';
+import styled from 'styled-components';
+
+const NoteContainer = props => {
+    return(
+        
+    )
+}
--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -2,17 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import { createStores } from 'redux';
-import { provide } from 'react-redux';
-import { connect } from 'react-redux';
-
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
