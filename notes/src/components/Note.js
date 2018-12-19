1fc38be4a18c4d584ce86d5415c6ae10bd3e01bd
1@$@1github <asaofdiamonds@gmail.com>
2018-12-19 12:36:07 -0700

    added truncated functionality to notes so they dont run out of the note

--- notes/src/components/Note.js
+++ notes/src/components/Note.js
@@ -3,8 +3,12 @@ import { NavLink } from "react-router-dom";
 
 const Note = props => {
     const { note } = props;
-    const title = note.title;
-    const text = note.textBody;
+    const title = note.title.length >= 12
+    ? `${note.title.substring(0, 12)}...`
+    : note.title;
+    const text = note.textBody.length >= 200 
+    ? `${note.textBody.substring(0, 200)}...`
+    : note.textBody;
     return (
         <div className='note'>
         <NavLink to={`/view/${note._id}`}>
