2148a1e9283de4c0c3c325dda9178c01acfba7ff
Tristen Neu <tristenneu@Tristens-iMac.local>
2018-12-28 17:27:11 -0600

    removed dev tools hook from store

--- fescpw/src/index.js
+++ fescpw/src/index.js
@@ -11,7 +11,7 @@ import rootReducer from './redux/reducers';
 
 // const reduxDevToolsHook = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-const store = createStore(rootReducer, compose(applyMiddleware(thunk, logger), reduxDevToolsHook));
+const store = createStore(rootReducer, compose(applyMiddleware(thunk, logger)));
 
 
 ReactDOM.render((
