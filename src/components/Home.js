58a1b10daefeb0ba150d65257404a6fead2618b8
David Loveday <35117916+AquilaVirtual@users.noreply.github.com>
2018-12-27 20:55:20 -0800

    Update Home.js
    fix background image

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,12 +5,12 @@ import './Home.css'
 const Home = () => {
 
     return (
-          <div>
+          <div className="main-wrapper">
           {localStorage.getItem("token") ? ( 
-          <div className="main-wrapper"> <div className="loggged-on">
+          <div> <div className="loggged-on">
           {`Welcome back, ${localStorage.getItem("name").split(" ")[0]}! You are currently logged on. `}
           </div></div>) : (
-           <div className="main-wrapper">
+           <div>
             <h3>Welcome to Lambda Notes</h3>
              <p>Please Sign In or Sign Up to begin making notes.</p>            
             </div>)
