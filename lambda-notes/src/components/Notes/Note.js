5a159858a96d2952a7ba274d98738a002bb12949
Nolan Allen <nolan.r.allen@gmail.com>
2018-12-27 22:16:16 -0600

    6th commit; view note functionality working.

--- lambda-notes/src/components/Notes/Note.js
+++ lambda-notes/src/components/Notes/Note.js
@@ -1,88 +1,34 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
 import axios from 'axios';
-// import styled from 'styled-components';
+import './Notes.css';
 
 export default class Note extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            _id: null,
-            title: '',
-            textBody: '',
-            show: false
+            note: ''
         }
     }
 
-    // componentDidMount() {
-    //     const note = this.state.notes.find(
-    //         note => `${note._id}` === this.props.match.params.id
-    //     );
-    //     this.setState(note);
-    // }
-
     componentDidMount() {
         axios
-            .get(`/note/${this.props.match.params.id}`)
+            .get(`https://fe-notes.herokuapp.com/note/get/${this.props.match.params.id}`)
             .then(response => {
-                this.setState(() => ({ notes: response.data }))
+                this.setState(() => ({ note: response.data }))
             })
             .catch(error => {
                 console.error('Server Error', error)
             });
     }
 
-    componentDidUpdate(prevProps, prevState) {
-        if (prevState.title === '' || this.props.match.params.id !== prevProps.match.params.id) {
-            const note = this.props.notes.find(
-                note => `${note._id}` === this.props.match.params.id
-            );
-            this.setState(note);
-        }
-    }
-
-    // getNote = event => {
-    //     event.preventDefault();
-        
-    //     const content = {
-    //         title: this.state.title,
-    //         textBody: this.state.textBody,
-    //         tag: this.state.tag
-    //     };
-
-    //     axios
-    //         .get(`/note/${note._id}`)
-    //         .then(response => {
-    //             this.setState(() => ({ note: response.data }))
-    //         })
-    //         .catch(error => {
-    //             console.error('Server Error', error)
-    //         });
-    // }
-
     render() {
         return(
             <div className="note-viewer">
-                <h2>{this.state.title}</h2>
-                <p>{this.state.textBody}</p>
+                <h2>{this.state.note.title}</h2>
+                <div className="note-textBody">
+                    <p>{this.state.note.textBody}</p>
+                </div>
             </div>
         );
     }
 }
-
-// function ViewedContents({ note }) {
-//     const { tags, title, textBody } = note;
-//     return(
-//         <Link to={`/notes/${note._id}`} className="note-link">
-//             <div className="note-card">
-//                 <h2>{title}</h2>
-//                 <div className="note-content">
-//                     {textBody}
-//                 </div>
-//                 <div className="note-tags">
-//                     {tags}
-//                 </div>
-//             </div>
-//         </Link>
-//     );
-// }
