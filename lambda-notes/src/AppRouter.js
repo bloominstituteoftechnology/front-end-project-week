f0863301d5a26eb15bb445d37db31a3be4735d32
Nolan Allen <nolan.r.allen@gmail.com>
2018-12-28 09:53:34 -0600

    reformatted AppRouter as functional component

--- lambda-notes/src/AppRouter.js
+++ lambda-notes/src/AppRouter.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-export default class AppRouter extends React.Component {
-  render() {
-    return (
-      <Router>
-        <SideBar />
-        <Switch>
-          <Route exact path="/" component={NoteList} />
-          <Route exact path="/create" component={CreateNote} />
-          <Route exact path="/:id" component={Note} />
-          <Route exact path="/edit/:id" component={EditNote} />
-        </Switch>
-      </Router>
-    );
-  }
+const AppRouter = () => {
+  return (
+    <Router>
+      <SideBar />
+      <Switch>
+        <Route exact path="/" component={NoteList} />
+        <Route exact path="/create" component={CreateNote} />
+        <Route exact path="/:id" component={Note} />
+        <Route exact path="/edit/:id" component={EditNote} />
+      </Switch>
+    </Router>
+  );
 }
+
+
+export default AppRouter;
