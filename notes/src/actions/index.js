c754ac95455e629728839bc22c0b08aad856b6c8
Trai Lynne <trailynne@gmail.com>
2018-12-31 14:40:58 -0500

    Defined logic for Read All notes - Action Types - Action Creator - Reducer

--- notes/src/actions/index.js
+++ notes/src/actions/index.js
@@ -1,5 +1,29 @@
 // Imports
+import axios from 'axios';
 
 // Action Type Exports
+export const FETCHING_NOTES = 'FETCHING_NOTES';
+export const NOTES_FETCH_SUCCESS = 'NOTES_FETCH_SUCCESS';
+export const NOTES_FETCH_FAIL = 'NOTES_FETCH_FAIL';
 
 // Action Creators
+export const fetchNotes = () => dispatch => {
+    dispatch({
+        type: FETCHING_NOTES
+    })
+
+    axios
+        .get('https://fe-notes.herokuapp.com/note/get/all')
+        .then(data => {
+            return dispatch({
+                type: NOTES_FETCH_SUCCESS,
+                payload: data
+            });
+        })
+        .catch(err => {
+            return dispatch({
+                type: NOTES_FETCH_SUCCESS,
+                payload: err
+            });
+        });
+};
--- notes/src/reducers/index.js
+++ notes/src/reducers/index.js
@@ -1,16 +1,42 @@
 // Imports
 
 // Action Type Imports
+import {
+    // Read All
+    FETCHING_NOTES,
+    NOTES_FETCH_SUCCESS,
+    NOTES_FETCH_FAIL
+} from '../actions'
 
 // Initial State
 const initialState = {
-
+    error: null,
+    fetchingNotes: false,
+    notes: []
 }
 
 // Reducer
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+        case FETCHING_NOTES:
+            return {
+                ...state,
+                fetchingNotes: true
+            }
         
+        case NOTES_FETCH_SUCCESS:
+            return {
+                ...state,
+                fetchingNotes: false,
+                notes: action.payload,
+                error: null
+            }
+        
+        case NOTES_FETCH_FAIL:
+            return {
+                ...state,
+                error: action.payload
+            }
         default:
             return state
     }
