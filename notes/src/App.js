a3f25295195dbfe18d303768291c019fb1afdf08
Trai Lynne <trailynne@gmail.com>
2018-12-31 14:25:52 -0500

    Static NotesView created Defined Link and Route in App.js Defined NotesView Class Component Successfully Renders

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,26 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
+
+import {
+  NavLink,
+  Route
+} from 'react-router-dom';
+
+// Components
+import NotesView from './views/NotesView';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <nav>
+          <NavLink to='/'>Home</NavLink>
+        </nav>
+
+        <div className='app-container'>
+          <Route
+            exact
+            path='/'
+            component={NotesView}
+          />
+        </div>
+      </>
     );
   }
 }
---
+++ notes/src/views/NotesView.js
@@ -0,0 +1,13 @@
+import React, {
+    Component
+} from 'react';
+
+class NotesView extends Component {
+    render() {
+        return (
+            <h1>Notes View</h1>
+        )
+    }
+}
+
+export default NotesView;
