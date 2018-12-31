7a3386609b896450ae2e3cb9c36e0ffdf8cd7b08
Trai Lynne <trailynne@gmail.com>
2018-12-31 17:58:39 -0500

    Defined logic for Read One note - Action Types - Action Creator - Reducer

--- notes/src/actions/index.js
+++ notes/src/actions/index.js
@@ -2,11 +2,19 @@
 import axios from 'axios';
 
 // Action Type Exports
+// R - Read All
 export const FETCHING_NOTES = 'FETCHING_NOTES';
 export const NOTES_FETCH_SUCCESS = 'NOTES_FETCH_SUCCESS';
 export const NOTES_FETCH_FAIL = 'NOTES_FETCH_FAIL';
 
+// R - Read One
+export const FETCHING_NOTE = 'FETCHING_NOTE';
+export const NOTE_FETCH_SUCCESS = 'NOTE_FETCH_SUCCESS';
+export const NOTE_FETCH_FAIL = 'NOTE_FETCH_FAIL';
+
 // Action Creators
+
+// R - Read All
 export const fetchNotes = () => dispatch => {
     dispatch({
         type: FETCHING_NOTES
@@ -27,3 +35,24 @@ export const fetchNotes = () => dispatch => {
             });
         });
 };
+
+// R - Read One
+export const fetchNote = id => dispatch => {
+    dispatch({ type: FETCHING_NOTE });
+
+    axios
+        .get(`https://fe-notes.herokuapp.com/note/get/${id}`)
+        .then(data => {
+            return dispatch({
+                type: NOTE_FETCH_SUCCESS,
+                payload: data.data
+            });
+        })
+        .catch(err => {
+            return dispatch({
+                type: NOTE_FETCH_SUCCESS,
+                payload: err
+            });
+        });
+}
+
--- notes/src/reducers/index.js
+++ notes/src/reducers/index.js
@@ -5,19 +5,26 @@ import {
     // Read All
     FETCHING_NOTES,
     NOTES_FETCH_SUCCESS,
-    NOTES_FETCH_FAIL
+    NOTES_FETCH_FAIL,
+    // Read One
+    FETCHING_NOTE,
+    NOTE_FETCH_SUCCESS,
+    NOTE_FETCH_FAIL
 } from '../actions'
 
 // Initial State
 const initialState = {
     error: null,
     fetchingNotes: false,
-    notes: []
+    notes: [],
+    fetchingNote: false,
+    note: {}
 }
 
 // Reducer
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+        // R - Read All
         case FETCHING_NOTES:
             return {
                 ...state,
@@ -37,6 +44,28 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 error: action.payload
             }
+        
+        // R - Read One
+        case FETCHING_NOTE:
+            return {
+                ...state,
+                fetchingNote: true
+            }
+        
+        case NOTE_FETCH_SUCCESS:
+            return {
+                ...state,
+                fetchingNote: false,
+                note: action.payload,
+                error: null
+            }
+        
+        case NOTE_FETCH_FAIL:
+            return {
+                ...state,
+                error: action.payload
+            }
+        
         default:
             return state
     }
