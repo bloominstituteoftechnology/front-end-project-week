33b993835b39f1bd10ce69bb2363439480938b1b
Trai Lynne <trailynne@gmail.com>
2019-01-02 11:28:17 -0500

    Refactored NoteFormView to be Dynamic Connected to Redux Mapped State and Dspatch to Props Exported enhanced component

--- notes/src/views/NoteFormView.js
+++ notes/src/views/NoteFormView.js
@@ -1,12 +1,41 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
+
+// Redux Imports
+import {
+    connect
+} from 'react-redux';
+
+// Action Creators
+import {
+    createNote
+} from '../actions';
 
 // Component
 import NoteForm from '../components/NoteForm';
 
 class NoteFormView extends Component {
     render() {
-        return <NoteForm/>;
+        return (
+            <NoteForm />
+        )
     }
 }
 
-export default NoteFormView;
+const mapStateToProps = state => {
+    return {
+        ...state
+    };
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        createNote: newNote => dispatch(createNote(newNote))
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(NoteFormView);
