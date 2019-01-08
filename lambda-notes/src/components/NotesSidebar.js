7c02ef1c3c1b3dfb78dd21035595a86a8ca587c3
Ken Barlow <kenbrlow@gmail.com>
2019-01-08 06:23:26 -0800

    Added buttons to sidebar.

--- lambda-notes/src/components/NotesSidebar.js
+++ lambda-notes/src/components/NotesSidebar.js
@@ -4,7 +4,9 @@ import './styles/NotesSidebar.css'
 const NotesSidebar = () => {
     return (
         <div className="NotesSidebarMain">
-            
+            <h1 className="NotesSidebarTitle">Lambda Notes</h1>
+            <button class="NotesSidebarButton ButtonView">View Your Notes</button>
+            <button class="NotesSidebarButton ButtonCreate">+ Create New Note</button>
         </div>
     )
 }
