f8ae49ac15e5875064789d62712b44133bf55aa5
Ken Barlow <kenbrlow@gmail.com>
2019-01-08 06:08:51 -0800

    Started build out of the NotesSidebar. Also exported pdf's as png's to overlay in chrome for getting the designs right.

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import NotesSidebar from './components/NotesSidebar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <NotesSidebar/>
       </div>
     );
   }
---
+++ lambda-notes/src/components/NotesSidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/NotesSidebar.css'
+
+const NotesSidebar = () => {
+    return (
+        <div className="NotesSidebarMain">
+            
+        </div>
+    )
+}
+
+export default NotesSidebar;
