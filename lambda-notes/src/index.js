dc2cb3608c35206819515cb241f60f4a4d4257c0
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2019-02-02 02:13:03 -0500

    (NOTELIST) This commit should show the React NoteList code

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./index.css";
 import App from "./components/App";
 
 ReactDOM.render(
