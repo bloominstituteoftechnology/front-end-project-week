27bdb28da737c287f3cfee35218b229bce96a3ad
Ken Barlow <kenbrlow@gmail.com>
2019-02-03 19:42:45 -0800

    Finished UI of the create note view.

--- lambda-notes/src/components/NoteCreate.js
+++ lambda-notes/src/components/NoteCreate.js
@@ -7,8 +7,14 @@ class NoteCreate extends Component {
       <div className="NoteCreateMain">
         <h1 className="NoteCreateTitle">Create New Note:</h1>
         <form className="NoteCreateForm">
-            <input className="NoteCreateInputTitle"></input>
-            <textarea className="NoteCreateTextAreaContent"></textarea>
+            <input 
+              className="NoteCreateInputTitle"
+              placeholder="Note Title"
+              />
+            <textarea 
+              className="NoteCreateTextAreaContent"
+              placeholder="Note Content"
+            />
             <button className="NoteCreateButtonSave" type="submit">Save</button>
         </form>
       </div>
