e30b86d9d8e43ebd49a2fdc000780c1a7f8ff014
Jess <jessdev83@gmail.com>
2019-02-06 12:35:18 -0500

    Build for netlfiy fixed

---
+++ notes/build/precache-manifest.030f87f8a42d56f10c8f6a261c4d708f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "16dc6735ccc7d1c939f0",
+    "url": "/static/css/main.88897b36.chunk.css"
+  },
+  {
+    "revision": "16dc6735ccc7d1c939f0",
+    "url": "/static/js/main.16dc6735.chunk.js"
+  },
+  {
+    "revision": "3e85f0a3e4a936643302",
+    "url": "/static/js/1.3e85f0a3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2bd1a6854fd70805918eaabe8650521c",
+    "url": "/index.html"
+  }
+];
