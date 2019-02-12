ab48286b8265293fa43d8c067f9c87c96c019fe7
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-02-11 23:01:39 -0500

    ADD SIDEBAR WITH VIEW/ADD NOTES

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Sidebar from './components/Sidebar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+        <aside>
+          <Sidebar />
+        </aside>
+
       </div>
     );
   }
---
+++ lambda-notes/src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SideBar = () => {
+    return (
+        <div>
+            <button>View Your Notes</button>
+            <button> + Create Your Note</button>
+        </div>
+    )
+}
+
+export default SideBar;
