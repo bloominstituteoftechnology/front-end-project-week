73b73077252e16357a9f5692c3718ec6f494696d
Joseph Thompson <joe.thompson838@gmail.com>
2019-02-12 00:31:43 -0500

    removed console errors when deploying to netlify

--- notes/src/components/Sorting/index.js
+++ notes/src/components/Sorting/index.js
@@ -1,80 +1,86 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import React from "react";
+import { connect } from "react-redux";
 
-import { alpha, alphaReverse, sortById, search } from '../../actions';
+import { alpha, alphaReverse, sortById, search } from "../../actions";
 
-import './sorting.css';
+import "./sorting.css";
 
 class Sorting extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      searchTerm: '',
-      csvData: ''
-    }
+      searchTerm: "",
+      csvData: ""
+    };
   }
-  inputHandler = (e) => {
-    const search = e.target.value
-    this.setState({searchTerm: search})
+  inputHandler = e => {
+    const search = e.target.value;
+    this.setState({ searchTerm: search });
 
-    return this.props.search(search)
-  }
+    return this.props.search(search);
+  };
 
   convertCSVNotes = () => {
     let headers = Object.keys(this.props.notes[0]);
     let results = [];
-    results.push(headers.join(','))
+    results.push(headers.join(","));
     this.props.notes.map(note => {
       const noteStr = Object.values(note);
-      results.push(noteStr.join(','));
-    })
-    results = results.join('\n');
-    this.setState({csvData: results})
-    console.log(results)
+      return results.push(noteStr.join(","));
+    });
+    results = results.join("\n");
+    this.setState({ csvData: results });
+    console.log(results);
     return this.exportCSV(results);
-  }
+  };
 
   exportCSV = data => {
-    const blob = new Blob([data], {type: 'text/csv'});
+    const blob = new Blob([data], { type: "text/csv" });
     const url = window.URL.createObjectURL(blob);
 
-    const a = document.createElement('a');
+    const a = document.createElement("a");
 
-    a.setAttribute('hidden', '');
-    a.setAttribute('href', url);
-    a.setAttribute('download', 'Lambda-Notes.csv');
+    a.setAttribute("hidden", "");
+    a.setAttribute("href", url);
+    a.setAttribute("download", "Lambda-Notes.csv");
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
-  }
+  };
 
-  render () {
+  render() {
     return (
-      <section className='sorting-container'>
-        <section className='search'>
+      <section className="sorting-container">
+        <section className="search">
           <p>Search</p>
-          <input type='text' onChange={this.inputHandler} value={this.state.searchTerm} />
+          <input
+            type="text"
+            onChange={this.inputHandler}
+            value={this.state.searchTerm}
+          />
         </section>
         <section>
           <button onClick={this.convertCSVNotes}>Export Notes</button>
         </section>
-        <section className='sorting'>
+        <section className="sorting">
           <p>Sort:</p>
           <select>
-            <option></option>
+            <option />
             <option onClick={this.props.alpha}>A-Z</option>
             <option onClick={this.props.alphaReverse}>Z-A</option>
             <option onClick={this.props.sortById}>By Id</option>
           </select>
         </section>
       </section>
-    )
+    );
   }
 }
 const mapStateToProps = state => {
-  return {notes: state.notesReducer.notes}
-}
+  return { notes: state.notesReducer.notes };
+};
 
-export default connect(mapStateToProps, { alpha, alphaReverse, sortById, search })(Sorting);
+export default connect(
+  mapStateToProps,
+  { alpha, alphaReverse, sortById, search }
+)(Sorting);
