5990d73fb30f7b6d5a2769b30acd1c4276dd4efd
Colin Dismuke <colin.dismuke@gmail.com>
2019-02-12 14:29:59 -0600

    Added reducers and outline of actions

---
+++ notes/src/actions/noteActions.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+export const LOADING = "LOADING";
+export const ERROR = "ERROR";
+export const GET_NOTES = "GET_NOTES";
+export const DELETE_NOTE = "DELETE_NOTE";
+export const UPDATE_NOTE = "UPDATE_NOTE";
+export const FETCH_NOTE = "FETCH_NOTE";
+export const ADD_NOTE = 'ADD_NOTE'
+
+const URL = "https://onedrousdev.herokuapp.com/api";
+
+export const fetchNotes = () => dispatch => {
+
+};
+
+export const fetchNote = id => dispatch => {
+
+};
+
+export const addNote = newNote => dispatch => {
+
+};
+
+export const deleteNote = id => dispatch => {
+
+};
+
+export const editNote = editedNote => dispatch => {
+
+};
+
+
