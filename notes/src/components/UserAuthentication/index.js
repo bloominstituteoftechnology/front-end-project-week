e707964b27879e768c95d4916755284f0f9c2772
Joseph Thompson <joe.thompson838@gmail.com>
2019-02-12 22:26:29 -0500

    started authentication

---
+++ notes/src/components/UserAuthentication/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class UserAuthentication extends React.component {
+  render() {
+    return (
+      <section>
+        <h1>Sign In</h1>
+      </section>
+    );
+  }
+}
+
+export default UserAuthentication;
