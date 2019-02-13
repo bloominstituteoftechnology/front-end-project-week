473067e22e2d53533f8620bf7a8a87a8429366dc
Kseniya Platonava <ksu2008@gmail.com>
2019-02-13 00:06:07 -0500

    Task 1
    A list of notes is displayed with styling

--- notes/src/components/Notes.js
+++ notes/src/components/Notes.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Nodes.css'
+import axios from 'axios';
 
-const Notes = props => {
+class Notes extends Component {
+  constructor(props){
+  super(props);
+  this.state ={
+    notes:[],
+    loading:true,
+  }  
+}
+componentDidMount() {
+  axios
+      .get(`https://fe-notes.herokuapp.com/note/get/all`)
+     
+      .then(response => {
+        console.log(response)
+          this.setState({notes:response.data,
+          loading:false,
+        })
+      })
+      .catch(err => console.log(err));
+
+}
+render() {
   return (
     <div className="contentContainer">
       <h2>Your Notes: </h2>
+      <div className="notesContainer">
+        {this.state.notes.map((note) => (
+          <div className="note">
+         
+            <h3>{note.title}</h3>
+            <p>{note.textBody}</p>   
+          </div>  
+        ))}           
+      </div>
+        
     </div>
   )
 }
-
+}
 export default Notes; 
