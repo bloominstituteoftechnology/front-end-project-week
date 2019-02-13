94a5ee74737ea29fcd1095211b6cbc67d2bb111e
Joseph Thompson <joe.thompson838@gmail.com>
2019-02-12 23:52:34 -0500

    added signin and signup forms

--- notes/src/components/SignIn/index.js
+++ notes/src/components/SignIn/index.js
@@ -1,26 +1,44 @@
 import React from "react";
-import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { Button, Form, FormGroup, Input } from "reactstrap";
 
 class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+  changeHandler = e => {
+    return this.setState({ [e.target.name]: e.target.value });
+  };
+  createUserObject = e => {
+    e.preventDefault();
+
+    const user = Object.assign({}, this.state);
+
+    return this.props.registerUser(user);
+  };
   render() {
     return (
       <Form>
         <FormGroup>
-          <Label for="exampleEmail">Email</Label>
           <Input
             type="email"
             name="email"
-            id="exampleEmail"
-            placeholder="with a placeholder"
+            placeholder="email"
+            onChange={this.changeHandler}
+            value={this.state.email}
           />
         </FormGroup>
         <FormGroup>
-          <Label for="examplePassword">Password</Label>
           <Input
             type="password"
             name="password"
-            id="examplePassword"
-            placeholder="password placeholder"
+            placeholder="password"
+            onChange={this.changeHandler}
+            value={this.state.password}
           />
         </FormGroup>
         <Button>Submit</Button>
--- notes/src/components/SignUp/index.js
+++ notes/src/components/SignUp/index.js
@@ -1,29 +1,67 @@
 import React from "react";
-import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { Button, Form, FormGroup, Input } from "reactstrap";
 
 class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      password2: ""
+    };
+  }
+  changeHandler = e => {
+    return this.setState({ [e.target.name]: e.target.value });
+  };
+  createUserObject = e => {
+    e.preventDefault();
+
+    const user = Object.assign({}, this.state);
+
+    return this.props.registerUser(user);
+  };
   render() {
     return (
-      <Form>
+      <Form onSubmit={this.createUserObject}>
         <FormGroup>
-          <Label for="exampleEmail">Email</Label>
+          <Input
+            type="text"
+            name="name"
+            placeholder="name"
+            onChange={this.changeHandler}
+            value={this.state.name}
+          />
+        </FormGroup>
+        <FormGroup>
           <Input
             type="email"
             name="email"
-            id="exampleEmail"
-            placeholder="with a placeholder"
+            placeholder="email"
+            onChange={this.changeHandler}
+            value={this.state.email}
           />
         </FormGroup>
         <FormGroup>
-          <Label for="examplePassword">Password</Label>
           <Input
             type="password"
             name="password"
-            id="examplePassword"
-            placeholder="password placeholder"
+            placeholder="password"
+            onChange={this.changeHandler}
+            value={this.state.password}
           />
         </FormGroup>
-        <Button>Submit</Button>
+        <FormGroup>
+          <Input
+            type="password"
+            name="password2"
+            placeholder="retype password"
+            onChange={this.changeHandler}
+            value={this.state.password2}
+          />
+        </FormGroup>
+        <Button type="submit">Submit</Button>
       </Form>
     );
   }
