5580d9f9febc1e184f16906cd3912b450d4e27e7
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-02-12 19:06:08 -0500

    ADD ROUTER AND WRAP THE APP

---
+++ lambda-notes/src/components/Note.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Note = props => {
+    return(
+        <div className="card">
+            <div className="card-title">
+                <h2>{props.note.title}</h2>
+            </div>
+            <div className="card-body">
+                <h3>{props.note.textBody}</h3>
+            </div>
+            <div className="card-tags">
+                {props.note.tags.map((tag, index) => {
+                    return (
+                        <span>{tag}</span>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default Note;
