1a8ac183be53bc40b3925c4e8b9cce7a9670aef7
Kseniya Platonava <ksu2008@gmail.com>
2019-02-16 14:35:59 -0500

    Working On EditNote

--- notes/src/components/EditNote.js
+++ notes/src/components/EditNote.js
@@ -12,7 +12,34 @@ class EditNote extends Component {
           loading: true
       }
   }
-
-
-  
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    this.fetchNote(id);
 }
+  updateTitleInputChangehandler
+  updateTeaxtBodyInputChangehandler
+  fetchNote 
+  submitEditedNote
+
+  render() {
+    return (
+      <div className="contentContainer">
+          <h2>Edit Note:</h2>
+        <form className="form" onSubmit={this.addNew}>
+          <input className="title" 
+                type='text' 
+                name='title'
+                onChange={this.updateTitleInputChangehandler}
+                value={this.state.noteToEdit.title} />
+  
+            <input className="textBody" 
+                type='text' 
+                name='textBody'
+                onChange={this.updateTeaxtBodyInputChangehandler}
+                value={this.state.textBody} />
+            <button className="button" type='submit'>Save</button>       
+        </form>
+      </div>
+    )}
+  }
+  export default EditNote;
