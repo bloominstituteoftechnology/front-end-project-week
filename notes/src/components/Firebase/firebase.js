00fd0e8a9725f3b5c284eb29e3917f26e32d7ef8
Colin Dismuke <colin.dismuke@gmail.com>
2019-02-19 14:58:21 -0600

    Added Firebase component and context provider

--- notes/src/components/Firebase/firebase.js
+++ notes/src/components/Firebase/firebase.js
@@ -1,3 +1,5 @@
+import app from 'firebase/app'
+
 const config = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -7,3 +9,10 @@ const config = {
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 
+class Firebase {
+    constructor() {
+        app.initializeApp(config)
+    }
+}
+
+export default Firebase;
--- notes/src/index.js
+++ notes/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import App from "./components/App/App";
 import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter as Router } from "react-router-dom";
+import Firebase, { FirebaseContext } from './components/Firebase'
 import thunk from "redux-thunk"
 import logger from 'redux-logger'
 import rootReducer from "./reducers"
@@ -15,10 +16,12 @@ const middleware = applyMiddleware(logger, thunk);
 const store = createStore(rootReducer, middleware);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
+  <FirebaseContext.Provider value={new Firebase()}>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </FirebaseContext.Provider>,
   document.getElementById("root")
 );
