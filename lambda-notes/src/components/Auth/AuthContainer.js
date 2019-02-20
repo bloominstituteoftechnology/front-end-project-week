667e7d0514cbf01d46ce1233b3e74306ce08620b
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-02-19 23:13:30 -0500

    added AuthContainer component

---
+++ lambda-notes/src/components/Auth/AuthContainer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+// import Login from './Login'
+// import Register from './Register'
+
+class AuthContainer extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default AuthContainer;
