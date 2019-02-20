6b66631239940fa6107a0d1d9cdeec8c09073193
Colin Dismuke <colin.dismuke@gmail.com>
2019-02-19 23:27:34 -0600

    Added forgot password component

--- notes/src/components/App/App.js
+++ notes/src/components/App/App.js
@@ -38,7 +38,7 @@ class App extends Component {
           {/* <Route path={ROUTES.LANDING} exact component={LandingPage} /> */}
           <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
           <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-          {/* <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} /> */}
+          <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
           {/* <Route path={ROUTES.ACCOUNT} component={AccountPage} /> */}
           {/* <Route path={ROUTES.ADMIN} component={AdminPage} /> */}
           <Route path={ROUTES.HOME} render={props => <Notes {...props} />} />
