d489ced08d459caa8f39bbc71390d565b8c3eac1
louislee7 <7.louislee@gmail.com>
2019-02-20 13:28:47 -0800

    added build command

---
+++ build/precache-manifest.c8206488f14e882d6ca1ea037517acac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e03374e926fd622cc31",
+    "url": "/static/css/main.33ecce89.chunk.css"
+  },
+  {
+    "revision": "2e03374e926fd622cc31",
+    "url": "/static/js/main.2e03374e.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "268e8a7385b07ac64103",
+    "url": "/static/js/2.268e8a73.chunk.js"
+  },
+  {
+    "revision": "f387d8369626444f7cb5a5324278ae38",
+    "url": "/static/media/tag.f387d836.ico"
+  },
+  {
+    "revision": "4acc73ae962ca0dc539aeb5727868e09",
+    "url": "/index.html"
+  }
+];
