74843ef16507002d3f79fd3ffb26a4a5464dd59a
Lloyd Napier <lloyd.a.napier@gmail.com>
2019-02-25 07:32:43 -0800

    Test

--- notes/src/index.js
+++ notes/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router} from 'react-router-dom'
 import Popup from 'react-popup';
 
