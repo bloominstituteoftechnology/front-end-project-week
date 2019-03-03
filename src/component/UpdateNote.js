017c955f5cf39b70c55f57355497dd875fb29815
Witanday <witanday.cd@gmail.com>
2019-03-02 21:52:31 -0500

    adding some changes on update componment

--- src/component/UpdateNote.js
+++ src/component/UpdateNote.js
@@ -20,13 +20,11 @@ class UpdateNote extends Component {
 
   handleUpdateNote = (ev) => {
    let id = this.props.id
-   console.log(this.props)
     ev.preventDefault();
-    
           axios
           //.put(`https://fe-notes.herokuapp.com/note/edit/${id}`,
           //.put(`http://localhost:3111/note/edit/${id}`,
-          .put(`https://witandaynoteserver.herokuapp.com/note/edit/${id}`,
+          .puty(`https://witandaynoteserver.herokuapp.com/note/edit/${id}`,
           {
             title:this.state.titleUpdate,
             textBody:this.state.textBodyUpdate,
@@ -60,7 +58,7 @@ render(){
   <div class="form-group">
     <textarea class="form-control textarea"  placeholder={this.props.textBody} name ="textBodyUpdate" onChange={this.handleInputChange2}></textarea>
   </div>
-  <Link to={`/Notes/${this.props.id}`} type ="submit" className="btn btnhome btn-primary"onClick={this.handleUpdateNote}>Update</Link>
+  <Link to={`/Notes/${this.props.id}`} type ="submit" className="btn btnhome btn-primary" onClick={this.handleUpdateNote}>Update</Link>
     <Link to={`/Notes/${this.props.id}`} className="btn btnhome btn-warning" >Cancel</Link>
   </form>
   </div>
