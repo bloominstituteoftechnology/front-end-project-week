ee5c7eeb068febe7192f8587b66c6922dd3b5544
Javier Alavrez <javieralvarez82@live.com>
2019-03-27 02:58:46 -0700

    cleaned up model

--- lambda_notes/src/components/Display_Components/DeleteModal.js
+++ lambda_notes/src/components/Display_Components/DeleteModal.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { deleteNote } from '../Actions/index';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import { Redirect, withRouter } from 'react-router-dom';
 import Modal from 'react-responsive-modal';
 import { DangerBtn, BtnStyle, ModalText, ButtonWrap } from '../Styles/DeleteModalStyle';
 
@@ -12,6 +12,7 @@ class DeleteModal extends Component {
     state = {
         deleteMessage: '',
         open: false,
+        deletedNote: false,
     }
 
     onOpenModal = () => {
@@ -25,15 +26,20 @@ class DeleteModal extends Component {
     onDelete = (e) => {
         e.preventDefault();
         this.props.deleteNote(this.props.id);
-         alert("Delete Successful");
+         this.setState({
+             deletedNote: true,
+         })
+        
     }
 
   render() {
-    const { open } = this.state;
+        if(this.state.deletedNote) {
+            return <Redirect to='/' />
+        }
       return (
             <div>
                 <div onClick={this.onOpenModal}>Delete</div>
-                <Modal closeIconSize={0} open={open}  center>
+                <Modal closeIconSize={0} open={"open"}  center>
                 <ModalText>Are you sure you want to delete this?</ModalText>
                     <ButtonWrap>
                         <DangerBtn onClick={this.onDelete} onClickCapture={this.onCloseModal}>Delete</DangerBtn>
