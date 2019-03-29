cab4513a4f93ea4e62428ff16623b6080d79416b
ckopecky <kopecky12112@gmail.com>
2019-03-29 16:29:41 -0700

    get rid of dummydata and refactor fakerdata.js

--- controllers/FakerData.js
+++ controllers/FakerData.js
@@ -1,17 +1,10 @@
 const faker = require("faker")
 
-const Notes = [];
-
-const Note = {
-    
-}
-
 const fakerData = () => {
-    for(i = 0; i < 100; i++) {
-        Notes.push({title: faker.lorem.words(5).toUpperCase(),
-        body: faker.lorem.sentences(10)});
+    return {
+    title: faker.lorem.words(5).toUpperCase(),
+    body: faker.lorem.sentences(10)
     }
-    return Notes;
 }
 
 module.exports = fakerData;
