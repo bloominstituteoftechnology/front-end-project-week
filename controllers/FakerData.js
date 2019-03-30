7c0d4970a620f6e6b0c23e1a146d12c6dac15352
ckopecky <kopecky12112@gmail.com>
2019-03-29 17:52:15 -0700

    Add faker uuid

--- controllers/FakerData.js
+++ controllers/FakerData.js
@@ -2,6 +2,7 @@ const faker = require("faker")
 
 const fakerData = () => {
     return {
+    id: faker.random.uuid(),
     title: faker.lorem.words(5).toUpperCase(),
     body: faker.lorem.sentences(10)
     }
