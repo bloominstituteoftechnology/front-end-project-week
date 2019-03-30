41127357a791960f54ea598d8fad06940b081007
ckopecky <kopecky12112@gmail.com>
2019-03-29 17:04:02 -0700

    Add notes prop

--- client/src/NoteList.js
+++ client/src/NoteList.js
@@ -2,18 +2,14 @@ import React, { Component } from 'react';
 import './index.css';
 import NoteCard from './NoteCard';
 
-export const Header = (props) =>{
-    return(
-        <div className="main-container-header single-note-header">
-            <h2>{props.text}</h2>
-        </div>
-    )
-}
 class NoteList extends Component {
     render() {
         return (
             <div className="notelist-wrapper">
-                <NoteCard text="Your Notes"/>
+                <NoteCard 
+                    text="Your Notes"
+                    notes={this.props.notes}
+                    />
             </div>
         );
     }
