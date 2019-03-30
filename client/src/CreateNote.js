43ca21894038c65e485e1d068be33c6450730e50
ckopecky <kopecky12112@gmail.com>
2019-03-29 17:58:59 -0700

    pass props to form

--- client/src/CreateNote.js
+++ client/src/CreateNote.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
-import './index.css';
-import Sidebar from './Sidebar';
-import { Header } from './NoteList';
+import './CreateNote.css';
+import { Header } from './Header';
 import Form from './Form';
 class CreateNote extends Component {
     render() {
         return (
             <div className="note-component-wrapper">
-               <div className="create-header">
-               <Header text="Create New Note" />
-               <Form />
-               </div>
+                <div className="create-header">
+                    <Header text="Create New Note" />
+                    <Form 
+                        handleChange={this.props.handleChange}
+                        title=""
+                        body=""
+                    />
+                </div>
             </div>
         );
     }
