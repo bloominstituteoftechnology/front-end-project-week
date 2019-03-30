8030db10771ccd36b9195c7d4b9a9174b04d6139
ckopecky <kopecky12112@gmail.com>
2019-03-29 17:04:18 -0700

    move CDM to App.js

--- client/src/NoteCard.js
+++ client/src/NoteCard.js
@@ -1,47 +1,27 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 
 class NoteCard extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            notes:[]
-        }
-    }
-
-    componentDidMount() {
-        let promise = axios.get("http://localhost:5555/api/notes/faker");
-
-        promise 
-            .then(response => {
-                console.log(response);
-                this.setState({notes: response.data});
-            })
-            .catch(error => {
-                console.log(error);
-            })
-    }
 
     render() {
-        // console.log(this.state.notes[0].dummydata);
+        console.log(this.props.notes);
         return (
             <div className="card-container">
                 <h2 className="main-container-header">{this.props.text}</h2>
                 <ul className="card-list">
-                  {/* {this.state.notes[0].dummydata.map(element =>{
+                  {this.props.notes.map(element =>{
                       return(
                           <li className="card" key={element.id}>
                          <Link 
-                            to={{pathname:`/notes/${element.id}`, state: this.state.notes}}>
-                            {element.note_title}
+                            to={{pathname:`/notes/${element.id}`, state: this.props.notes}}>
+                            {element.title}
                         </Link>
                             <hr/>
-                            {element.note_body}
+                            {element.body}
                           </li>
 
                       )
-                  })}   */}
+                  })}  
                 
                 </ul>
             </div>
