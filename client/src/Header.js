a16e7b81bea75c9ab7cbbe27cf82d2146edf3752
ckopecky <kopecky12112@gmail.com>
2019-04-04 15:03:29 -0700

    add class for register header

--- client/src/Header.js
+++ client/src/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 export const Header = (props) =>{
     return(
-        <div className="main-container-header single-note-header login-header-h2">
+        <div className="main-container-header single-note-header login-header-h2 register-header-h2">
             <h2>{props.text}<span> {props.span}</span></h2>
         </div>
     )
