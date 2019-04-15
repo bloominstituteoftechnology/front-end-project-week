872705141ba50e2659cbf4bf00cc7484af7f65df
David Loveday <35117916+AquilaVirtual@users.noreply.github.com>
2019-04-14 23:31:10 -0700

    Update User.js

--- src/components/User.js
+++ src/components/User.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 
 
-const backend = "https://lamb-notes.herokuapp.com/";
+const backend = "https://contact-notes.herokuapp.com/";
 // let backend = 'http://localhost:5000/';
 // let heroku = 'https://lamb-notes.herokuapp.com/';
 // if (typeof(backend) !== 'string') {
