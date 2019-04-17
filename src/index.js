94b0fc6808a7f5c0757583811786c7398cef7598
Eddy <edward.gonzalez9708@gmail.com>
2019-04-17 13:52:22 -0400

    Modified index.js to follow code styling guidelines.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
-registerServiceWorker();
+import App from './App'
+
+import './index.css'
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>, document.getElementById('root'))
+
+registerServiceWorker()
