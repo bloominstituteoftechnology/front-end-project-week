e13f1d28c876d741063ab6134d999c314085e7d8
Eddy <edward.gonzalez9708@gmail.com>
2019-04-19 14:32:19 -0400

    Add no-trailing-spaces property to the eslint rules list and modified the App componet to follow this new rule.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -15,28 +15,44 @@ import './index.css'
 
 class App extends Component {
   render() {
+    const TOKEN = localStorage.getItem('jwt')
+    const USER_ID = localStorage.getItem('userId')
     return (
       <div className="App">
         <Navigation />
-        {localStorage.getItem('jwt') && localStorage.getItem('userId')
-          ? <Route exact path='/:userId' component={Notes} /> 
-          : <Route exact path='/' component={About} />}
-        {localStorage.getItem('jwt') && localStorage.getItem('userId')
+        {TOKEN && USER_ID
+          ? <Route
+            exact path='/:userId'
+            component={Notes} />
+          : <Route
+            exact path='/'
+            component={About} />}
+        {TOKEN && USER_ID
           ? null
-          : <Route exact path='/signup' component={Signup} />}
-        {localStorage.getItem('jwt') && localStorage.getItem('userId')
+          : <Route
+            exact path='/signup'
+            component={Signup} />}
+        {TOKEN && USER_ID
           ? null
-          : <Route exact path='/login' component={Login} />}
-        {localStorage.getItem('jwt') && localStorage.getItem('userId')
-          ? <Route exact path='/:userId/createnote' component={CreateNote} /> 
+          : <Route
+            exact path='/login'
+            component={Login} />}
+        {TOKEN && USER_ID
+          ? <Route
+            exact path='/:userId/createnote'
+            component={CreateNote} />
           : null}
-        {localStorage.getItem('jwt') && localStorage.getItem('userId')
-          ? <Route exact path='/:userId/notes/:noteId' component={Note} />
+        {TOKEN && USER_ID
+          ? <Route
+            exact path='/:userId/notes/:noteId'
+            component={Note} />
           : null}
-        {localStorage.getItem('jwt') && localStorage.getItem('userId')
-          ? <Route exact path='/:userId/notes/:noteId/editnote' component={EditNote} />
+        {TOKEN && USER_ID
+          ? <Route
+            exact path='/:userId/notes/:noteId/editnote'
+            component={EditNote} />
           : null}
-        {localStorage.getItem('jwt') && localStorage.getItem('userId') && window.location.pathname === '/'
+        {TOKEN && USER_ID && window.location.pathname === '/'
           ? <Redirect to={`/${localStorage.getItem('userId')}`} />
           : null}
       </div>
