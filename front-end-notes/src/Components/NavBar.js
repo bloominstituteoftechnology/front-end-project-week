7b0c592aaa6cf662759697ed88e4adeebbec8666
tryingtokeepup <kl.hawaii@gmail.com>
2019-04-23 12:41:48 -0700

    Changed name of my rad project.

--- front-end-notes/src/Components/NavBar.js
+++ front-end-notes/src/Components/NavBar.js
@@ -1,36 +1,38 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 import './NavBar.css';
-
 
 const CoolNavBar = styled.div`
   border: 1px solid black;
   padding: 10px;
   width: 25%;
   font-size: 2rem;
-  
+
   display: flex;
   flex-direction: column;
   color: dark grey;
-  background: #D3D2D3;
+  background: #d3d2d3;
 `;
 
 const Header = styled.div`
-font-size: 6rem;
-font-weight: bold;
-line-height: 50px;
+  font-size: 6rem;
+  font-weight: bold;
+  line-height: 50px;
 `;
-
-
 
 const NavBar = props => {
   return (
     <CoolNavBar>
-      <Header>Lambda Notes</Header>
-      <NavLink className="NavLink" to="/notes">View Your Notes</NavLink>
-      <br></br>
-      <NavLink className="NavLink" to='/createNote'> + Create a Note! </NavLink>
+      <Header>Kai's Rad Notes</Header>
+      <NavLink className="NavLink" to="/notes">
+        View Your Notes
+      </NavLink>
+      <br />
+      <NavLink className="NavLink" to="/createNote">
+        {' '}
+        + Create a Note!{' '}
+      </NavLink>
     </CoolNavBar>
   );
 };
