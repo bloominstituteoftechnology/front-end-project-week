2f6e348370881bcbbf96064c69c9f52e59dac4cb
Sukhada Gholba <sukhadagholba@gmail.com>
2019-04-25 14:17:50 -0700

    edited onClick event

--- lambda-notes/src/components/ViewNotes/LambdaLeftDiv.js
+++ lambda-notes/src/components/ViewNotes/LambdaLeftDiv.js
@@ -6,7 +6,7 @@ const LambdaLeftDiv = (props) => {
 	return(
 		<div className="left-div">
 		<h1 className="lambda-notes-header">Notes App</h1>
-		<Link  onClick={(e)=>props.viewNotesHandler(e)}  style={{ textDecoration: 'none' }} to="/notes"><div className="teal-div">View Your Notes</div></Link>
+		<Link  onClick={props.viewNotesHandler}  style={{ textDecoration: 'none' }} to="/"><div className="teal-div">View Your Notes</div></Link>
 		<Link  onClick={props.createNotesReset} style={{ textDecoration: 'none' }} to="/add"><div className="teal-div">+ Create New Note</div></Link>
 		</div>
 	);
