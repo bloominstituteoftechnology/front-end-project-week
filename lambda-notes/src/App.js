7c55908761a8eeca5a364280a85a05abfe7ffce8
adrian <adrianadames@gmail.com>
2019-06-06 22:06:07 -0400

    Made host variable that will look to env for definition. This host variable represents the backend api host that we will make API calls to.

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 import NotesList from './Components/NotesList';
 import CreateNoteForm from './Components/CreateNoteForm';
