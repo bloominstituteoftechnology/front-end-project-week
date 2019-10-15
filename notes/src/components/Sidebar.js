f2009ab507d5d90005117f643e3a1f58997c035a
Angela La Salle <angela.lasalle91@gmail.com>
2019-10-15 01:29:04 -0400

    changed sidebar to load components based on loggedIn status

--- notes/src/components/Sidebar.js
+++ notes/src/components/Sidebar.js
@@ -1,36 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import { connect } from "react-redux";
+import { connect } from "react-redux";
 
-// import { logout } from "../actions/actions";
+import { logout } from "../actions/actions";
 
 import "../styles/Sidebar.css";
 
-export default class SideBar extends React.Component {
+class SideBar extends React.Component {
   render() {
     return (
       <div className="sidebar">
         <div className="sidebarWrapper">
           <h1 className="sidebarTitle">Lambda Notes</h1>
-          <div className="sidebarButton notesButton"><Link to="/">View Your Notes</Link></div>
-          <div className="sidebarButton notesButton"><Link to="/create">+ Create New Note</Link></div>
-          {/* <div className="sidebarButton notesButton"><Link to="/register">Register</Link></div>
-          <div className="sidebarButton notesButton"><Link to="/login">Login</Link></div>
-          <div className="sidebarButton notesButton" onClick={this.props.logout}><Link to="/">Logout</Link></div> */}
+          <div className="sidebarButton notesButton"><Link to="/">View All Notes</Link></div>
+          {this.props.loggedIn ? <div className="sidebarButton notesButton"><Link to="/my-notes">View Your Notes</Link></div> : null}
+          {this.props.loggedIn ? <div className="sidebarButton notesButton"><Link to="/create">+ Create New Note</Link></div> : null}
+          {this.props.loggedIn ? null : <div className="sidebarButton notesButton"><Link to="/register">Register</Link></div>}
+          {this.props.loggedIn ? null : <div className="sidebarButton notesButton"><Link to="/login">Login</Link></div>}
+          {this.props.loggedIn ? <div className="sidebarButton notesButton" onClick={this.props.logout}><Link to="/">Logout</Link></div> : null}
         </div>
       </div>
     );
   }
 }
 
-// const mapStateToProps = state => {
-//   return {
-//     registered: state.registered,
-//     loggedIn: state.loggedIn
-//   };
-// };
-// export default connect(
-//   mapStateToProps
-//   // ,
-//   // { logout }
-// )(SideBar);
+const mapStateToProps = state => {
+  return {
+    registered: state.registered,
+    loggedIn: state.loggedIn
+  };
+};
+export default connect(
+  mapStateToProps
+  ,
+  { logout }
+)(SideBar);
