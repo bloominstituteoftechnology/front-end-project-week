eb98efffb346be59cf6eef9696a77553e6479413
Angela La Salle <angela.lasalle91@gmail.com>
2019-10-15 01:26:26 -0400

    added withRouter to App so I could use connect"

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
 
 
 import SideBar from "./components/Sidebar";
@@ -10,25 +11,49 @@ import EditNote from "./components/EditNote";
 import Register from "./components/Register";
 import Login from "./components/Login";
 
+import {
+  returnUser
+} from "./actions/actions";
+
 import "./styles/App.css";
 
-const App = () => {
+
+class App extends React.Component {
+
+  componentDidMount() {
+    const token = localStorage.getItem("jwt")
+    return token 
+    ? this.props.returnUser(token)
+    : null
+
+  }
+  render() {
+    return (
+      <div className="App">
+        <SideBar />
+        <Route exact path="/" component={Notes} />
+        <Route exact path="/my-notes" component={Notes} />
+        <Route exact path="/create" component={CreateNote} />
+        <Route exact path="/notes/:id" render={props => <NotePage {...props} />} />
+        <Route
+          exact
+          path="/note/:id/edit"
+          render={props => <EditNote {...props} />}
+        />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </div>
+    )
+  }
   
-  return (
-    <div className="App">
-      <SideBar />
-      <Route exact path="/" component={Notes} />
-      <Route exact path="/create" component={CreateNote} />
-      <Route exact path="/notes/:id" render={props => <NotePage {...props} />} />
-      <Route
-        exact
-        path="/note/:id/edit"
-        render={props => <EditNote {...props} />}
-      />
-      <Route exact path="/register" component={Register} />
-      <Route exact path="/login" component={Login} />
-    </div>
-  );
 };
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    loggedIn: state.loggedIn
+  };
+};
+export default withRouter(connect(
+  mapStateToProps,
+  { returnUser }
+)(App));
