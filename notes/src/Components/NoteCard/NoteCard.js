4bb26b0aa6c238925f69802254aa96fa17c8e0ec
Mitchell Robles <mitchellr92@live.com>
2019-12-13 18:25:29 -0800

    working on display

--- notes/src/Components/NoteCard/NoteCard.js
+++ notes/src/Components/NoteCard/NoteCard.js
@@ -1,29 +1,30 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Menu from "../Menu/Menu";
+import "./NoteCard.css";
 
 class NoteCard extends Component {
   render() {
     const noteId = this.props.match.params.id;
     const notes = this.props.notes;
-    let note;
     let title;
     let body;
 
     for (let i = 0; i < notes.length; i++) {
       if (notes[i].id == noteId) {
-        note = notes[i];
         title = notes[i].title;
         body = notes[i].body;
       }
     }
-    console.log(title, body);
 
     return (
       <div className="container">
         <Menu />
         <div className="note-container">
-          <div className="notecard">{title}{body}</div>
+          <div className="notecard">
+            <h2>{title}</h2>
+            <div className="body">{body}</div>
+          </div>
         </div>
       </div>
     );
