67d9982fbcb3f5c6cdc8bcf00ca6107c39c10c1e
Mitchell Robles <mitchellr92@live.com>
2019-12-20 21:15:05 -0800

    tyring to get edit note to work

--- notes/src/Components/EditNote/EditNote.js
+++ notes/src/Components/EditNote/EditNote.js
@@ -1,40 +1,66 @@
-import React, { Component } from 'react';
-import Menu from '../Menu/Menu';
-import { Link } from 'react-router-dom';
-import './EditNote.css';
+import React, { Component } from "react";
+import Menu from "../Menu/Menu";
+import { Link } from "react-router-dom";
+import "./EditNote.css";
 
 class EditNote extends Component {
+  constructor(props) {
+    super(props);
 
-    render() {
-        console.log('props', this.props.editNote);
-        return (
-            <div className='edit-container'>
-                <Menu />
-                <div className='input-container'>
-                    <h2>
-                        Edit Note:
-                    </h2>
-                    <input
-                        className='new-note-title'
-                        placeholder='Note Title'
-                        name='title'
-                        onChange={this.inputHandleChange}
-                        // value={this.state.title}
-                    />
-                    <textarea
-                        className='note-content'
-                        placeholder='Note Content'
-                        name='body'
-                        onChange={this.inputHandleChange}
-                        // value={this.state.body}
-                    />
-                    <Link to='/' className='save-button' onClick={this.editNote}>
-                        Update
-                    </Link>
-                </div>
-            </div>
-        )
-    }
+    this.state = {
+      title: "",
+      body: ""
+    };
+  }
+
+  inputHandleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  updateNote = event => {
+    event.preventDefault();
+    const note = {
+      title: this.state.title,
+      body: this.state.body,
+      id: this.props.match.params.id
+    };
+
+    this.props.editNote(note);
+
+    this.setState({
+      title: "",
+      body: ""
+    });
+  };
+
+  render() {
+    console.log("props", this.props.editNote);
+    return (
+      <div className="edit-container">
+        <Menu />
+        <div className="input-container">
+          <h2>Edit Note:</h2>
+          <input
+            className="new-note-title"
+            placeholder="Note Title"
+            name="title"
+            onChange={this.inputHandleChange}
+            // value={this.state.title}
+          />
+          <textarea
+            className="note-content"
+            placeholder="Note Content"
+            name="body"
+            onChange={this.inputHandleChange}
+            // value={this.state.body}
+          />
+          <Link to="/" className="save-button" onClick={this.editNote}>
+            Update
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default EditNote;
