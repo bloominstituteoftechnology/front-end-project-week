3968d9b362b3bda84d9c213016ac045e49f02ed0
Daniel Lim <danheelim@gmail.com>
2020-02-18 19:39:45 -0800

    starting to change from styled components

--- lambda-notes/src/components/AllNotes.js
+++ lambda-notes/src/components/AllNotes.js
@@ -1,29 +1,30 @@
+//React
 import React, { Component } from 'react';
-
-//React 
 import axios from 'axios';
 import { Link } from "react-router-dom";
 import styled from 'styled-components';
 
+//CSS
+import '../CSS/Note.css'
 
 //Components
 import Notes from '../components/Notes';
 
 //styled components
-const MainNoteDiv = styled.div`
-    background-color: #e5e5e5;
-    width: 900px;
-`
+// const MainNoteDiv = styled.div`
+//     background-color: #e5e5e5;
+//     width: 900px;
+// `
     const MainNoteHeader = styled.div`
         margin: 30px 0px 0px 60px;
         font-weight: bold;
         font-size: 40px;
     `
-    const NoteContainer = styled.div`
-        display:flex;
-        flex-wrap:wrap;
-        justify-content: center;
-    `
+    // const NoteContainer = styled.div`
+    //     display:flex;
+    //     flex-wrap:wrap;
+    //     justify-content: center;
+    // `
         const Note = styled.div`
             background-color: white;
             margin: 10px 10px 10px 10px;
@@ -44,6 +45,7 @@ export default class AllNotes extends Component {
             .get("https://fe-notes.herokuapp.com/note/get/all")
             .then(response => {
                 this.setState(() => ({notes: response.data}))
+                // console.log(response)
             })
             .catch(error=> {
                 console.log(error)
@@ -60,9 +62,9 @@ export default class AllNotes extends Component {
         // console.log(this.props)
         // console.log(this.state.notes)
         return (
-            <MainNoteDiv>
+            <div className="NoteContainer">
                 <MainNoteHeader>Your Notes:</MainNoteHeader>
-                <NoteContainer>
+                <div className="main_note_div">
                     {this.state.notes.map(note => (
                         <Note key ={note._id}>
                             <NoteDetails
@@ -72,8 +74,8 @@ export default class AllNotes extends Component {
                                 />
                         </Note>
                     ))}
-                </NoteContainer>
-            </MainNoteDiv>
+                </div>
+            </div>
         )
     }
 }
--- lambda-notes/src/components/Modal.js
+++ lambda-notes/src/components/Modal.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
+// import '../CSS/Note.css'
 
 const ModalDiv = styled.div`
     width: 800px;
