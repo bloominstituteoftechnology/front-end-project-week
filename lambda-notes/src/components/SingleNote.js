8ac4ae0bd85240a6ed1ef20fd0eb75b37c75ea39
Daniel Lim <danheelim@gmail.com>
2020-02-19 20:11:10 -0800

    refactor singleNote pt2

--- lambda-notes/src/components/SingleNote.js
+++ lambda-notes/src/components/SingleNote.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 import axios from 'axios';
 import { Link } from "react-router-dom";
-import styled from 'styled-components';
 
 import '../CSS/Note.css'
 
@@ -11,31 +10,18 @@ import Notes from './Notes'
 import Modal from './Modal'
 
 
-const MainDiv = styled.div`
-    display:flex;
-    flex-direction:column;
-    flex-wrap:wrap;
-`
-const SingleNoteDiv = styled.div`
-    width: 800px;
-`
-
 
 export default class SingleNote extends Component {
-    // console.log(this.props)
     constructor(props) {
         super(props);
         this.state = {
             note: [],
             isOpen: false
-            
         }
-    
     }
 
     componentDidMount() {
         const id = this.props.match.params.id;
-        // console.log(id)
         this.fetchNote(id)
     }
     
@@ -51,9 +37,7 @@ export default class SingleNote extends Component {
         axios
             .get(`https://fe-notes.herokuapp.com/note/get/${id}`)
             .then(response => {
-                // console.log(this.state)
                 this.setState({ note: response.data })
-                // this.props.updateNote(this.state)
                
             })
             .catch(error => {
@@ -63,14 +47,9 @@ export default class SingleNote extends Component {
 
     deleteNote = () => {
         const id = this.props.match.params.id
-        // console.log(id)
-        // console.log(this.state)
         axios
             .delete(`https://fe-notes.herokuapp.com/note/delete/${id}`)
             .then(response => 
-                // console.log(response))
-                // this.state.history.push('/'))
-                // window.location.href="/"
                 this.returnHome()
 
                 )
@@ -83,12 +62,10 @@ export default class SingleNote extends Component {
         if(this.state.note.length===0) {
             return <div>Loading...</div>
         }
-        // console.log(this.state)
         return(
-            <MainDiv>
+            <div className="MainDiv">
                 <div>
                     <Modal show={this.state.isOpen} onClose={this.toggleModal} delete={this.deleteNote}>
-                   
                     </Modal>
                 </div>
 
@@ -97,17 +74,16 @@ export default class SingleNote extends Component {
                         <Link to={`/EditNote/${this.props.match.params.id}`}>
                             <button className="EditDeleteButton">Edit</button>
                         </Link>
-                
                         <button className="EditDeleteButton" onClick={this.toggleModal}>Delete</button>
                     </div>
                </div>
-                <SingleNoteDiv>
+                <div className="SingleNoteDiv">
                     <Notes
                         title = {this.state.note.title}
                         textBody = {this.state.note.textBody}
                         />
-                </SingleNoteDiv>
-            </MainDiv>
+                </div >
+            </div >
         )
     }
 }
